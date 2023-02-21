592cee9834c09ac1cb80b9e292e2a221c25d72f9
Palash Karia <palash.karia@gmail.com>
2023-02-21 20:50:03 +0530

    chore: fix mocksmodal
    - current bug: clicking on file or mock, both redirect to mock screen
    - uploading is allowed on rule page but it convoluted and doesn't refresh automatically

--- app/src/components/features/mocksV2/MockList/MocksModal/index.tsx
+++ app/src/components/features/mocksV2/MockList/MocksModal/index.tsx
@@ -1,22 +1,14 @@
-import { getMocks } from "backend/mocks/getMocks";
-import { fetchUserMocks } from "components/features/filesLibrary/FilesLibraryIndexPage/actions";
 import SpinnerCard from "components/misc/SpinnerCard";
-import React, { useCallback, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { getUserAuthDetails } from "store/selectors";
 import {
-  redirectToFileMockEditorEditMock,
   redirectToFileViewer,
   redirectToMockEditorEditMock,
   redirectToMocksList,
 } from "utils/RedirectionUtils";
-import { MockType, RQMockMetadataSchema } from "../../types";
-import {
-  oldFileMockToNewMockMetadataAdapter,
-  oldMockToNewMockMetadataAdapter,
-} from "../../utils/oldMockAdapter";
+import { GettingStartedWithMocks } from "../GettingStartedWithMocks";
 import MocksTable from "../MocksTable";
+import { useFetchMocks } from "../useFetchMocks";
 
 /* eslint-disable no-unused-vars */
 export enum MockListSource {
@@ -25,80 +17,12 @@ export enum MockListSource {
 /* eslint-enable no-unused-vars */
 
 interface Props {
-  source?: MockListSource;
-  type?: MockType;
   mockSelectionCallback?: (url: string) => void;
 }
 
-const MockListIndex: React.FC<Props> = ({
-  source = MockListSource.PICKER_MODAL,
-  mockSelectionCallback,
-  type,
-}) => {
+const MocksModal: React.FC<Props> = ({ mockSelectionCallback }) => {
   const navigate = useNavigate();
-  const user = useSelector(getUserAuthDetails);
-  const uid = user?.details?.profile?.uid;
-
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [mocksList, setMocksList] = useState<RQMockMetadataSchema[]>([]);
-  const [oldMocksList, setOldMocksList] = useState<RQMockMetadataSchema[]>([]);
-  // TODO: Remove this after all mocks are migrated to new schema
-  const fetchOldMocks = useCallback(() => {
-    fetchUserMocks().then((list: any[]) => {
-      let mocksData = [];
-      let filesData = [];
-      let i = 0;
-      for (i = 0; i < list.length; i++) {
-        if (list[i]?.isMock === true) {
-          mocksData.push(list[i]);
-        } else {
-          filesData.push(list[i]);
-        }
-      }
-
-      let adaptedData: RQMockMetadataSchema[] = [];
-      if (type === MockType.API) {
-        // mocksData
-        adaptedData = mocksData.map((oldData) => {
-          return oldMockToNewMockMetadataAdapter(uid, oldData);
-        });
-      } else if (type === MockType.FILE) {
-        // filesData
-        adaptedData = filesData.map((oldData) => {
-          return oldFileMockToNewMockMetadataAdapter(uid, oldData);
-        });
-      } else {
-        // mocksData + filesData
-        const mocksAdaptedData = mocksData.map((oldData) => {
-          return oldMockToNewMockMetadataAdapter(uid, oldData);
-        });
-        const filesAdaptedData = filesData.map((oldData) => {
-          return oldFileMockToNewMockMetadataAdapter(uid, oldData);
-        });
-        adaptedData = [...mocksAdaptedData, ...filesAdaptedData];
-      }
-      setOldMocksList([...adaptedData]);
-    });
-  }, [type, uid]);
-
-  const fetchMocks = useCallback(() => {
-    // API|FILE|null
-    getMocks(uid, type)
-      .then((data) => {
-        setMocksList(data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setMocksList([]);
-        setIsLoading(false);
-      });
-  }, [type, uid]);
-
-  useEffect(() => {
-    fetchMocks();
-    fetchOldMocks();
-  }, [fetchMocks, fetchOldMocks]);
-
+  const { mocksList, isLoading } = useFetchMocks();
   const handleCreateNewMock = () => {
     return redirectToMocksList(navigate, true);
   };
@@ -107,24 +31,13 @@ const MockListIndex: React.FC<Props> = ({
     handleEditAction(mockId, isOldMock);
   };
 
-  const handleItemSelect = (
-    mockId: string,
-    url: string,
-    isOldMock: boolean
-  ) => {
-    if (source === MockListSource.PICKER_MODAL) {
-      return handleSelectAction(url);
-    }
-
-    return handleEditAction(mockId, isOldMock);
+  const handleItemSelect = (mockId: string, url: string) => {
+    return handleSelectAction(url);
   };
 
   const handleEditAction = (mockId: string, isOldMock: boolean) => {
     if (isOldMock) {
       return redirectToFileViewer(navigate, mockId);
-    }
-    if (type === MockType.FILE) {
-      return redirectToFileMockEditorEditMock(navigate, mockId);
     }
     return redirectToMockEditorEditMock(navigate, mockId);
   };
@@ -135,18 +48,28 @@ const MockListIndex: React.FC<Props> = ({
 
   if (isLoading) {
     return <SpinnerCard customLoadingMessage="Loading Mocks" />;
-  } else {
+  }
+
+  if (mocksList.length === 0) {
     return (
-      <MocksTable
-        mocks={[...mocksList, ...oldMocksList]}
-        mockType={type}
-        handleItemSelect={handleItemSelect}
+      <GettingStartedWithMocks
+        mockType={""}
         handleCreateNew={handleCreateNewMock}
-        handleNameClick={handleNameClick}
-        handleSelectAction={handleSelectAction}
+        handleUploadAction={handleCreateNewMock}
       />
     );
   }
+
+  return (
+    <MocksTable
+      mocks={mocksList}
+      mockType={""}
+      handleItemSelect={handleItemSelect}
+      handleCreateNew={handleCreateNewMock}
+      handleNameClick={handleNameClick}
+      handleSelectAction={handleSelectAction}
+    />
+  );
 };
 
-export default MockListIndex;
+export default MocksModal;
