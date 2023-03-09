9b017bd17ffffcd398372e68323578f6fa2d13aa
rohanmathur91 <mathurrohan04@gmail.com>
2023-03-09 18:02:48 +0530

    add `getColorFromString` utility

---
+++ app/src/utils/getColorFromString.js
@@ -0,0 +1,13 @@
+export const getColorFromString = (
+  string = "#ffffff4d",
+  saturation = 60,
+  lightness = 40
+) => {
+  const hue = string.split("").reduce((hue, char) => {
+    let hash = char.charCodeAt(0) + ((hue << 5) - hue);
+    hash &= hash;
+    return hash;
+  }, 0);
+
+  return `hsl(${hue % 360}, ${saturation}%, ${lightness}%)`;
+};
