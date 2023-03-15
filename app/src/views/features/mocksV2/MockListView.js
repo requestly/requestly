8696758f61d0706e7b60a827102f688f57276553
nsrCodes <navdeepsinghrathore2000@gmail.com>
2023-03-15 21:56:38 +0530

    RQLY-1257 feat: put file and mock server behind feature flag

--- app/src/views/features/mocksV2/MockListView.js
+++ app/src/views/features/mocksV2/MockListView.js
@@ -1,8 +1,18 @@
 import React from "react";
 import MockListIndex from "components/features/mocksV2/MockList";
 import { MockType } from "components/features/mocksV2/types";
+import FEATURES from "config/constants/sub/features";
+import { useSelector } from "react-redux";
+import { getIsWorkspaceMode } from "store/features/teams/selectors";
+import TeamFeatureComingSoon from "components/landing/TeamFeatureComingSoon";
+import featureFlag from "utils/feature-flag";
 
 const MockListView = () => {
+  const isWorkspaceMode = useSelector(getIsWorkspaceMode);
+
+  if (isWorkspaceMode && !featureFlag.getValue(FEATURES.WORKSPACE_MOCKS, false))
+    return <TeamFeatureComingSoon title="Mock server" />;
+
   return <MockListIndex type={MockType.API} />;
 };
 
