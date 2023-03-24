9cdc68e277a9bd8466007bb45a0310fc95c30738
RuntimeTerror10 <parthb.git@gmail.com>
2023-03-24 09:56:54 +0530

    RQ-95:rollback ref install date

--- app/src/components/misc/PersonaSurvey/utils.ts
+++ app/src/components/misc/PersonaSurvey/utils.ts
@@ -3,7 +3,7 @@ import { multipleChoiceOption, Option } from "./types";
 
 export const shouldShowPersonaSurvey = async (appMode: string) => {
   const installDate = await getAndUpdateInstallationDate(appMode, false, false);
-  if (new Date(installDate) >= new Date("2020-03-06")) return true;
+  if (new Date(installDate) >= new Date("2023-03-06")) return true;
   else return false;
 };
 
