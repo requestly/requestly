92e61be85a59aaeea04a03b78c6f7381ab8553bc
Sagar Soni <sagar.soni@zoho.com>
2023-03-24 16:34:28 +0530

    Update posthog

--- app/src/modules/analytics/integrations/posthog.ts
+++ app/src/modules/analytics/integrations/posthog.ts
@@ -24,7 +24,7 @@ class PosthogIntegration implements IAnalyticsIntegration {
 
     // @ts-ignore
     if (!window.POSTHOG_INTEGRATION_DONE) {
-      posthog.init("phc_7D52fDZhcwbC2BRlC3Py7jSEGPWFoFStHk00xnx7zSg", {
+      posthog.init("phc_uNIYhy7rJ7GZF0H7aYtHvCOHIaCjB6wKICG9dkNUEDy", {
         api_host: "https://app.posthog.com",
         persistence: "memory",
         disable_session_recording: true,
