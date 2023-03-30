54e9bea4b4f25a007ef62c5cdbeeb7ae23f5d846
Rohan Mathur <mathurrohan04@gmail.com>
2023-03-30 11:28:05 +0530

    RQ-75: show docs on rule editor (#427)
    * add `Help` component
    
    * refactor types
    
    * fix(style): overflow
    
    * chore: base class
    
    * refactor: editor title props
    
    * feat: add rule editor help section toggle
    
    * fix: icons
    
    * fix: external links
    
    * add `Help` component
    
    * chore: add analytics
    
    * fix: `RQCollapse` expand icon
    
    * add faqs in docs sidebar
    
    * fix: docs content
    
    * fix: docs image zoom
    
    * fix: content scroll issue
    
    * refactor: flag name
    
    * fix(style): back btn
    
    * fix: hide docs in sharedList view
    
    * fix(style): help btn
    
    * chore: fix docs title + added docs close analytic event
    
    * refactor: get started section
    
    * fix: docs default open
    
    * fix: docs visible condition

--- app/src/components/features/rules/RuleBuilder/index.js
+++ app/src/components/features/rules/RuleBuilder/index.js
@@ -1,44 +1,46 @@
 import React, { useEffect, useState, useCallback, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
 import isEmpty from "is-empty";
+import { Button, Col, Row } from "antd";
 import { actions } from "../../../../../../app/src/store";
-//COMPONENTS
 import Header from "./Header";
 import Body from "./Body";
 import ChangeRuleGroupModal from "../ChangeRuleGroupModal";
 import SpinnerCard from "../../../misc/SpinnerCard";
 import CreateSharedListModal from "../../../../components/features/sharedLists/CreateSharedListModal";
-//CONSTANTS
 import APP_CONSTANTS from "../../../../config/constants";
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import { AUTH } from "modules/analytics/events/common/constants";
-//EXTERNALS
 import { StorageService } from "../../../../init";
-//ACTIONS
 import {
+  cleanup,
   getModeData,
+  getSelectedRules,
   setCurrentlySelectedRule,
   initiateBlankCurrentlySelectedRule,
   setCurrentlySelectedRuleConfig,
-  cleanup,
-  getSelectedRules,
 } from "./actions";
 import { fetchSharedLists } from "../../../../components/features/sharedLists/SharedListsIndexPage/actions";
-//UTILITIES
 import {
-  getAllRules,
   getAppMode,
-  getCurrentlySelectedRuleConfig,
-  getCurrentlySelectedRuleData,
-  getIsCurrentlySelectedRuleHasUnsavedChanges,
+  getAllRules,
   getUserAuthDetails,
+  getCurrentlySelectedRuleData,
+  getCurrentlySelectedRuleConfig,
+  getIsCurrentlySelectedRuleHasUnsavedChanges,
 } from "../../../../store/selectors";
 import * as RedirectionUtils from "../../../../utils/RedirectionUtils";
-import { useDispatch, useSelector } from "react-redux";
 import useExternalRuleCreation from "./useExternalRuleCreation";
 import Logger from "lib/logger";
-import { trackRuleEditorViewed } from "modules/analytics/events/common/rules";
+import {
+  trackDocsSidebarViewed,
+  trackRuleEditorViewed,
+} from "modules/analytics/events/common/rules";
 import { ProductWalkthrough } from "components/misc/ProductWalkthrough";
+import { ReactComponent as DownArrow } from "assets/icons/down-arrow.svg";
+import Help from "./Help";
+import "./RuleBuilder.css";
 
 //CONSTANTS
 const { RULE_EDITOR_CONFIG, RULE_TYPES_CONFIG } = APP_CONSTANTS;
@@ -66,6 +68,12 @@ const RuleBuilder = (props) => {
   const appMode = useSelector(getAppMode);
   const user = useSelector(getUserAuthDetails);
 
+  // TODO: use feature flag
+  const __IS_FEATURE_FLAG_ON__ = false;
+  const isRedirectRuleWithFeatureFlagOn =
+    currentlySelectedRuleData.ruleType ===
+      GLOBAL_CONSTANTS.RULE_TYPES.REDIRECT && __IS_FEATURE_FLAG_ON__;
+
   //References
   const isCleaningUpRef = useRef(false);
   //Component State
@@ -73,7 +81,6 @@ const RuleBuilder = (props) => {
   ruleSelection[currentlySelectedRuleData.id] = true;
 
   const [isShareRulesModalActive, setIsShareRulesModalActive] = useState(false);
-
   const [fetchAllRulesComplete, setFetchAllRulesComplete] = useState(false);
   const [
     isChangeRuleGroupModalActive,
@@ -83,6 +90,7 @@ const RuleBuilder = (props) => {
     getSelectedRules(ruleSelection)
   );
   const [startWalkthrough, setStartWalkthrough] = useState(false);
+  const [showDocs, setShowDocs] = useState(true);
 
   useExternalRuleCreation(MODE);
 
@@ -326,6 +334,9 @@ const RuleBuilder = (props) => {
     return <SpinnerCard renderHeader />;
   }
 
+  const isDocsVisible =
+    showDocs && isRedirectRuleWithFeatureFlagOn && !props.isSharedListViewRule;
+
   return (
     <>
       <ProductWalkthrough
@@ -343,10 +354,41 @@ const RuleBuilder = (props) => {
         />
       ) : null}
 
-      <Body
-        mode={MODE}
-        currentlySelectedRuleConfig={currentlySelectedRuleConfig}
-      />
+      <Row className="w-full relative">
+        <Col span={isDocsVisible ? 17 : 24}>
+          <Body
+            mode={MODE}
+            showDocs={isDocsVisible}
+            currentlySelectedRuleConfig={currentlySelectedRuleConfig}
+          />
+        </Col>
+
+        {!props.isSharedListViewRule && isRedirectRuleWithFeatureFlagOn ? (
+          <>
+            {!showDocs ? (
+              <Button
+                className="rule-editor-help-btn"
+                onClick={() => {
+                  setShowDocs(true);
+                  trackDocsSidebarViewed(currentlySelectedRuleData.ruleType);
+                }}
+              >
+                Help
+                <span>
+                  <DownArrow />
+                </span>
+              </Button>
+            ) : (
+              <Col span={7}>
+                <Help
+                  setShowDocs={setShowDocs}
+                  ruleType={currentlySelectedRuleData.ruleType}
+                />
+              </Col>
+            )}
+          </>
+        ) : null}
+      </Row>
 
       {/* Modals */}
       {isChangeRuleGroupModalActive ? (
