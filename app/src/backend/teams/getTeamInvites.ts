7896c4999d9fca4a6474fd8a707a4d02e1d40322
rohanmathur91 <mathurrohan04@gmail.com>
2023-03-31 12:31:37 +0530

    fix: invites order

--- app/src/backend/teams/getTeamInvites.ts
+++ app/src/backend/teams/getTeamInvites.ts
@@ -1,31 +1,33 @@
-import firebaseApp from "../../firebase";
 import {
   where,
   query,
+  orderBy,
   getDocs,
-  // Timestamp,
+  Timestamp,
   collection,
   getFirestore,
 } from "firebase/firestore";
-// import { InviteStatus, InviteType } from "types";
+import firebaseApp from "../../firebase";
+import { InviteStatus, InviteType, TeamInvite } from "types";
 
-export const getTeamInvites = async (email: string): Promise<any> => {
+export const getTeamInvites = async (
+  email: string
+): Promise<TeamInvite[] | null> => {
   const db = getFirestore(firebaseApp);
   const invitesRef = collection(db, "invites");
   const inviteQuery = query(
     invitesRef,
-    where("email", "==", email)
-    // where("type", "==", InviteType.teams),
-    // where("status", "==", InviteStatus.pending),
-    // where("expireTs", ">", Timestamp.now().toMillis())
+    where("email", "==", email),
+    where("type", "==", InviteType.teams),
+    where("status", "==", InviteStatus.pending),
+    where("expireTs", ">", Timestamp.now().toMillis()),
+    orderBy("expireTs", "desc"), // https://firebase.google.com/docs/firestore/query-data/order-limit-data#limitations
+    orderBy("createdTs", "desc")
   );
 
   const querySnapshot = await getDocs(inviteQuery);
 
-  console.log("------ logging ------ ::", querySnapshot.docs);
-  if (!querySnapshot.empty) {
-    return querySnapshot.docs;
-  } else {
-    return null;
-  }
+  return !querySnapshot.empty
+    ? querySnapshot.docs.map((doc) => doc.data() as TeamInvite)
+    : null;
 };
