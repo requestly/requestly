597151df2f33e4d366aa267d325f206156c056d2
Rohan Mathur <mathurrohan04@gmail.com>
2023-04-01 23:19:55 +0530

    RQ-102: direct joining workspace from app (#463)

--- app/src/modules/analytics/events/features/teams/index.js
+++ app/src/modules/analytics/events/features/teams/index.js
@@ -31,12 +31,31 @@ export const trackWorkspaceInviteLinkRevoked = (team_id) => {
   trackEvent(TEAMS.WORKSPACE_INVITE_LINK_REVOKED, params);
 };
 
-export const trackWorkspaceInviteAccepted = (team_id, invite_id, source, usage_type) => {
+export const trackWorkspaceInviteAccepted = (
+  team_id,
+  invite_id,
+  source,
+  usage_type
+) => {
   const params = { team_id, source, invite_id, usage_type };
   trackEvent(TEAMS.WORKSPACE_INVITE_ACCEPTED, params);
 };
 
-export const trackWorkspaceInviteScreenError = (error_type, team_id, invite_id) => {
+export const trackWorkspaceInviteScreenError = (
+  error_type,
+  team_id,
+  invite_id
+) => {
   const params = { team_id, invite_id, error_type };
   trackEvent(TEAMS.WORKSPACE_INVITE_SCREEN_ERROR, params);
+};
+
+export const trackWorkspaceJoiningModalOpened = (pending_invites) => {
+  const params = { pending_invites };
+  trackEvent(TEAMS.WORKSPACE_JOINING_MODAL_OPENED, params);
+};
+
+export const trackWorkspaceJoinClicked = (team_id, source) => {
+  const params = { team_id, source };
+  trackEvent(TEAMS.WORKSPACE_JOIN_CLICKED, params);
 };
