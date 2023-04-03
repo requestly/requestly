5c09de8b970334fa05231615bc72b84b03f3a1d3
rohanmathur91 <mathurrohan04@gmail.com>
2023-04-03 17:31:37 +0530

    add `FeatureCard` component

---
+++ app/src/components/features/rules/GettingStarted/PersonaRecommendation/FeatureCard/index.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { actions } from "store";
+import APP_CONSTANTS from "config/constants";
+import { AUTH } from "modules/analytics/events/common/constants";
+import { Feature } from "../types";
+import "./FeatureCard.css";
+
+interface Props extends Feature {
+  isUserLoggedIn: boolean;
+}
+
+export const FeatureCard: React.FC<Props> = ({
+  icon,
+  title,
+  subTitle,
+  link,
+  isUserLoggedIn,
+}) => {
+  const Icon = icon;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const showAuthModal = useCallback(
+    (callback: () => void) => {
+      dispatch(
+        actions.toggleActiveModal({
+          modalName: "authModal",
+          newValue: true,
+          newProps: {
+            callback,
+            userActionMessage: "Please sign up.",
+            src: APP_CONSTANTS.FEATURES.RULES,
+            authMode: APP_CONSTANTS.AUTH.ACTION_LABELS.SIGN_UP,
+            eventSource: AUTH.SOURCE.PERSONA_RECOMMENDATION_SCREEN,
+          },
+        })
+      );
+    },
+    [dispatch]
+  );
+
+  const handleNavigation = (e: React.MouseEvent<HTMLElement>) => {
+    if (isUserLoggedIn) {
+      navigate(link);
+    } else {
+      showAuthModal(() => navigate(link));
+    }
+  };
+
+  return (
+    <div className="feature-card" onClick={handleNavigation}>
+      <div className="feature-title-wrapper">
+        <span className="feature-icon">{<Icon />}</span>
+        <span className="feature-title">{title}</span>
+      </div>
+      <div className="feature-subtitle">{subTitle}</div>
+    </div>
+  );
+};
