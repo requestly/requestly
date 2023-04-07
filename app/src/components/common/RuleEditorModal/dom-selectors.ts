af42e1b3b4851cdbc0908fa7d81dfd630c071f89
rohanmathur91 <mathurrohan04@gmail.com>
2023-04-07 16:45:30 +0530

    rename dom selectors file

--- app/src/components/common/RuleEditorModal/dom-selectors.ts
+++ app/src/components/common/RuleEditorModal/dom-selectors.ts
@@ -1,5 +1,9 @@
 import { RuleType } from 'types';
 
+/**
+ * @description  Use it as 'input[data-selectionid="value"]' to focus rule editor fields
+ */
+
 export const RULE_EDITOR_FIELD_SELECTOR = {
   [RuleType.HEADERS]: 'header-name',
   [RuleType.REDIRECT]: 'destination-url',
