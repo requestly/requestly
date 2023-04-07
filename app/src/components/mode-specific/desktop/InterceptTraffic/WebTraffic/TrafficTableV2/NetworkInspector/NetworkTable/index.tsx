2891164cc11859871d92dd83ca7af43ef6559416
rohanmathur91 <mathurrohan04@gmail.com>
2023-04-07 10:26:56 +0530

    rebase

--- app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/NetworkInspector/NetworkTable/index.tsx
+++ app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/NetworkInspector/NetworkTable/index.tsx
@@ -11,11 +11,6 @@ import { ProductWalkthrough } from 'components/misc/ProductWalkthrough';
 import FEATURES from 'config/constants/sub/features';
 import { ContextMenu } from '../ContextMenu';
 
-import { getColumnKey } from '../utils';
-import { VirtualTable } from './VirtualTable';
-import AppliedRules from '../../Tables/columns/AppliedRules';
-import { ContextMenu } from '../ContextMenu';
-
 export const ITEM_SIZE = 30;
 
 interface Props {
@@ -24,6 +19,9 @@ interface Props {
 }
 
 const NetworkTable: React.FC<Props> = ({ logs, onRow }) => {
+  const dispatch = useDispatch();
+  const isTrafficTableTourCompleted = useSelector(getIsTrafficTableTourCompleted);
+
   const columns = [
     {
       id: 'time',
@@ -97,7 +95,7 @@ const NetworkTable: React.FC<Props> = ({ logs, onRow }) => {
 
     return (
       <ContextMenu log={log}>
-        <Table.Row id={log.id} {...rowProps}>
+        <Table.Row id={log.id} {...rowProps} data-tour-id={index === 0 ? 'traffic-table-row' : null}>
           {columns.map((column: any) => {
             const columnData = _.get(log, getColumnKey(column?.dataIndex));
 
