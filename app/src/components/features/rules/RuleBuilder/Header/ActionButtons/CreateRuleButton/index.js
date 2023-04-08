55f56fc7500dd018df5f20ab2917b438fa2c9cdf
rohanmathur91 <mathurrohan04@gmail.com>
2023-04-08 15:13:30 +0530

    remove unused import

--- app/src/components/features/rules/RuleBuilder/Header/ActionButtons/CreateRuleButton/index.js
+++ app/src/components/features/rules/RuleBuilder/Header/ActionButtons/CreateRuleButton/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Tooltip } from "antd";
 import { RQButton } from "lib/design-system/components";
