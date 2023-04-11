50cd568fdd67128d5c004ab258bc4667a0370405
Nafees Nehar <nafees87n@gmail.com>
2023-04-11 13:54:57 +0530

    RQ-145: connect apps CTA placeholder in empty table

--- app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/Tables/GroupByNone.js
+++ app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/Tables/GroupByNone.js
@@ -1,30 +1,19 @@
 import React from "react";
-import { Button, Col, Row } from "antd";
+import { Button, Col, Row, Space, Typography } from "antd";
 import { useNavigate } from "react-router-dom";
 
 import { redirectToApps } from "utils/RedirectionUtils";
 import NetworkInspector from "components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/NetworkInspector";
+import { RQButton } from "lib/design-system/components";
 
-const GroupByNone = ({
-  requestsLog,
-  handleRowClick,
-  emptyCtaText,
-  emptyCtaAction,
-  emptyDesc,
-}) => {
+const GroupByNone = ({ requestsLog, handleRowClick, emptyCtaText, emptyCtaAction, emptyDesc }) => {
   const navigate = useNavigate();
 
   const renderNoTrafficCTA = () => {
     if (emptyCtaAction && emptyCtaText) {
       return (
         <>
-          <Button
-            type="primary"
-            href={emptyCtaAction}
-            target="_blank"
-            style={{ margin: 8 }}
-            size="small"
-          >
+          <Button type="primary" href={emptyCtaAction} target="_blank" style={{ margin: 8 }} size="small">
             {emptyCtaText}
           </Button>
           <p>{emptyDesc}</p>
@@ -34,16 +23,14 @@ const GroupByNone = ({
 
     return (
       <Row justify={"center"}>
-        <Col style={{ textAlign: "center" }}>
-          <Button
-            type="primary"
-            shape="round"
-            onClick={() => redirectToApps(navigate)}
-            style={{ margin: 8 }}
-          >
-            Connect App
-          </Button>
-          <p>Connect an App to start intercepting traffic</p>
+        <Col>
+          <Space direction="vertical" align="center">
+            <Typography.Text>Connect apps to start intercepting traffic</Typography.Text>
+            <RQButton type="primary">Connect Apps</RQButton>
+            <Typography.Text>Or</Typography.Text>
+            <Typography.Text>Capture all the requests from this device</Typography.Text>
+            <RQButton>Enable Requestly Everywhere</RQButton>
+          </Space>
         </Col>
       </Row>
     );
