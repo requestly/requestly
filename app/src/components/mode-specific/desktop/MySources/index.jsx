e0da62be0ac7866ce3fb6e86e56ba453160bf162
Nafees Nehar <nafees87n@gmail.com>
2023-04-12 20:04:19 +0530

    chore: cleanup app sources routes

--- app/src/components/mode-specific/desktop/MySources/index.jsx
+++ app/src/components/mode-specific/desktop/MySources/index.jsx
@@ -1,42 +1,21 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
-// SUB COMPONENTS
-import SpinnerColumn from "../../../misc/SpinnerColumn";
 // UTILS
-import {
-  getAppMode,
-  getDesktopSpecificDetails,
-} from "../../../../store/selectors";
-import { redirectToRules } from "../../../../utils/RedirectionUtils";
+import { getAppMode } from "../../../../store/selectors";
+import { redirectToRules, redirectToTraffic } from "../../../../utils/RedirectionUtils";
 // CONSTANTS
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
-import ProCard from "@ant-design/pro-card";
-import Sources from "./Sources";
 import { useSelector } from "react-redux";
 
 const MySources = () => {
   const navigate = useNavigate();
 
-  //Global State
   const appMode = useSelector(getAppMode);
-  const desktopSpecificDetails = useSelector(getDesktopSpecificDetails);
-  const { isBackgroundProcessActive } = desktopSpecificDetails;
 
   if (appMode === GLOBAL_CONSTANTS.APP_MODES.EXTENSION) {
     redirectToRules(navigate);
   }
 
-  if (!isBackgroundProcessActive) {
-    return <SpinnerColumn message="Loading background process" />;
-  }
-
-  return (
-    <React.Fragment>
-      <ProCard className="primary-card github-like-border">
-        <Sources />
-      </ProCard>
-    </React.Fragment>
-  );
+  redirectToTraffic(navigate);
 };
 
 export default MySources;
