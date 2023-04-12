a79e0728069e4924fc4ce76f8aa13dafabb6173e
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2023-04-13 00:55:52 +0530

    RQ-33 sync persona data with firebase (#469)
    * RQ-33 sync persona data with firbase
    
    * refactor google auth
    
    * refactor email signup/signin
    
    * chore:cleanup
    
    * chore:minor fix
    
    * chore:refactor
    
    * chore:fix

--- app/src/components/misc/PersonaSurvey/utils.ts
+++ app/src/components/misc/PersonaSurvey/utils.ts
@@ -1,5 +1,7 @@
+import { actions } from "store";
 import { getAndUpdateInstallationDate } from "utils/Misc";
-import { multipleChoiceOption, Option } from "./types";
+import { multipleChoiceOption, Option, UserPersona } from "./types";
+import { getValueAsPromise, updateValue } from "actions/FirebaseActions";
 
 export const shouldShowPersonaSurvey = async (appMode: string) => {
   const installDate = await getAndUpdateInstallationDate(appMode, false, false);
@@ -21,4 +23,18 @@ export const shuffleOptions = (options: Option[]) => {
     [options[i], options[j]] = [options[j], options[i]];
   }
   return options;
+};
+
+export const syncUserPersona = async (
+  uid: string,
+  dispatch: any,
+  userPersona: UserPersona
+) => {
+  const profile: any = await getValueAsPromise(["users", uid, "profile"]);
+  if (!profile) {
+    return;
+  }
+  if (profile.userPersona)
+    dispatch(actions.setUserPersonaData(profile.userPersona));
+  else updateValue(["users", uid, "profile"], { userPersona });
 };
