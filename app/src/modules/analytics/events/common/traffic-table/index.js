f927c887de09f001d5aca6a640f19b4ec4dd9e7c
Rohan Mathur <mathurrohan04@gmail.com>
2023-04-17 15:40:44 +0530

    chore: add traffic table sidebar filter analytics (#543)

---
+++ app/src/modules/analytics/events/common/traffic-table/index.js
@@ -0,0 +1,17 @@
+import { trackEvent } from "modules/analytics";
+import { TRAFFIC_TABLE } from "../constants";
+
+export const trackSidebarFilterExpanded = (source, filter_type) => {
+  const params = { source, filter_type };
+  trackEvent(TRAFFIC_TABLE.SIDEBAR_FILTER_EXPANDED, params);
+};
+
+export const trackSidebarFilterCollapsed = (source, filter_type) => {
+  const params = { source, filter_type };
+  trackEvent(TRAFFIC_TABLE.SIDEBAR_FILTER_COLLAPSED, params);
+};
+
+export const trackSidebarFilterSelected = (source, filter_type, name) => {
+  const params = { source, filter_type, name };
+  trackEvent(TRAFFIC_TABLE.SIDEBAR_FILTER_SELECTED, params);
+};
