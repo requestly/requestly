13d26e4e82ac8be32fdc30369e1a2ed02b783ebf
Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>
2023-04-20 00:19:23 +0530

    feat: rule execution events (#538)

--- browser-extension/mv2/src/client/js/ruleExecutionHandler.js
+++ browser-extension/mv2/src/client/js/ruleExecutionHandler.js
@@ -2,11 +2,27 @@ RQ.RuleExecutionHandler = {
   appliedRuleIds: new Set(),
 };
 
+RQ.RuleExecutionHandler.sendRuleExecutionEvent = (rule) => {
+  const eventName = "rule_executed";
+  const eventParams = {
+    rule_type: rule.ruleType,
+    rule_id: rule.id,
+    platform: "extension",
+  };
+
+  RQ.ClientUtils.sendEventToBackground(eventName, eventParams);
+};
+
 RQ.RuleExecutionHandler.setup = () => {
   chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
     switch (message.action) {
-      case RQ.CLIENT_MESSAGES.UPDATE_APPLIED_RULE_ID:
-        RQ.RuleExecutionHandler.appliedRuleIds.add(message.ruleId);
+      case RQ.CLIENT_MESSAGES.NOTIFY_RULE_APPLIED:
+        const isFirstExecution = !RQ.RuleExecutionHandler.appliedRuleIds.has(message.rule.id);
+        RQ.RuleExecutionHandler.appliedRuleIds.add(message.rule.id);
+
+        if (isFirstExecution) {
+          RQ.RuleExecutionHandler.sendRuleExecutionEvent(message.rule);
+        }
         break;
 
       case RQ.CLIENT_MESSAGES.GET_APPLIED_RULE_IDS:
@@ -24,12 +40,15 @@ RQ.RuleExecutionHandler.setup = () => {
 
 RQ.RuleExecutionHandler.syncCachedAppliedRules = (appliedRuleDetails, isConsoleLoggerEnabled) => {
   appliedRuleDetails.forEach((appliedRule) => {
+    const isFirstExecution = !RQ.RuleExecutionHandler.appliedRuleIds.has(appliedRule.id);
     RQ.RuleExecutionHandler.appliedRuleIds.add(appliedRule.rule.id);
-
     RQ.ConsoleLogger.handleMessage({
       requestDetails: appliedRule.requestDetails,
       rule: appliedRule.rule,
       isConsoleLoggerEnabled,
     });
+    if (isFirstExecution) {
+      RQ.RuleExecutionHandler.sendRuleExecutionEvent(appliedRule.rule);
+    }
   });
 };
