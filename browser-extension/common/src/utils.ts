103797eebef1a250a2cd2596ccb2e18f02898c4a
Sagar Soni <sagar.soni@zoho.com>
2023-04-20 20:09:26 +0530

    [extension] remove unused fn: getLastUpdatedTS

--- browser-extension/common/src/utils.ts
+++ browser-extension/common/src/utils.ts
@@ -1,14 +1,10 @@
 import { v4 as uuid } from "uuid";
 import { STORAGE_KEYS } from "./constants";
-import { getRecord, saveRecord } from "./storage";
+import { saveRecord } from "./storage";
 
 export function generateUUID() {
   return uuid();
 }
-
-export const getLastUpdatedTS = () => {
-  return getRecord(STORAGE_KEYS.LAST_UPDATED_TS);
-};
 
 export const updateLastUpdatedTS = () => {
   return saveRecord(STORAGE_KEYS.LAST_UPDATED_TS, Date.now());
