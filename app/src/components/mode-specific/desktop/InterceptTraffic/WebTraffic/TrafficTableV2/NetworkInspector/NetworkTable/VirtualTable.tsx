64927d0d1d26c941b7d02af2cad7a5f5b219435f
Sahil Gupta <sahil865gupta@gmail.com>
2023-04-28 13:29:36 +0530

    testing react window fix

--- app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/NetworkInspector/NetworkTable/VirtualTable.tsx
+++ app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/NetworkInspector/NetworkTable/VirtualTable.tsx
@@ -2,7 +2,7 @@
  * Contains code for @devtools-ds/table with react-window
  */
 import { Table } from "@devtools-ds/table";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState, useRef, useContext } from "react";
 import { FixedSizeList, FixedSizeListProps } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
@@ -38,6 +38,17 @@ export const VirtualTable = ({
 } & Omit<FixedSizeListProps, "children" | "innerElementType">) => {
   const listRef = useRef<FixedSizeList | null>();
   const [top, setTop] = useState(0);
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  const usePrevious = (value: any) => {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  };
+
+  const prevTop = usePrevious(top);
 
   return (
     <VirtualTableContext.Provider value={{ top, setTop, header, footer, selectedRowData }}>
@@ -45,21 +56,34 @@ export const VirtualTable = ({
         {({ height, width }) => (
           <FixedSizeList
             {...rest}
+            overscanCount={15}
             height={height}
             width={width}
             innerElementType={Inner}
+            onScroll={(props) => 
+              {console.log(props.scrollDirection); setScrollDirection(props.scrollDirection);}
+            }
             onItemsRendered={(props) => {
               const style =
                 listRef.current &&
                 // @ts-ignore private method access
                 listRef.current._getItemStyle(props.overscanStartIndex);
+                console.log(props, scrollDirection);
+                console.log(prevTop, top, style?.top);
+                
+                // if(prevTop !== style?.top && props.overscanStopIndex !== props.visibleStopIndex) {
+                if(true) {
+                  // Fix resizing making empty because of listRef being empty
+                  console.log("SetTop");
+                  if (style) {
+                    setTop(style.top);
+                  } else {
+                    setTop(top || 0);
+                  }
+                }else {
+                  console.log("Prev top and style?.top same")
+                }
 
-              // Fix resizing making empty because of listRef being empty
-              if (style) {
-                setTop(style.top);
-              } else {
-                setTop(top || 0);
-              }
               // Call the original callback
               rest.onItemsRendered && rest.onItemsRendered(props);
             }}
@@ -103,6 +127,8 @@ const Inner = React.forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>(
   // Prevent flickering due to alternating colors
   const isFirstElementEvenIndex = children?.[0]?.props?.index % 2 === 0;
 
+  console.log("rerender")
+
   return (
     <div {...rest} ref={ref}>
       <Table
@@ -124,6 +150,54 @@ const Inner = React.forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>(
           <Table.Body>
             {isFirstElementEvenIndex ? null : <tr style={{}}></tr>}
             {children}
+            <tr style={{}}></tr>
+            <tr style={{}}></tr>
+          </Table.Body>
+        </ContextMenu>
+        {footer}
+      </Table>
+    </div>
+  );
+});
+
+
+const Outer = React.forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>(function Outer(
+  { children, ...rest }: any,
+  ref
+) {
+  // To higlight the row
+  const [selected, setSelected] = useState(null);
+
+  const { header, footer, top, selectedRowData } = useContext(VirtualTableContext);
+
+  // Prevent flickering due to alternating colors
+  const isFirstElementEvenIndex = children?.[0]?.props?.index % 2 === 0;
+
+  console.log("rerender")
+
+  return (
+    <div {...rest} ref={ref}>
+      <Table
+        style={{
+          top,
+          position: "absolute",
+          width: "100%",
+          cursor: "pointer",
+        }}
+        selected={selected}
+        onSelected={(id) => {
+          setSelected(id);
+        }}
+        //@ts-ignore
+        onContextMenu={(e) => setSelected(e.target?.parentElement.id)}
+      >
+        {header}
+        <ContextMenu log={selectedRowData}>
+          <Table.Body>
+            {isFirstElementEvenIndex ? null : <tr style={{}}></tr>}
+            {children}
+            <tr style={{}}></tr>
+            <tr style={{}}></tr>
           </Table.Body>
         </ContextMenu>
         {footer}
