17652afe75825005d931f1a4749de9fd718f1eae
Sahil Gupta <sahil865gupta@gmail.com>
2023-04-30 16:20:06 +0530

    fix: network table flickering issues fix

--- app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/NetworkInspector/NetworkTable/index.tsx
+++ app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/NetworkInspector/NetworkTable/index.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { actions } from "store";
 import { getIsTrafficTableTourCompleted } from "store/selectors";
 import { Table } from "@devtools-ds/table";
 import _ from "lodash";
 import { getColumnKey } from "../utils";
-import { VirtualTable } from "./VirtualTable";
 import AppliedRules from "../../Tables/columns/AppliedRules";
 import { ProductWalkthrough } from "components/misc/ProductWalkthrough";
 import FEATURES from "config/constants/sub/features";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import AutoSizer from "react-virtualized-auto-sizer";
+import "./index.css";
+import { ContextMenu } from "../ContextMenu";
 
 export const ITEM_SIZE = 30;
 
@@ -16,68 +19,82 @@ interface Props {
   logs: any;
   onRow: Function;
 }
+const columns = [
+  {
+    id: "time",
+    title: "Time",
+    dataIndex: "timestamp",
+    width: "7%",
+    render: (timestamp: any) => {
+      return new Date(timestamp * 1000).toLocaleTimeString(undefined, {
+        hour12: false,
+      });
+    },
+  },
+  {
+    id: "url",
+    title: "URL",
+    dataIndex: "url",
+    width: "48%",
+  },
+  {
+    id: "method",
+    title: "Method",
+    dataIndex: ["request", "method"], // corresponds to request.method
+    width: "5%",
+  },
+  {
+    id: "contentType",
+    title: "Content-Type",
+    dataIndex: ["response", "contentType"],
+    width: "10%",
+  },
+  {
+    title: "Rules Applied",
+    dataIndex: ["actions"],
+    width: "10%",
+    responsive: ["xs", "sm"],
+    render: (actions: any) => {
+      if (!actions || actions === "-" || actions.length === 0) {
+        return "-";
+      }
+      return <AppliedRules actions={actions} />;
+    },
+  },
+  {
+    id: "status",
+    title: "Status",
+    dataIndex: ["response", "statusCode"],
+    width: "5%",
+  },
+];
 
 const NetworkTable: React.FC<Props> = ({ logs, onRow }) => {
+  const [selected, setSelected] = useState(null); // Selection for ds-devtools
   const [selectedRowData, setSelectedRowData] = useState({});
   const dispatch = useDispatch();
   const isTrafficTableTourCompleted = useSelector(getIsTrafficTableTourCompleted);
 
-  const columns = [
-    {
-      id: "time",
-      title: "Time",
-      dataIndex: "timestamp",
-      width: "7%",
-      render: (timestamp: any) => {
-        return new Date(timestamp * 1000).toLocaleTimeString(undefined, {
-          hour12: false,
-        });
-      },
-    },
-    {
-      id: "url",
-      title: "URL",
-      dataIndex: "url",
-      width: "48%",
-    },
-    {
-      id: "method",
-      title: "Method",
-      dataIndex: ["request", "method"], // corresponds to request.method
-      width: "5%",
-    },
-    {
-      id: "contentType",
-      title: "Content-Type",
-      dataIndex: ["response", "contentType"],
-      width: "10%",
-    },
-    {
-      title: "Rules Applied",
-      dataIndex: ["actions"],
-      width: "10%",
-      responsive: ["xs", "sm"],
-      render: (actions: any) => {
-        if (!actions || actions === "-" || actions.length === 0) {
-          return "-";
-        }
-        return <AppliedRules actions={actions} />;
-      },
-    },
-    {
-      id: "status",
-      title: "Status",
-      dataIndex: ["response", "statusCode"],
-      width: "5%",
-    },
-  ];
+  const parentRef = useRef(null);
+
+  const rowVirtualizer = useVirtualizer({
+    count: logs.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => ITEM_SIZE,
+    overscan: 5,
+  });
+
+  // https://github.com/bvaughn/react-window/issues/60#issuecomment-781540658
+  const items = rowVirtualizer.getVirtualItems();
+  const paddingTop = items.length > 0 ? items[0].start : 0;
+  const paddingBottom = items.length > 0 ? rowVirtualizer.getTotalSize() - items[items.length - 1].end : 0;
 
   const renderHeader = () => {
     return (
       <Table.Head style={{ zIndex: 1000 }}>
         <Table.Row>
           {columns.map((column: any) => (
-            <Table.HeadCell key={column.id} style={{ width: column.width }}>
+            <Table.HeadCell key={column.id} style={{ width: column.width, position: "sticky", top: 0 }}>
               {column.title}
             </Table.HeadCell>
           ))}
@@ -86,7 +103,7 @@ const NetworkTable: React.FC<Props> = ({ logs, onRow }) => {
     );
   };
 
-  const renderLogRow = (log: any, index: number, style: any) => {
+  const renderLogRow = (log: any, index: number) => {
     if (!log) {
       return null;
     }
@@ -95,6 +112,7 @@ const NetworkTable: React.FC<Props> = ({ logs, onRow }) => {
 
     return (
       <Table.Row
+        key={index}
         id={log.id}
         onContextMenu={() => setSelectedRowData(log)}
         {...rowProps}
@@ -109,10 +127,6 @@ const NetworkTable: React.FC<Props> = ({ logs, onRow }) => {
     );
   };
 
-  const Row = ({ index, style }: any) => {
-    return renderLogRow(logs[index], index, style);
-  };
-
   return (
     <>
       <ProductWalkthrough
@@ -120,16 +134,47 @@ const NetworkTable: React.FC<Props> = ({ logs, onRow }) => {
         startWalkthrough={!isTrafficTableTourCompleted}
         onTourComplete={() => dispatch(actions.updateTrafficTableTourCompleted({}))}
       />
-      <VirtualTable
-        height="100%"
-        width="100%"
-        itemCount={logs?.length ?? 0}
-        itemSize={ITEM_SIZE}
-        header={renderHeader()}
-        row={Row}
-        footer={null}
-        selectedRowData={selectedRowData}
-      />
+      <AutoSizer>
+        {({ height, width }) => {
+          return (
+            <div
+              ref={parentRef}
+              style={{
+                height: height,
+                width: width,
+                overflow: "auto",
+              }}
+            >
+              <Table
+                id="vtable"
+                style={
+                  {
+                    "--virtualPaddingTop": paddingTop + "px",
+                    "--virtualPaddingBottom": paddingBottom + "px",
+                  } as React.CSSProperties
+                }
+                selected={selected}
+                onSelected={(id: string) => {
+                  setSelected(id);
+                }}
+              >
+                {renderHeader()}
+                <ContextMenu log={selectedRowData}>
+                  <Table.Body id="vtbody" style={{}}>
+                    {/* Hack to fix alternate colors flickering due to virtualization*/}
+                    {rowVirtualizer.getVirtualItems()?.[0]?.index % 2 === 0 ? null : <tr></tr>}
+
+                    {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+                      const log = logs[virtualRow.index];
+                      return renderLogRow(log, virtualRow.index);
+                    })}
+                  </Table.Body>
+                </ContextMenu>
+              </Table>
+            </div>
+          );
+        }}
+      </AutoSizer>
     </>
   );
 };
