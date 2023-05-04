52c0b66d7d518e779bbdeb51781a0347c381ab60
RuntimeTerror10 <parthb.git@gmail.com>
2023-05-04 13:23:21 +0530

    add clear logs button

--- app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/LogFilter.tsx
+++ app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/LogFilter.tsx
@@ -5,20 +5,22 @@ interface FilterProps {
   filterId: string;
   filterLabel: string;
   filterPlaceholder: string;
+  value: string[];
   options: [];
   handleFilterChange?: (options: []) => void;
 }
 
-export const TrafficFilter: React.FC<FilterProps> = ({
+export const LogFilter: React.FC<FilterProps> = ({
   filterId,
   filterLabel,
   filterPlaceholder,
+  value,
   options,
   handleFilterChange,
 }) => {
   return (
     <Space direction="vertical">
-      <label htmlFor={filterId} className="traffic-table-filter-label" style={{ marginLeft: 25 }}>
+      <label htmlFor={filterId} className="traffic-table-filter-label">
         {filterLabel}
       </label>
       <Select
@@ -26,6 +28,7 @@ export const TrafficFilter: React.FC<FilterProps> = ({
         id={filterId}
         className="traffic-table-filter-select"
         mode="multiple"
+        value={value}
         placeholder={filterPlaceholder}
         options={options}
         onChange={(options: any) => handleFilterChange(options)}
