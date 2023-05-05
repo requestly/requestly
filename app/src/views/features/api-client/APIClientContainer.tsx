893516c1f78c6e170c62480debd919bf3ef940f4
Vaibhav Nigam <lazyvab@gmail.com>
2023-05-06 00:19:39 +0530

    RQ-216 Refactored Import request dialog

--- app/src/views/features/api-client/APIClientContainer.tsx
+++ app/src/views/features/api-client/APIClientContainer.tsx
@@ -3,17 +3,14 @@ import APIClientSidebar, { NO_HISTORY_INDEX } from "./sidebar/APIClientSidebar";
 import APIClientView from "./client-view/APIClientView";
 import { RQAPI } from "./types";
 import { addToHistoryInStore, clearHistoryFromStore, getHistoryFromStore } from "./historyStore";
-import { Input, Modal } from "antd";
-import { getEmptyAPIEntry, parseCurlRequest } from "./apiUtils";
-import { toast } from "utils/Toast";
+import { getEmptyAPIEntry } from "./apiUtils";
 import {
-  trackCurlImportFailed,
-  trackCurlImported,
   trackHistoryCleared,
   trackImportCurlClicked,
   trackNewRequestClicked,
 } from "modules/analytics/events/features/apiClient";
 import "./apiClientContainer.scss";
+import ImportRequestModal from "./ImportRequestModal";
 
 interface Props {}
 
@@ -22,7 +19,6 @@ const APIClientContainer: React.FC<Props> = () => {
   const [selectedHistoryIndex, setSelectedHistoryIndex] = useState<number>(NO_HISTORY_INDEX);
   const [selectedEntry, setSelectedEntry] = useState<RQAPI.Entry>();
   const [isImportModalOpen, setIsImportModalOpen] = useState(false);
-  const [curlCommand, setCurlCommand] = useState("");
 
   const addToHistory = useCallback((apiEntry: RQAPI.Entry) => {
     setHistory((history) => [...history, apiEntry]);
@@ -35,25 +31,16 @@ const APIClientContainer: React.FC<Props> = () => {
     trackHistoryCleared();
   }, [setHistory]);
 
-  const onImportFromCurl = useCallback(() => {
-    const requestFromCurl: RQAPI.Request = parseCurlRequest(curlCommand);
-    if (!requestFromCurl || !requestFromCurl.url) {
-      toast.error("Could not process the cURL command");
-      trackCurlImportFailed();
-      return;
-    }
-
+  const handleImportRequest = useCallback((request: RQAPI.Request) => {
     const newApiEntry: RQAPI.Entry = {
-      request: requestFromCurl,
+      request,
       response: null,
     };
 
     setSelectedEntry(newApiEntry);
     setSelectedHistoryIndex(NO_HISTORY_INDEX);
     setIsImportModalOpen(false);
-    setCurlCommand("");
-    trackCurlImported();
-  }, [curlCommand]);
+  }, []);
 
   const onImportClick = useCallback(() => {
     setIsImportModalOpen(true);
@@ -78,23 +65,11 @@ const APIClientContainer: React.FC<Props> = () => {
         onImportClick={onImportClick}
       />
       <APIClientView apiEntry={selectedEntry} notifyApiRequestFinished={addToHistory} />
-      <Modal
-        className="import-modal"
-        centered
-        title="Import from cURL"
-        open={isImportModalOpen}
-        okText="Import"
-        onOk={onImportFromCurl}
-        onCancel={() => setIsImportModalOpen(false)}
-        width="70%"
-      >
-        <Input.TextArea
-          className="curl-command-input"
-          value={curlCommand}
-          onChange={(e) => setCurlCommand(e.target.value)}
-          placeholder="curl https://example.com"
-        />
-      </Modal>
+      <ImportRequestModal
+        isOpen={isImportModalOpen}
+        handleImportRequest={handleImportRequest}
+        onClose={() => setIsImportModalOpen(false)}
+      />
     </div>
   );
 };
