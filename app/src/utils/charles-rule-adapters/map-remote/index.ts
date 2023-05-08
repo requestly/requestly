0f0d63aa50ab1c229d44078d6e60d4362664b4c3
Rohan Mathur <mathurrohan04@gmail.com>
2023-05-08 19:05:10 +0530

    RQ-273: map-remote rule adapter (#601)
    * fix typo
    
    * fix: `createNewGroup` for status
    
    * fix: imported rules group status
    
    * chore: update types
    
    * chore: update map-remote rule types
    
    * refactor utils
    
    * feat: add map-remote adapter
    
    * refactor: save rules to storage

---
+++ app/src/utils/charles-rule-adapters/map-remote/index.ts
@@ -0,0 +1,49 @@
+import { get } from "lodash";
+import { getNewRule } from "components/features/rules/RuleBuilder/actions";
+import { generateObjectId } from "utils/FormattingHelper";
+import { RuleType, Status, RedirectRule, RedirectDestinationType } from "types";
+import { createNewGroupAndSave, getLocation } from "../utils";
+import { CharlesRuleType, MapRemoteRule, RemoteRuleMappings } from "../types";
+
+export const mapRemoteAdapter = <T = MapRemoteRule>(rules: T, appMode: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const mappings = get(rules, "map.mappings.mapMapping") as RemoteRuleMappings;
+    const updatedMappings = Array.isArray(mappings) ? mappings : [mappings];
+
+    if (!mappings) {
+      reject();
+      return;
+    }
+
+    const exportedRules = updatedMappings.map(({ sourceLocation, destLocation, enabled }) => {
+      const source = getLocation(sourceLocation);
+      const destination = getLocation(destLocation);
+      const rule = getNewRule(RuleType.REDIRECT) as RedirectRule;
+      return {
+        ...rule,
+        isCharlesExported: true,
+        name: `${source.value} to ${destination.value}`,
+        status: enabled ? Status.ACTIVE : Status.INACTIVE,
+        pairs: [
+          {
+            ...rule.pairs[0],
+            id: generateObjectId(),
+            destination: destination.value,
+            destinationType: RedirectDestinationType.URL,
+            source: { ...rule.pairs[0].source, value: source.value, operator: source.operator },
+          },
+        ],
+      };
+    });
+
+    const isToolEnabled = get(rules, "map.toolEnabled");
+    createNewGroupAndSave({
+      appMode,
+      rules: exportedRules,
+      status: isToolEnabled,
+      onError: reject,
+      onSuccess: resolve,
+      groupName: CharlesRuleType.MAP_REMOTE,
+    });
+  });
+};
