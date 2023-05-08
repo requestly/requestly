0a2d4d26ea8e36ceb7e3f3297d536fc1c21417e2
rohanmathur91 <mathurrohan04@gmail.com>
2023-05-08 12:15:41 +0530

    refactor: headers + rule and group name

--- app/src/utils/charles-rule-adapters/types.ts
+++ app/src/utils/charles-rule-adapters/types.ts
@@ -1,5 +1,22 @@
 import { HeaderRuleActionType } from "types";
 
+export enum Header {
+  PRAGMA = "Pragma",
+  CACHE_CONTROL = "Cache-Control",
+  IF_MODIFIED_SINCE = "If-Modified-Since",
+  IF_NONE_MATCH = "If-None-Match",
+  EXPIRES = "Expires",
+  LAST_MODIFIED = "Last-Modified",
+  ETAG = "ETag",
+  COOKIE = "Cookie",
+  SET_COOKIE = "Set-Cookie",
+}
+
+export enum HeaderValue {
+  UNKNOWN = "",
+  NO_CACHE = "no-cache",
+}
+
 export enum CharlesRuleType {
   NO_CACHING = "No Caching",
   BLOCK_COOKIES = "Block Cookies",
