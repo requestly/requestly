033a1ecebf4afe875d095cc0fa89848427cb2a1d
rohanmathur91 <mathurrohan04@gmail.com>
2023-05-31 12:35:35 +0530

    base setup

---
+++ app/src/layouts/DashboardLayout/SidebarV2/SecondarySidebar/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./SecondarySidebar.css";
+
+export const SecondarySidebar: React.FC = () => {
+  return <div className="secondary-sidebar-container">SecondarySidebar</div>;
+};
