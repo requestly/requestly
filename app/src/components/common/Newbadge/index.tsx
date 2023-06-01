8f9a6c03f62c3b725fef70cda3c8ee93e3371a78
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-01 13:08:07 +0530

    add new badge component

---
+++ app/src/components/common/Newbadge/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Newbadge.css";
+
+export const Newbadge: React.FC = () => {
+  return (
+    <div className="rq-new-badge">
+      <span>NEW</span>
+    </div>
+  );
+};
