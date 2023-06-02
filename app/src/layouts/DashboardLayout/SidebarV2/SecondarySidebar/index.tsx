2e5751bb1128ff27e6bcb4723a8afe3ec641188d
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-02 15:09:24 +0530

    fix: app layout

--- app/src/layouts/DashboardLayout/SidebarV2/SecondarySidebar/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./SecondarySidebar.css";
-
-export const SecondarySidebar: React.FC = () => {
-  return <div className="secondary-sidebar-container">SecondarySidebar</div>;
-};
