8c8a30cd876159163989e94baaa63498008f51fa
Sahil Gupta <sahil865gupta@gmail.com>
2023-06-04 18:56:15 +0530

    chore: added sentry release id in log

--- app/sourcemaps.sh
+++ app/sourcemaps.sh
@@ -4,5 +4,5 @@ curl -sL https://sentry.io/get-cli/ | sh
 echo "Injecting sentry debug ids"
 sentry-cli sourcemaps inject ./build
 
-echo "Uploading sourcemaps to sentry"
+echo "Uploading sourcemaps to sentry with release=${GITHUB_SHA}"
 sentry-cli sourcemaps upload --release=${GITHUB_SHA} ./build
