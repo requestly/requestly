f6b8b1971a1747f9336206670e04d6abbac2094f
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-06 09:28:59 +0530

    update session routes

--- app/src/routes/sessions.route.tsx
+++ app/src/routes/sessions.route.tsx
@@ -1,6 +1,5 @@
 import { RouteObject } from "react-router-dom";
 import PATHS from "config/constants/sub/paths";
-import { SessionsContainer } from "views/containers/SessionsContainer";
 import SessionsHomeView from "views/features/sessions";
 import { DraftSessionViewer, SavedSessionViewer } from "views/features/sessions/SessionViewer";
 import NetworkSessionViewer from "views/features/sessions/SessionsIndexPageContainer/NetworkSessions/NetworkSessionViewer";
@@ -8,24 +7,18 @@ import NetworkSessionViewer from "views/features/sessions/SessionsIndexPageConta
 export const sessionRoutes: RouteObject[] = [
   {
     path: PATHS.SESSIONS.INDEX,
-    element: <SessionsContainer />,
-    children: [
-      {
-        index: true,
-        element: <SessionsHomeView />,
-      },
-      {
-        path: PATHS.SESSIONS.DRAFT.RELATIVE + "/:tabId",
-        element: <DraftSessionViewer />,
-      },
-      {
-        path: PATHS.SESSIONS.SAVED.RELATIVE + "/:id",
-        element: <SavedSessionViewer />,
-      },
-      {
-        path: PATHS.SESSIONS.NETWORK.RELATIVE + "/:id",
-        element: <NetworkSessionViewer />,
-      },
-    ],
+    element: <SessionsHomeView />,
+  },
+  {
+    path: PATHS.SESSIONS.DRAFT.RELATIVE + "/:tabId",
+    element: <DraftSessionViewer />,
+  },
+  {
+    path: PATHS.SESSIONS.SAVED.RELATIVE + "/:id",
+    element: <SavedSessionViewer />,
+  },
+  {
+    path: PATHS.SESSIONS.NETWORK.RELATIVE + "/:id",
+    element: <NetworkSessionViewer />,
   },
 ];
