76f7f889e4fa3ebff6fb133362cd5ac69c70ba8b
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-06 17:30:30 +0530

    update routes

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -3,5 +3,24 @@ import { ruleRoutes } from "./ruleRoutes";
 import { sessionRoutes } from "./sessionRoutes";
 import { apiClientRoutes } from "./apiClientRoutes";
 import { accountRoutes } from "./accountRoutes";
+import { authRoutes } from "./authRoutes";
+import { desktopRoutes } from "./desktopRoutes";
+import { mobileDebuggerRoutes } from "./mobileDebuggerRoutes";
+import { mockServerRoutes } from "./mockServerRoutes";
+import { onboardingRoutes } from "./onboardingRoutes";
+import { miscRoutes } from "./miscRoutes";
+import { settingRoutes } from "./settingRoutes";
 
-export const updatedRoutes: RouteObject[] = [...ruleRoutes, ...sessionRoutes, ...apiClientRoutes, ...accountRoutes];
+export const updatedRoutes: RouteObject[] = [
+  ...ruleRoutes,
+  ...sessionRoutes,
+  ...apiClientRoutes,
+  ...accountRoutes,
+  ...authRoutes,
+  ...desktopRoutes,
+  ...mobileDebuggerRoutes,
+  ...mockServerRoutes,
+  ...onboardingRoutes,
+  ...miscRoutes,
+  ...settingRoutes,
+];
