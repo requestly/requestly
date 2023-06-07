cd767147f153a88e433ae94351469d8beb2242a3
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-07 11:57:37 +0530

    remove console log

--- app/src/views/containers/RulesContainer/index.tsx
+++ app/src/views/containers/RulesContainer/index.tsx
@@ -3,8 +3,6 @@ import { RulesSidebar } from "./RulesSidebar";
 import { ContainerWithSecondarySidebar } from "../common/ContainerWithSecondarySidebar";
 
 export const RulesContainer: React.FC = () => {
-  console.log({ location: window.location.href });
-
   return (
     <ContainerWithSecondarySidebar>
       <RulesSidebar />
