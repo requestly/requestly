5def7ace7d36d16a5c3cd9d76adc936e7de2407e
Nafees Nehar <nafees87n@gmail.com>
2023-06-12 15:59:30 +0530

    RQ-453: added toggle to enable joining workspace if same domain

--- app/src/components/user/AccountIndexPage/ManageAccount/ManageTeams/TeamViewer/MembersDetails/PublicInviteLink/index.tsx
+++ app/src/components/user/AccountIndexPage/ManageAccount/ManageTeams/TeamViewer/MembersDetails/PublicInviteLink/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { useSelector } from "react-redux";
-import { Col, Row, Input, Divider } from "antd";
+import { Col, Row, Input, Divider, Typography, Switch, Space } from "antd";
 import { getCurrentlyActiveWorkspaceMembers } from "store/features/teams/selectors";
 //Firebase
 import { getFunctions, httpsCallable } from "firebase/functions";
@@ -15,30 +15,37 @@ import {
   trackWorkspaceInviteLinkGenerated,
   trackWorkspaceInviteLinkRevoked,
 } from "modules/analytics/events/features/teams";
+import { getDomainFromEmail } from "utils/FormattingHelper";
 
 interface Props {
   teamId: string;
 }
 
 const PublicInviteLink: React.FC<Props> = ({ teamId }) => {
+  const functions = getFunctions();
   // Component state
   const [isLoading, setIsLoading] = useState(false);
   const [publicInviteId, setPublicInviteId] = useState(null);
   const [publicInviteLoading, setPublicInviteLoading] = useState(false);
+  const [isInvitePublic, setIsInvitePublic] = useState(false);
+  const [domainJoiningEnabled, setDomainJoiningEnabled] = useState(false);
 
   // Global state
   const currentTeamMembers = useSelector(getCurrentlyActiveWorkspaceMembers);
   const user = useSelector(getUserAuthDetails);
   const isCurrentUserAdmin = currentTeamMembers[user?.details?.profile?.uid]?.isAdmin === true;
 
+  const userEmailDomain = useMemo(() => getDomainFromEmail(user?.details?.profile?.email), [
+    user?.details?.profile?.email,
+  ]);
+
   const handlePublicInviteCreateClicked = () => {
     trackWorkspaceInviteLinkGenerated(teamId);
     setIsLoading(true);
-    const functions = getFunctions();
     const createTeamInvite = httpsCallable(functions, "invites-createTeamInvite");
     createTeamInvite({ teamId: teamId, usage: "unlimited" }).then((res: any) => {
       if (res?.data?.success) {
-        setPublicInviteId(res?.data?.inviteId);
+        setIsInvitePublic(true);
       } else {
         toast.error("Only admins can invite people");
       }
@@ -49,11 +56,10 @@ const PublicInviteLink: React.FC<Props> = ({ teamId }) => {
   const handlePublicInviteRevokeClicked = () => {
     trackWorkspaceInviteLinkRevoked(teamId);
     setIsLoading(true);
-    const functions = getFunctions();
     const revokeInvite = httpsCallable(functions, "invites-revokeInvite");
     revokeInvite({ inviteId: publicInviteId }).then((res: any) => {
       if (res?.data?.success) {
-        setPublicInviteId(null);
+        setIsInvitePublic(false);
         toast.success("Successfully Revoked invite");
       } else {
         toast.error("Only admins can revoke invites");
@@ -74,6 +80,8 @@ const PublicInviteLink: React.FC<Props> = ({ teamId }) => {
       .then((res: any) => {
         if (res?.data?.success) {
           setPublicInviteId(res?.data?.inviteId);
+          setIsInvitePublic(res?.data?.public);
+          setDomainJoiningEnabled(res?.data?.domains?.length > 0);
         }
         setPublicInviteLoading(false);
       })
@@ -83,6 +91,16 @@ const PublicInviteLink: React.FC<Props> = ({ teamId }) => {
   };
 
   const stableFetchPublicInviteLink = useCallback(fetchPublicInviteLink, [teamId]);
+
+  const handleDomainToggle = useCallback(
+    (enabled: boolean) => {
+      const setDomainInInvite = httpsCallable(functions, "invites-updateInviteDomains");
+      setDomainInInvite({ teamId, domainEnabled: enabled }).then(() => {
+        setDomainJoiningEnabled(enabled);
+      });
+    },
+    [functions, teamId]
+  );
 
   useEffect(() => {
     stableFetchPublicInviteLink();
@@ -105,7 +123,7 @@ const PublicInviteLink: React.FC<Props> = ({ teamId }) => {
             <Row align="middle" justify="space-between">
               <Col className="title">Public Invite link</Col>
               <Col className="ml-auto">
-                {publicInviteId ? (
+                {isInvitePublic ? (
                   <RQButton loading={isLoading} danger type="primary" onClick={handlePublicInviteRevokeClicked}>
                     {isLoading ? "Revoking" : "Revoke"}
                   </RQButton>
@@ -124,7 +142,7 @@ const PublicInviteLink: React.FC<Props> = ({ teamId }) => {
         </Row>
       )}
 
-      {publicInviteId ? (
+      {isInvitePublic ? (
         <Row justify="space-between">
           <Col flex="1 0 auto" className="invite-link-input-container">
             <Input
@@ -140,6 +158,11 @@ const PublicInviteLink: React.FC<Props> = ({ teamId }) => {
           </Col>
         </Row>
       ) : null}
+
+      <Space className="mt-8">
+        <Typography.Text type="secondary">{`Anyone with ${userEmailDomain} can join this workspace`}</Typography.Text>
+        <Switch checked={domainJoiningEnabled} size="small" onChange={handleDomainToggle} />
+      </Space>
       <Divider className="manage-workspace-divider" />
     </>
   );
