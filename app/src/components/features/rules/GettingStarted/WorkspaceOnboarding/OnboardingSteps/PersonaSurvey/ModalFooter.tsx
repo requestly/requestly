a529330fd259d0b99cf70ccc09225d68b01d175c
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2023-06-12 14:36:38 +0530

    RQ-437: Workspace adoption full page persona survey (#703)
    * refactor full page header component
    
    * full page signup/login screen
    
    * persist onboarding screen
    
    * chore:fix
    
    * add persona survey banner
    
    * refactor persona survey
    
    * fix persona survey styles
    
    * move to workspaces step on survey completion
    
    * chore: add comment
    
    * chore:fix
    
    * chore:fix

---
+++ app/src/components/features/rules/GettingStarted/WorkspaceOnboarding/OnboardingSteps/PersonaSurvey/ModalFooter.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserPersonaSurveyDetails } from "store/selectors";
+import { actions } from "store";
+import { Col, Row } from "antd";
+import { RQButton } from "lib/design-system/components";
+import { OptionsConfig, SurveyConfig } from "./config";
+import APP_CONSTANTS from "config/constants";
+//@ts-ignore
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import { submitAttrUtil } from "utils/AnalyticsUtils";
+import { trackPersonaQ1Completed } from "modules/analytics/events/misc/personaSurvey";
+import "./index.css";
+import { QuestionnaireType } from "./types";
+
+interface FooterProps {
+  currentPage: number;
+  callback: () => void;
+}
+
+export const SurveyModalFooter: React.FC<FooterProps> = ({ currentPage, callback }) => {
+  const dispatch = useDispatch();
+  const userPersona = useSelector(getUserPersonaSurveyDetails);
+  const currentQuestionnaire = SurveyConfig[currentPage]?.render;
+
+  const disableContinue = () => {
+    if (userPersona[OptionsConfig[currentQuestionnaire]?.key]?.length) return false;
+    return true;
+  };
+
+  const handleMoveToNextPage = () => {
+    switch (currentQuestionnaire) {
+      case QuestionnaireType.PERSONA:
+        trackPersonaQ1Completed(userPersona.persona);
+        submitAttrUtil(APP_CONSTANTS.GA_EVENTS.ATTR.PERSONA, userPersona.persona);
+        break;
+      //   case 2:
+      //     trackPersonaQ2Completed(getFormattedUserUseCases(userPersona.useCases));
+      //     submitAttrUtil(
+      //       APP_CONSTANTS.GA_EVENTS.ATTR.USE_CASES,
+      //       getFormattedUserUseCases(userPersona.useCases)
+      //     );
+      //     break;
+      // case 3:
+      // trackPersonaQ3Completed(userPersona.referralChannel);
+      // submitAttrUtil(APP_CONSTANTS.GA_EVENTS.ATTR.REFERRAL_CHANNEL, userPersona.referralChannel);
+      // if (isSharedListUser || appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP) {
+      //   //don't show recommendation screen for shared list users or desktop users
+      //   dispatch(actions.updateIsPersonaSurveyCompleted(true));
+      //   return;
+      // }
+
+      // dispatch(actions.toggleActiveModal({ modalName: "personaSurveyModal", newValue: false }));
+      // navigate(PATHS.GETTING_STARTED, {
+      //   replace: true,
+      //   state: {
+      //     src: "persona_survey_modal",
+      //     redirectTo: location.pathname,
+      //   },
+      // });
+      // break;
+    }
+    dispatch(actions.updatePersonaSurveyPage(currentPage + 1));
+    if (currentPage === SurveyConfig.length - 1) {
+      // dispatch(actions.updateIsPersonaSurveyCompleted(true));
+      callback();
+    }
+  };
+  return (
+    <>
+      <div className="survey-footer w-full">
+        <Row justify="space-between" align="middle">
+          <Col>
+            {currentPage + 1} / {SurveyConfig?.length}
+          </Col>
+          <Col>
+            <RQButton
+              type="primary"
+              className={`text-bold ${disableContinue() && "survey-disable-continue"}`}
+              onClick={handleMoveToNextPage}
+            >
+              {currentQuestionnaire === QuestionnaireType.PERSONA ? "Get started" : "Continue"}
+            </RQButton>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
---
+++ app/src/components/features/rules/GettingStarted/WorkspaceOnboarding/OnboardingSteps/PersonaSurvey/index.tsx
@@ -0,0 +1,91 @@
+import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { getUserPersonaSurveyDetails } from "store/selectors";
+import { SurveyModalFooter } from "./ModalFooter";
+import { SurveyConfig, OptionsConfig } from "./config";
+import { shuffleOptions } from "./utils";
+import { Option, PageConfig, QuestionnaireType } from "./types";
+// import { trackPersonaSurveyViewed, trackPersonaSurveySignInClicked } from "modules/analytics/events/misc/personaSurvey";
+//@ts-ignore
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import { SurveyOption } from "./Option";
+import "./index.css";
+
+interface SurveyProps {
+  callback: () => void;
+}
+
+export const PersonaSurvey: React.FC<SurveyProps> = ({ callback }) => {
+  const userPersona = useSelector(getUserPersonaSurveyDetails);
+  const currentPage = userPersona.page;
+
+  const shuffledQ1 = useMemo(() => {
+    return shuffleOptions(OptionsConfig[QuestionnaireType.PERSONA].options);
+  }, []);
+
+  const renderPageHeader = (page: PageConfig) => {
+    return (
+      <>
+        <div className="text-center white text-bold survey-title">{page.title}</div>
+        <div className="w-full survey-subtitle-wrapper">
+          <div className="text-gray text-center mt-8">{page.subTitle}</div>
+        </div>
+      </>
+    );
+  };
+
+  const renderQuestionnaire = (optionSet: QuestionnaireType) => {
+    switch (optionSet) {
+      case QuestionnaireType.PERSONA:
+        return renderOptions(shuffledQ1, optionSet);
+      default:
+        return null;
+    }
+  };
+  const renderOptions = (options: Option[], optionSet: QuestionnaireType) => {
+    return (
+      <>
+        <div className="survey-options-container">
+          {options.map((option: Option, index: number) => (
+            <SurveyOption
+              key={index}
+              option={option}
+              questionType={OptionsConfig[optionSet].questionType}
+              isActive={OptionsConfig[optionSet].isActive}
+              action={OptionsConfig[optionSet].action}
+              fieldKey={OptionsConfig[optionSet].key}
+            />
+          ))}
+        </div>
+      </>
+    );
+  };
+
+  const renderPage = (page: PageConfig) => {
+    return (
+      <>
+        {renderPageHeader(page)}
+        <>{renderQuestionnaire(page.render)}</>
+      </>
+    );
+  };
+
+  // useEffect(() => {
+  //   if (currentPage > SurveyConfig?.length - 1) callback();
+  // }, [callback, currentPage]);
+
+  return (
+    <div className="persona-survey-container">
+      <div
+        className={`rq-modal-content survey-content-wrapper ${
+          currentPage === SurveyConfig.length - 1 && "survey-modal-border-radius"
+        }`}
+      >
+        {SurveyConfig.filter((config) => !config.skip).map((page: PageConfig, index) => (
+          <React.Fragment key={index}>{currentPage === page.pageId && <>{renderPage(page)}</>}</React.Fragment>
+        ))}
+      </div>
+      <SurveyModalFooter currentPage={currentPage} callback={callback} />
+    </div>
+  );
+};
