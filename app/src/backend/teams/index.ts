7b8372d690a7ba2153f840c82133396b616ed5fe
Nafees Nehar <nafees87n@gmail.com>
2023-06-14 20:14:56 +0530

    remove migration method

--- app/src/backend/teams/index.ts
+++ app/src/backend/teams/index.ts
@@ -1,11 +1,1 @@
-import firebaseApp from "../../firebase";
-import { doc, getFirestore, updateDoc } from "firebase/firestore";
 export * from "./getTeamInvites";
-
-export const migrateCurrentWorkspacePublicInvite = (inviteId: string, publicEnabled: boolean) => {
-  const db = getFirestore(firebaseApp);
-  const inviteRef = doc(db, "invites", inviteId);
-  updateDoc(inviteRef, {
-    public: publicEnabled,
-  });
-};
