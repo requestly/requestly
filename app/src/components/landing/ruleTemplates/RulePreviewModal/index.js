1441476fdd2b42817436bdd40662a332eb655308
nsrCodes <navdeepsinghrathore2000@gmail.com>
2023-06-14 20:32:44 +0530

    chore: remove lastModified from attribute of ruleToSave

--- app/src/components/landing/ruleTemplates/RulePreviewModal/index.js
+++ app/src/components/landing/ruleTemplates/RulePreviewModal/index.js
@@ -35,7 +35,6 @@ const RulePreviewModal = ({ rule, isOpen, toggle }) => {
         return;
       }
     }
-    const lastModified = Date.now();
     const modificationDate = Date.now();
 
     const ruleToSave = {
@@ -43,7 +42,6 @@ const RulePreviewModal = ({ rule, isOpen, toggle }) => {
       id: `${ruleObj.ruleDefinition.ruleType}_${generateObjectId()}`,
       createdBy,
       currentOwner,
-      lastModified,
       modificationDate,
     };
 
