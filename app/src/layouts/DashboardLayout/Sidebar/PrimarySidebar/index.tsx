3bfbcc3041b07574d1e7a99e5ef06886dcea5ad8
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-19 18:27:33 +0530

    fix: sidebar session option tooltip

--- app/src/layouts/DashboardLayout/Sidebar/PrimarySidebar/index.tsx
+++ app/src/layouts/DashboardLayout/Sidebar/PrimarySidebar/index.tsx
@@ -1,32 +1,39 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
-import { getAppMode, getUserAuthDetails } from "store/selectors";
+import { getAppMode, getNetworkSessionSaveInProgress, getUserAuthDetails } from "store/selectors";
 import { PrimarySidebarItem } from "../type";
 import PATHS from "config/constants/sub/paths";
+import { Tooltip } from "antd";
 import { ApiOutlined, MobileOutlined } from "@ant-design/icons";
 import { ReactComponent as NetworkTrafficIcon } from "assets/icons/network-traffic.svg";
 import { ReactComponent as HttpRulesIcon } from "assets/icons/http-rules.svg";
 import { ReactComponent as SessionIcon } from "assets/icons/session.svg";
 import { ReactComponent as MockServerIcon } from "assets/icons/mock-server.svg";
+import FEATURES from "config/constants/sub/features";
 //@ts-ignore
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import { ReactComponent as InviteIcon } from "assets/icons/invite.svg";
 import { PrimarySidebarLink } from "./PrimarySidebarLink";
 import { isUserUsingAndroidDebugger } from "components/features/mobileDebugger/utils/sdkUtils";
+import { isFeatureCompatible } from "utils/CompatibilityUtils";
 import { RQBadge } from "lib/design-system/components/RQBadge";
 import "./PrimarySidebar.css";
 
 export const PrimarySidebar: React.FC = () => {
   const appMode = useSelector(getAppMode);
   const user = useSelector(getUserAuthDetails);
+  const isSavingNetworkSession = useSelector(getNetworkSessionSaveInProgress);
   const [isAndroidDebuggerEnabled, setIsAndroidDebuggerEnabled] = useState(false);
 
   useEffect(() => {
     isUserUsingAndroidDebugger(user?.details?.profile?.uid).then(setIsAndroidDebuggerEnabled);
   }, [user?.details?.profile?.uid]);
 
-  const sidebarItems: PrimarySidebarItem[] = useMemo(
-    () => [
+  const sidebarItems: PrimarySidebarItem[] = useMemo(() => {
+    const showTooltipForSessionIcon = appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP && isSavingNetworkSession;
+
+    console.log({ appMode, showTooltipForSessionIcon, isSavingNetworkSession });
+    const items = [
       {
         id: 0,
         title: "Network traffic",
@@ -45,7 +52,15 @@ export const PrimarySidebar: React.FC = () => {
         id: 2,
         title: "Sessions",
         path: PATHS.SESSIONS.INDEX,
-        icon: <SessionIcon />,
+        icon: (
+          <Tooltip
+            placement="right"
+            open={isSavingNetworkSession}
+            title={showTooltipForSessionIcon ? "View and manage your saved sessions here" : ""}
+          >
+            <SessionIcon />
+          </Tooltip>
+        ),
         display: true,
       },
       {
@@ -64,6 +79,7 @@ export const PrimarySidebar: React.FC = () => {
             <ApiOutlined /> <RQBadge badgeText="BETA" />
           </span>
         ),
+        feature: FEATURES.API_CLIENT,
         display: true,
       },
       {
@@ -73,9 +89,10 @@ export const PrimarySidebar: React.FC = () => {
         icon: <MobileOutlined />,
         display: isAndroidDebuggerEnabled,
       },
-    ],
-    [appMode, isAndroidDebuggerEnabled]
-  );
+    ];
+
+    return items.filter((item) => !item.feature || isFeatureCompatible(item.feature));
+  }, [appMode, isAndroidDebuggerEnabled, isSavingNetworkSession]);
 
   return (
     <div className="primary-sidebar-container">
