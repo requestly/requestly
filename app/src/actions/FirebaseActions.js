e804c1daed430b4b8aefcd2db9b6b13749450ccc
RuntimeTerror10 <parthb.git@gmail.com>
2023-06-20 09:34:13 +0530

    chore:cleanup

--- app/src/actions/FirebaseActions.js
+++ app/src/actions/FirebaseActions.js
@@ -373,7 +373,6 @@ export const googleSignInDesktopApp = (callback, MODE, source) => {
       const credential = await signInWithCustomToken(auth, authToken);
       // Remove auth code from db as we no longer need it
       remove(oneTimeCodeRef);
-      let is_new_user = getAdditionalUserInfo(credential) || false;
 
       //Return the firebase user object
       resolve(credential.user);
