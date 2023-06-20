82a9ef4802da1a70ab997178340202d612fbb314
RuntimeTerror10 <parthb.git@gmail.com>
2023-06-20 16:21:57 +0530

    hide onboarding in /invite page

--- app/src/components/misc/PersonaSurvey/utils.ts
+++ app/src/components/misc/PersonaSurvey/utils.ts
@@ -6,7 +6,8 @@ import PATHS from "config/constants/sub/paths";
 
 export const shouldShowOnboarding = async (appMode: string) => {
   // Don't show persona survey on Browser if user is authenticating from desktop app
-  if (window.location.href.includes(PATHS.AUTH.DEKSTOP_SIGN_IN.RELATIVE)) return false;
+  if (window.location.href.includes(PATHS.AUTH.DEKSTOP_SIGN_IN.RELATIVE) || window.location.href.includes("/invite"))
+    return false;
 
   const installDate = await getAndUpdateInstallationDate(appMode, false, false);
   if (new Date(installDate) >= new Date("2023-03-06")) return true;
