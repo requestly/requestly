9befa29206d14f66b11bca5ef21b894b5cc7b72c
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-20 18:48:16 +0530

    fix: delay rule alert

--- app/src/components/features/rules/RuleBuilder/Body/RuleInfoBanner/index.tsx
+++ app/src/components/features/rules/RuleBuilder/Body/RuleInfoBanner/index.tsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Alert, Col, Row, Typography } from "antd";
 import { InfoCircleOutlined } from "@ant-design/icons";
 //@ts-ignore
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import LINKS from "config/constants/sub/links";
+import { trackDesktopAppPromoClicked } from "modules/analytics/events/common/onboarding";
 import "./ruleInfoBanner.css";
 
-const ruleInfoBannerContent: Record<string, { title: string; description: string; appMode?: string[] }> = {
-  [GLOBAL_CONSTANTS.RULE_TYPES.DELAY]: {
-    appMode: [GLOBAL_CONSTANTS.APP_MODES.EXTENSION],
-    title: "On extension delay is capped automatically to avoid browsing performance degradation.",
-    description: `For XHR/Fetch, max delay is ${GLOBAL_CONSTANTS.DELAY_REQUEST_CONSTANTS.MAX_DELAY_VALUE_XHR}ms & for other resources (JS, CSS, Images etc), max delay is ${GLOBAL_CONSTANTS.DELAY_REQUEST_CONSTANTS.MAX_DELAY_VALUE_NON_XHR}ms.`,
-  },
-};
-
 const RuleInfoBanner: React.FC<{ ruleType: string; appMode: string }> = ({ ruleType, appMode }) => {
-  if (!(ruleType in ruleInfoBannerContent)) return null;
+  const ruleInfoBannerContent: Record<
+    string,
+    { title: React.ReactNode; description: React.ReactNode; appMode?: string[] }
+  > = useMemo(
+    () => ({
+      [GLOBAL_CONSTANTS.RULE_TYPES.DELAY]: {
+        appMode: [GLOBAL_CONSTANTS.APP_MODES.EXTENSION],
+        title: "On browser extension, delay is capped automatically to avoid browsing performance degradation.",
+        description: (
+          <div>
+            For XHR/Fetch, max delay is {GLOBAL_CONSTANTS.DELAY_REQUEST_CONSTANTS.MAX_DELAY_VALUE_XHR} ms & for other
+            resources (JS, CSS, Images etc), max delay is{" "}
+            {GLOBAL_CONSTANTS.DELAY_REQUEST_CONSTANTS.MAX_DELAY_VALUE_NON_XHR} ms.
+            <br />
+            You can use{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={LINKS.REQUESTLY_DESKTOP_APP}
+              onClick={() => trackDesktopAppPromoClicked("delay_rule_editor")}
+            >
+              Desktop App
+            </a>{" "}
+            which has no such restrictions.
+          </div>
+        ),
+      },
+    }),
+    []
+  );
 
+  if (!(ruleType in ruleInfoBannerContent)) return null;
   // if appMode is not present in ruleInfoBannerContent then show for all app modes
   if (!ruleInfoBannerContent[ruleType]?.appMode?.includes(appMode)) return null;
 
