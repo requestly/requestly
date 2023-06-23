0ec7a69711bc115c33c0060e3f0b7c44d7ea3648
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-23 17:23:01 +0530

    fix: workspace tooltip display delay

--- app/src/layouts/DashboardLayout/MenuHeader/WorkspaceSelector/index.jsx
+++ app/src/layouts/DashboardLayout/MenuHeader/WorkspaceSelector/index.jsx
@@ -103,7 +103,7 @@ const WorkSpaceDropDown = ({ menu }) => {
           title={prettifyWorkspaceName(activeWorkspaceName)}
           placement={"bottomRight"}
           showArrow={false}
-          mouseEnterDelay={1}
+          mouseEnterDelay={2}
         >
           <span className="items-center active-workspace-name">
             <span className="active-workspace-name">{prettifyWorkspaceName(activeWorkspaceName)}</span>
