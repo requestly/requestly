63c66b8f9db39da6d3d279046a1ce4603e769328
rohanmathur91 <mathurrohan04@gmail.com>
2023-06-28 18:25:20 +0530

    chore: add config types

---
+++ app/src/views/features/sessions/types.ts
@@ -0,0 +1,22 @@
+import { SourceKey, SourceOperator } from "types";
+
+export enum AutoRecordingMode {
+  CUSTOM = "custom",
+  ALL_PAGES = "allPages",
+}
+
+export type PageSource = {
+  key: SourceKey;
+  value: string;
+  isActive?: boolean;
+  operator: SourceOperator;
+};
+
+export type SessionRecordingConfig = {
+  maxDuration: number;
+  pageSources: PageSource[];
+  autoRecording?: {
+    isActive: boolean;
+    mode: AutoRecordingMode;
+  };
+};
