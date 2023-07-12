22065e9e99ec756e0da26d47aaa8db11f65a84dc
rohanmathur91 <mathurrohan04@gmail.com>
2023-07-12 11:41:00 +0530

    `RulePairs` cleanup

--- app/src/components/features/rules/RulePairs/index.js
+++ app/src/components/features/rules/RulePairs/index.js
@@ -1,38 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Collapse, Tooltip } from "antd";
-import Filters from "./Filters";
 import { actions } from "store";
 import { addEmptyPair } from "../RuleBuilder/Body/Columns/AddPairButton/actions";
 import { getCurrentlySelectedRuleData, getResponseRuleResourceType } from "../../../../store/selectors";
-import { trackRuleFilterModalToggled } from "modules/analytics/events/common/rules/filters";
 import { FaTrash } from "react-icons/fa";
-import { setCurrentlySelectedRule } from "../RuleBuilder/actions";
 import ResponseRuleResourceTypes from "./ResponseRuleResourceTypes";
 import { rulePairComponents } from "./Pairs";
 import "./RulePairs.css";
 
 const RulePairs = (props) => {
-  //Global State
   const dispatch = useDispatch();
   const currentlySelectedRuleData = useSelector(getCurrentlySelectedRuleData);
   const responseRuleResourceType = useSelector(getResponseRuleResourceType);
-
   const isInputDisabled = props.mode === "shared-list-rule-view" ? true : false;
 
-  //Component State
-  const [ruleFilterActiveWithPairIndex, setRuleFilterActiveWithPairIndex] = useState(false);
-
   const getPairMarkup = (pair, pairIndex) => {
-    const helperFunctions = {
-      openFilterModal: openFilterModal,
-      getFilterCount: getFilterCount,
-    };
-
     const commonProps = {
       pair: pair,
       pairIndex: pairIndex,
-      helperFunctions: helperFunctions,
       ruleDetails: props.currentlySelectedRuleConfig,
       isInputDisabled: isInputDisabled,
     };
@@ -41,41 +27,13 @@ const RulePairs = (props) => {
     return <Component {...commonProps} />;
   };
 
-  const migrateToNewSourceFilterFormat = (pairIndex, copyOfCurrentlySelectedRule) => {
-    copyOfCurrentlySelectedRule.pairs[pairIndex].source.filters = [
-      copyOfCurrentlySelectedRule.pairs[pairIndex].source.filters,
-    ];
-    setCurrentlySelectedRule(dispatch, copyOfCurrentlySelectedRule);
-  };
-
-  let isSourceFilterFormatUpgraded = (pairIndex, copyOfCurrentlySelectedRule) => {
-    return Array.isArray(copyOfCurrentlySelectedRule.pairs[pairIndex].source.filters) ? true : false;
-  };
-
-  const openFilterModal = (pairIndex) => {
-    const copyOfCurrentlySelectedRule = JSON.parse(JSON.stringify(currentlySelectedRuleData));
-    if (!isSourceFilterFormatUpgraded(pairIndex, copyOfCurrentlySelectedRule)) {
-      migrateToNewSourceFilterFormat(pairIndex, copyOfCurrentlySelectedRule);
+  useEffect(() => {
+    console.log("running....");
+    if (currentlySelectedRuleData.pairs && currentlySelectedRuleData.pairs.length === 0) {
+      console.log("controlled running...");
+      addEmptyPair(currentlySelectedRuleData, props.currentlySelectedRuleConfig, dispatch);
     }
-    setRuleFilterActiveWithPairIndex(pairIndex);
-    trackRuleFilterModalToggled(true, currentlySelectedRuleData?.ruleType);
-  };
-
-  const closeFilterModal = () => {
-    setRuleFilterActiveWithPairIndex(false);
-    trackRuleFilterModalToggled(false, currentlySelectedRuleData?.ruleType);
-  };
-
-  const getFilterCount = (pairIndex) => {
-    const copyOfCurrentlySelectedRule = JSON.parse(JSON.stringify(currentlySelectedRuleData));
-    return isSourceFilterFormatUpgraded(pairIndex, copyOfCurrentlySelectedRule)
-      ? Object.keys(currentlySelectedRuleData.pairs[pairIndex].source.filters[0] || {}).length
-      : Object.keys(currentlySelectedRuleData.pairs[pairIndex].source.filters || {}).length;
-  };
-
-  if (currentlySelectedRuleData.pairs && currentlySelectedRuleData.pairs.length === 0) {
-    addEmptyPair(currentlySelectedRuleData, props.currentlySelectedRuleConfig, dispatch);
-  }
+  }, [dispatch, currentlySelectedRuleData, props.currentlySelectedRuleConfig]);
 
   const deleteButton = (pairIndex) =>
     (props.currentlySelectedRuleConfig.TYPE === "Replace" && !isInputDisabled) ||
@@ -122,14 +80,6 @@ const RulePairs = (props) => {
                 </Collapse.Panel>
               ))
             : null}
-
-          {ruleFilterActiveWithPairIndex !== false ? (
-            <Filters
-              pairIndex={ruleFilterActiveWithPairIndex}
-              closeModal={closeFilterModal}
-              isInputDisabled={isInputDisabled}
-            />
-          ) : null}
         </Collapse>
       ) : null}
     </>
