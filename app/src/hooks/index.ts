02c470144019b6589f0c8a73489395320041aa77
rohanmathur91 <mathurrohan04@gmail.com>
2023-07-13 15:04:27 +0530

    add `useIsExtensionEnabled` hook

--- app/src/hooks/index.ts
+++ app/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { usePrevious } from "./usePrevious";
 export { useHasChanged } from "./useHasChanged";
+export { useIsExtensionEnabled } from "./useIsExtensionEnabled";
---
+++ app/src/hooks/useIsExtensionEnabled.ts
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAppMode } from "store/selectors";
+// @ts-ignore
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import Logger from "lib/logger";
+import { StorageService } from "init";
+import APP_CONSTANTS from "config/constants";
+import { actions } from "store";
+
+export const useIsExtensionEnabled = () => {
+  const dispatch = useDispatch();
+  const appMode = useSelector(getAppMode);
+
+  console.log({ appMode });
+
+  useEffect(() => {
+    if (appMode === GLOBAL_CONSTANTS.APP_MODES.EXTENSION) {
+      Logger.log(`Reading storage in App`);
+      StorageService(appMode)
+        .getRecord(APP_CONSTANTS.RQ_SETTINGS)
+        .then((value) => {
+          if (value) {
+            dispatch(actions.updateIsExtensionEnabled(value.isExtensionEnabled));
+          } else {
+            dispatch(actions.updateIsExtensionEnabled(true));
+          }
+        });
+    }
+  }, [appMode, dispatch]);
+};
