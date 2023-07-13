7b72c529221dfdf6bbeb33c1a9eabc26988ba979
Navdeep Singh Rathore <navdeepsinghrathore2000@gmail.com>
2023-07-14 04:50:19 +0530

    feat: create CURL basesd on network event.

--- app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficExporter/harLogs/utils.ts
+++ app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficExporter/harLogs/utils.ts
@@ -1,7 +1,9 @@
 import { saveAs } from "file-saver";
 
 import { createLogsHar } from "./converter";
-import { Har, RQNetworkLog } from "./types";
+import { Har, HarEntry, RQNetworkLog } from "./types";
+import { cloneDeep } from "lodash";
+import { HTTPSnippet } from "httpsnippet";
 
 export function downloadLogs(logs: RQNetworkLog[]) {
   const harLogs = createLogsHar(logs);
@@ -13,4 +15,20 @@ export function downloadLogs(logs: RQNetworkLog[]) {
 export function downloadHar(har: Har, preferredName?: string) {
   const jsonBlob = new Blob([JSON.stringify(har, null, 2)], { type: "application/json" });
   saveAs(jsonBlob, preferredName ? `${preferredName}.har` : "requestly_logs.har");
+}
+
+export function getCurlFromHarEntry(harEntry: HarEntry): string {
+  try {
+    const entry = cloneDeep(harEntry);
+    // @ts-ignore because HTTPSnippet uses its own HAREntry Type
+    let requestCurl = new HTTPSnippet(entry).convert("shell", "curl", {
+      indent: " ",
+    });
+    if (typeof requestCurl === "boolean") return "";
+    if (Array.isArray(requestCurl)) return requestCurl[0]; // hacky & untested. Covering all possible types
+    return requestCurl;
+  } catch (err) {
+    console.error(`LoggerMiddleware.generate_curl_from_har Error: ${err}`);
+    return "";
+  }
 }
