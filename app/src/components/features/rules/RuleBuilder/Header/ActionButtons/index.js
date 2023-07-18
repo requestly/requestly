ced38d37d13cf6b05cb74a522af24e99c400a6af
Nafees Nehar <nafees87n@gmail.com>
2023-07-18 10:27:05 +0530

    fix: modify request compatibility check

--- app/src/components/features/rules/RuleBuilder/Header/ActionButtons/index.js
+++ app/src/components/features/rules/RuleBuilder/Header/ActionButtons/index.js
@@ -1,24 +1,18 @@
-import { useSelector } from "react-redux";
-import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 //Sub-components
 import CreateRuleButton from "./CreateRuleButton";
 import ShareRuleButton from "./ShareRuleButton";
-import { getCurrentlySelectedRuleData } from "store/selectors";
 import { getModeData } from "../../actions";
 import APP_CONSTANTS from "config/constants";
 import "./RuleEditorActionButtons.css";
 
 const ActionButtons = (props) => {
   const { MODE } = getModeData(props.location);
-  const rule = useSelector(getCurrentlySelectedRuleData);
-
-  const isDisabled = rule?.ruleType === GLOBAL_CONSTANTS.RULE_TYPES.REQUEST;
 
   return (
     <div className="rule-editor-header-action-btns">
       {MODE === APP_CONSTANTS.RULE_EDITOR_CONFIG.MODES.EDIT ? <ShareRuleButton /> : null}
 
-      <CreateRuleButton isDisabled={isDisabled} location={props.location} />
+      <CreateRuleButton location={props.location} />
     </div>
   );
 };
