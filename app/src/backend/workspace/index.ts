0a82cc04cee68077eeeae3465dca5ab357c53809
RuntimeTerror10 <parthb.git@gmail.com>
2023-07-20 16:55:27 +0530

    show org member numbers and avatars in nudge

--- app/src/backend/workspace/index.ts
+++ app/src/backend/workspace/index.ts
@@ -1,4 +1,5 @@
-import { doc, getFirestore, updateDoc } from "firebase/firestore";
+import firebaseApp from "../../firebase";
+import { doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
 
 export const renameWorkspace = async (teamId: string, newName: string) => {
   const db = getFirestore();
@@ -7,4 +8,21 @@ export const renameWorkspace = async (teamId: string, newName: string) => {
   return updateDoc(teamRef, {
     name: newName,
   });
+};
+
+export const getOrganizationMembers = async (domain: string) => {
+  const db = getFirestore(firebaseApp);
+  try {
+    const docRef = doc(db, "organisations", domain);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return docSnap.data();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching domain details:", error);
+    throw new Error("Error fetching domain details");
+  }
 };
--- app/src/components/misc/WorkspaceNudge/index.tsx
+++ app/src/components/misc/WorkspaceNudge/index.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getUserAuthDetails } from "store/selectors";
-import { Typography, Row } from "antd";
+import { Typography, Row, Avatar } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
 import { RQButton } from "lib/design-system/components";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { getDomainFromEmail } from "utils/FormattingHelper";
 import { actions } from "store";
 import { Invite, TeamInvite, TeamInviteMetadata } from "types";
-import "./index.css";
 import JoinWorkspaceModal from "components/user/AccountIndexPage/ManageAccount/ManageTeams/JoinWorkspaceModal";
 import { getUniqueTeamsFromInvites } from "utils/teams";
+import { getOrganizationMembers } from "backend/workspace";
+import "./index.css";
 
 export const WorkspaceNudge: React.FC = () => {
   const dispatch = useDispatch();
@@ -18,6 +19,14 @@ export const WorkspaceNudge: React.FC = () => {
   const [hasActiveWorkspace, setHasActiveWorkspace] = useState<boolean>(false);
   const [teamInvites, setTeamInvites] = useState<TeamInviteMetadata[]>(null);
   const [isJoinWorkspaceModalVisible, setIsJoinWorkspaceModalVisible] = useState<boolean>(false);
+  const [organizationMembers, setOrganizationMembers] = useState(null);
+
+  const memberCount = useMemo(() => {
+    if (organizationMembers) {
+      return Object.keys(organizationMembers).length;
+    } else return 0;
+  }, [organizationMembers]);
+
   const userEmailDomain = useMemo(() => getDomainFromEmail(user?.details?.profile?.email)?.split(".")[0], [
     user?.details?.profile?.email,
   ]);
@@ -30,6 +39,8 @@ export const WorkspaceNudge: React.FC = () => {
       ),
     []
   );
+
+  // const createOrgMap = useMemo(() => httpsCallable(getFunctions(), "createOrganizationMap"), []);
 
   const handleNudgeCTAClick = () => {
     if (hasActiveWorkspace) {
@@ -44,6 +55,12 @@ export const WorkspaceNudge: React.FC = () => {
       );
     }
   };
+
+  useEffect(() => {
+    getOrganizationMembers(getDomainFromEmail(user?.details?.profile?.email))
+      .then((result) => setOrganizationMembers(result))
+      .catch((e) => console.log(e));
+  }, [userEmailDomain, user?.details?.profile?.email]);
 
   useEffect(() => {
     if (user?.loggedIn) {
@@ -68,28 +85,46 @@ export const WorkspaceNudge: React.FC = () => {
 
   return (
     <>
-      <div className="nudge-container">
-        <Row justify="end">
-          <RQButton type="default" className="nudge-close-icon" iconOnly icon={<CloseOutlined />} />
-        </Row>
-        <div>IMAGES HERE</div>
-        <Typography.Text className="display-block nudge-text">
-          {hasActiveWorkspace
-            ? "22 users from Amazon are collaborating on a team workspace"
-            : "22 users from Amazon are using Requestly."}
-        </Typography.Text>
-        <RQButton type="primary" className="mt-8 text-bold" onClick={handleNudgeCTAClick}>
-          {hasActiveWorkspace ? "Join your teammates" : "Start collaborating"}
-        </RQButton>
-      </div>
-      {isJoinWorkspaceModalVisible && (
-        <JoinWorkspaceModal
-          isOpen={isJoinWorkspaceModalVisible}
-          teamInvites={teamInvites}
-          handleModalClose={() => setIsJoinWorkspaceModalVisible(false)}
-          allowCreateNewWorkspace={false}
-        />
-      )}
+      {memberCount >= 3 ? (
+        <>
+          <div className="nudge-container">
+            <Row justify="end">
+              <RQButton type="default" className="nudge-close-icon" iconOnly icon={<CloseOutlined />} />
+            </Row>
+            <div className="avatar-row-container">
+              <Avatar.Group maxCount={3}>
+                {Object.keys(organizationMembers).map((memberId) => (
+                  <Avatar
+                    src={
+                      organizationMembers[memberId].photoURL.length
+                        ? organizationMembers[memberId].photoURL
+                        : "https://www.gravatar.com/avatar/000?d=mp&f=y"
+                    }
+                    alt="organization member"
+                  />
+                ))}
+              </Avatar.Group>
+            </div>
+            <Typography.Text className="display-block nudge-text">
+              {hasActiveWorkspace
+                ? `${memberCount} users from ${userEmailDomain} are collaborating on a team workspace`
+                : `${memberCount} users from ${userEmailDomain} are using Requestly.`}
+            </Typography.Text>
+            <RQButton type="primary" className="mt-8 text-bold" onClick={handleNudgeCTAClick}>
+              {hasActiveWorkspace ? "Join your teammates" : "Start collaborating"}
+            </RQButton>
+            {/* <RQButton onClick={() => createOrgMap()}>TEST</RQButton> */}
+          </div>
+          {isJoinWorkspaceModalVisible && (
+            <JoinWorkspaceModal
+              isOpen={isJoinWorkspaceModalVisible}
+              teamInvites={teamInvites}
+              handleModalClose={() => setIsJoinWorkspaceModalVisible(false)}
+              allowCreateNewWorkspace={false}
+            />
+          )}
+        </>
+      ) : null}
     </>
   );
 };
