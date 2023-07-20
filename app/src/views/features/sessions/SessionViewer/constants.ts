c374c785b9fbde57b14c750b1274b37e0d50ede4
rohanmathur91 <mathurrohan04@gmail.com>
2023-07-20 22:51:40 +0530

    refactor: constants

---
+++ app/src/views/features/sessions/SessionViewer/constants.ts
@@ -0,0 +1,4 @@
+import { CheckboxValueType } from "antd/lib/checkbox/Group";
+import { DebugInfo } from "./types";
+
+export const defaultDebugInfo: CheckboxValueType[] = [DebugInfo.INCLUDE_NETWORK_LOGS, DebugInfo.INCLUDE_CONSOLE_LOGS];
