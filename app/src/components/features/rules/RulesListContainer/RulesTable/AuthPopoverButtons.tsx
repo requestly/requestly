5c2ceb152e19357f41a593a5d0d7c1f581886d1b
Nafees Nehar <nafees87n@gmail.com>
2023-07-21 14:17:41 +0530

    bypass login check on popover

---
+++ app/src/components/features/rules/RulesListContainer/RulesTable/AuthPopoverButtons.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Button } from "antd";
+import { AuthConfirmationPopover } from "components/hoc/auth/AuthConfirmationPopover";
+
+interface Props {
+  icon: React.ReactNode;
+  shape: any;
+  type?: any;
+  buttonText: string;
+  isTooltipShown: boolean;
+  onClickHandler: () => void;
+  hasPopconfirm: boolean;
+  tourId?: any;
+  trackClickEvent: () => void;
+  isScreenSmall: boolean;
+  isLoggedIn: boolean;
+  authSource?: string;
+}
+
+const AuthPopoverButton: React.FC<Props> = ({
+  icon,
+  shape,
+  type = null,
+  buttonText,
+  isTooltipShown,
+  onClickHandler,
+  hasPopconfirm = false,
+  tourId = null,
+  trackClickEvent = () => {},
+  isLoggedIn,
+  isScreenSmall,
+  authSource,
+}) => {
+  return (
+    <AuthConfirmationPopover
+      title={`You need to sign up to ${buttonText.toLowerCase()} rules`}
+      disabled={!hasPopconfirm}
+      callback={onClickHandler}
+      source={"cool" || authSource}
+    >
+      <Button
+        type={type || "default"}
+        shape={isScreenSmall ? shape : null}
+        onClick={() => {
+          trackClickEvent();
+          if (hasPopconfirm) {
+            isLoggedIn && onClickHandler();
+          } else {
+            onClickHandler();
+          }
+        }}
+        icon={icon}
+        data-tour-id={tourId}
+      >
+        {!isTooltipShown ? buttonText : isScreenSmall ? null : buttonText}
+      </Button>
+    </AuthConfirmationPopover>
+  );
+};
+
+export default AuthPopoverButton;
