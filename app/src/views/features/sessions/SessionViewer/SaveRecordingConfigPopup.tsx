2a09bc8fb66254780bd841f4c08d5eb3986a118a
Rohan Mathur <mathurrohan04@gmail.com>
2023-07-24 23:58:08 +0530

    [RQ-644] feat: session recording import/export (#868)
    * chore: update types
    
    * add import session modal
    
    * update session utils
    
    * add save session dropdown menu
    
    * chore: update analytics events
    
    * fix: add file type check + modal close on invalid file
    
    * fix(style): save btn icon
    
    * chore: updated types
    
    * fix: null check
    
    * refactor: session utils
    
    * fix: null check
    
    * add `SaveRecordingConfigPopup` component
    
    * refactor: constants
    
    * refactor: `DraftSessionViewer` compoennt
    
    * feat: add download cta on saved sessions
    
    * fix: split button type and style
    
    * cleanup
    
    * fix: draft session reload
    
    * fix: exported file extension
    
    * chore: add download analytic event for already saved session
    
    * cleanup
    
    * fix typo
    
    * fix: browse button type and size
    
    * fix: import modal message
    
    * add toast for session name change
    
    * cleanup
    
    * fix: update icon
    
    * update save and download button text
    
    * fix: import session modal trigger
    
    * fix: saved session logs config
    
    * fix: remove navigation to settings after signup
    
    * refactor: toggleImportSessionModal
    
    * cleanup
    
    * refactor: `SaveRecordingConfigPopup` component
    
    * cleanup
    
    * refactor: type `SessionRecording` to `SessionRecordingMetadata`
    
    * refactor: `sessionRecording` to `sessionRecordingMetadata` term
    
    * fix typo

---
+++ app/src/views/features/sessions/SessionViewer/SaveRecordingConfigPopup.tsx
@@ -0,0 +1,237 @@
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { CloseOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Radio, Row } from "antd";
+import { DebugInfo, SessionSaveMode } from "./types";
+import { CheckboxValueType } from "antd/lib/checkbox/Group";
+import {
+  compressEvents,
+  downloadSession,
+  getRecordingOptionsToSave,
+  getSessionEventsToSave,
+  getSessionRecordingOptions,
+  prepareSessionToExport,
+} from "./sessionEventsUtils";
+import { getSessionRecordingMetaData, getSessionRecordingEvents } from "store/features/session-recording/selectors";
+import { toast } from "utils/Toast";
+import { getUserAuthDetails } from "store/selectors";
+import { getCurrentlyActiveWorkspace } from "store/features/teams/selectors";
+import { actions } from "store";
+import APP_CONSTANTS from "config/constants";
+import { AUTH } from "modules/analytics/events/common/constants";
+import { saveRecording } from "backend/sessionRecording/saveRecording";
+import PATHS from "config/constants/sub/paths";
+import {
+  trackDraftSessionSaveFailed,
+  trackDraftSessionSaved,
+} from "modules/analytics/events/features/sessionRecording";
+
+interface Props {
+  onClose: (e?: React.MouseEvent) => void;
+}
+
+const { ACTION_LABELS: AUTH_ACTION_LABELS } = APP_CONSTANTS.AUTH;
+const defaultDebugInfo: CheckboxValueType[] = [DebugInfo.INCLUDE_NETWORK_LOGS, DebugInfo.INCLUDE_CONSOLE_LOGS];
+
+const SaveRecordingConfigPopup: React.FC<Props> = ({ onClose }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { tabId } = useParams();
+  const { pathname } = useLocation();
+  const user = useSelector(getUserAuthDetails);
+  const workspace = useSelector(getCurrentlyActiveWorkspace);
+  const sessionRecordingMetadata = useSelector(getSessionRecordingMetaData);
+  const sessionEvents = useSelector(getSessionRecordingEvents);
+  const [isSaving, setIsSaving] = useState(false);
+  const [sessionSaveMode, setSessionSaveMode] = useState<SessionSaveMode>(SessionSaveMode.ONLINE);
+  const [includedDebugInfo, setIncludedDebugInfo] = useState<CheckboxValueType[]>(defaultDebugInfo);
+  const isDraftSession = pathname.includes("draft");
+  const isSessionLogOptionsAlreadySaved = tabId === "imported" || !isDraftSession;
+
+  const savedSessionRecordingOptions = useMemo(() => getSessionRecordingOptions(sessionRecordingMetadata?.options), [
+    sessionRecordingMetadata?.options,
+  ]);
+
+  const isIncludeNetworkLogsDisabled =
+    isSessionLogOptionsAlreadySaved && !savedSessionRecordingOptions.includes(DebugInfo.INCLUDE_NETWORK_LOGS);
+  const isIncludeConsoleLogsDisabled =
+    isSessionLogOptionsAlreadySaved && !savedSessionRecordingOptions.includes(DebugInfo.INCLUDE_CONSOLE_LOGS);
+
+  useEffect(() => {
+    if (isSessionLogOptionsAlreadySaved) {
+      setIncludedDebugInfo(savedSessionRecordingOptions);
+    }
+  }, [isSessionLogOptionsAlreadySaved, savedSessionRecordingOptions]);
+
+  const saveDraftSession = useCallback(
+    (e: React.MouseEvent) => {
+      if (!user?.loggedIn) {
+        dispatch(
+          actions.toggleActiveModal({
+            modalName: "authModal",
+            newValue: true,
+            newProps: {
+              authMode: AUTH_ACTION_LABELS.SIGN_UP,
+              src: window.location.href,
+              eventSource: AUTH.SOURCE.SAVE_DRAFT_SESSION,
+            },
+          })
+        );
+        return;
+      }
+
+      if (isSaving) {
+        return;
+      }
+
+      if (!sessionRecordingMetadata?.name) {
+        toast.error("Name is required to save the recording.");
+        return;
+      }
+
+      const recordingOptionsToSave = getRecordingOptionsToSave(includedDebugInfo);
+
+      setIsSaving(true);
+      saveRecording(
+        user?.details?.profile?.uid,
+        workspace?.id,
+        sessionRecordingMetadata,
+        compressEvents(getSessionEventsToSave(sessionEvents, recordingOptionsToSave)),
+        recordingOptionsToSave
+      ).then((response) => {
+        if (response?.success) {
+          onClose();
+          toast.success("Recording saved successfully");
+          trackDraftSessionSaved(
+            sessionRecordingMetadata?.sessionAttributes?.duration,
+            recordingOptionsToSave,
+            SessionSaveMode.ONLINE
+          );
+          navigate(PATHS.SESSIONS.RELATIVE + "/saved/" + response?.firestoreId, {
+            replace: true,
+            state: { fromApp: true, viewAfterSave: true },
+          });
+        } else {
+          toast.error(response?.message);
+          trackDraftSessionSaveFailed(response?.message);
+          setIsSaving(false);
+        }
+      });
+    },
+    [
+      user?.loggedIn,
+      user?.details?.profile?.uid,
+      workspace?.id,
+      isSaving,
+      includedDebugInfo,
+      sessionEvents,
+      sessionRecordingMetadata,
+      dispatch,
+      navigate,
+      onClose,
+    ]
+  );
+
+  const handleDownloadFileClick = useCallback(
+    (e: React.MouseEvent) => {
+      if (!user?.loggedIn) {
+        dispatch(
+          actions.toggleActiveModal({
+            modalName: "authModal",
+            newValue: true,
+            newProps: {
+              authMode: AUTH_ACTION_LABELS.SIGN_UP,
+              src: window.location.href,
+              eventSource: AUTH.SOURCE.SAVE_DRAFT_SESSION,
+            },
+          })
+        );
+        return;
+      }
+
+      setIsSaving(true);
+      const recordingOptionsToSave = getRecordingOptionsToSave(includedDebugInfo);
+      const events = compressEvents(getSessionEventsToSave(sessionEvents, recordingOptionsToSave));
+      const metadata = {
+        name: sessionRecordingMetadata.name,
+        options: { ...recordingOptionsToSave },
+        sessionAttributes: { ...sessionRecordingMetadata.sessionAttributes },
+      };
+
+      prepareSessionToExport(events, metadata)
+        .then((fileContent) => downloadSession(fileContent, sessionRecordingMetadata.name))
+        .finally(() => {
+          toast.success("Recording downloaded successfully.");
+          onClose();
+          setIsSaving(false);
+          trackDraftSessionSaved(
+            sessionRecordingMetadata.sessionAttributes?.duration,
+            recordingOptionsToSave,
+            SessionSaveMode.LOCAL
+          );
+        });
+    },
+    [dispatch, user?.loggedIn, sessionEvents, sessionRecordingMetadata, includedDebugInfo, onClose]
+  );
+
+  return (
+    <div className="save-recording-config-popup">
+      <Row align="middle" justify="space-between" className="w-full header-container">
+        <div className="header">{isDraftSession ? "Save" : "Download"} your session recording</div>
+        <Button onClick={onClose}>
+          <CloseOutlined />
+        </Button>
+      </Row>
+      <div className="config-container">
+        {isDraftSession && (
+          <div>
+            <div className="config-label">Mode of saving the session</div>
+            <Radio.Group
+              value={sessionSaveMode}
+              className="mode-radio-group"
+              onChange={(e) => setSessionSaveMode(e.target.value)}
+            >
+              <Radio value={SessionSaveMode.ONLINE}>Save online</Radio>
+              <Radio value={SessionSaveMode.LOCAL}>Download as a file</Radio>
+            </Radio.Group>
+          </div>
+        )}
+
+        <div>
+          <div className="config-label">Information to include in session</div>
+          <Checkbox.Group
+            onChange={setIncludedDebugInfo}
+            value={includedDebugInfo}
+            options={[
+              {
+                label: "Network Logs",
+                value: "includeNetworkLogs",
+                disabled: isIncludeNetworkLogsDisabled,
+              },
+              {
+                label: "Console Logs",
+                value: "includeConsoleLogs",
+                disabled: isIncludeConsoleLogsDisabled,
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <Row className="w-full footer">
+        <Button
+          type="primary"
+          className="ml-auto"
+          loading={isSaving}
+          onClick={
+            isDraftSession && sessionSaveMode === SessionSaveMode.ONLINE ? saveDraftSession : handleDownloadFileClick
+          }
+        >
+          {isDraftSession && sessionSaveMode === SessionSaveMode.ONLINE ? "Save" : "Download"}
+        </Button>
+      </Row>
+    </div>
+  );
+};
+
+export default SaveRecordingConfigPopup;
---
+++ app/src/views/features/sessions/SessionViewer/constants.ts
@@ -0,0 +1,2 @@
+export const SESSION_EXPORT_TYPE = "SESSION";
+export const EXPORTED_SESSION_FILE_EXTENSION = "rqly";
