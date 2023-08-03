08d2a74dde2aa2dab1436b2e1b25e9eb88af20ab
rohanmathur91 <mathurrohan04@gmail.com>
2023-08-03 19:21:13 +0530

    chore: add comment

--- app/src/utils/isAppOpenedInIframe.ts
+++ app/src/utils/isAppOpenedInIframe.ts
@@ -2,6 +2,7 @@ export const isAppOpenedInIframe = (): boolean => {
   try {
     return window.self !== window.top;
   } catch (e) {
+    // Browsers can block access to window.top due to same origin policy.
     return true;
   }
 };
