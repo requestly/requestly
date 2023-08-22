09a3efcb507bd3ee2c69e1432e1e31647038a25e
RuntimeTerror10 <parthb.git@gmail.com>
2023-08-22 16:35:02 +0530

    disable test this rule box if unsaved changes are present

--- app/src/components/features/rules/TestThisRule/index.tsx
+++ app/src/components/features/rules/TestThisRule/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { getIsCurrentlySelectedRuleHasUnsavedChanges } from "store/selectors";
 import { Row, Typography } from "antd";
 import { RQButton, RQInput } from "lib/design-system/components";
 import { isValidUrl } from "utils/FormattingHelper";
@@ -7,6 +9,7 @@ import { BsFillLightningChargeFill } from "@react-icons/all-files/bs/BsFillLight
 import "./index.css";
 
 export const TestThisRuleRow: React.FC = () => {
+  const isCurrentlySelectedRuleHasUnsavedChanges = useSelector(getIsCurrentlySelectedRuleHasUnsavedChanges);
   const [pageUrl, setPageUrl] = useState("");
   const [error, setError] = useState(null);
 
@@ -24,26 +27,35 @@ export const TestThisRuleRow: React.FC = () => {
   };
 
   return (
-    <div className="test-this-rule-row w-full">
-      <div className="test-this-rule-row-header text-bold subtitle">Test this rule</div>
-      <div className="test-this-rule-row-body">
-        <Row>
-          <div style={{ flex: 1 }}>
-            <RQInput
-              placeholder="Enter the URL you want to test"
-              value={pageUrl}
-              onChange={(e) => setPageUrl(e.target.value)}
-              status={error ? "error" : ""}
-            />
-            <Typography.Text type="danger" className="caption">
-              {error}
-            </Typography.Text>
-          </div>
-          <RQButton type="primary" size="large" className="start-test-rule-btn" onClick={handleStartTestRule}>
-            <BsFillLightningChargeFill className="start-test-rule-btn-icon" /> Test Rule
-          </RQButton>
-        </Row>
-        {/* ADD CHECKBOX FOR SESSION REPLAY HERE IN V1 */}
+    <div className="test-this-rule-row-wrapper">
+      {isCurrentlySelectedRuleHasUnsavedChanges && (
+        <Typography.Text>Please save your changes first to test this rule</Typography.Text>
+      )}
+      <div
+        className={`test-this-rule-row w-full ${
+          isCurrentlySelectedRuleHasUnsavedChanges && "disabled-test-this-rule-row"
+        }`}
+      >
+        <div className="test-this-rule-row-header text-bold subtitle">Test this rule</div>
+        <div className="test-this-rule-row-body">
+          <Row>
+            <div style={{ flex: 1 }}>
+              <RQInput
+                placeholder="Enter the URL you want to test"
+                value={pageUrl}
+                onChange={(e) => setPageUrl(e.target.value)}
+                status={error ? "error" : ""}
+              />
+              <Typography.Text type="danger" className="caption">
+                {error}
+              </Typography.Text>
+            </div>
+            <RQButton type="primary" size="large" className="start-test-rule-btn" onClick={handleStartTestRule}>
+              <BsFillLightningChargeFill className="start-test-rule-btn-icon" /> Test Rule
+            </RQButton>
+          </Row>
+          {/* ADD CHECKBOX FOR SESSION REPLAY HERE IN V1 */}
+        </div>
       </div>
     </div>
   );
