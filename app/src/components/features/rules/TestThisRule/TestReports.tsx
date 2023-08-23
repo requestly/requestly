d10c8d5c060ca7c99e71e7471cc1b1848ffd7a96
RuntimeTerror10 <parthb.git@gmail.com>
2023-08-24 01:45:41 +0530

    show list of test reports

--- app/src/components/features/rules/TestThisRule/TestReports.tsx
+++ app/src/components/features/rules/TestThisRule/TestReports.tsx
@@ -1,18 +1,79 @@
-import React from "react";
-import { CheckOutlined } from "@ant-design/icons";
+import React, { useState, useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
+import { getAppMode, getCurrentlySelectedRuleData } from "store/selectors";
+import { StorageService } from "init";
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
+import { TestReport } from "./types";
+// //@ts-ignore
+// import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import "./index.css";
 
 export const TestReports: React.FC = () => {
+  const appMode = useSelector(getAppMode);
+  const currentlySelectedRuleData = useSelector(getCurrentlySelectedRuleData);
+  const [testReports, setTestReports] = useState(null);
+
+  const getFormattedReportCreatedTime = useCallback((timestamp: number) => {
+    const date = new Date(timestamp);
+
+    const day = date.getDate();
+    const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(date);
+    const year = date.getFullYear();
+
+    const hours = date.getHours() % 12 || 12;
+    const ampm = date.getHours() >= 12 ? "PM" : "AM";
+    const minutes = date.getMinutes();
+
+    const formattedDate = `${day} ${month} ${year} - ${hours}:${minutes < 10 ? "0" : ""}${minutes}${ampm}`;
+    return formattedDate;
+  }, []);
+
+  useEffect(() => {
+    StorageService(appMode)
+      .getRecord("test_reports") //TODO: Use the storage key constant after its fixed
+      .then((data) => {
+        if (data) {
+          const reports = Object.values(data).filter(
+            (report: TestReport) => report.ruleId === currentlySelectedRuleData.id
+          );
+          if (reports.length) setTestReports(reports);
+        }
+      });
+    StorageService(appMode)
+      .getAllRecords() //TODO: Use the storage key constant after its fixed
+      .then((data) => {
+        console.log({ data });
+      });
+  }, [appMode, currentlySelectedRuleData.id]);
+
+  console.log({ testReports });
+
   return (
     <>
-      <div className="test-this-rule-row-header test-this-rule-results-header text-bold subtitle">Previous results</div>
-      <div className="test-this-rule-report-row">
-        <div className="text-white text-bold">mail.google.com</div>
-        <div className="text-gray">16 July 2023- 11:40AM</div>
-        <div className="text-gray test-this-rule-report-status">
-          <CheckOutlined /> Rule executed
-        </div>
-      </div>
+      {testReports && (
+        <>
+          <div className="test-this-rule-row-header test-this-rule-results-header text-bold subtitle">
+            Previous results
+          </div>
+          {testReports.map((report: TestReport, index: number) => (
+            <div className="test-this-rule-report-row" key={index}>
+              <div className="text-white text-bold">{report.url}</div>
+              <div className="text-gray">{getFormattedReportCreatedTime(report.timestamp)}</div>
+              <div className="text-gray test-this-rule-report-status">
+                {report.appliedStatus ? (
+                  <>
+                    <CheckOutlined style={{ color: "var(--success" }} /> Rule executed
+                  </>
+                ) : (
+                  <>
+                    <CloseOutlined style={{ color: "var(--danger" }} /> Failed
+                  </>
+                )}
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </>
   );
 };
---
+++ app/src/components/features/rules/TestThisRule/types.ts
@@ -0,0 +1,8 @@
+export type TestReport = {
+  id: string;
+  ruleId: string;
+  url: string;
+  sessionLink: string;
+  timestamp: number;
+  appliedStatus: boolean;
+};
