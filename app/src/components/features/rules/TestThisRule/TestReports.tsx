1fb7876f852161e13388be2aa7bb877da943ec0b
RuntimeTerror10 <parthb.git@gmail.com>
2023-08-24 15:43:09 +0530

    refresh reports list on view result

--- app/src/components/features/rules/TestThisRule/TestReports.tsx
+++ app/src/components/features/rules/TestThisRule/TestReports.tsx
@@ -6,12 +6,15 @@ import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 import { TestReport } from "./types";
 //@ts-ignore
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import PageScriptMessageHandler from "config/PageScriptMessageHandler";
 import "./index.css";
 
 export const TestReports: React.FC = () => {
   const appMode = useSelector(getAppMode);
   const currentlySelectedRuleData = useSelector(getCurrentlySelectedRuleData);
   const [testReports, setTestReports] = useState(null);
+  const [newReportId, setNewReportId] = useState(null);
+  const [refreshTestReports, setRefreshTestReports] = useState(true);
 
   const getFormattedReportCreatedTime = useCallback((timestamp: number) => {
     const date = new Date(timestamp);
@@ -29,18 +32,37 @@ export const TestReports: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    StorageService(appMode)
-      .getRecord(GLOBAL_CONSTANTS.STORAGE_KEYS.TEST_REPORTS)
-      .then((data) => {
-        if (data) {
-          const reports = Object.values(data)
-            .filter((report: TestReport) => report.ruleId === currentlySelectedRuleData.id)
-            .sort((report1: TestReport, report2: TestReport) => report2.timestamp - report1.timestamp);
+    PageScriptMessageHandler.addMessageListener(
+      GLOBAL_CONSTANTS.EXTENSION_MESSAGES.NOTIFY_TEST_RULE_REPORT_UPDATED,
+      (message: { testReportId: string }) => {
+        setRefreshTestReports(true);
+        setNewReportId(message.testReportId);
+      }
+    );
+  }, []);
 
-          if (reports.length) setTestReports(reports);
-        }
-      });
-  }, [appMode, currentlySelectedRuleData.id]);
+  console.log({ newReportId });
+
+  useEffect(() => {
+    console.log("LIST");
+    if (refreshTestReports) {
+      console.log("LIST2");
+      StorageService(appMode)
+        .getRecord(GLOBAL_CONSTANTS.STORAGE_KEYS.TEST_REPORTS)
+        .then((data) => {
+          if (data) {
+            const reports = Object.values(data)
+              .filter((report: TestReport) => report.ruleId === currentlySelectedRuleData.id)
+              .sort((report1: TestReport, report2: TestReport) => report2.timestamp - report1.timestamp);
+
+            if (reports.length) setTestReports(reports);
+          }
+        })
+        .finally(() => {
+          setRefreshTestReports(false);
+        });
+    }
+  }, [appMode, currentlySelectedRuleData.id, refreshTestReports]);
 
   return (
     <>
