63d7c52f9929a2565dd5ddb8f56183a11f1011be
rohanmathur91 <mathurrohan04@gmail.com>
2023-09-14 18:58:22 +0530

    feat: add replay request in session network logs

--- app/src/views/features/sessions/SessionViewer/NetworkLogs/NetworkLogsPanel.tsx
+++ app/src/views/features/sessions/SessionViewer/NetworkLogs/NetworkLogsPanel.tsx
@@ -40,21 +40,11 @@ const NetworkLogsPanel: React.FC<Props> = ({ startTime, networkLogs, playerTimeO
     () =>
       [
         {
-          key: "copy_url",
-          label: "Copy URL",
-          onSelect: (key, log) => {
-            copyToClipBoard(log.entry.request.url, "URL copied to clipboard");
-            trackSessionRecordingNetworkLogContextMenuOptionClicked(key);
-          },
-        },
-        {
-          type: "divider",
-        },
-        {
-          key: "replay_request",
+          key: "replayRequest",
           label: "Replay Request",
+
           onSelect: (key, log) => {
-            const { url, method, headers, postData } = log.entry.request ?? {};
+            const { url, method, headers, postData } = log.entry.request;
 
             setSelectedRequestData({
               url,
