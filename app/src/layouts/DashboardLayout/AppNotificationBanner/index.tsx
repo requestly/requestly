5f432d7eb2784869730c4e2f60009e1012a0fca1
Sahil Gupta <sahil865gupta@gmail.com>
2023-09-18 13:45:42 +0530

    added markdown support

--- app/src/layouts/DashboardLayout/AppNotificationBanner/index.tsx
+++ app/src/layouts/DashboardLayout/AppNotificationBanner/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { actions } from "store";
 import { RQButton } from "lib/design-system/components";
-import { ReactComponent as RightArrow } from "assets/icons/right-arrow.svg";
 import { trackProductHuntBannerClicked } from "modules/analytics/events/common/onboarding/appBanner";
 import { useDispatch } from "react-redux";
 import { isAppOpenedInIframe } from "utils/AppUtils";
@@ -9,6 +8,8 @@ import "./appNotificationBanner.scss";
 import { useFeatureValue } from "@growthbook/growthbook-react";
 import { useSelector } from "react-redux";
 import { getAppNotificationBannerDismissTs } from "store/selectors";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 
 interface Banner {
   id: string;
@@ -50,24 +51,28 @@ export const AppNotificationBanner = () => {
           rel="noreferrer"
           className="app-banner"
           onClick={() => trackProductHuntBannerClicked()}
-          href={banner?.cta}
+          href={banner?.cta ? banner.cta : null}
           style={{ backgroundColor: banner?.backgroundColor || "000000" }}
         >
-          {banner.text}
-          <RightArrow className="right-arrow-icon" />
-          <RQButton
-            iconOnly
-            className="close-btn"
-            onClick={handleCloseBannerClick}
-            icon={
-              <svg width="11.67" height="11.67" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill="#ffffff"
-                  d="M1.08736 1.08492C1.31516 0.857111 1.68451 0.857111 1.91232 1.08492L4.99984 4.17244L8.08736 1.08492C8.31517 0.857111 8.68451 0.857111 8.91232 1.08492C9.14012 1.31272 9.14012 1.68207 8.91232 1.90988L5.8248 4.9974L8.91232 8.08492C9.14012 8.31272 9.14012 8.68207 8.91232 8.90988C8.68451 9.13768 8.31517 9.13768 8.08736 8.90988L4.99984 5.82235L1.91232 8.90988C1.68451 9.13768 1.31516 9.13768 1.08736 8.90988C0.859552 8.68207 0.859552 8.31272 1.08736 8.08492L4.17488 4.9974L1.08736 1.90988C0.859552 1.68207 0.859552 1.31272 1.08736 1.08492Z"
-                />
-              </svg>
-            }
-          />
+          <div className="app-banner-text">
+            {/* @ts-expect-error */}
+            <ReactMarkdown children={banner?.text} skipHtml={false} rehypePlugins={[rehypeRaw]} />
+          </div>
+          <div className="close-button-container">
+            <RQButton
+              iconOnly
+              className="close-btn"
+              onClick={handleCloseBannerClick}
+              icon={
+                <svg width="11.67" height="11.67" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill="#ffffff"
+                    d="M1.08736 1.08492C1.31516 0.857111 1.68451 0.857111 1.91232 1.08492L4.99984 4.17244L8.08736 1.08492C8.31517 0.857111 8.68451 0.857111 8.91232 1.08492C9.14012 1.31272 9.14012 1.68207 8.91232 1.90988L5.8248 4.9974L8.91232 8.08492C9.14012 8.31272 9.14012 8.68207 8.91232 8.90988C8.68451 9.13768 8.31517 9.13768 8.08736 8.90988L4.99984 5.82235L1.91232 8.90988C1.68451 9.13768 1.31516 9.13768 1.08736 8.90988C0.859552 8.68207 0.859552 8.31272 1.08736 8.08492L4.17488 4.9974L1.08736 1.90988C0.859552 1.68207 0.859552 1.31272 1.08736 1.08492Z"
+                  />
+                </svg>
+              }
+            />
+          </div>
         </a>
       );
     }
