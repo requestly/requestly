648edb9e8b527be8e25a1ed0558fdfcad0cf2799
Rohan Mathur <mathurrohan04@gmail.com>
2023-09-20 18:26:26 +0600

    [RQ-852]: add rule creation support from session recording network logs (#1061)
    * feat: add replay request in session network logs
    
    * refactor props
    
    * fix: request body format
    
    * cleanup
    
    * add copy url option
    
    * feat: rule creation from session recording network logs
    
    * fix: events
    
    * refactor
    
    * fix: request data type
    
    * wrapped in try-catch block

--- app/src/views/features/sessions/SessionViewer/NetworkLogs/helpers.ts
+++ app/src/views/features/sessions/SessionViewer/NetworkLogs/helpers.ts
@@ -4,6 +4,14 @@ import { RQNetworkEventErrorCodes, RQSessionAttributes } from "@requestly/web-sd
 import { RQNetworkLog } from "lib/design-system/components/RQNetworkTable/types";
 import { captureException } from "@sentry/react";
 
+const getStringifiedData = (data: unknown) => {
+  try {
+    return typeof data === "string" ? data : JSON.stringify(data) ?? "";
+  } catch (error) {
+    return "";
+  }
+};
+
 const getRequestObject = (networkLog: NetworkLog) => {
   let queryString = [] as RQNetworkLog["entry"]["request"]["queryString"];
 
@@ -27,7 +35,7 @@ const getRequestObject = (networkLog: NetworkLog) => {
       mimeType: "",
       text: networkLog?.errors?.includes(RQNetworkEventErrorCodes.REQUEST_TOO_LARGE)
         ? "Payload too large to capture"
-        : networkLog.requestData,
+        : getStringifiedData(networkLog.requestData),
       comment: "",
     },
   } as RQNetworkLog["entry"]["request"];
@@ -43,7 +51,7 @@ const getResponseObject = (networkLog: NetworkLog) => {
       mimeType: networkLog.contentType,
       text: networkLog?.errors?.includes(RQNetworkEventErrorCodes.RESPONSE_TOO_LARGE)
         ? "Payload too large to capture"
-        : networkLog.response,
+        : getStringifiedData(networkLog.response),
       comment: "",
     },
     redirectURL: networkLog.responseURL,
