1a6cd95b0a2c02fe606a39c153379cd6f73d2ae5
rohanmathur91 <mathurrohan04@gmail.com>
2023-09-22 16:59:16 +0530

    fix: accordion icons

--- app/src/components/common/PinExtensionPopup/index.tsx
+++ app/src/components/common/PinExtensionPopup/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Button, Collapse } from "antd";
 import { RQModal } from "lib/design-system/components";
-import { IoCloseOutline } from "@react-icons/all-files/io5/IoCloseOutline";
+import { TfiClose } from "@react-icons/all-files/tfi/TfiClose";
 import { BsPin } from "@react-icons/all-files/bs/BsPin";
+import { BiSliderAlt } from "@react-icons/all-files/bi/BiSliderAlt";
+import { PiCaretDownBold } from "@react-icons/all-files/pi/PiCaretDownBold";
+import { FiVideo } from "@react-icons/all-files/fi/FiVideo";
+import { FaToggleOff } from "@react-icons/all-files/fa6/FaToggleOff";
 import { ReactComponent as BlueUnderline } from "./assets/blue-underline.svg";
 import "./PinExtensionPopup.css";
 
@@ -21,6 +25,56 @@ export const PinExtensionPopup: React.FC<Props> = ({ isOpen, onCancel }) => {
     onCancel();
     // send analytics
   };
+
+  const collapsePanelItems = useMemo(
+    () => [
+      {
+        header: (
+          <div>
+            <BiSliderAlt /> Activate/Deactivate rules
+          </div>
+        ),
+        image: {
+          alt: "Activate/Deactivate rules",
+          src: "https://dummyimage.com/600x400/000/fff",
+        },
+      },
+      {
+        header: (
+          <div>
+            <BiSliderAlt style={{ transform: "rotate(90deg)" }} /> Know which rules executed on a web page
+          </div>
+        ),
+        image: {
+          alt: "Executed rules",
+          src: "https://dummyimage.com/600x400/000/fff",
+        },
+      },
+      {
+        header: (
+          <div>
+            <FiVideo /> Record and replay browser activity
+          </div>
+        ),
+        image: {
+          alt: "Record and replay session",
+          src: "https://dummyimage.com/600x400/000/fff",
+        },
+      },
+      {
+        header: (
+          <div>
+            <FaToggleOff /> Pause Requestly when not in use
+          </div>
+        ),
+        image: {
+          alt: "Pause requestly extension",
+          src: "https://dummyimage.com/600x400/000/fff",
+        },
+      },
+    ],
+    []
+  );
 
   return (
     <RQModal
@@ -54,49 +108,25 @@ export const PinExtensionPopup: React.FC<Props> = ({ isOpen, onCancel }) => {
 
         <div>
           <div className="subtitle">Access Requestly features quickly from the extension</div>
-          <Collapse ghost accordion>
-            <Collapse.Panel header="Activate/Deactivate rules" key="0">
-              <img
-                width={338}
-                height={196}
-                className="feature-gif"
-                alt="Activate/Deactivate rules"
-                src="https://dummyimage.com/600x400/000/fff"
-              />
-            </Collapse.Panel>
-            <Collapse.Panel header="Know which rules executed on a web page" key="1">
-              <img
-                width={338}
-                height={196}
-                className="feature-gif"
-                alt="Executed rules"
-                src="https://dummyimage.com/600x400/000/fff"
-              />
-            </Collapse.Panel>
-            <Collapse.Panel header="Record and replay browser activity" key="2">
-              <img
-                width={338}
-                height={196}
-                className="feature-gif"
-                alt="Record and replay session"
-                src="https://dummyimage.com/600x400/000/fff"
-              />
-            </Collapse.Panel>
-            <Collapse.Panel header="Pause Requestly when not in use" key="3">
-              <img
-                width={338}
-                height={196}
-                className="feature-gif"
-                alt="Pause requestly extension"
-                src="https://dummyimage.com/600x400/000/fff"
-              />
-            </Collapse.Panel>
+          <Collapse
+            ghost
+            accordion
+            expandIconPosition="end"
+            expandIcon={({ isActive }) => (
+              <PiCaretDownBold className={`header-down-caret ${isActive ? "rotate" : ""}`} />
+            )}
+          >
+            {collapsePanelItems.map(({ header, image }, index) => (
+              <Collapse.Panel header={header} key={index}>
+                <img width={338} height={196} className="feature-gif" alt={image.alt} src={image.src} />
+              </Collapse.Panel>
+            ))}
           </Collapse>
         </div>
 
         <div className="action-btns">
           <Button type="link" onClick={handleCloseClick}>
-            <IoCloseOutline /> Close
+            <TfiClose /> Close
           </Button>
           <Button type="link" onClick={handleAlreadyPinnedClick}>
             <BsPin /> I've already pinned
