887bad98939841b32bd8c45bff14d035fc9a8d94
rohanmathur91 <mathurrohan04@gmail.com>
2023-09-25 12:12:00 +0530

    fix usePinExtensionPopup hook

--- app/src/components/common/PinExtensionPopup/hooks/usePinExtensionPopup.ts
+++ app/src/components/common/PinExtensionPopup/hooks/usePinExtensionPopup.ts
@@ -1,14 +1,14 @@
 import { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
-import { getAllRules } from "store/selectors";
+import { getUserRulesCount } from "store/selectors";
 
 const IS_PIN_EXTENSION_POPUP_ACTIVE = "is_rq_pin_extension_popup_active";
 
 export const usePinExtensionPopup = () => {
-  const allRules = useSelector(getAllRules);
+  const rulesCount = useSelector(getUserRulesCount);
 
   const [isPinExtensionPopupActive, setIsPinExtensionPopupActive] = useState<boolean>(
-    JSON.parse(window.localStorage.getItem(IS_PIN_EXTENSION_POPUP_ACTIVE)) ?? allRules?.length > 0
+    JSON.parse(window.localStorage.getItem(IS_PIN_EXTENSION_POPUP_ACTIVE)) ?? rulesCount > 0
   );
 
   const closePinExtensionPopup = useCallback(() => {
