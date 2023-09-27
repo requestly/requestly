921a7e54d0fe53d90e3954fad851532c2af5176d
nsrCodes <navdeepsinghrathore2000@gmail.com>
2023-09-27 16:38:07 +0530

    fix: email password auth for desktop app

--- app/src/components/authentication/AuthForm/index.js
+++ app/src/components/authentication/AuthForm/index.js
@@ -16,16 +16,17 @@ import GoogleIcon from "../../../assets/img/icons/common/google.svg";
 //UTILS
 // import { syncUserPersona } from "components/features/rules/GettingStarted/WorkspaceOnboarding/OnboardingSteps/PersonaSurvey/utils";
 import { getGreeting } from "utils/FormattingHelper";
-// import { getAuthErrorMessage, AuthTypes } from "../utils";
+import { getAuthErrorMessage, AuthTypes } from "../utils";
 
 //CONSTANTS
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import APP_CONSTANTS from "../../../config/constants";
 import PATHS from "config/constants/sub/paths";
 
 //ACTIONS
 import {
-  // handleEmailSignIn,
-  // handleEmailSignUp,
+  handleEmailSignIn,
+  handleEmailSignUp,
   handleForgotPasswordButtonOnClick,
   handleGoogleSignIn,
   handleResetPasswordOnClick,
@@ -114,62 +115,62 @@ const AuthForm = ({
       });
   };
 
-  // const handleEmailSignUpButtonClick = (event) => {
-  //   event.preventDefault();
-  //   setActionPending(true);
-  //   handleEmailSignUp(name, email, password, referralCode, eventSource)
-  //     .then(({ status, errorCode }) => {
-  //       if (status) {
-  //         handleEmailSignIn(email, password, true, eventSource)
-  //           .then(({ result }) => {
-  //             if (result.user.uid) {
-  //               !isOnboardingForm && toast.info(`${getGreeting()}, ${result.user.displayName.split(" ")[0]}`);
-  //               setEmail("");
-  //               setPassword("");
-  //               // syncUserPersona(result.user.uid, dispatch, userPersona); TEMP DISABLED
-  //               onSignInSuccess && onSignInSuccess(result.user.uid, true);
-  //             }
-  //           })
-  //           .catch(({ errorCode }) => {
-  //             toast.error(getAuthErrorMessage(AuthTypes.SIGN_UP, errorCode));
-  //             setActionPending(false);
-  //             setEmail("");
-  //             setPassword("");
-  //           });
-  //       } else {
-  //         toast.error(getAuthErrorMessage(AuthTypes.SIGN_UP, errorCode));
-  //         setActionPending(false);
-  //       }
-  //     })
-  //     .catch(({ errorCode }) => {
-  //       toast.error(getAuthErrorMessage(AuthTypes.SIGN_UP, errorCode));
-  //       setActionPending(false);
-  //     });
-  // };
+  const handleEmailSignUpButtonClick = (event) => {
+    event.preventDefault();
+    setActionPending(true);
+    handleEmailSignUp(email, password, referralCode, eventSource)
+      .then(({ status, errorCode }) => {
+        if (status) {
+          handleEmailSignIn(email, password, true, eventSource)
+            .then(({ result }) => {
+              if (result.user.uid) {
+                !isOnboardingForm && toast.info(`${getGreeting()}, ${result.user.displayName.split(" ")[0]}`);
+                setEmail("");
+                setPassword("");
+                // syncUserPersona(result.user.uid, dispatch, userPersona); TEMP DISABLED
+                onSignInSuccess && onSignInSuccess(result.user.uid, true);
+              }
+            })
+            .catch(({ errorCode }) => {
+              toast.error(getAuthErrorMessage(AuthTypes.SIGN_UP, errorCode));
+              setActionPending(false);
+              setEmail("");
+              setPassword("");
+            });
+        } else {
+          toast.error(getAuthErrorMessage(AuthTypes.SIGN_UP, errorCode));
+          setActionPending(false);
+        }
+      })
+      .catch(({ errorCode }) => {
+        toast.error(getAuthErrorMessage(AuthTypes.SIGN_UP, errorCode));
+        setActionPending(false);
+      });
+  };
 
-  // const handleEmailSignInButtonClick = (event) => {
-  //   event.preventDefault();
-  //   setActionPending(true);
-  //   handleEmailSignIn(email, password, false, eventSource)
-  //     .then(({ result }) => {
-  //       if (result.user.uid) {
-  //         !isOnboardingForm && toast.info(`${getGreeting()}, ${result.user.displayName.split(" ")[0]}`);
-  //         setEmail("");
-  //         setPassword("");
-  //         // syncUserPersona(result.user.uid, dispatch, userPersona); TEMP DISABLED
-  //         onSignInSuccess && onSignInSuccess(result.user.uid, false);
-  //       } else {
-  //         toast.error("Sorry we couldn't log you in. Can you please retry?");
-  //         setActionPending(true);
-  //       }
-  //     })
-  //     .catch(({ errorCode }) => {
-  //       toast.error(getAuthErrorMessage(AuthTypes.SIGN_IN, errorCode));
-  //       setActionPending(false);
-  //       setEmail("");
-  //       setPassword("");
-  //     });
-  // };
+  const handleEmailSignInButtonClick = (event) => {
+    event.preventDefault();
+    setActionPending(true);
+    handleEmailSignIn(email, password, false, eventSource)
+      .then(({ result }) => {
+        if (result.user.uid) {
+          !isOnboardingForm && toast.info(`${getGreeting()}, ${result.user.displayName.split(" ")[0]}`);
+          setEmail("");
+          setPassword("");
+          // syncUserPersona(result.user.uid, dispatch, userPersona); TEMP DISABLED
+          onSignInSuccess && onSignInSuccess(result.user.uid, false);
+        } else {
+          toast.error("Sorry we couldn't log you in. Can you please retry?");
+          setActionPending(true);
+        }
+      })
+      .catch(({ errorCode }) => {
+        toast.error(getAuthErrorMessage(AuthTypes.SIGN_IN, errorCode));
+        setActionPending(false);
+        setEmail("");
+        setPassword("");
+      });
+  };
 
   const SocialAuthButtons = () => {
     switch (MODE) {
@@ -230,6 +231,13 @@ const AuthForm = ({
     switch (MODE) {
       default:
       case AUTH_ACTION_LABELS.LOG_IN:
+        if (appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP) {
+          return (
+            <RQButton type="primary" className="form-elements-margin w-full" onClick={handleEmailSignInButtonClick}>
+              Sign In with Email
+            </RQButton>
+          );
+        }
         return (
           <GenerateLoginLinkBtn
             email={email}
@@ -242,6 +250,13 @@ const AuthForm = ({
         );
 
       case AUTH_ACTION_LABELS.SIGN_UP:
+        if (appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP) {
+          return (
+            <RQButton type="primary" className="form-elements-margin w-full" onClick={handleEmailSignUpButtonClick}>
+              Create Account
+            </RQButton>
+          );
+        }
         return (
           <GenerateLoginLinkBtn
             email={email}
@@ -446,6 +461,7 @@ const AuthForm = ({
                 <div className="auth-modal-message w-full">Sign Up with email</div>
 
                 {renderEmailField()}
+                {appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP && renderPasswordField()}
                 <FormSubmitButton />
                 <Typography.Text className="secondary-text form-elements-margin">
                   I agree to the{" "}
@@ -478,6 +494,7 @@ const AuthForm = ({
             <div className="auth-modal-divider w-full">or</div>
             <div className="auth-modal-message w-full">Sign In with email</div>
             {renderEmailField()}
+            {appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP && renderPasswordField()}
             <FormSubmitButton />
           </Row>
         </Col>
