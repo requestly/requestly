24ed0572f735ddbbf05738dec94cb51e61db80ea
RuntimeTerror10 <parthb.git@gmail.com>
2023-09-29 14:43:31 +0530

    chore: remove log

--- app/src/lib/design-system/components/RQNetworkTable/GenericNetworkTable/components/FiltersToolbar/StatusCodeFilter.tsx
+++ app/src/lib/design-system/components/RQNetworkTable/GenericNetworkTable/components/FiltersToolbar/StatusCodeFilter.tsx
@@ -13,20 +13,19 @@ export const StatusCodeFilter: React.FC<StatusCodeFilterProps> = ({
   statusCodeFilters,
   handleStatusCodeFilterChange,
 }) => {
-  console.log({ statusCodeFilters });
   return (
     <Col className="status-code-filters-wrapper">
       <Typography.Text>Status code</Typography.Text>
       <Row className="status-code-filter-btns">
-        {statusCodesGroups.map((code, index) => (
+        {statusCodesGroups.map((group, index) => (
           <RQButton
             type="default"
             size="small"
             key={index}
-            onClick={() => handleStatusCodeFilterChange(code)}
-            className={`filter-btn ${statusCodeFilters.indexOf(code) !== -1 ? "active-filter-btn" : null} `}
+            onClick={() => handleStatusCodeFilterChange(group)}
+            className={`filter-btn ${statusCodeFilters.indexOf(group) !== -1 ? "active-filter-btn" : null} `}
           >
-            {code}
+            {group}
           </RQButton>
         ))}
       </Row>
