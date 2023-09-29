48ea2b691c50b6b226864a2927ab85151a106d1d
rohanmathur91 <mathurrohan04@gmail.com>
2023-09-29 10:35:16 +0530

    fix: pin extension popup shown when extension not installed

--- app/src/components/common/PinExtensionPopup/hooks/usePinExtensionPopup.ts
+++ app/src/components/common/PinExtensionPopup/hooks/usePinExtensionPopup.ts
@@ -1,6 +1,8 @@
 import { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import { getUserRulesCount } from "store/selectors";
+import { isExtensionInstalled } from "actions/ExtensionActions";
+import { isDesktopMode } from "utils/AppUtils";
 
 const IS_PIN_EXTENSION_POPUP_ACTIVE = "is_rq_pin_extension_popup_active";
 
@@ -8,7 +10,9 @@ export const usePinExtensionPopup = () => {
   const rulesCount = useSelector(getUserRulesCount);
 
   const [isPinExtensionPopupActive, setIsPinExtensionPopupActive] = useState<boolean>(
-    JSON.parse(window.localStorage.getItem(IS_PIN_EXTENSION_POPUP_ACTIVE)) ?? rulesCount > 0
+    !isDesktopMode() &&
+      isExtensionInstalled() &&
+      (JSON.parse(window.localStorage.getItem(IS_PIN_EXTENSION_POPUP_ACTIVE)) ?? rulesCount > 0)
   );
 
   const closePinExtensionPopup = useCallback(() => {
