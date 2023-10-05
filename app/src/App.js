563eee66d786f5e3c2a43b92ecb874d57dd3eb75
Rohan Mathur <mathurrohan04@gmail.com>
2023-10-05 18:27:30 +0530

    [RQ-957]: default on auto session recording for new users  (#1119)
    * default on auto session recording on all pages
    
    * rename hook

--- app/src/App.js
+++ app/src/App.js
@@ -22,6 +22,7 @@ import FeatureUsageEvent from "hooks/FeatureUsageEvent";
 import ActiveWorkspace from "hooks/ActiveWorkspace";
 import AuthHandler from "hooks/AuthHandler";
 import ExtensionContextInvalidationNotice from "components/misc/ExtensionContextInvalidationNotice";
+import { useInitializeNewUserSessionRecordingConfig } from "views/features/sessions/SessionsSettingsPage/hooks";
 import { useIsExtensionEnabled } from "hooks";
 
 const { PATHS } = APP_CONSTANTS;
@@ -36,6 +37,7 @@ const App = () => {
 
   useGeoLocation();
   useIsExtensionEnabled();
+  useInitializeNewUserSessionRecordingConfig();
 
   submitAppDetailAttributes();
 
