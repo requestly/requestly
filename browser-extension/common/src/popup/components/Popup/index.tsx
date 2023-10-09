fac0938d0188dfb9e070d822c3fcdbe9fb00dc7f
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2023-10-09 12:02:29 +0530

    [RQ-952]: update enable/disable extension UI (#1139)
    * [RQ-954] chore: add new color variables (#1116)
    
    * style header and add pause switch
    
    * show tooltip when extension is paused
    
    * fix tooltip style and add paused overlay
    
    * [RQ-957]: default on auto session recording for new users  (#1119)
    
    * default on auto session recording on all pages
    
    * rename hook
    
    * fix:extension paused tooltip flicker
    
    * review comments
    
    * [RQ-961]: popup onboarding screen (#1122)
    
    * cleanup
    
    * add external link icon
    
    * fix: background color
    
    * update rule svgs
    
    * add http rules section
    
    * fix footer
    
    * update assets
    
    * cleanup
    
    * update session recording section
    
    * cleanup
    
    * fix
    
    * cleanup
    
    * update tooltip arrow
    
    * [RQ-952]: update enable/disable extension UI (#1121)
    
    * style header and add pause switch
    
    * show tooltip when extension is paused
    
    * fix tooltip style and add paused overlay
    
    * fix:extension paused tooltip flicker
    
    * review comments
    
    ---------
    
    Co-authored-by: Rohan Mathur <mathurrohan04@gmail.com>

--- browser-extension/common/src/popup/components/Popup/index.tsx
+++ browser-extension/common/src/popup/components/Popup/index.tsx
@@ -10,7 +10,7 @@ import "./popup.css";
 
 const Popup: React.FC = () => {
   const [ifNoRulesPresent, setIfNoRulesPresent] = useState<boolean>(true);
-  const [isExtensionEnabled, setIsExtensionEnabled] = useState<boolean>(false);
+  const [isExtensionEnabled, setIsExtensionEnabled] = useState<boolean>(true);
 
   useEffect(() => {
     chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.CHECK_IF_NO_RULES_PRESENT }, (noRulesPresent) => {
@@ -37,12 +37,14 @@ const Popup: React.FC = () => {
           isExtensionEnabled={isExtensionEnabled}
           handleToggleExtensionStatus={handleToggleExtensionStatus}
         />
-
-        <div className="popup-content">
-          {ifNoRulesPresent ? <HttpsRuleOptions /> : <PopupTabs />}
-          <SessionRecordingView />
+        <div className="popup-body">
+          {!isExtensionEnabled && <div className="extension-paused-overlay"></div>}
+          <div className="popup-content">
+            {ifNoRulesPresent ? <HttpsRuleOptions /> : <PopupTabs />}
+            <SessionRecordingView />
+          </div>
+          <PopupFooter />
         </div>
-        <PopupFooter />
       </div>
     </>
   );
