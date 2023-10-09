fcbae105158b8dbc652260d513ff9615e4ca4e7b
RuntimeTerror10 <parthb.git@gmail.com>
2023-10-09 09:52:43 +0530

    review comments

--- browser-extension/common/src/popup/components/Popup/PopupHeader.tsx
+++ browser-extension/common/src/popup/components/Popup/PopupHeader.tsx
@@ -6,20 +6,13 @@ import { EVENT, sendEvent } from "../../events";
 interface PopupHeaderProps {
   isExtensionEnabled: boolean;
   handleToggleExtensionStatus: () => void;
-  initPauseToolip: boolean;
 }
 
-const PopupHeader: React.FC<PopupHeaderProps> = ({
-  isExtensionEnabled,
-  handleToggleExtensionStatus,
-  initPauseToolip,
-}) => {
+const PopupHeader: React.FC<PopupHeaderProps> = ({ isExtensionEnabled, handleToggleExtensionStatus }) => {
   const onOpenAppButtonClick = useCallback(() => {
     window.open(`${config.WEB_URL}?source=popup`, "_blank");
     sendEvent(EVENT.OPEN_APP_CLICKED);
   }, []);
-
-  console.log({ initPauseToolip });
 
   return (
     <div className="popup-header">
@@ -44,7 +37,7 @@ const PopupHeader: React.FC<PopupHeaderProps> = ({
         <Col>
           <Row align="middle">
             <Tooltip
-              open={!isExtensionEnabled && initPauseToolip}
+              open={!isExtensionEnabled}
               title="Please switch on the Requestly extension. When paused, rules won't be applied and sessions won't be recorded."
               overlayClassName="enable-extension-tooltip"
               color="var(--neutrals-black)"
