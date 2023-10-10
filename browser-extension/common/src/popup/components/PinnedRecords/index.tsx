f8f8c6200dd544bbe6a317f8aa49533d34fb3551
RuntimeTerror10 <parthb.git@gmail.com>
2023-10-10 08:09:15 +0530

    add tab empty states

--- browser-extension/common/src/popup/components/PinnedRecords/index.tsx
+++ browser-extension/common/src/popup/components/PinnedRecords/index.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import { Typography } from "antd";
 import RuleItem from "../common/RuleItem";
 import GroupItem from "../common/GroupItem";
 import TabContentSection from "../common/TabContentSection";
 import { useRecords } from "../../contexts/RecordsContext";
-import RecentRecords from "../RecentRecords";
+import { EmptyPopupTab } from "../PopupTabs/EmptyPopupTab";
+import { PrimaryActionButton } from "../common/PrimaryActionButton";
 import "./pinnedRecords.css";
 
 const PinnedRecords: React.FC = () => {
   const { pinnedRules, pinnedGroups } = useRecords();
 
   return !pinnedGroups.length && !pinnedRules.length ? (
-    <>
-      <div className="empty-pinned-rules-message">
-        <div className="empty-records-title">No pinned rules found</div>
-        <Typography.Text type="secondary" className="empty-pinned-rules-caption-message">
-          Here are some recently used rules that you may want to pin
-        </Typography.Text>
-      </div>
-      <RecentRecords />
-    </>
+    <EmptyPopupTab
+      title="You haven't pinned any rules yet!"
+      description=" Feel free to pin your recently used rules for quick access."
+      actionButton={<PrimaryActionButton size="small">See recently used rules</PrimaryActionButton>}
+    />
   ) : (
     <TabContentSection>
       <ul className="record-list">
