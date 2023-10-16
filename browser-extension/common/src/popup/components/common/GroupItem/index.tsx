edf7a15dfbfcf53e5899d9dd895b7c6f4dc2a601
rohanmathur91 <mathurrohan04@gmail.com>
2023-10-16 18:57:16 +0530

    fix: rule hover effect

--- browser-extension/common/src/popup/components/common/GroupItem/index.tsx
+++ browser-extension/common/src/popup/components/common/GroupItem/index.tsx
@@ -57,7 +57,7 @@ const GroupItem: React.FC<GroupItemProps> = ({ group }) => {
         </Col>
 
         <Col span={2} className="icon-container">
-          <Row align="middle" justify="center">
+          <Row align="middle">
             <PinAction record={group} updateRecord={updateGroup} />
           </Row>
         </Col>
@@ -68,7 +68,7 @@ const GroupItem: React.FC<GroupItemProps> = ({ group }) => {
       </Row>
 
       {isExpanded && (
-        <Row>
+        <Row className="group-rules">
           <Col span={24}>
             {group.children.length > 0 ? (
               group.children.map((rule: Rule) => <RuleItem key={rule.id} rule={rule} isChildren={true} />)
