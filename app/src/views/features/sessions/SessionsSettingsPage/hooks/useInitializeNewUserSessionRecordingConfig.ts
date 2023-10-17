9b3d15e2502ba1907324a7273b10e5dba8f1f42b
rohanmathur91 <mathurrohan04@gmail.com>
2023-10-17 10:31:51 +0530

    refactor: extension installed check

--- app/src/views/features/sessions/SessionsSettingsPage/hooks/useInitializeNewUserSessionRecordingConfig.ts
+++ app/src/views/features/sessions/SessionsSettingsPage/hooks/useInitializeNewUserSessionRecordingConfig.ts
@@ -1,18 +1,18 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { getAppMode, getExtensionInstallDate } from "store/selectors";
+import { getAppMode } from "store/selectors";
 import { AutoRecordingMode, SessionRecordingConfig } from "../../types";
 import { defaultSessionRecordingConfig } from "../";
 import { StorageService } from "init";
 // @ts-ignore
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import { isExtensionInstalled } from "actions/ExtensionActions";
 
 export const useInitializeNewUserSessionRecordingConfig = () => {
   const appMode = useSelector(getAppMode);
-  const extenionInstallDate = useSelector(getExtensionInstallDate);
 
   useEffect(() => {
-    if (!extenionInstallDate) return;
+    if (!isExtensionInstalled()) return;
 
     if (appMode !== GLOBAL_CONSTANTS.APP_MODES.EXTENSION) return;
 
@@ -32,5 +32,5 @@ export const useInitializeNewUserSessionRecordingConfig = () => {
           StorageService(appMode).saveSessionRecordingPageConfig(newUserRecordingConfig);
         }
       });
-  }, [appMode, extenionInstallDate]);
+  }, [appMode]);
 };
