8584ff36205af748fd781d28d7c599a9926145d1
Rohan Mathur <mathurrohan04@gmail.com>
2023-10-21 19:05:13 +0530

    [RQ-944]: new extension popup experience (#1117)

--- browser-extension/common/src/popup/events.ts
+++ browser-extension/common/src/popup/events.ts
@@ -7,12 +7,14 @@ export enum EVENT {
   POPUP_OPENED = "popup_opened",
   POPUP_TAB_SELECTED = "popup_tab_selected",
   RULE_TOGGLED = "rule_toggled",
+  NEW_RULE_BUTTON_CLICKED = "new_rule_button_clicked",
+  RULE_CREATION_WORKFLOW_STARTED = "rule_creation_workflow_started",
   START_RECORDING_CLICKED = "start_recording_clicked",
   STOP_RECORDING_CLICKED = "stop_recording_clicked",
   VIEW_RECORDING_CLICKED = "view_recording_clicked",
-  EXTENSION_WATCH_DEMO_VIDEO_CLICKED = "extension_watch_demo_video_clicked",
   EXTENSION_RULE_CLICKED = "extension_rule_clicked",
   EXTENSION_VIEW_ALL_MODIFICATIONS_CLICKED = "extension_view_all_modifications_clicked",
+  SESSION_RECORDINGS_CONFIG_OPENED = "session_recordings_config_opened",
 }
 
 export const sendEvent = (eventName: EVENT, eventParams: Record<string, any> = {}) => {
