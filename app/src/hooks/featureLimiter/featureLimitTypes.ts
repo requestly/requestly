09e59b5731550c3976578d9e7448b6d02b42b600
rohanmathur91 <mathurrohan04@gmail.com>
2023-10-23 18:16:47 +0530

    update feature types

--- app/src/hooks/featureLimiter/featureLimitTypes.ts
+++ app/src/hooks/featureLimiter/featureLimitTypes.ts
@@ -5,6 +5,14 @@ export const featureLimits: PlanFeatureLimits = {
   [APP_CONSTANTS.PRICING.PLAN_NAMES.FREE]: {
     [FeatureLimitType.num_rules]: 10,
     [FeatureLimitType.num_active_rules]: 3,
+    [FeatureLimitType.response_rule]: true,
+    [FeatureLimitType.request_rule]: true,
+    [FeatureLimitType.script_rule]: true,
+    [FeatureLimitType.add_new_rule_pair]: true,
+    [FeatureLimitType.graphql_resource_type]: true,
+    [FeatureLimitType.dynamic_response]: true,
+    [FeatureLimitType.dynamic_request]: true,
+    [FeatureLimitType.rules_sharing]: true,
   },
   [APP_CONSTANTS.PRICING.PLAN_NAMES.BASIC]: {
     [FeatureLimitType.num_rules]: 25,
--- app/src/hooks/featureLimiter/types.ts
+++ app/src/hooks/featureLimiter/types.ts
@@ -1,8 +1,16 @@
 export enum FeatureLimitType {
   num_rules = "num_rules",
   num_active_rules = "num_active_rules",
+  response_rule = "response_rule",
+  request_rule = "request_rule",
+  script_rule = "script_rule",
+  add_new_rule_pair = "add_new_rule_pair",
+  graphql_resource_type = "graphql_resource_type",
+  dynamic_response = "dynamic_response",
+  dynamic_request = "dynamic_request",
+  rules_sharing = "rules_sharing",
 }
 
-export type FeatureLimits = Record<FeatureLimitType, number | boolean>;
+export type FeatureLimits = Partial<Record<FeatureLimitType, number | boolean>>;
 
 export type PlanFeatureLimits = Record<string, FeatureLimits>;
