7483864a3b31e91fd2109697357181e70d282dd8
Nafees Nehar <nafees87n@gmail.com>
2023-10-27 13:35:44 +0530

    fix: added features to basic_v2

--- app/src/hooks/featureLimiter/featureLimitTypes.ts
+++ app/src/hooks/featureLimiter/featureLimitTypes.ts
@@ -29,5 +29,13 @@ export const featureLimits: PlanFeatureLimits = {
   [APP_CONSTANTS.PRICING.PLAN_NAMES.BASIC_V2]: {
     [FeatureLimitType.num_rules]: 10,
     [FeatureLimitType.num_active_rules]: 5,
+    [FeatureLimitType.response_rule]: true,
+    [FeatureLimitType.request_rule]: true,
+    [FeatureLimitType.script_rule]: true,
+    [FeatureLimitType.add_new_rule_pair]: true,
+    [FeatureLimitType.graphql_resource_type]: true,
+    [FeatureLimitType.dynamic_response_body]: true,
+    [FeatureLimitType.dynamic_request_body]: true,
+    [FeatureLimitType.share_rules]: true,
   },
 };
