c9b0aa8604cf440700cf06dd0bf506e13b810f63
Rohan Mathur <mathurrohan04@gmail.com>
2023-10-30 14:03:42 +0530

    [RQ-1033] chore: add paid feature nudge viewed event (#1193)

--- app/src/components/common/PremiumIcon/index.tsx
+++ app/src/components/common/PremiumIcon/index.tsx
@@ -3,13 +3,26 @@ import { Link } from "react-router-dom";
 import { Tooltip, TooltipProps } from "antd";
 import { ReactComponent as CrownIcon } from "assets/icons/crown.svg";
 import PATHS from "config/constants/sub/paths";
-import { trackViewPricingPlansClicked } from "modules/analytics/events/common/pricing";
+import {
+  FeatureType,
+  PaidFeatureNudgeViewedSource,
+  trackPaidFeatureNudgeViewed,
+  trackViewPricingPlansClicked,
+} from "modules/analytics/events/common/pricing";
 import "./premiumIcon.css";
 
-export const PremiumIcon: React.FC<TooltipProps & { onSeePlansClick?: () => void }> = (props) => {
+export const PremiumIcon: React.FC<
+  TooltipProps & { onSeePlansClick?: () => void } & {
+    featureType: FeatureType;
+    source?: PaidFeatureNudgeViewedSource;
+  }
+> = (props) => {
   return (
     <Tooltip
       {...props}
+      onOpenChange={(isOpen) => {
+        if (isOpen) trackPaidFeatureNudgeViewed(props.featureType, props.source);
+      }}
       title={
         <div>
           This is a paid feature. Consider upgrading for uninterrupted usage.{" "}
