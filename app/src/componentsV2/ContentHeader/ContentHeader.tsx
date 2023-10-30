1f4303b623d01778e3cac4088b8d44bb2b8ddff2
Sahil Gupta <sahil865gupta@gmail.com>
2023-10-30 09:34:39 +0530

    added rules status toggle action

--- app/src/componentsV2/ContentHeader/ContentHeader.tsx
+++ app/src/componentsV2/ContentHeader/ContentHeader.tsx
@@ -1,10 +1,31 @@
+import { Col, Row } from "antd";
 import "./contentHeader.scss";
 
-export interface Props {}
+import React, { ReactNode } from "react";
+
+export interface Props {
+  title: string;
+  subtitle: string;
+  actions: ReactNode[];
+}
 
 // Contains common design and colors for app
-const ContentHeader: React.FC<Props> = ({}) => {
-  return <div>Content Header</div>;
+const ContentHeader: React.FC<Props> = ({ title, subtitle, actions }: Props) => {
+  return (
+    <Row justify={"space-between"} align={"bottom"}>
+      <Col span={12}>
+        <div>{title}</div>
+        <div>{subtitle}</div>
+      </Col>
+      <Col span={12}>
+        <Row align={"bottom"}>
+          {actions.map((action) => {
+            return <Col>{action}</Col>;
+          })}
+        </Row>
+      </Col>
+    </Row>
+  );
 };
 
 export default ContentHeader;
