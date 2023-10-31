f07259dfe8f8613fb219848965092a57163cd4c3
Sahil Gupta <sahil865gupta@gmail.com>
2023-10-31 10:18:07 +0530

    added content table styling

--- app/src/features/rules/components/RulesList/components/RulesTable/hooks/useRuleTableActions.tsx
+++ app/src/features/rules/components/RulesList/components/RulesTable/hooks/useRuleTableActions.tsx
@@ -1,4 +1,4 @@
-import { RuleObj } from "features/rules/types/rules";
+import { RuleObj, RuleObjStatus } from "features/rules/types/rules";
 import { RuleTableDataType } from "../types";
 import { useSelector } from "react-redux";
 import { getAppMode, getUserAuthDetails } from "store/selectors";
@@ -9,6 +9,9 @@ import { StorageService } from "init";
 
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import { toast } from "utils/Toast";
+import { actions } from "store";
+import APP_CONSTANTS from "config/constants";
+import { AUTH } from "modules/analytics/events/common/constants";
 
 const useRuleTableActions = () => {
   const user = useSelector(getUserAuthDetails);
@@ -24,51 +27,94 @@ const useRuleTableActions = () => {
     // TODO: Add logic to propogate the changes to storageservice;
 
     rules.forEach((rule) => {
-      const status = checked ? "Active" : "Inactive";
+      const status = checked ? RuleObjStatus.ACTIVE : RuleObjStatus.INACTIVE;
       changeRuleStatus(status, rule);
     });
   };
 
-  const changeRuleStatus = (newStatus: string, rule: RuleObj) => {
-    let currentOwner;
+  const changeRuleStatus = (newStatus: RuleObjStatus, rule: RuleObj) => {
+    // TODO: Handle Group status toggle
+    console.log({ rule, user });
 
-    if (rule.currentOwner) {
-      currentOwner = user?.details?.profile?.uid || null;
-    } else {
-      currentOwner = rule.currentOwner;
-    }
-    const updatedRule = {
+    // TODO: Why is this added??
+    // if (rule.currentOwner) {
+    //   currentOwner = user?.details?.profile?.uid || null;
+    // } else {
+    //   currentOwner = rule.currentOwner;
+    // }
+    const updatedRule: RuleObj = {
       ...rule,
-      currentOwner,
       status: newStatus,
     };
 
-    dispatch(rulesActions.ruleObjUpsert(updatedRule));
     Logger.log("Writing storage in RulesTable changeRuleStatus");
-    StorageService(appMode)
+    updateRuleInStorage(updatedRule, rule).then(() => {
+      //Push Notify
+      newStatus === GLOBAL_CONSTANTS.RULE_STATUS.ACTIVE
+        ? toast.success(`Rule is now ${newStatus.toLowerCase()}`)
+        : toast.success(`Rule is now ${newStatus.toLowerCase()}`);
+
+      // TODO: Add Analytics
+      //Analytics
+      // if (newStatus.toLowerCase() === "active") {
+      //   trackRQLastActivity("rule_activated");
+      //   trackRuleActivatedStatusEvent(rule.ruleType);
+      // } else {
+      //   trackRQLastActivity("rule_deactivated");
+      //   trackRuleDeactivatedStatus(rule.ruleType);
+      // }
+    });
+  };
+
+  const toggleSharingModal = (rule: RuleObj) => {
+    dispatch(
+      actions.toggleActiveModal({
+        modalName: "sharingModal",
+        newValue: true,
+        newProps: {
+          selectedRules: [rule.id],
+        },
+      })
+    );
+  };
+
+  const promptUserToSignup = (source: string) => {
+    dispatch(
+      actions.toggleActiveModal({
+        modalName: "authModal",
+        newValue: true,
+        newProps: {
+          redirectURL: window.location.href,
+          src: APP_CONSTANTS.FEATURES.RULES,
+          userActionMessage: "Sign up to generate a public shareable link",
+          authMode: APP_CONSTANTS.AUTH.ACTION_LABELS.SIGN_UP,
+          eventSource: source,
+        },
+      })
+    );
+  };
+
+  const handleRuleShare = (rule: RuleObj) => {
+    // TODO
+    // trackShareButtonClicked("rules_list");
+    user.loggedIn ? toggleSharingModal(rule) : promptUserToSignup(AUTH.SOURCE.SHARE_RULES);
+  }
+
+  // Generic
+  const updateRuleInStorage = async (updatedRule: RuleObj, originalRule: RuleObj) => {
+    dispatch(rulesActions.ruleObjUpsert(updatedRule));
+
+    return StorageService(appMode)
       .saveRuleOrGroup(updatedRule, { silentUpdate: true })
       .then((rule) => {
-        //Push Notify
-        newStatus === GLOBAL_CONSTANTS.RULE_STATUS.ACTIVE
-          ? toast.success(`Rule is now ${newStatus.toLowerCase()}`)
-          : toast.success(`Rule is now ${newStatus.toLowerCase()}`);
-
-        // TODO: Add Analytics
-        //Analytics
-        // if (newStatus.toLowerCase() === "active") {
-        //   trackRQLastActivity("rule_activated");
-        //   trackRuleActivatedStatusEvent(rule.ruleType);
-        // } else {
-        //   trackRQLastActivity("rule_deactivated");
-        //   trackRuleDeactivatedStatus(rule.ruleType);
-        // }
+        console.log("Rule updated in Storage Service");
       })
       .catch(() => {
-        dispatch(rulesActions.ruleObjUpsert(rule));
+        dispatch(rulesActions.ruleObjUpsert(originalRule));
       });
   };
 
-  return { handlePin, handleStatusToggle };
+  return { handlePin, handleStatusToggle, handleRuleShare };
 };
 
 export default useRuleTableActions;
