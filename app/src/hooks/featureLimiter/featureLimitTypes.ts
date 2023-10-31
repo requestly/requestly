1bb70d583cf05920201082f1042bc545600e298b
Nafees Nehar <nafees87n@gmail.com>
2023-10-31 13:51:01 +0530

    [RQ-1022] updated limits for basic plan (#1187)
    * [RQ-1022] updated limits for basic plan
    
    * fix: added features to basic_v2
    
    * fix:  handle basic plan name display
    
    * fix: planName on banner
    
    * fix: use planId in feature limiter
    
    * fix: prettyPlanName

--- app/src/hooks/featureLimiter/featureLimitTypes.ts
+++ app/src/hooks/featureLimiter/featureLimitTypes.ts
@@ -3,7 +3,7 @@ import { FeatureLimitType, PlanFeatureLimits } from "./types";
 
 export const featureLimits: PlanFeatureLimits = {
   [APP_CONSTANTS.PRICING.PLAN_NAMES.FREE]: {
-    [FeatureLimitType.num_rules]: 10,
+    [FeatureLimitType.num_rules]: 5,
     [FeatureLimitType.num_active_rules]: 3,
     [FeatureLimitType.response_rule]: false,
     [FeatureLimitType.request_rule]: false,
@@ -17,6 +17,18 @@ export const featureLimits: PlanFeatureLimits = {
   [APP_CONSTANTS.PRICING.PLAN_NAMES.BASIC]: {
     [FeatureLimitType.num_rules]: 25,
     [FeatureLimitType.num_active_rules]: 10,
+    [FeatureLimitType.response_rule]: true,
+    [FeatureLimitType.request_rule]: true,
+    [FeatureLimitType.script_rule]: true,
+    [FeatureLimitType.add_new_rule_pair]: true,
+    [FeatureLimitType.graphql_resource_type]: true,
+    [FeatureLimitType.dynamic_response_body]: true,
+    [FeatureLimitType.dynamic_request_body]: true,
+    [FeatureLimitType.share_rules]: true,
+  },
+  [APP_CONSTANTS.PRICING.PLAN_NAMES.BASIC_V2]: {
+    [FeatureLimitType.num_rules]: 10,
+    [FeatureLimitType.num_active_rules]: 5,
     [FeatureLimitType.response_rule]: true,
     [FeatureLimitType.request_rule]: true,
     [FeatureLimitType.script_rule]: true,
