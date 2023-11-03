a7e3a0b197ffc5e65d7601d188f0c110764d01f8
Sahil Gupta <sahil865gupta@gmail.com>
2023-11-03 17:45:59 +0530

    feat: sso working on beta

---
+++ app/src/backend/auth/sso.ts
@@ -0,0 +1,23 @@
+import firebaseApp from "../../firebase";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+
+export const getSSOProviderId = async (email: string) => {
+  try {
+    const domain = email.split("@")[1];
+
+    const db = getFirestore(firebaseApp);
+    const docRef = doc(db, "sso", domain);
+
+    const snapshot = await getDoc(docRef);
+
+    if (snapshot.exists()) {
+      const data = snapshot.data();
+      console.log({ data });
+      return data?.providerId;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
