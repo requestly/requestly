ea134a8cf9f2bfb28bf70fa8b11face795aca0a8
rohanmathur91 <mathurrohan04@gmail.com>
2023-11-06 20:41:57 +0530

    chore: update rules slice

--- app/src/store/features/rules/selectors.ts
+++ app/src/store/features/rules/selectors.ts
@@ -1,7 +1,16 @@
 import { ReducerKeys } from "store/constants";
-import { rulesAdapter } from "./slice";
+import { RulesState, rulesAdapter } from "./slice";
 
 const storeKey = ReducerKeys.RULES;
 const rulesEntitySelectors = rulesAdapter.getSelectors((state: any) => state[storeKey]["ruleObjs"]);
+
+// TODO: define global state type
+export const getRulesState = (state: any) => {
+  return state[storeKey] as RulesState;
+};
+
+export const getRuleActiveModals = (state: any) => {
+  return getRulesState(state)["ruleActiveModals"];
+};
 
 export const getAllRuleObjs = rulesEntitySelectors.selectAll;
