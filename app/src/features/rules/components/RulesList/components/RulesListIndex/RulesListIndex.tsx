d2510320ecedc5bce4a1edff90706316be720e96
rohanmathur91 <mathurrohan04@gmail.com>
2023-11-06 20:46:27 +0530

    add rule modals

--- app/src/features/rules/components/RulesList/components/RulesListIndex/RulesListIndex.tsx
+++ app/src/features/rules/components/RulesList/components/RulesListIndex/RulesListIndex.tsx
@@ -4,15 +4,20 @@ import { Button } from "antd";
 import RulesTable from "../RulesTable/RulesTable";
 import ContentHeader from "componentsV2/ContentHeader/ContentHeader";
 import { RuleObj } from "features/rules/types/rules";
-import { getAllRuleObjs } from "store/features/rules/selectors";
+import { getAllRuleObjs, getRuleActiveModals } from "store/features/rules/selectors";
 import useFetchAndUpdateRules from "./hooks/useFetchAndUpdateRules";
-import { RulesProvider } from "./context";
+import DuplicateRuleModal from "components/features/rules/DuplicateRuleModal";
+import RenameGroupModal from "components/features/rules/RenameGroupModal";
+import useRuleTableActions from "../RulesTable/hooks/useRuleTableActions";
+import { Rule } from "types";
 import "./rulesListIndex.scss";
 
 interface Props {}
 
 const RulesListIndex: React.FC<Props> = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const activeModals = useSelector(getRuleActiveModals);
+  const { closeDuplicateRuleModal, closeRenameGroupModal } = useRuleTableActions();
 
   // FIXME: Fetching multiple times
   // Fetch Rules here from Redux
@@ -20,19 +25,37 @@ const RulesListIndex: React.FC<Props> = () => {
 
   useFetchAndUpdateRules({ setIsLoading: setIsLoading });
 
-  return (
-    <RulesProvider>
-      <div className="rq-rules-list-container">
-        {/* TODO: Add Feature Limiter Banner Here */}
+  console.log({ x: activeModals });
 
-        <ContentHeader
-          title="My Rules"
-          subtitle="Create and manage your rules from here"
-          actions={[<Button type="primary">New Rule</Button>]}
+  return (
+    <div className="rq-rules-list-container">
+      {/* TODO: Add Feature Limiter Banner Here */}
+
+      {/* TODO: Add Modals Required in Rules List here */}
+      {activeModals?.duplicateRuleModal?.isActive ? (
+        <DuplicateRuleModal
+          close={closeDuplicateRuleModal}
+          onDuplicate={closeDuplicateRuleModal}
+          isOpen={activeModals?.duplicateRuleModal?.isActive}
+          rule={activeModals?.duplicateRuleModal?.props?.ruleToDuplicate as Rule}
         />
-        <div className="rq-rules-table">
-          <RulesTable rules={ruleObjs as RuleObj[]} loading={isLoading} />
-        </div>
+      ) : null}
+
+      {activeModals?.renameGroupModal?.isActive ? (
+        <RenameGroupModal
+          toggle={closeRenameGroupModal}
+          isOpen={activeModals.renameGroupModal.isActive}
+          groupId={activeModals.renameGroupModal.props.groupId}
+        />
+      ) : null}
+
+      <ContentHeader
+        title="My Rules"
+        subtitle="Create and manage your rules from here"
+        actions={[<Button type="primary">New Rule</Button>]}
+      />
+      <div className="rq-rules-table">
+        <RulesTable rules={ruleObjs as RuleObj[]} loading={isLoading} />
       </div>
     </RulesProvider>
   );
