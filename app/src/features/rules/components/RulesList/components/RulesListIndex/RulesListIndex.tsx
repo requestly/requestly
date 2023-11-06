cb3aa11d3a7f9f0c99940c4407876feca43ca7ae
rohanmathur91 <mathurrohan04@gmail.com>
2023-11-06 20:46:27 +0530

    add rule modals

--- app/src/features/rules/components/RulesList/components/RulesListIndex/RulesListIndex.tsx
+++ app/src/features/rules/components/RulesList/components/RulesListIndex/RulesListIndex.tsx
@@ -1,30 +1,54 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Button } from "antd";
 import RulesTable from "../RulesTable/RulesTable";
 import ContentHeader from "componentsV2/ContentHeader/ContentHeader";
 import { RuleObj } from "features/rules/types/rules";
-import { useSelector } from "react-redux";
-import { getAllRuleObjs } from "store/features/rules/selectors";
+import { getAllRuleObjs, getRuleActiveModals } from "store/features/rules/selectors";
 import useFetchAndUpdateRules from "./hooks/useFetchAndUpdateRules";
-import { Button } from "antd";
-
+import DuplicateRuleModal from "components/features/rules/DuplicateRuleModal";
+import RenameGroupModal from "components/features/rules/RenameGroupModal";
+import useRuleTableActions from "../RulesTable/hooks/useRuleTableActions";
+import { Rule } from "types";
 import "./rulesListIndex.scss";
 
 interface Props {}
 
 const RulesListIndex: React.FC<Props> = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const activeModals = useSelector(getRuleActiveModals);
+  const { closeDuplicateRuleModal, closeRenameGroupModal } = useRuleTableActions();
 
   // FIXME: Fetching multiple times
   // Fetch Rules here from Redux
   const ruleObjs = useSelector(getAllRuleObjs);
-  console.log({ ruleObjs });
 
   useFetchAndUpdateRules({ setIsLoading: setIsLoading });
+
+  console.log({ x: activeModals });
 
   return (
     <div className="rq-rules-list-container">
       {/* TODO: Add Feature Limiter Banner Here */}
+
       {/* TODO: Add Modals Required in Rules List here */}
+      {activeModals?.duplicateRuleModal?.isActive ? (
+        <DuplicateRuleModal
+          close={closeDuplicateRuleModal}
+          onDuplicate={closeDuplicateRuleModal}
+          isOpen={activeModals?.duplicateRuleModal?.isActive}
+          rule={activeModals?.duplicateRuleModal?.props?.ruleToDuplicate as Rule}
+        />
+      ) : null}
+
+      {activeModals?.renameGroupModal?.isActive ? (
+        <RenameGroupModal
+          toggle={closeRenameGroupModal}
+          isOpen={activeModals.renameGroupModal.isActive}
+          groupId={activeModals.renameGroupModal.props.groupId}
+        />
+      ) : null}
+
       <ContentHeader
         title="My Rules"
         subtitle="Create and manage your rules from here"
