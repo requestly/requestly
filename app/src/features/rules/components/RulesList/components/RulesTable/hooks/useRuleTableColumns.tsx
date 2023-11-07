c9f74dbf2d8dab14823750cd31f338dc8501110b
rohanmathur91 <mathurrohan04@gmail.com>
2023-11-07 16:49:49 +0530

    move modals in RulesTable

--- app/src/features/rules/components/RulesList/components/RulesTable/hooks/useRuleTableColumns.tsx
+++ app/src/features/rules/components/RulesList/components/RulesTable/hooks/useRuleTableColumns.tsx
@@ -152,7 +152,7 @@ const useRuleTableColumns = (options: Record<string, boolean>) => {
 
         const recordActions: MenuProps["items"] = [
           {
-            key: "0",
+            key: 0,
             onClick: () => {
               isRule ? handleChangeRuleGroupClick(rule) : handleRenameGroupClick(rule);
             },
@@ -174,22 +174,12 @@ const useRuleTableColumns = (options: Record<string, boolean>) => {
             key: 1,
             disabled: !isRule,
             onClick: () => {
-              isRule ? handleDuplicateRuleClick(rule) : console.log("Ungroup selected rules");
+              handleDuplicateRuleClick(rule);
             },
             label: (
-              // FIXME: add ungroup rules action
               <Row>
-                {isRule ? (
-                  <>
-                    <RiFileCopy2Line />
-                    Duplicate
-                  </>
-                ) : (
-                  <>
-                    <RiFileCopy2Line />
-                    Ungroup rules
-                  </>
-                )}
+                <RiFileCopy2Line />
+                Duplicate
               </Row>
             ),
           },
