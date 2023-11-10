89ef65520c615805a67668a305cbc52018d52c37
rohanmathur91 <mathurrohan04@gmail.com>
2023-11-10 10:09:19 +0530

    refactor utils

--- app/src/features/rules/components/RulesList/components/RulesTable/utils/index.ts
+++ app/src/features/rules/components/RulesList/components/RulesTable/utils/index.ts
@@ -13,7 +13,13 @@ export const getActiveRules = (records: RuleObj[]) => {
   return records.filter((record) => record.status === RuleObjStatus.ACTIVE);
 };
 
-export const convertToArray = <T>(record: T | T[]): T[] => (Array.isArray(record) ? record : [record]);
+export const convertToArray = <T>(record: T | T[]): T[] => {
+  return Array.isArray(record) ? record : [record];
+};
+
+export const getActiveRules = (records: RuleObj[]) => {
+  return records.filter((record) => record.status === RuleObjStatus.ACTIVE);
+};
 
 // FIXME: Performance Improvements
 export const rulesToContentTableDataAdapter = (rules: RuleObj[]): RuleTableDataType[] => {
