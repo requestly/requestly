0863b047d46f74441e19489d2060d7af60a33693
rohanmathur91 <mathurrohan04@gmail.com>
2023-12-07 18:31:27 +0530

    resolve merge conflicts

--- app/src/features/rules/components/RulesList/components/RulesTable/utils/index.ts
+++ app/src/features/rules/components/RulesList/components/RulesTable/utils/index.ts
@@ -13,22 +13,12 @@ export const getActiveRules = (records: RuleObj[]) => {
   return records.filter((record) => record.status === RuleObjStatus.ACTIVE);
 };
 
-export const convertToArray = <T>(record: T | T[]): T[] => {
-  return Array.isArray(record) ? record : [record];
-};
-
-export const getActiveRules = (records: RuleObj[]) => {
-  return records.filter((record) => record.status === RuleObjStatus.ACTIVE);
-};
-
 // FIXME: Performance Improvements
 export const rulesToContentTableDataAdapter = (rules: RuleObj[]): RuleTableDataType[] => {
   const ruleTableDataTypeMap: { [id: string]: RuleTableDataType } = {};
 
   const groupRules = rules.filter((rule) => !!rule.groupId) as Rule[];
-  const nonGroupRules: RuleObj[] = rules
-    .filter((rule) => !rule.groupId)
-    .map((rule) => (rule.objectType === RuleObjType.GROUP ? { ...rule, children: [] } : rule));
+  const nonGroupRules: RuleObj[] = rules.filter((rule) => !rule.groupId);
 
   nonGroupRules.forEach((rule) => {
     ruleTableDataTypeMap[rule.id] = rule;
@@ -38,9 +28,10 @@ export const rulesToContentTableDataAdapter = (rules: RuleObj[]): RuleTableDataT
     if (ruleTableDataTypeMap[rule.groupId]) {
       const updatedGroup = {
         ...ruleTableDataTypeMap[rule.groupId],
-        children: [...ruleTableDataTypeMap[rule.groupId].children, rule],
+        children: ruleTableDataTypeMap[rule.groupId]?.children
+          ? [...ruleTableDataTypeMap[rule.groupId].children, rule]
+          : [rule],
       };
-
       ruleTableDataTypeMap[rule.groupId] = updatedGroup;
     } else {
       // GroupId doesn't exist
