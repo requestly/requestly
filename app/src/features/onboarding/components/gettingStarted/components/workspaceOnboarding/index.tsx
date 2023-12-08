b2b40f41c117605318581149b07e253f1ad3fa7e
RuntimeTerror10 <parthb.git@gmail.com>
2023-12-08 10:59:17 +0530

    add join teams view

--- app/src/features/onboarding/components/gettingStarted/components/workspaceOnboarding/index.tsx
+++ app/src/features/onboarding/components/gettingStarted/components/workspaceOnboarding/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { getAppOnboardingDetails } from "store/selectors";
 import { DefaultTeamView } from "./components/defaultTeamView";
+import { JoinTeamView } from "./components/joinTeamsView";
 import { Invite } from "types";
 
 interface WorkspaceOnboardingViewProps {
@@ -10,6 +11,6 @@ interface WorkspaceOnboardingViewProps {
 
 export const WorkspaceOnboardingView: React.FC<WorkspaceOnboardingViewProps> = ({ pendingInvites }) => {
   const appOnboardingDetails = useSelector(getAppOnboardingDetails);
-  if (pendingInvites.length) return <>PENDING INVITES</>;
+  if (pendingInvites.length) return <JoinTeamView pendngInvites={pendingInvites} />;
   if (appOnboardingDetails.createdWorkspace) return <DefaultTeamView />;
 };
