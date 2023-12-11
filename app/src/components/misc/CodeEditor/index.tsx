da3dd0b6b4d128e369f439690a79559fc82a4d43
nsrCodes <navdeepsinghrathore2000@gmail.com>
2023-12-11 23:47:23 +0530

    chore: add sentry message for when code editor is stuck on loading for more than 3.5 seconds

--- app/src/components/misc/CodeEditor/index.tsx
+++ app/src/components/misc/CodeEditor/index.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import * as Sentry from "@sentry/react";
 import lazyWithRetry from "utils/lazyWithRetry";
+import PageLoader from "../PageLoader";
 
-export default lazyWithRetry(() => import(/* webpackChunkName: "CodeEditor" */ "./CodeEditor"));
+function FallbackToTrackInfiniteLoading() {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      Sentry.captureMessage("CodeEditor lazy loaded for too long", "fatal");
+    }, 3500);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+  return <PageLoader />;
+}
+
+export default lazyWithRetry(
+  () => import(/* webpackChunkName: "CodeEditor" */ "./CodeEditor"),
+  <FallbackToTrackInfiniteLoading />
+);
 
 // export { default } from "./CodeEditor";
