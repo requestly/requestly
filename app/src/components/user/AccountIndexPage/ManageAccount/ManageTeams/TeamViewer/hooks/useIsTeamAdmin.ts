cd647f648fb13f4509d98d2fff383db9b69095ac
Nafees Nehar <nafees87n@gmail.com>
2023-12-13 22:50:01 +0530

    fix: isTeamAdminCheck

--- app/src/components/user/AccountIndexPage/ManageAccount/ManageTeams/TeamViewer/hooks/useIsTeamAdmin.ts
+++ app/src/components/user/AccountIndexPage/ManageAccount/ManageTeams/TeamViewer/hooks/useIsTeamAdmin.ts
@@ -1,27 +1,31 @@
 import { useEffect, useState } from "react";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import Logger from "lib/logger";
+import { useSelector } from "react-redux";
+import { getUserAuthDetails } from "store/selectors";
 
 export const useIsTeamAdmin = (teamId: string): { isLoading: boolean; isTeamAdmin: boolean } => {
+  const user = useSelector(getUserAuthDetails);
+
   const [isLoading, setIsLoading] = useState(false);
   const [isTeamAdmin, setIsTeamAdmin] = useState(false);
 
   useEffect(() => {
-    const functions = getFunctions();
+    if (user.loggedIn) {
+      setIsLoading(true);
+      const getIsTeamAdmin = httpsCallable(getFunctions(), "teams-isTeamAdmin");
 
-    setIsLoading(true);
-    const getIsTeamAdmin = httpsCallable(functions, "teams-isTeamAdmin");
-
-    getIsTeamAdmin({ teamId })
-      .then((res) => {
-        //@ts-ignore
-        if (res.data?.success) setIsTeamAdmin(res.data?.isAdmin);
-      })
-      .catch((err) => {
-        Logger.log("Error while checking team admin", err);
-      })
-      .finally(() => setIsLoading(false));
-  }, [teamId]);
+      getIsTeamAdmin({ teamId })
+        .then((res) => {
+          //@ts-ignore
+          if (res.data?.success) setIsTeamAdmin(res.data?.isAdmin);
+        })
+        .catch((err) => {
+          Logger.log("Error while checking team admin", err);
+        })
+        .finally(() => setIsLoading(false));
+    }
+  }, [teamId, user]);
 
   return { isLoading, isTeamAdmin };
 };
