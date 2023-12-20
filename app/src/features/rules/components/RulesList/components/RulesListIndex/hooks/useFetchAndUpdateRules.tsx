1f3500912dc5d1c436c12dff090bc04130d356d5
rohanmathur91 <mathurrohan04@gmail.com>
2023-12-20 17:18:02 +0530

    send user attributes

--- app/src/features/rules/components/RulesList/components/RulesListIndex/hooks/useFetchAndUpdateRules.tsx
+++ app/src/features/rules/components/RulesList/components/RulesListIndex/hooks/useFetchAndUpdateRules.tsx
@@ -1,14 +1,16 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-
-// @ts-ignore
-import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import { getAppMode, getIsHardRefreshRulesPending, getIsRefreshRulesPending } from "store/selectors";
 import { useHasChanged } from "hooks";
 import { StorageService } from "init";
 import { useDispatch } from "react-redux";
 import { isGroupsSanitizationPassed } from "components/features/rules/RulesIndexPage/actions";
 import { rulesActions } from "store/features/rules/slice";
+import { Group, Rule, RuleObjStatus, RuleObjType } from "features/rules/types/rules";
+import { submitAttrUtil } from "utils/AnalyticsUtils";
+import APP_CONSTANTS from "config/constants";
+
+const TRACKING = APP_CONSTANTS.GA_EVENTS;
 
 interface Props {
   setIsLoading: (isLoading: boolean) => void;
@@ -24,49 +26,44 @@ const useFetchAndUpdateRules = ({ setIsLoading }: Props) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    console.log("runnning....");
     if (hasIsRulesListHardRefreshPendingChanged) {
       setIsLoading(true);
     }
 
     // FIXME: This can be fetched in one call. getRecords([RULE, GROUP]);
-    const groupsPromise = StorageService(appMode).getRecords(GLOBAL_CONSTANTS.OBJECT_TYPES.GROUP);
-    const rulesPromise = StorageService(appMode).getRecords(GLOBAL_CONSTANTS.OBJECT_TYPES.RULE);
+    const groupsPromise = StorageService(appMode).getRecords(RuleObjType.GROUP);
+    const rulesPromise = StorageService(appMode).getRecords(RuleObjType.RULE);
     Promise.all([groupsPromise, rulesPromise]).then(async (data) => {
-      const groups = data[0];
-      const rules = data[1];
+      const groups = data[0] as Group[];
+      const rules = data[1] as Rule[];
 
       // FIXME: This can be removed/improved. Move this logic to src/features
       const isGroupsSanitizationPassedResult = await isGroupsSanitizationPassed({ rules, groups, appMode });
 
       const finalRuleObjs = [...isGroupsSanitizationPassedResult.updatedRules, ...groups];
 
-      if (isGroupsSanitizationPassedResult.success === false) {
-        // Sanitization has updated the storage! but we dont need to fetch again as we have the updated copy
-        dispatch(rulesActions.ruleObjsSetAll(finalRuleObjs));
-      } else {
-        // Sanitization required doing nothing, so continue as it is
-        dispatch(rulesActions.ruleObjsSetAll(finalRuleObjs));
-      }
+      dispatch(rulesActions.ruleObjsSetAll(finalRuleObjs));
 
       setIsLoading(false);
 
       // TODO: Cleanup
       //ANALYTICS
-      // const ruleTypes = rules.reduce((result, { ruleType }) => result.add(ruleType), new Set());
-      // const numRuleTypes = window.localStorage.getItem("num_rule_types") || 0;
-      // submitAttrUtil(TRACKING.ATTR.NUM_RULE_TYPES_TRIED, Math.max(numRuleTypes, ruleTypes.size));
-      // submitAttrUtil(TRACKING.ATTR.NUM_RULES, rules.length);
-      // submitAttrUtil(TRACKING.ATTR.NUM_RULE_TYPES, ruleTypes.size);
-      // window.localStorage.setItem("num_rule_types", ruleTypes.size);
-      // submitAttrUtil(
-      //   TRACKING.ATTR.NUM_ACTIVE_RULES,
-      //   rules.filter((rule) => rule.status === GLOBAL_CONSTANTS.RULE_STATUS.ACTIVE).length
-      // );
-      // submitAttrUtil(TRACKING.ATTR.NUM_GROUPS, groups.length);
-      // submitAttrUtil(
-      //   TRACKING.ATTR.NUM_ACTIVE_GROUPS,
-      //   groups.filter((group) => group.status === GLOBAL_CONSTANTS.GROUP_STATUS.ACTIVE).length
-      // );
+      const ruleTypes = rules.reduce((result, { ruleType }) => result.add(ruleType), new Set());
+      const numRuleTypes = parseInt(window.localStorage.getItem("num_rule_types") || "0");
+      submitAttrUtil(TRACKING.ATTR.NUM_RULE_TYPES_TRIED, Math.max(numRuleTypes, ruleTypes.size));
+      submitAttrUtil(TRACKING.ATTR.NUM_RULES, rules.length);
+      submitAttrUtil(TRACKING.ATTR.NUM_RULE_TYPES, ruleTypes.size);
+      window.localStorage.setItem("num_rule_types", JSON.stringify(ruleTypes.size));
+      submitAttrUtil(
+        TRACKING.ATTR.NUM_ACTIVE_RULES,
+        rules.filter((rule) => rule.status === RuleObjStatus.ACTIVE).length
+      );
+      submitAttrUtil(TRACKING.ATTR.NUM_GROUPS, groups.length);
+      submitAttrUtil(
+        TRACKING.ATTR.NUM_ACTIVE_GROUPS,
+        groups.filter((group) => group.status === RuleObjStatus.ACTIVE).length
+      );
     });
   }, [
     dispatch,
