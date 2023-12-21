9b3b3b5ac41fc44fd7741cf951b5b3b0be1e1dfd
RuntimeTerror10 <parthb.git@gmail.com>
2023-12-21 16:13:44 +0530

    add changelog card

--- app/src/components/Home/index.tsx
+++ app/src/components/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Col, Row } from "antd";
-import { RulesCard } from "./components/RulesCard";
+import { TeamsCard } from "./components/TeamsCard";
 import "./home.scss";
-import { SessionsCard } from "./components/SessionsCard";
+import { ChangeLogCard } from "./components/ChangelogCard";
 
 export const Home: React.FC = () => {
   return (
@@ -10,10 +10,22 @@ export const Home: React.FC = () => {
       <Col className="homepage-content">
         <Row className="homepage-primary-cards-wrapper">
           <Col className="homepage-primary-card" span={12}>
-            <RulesCard />
+            <>RULES HERE</>
           </Col>
           <Col className="homepage-primary-card" span={12}>
-            <SessionsCard />
+            <>SESSIONS HERE</>
+          </Col>
+        </Row>
+        <Row>TEMPLATES HERE</Row>
+        <Row className="homepage-bottom-section">
+          <Col span={12} className="homepage-teams-card homepage-primary-card">
+            <TeamsCard />
+          </Col>
+          <Col span={6} className="homepage-help-card homepage-primary-card">
+            HELP HERE
+          </Col>
+          <Col span={6} className="homepage-changelog-card homepage-primary-card">
+            <ChangeLogCard />
           </Col>
         </Row>
       </Col>
