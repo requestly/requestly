165a209f3dfa8345bd837e1d4c3c6d841cd070fb
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2023-12-26 10:51:23 +0530

    RQ-1182: Homepage help changelog cards (#1291)

---
+++ app/src/components/Home/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Col, Row } from "antd";
+import { ChangeLogCard } from "./components/ChangelogCard";
+import { HelpCard } from "./components/HelpCard";
+import { TeamsCard } from "./components/TeamsCard";
+import "./home.scss";
+
+export const Home: React.FC = () => {
+  return (
+    <Col className="homepage-wrapper">
+      <Col className="homepage-content">
+        <Row className="homepage-primary-cards-wrapper">
+          <Col className="homepage-primary-card" span={12}>
+            <>RULES HERE</>
+          </Col>
+          <Col className="homepage-primary-card" span={12}>
+            <>SESSIONS HERE</>
+          </Col>
+        </Row>
+        <Row>TEMPLATES HERE</Row>
+        <Row className="homepage-bottom-section">
+          <Col span={12} className="homepage-teams-card homepage-primary-card">
+            <TeamsCard />
+          </Col>
+          <Col span={6} className="homepage-help-card homepage-primary-card">
+            <HelpCard />
+          </Col>
+          <Col span={6} className="homepage-changelog-card homepage-primary-card">
+            <ChangeLogCard />
+          </Col>
+        </Row>
+      </Col>
+    </Col>
+  );
+};
