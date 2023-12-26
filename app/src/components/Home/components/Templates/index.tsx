c3c858a1a711ff9dbb2e510fe6f6b2ec56918e97
RuntimeTerror10 <parthb.git@gmail.com>
2023-12-26 16:27:00 +0530

    chore: fix rule preview modal styling

--- app/src/components/Home/components/Templates/index.tsx
+++ app/src/components/Home/components/Templates/index.tsx
@@ -1,51 +1,73 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Col, Row, Typography } from "antd";
+import RulePreviewModal from "components/landing/ruleTemplates/RulePreviewModal";
 import { ruleIcons } from "components/common/RuleIcon/ruleIcons";
 import APP_CONSTANTS from "config/constants";
 import templatesMap from "../../../landing/ruleTemplates/templates.json";
 import { RuleType } from "types";
-import "./index.scss";
 import { Link } from "react-router-dom";
 import PATHS from "config/constants/sub/paths";
 import { RQButton } from "lib/design-system/components";
+import "./index.scss";
 
 export const Templates: React.FC = () => {
+  const [ruleToPreview, setRuleToPreview] = useState(null);
+  const [isPreviewModalOpen, setIsPreviewModalOpen] = useState(false);
+
   const filteredTemplates = useMemo(() => templatesMap.templates.filter((template: any) => template.isFeatured), []);
-  console.log({ filteredTemplates });
+
+  const handleShowRulePreview = (ruleData: any) => {
+    setRuleToPreview(ruleData);
+    setIsPreviewModalOpen(true);
+  };
 
   return (
-    <Col className="home-templates-wrapper">
-      <Typography.Title level={5} className="home-templates-title">
-        Start with a template
-      </Typography.Title>
-      <Col span={24} className="home-templates-row">
-        {filteredTemplates.map((template: any) => {
-          const ruleType = template.data.ruleDefinition.ruleType;
-          return (
-            <div className="homepage-primary-card home-templates-row-card">
-              <Typography.Text className="home-templates-row-card-title">{template.name}</Typography.Text>
-              <Row gutter={8} align="middle" className="home-templates-row-card-tag">
-                <Col className="home-templates-row-card-icon">{ruleIcons[ruleType as RuleType]}</Col>
-                <Col className="home-templates-row-card-ruletype">
-                  {APP_CONSTANTS.RULE_TYPES_CONFIG[ruleType]?.NAME}
-                </Col>
-              </Row>
-              <Col className="home-templates-card-action-bar">
-                <RQButton type="default" size="small" className="home-templates-card-action-btn">
-                  Preview
-                </RQButton>
-                <RQButton type="primary" size="small" className="home-templates-card-action-btn">
+    <>
+      {isPreviewModalOpen && (
+        <RulePreviewModal
+          isOpen={isPreviewModalOpen}
+          toggle={() => setIsPreviewModalOpen((prev) => !prev)}
+          rule={ruleToPreview}
+        />
+      )}
+      <Col className="home-templates-wrapper">
+        <Typography.Title level={5} className="home-templates-title">
+          Start with a template
+        </Typography.Title>
+        <Col span={24} className="home-templates-row">
+          {filteredTemplates.map((template: any) => {
+            const ruleType = template.data.ruleDefinition.ruleType;
+            return (
+              <div className="homepage-primary-card home-templates-row-card">
+                <Typography.Text className="home-templates-row-card-title">{template.name}</Typography.Text>
+                <Row gutter={8} align="middle" className="home-templates-row-card-tag">
+                  <Col className="home-templates-row-card-icon">{ruleIcons[ruleType as RuleType]}</Col>
+                  <Col className="home-templates-row-card-ruletype">
+                    {APP_CONSTANTS.RULE_TYPES_CONFIG[ruleType]?.NAME}
+                  </Col>
+                </Row>
+                <Col className="home-templates-card-action-bar">
+                  <RQButton
+                    type="primary"
+                    size="small"
+                    className="home-templates-card-action-btn"
+                    onClick={() => handleShowRulePreview(template.data)}
+                  >
+                    Preview
+                  </RQButton>
+                  {/* <RQButton type="primary" size="small" className="home-templates-card-action-btn">
                   Use this template
-                </RQButton>
-              </Col>
-            </div>
-          );
-        })}
-      </Col>
+                </RQButton> */}
+                </Col>
+              </div>
+            );
+          })}
+        </Col>
 
-      <Link to={PATHS.RULES.TEMPLATES.ABSOLUTE} className="home-templates-view-all-link">
-        View all templates
-      </Link>
-    </Col>
+        <Link to={PATHS.RULES.TEMPLATES.ABSOLUTE} className="home-templates-view-all-link">
+          View all templates
+        </Link>
+      </Col>
+    </>
   );
 };
