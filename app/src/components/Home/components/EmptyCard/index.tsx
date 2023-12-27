b16603a42ebb74063eface9290d265e5575ce372
RuntimeTerror10 <parthb.git@gmail.com>
2023-12-27 14:01:24 +0530

    add empty state card

---
+++ app/src/components/Home/components/EmptyCard/index.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+
+interface Props {
+  icon: string;
+  title: string;
+  description?: string;
+  primaryButton?: ReactNode;
+  secondaryButton?: ReactNode;
+}
+
+export const HomepageEmptyCard: React.FC<Props> = ({ icon, title, description, primaryButton, secondaryButton }) => {
+  return (
+    <div className="homepage-emptycard">
+      <img src={icon} className="homepage-emptycard-icon" />
+      <div className="homepage-emptycard-title">{title}</div>
+      <div className="mt-8">{description}</div>
+      <div className="homepage-emptycard-actions">
+        <div>{primaryButton}</div>
+        <div className="mt-8">{secondaryButton}</div>
+      </div>
+    </div>
+  );
+};
--- app/src/components/Home/components/SessionsCard/index.tsx
+++ app/src/components/Home/components/SessionsCard/index.tsx
@@ -1,9 +1,15 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Button, Col, Row, Skeleton } from "antd";
-import sessionBookIcon from "../../assets/sessionbook.svg";
+import { useSelector } from "react-redux";
+import { Button, Col, Row, Skeleton, Tooltip } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { getUserAuthDetails } from "store/selectors";
+import { getCurrentlyActiveWorkspace } from "store/features/teams/selectors";
+import { useHasChanged } from "hooks";
 import { RQButton } from "lib/design-system/components";
 import { IoMdAdd } from "@react-icons/all-files/io/IoMdAdd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
 import { MdOutlineSettings } from "@react-icons/all-files/md/MdOutlineSettings";
+import sessionBookIcon from "../../assets/sessionbook.svg";
 import firebaseApp from "../../../../firebase";
 import {
   getFirestore,
@@ -15,13 +21,9 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 import { getOwnerId } from "backend/utils";
-import { useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { getUserAuthDetails } from "store/selectors";
-import { getCurrentlyActiveWorkspace } from "store/features/teams/selectors";
-import { useHasChanged } from "hooks";
 import { SessionRecording } from "views/features/sessions/types";
 import { SessionsListItem } from "./components/SessionsListItem";
+import { HomepageEmptyCard } from "../EmptyCard";
 import { redirectToSessionRecordingHome, redirectToSessionSettings } from "utils/RedirectionUtils";
 import PATHS from "config/constants/sub/paths";
 import "./sessionsCard.scss";
@@ -93,44 +95,42 @@ export const SessionsCard: React.FC = () => {
     }
   }, [hasUserChanged, workspace, stableFetchRecordings, user?.details?.profile?.uid, user.loggedIn]);
 
-  console.log({ sessionRecordings });
-
   return (
     <>
-      <Row align="middle" justify="space-between" className="w-full">
-        <Col span={5}>
-          <Row gutter={8} align="middle">
-            <Col>
-              <img width={16} height={16} src={sessionBookIcon} alt="rules" />
-            </Col>
-            <Col className="text-white primary-card-header">SessionBook</Col>
-          </Row>
-        </Col>
-        <Col span={19}>
-          <div className="sessions-card-action-btns">
-            <Button
-              type="text"
-              className="sessions-card-config-action-btn"
-              icon={<MdOutlineSettings className="mr-8" />}
-              onClick={() => redirectToSessionSettings(navigate)}
-            >
-              Configure auto-recording
-            </Button>
-            <RQButton
-              icon={<IoMdAdd className="mr-8" />}
-              type="default"
-              onClick={() => redirectToSessionRecordingHome(navigate)}
-            >
-              New Session
-            </RQButton>
-          </div>
-        </Col>
-      </Row>
       <Col className="sessions-card-body w-full mt-8">
         {isLoading ? (
           <Skeleton active paragraph={{ rows: 6 }} />
         ) : sessionRecordings.length > 0 ? (
           <>
+            <Row align="middle" justify="space-between" className="w-full">
+              <Col span={5}>
+                <Row gutter={8} align="middle">
+                  <Col>
+                    <img width={16} height={16} src={sessionBookIcon} alt="rules" />
+                  </Col>
+                  <Col className="text-white primary-card-header">SessionBook</Col>
+                </Row>
+              </Col>
+              <Col span={19}>
+                <div className="sessions-card-action-btns">
+                  <Button
+                    type="text"
+                    className="sessions-card-config-action-btn"
+                    icon={<MdOutlineSettings className="mr-8" />}
+                    onClick={() => redirectToSessionSettings(navigate)}
+                  >
+                    Configure auto-recording
+                  </Button>
+                  <RQButton
+                    icon={<IoMdAdd className="mr-8" />}
+                    type="default"
+                    onClick={() => redirectToSessionRecordingHome(navigate)}
+                  >
+                    New Session
+                  </RQButton>
+                </div>
+              </Col>
+            </Row>
             <div className="sessions-card-list">
               {sessionRecordings.map((session: SessionRecording, index: number) => {
                 return <SessionsListItem key={index} session={session} />;
@@ -145,7 +145,37 @@ export const SessionsCard: React.FC = () => {
             </>
           </>
         ) : (
-          <div> No sessions found</div>
+          <HomepageEmptyCard
+            icon={sessionBookIcon}
+            title="SessionBook"
+            description="Replay screen, mouse activity, network data, console logs."
+            primaryButton={
+              <RQButton type="primary" onClick={() => redirectToSessionRecordingHome(navigate)}>
+                Record new session
+              </RQButton>
+            }
+            secondaryButton={
+              <Row align="middle" gutter={4}>
+                <Col>
+                  <RQButton
+                    type="text"
+                    className="homepage-emptycard-secondary-btn"
+                    onClick={() => redirectToSessionSettings(navigate)}
+                  >
+                    Enable auto recording
+                  </RQButton>
+                </Col>
+                <Col style={{ alignSelf: "center" }}>
+                  <Tooltip
+                    color="#000"
+                    title="When enabled, we automatically save the last 5 min of recording locally in your browser so you don't have to go back and re-record bugs as you spot them. You can share it with the team in one click."
+                  >
+                    <QuestionCircleOutlined />
+                  </Tooltip>
+                </Col>
+              </Row>
+            }
+          />
         )}
       </Col>
     </>
