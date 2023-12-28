625e54a59c0602f2c0f9ef3915fc5f5494e77884
RuntimeTerror10 <parthb.git@gmail.com>
2023-12-28 16:46:22 +0530

    make homepage layout responsive

--- app/src/components/Home/index.tsx
+++ app/src/components/Home/index.tsx
@@ -5,30 +5,30 @@ import { HelpCard } from "./components/HelpCard";
 import { TeamsCard } from "./components/WorkspaceCard";
 import { SessionsCard } from "./components/SessionsCard";
 import { Templates } from "./components/Templates";
-import "./home.scss";
 import { RulesCard } from "./components/RulesCard";
+import "./home.scss";
 
 export const Home: React.FC = () => {
   return (
     <Col className="homepage-wrapper">
       <Col className="homepage-content">
         <Row className="homepage-primary-cards-wrapper">
-          <Col className="homepage-primary-card" span={12}>
+          <Col className="homepage-primary-card" xs={24} md={24} lg={12}>
             <RulesCard />
           </Col>
-          <Col className="homepage-primary-card" span={12}>
+          <Col className="homepage-primary-card" xs={24} md={24} lg={12}>
             <SessionsCard />
           </Col>
         </Row>
         <Templates />
-        <Row className="homepage-bottom-section">
-          <Col span={12} className="homepage-teams-card homepage-primary-card" style={{ padding: 0 }}>
+        <Row className="homepage-bottom-section" wrap={false}>
+          <Col xs={24} md={24} lg={12} className="homepage-teams-card homepage-primary-card" style={{ padding: 0 }}>
             <TeamsCard />
           </Col>
-          <Col span={6} className="homepage-help-card homepage-primary-card">
+          <Col xs={24} md={24} lg={6} className="homepage-help-card homepage-primary-card">
             <HelpCard />
           </Col>
-          <Col span={6} className="homepage-changelog-card homepage-primary-card">
+          <Col xs={24} md={24} lg={6} className="homepage-changelog-card homepage-primary-card">
             <ChangeLogCard />
           </Col>
         </Row>
