110ecac0bcc831af52ae961bbf4cf8c8bd9be8c1
RuntimeTerror10 <parthb.git@gmail.com>
2023-12-28 10:21:38 +0530

    add empty state UI

--- app/src/components/Home/components/RulesCard/index.tsx
+++ app/src/components/Home/components/RulesCard/index.tsx
@@ -9,7 +9,9 @@ import { useFeatureLimiter } from "hooks/featureLimiter/useFeatureLimiter";
 import { PremiumFeature } from "features/pricing";
 import { FeatureLimitType } from "hooks/featureLimiter/types";
 import { PremiumIcon } from "components/common/PremiumIcon";
-import { redirectToCreateNewRule } from "utils/RedirectionUtils";
+import { HomepageEmptyCard } from "../EmptyCard";
+import { RQButton } from "lib/design-system/components";
+import { redirectToCreateNewRule, redirectToTemplates } from "utils/RedirectionUtils";
 import RULE_TYPES_CONFIG from "config/constants/sub/rule-types";
 import { Rule, RuleType } from "types";
 import rulesIcon from "../../assets/rules.svg";
@@ -20,6 +22,7 @@ import { StorageService } from "init";
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import PATHS from "config/constants/sub/paths";
 import Logger from "lib/logger";
+import { isExtensionInstalled } from "actions/ExtensionActions";
 import "./rulesCard.scss";
 
 export const RulesCard: React.FC = () => {
@@ -69,71 +72,87 @@ export const RulesCard: React.FC = () => {
   }, [navigate, getFeatureLimitValue]);
 
   useEffect(() => {
-    StorageService(appMode)
-      .getRecords(GLOBAL_CONSTANTS.OBJECT_TYPES.RULE)
-      .then((res) => {
-        setRules(
-          res
-            .sort((a: Rule, b: Rule) => (b.creationDate as number) - (a.creationDate as number))
-            .slice(0, MAX_RULES_TO_SHOW + 1)
-        );
-      })
-      .catch((e) => {
-        Logger.log(e);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    if (isExtensionInstalled()) {
+      StorageService(appMode)
+        .getRecords(GLOBAL_CONSTANTS.OBJECT_TYPES.RULE)
+        .then((res) => {
+          setRules(
+            res
+              .sort((a: Rule, b: Rule) => (b.creationDate as number) - (a.creationDate as number))
+              .slice(0, MAX_RULES_TO_SHOW + 1)
+          );
+        })
+        .catch((e) => {
+          Logger.log(e);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
   }, [appMode, workspace, hasUserChanged]);
 
   if (isLoading) return <Skeleton active paragraph={{ rows: 6 }} />;
 
   return (
     <>
-      <Row justify="space-between" align="middle">
-        <Col span={16}>
-          <Row gutter={8} align="middle">
-            <Col>
-              <img width={16} height={16} src={rulesIcon} alt="rules" />
+      {rules.length ? (
+        <>
+          <Row justify="space-between" align="middle">
+            <Col span={16}>
+              <Row gutter={8} align="middle">
+                <Col>
+                  <img width={16} height={16} src={rulesIcon} alt="rules" />
+                </Col>
+                <Col className="text-white primary-card-header">HTTP Rules</Col>
+              </Row>
             </Col>
-            <Col className="text-white primary-card-header">HTTP Rules</Col>
+            <Col span={8} className="homepage-rules-card-header-action">
+              {/* TODO: create a separate component for this dropdown button and use it in rules table as well */}
+              <Dropdown.Button
+                overlay={dropdownMenu}
+                type="default"
+                icon={<MdExpandMore />}
+                className="rules-card-dropdown-btn"
+                trigger={["click"]}
+                onClick={() => redirectToCreateNewRule(navigate, null, "home")}
+              >
+                <>
+                  <IoMdAdd /> New rule
+                </>
+              </Dropdown.Button>
+            </Col>
           </Row>
-        </Col>
-        <Col span={8} className="homepage-rules-card-header-action">
-          {/* TODO: create a separate component for this dropdown button and use it in rules table as well */}
-          <Dropdown.Button
-            overlay={dropdownMenu}
-            type="default"
-            icon={<MdExpandMore />}
-            className="rules-card-dropdown-btn"
-            trigger={["click"]}
-            onClick={() => redirectToCreateNewRule(navigate, null, "home")}
-          >
-            <>
-              <IoMdAdd /> New rule
-            </>
-          </Dropdown.Button>
-        </Col>
-      </Row>
-      <>
-        {rules.length ? (
-          <>
-            <div className="homepage-rules-list">
-              {rules.map((rule: Rule, index: number) => {
-                if (index >= MAX_RULES_TO_SHOW) return null;
-                return <div className="homepage-rules-list-item">{rule.name}</div>;
-              })}
-            </div>
-            {rules.length > MAX_RULES_TO_SHOW && (
-              <Link className="homepage-view-all-link" to={PATHS.RULES.MY_RULES.ABSOLUTE}>
-                View all rules
-              </Link>
-            )}
-          </>
-        ) : (
-          <>EMPTY</>
-        )}
-      </>
+          <div className="homepage-rules-list">
+            {rules.map((rule: Rule, index: number) => {
+              if (index >= MAX_RULES_TO_SHOW) return null;
+              return <div className="homepage-rules-list-item">{rule.name}</div>;
+            })}
+          </div>
+          {rules.length > MAX_RULES_TO_SHOW && (
+            <Link className="homepage-view-all-link" to={PATHS.RULES.MY_RULES.ABSOLUTE}>
+              View all rules
+            </Link>
+          )}
+        </>
+      ) : (
+        <HomepageEmptyCard
+          icon={rulesIcon}
+          title="HTTP Rules"
+          description="Create rules to modify HTTP requests and responses, including URL redirects, API modifications, and header adjustments."
+          primaryButton={
+            <RQButton type="primary" onClick={() => redirectToCreateNewRule(navigate, null, "homepage")}>
+              Create new Rule
+            </RQButton>
+          }
+          secondaryButton={
+            <RQButton type="text" onClick={() => redirectToTemplates(navigate)}>
+              Start with a template
+            </RQButton>
+          }
+        />
+      )}
     </>
   );
 };
