3829ca7b5c44c1dac7e258a47fc5bbf2598229fb
RuntimeTerror10 <parthb.git@gmail.com>
2023-12-28 10:21:38 +0530

    add empty state UI

--- app/src/components/Home/components/EmptyCard/index.tsx
+++ app/src/components/Home/components/EmptyCard/index.tsx
@@ -13,7 +13,7 @@ export const HomepageEmptyCard: React.FC<Props> = ({ icon, title, description, p
     <div className="homepage-emptycard">
       <img src={icon} className="homepage-emptycard-icon" alt={title} />
       <div className="homepage-emptycard-title">{title}</div>
-      <div className="mt-8">{description}</div>
+      <div className="mt-8 text-center homepage-emptycard-description">{description}</div>
       <div className="homepage-emptycard-actions">
         <div>{primaryButton}</div>
         <div className="mt-8">{secondaryButton}</div>
