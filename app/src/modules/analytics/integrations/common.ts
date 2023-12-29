9fc0826e4dc2e73e6977e91f0d6f582873fddc3f
Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>
2023-12-29 16:10:07 +0530

    feat: workspace tracking in analytics group context (#1293)
    * feat: workspace tracking in analytics group context
    
    * chore: rename group analytics function

--- app/src/modules/analytics/integrations/common.ts
+++ app/src/modules/analytics/integrations/common.ts
@@ -2,7 +2,9 @@ export interface IAnalyticsIntegration {
   enabled: boolean;
   isIntegrationDone: boolean;
   startTime: number;
+  groupId?: string | null;
   init: (user: any) => void;
   trackEvent: (eventName: string, eventParams: any, retry?: number) => void;
   trackAttr: (name: string, value: string, retry?: number) => void;
+  onGroupChange?: (groupId: string, groupName: string) => void;
 }
