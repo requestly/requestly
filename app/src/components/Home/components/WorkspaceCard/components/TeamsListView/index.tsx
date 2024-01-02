09806472da11eb42d464107d72b7bb4dbf7f8748
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-02 12:19:21 +0530

    chore: add transition

--- app/src/components/Home/components/WorkspaceCard/components/TeamsListView/index.tsx
+++ app/src/components/Home/components/WorkspaceCard/components/TeamsListView/index.tsx
@@ -3,6 +3,7 @@ import { Col, Typography } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { RQButton } from "lib/design-system/components";
 import { TeamsListItem } from "./components/TeamsListItem";
+import { m, AnimatePresence } from "framer-motion";
 import { IoMdAdd } from "@react-icons/all-files/io/IoMdAdd";
 import { Invite, Team } from "types";
 import { actions } from "store";
@@ -20,53 +21,63 @@ export const TeamsListView: React.FC<Props> = ({ pendingInvites, heading, subhea
   const availableTeams = useSelector(getAvailableTeams);
 
   return (
-    <Col style={{ position: "relative" }} className="h-full">
-      <Col className="homepage-teams-card-content">
-        <Typography.Text className="homepage-teams-list-title">{heading}</Typography.Text>
-        <Col>
-          {subheading && <Typography.Text className="mt-8 homepage-teams-list-subtitle">{subheading}</Typography.Text>}
-          <Col className="homepage-teams-list">
-            {pendingInvites ? (
-              <>
-                {pendingInvites.map((invite: Invite, index: number) => (
-                  <TeamsListItem
-                    key={index}
-                    inviteId={invite.id}
-                    teamId={invite.metadata.teamId as string}
-                    teamName={invite.metadata.teamName as string}
-                  />
-                ))}
-              </>
-            ) : (
-              <>
-                {availableTeams.map((team: Team, index: number) => (
-                  <TeamsListItem key={index} teamId={team.id} teamName={team.name} />
-                ))}
-              </>
+    <AnimatePresence>
+      <m.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        style={{ position: "relative" }}
+        className="h-full"
+      >
+        <Col className="homepage-teams-card-content">
+          <Typography.Text className="homepage-teams-list-title">{heading}</Typography.Text>
+          <Col>
+            {subheading && (
+              <Typography.Text className="mt-8 homepage-teams-list-subtitle">{subheading}</Typography.Text>
             )}
+            <Col className="homepage-teams-list">
+              {pendingInvites ? (
+                <>
+                  {pendingInvites.map((invite: Invite, index: number) => (
+                    <TeamsListItem
+                      key={index}
+                      inviteId={invite.id}
+                      teamId={invite.metadata.teamId as string}
+                      teamName={invite.metadata.teamName as string}
+                    />
+                  ))}
+                </>
+              ) : (
+                <>
+                  {availableTeams.map((team: Team, index: number) => (
+                    <TeamsListItem key={index} teamId={team.id} teamName={team.name} />
+                  ))}
+                </>
+              )}
+            </Col>
           </Col>
         </Col>
-      </Col>
-      <RQButton
-        size="large"
-        block
-        icon={<IoMdAdd className="mr-8" />}
-        type="default"
-        className="homepage-teams-list-create-workspace-btn"
-        onClick={() => {
-          dispatch(
-            actions.toggleActiveModal({
-              modalName: "createWorkspaceModal",
-              newValue: true,
-              newProps: {
-                source: "homepage",
-              },
-            })
-          );
-        }}
-      >
-        New workspace
-      </RQButton>
-    </Col>
+        <RQButton
+          size="large"
+          block
+          icon={<IoMdAdd className="mr-8" />}
+          type="default"
+          className="homepage-teams-list-create-workspace-btn"
+          onClick={() => {
+            dispatch(
+              actions.toggleActiveModal({
+                modalName: "createWorkspaceModal",
+                newValue: true,
+                newProps: {
+                  source: "homepage",
+                },
+              })
+            );
+          }}
+        >
+          New workspace
+        </RQButton>
+      </m.div>
+    </AnimatePresence>
   );
 };
