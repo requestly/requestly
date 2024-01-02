a6397412659824f11d066944bf30abf7c27f040d
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-02 15:12:37 +0530

    setup A/B experiment

--- app/src/components/Home/index.tsx
+++ app/src/components/Home/index.tsx
@@ -6,33 +6,39 @@ import { TeamsCard } from "./components/WorkspaceCard";
 import { Templates } from "./components/Templates";
 import { RulesCard } from "./components/RulesCard";
 import { MocksCard } from "./components/MocksCard";
+import { useFeatureValue } from "@growthbook/growthbook-react";
+import { HomePage } from "components/landing/Home";
 import "./home.scss";
 
 export const Home: React.FC = () => {
-  return (
-    <Col className="homepage-wrapper">
-      <Col className="homepage-content">
-        <Row className="homepage-primary-cards-wrapper">
-          <Col className="homepage-primary-card" xs={24} md={24} lg={12}>
-            <RulesCard />
-          </Col>
-          <Col className="homepage-primary-card" span={12}>
-            <MocksCard />
-          </Col>
-        </Row>
-        <Templates />
-        <Row className="homepage-bottom-section" wrap={false}>
-          <Col xs={24} md={24} lg={12} className="homepage-teams-card homepage-primary-card" style={{ padding: 0 }}>
-            <TeamsCard />
-          </Col>
-          <Col xs={24} md={24} lg={6} className="homepage-help-card homepage-primary-card">
-            <HelpCard />
-          </Col>
-          <Col xs={24} md={24} lg={6} className="homepage-changelog-card homepage-primary-card">
-            <ChangeLogCard />
-          </Col>
-        </Row>
+  const shouldShowNewHomePage = useFeatureValue("homepage-experiment", true);
+
+  if (shouldShowNewHomePage) {
+    return (
+      <Col className="homepage-wrapper">
+        <Col className="homepage-content">
+          <Row className="homepage-primary-cards-wrapper">
+            <Col className="homepage-primary-card" xs={24} md={24} lg={12}>
+              <RulesCard />
+            </Col>
+            <Col className="homepage-primary-card" span={12}>
+              <MocksCard />
+            </Col>
+          </Row>
+          <Templates />
+          <Row className="homepage-bottom-section" wrap={false}>
+            <Col xs={24} md={24} lg={12} className="homepage-teams-card homepage-primary-card" style={{ padding: 0 }}>
+              <TeamsCard />
+            </Col>
+            <Col xs={24} md={24} lg={6} className="homepage-help-card homepage-primary-card">
+              <HelpCard />
+            </Col>
+            <Col xs={24} md={24} lg={6} className="homepage-changelog-card homepage-primary-card">
+              <ChangeLogCard />
+            </Col>
+          </Row>
+        </Col>
       </Col>
-    </Col>
-  );
+    );
+  } else return <HomePage />;
 };
