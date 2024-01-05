e791d253c2e5e679678f75763030bd6794182abc
Rohan Mathur <mathurrohan04@gmail.com>
2024-01-05 18:03:10 +0600

    rules table revamp (#1304)

--- app/src/components/common/SharingModal/ShareLinkView.tsx
+++ app/src/components/common/SharingModal/ShareLinkView.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
-import { getAllRules, getAppMode, getGroupwiseRulesToPopulate } from "store/selectors";
+import { getAppMode } from "store/selectors";
 import { Radio, Space, Tooltip } from "antd";
 import { RQButton, RQInput } from "lib/design-system/components";
 import { CopyValue } from "components/misc/CopyValue";
@@ -14,8 +14,9 @@ import { httpsCallable, getFunctions } from "firebase/functions";
 import { AiFillCheckCircle } from "@react-icons/all-files/ai/AiFillCheckCircle";
 import { AiOutlineInfoCircle } from "@react-icons/all-files/ai/AiOutlineInfoCircle";
 import { SharedLinkVisibility } from "./types";
-import { Rule } from "types";
 import Logger from "lib/logger";
+import { getAllRuleObjs } from "store/features/rules/selectors";
+import { RuleObj } from "features/rules/types/rules";
 import { trackSharedListCreatedEvent, trackSharedListUrlCopied } from "modules/analytics/events/features/sharedList";
 import "./index.css";
 import EmailInputWithDomainBasedSuggestions from "../EmailInputWithDomainBasedSuggestions";
@@ -23,14 +24,14 @@ import EmailInputWithDomainBasedSuggestions from "../EmailInputWithDomainBasedSu
 interface ShareLinkProps {
   selectedRules: string[];
   source: string;
+  onSharedLinkCreated?: () => void;
 }
 
 // TODO: handle copy changes for session replay in V1
 
-export const ShareLinkView: React.FC<ShareLinkProps> = ({ selectedRules, source }) => {
+export const ShareLinkView: React.FC<ShareLinkProps> = ({ selectedRules, source, onSharedLinkCreated = () => {} }) => {
   const appMode = useSelector(getAppMode);
-  const rules = useSelector(getAllRules);
-  const groupwiseRulesToPopulate = useSelector(getGroupwiseRulesToPopulate);
+  const rules = useSelector(getAllRuleObjs);
   const [sharedLinkVisibility, setSharedLinkVisibility] = useState(SharedLinkVisibility.PUBLIC);
   const [sharedListRecipients, setSharedListRecipients] = useState([]);
   const [sharedListName, setSharedListName] = useState(null);
@@ -42,7 +43,7 @@ export const ShareLinkView: React.FC<ShareLinkProps> = ({ selectedRules, source 
   const sendSharedListShareEmail = useMemo(() => httpsCallable(getFunctions(), "sharedLists-sendShareEmail"), []);
   const singleRuleData = useMemo(
     () =>
-      selectedRules && selectedRules?.length === 1 ? rules.find((rule: Rule) => rule.id === selectedRules[0]) : null,
+      selectedRules && selectedRules?.length === 1 ? rules.find((rule: RuleObj) => rule.id === selectedRules[0]) : null,
     [rules, selectedRules]
   );
 
@@ -132,57 +133,53 @@ export const ShareLinkView: React.FC<ShareLinkProps> = ({ selectedRules, source 
 
     try {
       setIsLinkGenerating(true);
-      createSharedList(
-        appMode,
-        selectedRules,
-        sharedListName,
-        groupwiseRulesToPopulate,
-        sharedLinkVisibility,
-        sharedListRecipients
-      ).then(({ sharedListId, sharedListName, sharedListData, nonRQEmails }: any) => {
-        trackRQLastActivity("sharedList_created");
-        if (sharedLinkVisibility === SharedLinkVisibility.PRIVATE && sharedListRecipients.length) {
-          sendSharedListShareEmail({
-            sharedListData: sharedListData,
-            recipientEmails: sharedListRecipients,
-          })
-            .then((res: any) => {
-              if (res.data.success) setIsMailSent(true);
+      createSharedList(appMode, selectedRules, sharedListName, sharedLinkVisibility, sharedListRecipients).then(
+        ({ sharedListId, sharedListName, sharedListData, nonRQEmails }: any) => {
+          trackRQLastActivity("sharedList_created");
+          onSharedLinkCreated();
+          if (sharedLinkVisibility === SharedLinkVisibility.PRIVATE && sharedListRecipients.length) {
+            sendSharedListShareEmail({
+              sharedListData: sharedListData,
+              recipientEmails: sharedListRecipients,
             })
-            .catch((err) => {
-              Logger.log("send shared list email failed : ", err);
-              toast.error("Opps! Couldn't send the notification");
-            });
+              .then((res: any) => {
+                if (res.data.success) setIsMailSent(true);
+              })
+              .catch((err) => {
+                Logger.log("send shared list email failed : ", err);
+                toast.error("Opps! Couldn't send the notification");
+              });
+          }
+          setShareableLinkData({
+            link: getSharedListURL(sharedListId, sharedListName),
+            visibility: sharedLinkVisibility,
+          });
+
+          const nonRQEmailsCount = sharedLinkVisibility === SharedLinkVisibility.PRIVATE ? nonRQEmails?.length : null;
+          const recipientsCount =
+            sharedLinkVisibility === SharedLinkVisibility.PRIVATE ? sharedListRecipients.length : null;
+
+          trackSharedListCreatedEvent(
+            sharedListId,
+            sharedListName,
+            selectedRules.length,
+            source,
+            sharedLinkVisibility,
+            nonRQEmailsCount,
+            recipientsCount
+          );
+          setIsLinkGenerating(false);
         }
-        setShareableLinkData({
-          link: getSharedListURL(sharedListId, sharedListName),
-          visibility: sharedLinkVisibility,
-        });
-
-        const nonRQEmailsCount = sharedLinkVisibility === SharedLinkVisibility.PRIVATE ? nonRQEmails?.length : null;
-        const recipientsCount =
-          sharedLinkVisibility === SharedLinkVisibility.PRIVATE ? sharedListRecipients.length : null;
-
-        trackSharedListCreatedEvent(
-          sharedListId,
-          sharedListName,
-          selectedRules.length,
-          source,
-          sharedLinkVisibility,
-          nonRQEmailsCount,
-          recipientsCount
-        );
-        setIsLinkGenerating(false);
-      });
+      );
     } catch (e) {
       setIsLinkGenerating(false);
     }
   }, [
     appMode,
     source,
+    onSharedLinkCreated,
     selectedRules,
     sharedListName,
-    groupwiseRulesToPopulate,
     sharedLinkVisibility,
     sharedListRecipients,
     sendSharedListShareEmail,
---
+++ app/src/features/rules/routes/index.tsx
@@ -0,0 +1,93 @@
+import { Navigate, RouteObject } from "react-router-dom";
+import TemplatesIndexPage from "components/landing/ruleTemplates";
+import PATHS from "config/constants/sub/paths";
+import { RulesContainer } from "views/containers/RulesContainer";
+// import RulesIndexView from "views/features/rules/RulesIndexView";
+import SharedListsIndexPage from "components/features/sharedLists/SharedListsIndexPage";
+import TrashIndexPage from "components/features/trash/TrashIndexPage";
+import RuleEditor from "views/features/rules/RuleEditor";
+import SharedListViewerIndexPage from "components/features/sharedLists/SharedListViewerIndexPage";
+import { joinPaths } from "utils/PathUtils";
+import ImportSharedListIndexPage from "components/features/sharedLists/ImportSharedListIndexPage";
+import ProtectedRoute from "components/authentication/ProtectedRoute";
+import RootComponent from "components/redirects/RootComponent";
+import RulesListIndex from "../components/RulesList/components/RulesListIndex/RulesListIndex";
+import SharedListViewer from "../components/SharedList/components/SharedListViewer/SharedListViewer";
+import RulesIndexViewWrapper from "../components/RulesList/components/RulesListIndex/RulesIndexViewWrapper";
+
+export const ruleRoutes: RouteObject[] = [
+  {
+    path: PATHS.RULES.INDEX,
+    element: <RulesContainer />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to={PATHS.RULES.MY_RULES.RELATIVE} />,
+      },
+      {
+        index: true,
+        path: PATHS.RULES.MY_RULES.RELATIVE,
+        element: <RulesIndexViewWrapper />,
+      },
+      {
+        path: joinPaths(PATHS.RULE_EDITOR.RELATIVE, PATHS.ANY),
+        element: <RuleEditor />,
+      },
+      {
+        path: joinPaths(PATHS.RULE_EDITOR.CREATE_RULE.RELATIVE, "/:ruleType"),
+        element: <RuleEditor />,
+      },
+      {
+        path: PATHS.SHARED_LISTS.RELATIVE,
+        element: <SharedListsIndexPage />,
+      },
+      {
+        path: joinPaths(PATHS.SHARED_LISTS.VIEWER.RELATIVE, PATHS.ANY),
+        element: <SharedListViewerIndexPage />,
+      },
+      {
+        path: PATHS.SHARED_LISTS.VIEWER.RELATIVE, // currently broken in prod
+        element: <Navigate to={PATHS.SHARED_LISTS.ABSOLUTE} />,
+      },
+      {
+        path: PATHS.SHARED_LISTS.IMPORT_LIST.RELATIVE, // for desktop app
+        element: <ProtectedRoute component={ImportSharedListIndexPage} />,
+      },
+      {
+        path: PATHS.RULES.TEMPLATES.RELATIVE,
+        element: <TemplatesIndexPage />,
+      },
+      {
+        path: PATHS.RULES.TRASH.RELATIVE,
+        element: <TrashIndexPage />,
+      },
+    ],
+  },
+  {
+    path: `/v2${PATHS.RULES.INDEX}`,
+    element: <RulesContainer />,
+    children: [
+      {
+        path: "my-rules", // FIXME: Move to constants and remove old path
+        element: <RulesListIndex />,
+      },
+      {
+        path: joinPaths(`${PATHS.SHARED_LISTS.VIEWER.RELATIVE}`, ":sharedListParam"),
+        element: <SharedListViewer />,
+      },
+    ],
+  },
+  // redirects
+  {
+    path: PATHS.MARKETPLACE.RELATIVE,
+    element: <Navigate to={PATHS.RULES.MY_RULES.ABSOLUTE} />,
+  },
+  {
+    path: PATHS.ROOT,
+    element: <RootComponent />,
+  },
+  {
+    path: PATHS.INDEX_HTML,
+    element: <RootComponent />,
+  },
+];
