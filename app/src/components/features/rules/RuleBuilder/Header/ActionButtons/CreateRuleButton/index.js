0b55d1ff2fbb3fa5b1fa2cb27b6967bb51c74f84
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-06 22:42:49 +0530

    fix: extension install popup visible when saving rule in desktop app

--- app/src/components/features/rules/RuleBuilder/Header/ActionButtons/CreateRuleButton/index.js
+++ app/src/components/features/rules/RuleBuilder/Header/ActionButtons/CreateRuleButton/index.js
@@ -114,7 +114,7 @@ const CreateRuleButton = ({
   const currentActionText = MODE === APP_CONSTANTS.RULE_EDITOR_CONFIG.MODES.EDIT ? "Save" : "Create";
 
   const handleBtnOnClick = async () => {
-    if (!isExtensionInstalled()) {
+    if (appMode !== GLOBAL_CONSTANTS.APP_MODES.DESKTOP && !isExtensionInstalled()) {
       dispatch(actions.toggleActiveModal({ modalName: "extensionModal", newValue: true }));
       return;
     }
