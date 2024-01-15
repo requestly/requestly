af35864e4aad012eed151286c733863e74474e92
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-15 14:02:36 +0530

    add primary sidebar

--- app/src/features/settings/components/SettingsIndex/index.tsx
+++ app/src/features/settings/components/SettingsIndex/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Col } from "antd";
+import { SettingsPrimarySidebar } from "../SettingsPrimarySidebar";
 import "./index.scss";
 
 const SettingsIndex: React.FC = () => {
   return (
     <div className="settings-index">
-      <Col className="settings-sidebar">SIDEBAR HERE</Col>
+      <SettingsPrimarySidebar />
       <Col className="settings-content-wrapper">
         <Col className="settings-content">CONTENT HERE</Col>
       </Col>
