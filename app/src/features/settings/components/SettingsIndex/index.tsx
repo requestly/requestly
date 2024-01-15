f7036880842b60ea8e6a1d078d7670703a736da7
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-15 15:19:32 +0530

    add global and desktop settings

--- app/src/features/settings/components/SettingsIndex/index.tsx
+++ app/src/features/settings/components/SettingsIndex/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col } from "antd";
 import { SettingsPrimarySidebar } from "../SettingsPrimarySidebar";
+import { Outlet } from "react-router-dom";
 import "./index.scss";
 
 const SettingsIndex: React.FC = () => {
@@ -8,7 +9,9 @@ const SettingsIndex: React.FC = () => {
     <div className="settings-index">
       <SettingsPrimarySidebar />
       <Col className="settings-content-wrapper">
-        <Col className="settings-content">CONTENT HERE</Col>
+        <Col className="settings-content">
+          <Outlet />
+        </Col>
       </Col>
     </div>
   );
--- app/src/features/settings/routes/index.tsx
+++ app/src/features/settings/routes/index.tsx
@@ -1,10 +1,23 @@
 import { Navigate, RouteObject } from "react-router-dom";
 import PATHS from "config/constants/sub/paths";
 import SettingsIndexPage from "../components/SettingsIndex";
+import { GlobalSettings } from "../components/GlobalSettings";
+import { DesktopSettings } from "../components/DesktopSettings";
+
 export const settingRoutes: RouteObject[] = [
   {
     path: PATHS.SETTINGS.RELATIVE,
     element: <SettingsIndexPage />,
+    children: [
+      {
+        path: PATHS.SETTINGS.GLOBAL_SETTINGS.RELATIVE,
+        element: <GlobalSettings />,
+      },
+      {
+        path: PATHS.SETTINGS.DESKTOP_SETTINGS.RELATIVE,
+        element: <DesktopSettings />,
+      },
+    ],
   },
   {
     path: PATHS.SETTINGS.STORAGE_SETTINGS.RELATIVE,
