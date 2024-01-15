d040d1417c41cd3ce50cd08e1c378c9480ec59df
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-16 00:12:03 +0530

    add secondary sidebar base

--- app/src/features/settings/components/SettingsIndex/index.tsx
+++ app/src/features/settings/components/SettingsIndex/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Col } from "antd";
 import { SettingsPrimarySidebar } from "../SettingsPrimarySidebar";
-import { Outlet } from "react-router-dom";
+import { SettingsSecondarySidebar } from "../SettingsSecondarySidebar";
+import { Outlet, useLocation } from "react-router-dom";
 import "./index.scss";
 
 const SettingsIndex: React.FC = () => {
+  // TODO: FIX THIS
+  const location = useLocation();
+  const isSecondarySidebarVisible = location.pathname.includes("billing");
+
   return (
     <div className="settings-index">
-      <SettingsPrimarySidebar />
+      <div className="settings-index-sidebar-wrapper">
+        <SettingsPrimarySidebar />
+        {isSecondarySidebarVisible && <SettingsSecondarySidebar>Secondary sidebar</SettingsSecondarySidebar>}
+      </div>
       <Col className="settings-content-wrapper">
         <Col className="settings-content">
           <Outlet />
