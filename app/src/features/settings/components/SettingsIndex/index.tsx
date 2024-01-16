4919be79c65274f27b68a8976ef1c6017b17749d
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-16 19:47:55 +0530

    chore:fix

--- app/src/features/settings/components/SettingsIndex/index.tsx
+++ app/src/features/settings/components/SettingsIndex/index.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col } from "antd";
 import { SettingsPrimarySidebar } from "../SettingsPrimarySidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { redirectToGlobalSettings } from "utils/RedirectionUtils";
+import APP_CONSTANTS from "config/constants";
 import "./index.scss";
 
 const SettingsIndex: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === APP_CONSTANTS.PATHS.SETTINGS.RELATIVE) {
+      redirectToGlobalSettings(navigate);
+    }
+  }, [location.pathname, navigate]);
+
   return (
     <div className="settings-index">
       <SettingsPrimarySidebar />
