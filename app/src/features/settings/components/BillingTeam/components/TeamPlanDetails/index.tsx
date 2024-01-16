4d28e021dea16e3820ade9a5253ce7df4e8bbf13
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-16 14:28:47 +0530

    add plan details popover

--- app/src/features/settings/components/BillingTeam/components/TeamPlanDetails/index.tsx
+++ app/src/features/settings/components/BillingTeam/components/TeamPlanDetails/index.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { Col, Row } from "antd";
+import React, { useState } from "react";
+import { Col, Popover, Row } from "antd";
 import { RQButton } from "lib/design-system/components";
 import { MdOutlineCancel } from "@react-icons/all-files/md/MdOutlineCancel";
 import { MdOutlinePreview } from "@react-icons/all-files/md/MdOutlinePreview";
 import UpgradeIcon from "../../assets/upgrade.svg";
 import { TeamPlanStatus } from "../TeamPlanStatus";
+import { TeamPlanDetailsPopover } from "./components/TeamPlanDetailsPopover";
 import "./index.scss";
 
 export const TeamPlanDetails: React.FC = () => {
+  const [isPlanDetailsPopoverVisible, setIsPlanDetailsPopoverVisible] = useState(false);
   return (
     <Col className="team-plan-details-card">
       <Row className="team-plan-details-card-header" justify="space-between" align="middle">
@@ -28,10 +30,27 @@ export const TeamPlanDetails: React.FC = () => {
           <Row align="middle" gutter={8}>
             <Col className="team-plan-details-section-plan-name">Professional team plan</Col>
             <Col>
-              {/* TODO: ADD POPOVER and show plan details in it based on team plan */}
-              <RQButton size="small" type="text" icon={<MdOutlinePreview />} className="team-plan-details-btn">
-                Plan details
-              </RQButton>
+              <Popover
+                content={<TeamPlanDetailsPopover />}
+                title={null}
+                trigger="click"
+                placement="bottom"
+                overlayClassName="team-plan-details-popover"
+                showArrow={false}
+                open={isPlanDetailsPopoverVisible}
+                color="var(--surface-1)"
+                onOpenChange={(open) => setIsPlanDetailsPopoverVisible(open)}
+              >
+                <RQButton
+                  size="small"
+                  type="text"
+                  icon={<MdOutlinePreview />}
+                  className="team-plan-details-btn"
+                  onClick={() => setIsPlanDetailsPopoverVisible(true)}
+                >
+                  Plan details
+                </RQButton>
+              </Popover>
             </Col>
           </Row>
           <Col
