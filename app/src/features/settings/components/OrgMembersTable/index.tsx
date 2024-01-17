329b1319af8830df939be4d6198bb0658283832a
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-17 12:48:23 +0530

    add org members table

--- app/src/features/settings/components/OrgMembersTable/index.tsx
+++ app/src/features/settings/components/OrgMembersTable/index.tsx
@@ -1,18 +1,29 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { ReactNode, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { getUserAuthDetails } from "store/selectors";
-import { Col, Input, Table } from "antd";
+import { Col, Empty, Input, Row, Table, TableProps } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import { getDomainFromEmail, isCompanyEmail } from "utils/FormattingHelper";
 import { isEmailVerified } from "utils/AuthUtils";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import "./index.scss";
 
-export const OrgMembersTable: React.FC = () => {
+interface OrgMembersTableProps {
+  actionButtons: (record: any) => ReactNode;
+}
+
+export const OrgMembersTable: React.FC<OrgMembersTableProps> = ({ actionButtons }) => {
   const user = useSelector(getUserAuthDetails);
-  const [organizationMembers, setOrganizationMembers] = useState([]);
+  const [organizationMembers, setOrganizationMembers] = useState<{ total: number; users: unknown[] }>(null);
   const [search, setSearch] = useState("");
   const getOrganizationUsers = useMemo(() => httpsCallable(getFunctions(), "users-getOrganizationUsers"), []);
+
+  const searchedMembers = useMemo(() => {
+    if (!organizationMembers?.users) return [];
+    return organizationMembers?.users?.filter((member: any) => {
+      return member?.email?.includes(search);
+    });
+  }, [organizationMembers?.users, search]);
 
   useEffect(() => {
     isEmailVerified(user?.details?.profile?.uid).then((result) => {
@@ -26,7 +37,48 @@ export const OrgMembersTable: React.FC = () => {
     });
   }, [getOrganizationUsers, user?.details?.profile?.email, user?.details?.profile?.uid]);
 
-  console.log(organizationMembers);
+  const columns: TableProps<any>["columns"] = useMemo(
+    () => [
+      {
+        title: "Member",
+        key: "member",
+        width: 350,
+        render: (_: any, record: any) => {
+          console.log(record?.photoURL, record?.email);
+          return (
+            <Row align="middle" gutter={8}>
+              <Col>
+                <img
+                  className="org-member-avatar"
+                  src={
+                    record?.photoURL.length
+                      ? record?.photoURL
+                      : "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+                  }
+                  alt={record?.email}
+                />
+              </Col>
+              <Col className="org-member-email">{record?.email}</Col>
+            </Row>
+          );
+        },
+        defaultSortOrder: "ascend",
+        showSorterTooltip: false,
+        sorter: {
+          compare: (a: any, b: any) => a.email.localeCompare(b.email),
+        },
+      },
+      {
+        title: "",
+        key: "action",
+        render: (_: any, record: any) => {
+          return actionButtons(record);
+        },
+      },
+    ],
+
+    [actionButtons]
+  );
 
   return (
     <Col className="org-member-table">
@@ -39,7 +91,17 @@ export const OrgMembersTable: React.FC = () => {
           suffix={<SearchOutlined />}
         />
       </Col>
-      <Table className="billing-table" dataSource={organizationMembers} />
+      <Table
+        className="billing-table"
+        dataSource={searchedMembers}
+        columns={columns}
+        pagination={false}
+        scroll={{ y: "70vh" }}
+        loading={!organizationMembers?.users}
+        locale={{
+          emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No member found" />,
+        }}
+      />
     </Col>
   );
 };
