70e6f475aaa27893c0e4e0a53dded6858c7a9a45
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-18 06:32:04 +0530

    org table fixes

--- app/src/features/settings/components/BillingTeam/components/MyBillingTeam/components/BillingTeamMembers/components/MemberTableActions.tsx
+++ app/src/features/settings/components/BillingTeam/components/MyBillingTeam/components/BillingTeamMembers/components/MemberTableActions.tsx
@@ -1,53 +1,73 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { Col, Row } from "antd";
+import { getUserAuthDetails } from "store/selectors";
 import { RQButton } from "lib/design-system/components";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { TbChecks } from "@react-icons/all-files/tb/TbChecks";
 import { IoMdAdd } from "@react-icons/all-files/io/IoMdAdd";
+import { getBillingTeamMembers } from "store/features/billing/selectors";
+import { BillingTeamRoles } from "features/settings/components/BillingTeam/types";
 import Logger from "lib/logger";
 
-export const MemberTableActions: React.FC<{ record: any; billingTeamId?: string }> = ({ record, billingTeamId }) => {
-  const [isAddingMember, setIsAddingMember] = useState(false);
-  const [isMemberAdded, setIsMemberAdded] = useState(false);
+export const MemberTableActions: React.FC<{ record: any }> = ({ record }) => {
+  const { billingId } = useParams();
+  const billingTeamMembers = useSelector(getBillingTeamMembers(billingId));
+  const user = useSelector(getUserAuthDetails);
+  const isUserAlreadyAdded = useMemo(
+    () => Object.values(billingTeamMembers).some((member) => member.email === record.email),
+    [billingTeamMembers, record.email]
+  );
+
+  const [isAddingUser, setIsAddingUser] = useState(false);
+  const [isUserAdded, setIsUserAdded] = useState(false);
+
+  const isUserAdmin =
+    billingTeamMembers?.[user?.details?.profile?.uid] &&
+    billingTeamMembers?.[user?.details?.profile?.uid]?.role !== BillingTeamRoles.Member;
 
   const handleAddUserToBillingTeam = useCallback(() => {
     const addUserToBillingteam = httpsCallable(getFunctions(), "billing-addUsers");
-    setIsAddingMember(true);
-    addUserToBillingteam({ userEmails: [record.email], billingTeamId })
+    setIsAddingUser(true);
+    addUserToBillingteam({ userEmails: [record.email], billingTeamId: billingId })
       .then(() => {
-        setIsMemberAdded(true);
+        setIsUserAdded(true);
       })
       .catch((e) => {
         Logger.log(e);
       })
-      .finally(() => setIsAddingMember(false));
-  }, [billingTeamId, record.email]);
+      .finally(() => setIsAddingUser(false));
+  }, [billingId, record.email]);
 
   return (
     <>
-      {/* TODO: ALSO check if user is already present in the team */}
-      {isMemberAdded ? (
-        <Row gutter={8} align="middle" className="billing-team-members-added-label">
+      {isUserAdded || isUserAlreadyAdded ? (
+        <Row gutter={8} align="middle" justify="end" className="billing-team-members-added-label">
           <Col>
             <TbChecks fontSize={14} />
           </Col>
-          <Col>Already added</Col>
+          <Col>Added</Col>
         </Row>
       ) : (
-        <Row justify="end">
-          <RQButton
-            type="default"
-            icon={<IoMdAdd />}
-            className="billing-team-members-add-btn"
-            loading={isAddingMember}
-            onClick={(e) => {
-              e.stopPropagation();
-              handleAddUserToBillingTeam();
-            }}
-          >
-            Add
-          </RQButton>
-        </Row>
+        <>
+          {isUserAdmin && (
+            <Row justify="end">
+              <RQButton
+                type="default"
+                icon={<IoMdAdd />}
+                className="billing-team-members-add-btn"
+                loading={isAddingUser}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleAddUserToBillingTeam();
+                }}
+              >
+                Add
+              </RQButton>
+            </Row>
+          )}
+        </>
       )}
     </>
   );
