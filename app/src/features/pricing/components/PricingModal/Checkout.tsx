26878f571e20e34de6040a4143da1247494bd724
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-22 19:44:18 +0530

    redirect to created billing team post checkout

--- app/src/features/pricing/components/PricingModal/Checkout.tsx
+++ app/src/features/pricing/components/PricingModal/Checkout.tsx
@@ -8,8 +8,12 @@ import { LoadingOutlined } from "@ant-design/icons";
 import { MdArrowBack } from "@react-icons/all-files/md/MdArrowBack";
 import { trackPricingModalStripeWindowOpened } from "features/pricing/analytics";
 import { redirectToAccountDetails } from "utils/RedirectionUtils";
-import "./index.scss";
 import { trackCheckoutCompletedEvent } from "modules/analytics/events/misc/business/checkout";
+import { useSelector } from "react-redux";
+import { getUserAuthDetails } from "store/selectors";
+import { getBillingTeamsById } from "backend/billing/getBillingTeamsById";
+import APP_CONSTANTS from "config/constants";
+import "./index.scss";
 
 interface CheckoutProps {
   clientSecret: string;
@@ -32,10 +36,32 @@ export const Checkout: React.FC<CheckoutProps> = ({
   onCheckoutCompleted,
 }) => {
   const navigate = useNavigate();
+  const user = useSelector(getUserAuthDetails);
+
+  const redirectTolatestBillingTeam = async () => {
+    getBillingTeamsById(user?.details?.profile?.uid).then((billingTeams) => {
+      if (billingTeams.length === 0) return;
+
+      if (billingTeams.length === 1) {
+        navigate(APP_CONSTANTS.PATHS.SETTINGS.BILLING.RELATIVE + "/" + billingTeams[0].id);
+        toggleModal();
+      } else {
+        const sortedTeams = billingTeams.sort(
+          (a: any, b: any) =>
+            b.data.subscriptionDetails.subscriptionCreated - a.data.subscriptionDetails.subscriptionCreated
+        );
+        navigate(APP_CONSTANTS.PATHS.SETTINGS.BILLING.RELATIVE + "/" + sortedTeams[0].id);
+        toggleModal();
+      }
+    });
+  };
+
   const options = {
     clientSecret,
     onComplete: () => {
+      console.log("onComplete called");
       trackCheckoutCompletedEvent(source);
+      redirectTolatestBillingTeam();
       onCheckoutCompleted?.();
     },
   };
