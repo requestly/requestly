ffde285e5c7e83f16da9d2aa9f527d2bab802b5e
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-23 19:01:40 +0530

    refactor members table

--- app/src/features/settings/components/BillingTeam/components/OtherBillingTeam/index.tsx
+++ app/src/features/settings/components/BillingTeam/components/OtherBillingTeam/index.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Col, Popover, Row, Table } from "antd";
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getBillingTeamById, getBillingTeamMembers } from "store/features/billing/selectors";
@@ -19,57 +19,60 @@ export const OtherBillingTeam: React.FC = () => {
   const membersTableSource = billingTeamMembers ? Object.values(billingTeamMembers) : [];
   const [isPlanDetailsPopoverOpen, setIsPlanDetailsPopoverOpen] = React.useState(false);
 
-  const columns = [
-    {
-      title: `Member (${membersTableSource.length})`,
-      key: "id",
-      width: "80%",
-      render: (_: any, record: Record<string, any>) => (
-        <Row>
-          <div className="billing-team-member-avatar-wrapper">
-            <Avatar size={34} shape="circle" src={record.photoUrl} alt={record.displayName} />
-          </div>
-          <div>
-            <Row align={"middle"} gutter={4}>
-              <Col>
-                <span className="text-bold text-white">{`${record.displayName ?? "User"}`}</span>
-              </Col>
-              <Col>
-                {record.role === BillingTeamRoles.Manager ? (
-                  <Row className="icon__wrapper success" align="middle">
-                    <MdOutlinePaid style={{ marginRight: "2px" }} />
-                    <span className="caption">Billing manager</span>
-                  </Row>
-                ) : record.role === BillingTeamRoles.Admin ? (
-                  <Row className="icon__wrapper warning" align="middle">
-                    <MdOutlineAdminPanelSettings style={{ marginRight: "2px" }} />
-                    <span className="caption">Admin</span>
-                  </Row>
-                ) : null}
-              </Col>
-            </Row>
-            <div>
-              <span className="billing-team-member-email">{record.email}</span>
+  const columns = useMemo(
+    () => [
+      {
+        title: `Member (${membersTableSource.length})`,
+        key: "id",
+        width: "80%",
+        render: (_: any, record: Record<string, any>) => (
+          <Row>
+            <div className="billing-team-member-avatar-wrapper">
+              <Avatar size={34} shape="circle" src={record.photoUrl} alt={record.displayName} />
             </div>
-          </div>
-        </Row>
-      ),
-    },
-    {
-      title: "Added on",
-      dataIndex: "joiningDate",
-      render: (joiningDate: number) => (
-        <div className="text-white">{joiningDate ? getLongFormatDateString(new Date(joiningDate)) : "-"}</div>
-      ),
-    },
-  ];
+            <div>
+              <Row align={"middle"} gutter={4}>
+                <Col>
+                  <span className="text-bold text-white">{`${record.displayName ?? "User"}`}</span>
+                </Col>
+                <Col>
+                  {record.role === BillingTeamRoles.Manager ? (
+                    <Row className="icon__wrapper success" align="middle">
+                      <MdOutlinePaid style={{ marginRight: "2px" }} />
+                      <span className="caption">Billing manager</span>
+                    </Row>
+                  ) : record.role === BillingTeamRoles.Admin ? (
+                    <Row className="icon__wrapper warning" align="middle">
+                      <MdOutlineAdminPanelSettings style={{ marginRight: "2px" }} />
+                      <span className="caption">Admin</span>
+                    </Row>
+                  ) : null}
+                </Col>
+              </Row>
+              <div>
+                <span className="billing-team-member-email">{record.email}</span>
+              </div>
+            </div>
+          </Row>
+        ),
+      },
+      {
+        title: "Added on",
+        dataIndex: "joiningDate",
+        render: (joiningDate: number) => (
+          <div className="text-white">{joiningDate ? getLongFormatDateString(new Date(joiningDate)) : "-"}</div>
+        ),
+      },
+    ],
+    [membersTableSource.length]
+  );
 
   return (
     <>
       <Col className="my-billing-team-title">{billingTeamDetails.name}</Col>
       <Col className="billing-teams-primary-card mt-8">
         <Row className="items-center other-team-members-table-header" align="middle" gutter={8}>
-          <Col style={{ padding: 0 }}>
+          <Col>
             <TeamPlanStatus subscriptionStatus={billingTeamDetails.subscriptionDetails.subscriptionStatus} />
           </Col>
           <Col>
