741e7cb2febe0de5efcfe817d9896f5f3ef34376
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-25 16:19:06 +0530

    update plan action CTAs based on plan

--- app/src/features/settings/components/BillingTeam/components/MyBillingTeam/components/CancelPlanModal/index.tsx
+++ app/src/features/settings/components/BillingTeam/components/MyBillingTeam/components/CancelPlanModal/index.tsx
@@ -1,15 +1,40 @@
-import React, { useState } from "react";
-import { Col, Input, Modal, Row } from "antd";
+import React, { useCallback, useState } from "react";
+import { Col, Modal, Row } from "antd";
 import { RQButton } from "lib/design-system/components";
+import { getFunctions, httpsCallable } from "firebase/functions";
+import Logger from "lib/logger";
+import { toast } from "utils/Toast";
+import { getPrettyPlanName } from "utils/FormattingHelper";
+import { getLongFormatDateString } from "utils/DateTimeUtils";
 import "./index.scss";
 
 interface Props {
   isOpen: boolean;
+  subscriptionDetails: any;
   closeModal: () => void;
 }
 
-export const CancelPlanModal: React.FC<Props> = ({ isOpen, closeModal }) => {
-  const [reason, setReason] = useState("");
+export const CancelPlanModal: React.FC<Props> = ({ isOpen, closeModal, subscriptionDetails }) => {
+  // const [reason, setReason] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleRequestCancellation = useCallback(() => {
+    setIsLoading(true);
+    const requestPlanCancellation = httpsCallable(getFunctions(), "premiumNotifications-requestPlanCancellation");
+    requestPlanCancellation({
+      currentPlan: getPrettyPlanName(subscriptionDetails?.plan),
+    })
+      .then(() => {
+        toast.success("Cancellation request sent successfully, we will get back to you soon.");
+      })
+      .catch((e) => {
+        Logger.log(e);
+      })
+      .finally(() => {
+        setIsLoading(false);
+        closeModal();
+      });
+  }, [closeModal, subscriptionDetails?.plan]);
 
   return (
     <Modal
@@ -23,17 +48,19 @@ export const CancelPlanModal: React.FC<Props> = ({ isOpen, closeModal }) => {
           <RQButton type="default" onClick={closeModal}>
             Close
           </RQButton>
-          <RQButton type="primary" danger>
+          <RQButton type="primary" loading={isLoading} danger onClick={handleRequestCancellation}>
             Send cancellation request
           </RQButton>
         </Row>
       }
     >
       <Col className="cancel-plan-modal-description">
-        Once cancelled, your professional plan stays active until Wednesday, Dec 23, 2023. After that, all premium
-        features won't be accessible to you. We highly recommend you to export your rules/data before cancelling.
+        Once cancelled, your <span className="text-white">{getPrettyPlanName(subscriptionDetails?.plan)} plan</span>{" "}
+        stays active until {getLongFormatDateString(new Date(subscriptionDetails.subscriptionCurrentPeriodEnd * 1000))}.
+        After that, all premium features won't be accessible to you. We highly recommend you to export your rules/data
+        before cancelling.
       </Col>
-      <Col className="mt-16">
+      {/* <Col className="mt-16">
         <label className="text-bold text-white">Please let us know the reason for plan cancellation (Optional)</label>
         <Input.TextArea
           value={reason}
@@ -41,7 +68,7 @@ export const CancelPlanModal: React.FC<Props> = ({ isOpen, closeModal }) => {
           className="cancel-plan-modal-textarea"
           style={{ height: 80, resize: "none" }}
         />
-      </Col>
+      </Col> */}
     </Modal>
   );
 };
