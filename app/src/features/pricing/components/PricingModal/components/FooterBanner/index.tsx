b544aef7438cc0cf1f18b81a17932fba99b5f456
RuntimeTerror10 <parthb.git@gmail.com>
2024-01-31 11:20:19 +0530

    add varaint 2

---
+++ app/src/features/pricing/components/PricingModal/components/FooterBanner/index.tsx
@@ -0,0 +1,28 @@
+import { useFeatureValue } from "@growthbook/growthbook-react";
+import { RQButton } from "lib/design-system/components";
+import { redirectToUrl } from "utils/RedirectionUtils";
+import "./index.scss";
+
+export const PricingModalFooterBanner = () => {
+  const monetizationExp = useFeatureValue("monetization_exp_1", null);
+
+  console.log("monetizationExp", monetizationExp);
+  return (
+    <div className="pricing-modal-footer-banner">
+      {monetizationExp === "variant1" ? (
+        <>VARIANT 1 content here</>
+      ) : (
+        <div className="pricing-modal-footer-banner-content">
+          <span className="pricing-modal-footer-banner-text">Need help finding the right plan for your team?</span>
+          <RQButton
+            type="default"
+            className="pricing-modal-footer-banner-btn"
+            onClick={() => redirectToUrl("https://calendly.com/requestly/sagar", true)}
+          >
+            Contact Us
+          </RQButton>
+        </div>
+      )}
+    </div>
+  );
+};
