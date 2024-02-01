d85e2fed29cf34da6a775ff7d90009616610c43c
RuntimeTerror10 <parthb.git@gmail.com>
2024-02-01 11:35:36 +0530

    remove request trial access footer from pricing modal

--- app/src/features/pricing/components/PricingModal/components/FooterBanner/index.tsx
+++ app/src/features/pricing/components/PricingModal/components/FooterBanner/index.tsx
@@ -9,7 +9,17 @@ export const PricingModalFooterBanner = () => {
   console.log("monetizationExp", paywallIntensityExp);
   return (
     <div className="pricing-modal-footer-banner">
-      {paywallIntensityExp === "variantA" ? (
+      <div className="pricing-modal-footer-banner-content">
+        <span className="pricing-modal-footer-banner-text">Need help finding the right plan for your team?</span>
+        <RQButton
+          type="default"
+          className="pricing-modal-footer-banner-btn"
+          onClick={() => redirectToUrl("https://calendly.com/requestly/sagar", true)}
+        >
+          Contact Us
+        </RQButton>
+      </div>
+      {/* {paywallIntensityExp === "variantA" ? (
         <div className="pricing-modal-footer-banner-content">
           <span className="pricing-modal-footer-banner-text">Request access for a 30 day free trial</span>
           <RQButton
@@ -20,18 +30,8 @@ export const PricingModalFooterBanner = () => {
             Request now
           </RQButton>
         </div>
-      ) : (
-        <div className="pricing-modal-footer-banner-content">
-          <span className="pricing-modal-footer-banner-text">Need help finding the right plan for your team?</span>
-          <RQButton
-            type="default"
-            className="pricing-modal-footer-banner-btn"
-            onClick={() => redirectToUrl("https://calendly.com/requestly/sagar", true)}
-          >
-            Contact Us
-          </RQButton>
-        </div>
-      )}
+      ) : ( */}
+      {/* )} */}
     </div>
   );
 };
