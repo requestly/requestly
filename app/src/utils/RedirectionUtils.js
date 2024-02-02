b76fd014ba79df1b28e1830edca8f8d8b970d5bf
Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>
2024-02-02 16:18:29 +0530

    fix: desktop sessions redirects (#1383)

--- app/src/utils/RedirectionUtils.js
+++ app/src/utils/RedirectionUtils.js
@@ -116,6 +116,16 @@ export const redirectToSessionSettings = (navigate, redirectUrl, source) => {
   navigate(PATHS.SETTINGS.SESSION_BOOK.ABSOLUTE, { state: { redirectUrl, source } });
 };
 
+export const redirectToNetworkSessionHome = (navigate, isDesktopSessionsCompatible = false) => {
+  if (isDesktopSessionsCompatible) {
+    navigate(PATHS.SESSIONS.DESKTOP.SAVED_LOGS.ABSOLUTE);
+    return;
+  } else {
+    navigate(PATHS.SESSIONS.ABSOLUTE);
+    return;
+  }
+};
+
 export const redirectToNetworkSession = (navigate, id, isDesktopSessionsCompatible = false) => {
   if (isDesktopSessionsCompatible) {
     if (id) {
@@ -123,7 +133,7 @@ export const redirectToNetworkSession = (navigate, id, isDesktopSessionsCompatib
       navigate(path);
       return;
     }
-    const path = PATHS.SESSIONS.DESKTOP.SAVED_LOGS.ABSOLUTE;
+    const path = PATHS.SESSIONS.DESKTOP.NETWORK.ABSOLUTE;
     navigate(path);
     return;
   } else {
