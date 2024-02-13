28fefdd1ea57e93b48977f8c52ae1172c46440f1
Nafees Nehar <nafees87n@gmail.com>
2024-02-13 20:36:48 +0530

    chore: remove unused payment components

--- app/src/routes/fullScreenRoutes.tsx
+++ app/src/routes/fullScreenRoutes.tsx
@@ -1,14 +1,9 @@
 import PATHS from "config/constants/sub/paths";
 import { Navigate, RouteObject } from "react-router-dom";
-import Checkout from "views/misc/payments/Checkout";
 import PaymentFail from "views/misc/payments/paymentFail";
 import PaymentSuccess from "views/misc/payments/paymentSuccess";
 
 export const fullScreenRoutes: RouteObject[] = [
-  {
-    path: PATHS.CHECKOUT.ABSOLUTE,
-    element: <Checkout />,
-  },
   {
     path: PATHS.PAYMENT_SUCCESS.ABSOLUTE,
     element: <PaymentSuccess />,
