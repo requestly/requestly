db831c2f4b3efce529fc2d2e7fa2363cc427e9ef
RuntimeTerror10 <parthb.git@gmail.com>
2024-02-19 21:13:28 +0530

    refactor app banner

--- app/src/layouts/DashboardLayout/AppNotificationBanner/index.tsx
+++ app/src/layouts/DashboardLayout/AppNotificationBanner/index.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import { actions } from "store";
 import { RQButton } from "lib/design-system/components";
 import { useDispatch } from "react-redux";
 import { isAppOpenedInIframe } from "utils/AppUtils";
-import "./appNotificationBanner.scss";
 import { useFeatureValue } from "@growthbook/growthbook-react";
 import { useSelector } from "react-redux";
-import { getAppNotificationBannerDismissTs } from "store/selectors";
+import { getAppNotificationBannerDismissTs, getUserAuthDetails } from "store/selectors";
+import { OrgNotificationBanner } from "./OrgNotificationBanner";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
-import { OrgNotificationBanner } from "./OrgNotificationBanner";
 import { trackAppNotificationBannerViewed } from "modules/analytics/events/common/onboarding/appBanner";
+import { BANNER_ACTIONS } from "./types";
+import { ButtonType } from "antd/lib/button";
+import { capitalize } from "lodash";
+import { redirectToUrl } from "utils/RedirectionUtils";
+import { getDomainFromEmail } from "utils/FormattingHelper";
+import "./appNotificationBanner.scss";
 
 interface Banner {
   id: string;
@@ -24,9 +29,43 @@ interface Banner {
 
 export const AppNotificationBanner = () => {
   const dispatch = useDispatch();
+  const user = useSelector(getUserAuthDetails);
   const lastAppBannerDismissTs = useSelector(getAppNotificationBannerDismissTs);
   const banners = useFeatureValue("app_banner", []);
   const newBanners = banners.filter((banner: Banner) => banner.createdTs > (lastAppBannerDismissTs || 0));
+
+  const bannerActionButtons = useMemo(() => {
+    return {
+      [BANNER_ACTIONS.UPGRADE]: {
+        label: "upgrade",
+        type: "primary",
+        onClick: () => {
+          dispatch(actions.toggleActiveModal({ modalName: "pricingModal", newValue: true }));
+        },
+      },
+      [BANNER_ACTIONS.CONTACT_US]: {
+        label: "contact us",
+        type: "default",
+        onClick: () => {
+          redirectToUrl("https://calendly.com/requestly/sagar", true);
+        },
+      },
+    };
+  }, [dispatch]);
+
+  const renderBannerText = useCallback(
+    (bannerId: string, text: string) => {
+      switch (bannerId) {
+        case "commercial_license": {
+          const domain = getDomainFromEmail(user?.details?.profile?.email || "")?.split(".")[0];
+          return `Dear ${capitalize(domain)} user, ${text}`;
+        }
+        default:
+          return text;
+      }
+    },
+    [user?.details?.profile?.email]
+  );
 
   const handleCloseBannerClick = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -41,20 +80,45 @@ export const AppNotificationBanner = () => {
   }, [newBanners?.length]);
 
   const renderAppBanner = () => {
-    const banner: Banner = newBanners ? newBanners[0] : null;
+    const banner = {
+      id: "commercial_license",
+      short_text: "Alert!",
+      text:
+        "commercial usage (using Requestly for work) is not permitted in the free plan. Please upgrade to continue using Requestly for work.",
+      createdTs: 1708349983421,
+      backgroundColor: "#003cca",
+      badgeColor: "#004eeb",
+      isDismissable: true,
+      actions: ["upgrade", "contact_us"],
+    };
 
     if (banner) {
       return (
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="app-banner"
-          href={banner?.cta ? banner.cta : null}
-          style={{ backgroundColor: banner?.backgroundColor || "000000" }}
-        >
+        <div className="app-banner" style={{ backgroundColor: banner?.backgroundColor || "#000000" }}>
+          {banner?.short_text && (
+            <span className="app-banner-badge" style={{ backgroundColor: banner?.badgeColor || "#000000" }}>
+              {banner.short_text}
+            </span>
+          )}
           <div className="app-banner-text">
-            {/* @ts-expect-error */}
-            <ReactMarkdown children={banner?.text} skipHtml={false} rehypePlugins={[rehypeRaw]} />
+            <ReactMarkdown
+              children={renderBannerText(banner.id, banner.text)}
+              skipHtml={false}
+              // @ts-ignore
+              rehypePlugins={[rehypeRaw]}
+            />
+          </div>
+          <div className="app-banner-action-buttons">
+            {banner.actions?.map((action: BANNER_ACTIONS) => {
+              return (
+                <RQButton
+                  type={bannerActionButtons[action].type as ButtonType}
+                  onClick={bannerActionButtons[action].onClick}
+                >
+                  {capitalize(bannerActionButtons[action].label)}
+                </RQButton>
+              );
+            })}
           </div>
           {banner?.isDismissable === false ? null : (
             <div className="close-button-container">
@@ -73,7 +137,7 @@ export const AppNotificationBanner = () => {
               />
             </div>
           )}
-        </a>
+        </div>
       );
     }
 
