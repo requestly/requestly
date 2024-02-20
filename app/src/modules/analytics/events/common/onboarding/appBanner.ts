9a941e9c3ba3669dbed0974dfcce02ae9c285fca
RuntimeTerror10 <parthb.git@gmail.com>
2024-02-20 14:31:04 +0530

    add analytics and visibility checks

--- app/src/modules/analytics/events/common/onboarding/appBanner.ts
+++
@@ -1,6 +0,0 @@
-import { trackEvent } from "modules/analytics";
-import { ONBOARDING } from "../constants";
-
-export const trackAppNotificationBannerViewed = () => {
-  trackEvent(ONBOARDING.APP_NOTIFICATION_BANNER_VIEWED);
-};
