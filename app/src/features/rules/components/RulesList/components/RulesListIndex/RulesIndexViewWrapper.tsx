268938cc7d1c6b3e2fbba79cd2e8f0ff4a64c404
RuntimeTerror10 <parthb.git@gmail.com>
2024-02-26 16:55:20 +0530

    fix: rules table filtering and status click event propagation

--- app/src/features/rules/components/RulesList/components/RulesListIndex/RulesIndexViewWrapper.tsx
+++ app/src/features/rules/components/RulesList/components/RulesListIndex/RulesIndexViewWrapper.tsx
@@ -1,9 +1,11 @@
-import { useFeatureIsOn } from "@growthbook/growthbook-react";
+// import { useFeatureIsOn } from "@growthbook/growthbook-react";
 import RulesListIndex from "./RulesListIndex";
 import RulesIndexView from "views/features/rules/RulesIndexView";
 
 const RulesIndexViewWrapper = () => {
-  const shouldShowNewRulesList = useFeatureIsOn("new-rule-table");
+  // const shouldShowNewRulesList = useFeatureIsOn("new-rule-table");
+  const shouldShowNewRulesList = true;
+
   return shouldShowNewRulesList ? <RulesListIndex /> : <RulesIndexView />;
 };
 
