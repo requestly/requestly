1aef4f2dedd2dab77f0aaa8b9855851bc989cede
Nafees Nehar <nafees87n@gmail.com>
2024-02-29 11:28:18 +0530

    fix: asset imports

--- app/src/components/misc/Marquee/index.jsx
+++ app/src/components/misc/Marquee/index.jsx
@@ -1,41 +1,41 @@
+import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
-import adobe from "assets/img/icons/common/adobe.svg";
-import indeed from "assets/img/icons/common/indeed.svg";
-import amazon from "assets/img/icons/common/amazon.svg";
-import atlassian from "assets/img/icons/common/atlassian.svg";
-import wix from "assets/img/icons/common/wix.svg";
-import atnt from "assets/img/icons/common/atnt.svg";
-import sendx from "assets/img/icons/common/sendx.svg";
-import hubspot from "assets/img/icons/common/hubspot.svg";
-import pushowl from "assets/img/icons/common/pushowl.svg";
-import verizon from "assets/img/icons/common/verizon.svg";
-import cbs from "assets/img/icons/common/cbs.svg";
-import salesforce from "assets/img/icons/common/salesforce.svg";
-import pearson from "assets/img/icons/common/pearson.svg";
 import "./marquee.css";
 
+const companies = [
+  "adobe",
+  "indeed",
+  "amazon",
+  "atlassian",
+  "wix",
+  "atnt",
+  "sendx",
+  "hubspot",
+  "pushowl",
+  "verizon",
+  "cbs",
+  "salesforce",
+  "pearson",
+];
+
 export const CompanyMarquee = () => {
-  const companies = {
-    adobe: adobe,
-    indeed: indeed,
-    amazon: amazon,
-    atlassian: atlassian,
-    wix: wix,
-    atnt: atnt,
-    sendx: sendx,
-    hubspot: hubspot,
-    pushowl: pushowl,
-    verizon: verizon,
-    cbs: cbs,
-    salesforce: salesforce,
-    pearson: pearson,
-  };
+  const [icons, setIcons] = useState([]);
+
+  useEffect(() => {
+    Promise.all(
+      companies.map((company) =>
+        import(`../../../assets/img/icons/common/${company}.svg`).then((module) => module.default)
+      )
+    )
+      .then(setIcons)
+      .catch(() => setIcons([]));
+  }, []);
 
   return (
     <div className="marquee-container">
       <Marquee gradientWidth={200} gradientColor={["33", "33", "37"]} speed={20}>
-        {Object.entries(companies).map(([company, image], index) => (
-          <img key={index} src={image} alt={company} className="marquee-icons" />
+        {icons.map((companyIcon, index) => (
+          <img key={index} src={companyIcon} alt={companies[index]} className="marquee-icons" />
         ))}
       </Marquee>
     </div>
