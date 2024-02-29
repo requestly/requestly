a56efa3670fc4bba0a2bf9eb174c536cdc500b23
Kanishk Rawat <rawat27kanishk@gmail.com>
2024-02-29 12:48:08 +0530

    Updated Formbricks

--- app/src/hooks/AuthHandler.js
+++ app/src/hooks/AuthHandler.js
@@ -145,9 +145,6 @@ const AuthHandler = (onComplete) => {
         // Unset isSyncEnabled in window
         window.isSyncEnabled = null;
         window.keySetDoneisSyncEnabled = true;
-        if (window.FORMBRICKS_INTEGRATION_DONE) {
-          formbricks.logout();
-        }
         resetUserDetails();
 
         dispatch(actions.updateUserInfo({ loggedIn: false, details: null }));
