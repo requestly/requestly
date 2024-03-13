efc019bf3aea3479d40a2b6a653fae9000862e88
RuntimeTerror10 <parthb.git@gmail.com>
2024-03-13 09:37:06 +0530

    refactor

--- app/src/features/settings/components/BillingTeam/components/RequestTeamAccess/components/Reminder/index.tsx
+++ app/src/features/settings/components/BillingTeam/components/RequestTeamAccess/components/Reminder/index.tsx
@@ -1,22 +1,17 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useFeatureValue } from "@growthbook/growthbook-react";
 import { getUserAuthDetails } from "store/selectors";
 import { Modal, Col } from "antd";
 import { getAvailableBillingTeams } from "store/features/billing/selectors";
-import { BillingTeamDetails } from "../../types";
-import { BillingTeamCard } from "./components/TeamCard";
+import { BillingTeamDetails } from "features/settings/components/BillingTeam/types";
+import { BillingTeamCard } from "../TeamCard";
 import { trackRequestBillingTeamAccessModalViewed } from "features/settings/analytics";
 import APP_CONSTANTS from "config/constants";
-import "./index.scss";
+import { openEmailClientWithDefaultEmailBody } from "utils/Misc";
+import "../../index.scss";
 
-interface Props {
-  isOpen?: boolean;
-  onClose?: () => void;
-  isReminder?: boolean;
-}
-
-export const RequestBillingTeamAccessModal: React.FC<Props> = ({ isOpen, onClose, isReminder = false }) => {
+export const RequestBillingTeamAccessReminder = () => {
   const joinTeamReminder = useFeatureValue("join_team_reminder", null);
   const user = useSelector(getUserAuthDetails);
   const billingTeams = useSelector(getAvailableBillingTeams);
@@ -24,16 +19,16 @@ export const RequestBillingTeamAccessModal: React.FC<Props> = ({ isOpen, onClose
   const [isModalClosable, setIsModalClosable] = useState(true);
 
   const emailSubject = "Request a new Billing Team";
-  const body = `Hey Requestly Team\n\n We'd like to setup a new Billing Team. Could you please assist with the next step here?\n\nThanks\n${user?.details?.profile?.displayName}`;
+  const emailBody = `Hey Requestly Team\n\n We'd like to setup a new Billing Team. Could you please assist with the next step here?\n\nThanks\n${user?.details?.profile?.displayName}`;
 
   useEffect(() => {
-    if (isOpen || isModalVisible) {
+    if (isModalVisible) {
       trackRequestBillingTeamAccessModalViewed();
     }
-  }, [isOpen]);
+  }, [isModalVisible]);
 
   useEffect(() => {
-    if (joinTeamReminder && isReminder) {
+    if (joinTeamReminder) {
       const lastReminderDate = localStorage.getItem("lastReminderDate");
       const currentDate = new Date();
       const reminderStartDate = new Date(joinTeamReminder.startDate);
@@ -52,13 +47,12 @@ export const RequestBillingTeamAccessModal: React.FC<Props> = ({ isOpen, onClose
         setIsModalVisible(true);
       }
     }
-  }, [joinTeamReminder, isReminder]);
+  }, [joinTeamReminder]);
 
   const handleModalClose = useCallback(() => {
     setIsModalVisible(false);
     localStorage.setItem("lastReminderDate", new Date().toISOString().slice(0, 10));
-    onClose?.();
-  }, [onClose]);
+  }, []);
 
   return (
     <Modal
@@ -69,20 +63,18 @@ export const RequestBillingTeamAccessModal: React.FC<Props> = ({ isOpen, onClose
       closable={isModalClosable}
       wrapClassName="custom-rq-modal"
       width={600}
-      open={isOpen ?? isModalVisible}
+      open={isModalVisible}
       onCancel={handleModalClose}
       centered
-      title={isReminder ? "Join a Billing Team" : "Your Organization Billing Teams"}
+      title="Join a Billing Team"
       footer={null}
     >
       <Col>
-        {isReminder && (
-          <div className="text-white">
-            To continue using Requestly, you need a license. We have found the following billing teams in your
-            Organization. If you are part of one of these teams, you can request to join them or create a new Billing
-            Team.
-          </div>
-        )}
+        <div className="text-white">
+          To continue using Requestly, you need a license. We have found the following billing teams in your
+          Organization. If you are part of one of these teams, you can request to join them or create a new Billing
+          Team.
+        </div>
 
         <div className="mt-8 billing-teams-card-wrapper">
           {billingTeams?.map((team: BillingTeamDetails, index: number) => {
@@ -94,18 +86,10 @@ export const RequestBillingTeamAccessModal: React.FC<Props> = ({ isOpen, onClose
         </div>
       </Col>
       <div className="text-white text-bold mt-24">
-        {isReminder
-          ? "Want to setup a new billing team? please write to us at"
-          : "If you have any questions, please write to us at"}{" "}
+        Want to setup a new billing team? please write to us at{" "}
         <a
           className="external-link"
-          href={
-            isReminder
-              ? `mailto:enterprise.support@requestly.io?subject=${encodeURIComponent(
-                  emailSubject
-                )}&body=${encodeURIComponent(body)}`
-              : "mailto:enterprise.support@requestly.io"
-          }
+          href={openEmailClientWithDefaultEmailBody("enterprise.support@requestly.io", emailSubject, emailBody)}
         >
           enterprise.support@requestly.io
         </a>
