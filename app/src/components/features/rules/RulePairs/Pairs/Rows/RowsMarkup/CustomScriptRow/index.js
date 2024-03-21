492ead95c3b52a0f2e9123db1b5769b8ca26917e
nsrCodes <navdeepsinghrathore2000@gmail.com>
2024-03-21 07:22:13 +0530

    fix: don't show as unsaved changes for first render of script rule with attributes

--- app/src/components/features/rules/RulePairs/Pairs/Rows/RowsMarkup/CustomScriptRow/index.js
+++ app/src/components/features/rules/RulePairs/Pairs/Rows/RowsMarkup/CustomScriptRow/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable default-case */
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Row, Col, Input, Tooltip, Typography, Menu, Dropdown, Popconfirm, Button } from "antd";
 import { actions } from "store";
@@ -28,6 +28,7 @@ const CustomScriptRow = ({
 }) => {
   const dispatch = useDispatch();
 
+  const isFirstRender = useRef(true); // to maintain a state for unsaved changes
   const [isCodeTypePopupVisible, setIsCodeTypePopupVisible] = useState(false);
   const [isSourceTypePopupVisible, setIsSourceTypePopupVisible] = useState(false);
   const [codeTypeSelection, setCodeTypeSelection] = useState(GLOBAL_CONSTANTS.SCRIPT_CODE_TYPES.JS);
@@ -186,6 +187,12 @@ const CustomScriptRow = ({
 
   const handleEditorUpdate = useCallback(
     (value) => {
+      const shouldTriggerUnsavedChanges = !isFirstRender.current;
+
+      if (isFirstRender.current) {
+        isFirstRender.current = false;
+      }
+
       if (script.type === GLOBAL_CONSTANTS.SCRIPT_TYPES.URL) {
         /* THIS IS TEMPORARY REPRESENTATION OF SCRIPT ATTRIBUTE */
         dispatch(
@@ -194,13 +201,14 @@ const CustomScriptRow = ({
             updates: {
               [`scripts[${scriptIndex}].wrapperElement`]: value,
             },
+            triggerUnsavedChangesIndication: shouldTriggerUnsavedChanges,
           })
         );
       } else {
         dispatch(
           actions.updateRulePairAtGivenPath({
             pairIndex,
-            triggerUnsavedChangesIndication: !isCodeFormatted,
+            triggerUnsavedChangesIndication: !isCodeFormatted && shouldTriggerUnsavedChanges,
             updates: {
               [`scripts[${scriptIndex}].value`]: value,
             },
