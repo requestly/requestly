63df8af12265198962224a94a91c6f8be65d1208
Nafees Nehar <nafees87n@gmail.com>
2024-03-28 21:14:36 +0530

    [ENGG-1504] fix: CSP errors in modify response rule using external script

--- browser-extension/mv3/src/content-scripts/client/index.ts
+++ browser-extension/mv3/src/content-scripts/client/index.ts
@@ -1,4 +1,4 @@
-import { EXTENSION_MESSAGES } from "common/constants";
+import { CLIENT_MESSAGES, EXTENSION_MESSAGES } from "common/constants";
 import { initRuleExecutionHandler } from "./ruleExecution";
 import { initSessionRecording } from "./sessionRecorder";
 
@@ -8,4 +8,21 @@ if (document.doctype?.name === "html" || document.contentType?.includes("html"))
   chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.HANDSHAKE_CLIENT });
   initSessionRecording();
   initRuleExecutionHandler();
+
+  chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
+    switch (message.action) {
+      case CLIENT_MESSAGES.EXECUTE_SCRIPT:
+        executeScriptOnPageExternally(message.code);
+        break;
+    }
+    return false;
+  });
 }
+
+const executeScriptOnPageExternally = (code: string) => {
+  const script = document.createElement("script");
+  script.src = chrome.runtime.getURL("executeScript.js");
+  script.onload = () => script.remove();
+  script.dataset.params = JSON.stringify({ code });
+  (document.head || document.documentElement).appendChild(script);
+};
