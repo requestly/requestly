58c72427bff4905167a4ec2020954f49b29bed89
Nafees Nehar <nafees87n@gmail.com>
2024-04-08 14:57:36 +0530

    [ENGG-1504 ENGG-1521 ENGG-1522] MV3 - fix CSP errors and match request/response rule client side (#1533)
    * [ENGG-1504] fix: CSP errors in modify response rule using external script
    
    * invoke request rule using page script
    
    * fix: request rule JS mode not working
    
    * fix: folder
    
    * [ENGG-1521] MV3 cache and match request/response rules on client side  (#1536)
    
    * fix: match responseRule on client side
    
    * fix: response rule URL in XHR
    
    * fix: response rule matching in client
    
    * rename executeScript
    
    * remove unused files
    
    * fix: check for rules length
    
    * [ENGG-1522] fix: graphQL filter not working in MV3 (#1544)
    
    * fix: request rule filters
    
    * fix: JSON traverse breaking
    
    * rename ext

---
+++ browser-extension/mv3/src/content-scripts/client/requestRuleHandler.ts
@@ -0,0 +1,33 @@
+import { getEnabledRules, onRuleOrGroupChange } from "common/rulesStore";
+import { RequestRulePair, RuleType } from "common/types";
+import { executeDynamicScriptOnPage } from "../utils";
+import { PUBLIC_NAMESPACE } from "common/constants";
+
+const cacheRequestRules = async () => {
+  const requestRules = await getEnabledRules(RuleType.REQUEST);
+
+  if (!requestRules.length) {
+    return;
+  }
+
+  executeDynamicScriptOnPage(
+    `
+    window.${PUBLIC_NAMESPACE}=window.${PUBLIC_NAMESPACE}||{};
+    window.${PUBLIC_NAMESPACE}.requestRules=${JSON.stringify(
+      requestRules.map((rule) => {
+        const requestRulePair = rule.pairs[0] as RequestRulePair;
+        return {
+          id: rule.id,
+          source: requestRulePair.source,
+          request: requestRulePair.request,
+        };
+      })
+    )};
+    `
+  );
+};
+
+export const initRequestRuleHandler = () => {
+  onRuleOrGroupChange(cacheRequestRules);
+  cacheRequestRules();
+};
