eb31560a196a9653ab764e28dbfef6b87963d3e7
Nafees Nehar <nafees87n@gmail.com>
2024-04-08 14:46:37 +0530

    [ENGG-1521] MV3 cache and match request/response rules on client side  (#1536)
    * fix: match responseRule on client side
    
    * fix: response rule URL in XHR
    
    * fix: response rule matching in client
    
    * rename executeScript
    
    * remove unused files
    
    * fix: check for rules length
    
    * [ENGG-1522] fix: graphQL filter not working in MV3 (#1544)
    
    * fix: request rule filters
    
    * fix: JSON traverse breaking
    
    * rename ext

--- browser-extension/mv3/src/content-scripts/client/index.ts
+++ browser-extension/mv3/src/content-scripts/client/index.ts
@@ -1,6 +1,9 @@
 import { CLIENT_MESSAGES, EXTENSION_MESSAGES } from "common/constants";
 import { initRuleExecutionHandler } from "./ruleExecution";
 import { initSessionRecording } from "./sessionRecorder";
+import { executeDynamicScriptOnPage } from "../utils";
+import { initResponseRuleHandler } from "./responseRuleHandler";
+import { initRequestRuleHandler } from "./requestRuleHandler";
 
 console.log("Hello from Requestly!");
 
@@ -8,21 +11,15 @@ if (document.doctype?.name === "html" || document.contentType?.includes("html"))
   chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.HANDSHAKE_CLIENT });
   initSessionRecording();
   initRuleExecutionHandler();
+  initResponseRuleHandler();
+  initRequestRuleHandler();
 
   chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
     switch (message.action) {
       case CLIENT_MESSAGES.EXECUTE_SCRIPT:
-        executeScriptOnPageExternally(message.code);
+        executeDynamicScriptOnPage(message.code);
         break;
     }
     return false;
   });
 }
-
-const executeScriptOnPageExternally = (code: string) => {
-  const script = document.createElement("script");
-  script.src = chrome.runtime.getURL("libs/executeScript.js");
-  script.onload = () => script.remove();
-  script.dataset.params = JSON.stringify({ code });
-  (document.head || document.documentElement).appendChild(script);
-};
