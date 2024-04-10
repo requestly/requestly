74aa29c79f0addb42d6179207b9055da11044a6f
Nafees Nehar <nafees87n@gmail.com>
2024-04-10 12:36:26 +0530

    fix: build zip

--- browser-extension/mv3/builds/chrome/requestly_mv3_v24.4.5_prod_2024-04-05/devtools/devtools.js
+++
@@ -1,4 +0,0 @@
-// Add panel for non-extension pages
-if (chrome.devtools.inspectedWindow.tabId > 0) {
-  chrome.devtools.panels.create("🔀 Requestly", "/resources/images/48x48.png", "/devtools/index.html");
-}
--- browser-extension/mv3/builds/chrome/requestly_mv3_v24.4.5_prod_2024-04-05/libs/executeScript.js
+++
@@ -1,8 +0,0 @@
-!(function () {
-  "use strict";
-  const t = JSON.parse(document.currentScript.dataset.params),
-    { code: c } = t;
-  if (c) {
-    new Function(c)();
-  }
-})();
