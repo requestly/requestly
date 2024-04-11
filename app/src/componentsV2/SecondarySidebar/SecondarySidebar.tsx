f5856d930a68cac1938ca4bfd923daeb7fa07902
Sahil Gupta <sahil865gupta@gmail.com>
2024-04-11 23:57:46 +0530

    added SecondarySidebarLayout

--- app/src/componentsV2/SecondarySidebar/SecondarySidebar.tsx
+++ app/src/componentsV2/SecondarySidebar/SecondarySidebar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import { SecondarySidebarItem } from "./components/SecondarySidebarItem/SecondarySidebarItem";
+import { getIsSecondarySidebarCollapsed } from "store/selectors";
 
-import "./SecondarySidebar.css";
+import "./SecondarySidebar.scss";
 
-interface Props {
+export interface SecondarySidebarProps {
   items: {
     title: string;
     path: string;
@@ -11,9 +14,16 @@ interface Props {
   }[];
 }
 
-const SecondarySidebar: React.FC<Props> = ({ items }) => {
+const SecondarySidebar: React.FC<SecondarySidebarProps> = ({ items }) => {
+  // Move this to local state once button is also moved to this component
+  const isSecondarySidebarCollapsed = useSelector(getIsSecondarySidebarCollapsed);
+
+  if (isSecondarySidebarCollapsed) {
+    return <div className="secondary-sidebar-container collapsed"></div>;
+  }
+
   return (
-    <div className="rq-secondary-sidebar-container">
+    <div className="secondary-sidebar-container">
       <ul>
         {items.map(({ path, title, icon }) => {
           return (
