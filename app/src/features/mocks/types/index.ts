321073ba5a1d7e34af3e8e425b39b7e7d4a6f3e8
rohanmathur91 <mathurrohan04@gmail.com>
2024-04-16 14:41:59 +0530

    fix: types

---
+++ app/src/features/mocks/types/index.ts
@@ -0,0 +1,30 @@
+import { MockMetadataSchema, MockSchema } from "@requestly/mock-server";
+
+export enum MockType {
+  API = "API",
+  FILE = "FILE",
+}
+
+export enum FileType {
+  JS = "JS",
+  CSS = "CSS",
+  HTML = "HTML",
+  IMAGE = "IMAGE",
+}
+
+// Extends MockSchema and adds powers Requestly Mock Server specific features
+export interface RQMockMetadataSchema extends MockMetadataSchema {
+  type: MockType;
+  fileType?: FileType;
+  isOldMock?: boolean;
+  oldMockFilePath?: string;
+  url?: string;
+  createdBy?: string;
+  lastUpdatedBy?: string;
+}
+
+export interface RQMockSchema extends MockSchema, RQMockMetadataSchema {}
+
+export enum MockListSource {
+  PICKER_MODAL,
+}
