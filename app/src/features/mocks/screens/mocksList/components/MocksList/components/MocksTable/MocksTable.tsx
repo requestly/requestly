366ac993a8038d3d9ef1f8c8c252dd0bda757af9
rohanmathur91 <mathurrohan04@gmail.com>
2024-04-16 15:05:27 +0530

    fix: style

--- app/src/features/mocks/screens/mocksList/components/MocksList/components/MocksTable/MocksTable.tsx
+++ app/src/features/mocks/screens/mocksList/components/MocksList/components/MocksTable/MocksTable.tsx
@@ -275,6 +275,7 @@ export const MocksTable: React.FC<Props> = ({
       id="mock-list-table"
       pagination={false}
       size="middle"
+      customRowClassName={() => "rq-mocks-list-table-row"}
       scroll={{ y: `calc(100vh - ${isFeatureLimitbannerShown ? "(277px + 68px)" : "277px"})` }} // 68px is Feature limit banner height
       // @ts-ignore
       columns={columns}
