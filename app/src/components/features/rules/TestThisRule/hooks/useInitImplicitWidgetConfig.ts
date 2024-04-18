20f27db4d2afa4474ead59f5b3e70ff489649fc9
RuntimeTerror10 <parthb.git@gmail.com>
2024-04-19 00:50:26 +0530

    add feature flag for implicit test this rule flow

--- app/src/components/features/rules/TestThisRule/hooks/useInitImplicitWidgetConfig.ts
+++ app/src/components/features/rules/TestThisRule/hooks/useInitImplicitWidgetConfig.ts
@@ -1,28 +1,29 @@
+import { useEffect } from "react";
 import { StorageService } from "init";
 import { useSelector } from "react-redux";
 import { getAppMode } from "store/selectors";
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
-import { useCallback } from "react";
+import { useFeatureIsOn } from "@growthbook/growthbook-react";
+import { updateImplictRuleTestingWidgetConfig } from "features/settings";
 
-export const useInitImplicitWidgetConfig = () => {
+export const InitImplicitWidgetConfigHandler = () => {
   const appMode = useSelector(getAppMode);
+  const isImplicitRuleTestingEnabled = useFeatureIsOn("implicit_test_this_rule");
 
-  const initImplicitWidgetConfig = useCallback(() => {
-    StorageService(appMode)
-      .getRecord(GLOBAL_CONSTANTS.STORAGE_KEYS.IMPLICIT_RULE_TESTING_WIDGET_CONFIG)
-      .then((record) => {
-        if (record === undefined) {
-          StorageService(appMode).saveRecord({
-            [GLOBAL_CONSTANTS.STORAGE_KEYS.IMPLICIT_RULE_TESTING_WIDGET_CONFIG]: {
+  useEffect(() => {
+    if (isImplicitRuleTestingEnabled) {
+      StorageService(appMode)
+        .getRecord(GLOBAL_CONSTANTS.STORAGE_KEYS.IMPLICIT_RULE_TESTING_WIDGET_CONFIG)
+        .then((record) => {
+          if (record === undefined) {
+            updateImplictRuleTestingWidgetConfig(appMode, {
               visibility: GLOBAL_CONSTANTS.IMPLICIT_RULE_TESTING_WIDGET_VISIBILITY.ALL,
               ruleTypes: [],
-            },
-          });
-        }
-      });
-  }, [appMode]);
-
-  return {
-    initImplicitWidgetConfig,
-  };
+            });
+          }
+        });
+    } else {
+      StorageService(appMode).removeRecord(GLOBAL_CONSTANTS.STORAGE_KEYS.IMPLICIT_RULE_TESTING_WIDGET_CONFIG);
+    }
+  }, [appMode, isImplicitRuleTestingEnabled]);
 };
--- app/src/features/settings/index.ts
+++ app/src/features/settings/index.ts
@@ -1,3 +1,4 @@
 export { RequestBillingTeamAccessModal } from "./components/BillingTeam/components/RequestTeamAccess/components/Modal";
 export { RequestBillingTeamAccessReminder } from "./components/BillingTeam/components/RequestTeamAccess/components/Reminder";
 export * from "./components/BillingTeam/utils";
+export { updateImplictRuleTestingWidgetConfig } from "./components/GlobalSettings/components/ImplicitRuleTesting/utils";
