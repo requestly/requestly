9593ab30ad1ae270931644deddc0353aae836ca4
rohanmathur91 <mathurrohan04@gmail.com>
2024-04-18 17:35:47 +0530

    cleanup

--- app/src/features/mocks/screens/mocksList/MocksListScreen.tsx
+++ app/src/features/mocks/screens/mocksList/MocksListScreen.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { MockListSource, MockType } from "features/mocks/types";
+import { MockType } from "features/mocks/types";
 import MockList from "./components/MocksList/MocksList";
 
 interface Props {
   type?: MockType;
-  source?: MockListSource;
-  mockSelectionCallback?: (url: string) => void;
 }
 
-const MocksListScreen: React.FC<Props> = ({ source, mockSelectionCallback, type }) => {
-  return <MockList type={type} source={source} mockSelectionCallback={mockSelectionCallback} />;
+const MocksListScreen: React.FC<Props> = ({ type }) => {
+  return <MockList type={type} />;
 };
 
 export default MocksListScreen;
