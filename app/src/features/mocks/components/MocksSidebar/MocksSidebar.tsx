9a9148a30fcaca0e695dc963340aa673cc9771b3
rohanmathur91 <mathurrohan04@gmail.com>
2024-04-18 18:18:44 +0530

    fix: sidebar routes

--- app/src/features/mocks/components/MocksSidebar/MocksSidebar.tsx
+++ app/src/features/mocks/components/MocksSidebar/MocksSidebar.tsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 import PATHS from "config/constants/sub/paths";
 import { IoDocumentTextOutline } from "@react-icons/all-files/io5/IoDocumentTextOutline";
 import { MdOutlineUploadFile } from "@react-icons/all-files/md/MdOutlineUploadFile";
 import { SecondarySidebar } from "componentsV2/SecondarySidebar";
+import { actions } from "store";
 
-// TODO: remove /v2 prefix after development
 const mocksSidebarItems = [
   {
     title: "My Mock APIs",
-    path: "/v2" + PATHS.MOCK_SERVER_V2.ABSOLUTE,
+    path: PATHS.MOCK_SERVER_V2.ABSOLUTE,
     icon: <IoDocumentTextOutline />,
   },
   {
     title: "My Files",
-    path: "/v2" + PATHS.FILE_SERVER_V2.ABSOLUTE,
+    path: PATHS.FILE_SERVER_V2.ABSOLUTE,
     icon: <MdOutlineUploadFile />,
   },
 ];
 
 export const MocksSidebar: React.FC = () => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (location.pathname.includes("editor")) {
+      // @ts-ignore
+      dispatch(actions.updateSecondarySidebarCollapse(true));
+    } else {
+      // @ts-ignore
+      dispatch(actions.updateSecondarySidebarCollapse(false));
+    }
+  }, [dispatch, location]);
+
   return <SecondarySidebar items={mocksSidebarItems} />;
 };
