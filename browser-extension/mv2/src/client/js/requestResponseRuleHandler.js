60d5ff2f0e49b988d85d80f5ea8dd67c169c7834
Sahil Gupta <sahil865gupta@gmail.com>
2024-04-22 17:18:33 +0530

    refractor

--- browser-extension/mv2/src/client/js/requestResponseRuleHandler.js
+++ browser-extension/mv2/src/client/js/requestResponseRuleHandler.js
@@ -372,188 +372,6 @@ RQ.RequestResponseRuleHandler.interceptAJAXRequests = function ({
 
   const isContentTypeJSON = (contentType) => !!contentType?.includes("application/json");
 
-  // Intercept fetch API
-  const _fetch = fetch;
-  fetch = async (...args) => {
-    const [resource, initOptions = {}] = args;
-    const getOriginalResponse = () => _fetch(...args);
-
-    let request;
-
-    if (resource instanceof Request) {
-      request = resource.clone();
-    } else {
-      request = new Request(resource.toString(), initOptions);
-    }
-
-    let hasModifiedHeaders = false;
-
-    const url = getAbsoluteUrl(request.url);
-    const method = request.method;
-
-    const redirectRuleThatMatchesURL = getMatchingRedirectRule(url);
-    const replaceRuleThatMatchesURL = getMatchingReplaceRule(url);
-
-    // redirect/replace rule specific code that is applied only when redirect/replace rule matches the URL
-    if (redirectRuleThatMatchesURL || replaceRuleThatMatchesURL) {
-      // Stores Auth header to be set on redirected URL. Refer: https://github.com/requestly/requestly/issues/1208
-      ignoredHeadersOnRedirect.forEach((header) => {
-        const originalHeaderValue = request.headers.get(header);
-        if (isExtensionEnabled() && originalHeaderValue) {
-          hasModifiedHeaders = true;
-          request.headers.set(customHeaderPrefix + header, originalHeaderValue);
-        }
-      });
-    }
-
-    // Request body can be sent only for request methods other than GET and HEAD.
-    const canRequestBodyBeSent = !["GET", "HEAD"].includes(method);
-
-    const requestRule = canRequestBodyBeSent && getRequestRule(url);
-    if (requestRule) {
-      const originalRequestBody = await request.text();
-      const requestBody =
-        getCustomRequestBody(requestRule, {
-          method: request.method,
-          url,
-          body: originalRequestBody,
-          bodyAsJson: jsonifyValidJSONString(originalRequestBody, true),
-        }) || {};
-
-      request = new Request(request.url, {
-        method,
-        body: requestBody,
-        headers: request.headers,
-        referrer: request.referrer,
-        referrerPolicy: request.referrerPolicy,
-        mode: request.mode,
-        credentials: request.credentials,
-        cache: request.cache,
-        redirect: request.redirect,
-        integrity: request.integrity,
-      });
-
-      notifyRequestRuleApplied({
-        ruleDetails: requestRule,
-        requestDetails: {
-          url,
-          method: request.method,
-          type: "fetch",
-          timeStamp: Date.now(),
-        },
-      });
-    }
-
-    let requestData;
-    if (canRequestBodyBeSent) {
-      requestData = jsonifyValidJSONString(await request.clone().text()); // cloning because the request will be used to make API call
-    }
-
-    const responseRule = getResponseRule({ url, requestData, method });
-    let responseHeaders;
-    let fetchedResponse;
-
-    if (responseRule && shouldServeResponseWithoutRequest(responseRule)) {
-      const contentType = isJSON(responseRule.pairs[0].response.value) ? "application/json" : "text/plain";
-      responseHeaders = new Headers({ "content-type": contentType });
-    } else {
-      try {
-        if (requestRule || hasModifiedHeaders) {
-          // use modified request to fetch response
-          fetchedResponse = await _fetch(request);
-        } else {
-          fetchedResponse = await getOriginalResponse();
-        }
-
-        if (!responseRule) {
-          return fetchedResponse;
-        }
-
-        responseHeaders = fetchedResponse?.headers;
-      } catch (error) {
-        if (!responseRule) {
-          return Promise.reject(error);
-        }
-      }
-    }
-
-    isDebugMode &&
-      console.log("RQ", "Inside the fetch block for url", {
-        url,
-        resource,
-        initOptions,
-        fetchedResponse,
-      });
-
-    let customResponse;
-    const responseModification = responseRule.pairs[0].response;
-
-    if (responseModification.type === "code") {
-      const requestHeaders =
-        request.headers &&
-        Array.from(request.headers).reduce((obj, [key, val]) => {
-          obj[key] = val;
-          return obj;
-        }, {});
-
-      let evaluatorArgs = {
-        method,
-        url,
-        requestHeaders,
-        requestData,
-      };
-
-      if (fetchedResponse) {
-        const fetchedResponseData = await fetchedResponse.text();
-        const responseType = fetchedResponse.headers.get("content-type");
-        const fetchedResponseDataAsJson = jsonifyValidJSONString(fetchedResponseData, true);
-
-        evaluatorArgs = {
-          ...evaluatorArgs,
-          responseType,
-          response: fetchedResponseData,
-          responseJSON: fetchedResponseDataAsJson,
-        };
-      }
-
-      customResponse = getFunctionFromCode(responseModification.value)(evaluatorArgs);
-
-      // evaluator might return us Object but response.value is string
-      // So make the response consistent by converting to JSON String and then create the Response object
-      if (isPromise(customResponse)) {
-        customResponse = await customResponse;
-      }
-
-      if (typeof customResponse === "object" && isContentTypeJSON(evaluatorArgs?.responseType)) {
-        customResponse = JSON.stringify(customResponse);
-      }
-    } else {
-      customResponse = responseModification.value;
-    }
-
-    const requestDetails = {
-      url,
-      method,
-      type: "fetch",
-      timeStamp: Date.now(),
-    };
-
-    notifyResponseRuleApplied({
-      rule: responseRule,
-      requestDetails,
-    });
-
-    // For network failures, fetchedResponse is undefined but we still return customResponse with status=200
-    const finalStatusCode = parseInt(responseModification.statusCode || fetchedResponse?.status) || 200;
-    const requiresNullResponseBody = [204, 205, 304].includes(finalStatusCode);
-
-    return new Response(requiresNullResponseBody ? null : new Blob([customResponse]), {
-      status: finalStatusCode,
-      statusText: responseModification.statusText || fetchedResponse?.statusText,
-      headers: responseHeaders,
-    });
-  };
-
   // XHR Implementation
   const updateXhrReadyState = (xhr, readyState) => {
     // console.log("[RQ]", "updateXhrReadyState", readyState);
@@ -863,5 +681,187 @@ RQ.RequestResponseRuleHandler.interceptAJAXRequests = function ({
     }
 
     send.call(this, this._requestData);
+  };
+
+  // Intercept fetch API
+  const _fetch = fetch;
+  fetch = async (...args) => {
+    const [resource, initOptions = {}] = args;
+    const getOriginalResponse = () => _fetch(...args);
+
+    let request;
+
+    if (resource instanceof Request) {
+      request = resource.clone();
+    } else {
+      request = new Request(resource.toString(), initOptions);
+    }
+
+    let hasModifiedHeaders = false;
+
+    const url = getAbsoluteUrl(request.url);
+    const method = request.method;
+
+    const redirectRuleThatMatchesURL = getMatchingRedirectRule(url);
+    const replaceRuleThatMatchesURL = getMatchingReplaceRule(url);
+
+    // redirect/replace rule specific code that is applied only when redirect/replace rule matches the URL
+    if (redirectRuleThatMatchesURL || replaceRuleThatMatchesURL) {
+      // Stores Auth header to be set on redirected URL. Refer: https://github.com/requestly/requestly/issues/1208
+      ignoredHeadersOnRedirect.forEach((header) => {
+        const originalHeaderValue = request.headers.get(header);
+        if (isExtensionEnabled() && originalHeaderValue) {
+          hasModifiedHeaders = true;
+          request.headers.set(customHeaderPrefix + header, originalHeaderValue);
+        }
+      });
+    }
+
+    // Request body can be sent only for request methods other than GET and HEAD.
+    const canRequestBodyBeSent = !["GET", "HEAD"].includes(method);
+
+    const requestRule = canRequestBodyBeSent && getRequestRule(url);
+    if (requestRule) {
+      const originalRequestBody = await request.text();
+      const requestBody =
+        getCustomRequestBody(requestRule, {
+          method: request.method,
+          url,
+          body: originalRequestBody,
+          bodyAsJson: jsonifyValidJSONString(originalRequestBody, true),
+        }) || {};
+
+      request = new Request(request.url, {
+        method,
+        body: requestBody,
+        headers: request.headers,
+        referrer: request.referrer,
+        referrerPolicy: request.referrerPolicy,
+        mode: request.mode,
+        credentials: request.credentials,
+        cache: request.cache,
+        redirect: request.redirect,
+        integrity: request.integrity,
+      });
+
+      notifyRequestRuleApplied({
+        ruleDetails: requestRule,
+        requestDetails: {
+          url,
+          method: request.method,
+          type: "fetch",
+          timeStamp: Date.now(),
+        },
+      });
+    }
+
+    let requestData;
+    if (canRequestBodyBeSent) {
+      requestData = jsonifyValidJSONString(await request.clone().text()); // cloning because the request will be used to make API call
+    }
+
+    const responseRule = getResponseRule({ url, requestData, method });
+    let responseHeaders;
+    let fetchedResponse;
+
+    if (responseRule && shouldServeResponseWithoutRequest(responseRule)) {
+      const contentType = isJSON(responseRule.pairs[0].response.value) ? "application/json" : "text/plain";
+      responseHeaders = new Headers({ "content-type": contentType });
+    } else {
+      try {
+        if (requestRule || hasModifiedHeaders) {
+          // use modified request to fetch response
+          fetchedResponse = await _fetch(request);
+        } else {
+          fetchedResponse = await getOriginalResponse();
+        }
+
+        if (!responseRule) {
+          return fetchedResponse;
+        }
+
+        responseHeaders = fetchedResponse?.headers;
+      } catch (error) {
+        if (!responseRule) {
+          return Promise.reject(error);
+        }
+      }
+    }
+
+    isDebugMode &&
+      console.log("RQ", "Inside the fetch block for url", {
+        url,
+        resource,
+        initOptions,
+        fetchedResponse,
+      });
+
+    let customResponse;
+    const responseModification = responseRule.pairs[0].response;
+
+    if (responseModification.type === "code") {
+      const requestHeaders =
+        request.headers &&
+        Array.from(request.headers).reduce((obj, [key, val]) => {
+          obj[key] = val;
+          return obj;
+        }, {});
+
+      let evaluatorArgs = {
+        method,
+        url,
+        requestHeaders,
+        requestData,
+      };
+
+      if (fetchedResponse) {
+        const fetchedResponseData = await fetchedResponse.text();
+        const responseType = fetchedResponse.headers.get("content-type");
+        const fetchedResponseDataAsJson = jsonifyValidJSONString(fetchedResponseData, true);
+
+        evaluatorArgs = {
+          ...evaluatorArgs,
+          responseType,
+          response: fetchedResponseData,
+          responseJSON: fetchedResponseDataAsJson,
+        };
+      }
+
+      customResponse = getFunctionFromCode(responseModification.value)(evaluatorArgs);
+
+      // evaluator might return us Object but response.value is string
+      // So make the response consistent by converting to JSON String and then create the Response object
+      if (isPromise(customResponse)) {
+        customResponse = await customResponse;
+      }
+
+      if (typeof customResponse === "object" && isContentTypeJSON(evaluatorArgs?.responseType)) {
+        customResponse = JSON.stringify(customResponse);
+      }
+    } else {
+      customResponse = responseModification.value;
+    }
+
+    const requestDetails = {
+      url,
+      method,
+      type: "fetch",
+      timeStamp: Date.now(),
+    };
+
+    notifyResponseRuleApplied({
+      rule: responseRule,
+      requestDetails,
+    });
+
+    // For network failures, fetchedResponse is undefined but we still return customResponse with status=200
+    const finalStatusCode = parseInt(responseModification.statusCode || fetchedResponse?.status) || 200;
+    const requiresNullResponseBody = [204, 205, 304].includes(finalStatusCode);
+
+    return new Response(requiresNullResponseBody ? null : new Blob([customResponse]), {
+      status: finalStatusCode,
+      statusText: responseModification.statusText || fetchedResponse?.statusText,
+      headers: responseHeaders,
+    });
   };
 };
