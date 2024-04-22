6f37b10c41f3d1b16bb9291b1e6644317c68ce98
rohanmathur91 <mathurrohan04@gmail.com>
2024-04-22 17:48:13 +0530

    fix: fetch mocks

--- app/src/features/mocks/screens/mocksList/components/MocksList/hooks/useFetchMocks.ts
+++ app/src/features/mocks/screens/mocksList/components/MocksList/hooks/useFetchMocks.ts
@@ -10,7 +10,7 @@ import { useSelector } from "react-redux";
 import { getCurrentlyActiveWorkspace } from "store/features/teams/selectors";
 import { getUserAuthDetails } from "store/selectors";
 
-export const useFetchMocks = (type: MockType) => {
+export const useFetchMocks = (type: MockType, forceRender: boolean) => {
   const user = useSelector(getUserAuthDetails);
   const uid = user?.details?.profile?.uid;
   const workspace = useSelector(getCurrentlyActiveWorkspace);
@@ -81,7 +81,7 @@ export const useFetchMocks = (type: MockType) => {
     } else {
       setOldMocksList([]);
     }
-  }, [fetchMocks, fetchOldMocks, workspace, setOldMocksList]);
+  }, [fetchMocks, fetchOldMocks, workspace, setOldMocksList, forceRender]);
 
   return {
     mocks,
