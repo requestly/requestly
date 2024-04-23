7493911b2459286c430fc4898bbc25da86fa9d7c
RuntimeTerror10 <parthb.git@gmail.com>
2024-04-24 02:00:30 +0530

    add template preview modal

--- app/src/features/rules/screens/templatesList/components/TemplatesTable/hooks/useTemplatesTableColumns.tsx
+++ app/src/features/rules/screens/templatesList/components/TemplatesTable/hooks/useTemplatesTableColumns.tsx
@@ -2,8 +2,30 @@ import { Button, Table } from "antd";
 import { ContentListTableProps } from "componentsV2/ContentList";
 import { TemplateRecord } from "../types";
 import RuleTypeTag from "components/common/RuleTypeTag";
+import { useNavigate } from "react-router-dom";
+import { redirectToSharedListViewer } from "utils/RedirectionUtils";
+import { useCallback } from "react";
 
-const useTemplatesTableColumns = () => {
+interface Props {
+  handlePreviewTemplateInModal: (template: TemplateRecord) => void;
+}
+
+const useTemplatesTableColumns: (props: Props) => ContentListTableProps<TemplateRecord>["columns"] = ({
+  handlePreviewTemplateInModal,
+}) => {
+  const navigate = useNavigate();
+
+  const handlePreviewTemplate = useCallback(
+    (template: TemplateRecord) => {
+      if (template.isSharedList) {
+        redirectToSharedListViewer(navigate, template.data.shareId, template.data.sharedListName, true);
+      } else {
+        handlePreviewTemplateInModal(template);
+      }
+    },
+    [navigate, handlePreviewTemplateInModal]
+  );
+
   const columns: ContentListTableProps<TemplateRecord>["columns"] = [
     Table.SELECTION_COLUMN,
     {
@@ -51,7 +73,9 @@ const useTemplatesTableColumns = () => {
       render: (_: any, record: TemplateRecord) => {
         return (
           <div className="templates-actions-container">
-            <Button type="default">Use this</Button>
+            <Button type="default" onClick={() => handlePreviewTemplate(record)}>
+              Use this
+            </Button>
           </div>
         );
       },
