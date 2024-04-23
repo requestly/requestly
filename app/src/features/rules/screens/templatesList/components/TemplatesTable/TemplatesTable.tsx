9f7a557487ce66b73cd68a71497c0238751bed8e
RuntimeTerror10 <parthb.git@gmail.com>
2024-04-24 02:00:30 +0530

    add template preview modal

--- app/src/features/rules/screens/templatesList/components/TemplatesTable/TemplatesTable.tsx
+++ app/src/features/rules/screens/templatesList/components/TemplatesTable/TemplatesTable.tsx
@@ -2,25 +2,46 @@ import { ContentListTable, withContentListTableContext } from "componentsV2/Cont
 import { Empty } from "antd";
 import templateRecords from "./constants/templates";
 import useTemplatesTableColumns from "./hooks/useTemplatesTableColumns";
+import { useState } from "react";
+import { TemplatePreviewModal } from "../../modals/TemplatePreviewModal/TemplatePreviewModal";
+import { TemplateRecord } from "./types";
 import "./templatesTable.scss";
 
 const TemplatesTable = () => {
-  const tableColumns = useTemplatesTableColumns();
+  const [isTemplatePreviewModalVisible, setIsTemplatePreviewModalVisible] = useState(false);
+  const [templateToPreview, setTemplateToPreview] = useState(null);
+
+  const handlePreviewTemplateInModal = (template: TemplateRecord) => {
+    setTemplateToPreview(template.data);
+    setIsTemplatePreviewModalVisible(true);
+  };
+
+  const tableColumns = useTemplatesTableColumns({ handlePreviewTemplateInModal });
 
   return (
-    <div className="rq-templates-table">
-      <ContentListTable
-        id="templates-list-table"
-        size="large"
-        columns={tableColumns}
-        data={templateRecords}
-        locale={{
-          emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No Templates found" />,
-        }}
-        disableRowSelection
-        scroll={{ y: `calc(100vh - 210px)` }}
-      />
-    </div>
+    <>
+      {isTemplatePreviewModalVisible && (
+        <TemplatePreviewModal
+          rule={templateToPreview}
+          isOpen={isTemplatePreviewModalVisible}
+          toggle={() => setIsTemplatePreviewModalVisible(false)}
+          source="templates_screen"
+        />
+      )}
+      <div className="rq-templates-table">
+        <ContentListTable
+          id="templates-list-table"
+          size="large"
+          columns={tableColumns}
+          data={templateRecords}
+          locale={{
+            emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No Templates found" />,
+          }}
+          disableRowSelection
+          scroll={{ y: `calc(100vh - 210px)` }}
+        />
+      </div>
+    </>
   );
 };
 
