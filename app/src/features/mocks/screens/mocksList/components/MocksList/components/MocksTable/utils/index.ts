1fd189595a36814af29c48f70602a740b3de5b35
rohanmathur91 <mathurrohan04@gmail.com>
2024-04-24 15:58:13 +0530

    fix

--- app/src/features/mocks/screens/mocksList/components/MocksList/components/MocksTable/utils/index.ts
+++ app/src/features/mocks/screens/mocksList/components/MocksList/components/MocksTable/utils/index.ts
@@ -1,7 +1,7 @@
 import { MockRecordType, RQMockMetadataSchema } from "components/features/mocksV2/types";
 
-export const isRecordMockCollection = (recordType: RQMockMetadataSchema["recordType"]) => {
-  return recordType === MockRecordType.COLLECTION;
+export const isRecordMockCollection = (record: RQMockMetadataSchema) => {
+  return record.recordType === MockRecordType.COLLECTION;
 };
 
 export const isMockInCollection = (mock: RQMockMetadataSchema) => {
@@ -14,7 +14,7 @@ export const mocksToContentTableDataAdapter = (mocks: RQMockMetadataSchema[]) =>
   } = {};
 
   mocks.forEach((mock) => {
-    if (isRecordMockCollection(mock.recordType)) {
+    if (isRecordMockCollection(mock)) {
       mockCollections[mock.id] = { ...mock, children: [] };
     }
   });
@@ -26,7 +26,7 @@ export const mocksToContentTableDataAdapter = (mocks: RQMockMetadataSchema[]) =>
       if (mockCollections[mock.collectionId]) {
         mockCollections[mock.collectionId].children.push(mock);
       }
-    } else if (!isRecordMockCollection(mock.recordType)) {
+    } else if (!isRecordMockCollection(mock)) {
       otherMocks.push(mock);
     }
   });
