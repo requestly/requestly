a135a30bdeb9d6fe499dd80c55a99189aebeee93
rohanmathur91 <mathurrohan04@gmail.com>
2024-04-24 15:38:50 +0530

    add update bulk mock collection id controller

---
+++ app/src/backend/mocks/updateMocksCollectionId.ts
@@ -0,0 +1,48 @@
+import firebaseApp from "../../firebase";
+import { Timestamp, doc, getDoc, getFirestore, writeBatch } from "firebase/firestore";
+
+export const updateMocksCollectionId = async (
+  uid: string,
+  mockIds: string[],
+  updatedCollectionId: string
+): Promise<boolean> => {
+  if (!uid) {
+    return null;
+  }
+
+  const result = await updateMocksCollectionIdFromFirebase(uid, mockIds, updatedCollectionId);
+  return result;
+};
+
+const updateMocksCollectionIdFromFirebase = async (
+  uid: string,
+  mockIds: string[],
+  updatedCollectionId: string
+): Promise<boolean> => {
+  const db = getFirestore(firebaseApp);
+  const docRef = doc(db, "mocks", updatedCollectionId);
+  const snapshot = await getDoc(docRef);
+
+  if (!snapshot.exists()) {
+    return;
+  }
+
+  try {
+    const mocksbatch = writeBatch(db);
+
+    mockIds.forEach((id) => {
+      const mockRef = doc(db, "mocks", id);
+      mocksbatch.set(
+        mockRef,
+        { collectionId: updatedCollectionId, lastUpdatedBy: uid, updatedTs: Timestamp.now().toMillis() },
+        { merge: true }
+      );
+    });
+
+    await mocksbatch.commit();
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
