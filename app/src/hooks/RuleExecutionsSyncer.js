6710075e90d0620ecf0099bfb897e122db7d1fbf
RuntimeTerror10 <parthb.git@gmail.com>
2024-04-29 16:02:21 +0530

    ENGG-1608: remove send rule execution attribute

--- app/src/hooks/RuleExecutionsSyncer.js
+++ app/src/hooks/RuleExecutionsSyncer.js
@@ -4,7 +4,6 @@ import { StorageService } from "init";
 import { getAppMode, getUserAuthDetails } from "store/selectors";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import firebaseApp from "../firebase";
-import { submitAttrUtil } from "utils/AnalyticsUtils";
 import { trackRuleExecuted } from "modules/analytics/events/common/rules";
 import Logger from "lib/logger";
 
@@ -25,15 +24,6 @@ const RuleExecutionsSyncer = () => {
   };
 
   const stableWriteStuffToDB = useCallback(writeStuffToDB, [user?.details?.profile?.uid]);
-
-  const sendAsAttribute = (data, YYYY, MM) => {
-    for (const ruleType in data) {
-      const attrName = ruleType.toLowerCase() + "_executions_" + YYYY.toString() + "_" + MM.toString();
-      const attrValue = data[ruleType];
-
-      submitAttrUtil(attrName, attrValue);
-    }
-  };
 
   const trackRuleExecutions = useCallback((data, YYYY, MM) => {
     for (const ruleType in data) {
@@ -58,11 +48,9 @@ const RuleExecutionsSyncer = () => {
 
           if (user?.loggedIn) {
             if (storageEC?.[currentYYYY]?.[currentMM]) {
-              sendAsAttribute(storageEC?.[currentYYYY]?.[currentMM], currentYYYY, currentMM);
               stableWriteStuffToDB(currentMM, currentYYYY, storageEC[currentYYYY][currentMM]);
             }
             if (storageEC?.[prevYYYY]?.[prevMM]) {
-              sendAsAttribute(storageEC?.[prevYYYY]?.[prevMM], prevYYYY, prevMM);
               stableWriteStuffToDB(prevMM, prevYYYY, storageEC[prevYYYY][prevMM]);
             }
           }
