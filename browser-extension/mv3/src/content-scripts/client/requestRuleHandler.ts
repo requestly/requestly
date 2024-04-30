51e9a93716750ebf97d92b0c34df6592674d18bf
Nafees Nehar <nafees87n@gmail.com>
2024-04-30 13:28:42 +0530

    [ENGG-1685] feat: MV3 cache rules on page without using Function (#1639)
    * remove unused code
    
    * added method to cacheRules on page
    
    * fix: cache general json instead of rules

--- browser-extension/mv3/src/content-scripts/client/requestRuleHandler.ts
+++ browser-extension/mv3/src/content-scripts/client/requestRuleHandler.ts
@@ -1,7 +1,6 @@
 import { getEnabledRules, onRuleOrGroupChange } from "common/rulesStore";
 import { RequestRulePair, RuleType } from "common/types";
-import { executeDynamicScriptOnPage } from "../utils";
-import { PUBLIC_NAMESPACE } from "common/constants";
+import { cacheJsonOnPage } from "../../utility-scripts/cacheJson/cacheJsonUtils";
 
 const cacheRequestRules = async () => {
   const requestRules = await getEnabledRules(RuleType.REQUEST);
@@ -10,21 +9,16 @@ const cacheRequestRules = async () => {
     return;
   }
 
-  executeDynamicScriptOnPage(
-    `
-    window.${PUBLIC_NAMESPACE}=window.${PUBLIC_NAMESPACE}||{};
-    window.${PUBLIC_NAMESPACE}.requestRules=${JSON.stringify(
-      requestRules.map((rule) => {
-        const requestRulePair = rule.pairs[0] as RequestRulePair;
-        return {
-          id: rule.id,
-          source: requestRulePair.source,
-          request: requestRulePair.request,
-        };
-      })
-    )};
-    `
-  );
+  cacheJsonOnPage({
+    requestRules: requestRules.map((rule) => {
+      const requestRulePair = rule.pairs[0] as RequestRulePair;
+      return {
+        id: rule.id,
+        source: requestRulePair.source,
+        request: requestRulePair.request,
+      };
+    }),
+  });
 };
 
 export const initRequestRuleHandler = () => {
--- browser-extension/mv3/src/utility-scripts/executeScript.js
+++
@@ -1,7 +0,0 @@
-const params = JSON.parse(document.currentScript.dataset.params);
-const { code } = params;
-
-if (code) {
-  const functionFromString = new Function(code);
-  functionFromString();
-}
