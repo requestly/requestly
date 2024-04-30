f7bf3a9a506be1e47144d2715d0ccd5fc20ab7bb
Nafees Nehar <nafees87n@gmail.com>
2024-04-30 21:07:29 +0530

    added extension enabled check in content script

--- browser-extension/mv3/src/content-scripts/client/index.ts
+++ browser-extension/mv3/src/content-scripts/client/index.ts
@@ -3,11 +3,16 @@ import { initRuleExecutionHandler } from "./ruleExecution";
 import { initSessionRecording } from "./sessionRecorder";
 import { initResponseRuleHandler } from "./responseRuleHandler";
 import { initRequestRuleHandler } from "./requestRuleHandler";
+import { isExtensionEnabled } from "../../service-worker/services/utils";
 
 if (document.doctype?.name === "html" || document.contentType?.includes("html")) {
-  chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.HANDSHAKE_CLIENT });
-  initSessionRecording();
-  initRuleExecutionHandler();
-  initResponseRuleHandler();
-  initRequestRuleHandler();
+  isExtensionEnabled().then((isExtensionStatusEnabled) => {
+    if (isExtensionStatusEnabled) {
+      chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.HANDSHAKE_CLIENT });
+      initSessionRecording();
+      initRuleExecutionHandler();
+      initResponseRuleHandler();
+      initRequestRuleHandler();
+    }
+  });
 }
