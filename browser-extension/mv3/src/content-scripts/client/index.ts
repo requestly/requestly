7afa70a9aece365b9dcdf513ce75977129533dab
Nafees Nehar <nafees87n@gmail.com>
2024-04-30 21:24:06 +0530

    [ENGG-1557] fix: disable extension in MV3 for all rule types (#1584)
    * fix: extension enabled check
    
    * added check for extensionEnabled in response request rule
    
    * promisified runtime message response
    
    * address review comments
    
    * fix: client replay init check
    
    * chore: remove the vulnerable web-ext dev dependency (#1628)
    
    * chore: move web-ext to global npm
    
    * chore: small vulnerability fixes
    
    * chore: fix conflicting dependencies in common code
    
    * chore: move global web-ext install command to browser-extension/install.sh
    
    * chore: remove redundant .nvmrc
    
    * [ENGG-1625] feat:  MV3 - stitch sessions across navigation (#1631)
    
    * fix: auto recording
    
    * added stop recording function
    
    * added stop notification
    
    * added manual recording support
    
    * fix: session recording notification
    
    * added manual mode widget
    
    * added auto recording widget
    
    * fix: hide auto widget
    
    * added notify widget
    
    * added session replay toast
    
    * implemented tabService
    
    * refactor: tabservice file
    
    * connected tabService with session replay
    
    * cache session config
    
    * fix: do not show recording widget for all pages
    
    * added sessionRecorderState
    
    * feat: stitching across sessions
    
    * added recording mode
    
    * fix: recordingMode
    
    * stop recording on all tabs
    
    * handled prerendered pages
    
    * fix: chrome types
    
    * replaced tabs.sendMessage with tabService
    
    * fix: log
    
    * add comment
    
    * fix: add check for frameId
    
    * fix:
    
    * fix: wait for client page load
    
    * fix: stitching
    
    * remove sending message from tab service
    
    * fix: console log
    
    * fix: file naming
    
    * fix: used auto recording enum
    
    * fix: removed getInstance method
    
    * fix: package-lock
    
    * added extension enabled check in content script
    
    * fix: extension enabled check
    
    ---------
    
    Co-authored-by: Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>

--- browser-extension/mv3/src/content-scripts/client/index.ts
+++ browser-extension/mv3/src/content-scripts/client/index.ts
@@ -3,11 +3,16 @@ import { initRuleExecutionHandler } from "./ruleExecution";
 import { initSessionRecording } from "./sessionRecorder";
 import { initResponseRuleHandler } from "./responseRuleHandler";
 import { initRequestRuleHandler } from "./requestRuleHandler";
+import { Variable, getVariable } from "../../service-worker/variable";
 
 if (document.doctype?.name === "html" || document.contentType?.includes("html")) {
-  chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.HANDSHAKE_CLIENT });
-  initSessionRecording();
-  initRuleExecutionHandler();
-  initResponseRuleHandler();
-  initRequestRuleHandler();
+  getVariable<boolean>(Variable.IS_EXTENSION_ENABLED, true).then((isExtensionStatusEnabled) => {
+    if (isExtensionStatusEnabled) {
+      chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.HANDSHAKE_CLIENT });
+      initSessionRecording();
+      initRuleExecutionHandler();
+      initResponseRuleHandler();
+      initRequestRuleHandler();
+    }
+  });
 }
