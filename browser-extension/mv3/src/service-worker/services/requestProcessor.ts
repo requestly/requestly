85b36d3aebbe182f88573e3a2f24d87b69712e6b
Nafees Nehar <nafees87n@gmail.com>
2024-05-08 23:50:49 +0530

    added predefined function session rule

--- browser-extension/mv3/src/service-worker/services/requestProcessor.ts
+++ browser-extension/mv3/src/service-worker/services/requestProcessor.ts
@@ -16,11 +16,13 @@ class RequestProcessor {
   private cachedRules: Record<string, Rule[]> = {
     redirectRules: [],
     replaceRules: [],
+    headerRules: [],
   };
 
   private updateCachedRules = async () => {
     this.cachedRules.redirectRules = await getEnabledRules(RuleType.REDIRECT);
     this.cachedRules.replaceRules = await getEnabledRules(RuleType.REPLACE);
+    this.cachedRules.headerRules = await getEnabledRules(RuleType.HEADERS);
   };
 
   constructor() {
@@ -121,12 +123,85 @@ class RequestProcessor {
     });
   };
 
+  private handlePredefinedFunctions = async (tabId: number, requestDetails: AJAXRequestDetails) => {
+    const matchedRule = this.findMatchingRule(this.cachedRules.headerRules, requestDetails.url);
+
+    if (!matchedRule) {
+      return;
+    }
+
+    const headerKeyValueMap: Record<"Response" | "Request", Record<string, string>> = {
+      Request: {},
+      Response: {},
+    };
+
+    matchedRule.pairs.forEach((pair) => {
+      if (matchSourceUrl(pair.source, requestDetails.url)) {
+        if (pair.modifications?.Request?.length) {
+          pair.modifications.Request.forEach((header) => {
+            if (header.value === "rq_request_initiator_origin()") {
+              headerKeyValueMap.Request[header.header] = requestDetails.initiatorDomain;
+            }
+          });
+        }
+
+        if (pair.modifications?.Response?.length) {
+          pair.modifications.Response.forEach((header) => {
+            if (header.value === "rq_request_initiator_origin()") {
+              headerKeyValueMap.Response[header.header] = requestDetails.initiatorDomain;
+            }
+          });
+        }
+      }
+    });
+
+    const ruleAction: {
+      requestHeaders?: chrome.declarativeNetRequest.RuleAction["requestHeaders"];
+      responseHeaders?: chrome.declarativeNetRequest.RuleAction["responseHeaders"];
+    } = {};
+
+    if (Object.keys(headerKeyValueMap.Request).length) {
+      ruleAction.requestHeaders = Object.entries(headerKeyValueMap.Request).map(([header, value]) => ({
+        header,
+        value,
+        operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+      }));
+    }
+
+    if (Object.keys(headerKeyValueMap.Response).length) {
+      ruleAction.responseHeaders = Object.entries(headerKeyValueMap.Response).map(([header, value]) => ({
+        header,
+        value,
+        operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+      }));
+    }
+
+    if (!Object.keys(ruleAction).length) {
+      return;
+    }
+
+    await this.updateRequestSpecificRules(tabId, requestDetails.url, {
+      action: {
+        ...ruleAction,
+        type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+      },
+      condition: {
+        urlFilter: `|${requestDetails.url}|`,
+        resourceTypes: [chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST],
+        tabIds: [tabId],
+        requestMethods: [requestDetails.method.toLowerCase() as chrome.declarativeNetRequest.RequestMethod],
+        excludedInitiatorDomains: ["requestly.io", "requestly.com"],
+      },
+    });
+  };
+
   onBeforeAJAXRequest = async (tabId: number, requestDetails: AJAXRequestDetails): Promise<void> => {
     if (Object.values(this.cachedRules).every((rules) => rules.length === 0)) {
       return;
     }
 
     await this.forwardIgnoredHeadersOnRedirect(tabId, requestDetails);
+    await this.handlePredefinedFunctions(tabId, requestDetails);
   };
 }
 
