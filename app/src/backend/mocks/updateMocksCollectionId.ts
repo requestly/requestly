d1ddf1350aaf95b5f0486f96879d3e503114f284
Rohan Mathur <mathurrohan04@gmail.com>
2024-05-13 10:12:26 +0600

    [ENGG-1646] feat: collection support for mocks (#1634)
    * add MockCollectionModal
    
    * chore: update types
    
    * add `createMockCollection` controller
    
    * add create new collection cta
    
    * add collection modal on mocks listing
    
    * fix: fetch mocks
    
    * chore: mock metadata
    
    * render collections
    
    * add `updateMockCollection` controller
    
    * rename
    
    * add update bulk mock collection id controller
    
    * add delete mocks controller
    
    * cleanup
    
    * fix: delete mock
    
    * add deleteMockCollection
    
    * add deleteMocks
    
    * add update and delete collection modals
    
    * fix
    
    * add row actions
    
    * fix: toggle modal
    
    * rename createCollection
    
    * rename deleteCollectionModal
    
    * rename createCollectionModal
    
    * rename
    
    * fix: bulk update collections
    
    * add comment
    
    * refactor
    
    * add update mock collection modal
    
    * fix: row expand
    
    * chore: add comment
    
    * fix: style
    
    * refactor: delete mock modal
    
    * feat: add star mock suuport
    
    * fix: style
    
    * chore: fix type
    
    * add table filters
    
    * fix: all filter
    
    * chore: update analytics
    
    * [ENGG-1686] refactor: mocks feature (#1648)
    
    * fix: mock picker modal
    
    * add CreateCollectionModalWrapper
    
    * refactor: delete modal
    
    * refactor: delete mock modal
    
    * rebase
    
    * fix: force re-renders
    
    * update mock collection modal
    
    * cleanup
    
    * fix: import
    
    * fix: force render
    
    * cleanup
    
    * fix: empty actions
    
    * refactor: mock uploader modal
    
    * refactor: new file modal
    
    * fix: mock name overflow
    
    * fix: star option
    
    * cleanup
    
    * fix: create mock from picker modal
    
    * add comment
    
    * fix: toast
    
    * address review comments
    
    * refactor
    
    * fix: modal header
    
    * refactor
    
    * refactor
    
    * fix: delete collection
    
    * fix: select option
    
    * fix: style
    
    * fix: style
    
    * refactor
    
    * refactor: mocks -> mockRecords
    
    * fix: mock description
    
    * add remove mock from collection action
    
    * fix: delete collection
    
    * fix: created by
    
    * refactor
    
    * fix: search
    
    * fix: deps
    
    * fix: filters
    
    * fix: actions
    
    * fix: style
    
    * fix: typo
    
    * fix: collection creation
    
    * fix: row style
    
    * fix: action order
    
    * cleanup
    
    * refactor
    
    * fix: collection selection
    
    * chore: add comment
    
    * refactor
    
    * refactor
    
    * refactor: delete mocks
    
    * chore: fix types
    
    * refactor: for bulk onsuccess
    
    * fix: style
    
    Signed-off-by: rohanmathur91 <mathurrohan04@gmail.com>
    
    * add bulk actions
    
    Signed-off-by: rohanmathur91 <mathurrohan04@gmail.com>
    
    * fix: style
    
    * fix: style
    
    * fix: rerender issue
    
    * fix: collection order
    
    * fix: onSuccess
    
    * fix: style
    
    * fix: style
    
    * fix: style
    
    * fix: style
    
    * fix: label
    
    * fix: mock actions
    
    * fix: action
    
    * refactor
    
    * cleanup
    
    * refactor: updateMocksCollectionAction
    
    * refactor: uploadMockAction
    
    * refactor: createNewFileAction
    
    * fix: typo
    
    * fix: typo
    
    * refactor: delete records action
    
    * refactor
    
    * refactor
    
    ---------
    
    Signed-off-by: rohanmathur91 <mathurrohan04@gmail.com>

---
+++ app/src/backend/mocks/updateMocksCollectionId.ts
@@ -0,0 +1,41 @@
+import firebaseApp from "../../firebase";
+import { Timestamp, doc, getFirestore, writeBatch } from "firebase/firestore";
+
+export const updateMocksCollectionId = async (
+  uid: string,
+  mockIds: string[],
+  updatedCollectionId: string
+): Promise<boolean> => {
+  if (!uid) {
+    return null;
+  }
+
+  const result = await updateMocksCollectionIdInFirebase(uid, mockIds, updatedCollectionId);
+  return result;
+};
+
+const updateMocksCollectionIdInFirebase = async (
+  uid: string,
+  mockIds: string[],
+  updatedCollectionId: string
+): Promise<boolean> => {
+  try {
+    const db = getFirestore(firebaseApp);
+    const mocksbatch = writeBatch(db);
+
+    mockIds.forEach((id) => {
+      const mockRef = doc(db, "mocks", id);
+      mocksbatch.set(
+        mockRef,
+        { collectionId: updatedCollectionId, lastUpdatedBy: uid, updatedTs: Timestamp.now().toMillis() },
+        { merge: true }
+      );
+    });
+
+    await mocksbatch.commit();
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
---
+++ app/src/features/mocks/modals/UpdateMocksCollectionModalWrapper/index.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { RQMockCollection, RQMockMetadataSchema } from "components/features/mocksV2/types";
+import { useMocksModalsContext } from "features/mocks/contexts/modals";
+import { UpdateMocksCollectionModal } from "./UpdateMocksCollectionModal";
+import {
+  isRecordMock,
+  isRecordMockCollection,
+} from "features/mocks/screens/mocksList/components/MocksList/components/MocksTable/utils";
+
+export const UpdateMocksCollectionModalWrapper: React.FC<{
+  forceRender: () => void;
+  mocks: RQMockMetadataSchema[];
+}> = ({ mocks, forceRender }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [mocksToBeUpdated, setMocksToBeUpdated] = useState<RQMockMetadataSchema[]>([]);
+  const [onSuccess, setOnSuccess] = useState(() => () => {});
+
+  const { setOpenUpdateMocksCollectionModalAction } = useMocksModalsContext();
+
+  useEffect(() => {
+    const openModal = (records: RQMockMetadataSchema[], onSuccess?: () => void) => {
+      const mocks = records.filter(isRecordMock);
+      setMocksToBeUpdated(mocks);
+      setOnSuccess(() => onSuccess);
+      setIsVisible(true);
+    };
+
+    setOpenUpdateMocksCollectionModalAction(() => openModal);
+  }, [setOpenUpdateMocksCollectionModalAction]);
+
+  const onClose = () => {
+    setIsVisible(false);
+    setMocksToBeUpdated([]);
+  };
+
+  const collections = (mocks.filter((mock) => isRecordMockCollection(mock)) as unknown) as RQMockCollection[];
+
+  return isVisible ? (
+    <UpdateMocksCollectionModal
+      mocks={mocksToBeUpdated}
+      collections={collections}
+      visible={isVisible}
+      toggleModalVisibility={onClose}
+      onSuccess={() => {
+        forceRender();
+        onSuccess?.();
+      }}
+    />
+  ) : null;
+};
