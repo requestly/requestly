fd5556acdb25faf7b7d55bcda91aa73660c7715c
Sahil Gupta <sahil865gupta@gmail.com>
2024-05-13 18:40:13 +0530

    added v3 migrations

--- app/src/modules/extension/utils.ts
+++ app/src/modules/extension/utils.ts
@@ -12,41 +12,34 @@ enum RuleMigrationChange {
 }
 
 export const migrateAllRulesToMV3 = (rules: Rule[], currentWorkspaceId: string): Rule[] => {
-  if (rules.length === 0) {
-    return rules;
-  }
+  console.log("[Debug] Before Migration", { rules });
 
-  const migrationStatus = getMV3MigrationStatus();
-  const workspaceId = currentWorkspaceId ? currentWorkspaceId : null;
-
-  // console.log("!!!debug", "migration Staus", migrationStatus);
-  // if (migrationStatus?.[workspaceId ?? "private"]?.rulesMigrated) {
-  //   return rules;
-  // }
-
-  const rulesMigrationLogs: Record<string, any>[] = [];
+  const rulesMigrationLogs: Record<string, any> = {};
 
   const migratedRules = rules.map((rule) => {
     const { rule: migratedRule, ruleMigrationLogs } = migrateRuleToMV3(rule);
 
     if (ruleMigrationLogs) {
-      rulesMigrationLogs.push(ruleMigrationLogs);
+      rulesMigrationLogs[migratedRule.id] = ruleMigrationLogs;
     }
 
     return migratedRule;
   });
 
-  console.log("!!!debug", "rulesMigeationLogs all rules", rulesMigrationLogs);
+  const workspaceId = currentWorkspaceId ? currentWorkspaceId : null;
+  const migrationData = getMV3MigrationData() || {};
+  const currentWorkspaceMigrationData = migrationData[workspaceId ? workspaceId : "private"];
+
+  console.log("[Debug] After Migration", { currentWorkspaceMigrationData, rulesMigrationLogs, migratedRules });
 
   StorageService(GLOBAL_CONSTANTS.APP_MODES.EXTENSION)
     .saveMultipleRulesOrGroups(migratedRules, { workspaceId: workspaceId })
     .then(() => {
       // StorageService(GLOBAL_CONSTANTS.APP_MODES.EXTENSION).saveRecord({ [APP_CONSTANTS.LAST_UPDATED_TS]: Date.now() });
-      saveMV3MigrationStatus({
-        ...migrationStatus,
+      saveMV3MigrationData({
+        ...migrationData,
         [workspaceId ? workspaceId : "private"]: {
-          rulesMigrated: true,
-          rulesMigrationLogs,
+          rulesMigrationLogs: { ...currentWorkspaceMigrationData?.rulesMigrationLogs, ...rulesMigrationLogs },
         },
       });
     });
@@ -54,34 +47,46 @@ export const migrateAllRulesToMV3 = (rules: Rule[], currentWorkspaceId: string):
   return migratedRules;
 };
 
-export const migrateRuleToMV3 = (rule: Rule) => {
-  const ruleMigrationLogs: Record<string, any> = {
-    id: rule.id,
-    migrationChanges: [],
-  };
+export const migrateRuleToMV3 = (rule: Rule): { rule: Rule; ruleMigrationLogs: any } => {
+  if (rule?.version === "3.0.0") {
+    console.log("[Debug] Rule already migrated to v3.0.0", { rule });
+    return {
+      rule,
+      ruleMigrationLogs: null,
+    };
+  }
+
+  const ruleMigrationLogs: Record<string, any>[] = [];
 
   rule.pairs.forEach((pair) => {
     const pathMigrationStatus = migratePathOperator(pair.source);
     if (pathMigrationStatus) {
-      ruleMigrationLogs.migrationChanges.push(pathMigrationStatus);
+      console.log("!!!debug", "pathMigration Status", pathMigrationStatus);
+      ruleMigrationLogs.push(pathMigrationStatus);
     }
 
     const pageUrlMigrationStatus = migratePageURLtoPageDomain(pair.source);
     if (pageUrlMigrationStatus) {
-      ruleMigrationLogs.migrationChanges.push(pageUrlMigrationStatus);
+      console.log("!!!debug", "pageURLMigrationStatus", pageUrlMigrationStatus);
+      ruleMigrationLogs.push(pageUrlMigrationStatus);
     }
   });
 
+  const finalRule = {
+    ...rule,
+    extensionRules: parseDNRRules(rule),
+    version: "3.0.0",
+  };
+
+  console.log("[Debug] Migrated Rule", { beforeRule: rule, finalRule });
+
   return {
-    rule: {
-      ...rule,
-      extensionRules: parseDNRRules(rule),
-    },
-    ruleMigrationLogs: ruleMigrationLogs.migrationChanges.length ? ruleMigrationLogs : null,
+    rule: finalRule,
+    ruleMigrationLogs: ruleMigrationLogs.length ? ruleMigrationLogs : null,
   };
 };
 
-export const getMV3MigrationStatus = () => {
+export const getMV3MigrationData = () => {
   const mv3MigrationStatus = window.localStorage.getItem(MV3_MIGRATION_DATA);
   try {
     return JSON.parse(mv3MigrationStatus);
@@ -90,7 +95,7 @@ export const getMV3MigrationStatus = () => {
   }
 };
 
-export const saveMV3MigrationStatus = (migrationData: any) => {
+export const saveMV3MigrationData = (migrationData: any) => {
   window.localStorage.setItem(MV3_MIGRATION_DATA, JSON.stringify(migrationData));
 };
 
