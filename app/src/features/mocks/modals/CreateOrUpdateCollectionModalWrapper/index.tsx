535d3b5584335d8e9ddf26811818826164567028
Rohan Mathur <mathurrohan04@gmail.com>
2024-05-13 10:12:26 +0600

    [ENGG-1646] feat: collection support for mocks (#1634)
    * add MockCollectionModal
    
    * chore: update types
    
    * add `createMockCollection` controller
    
    * add create new collection cta
    
    * add collection modal on mocks listing
    
    * fix: fetch mocks
    
    * chore: mock metadata
    
    * render collections
    
    * add `updateMockCollection` controller
    
    * rename
    
    * add update bulk mock collection id controller
    
    * add delete mocks controller
    
    * cleanup
    
    * fix: delete mock
    
    * add deleteMockCollection
    
    * add deleteMocks
    
    * add update and delete collection modals
    
    * fix
    
    * add row actions
    
    * fix: toggle modal
    
    * rename createCollection
    
    * rename deleteCollectionModal
    
    * rename createCollectionModal
    
    * rename
    
    * fix: bulk update collections
    
    * add comment
    
    * refactor
    
    * add update mock collection modal
    
    * fix: row expand
    
    * chore: add comment
    
    * fix: style
    
    * refactor: delete mock modal
    
    * feat: add star mock suuport
    
    * fix: style
    
    * chore: fix type
    
    * add table filters
    
    * fix: all filter
    
    * chore: update analytics
    
    * [ENGG-1686] refactor: mocks feature (#1648)
    
    * fix: mock picker modal
    
    * add CreateCollectionModalWrapper
    
    * refactor: delete modal
    
    * refactor: delete mock modal
    
    * rebase
    
    * fix: force re-renders
    
    * update mock collection modal
    
    * cleanup
    
    * fix: import
    
    * fix: force render
    
    * cleanup
    
    * fix: empty actions
    
    * refactor: mock uploader modal
    
    * refactor: new file modal
    
    * fix: mock name overflow
    
    * fix: star option
    
    * cleanup
    
    * fix: create mock from picker modal
    
    * add comment
    
    * fix: toast
    
    * address review comments
    
    * refactor
    
    * fix: modal header
    
    * refactor
    
    * refactor
    
    * fix: delete collection
    
    * fix: select option
    
    * fix: style
    
    * fix: style
    
    * refactor
    
    * refactor: mocks -> mockRecords
    
    * fix: mock description
    
    * add remove mock from collection action
    
    * fix: delete collection
    
    * fix: created by
    
    * refactor
    
    * fix: search
    
    * fix: deps
    
    * fix: filters
    
    * fix: actions
    
    * fix: style
    
    * fix: typo
    
    * fix: collection creation
    
    * fix: row style
    
    * fix: action order
    
    * cleanup
    
    * refactor
    
    * fix: collection selection
    
    * chore: add comment
    
    * refactor
    
    * refactor
    
    * refactor: delete mocks
    
    * chore: fix types
    
    * refactor: for bulk onsuccess
    
    * fix: style
    
    Signed-off-by: rohanmathur91 <mathurrohan04@gmail.com>
    
    * add bulk actions
    
    Signed-off-by: rohanmathur91 <mathurrohan04@gmail.com>
    
    * fix: style
    
    * fix: style
    
    * fix: rerender issue
    
    * fix: collection order
    
    * fix: onSuccess
    
    * fix: style
    
    * fix: style
    
    * fix: style
    
    * fix: style
    
    * fix: label
    
    * fix: mock actions
    
    * fix: action
    
    * refactor
    
    * cleanup
    
    * refactor: updateMocksCollectionAction
    
    * refactor: uploadMockAction
    
    * refactor: createNewFileAction
    
    * fix: typo
    
    * fix: typo
    
    * refactor: delete records action
    
    * refactor
    
    * refactor
    
    ---------
    
    Signed-off-by: rohanmathur91 <mathurrohan04@gmail.com>

---
+++ app/src/features/mocks/modals/CreateOrUpdateCollectionModalWrapper/index.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { CreateOrUpdateCollectionModal } from "./CreateOrUpdateCollectionModal";
+import { MockType, RQMockMetadataSchema } from "components/features/mocksV2/types";
+import { useMocksModalsContext } from "features/mocks/contexts/modals";
+
+export const CreateOrUpdateCollectionModalWrapper: React.FC<{ forceRender: () => void }> = ({ forceRender }) => {
+  const [collection, setCollection] = useState<RQMockMetadataSchema>();
+  const [mockType, setMockType] = useState<MockType>();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const { setOpenCollectionModalAction } = useMocksModalsContext();
+
+  useEffect(() => {
+    const openModal = (mockType: MockType, record?: RQMockMetadataSchema) => {
+      setIsVisible(true);
+      setMockType(mockType);
+
+      if (record) {
+        setCollection(record);
+      }
+    };
+
+    setOpenCollectionModalAction(() => openModal);
+  }, [setOpenCollectionModalAction]);
+
+  const onClose = () => {
+    setIsVisible(false);
+    setCollection(undefined);
+    setMockType(undefined);
+  };
+
+  return isVisible ? (
+    <CreateOrUpdateCollectionModal
+      id={collection?.id}
+      name={collection?.name}
+      description={collection?.desc}
+      mockType={mockType}
+      visible={isVisible}
+      toggleModalVisibility={onClose}
+      onSuccess={forceRender}
+    />
+  ) : null;
+};
--- app/src/modules/analytics/events/features/mocksV2/index.ts
+++ app/src/modules/analytics/events/features/mocksV2/index.ts
@@ -1,23 +1,32 @@
 import { trackEvent } from "modules/analytics";
 import { MOCKSV2 } from "../constants";
+import { MockType } from "components/features/mocksV2/types";
 
-export const trackCreateMockEvent = (id: string, type: string, file_type: string, source?: string) => {
+export const trackCreateMockEvent = (
+  id: string,
+  type: string,
+  file_type: string,
+  source: string = "",
+  collectionId: string = ""
+) => {
   const params = {
     version: 2,
     id,
     type,
     file_type,
     source,
+    collectionId,
   };
   trackEvent(MOCKSV2.CREATED, params);
 };
 
-export const trackUpdateMockEvent = (id: string, type: string, file_type: string) => {
+export const trackUpdateMockEvent = (id: string, type: string, file_type: string, collectionId: string = "") => {
   const params = {
     version: 2,
     id,
     type,
     file_type,
+    collectionId,
   };
   trackEvent(MOCKSV2.UPDATED, params);
 };
@@ -30,6 +39,17 @@ export const trackDeleteMockEvent = (id: string, type: string, file_type: string
     file_type,
   };
   trackEvent(MOCKSV2.DELETED, params);
+};
+
+export const trackMockStarToggledEvent = (id: string, type: string, file_type: string, starred: boolean = false) => {
+  const params = {
+    version: 2,
+    id,
+    type,
+    file_type,
+    starred,
+  };
+  trackEvent(MOCKSV2.STAR_TOGGLED, params);
 };
 
 export const trackNewMockButtonClicked = (type: string, source: string) => {
@@ -109,4 +129,40 @@ export const trackMockPasswordSaveError = () => {
 export const trackMockPasswordDeleted = () => {
   const params = {};
   trackEvent(MOCKSV2.MOCK_PASSWORD_DELETED, params);
+};
+
+// mock collections
+export const trackMockCollectionCreated = (
+  source: string,
+  workspaceId: string,
+  workspaceName: string,
+  workspaceMembersCount: number
+) => {
+  const params = { source, workspaceId, workspaceName, workspaceMembersCount };
+  trackEvent(MOCKSV2.MOCK_COLLECTION_CREATED, params);
+};
+
+export const trackMockCollectionUpdated = (
+  source: string,
+  workspaceId: string,
+  workspaceName: string,
+  workspaceMembersCount: number
+) => {
+  const params = { source, workspaceId, workspaceName, workspaceMembersCount };
+  trackEvent(MOCKSV2.MOCK_COLLECTION_UPDATED, params);
+};
+
+export const trackMockCollectionDeleted = (
+  source: string,
+  num_mocks: number,
+  type: "delete_only_collection" | "delete_mocks_and_collection"
+) => {
+  const params = { source, num_mocks, type };
+  trackEvent(MOCKSV2.MOCK_COLLECTION_DELETED, params);
+};
+
+// bulk actions
+export const trackMocksListBulkActionPerformed = (action: string, mockType: MockType) => {
+  const params = { action, mockType };
+  trackEvent(MOCKSV2.MOCKS_LIST_BULK_ACTION_PERFORMED, params);
 };
