c9ce4d54e396dd6a083eff33e65558698b1de748
RuntimeTerror10 <parthb.git@gmail.com>
2024-05-16 01:49:55 +0530

    chore: remove implicit test rule feature flag

--- app/src/components/features/rules/TestThisRule/hooks/InitImplicitWidgetConfigHandler.ts
+++ app/src/components/features/rules/TestThisRule/hooks/InitImplicitWidgetConfigHandler.ts
@@ -1,35 +1,25 @@
 import { useEffect } from "react";
-import { StorageService } from "init";
 import { useSelector } from "react-redux";
 import { getAppMode } from "store/selectors";
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
-import { useFeatureValue } from "@growthbook/growthbook-react";
 import { getImplicitRuleTestingWidgetConfig, updateImplictRuleTestingWidgetConfig } from "features/settings";
 import { isFeatureCompatible } from "utils/CompatibilityUtils";
 import APP_CONSTANTS from "config/constants";
-import { isNull } from "lodash";
 
 export const InitImplicitWidgetConfigHandler = () => {
   const appMode = useSelector(getAppMode);
-  const implicitRuleTestingFlag = useFeatureValue("implicit_test_this_rule", null);
 
   useEffect(() => {
     if (isFeatureCompatible(APP_CONSTANTS.FEATURES.IMPLICIT_TEST_THIS_RULE)) {
-      if (!isNull(implicitRuleTestingFlag)) {
-        if (implicitRuleTestingFlag.enabled) {
-          getImplicitRuleTestingWidgetConfig(appMode).then((record) => {
-            if (record === undefined) {
-              updateImplictRuleTestingWidgetConfig(appMode, {
-                enabled: true,
-                visibility: GLOBAL_CONSTANTS.IMPLICIT_RULE_TESTING_WIDGET_VISIBILITY.ALL,
-                ruleTypes: [],
-              });
-            }
+      getImplicitRuleTestingWidgetConfig(appMode).then((record) => {
+        if (record === undefined) {
+          updateImplictRuleTestingWidgetConfig(appMode, {
+            enabled: true,
+            visibility: GLOBAL_CONSTANTS.IMPLICIT_RULE_TESTING_WIDGET_VISIBILITY.ALL,
+            ruleTypes: [],
           });
-        } else {
-          StorageService(appMode).removeRecord(GLOBAL_CONSTANTS.STORAGE_KEYS.IMPLICIT_RULE_TESTING_WIDGET_CONFIG);
         }
-      }
+      });
     }
-  }, [appMode, implicitRuleTestingFlag]);
+  }, [appMode]);
 };
