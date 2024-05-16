2ee12039a5132dac1630392f90b84b5e79117335
Sahil Gupta <sahil865gupta@gmail.com>
2024-05-16 12:29:07 +0530

    [ENGG-1567] chore: add extra event properties for migration (#1695)
    * add extra event properties for migration
    
    * updated app version

--- app/src/modules/analytics/events/migrations.ts
+++ app/src/modules/analytics/events/migrations.ts
@@ -4,8 +4,20 @@ export const trackMv3MigrationStarted = (total_count: number) => {
   trackEvent("mv3_migration_started", { total_count });
 };
 
-export const trackMv3MigrationRulesMigrated = (total_count: number, migrated_count: number) => {
-  trackEvent("mv3_migration_rules_migrated", { total_count, migrated_count });
+export const trackMv3MigrationRulesMigrated = (
+  total_count: number,
+  migrated_count: number,
+  impacted_count: number,
+  path_impacted_count: number,
+  page_url_impacted_count: number
+) => {
+  trackEvent("mv3_migration_rules_migrated", {
+    total_count,
+    migrated_count,
+    impacted_count,
+    path_impacted_count,
+    page_url_impacted_count,
+  });
 };
 
 export const trackMv3MigrationCompleted = (total_count: number, migrated_count: number) => {
