6d87bbe91c3ea67e76c84369f5a5c8272f942423
RuntimeTerror10 <parthb.git@gmail.com>
2024-05-21 01:04:24 +0530

    checklist modal and screen component

---
+++ app/src/features/Incentives/index.ts
@@ -0,0 +1,2 @@
+export { IncentiveTasksListScreen } from "./components/IncentiveTasksListScreen/IncentiveTasksListScreen";
+export { IncentiveTasksListModal } from "./components/IncentiveTasksListModal/IncentiveTasksListModal";
