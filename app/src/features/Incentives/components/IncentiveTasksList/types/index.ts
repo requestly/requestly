286505498cd5f4ca85fc1a99b92d970a344c2973
RuntimeTerror10 <parthb.git@gmail.com>
2024-05-20 22:45:24 +0530

    checklist UI components

---
+++ app/src/features/Incentives/components/IncentiveTasksList/types/index.ts
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+export enum IncentiveTask {
+  CREATE_FIRST_RULE = "create_first_rule",
+  CREATE_PREMIUM_RULE = "create_premium_rule",
+  CREATE_A_TEAM_WORKSPACE = "create_a_team_workspace",
+  CREATE_API_MOCK = "create_api_mock",
+  RECORD_A_SESSION = "record_a_session",
+  RATE_ON_CHROME_STORE = "rate_on_chrome_store",
+}
+
+export type IncentiveTaskListItem = {
+  id: IncentiveTask;
+  title: string;
+  icon: ReactNode;
+  description: string;
+  helpLink: {
+    text: string;
+    url: string;
+  };
+};
