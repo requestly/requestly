eaa47d035f0bfeb51542b5b9561fe766b7ad035b
RuntimeTerror10 <parthb.git@gmail.com>
2024-05-21 01:04:24 +0530

    checklist modal and screen component

--- app/src/features/Incentives/components/IncentiveTasksList/IncentiveTasksList.tsx
+++ app/src/features/Incentives/components/IncentiveTasksList/IncentiveTasksList.tsx
@@ -3,21 +3,23 @@ import { CreditsProgressBar } from "../CreditsProgressbar/CreditsProgessbar";
 import { IncentiveSectionHeader } from "../IncentiveSectionHeader";
 import { incentiveTasksList } from "./constants/incentiveTasks";
 import { PiCaretDownBold } from "@react-icons/all-files/pi/PiCaretDownBold";
+import { MdCheckCircle } from "@react-icons/all-files/md/MdCheckCircle";
 import { IncentiveTaskListItem } from "./types";
 import "./incentiveTasksList.scss";
 
 export const IncentiveTasksList = () => {
   const renderTaskHeader = (task: IncentiveTaskListItem) => {
     // TODO: HANDLE COMPLETE TASKS
+    const isTaskCompleted = false;
     return (
-      <div className="incentive-task-header">
+      <div className={`incentive-task-header ${isTaskCompleted ? "completed-task" : ""}`}>
         <div className="incentive-task-title-container">
-          {task.icon}
+          {isTaskCompleted ? <MdCheckCircle className="task-checked-icon" /> : task.icon}
           <span className="incentive-task-title">{task.title}</span>
         </div>
         <div className="task-credit-value">
           {/* TODO: GET CREDIT VALUE FROM MAP */}
-          <span>$10</span> Credits
+          <span>$10</span> {isTaskCompleted ? "Credits earned" : "Credits"}
         </div>
       </div>
     );
@@ -26,7 +28,9 @@ export const IncentiveTasksList = () => {
   return (
     <div className="incentive-tasks-list-container">
       <IncentiveSectionHeader title="Complete onboarding and earn $65 Free credits" />
-      <CreditsProgressBar />
+      <div className="mt-24">
+        <CreditsProgressBar />
+      </div>
       <div className="incentive-tasks-list">
         <Collapse
           className="incentive-tasks-list-collapse"
