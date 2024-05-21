fc96f24f3319ec15ca91373b57795b9ed3ad44e1
RuntimeTerror10 <parthb.git@gmail.com>
2024-05-21 15:59:40 +0530

    add actions and style fix

--- app/src/features/Incentives/components/IncentiveTasksList/constants/incentiveTasks.tsx
+++ app/src/features/Incentives/components/IncentiveTasksList/constants/incentiveTasks.tsx
@@ -3,6 +3,10 @@ import { MdPlaylistAdd } from "@react-icons/all-files/md/MdPlaylistAdd";
 import { MdOutlineDiversity1 } from "@react-icons/all-files/md/MdOutlineDiversity1";
 import { MdOutlineDns } from "@react-icons/all-files/md/MdOutlineDns";
 import { PiRecordFill } from "@react-icons/all-files/pi/PiRecordFill";
+import { NewRuleButtonWithDropdown } from "features/rules/screens/rulesList/components/RulesList/components";
+import { Button } from "antd";
+import { redirectToMocks, redirectToSessionRecordingHome } from "utils/RedirectionUtils";
+import { actions } from "store";
 
 export const incentiveTasksList: IncentiveTaskListItem[] = [
   {
@@ -11,6 +15,7 @@ export const incentiveTasksList: IncentiveTaskListItem[] = [
     description:
       "Use rules to intercept & modify network requests, headers, API requests, inject scripts & much more. Upon creating your first rule you will earn $25 Free credits for professional plan.",
     icon: <MdPlaylistAdd />,
+    action: () => <NewRuleButtonWithDropdown />,
     helpLink: {
       text: "Learn how to create Rules",
       url: "https://docs.automatio.co/rules",
@@ -19,8 +24,10 @@ export const incentiveTasksList: IncentiveTaskListItem[] = [
   {
     id: IncentiveTask.CREATE_PREMIUM_RULE,
     title: "Create other rules",
-    description: "",
+    description:
+      "Use rules to intercept & modify network requests, headers, API requests, inject scripts & much more. Upon creating your first rule you will earn $25 Free credits for professional plan.",
     icon: <MdPlaylistAdd />,
+    action: () => <NewRuleButtonWithDropdown />,
     helpLink: {
       text: "Learn how to create Rules",
       url: "https://docs.automatio.co/rules",
@@ -29,8 +36,33 @@ export const incentiveTasksList: IncentiveTaskListItem[] = [
   {
     id: IncentiveTask.CREATE_A_TEAM_WORKSPACE,
     title: "Create a Team Workspace ",
-    description: "",
+    description:
+      "Team Workspaces let you share your debugging workflows with your teammates in real time. Everyone can collaborate on things like Rules, Mock APIs and Session replays.",
     icon: <MdOutlineDiversity1 />,
+    action: ({ dispatch }) => (
+      <Button
+        type="primary"
+        onClick={() =>
+          dispatch(
+            // @ts-ignore
+            actions.toggleActiveModal({
+              modalName: "createWorkspaceModal",
+              newValue: true,
+              newProps: {
+                callback: () => {
+                  // @ts-ignore
+                  dispatch(actions.updateJoinWorkspaceCardVisible(false));
+                },
+                source: "join_workspace_card",
+              },
+            })
+          )
+        }
+      >
+        Create a new workspace
+      </Button>
+    ),
+
     helpLink: {
       text: "Learn how to create Team Workspace",
       url: "https://docs.automatio.co/team-workspace",
@@ -39,8 +71,14 @@ export const incentiveTasksList: IncentiveTaskListItem[] = [
   {
     id: IncentiveTask.CREATE_API_MOCK,
     title: "Create an API Mock",
-    description: "",
+    description: "Create mocks for your APIs with different status codes, delay, response headers or body",
     icon: <MdOutlineDns />,
+    action: ({ navigate }) => (
+      <Button type="primary" onClick={() => redirectToMocks(navigate)}>
+        Create a mock API
+      </Button>
+    ),
+
     helpLink: {
       text: "Learn how to create API Mock",
       url: "https://docs.automatio.co/api-mock",
@@ -49,8 +87,14 @@ export const incentiveTasksList: IncentiveTaskListItem[] = [
   {
     id: IncentiveTask.RECORD_A_SESSION,
     title: "Record a session",
-    description: "Record a session and earn $5 free credits",
+    description:
+      "Session replays allows you to capture, report, and debug errors with the power of session replay and network & console logs.",
     icon: <PiRecordFill />,
+    action: ({ navigate }) => (
+      <Button type="primary" onClick={() => redirectToSessionRecordingHome(navigate)}>
+        Record a session
+      </Button>
+    ),
     helpLink: {
       text: "Learn how to record a session",
       url: "https://docs.automatio.co/session-recording",
