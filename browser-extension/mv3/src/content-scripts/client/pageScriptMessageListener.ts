46ce0a2ce34a31419c87940158f07593a43e6235
Nafees Nehar <nafees87n@gmail.com>
2024-05-22 02:40:27 +0530

    fix: CSP error dynamic error

--- browser-extension/mv3/src/content-scripts/client/pageScriptMessageListener.ts
+++ browser-extension/mv3/src/content-scripts/client/pageScriptMessageListener.ts
@@ -27,6 +27,17 @@ export const initPageScriptMessageListener = () => {
           );
         });
         break;
+      case EXTENSION_MESSAGES.ON_CSP_ERROR:
+        chrome.runtime.sendMessage(event.data, () => {
+          window.postMessage(
+            {
+              source: "requestly:client",
+              action: CLIENT_MESSAGES.ON_CSP_ERROR_PROCESSED,
+            },
+            window.location.href
+          );
+        });
+        break;
     }
   });
 };
--- browser-extension/mv3/src/service-worker/services/requestProcessor/index.ts
+++ browser-extension/mv3/src/service-worker/services/requestProcessor/index.ts
@@ -3,6 +3,7 @@ import { forwardHeadersOnRedirect } from "./handleHeadersOnRedirect";
 import { handleInitiatorDomainFunction } from "./handleInitiatorDomainFunction";
 import rulesStorageService from "../../../rulesStorageService";
 import { RuleType } from "common/types";
+import { handleCSPError } from "./handleCSPError";
 
 class RequestProcessor {
   constructor() {}
@@ -17,12 +18,25 @@ class RequestProcessor {
     const redirectReplaceRules = enabledRules.filter(
       (rule) => rule.ruleType === RuleType.REDIRECT || rule.ruleType === RuleType.REPLACE
     );
-
     const headerRules = enabledRules.filter((rule) => rule.ruleType === RuleType.HEADERS);
 
     await forwardHeadersOnRedirect(tabId, requestDetails, redirectReplaceRules);
 
     await handleInitiatorDomainFunction(tabId, requestDetails, headerRules);
+
+    // console.log("!!!debug", "headers", requestDetails, requestDetails.requestHeaders);
+  };
+
+  onCSPError = async (tabId: number, requestDetails: AJAXRequestDetails): Promise<void> => {
+    const enabledRules = await rulesStorageService.getEnabledRules();
+
+    if (enabledRules.length === 0) {
+      return;
+    }
+
+    await handleCSPError(tabId, requestDetails);
+
+    // console.log("!!!debug", "headers", requestDetails, requestDetails.requestHeaders);
   };
 }
 
