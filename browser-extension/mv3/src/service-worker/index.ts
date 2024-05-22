3716fce54fac462e0fa8898e814ffbd35297e43e
Sahil Gupta <sahil865gupta@gmail.com>
2024-05-22 18:50:48 +0530

    init client.js asap (#1727)

--- browser-extension/mv3/src/service-worker/index.ts
+++ browser-extension/mv3/src/service-worker/index.ts
@@ -1,3 +1,4 @@
+import { initClientHandler } from "./services/clientHandler";
 import { registerCommands } from "./services/commands";
 import { initContextMenu } from "./services/contextMenu";
 import { handleInstallUninstall } from "./services/installUninstall";
@@ -7,6 +8,7 @@ import { initWebRequestInterceptor } from "./services/webRequestInterceptor";
 
 // initialize
 (async () => {
+  initClientHandler();
   registerCommands();
   handleInstallUninstall();
   initRulesManager();
