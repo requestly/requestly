0f073d3d4780b2e81a66a33c31a3be356e76ca23
RuntimeTerror10 <parthb.git@gmail.com>
2024-05-22 13:44:27 +0530

    integrate bot

---
+++ app/src/features/requestBot/index.ts
@@ -0,0 +1,1 @@
+export { RequestBot } from "./RequestBot";
--- app/src/layouts/DashboardLayout/MenuHeader/index.js
+++ app/src/layouts/DashboardLayout/MenuHeader/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Layout, Button, Row, Col, Tooltip, Divider } from "antd";
@@ -8,7 +8,7 @@ import HeaderUser from "./HeaderUser";
 import HeaderText from "./HeaderText";
 import { SearchOutlined } from "@ant-design/icons";
 import { redirectToSettings } from "utils/RedirectionUtils";
-import GitHubButton from "react-github-btn";
+// import GitHubButton from "react-github-btn";
 import { useMediaQuery } from "react-responsive";
 import Settings from "assets/icons/settings.svg?react";
 import LINKS from "config/constants/sub/links";
@@ -21,6 +21,8 @@ import ProductsDropDown from "./ProductsDropDown";
 import PremiumPlanBadge from "./PremiumPlanBadge/PremiumPlanBadge";
 import APP_CONSTANTS from "config/constants";
 import { PlanExpiredBadge } from "./PlanExpiredBadge";
+import { RequestBot } from "features/requestBot";
+import BotIcon from "./assets/bot.svg";
 import "./MenuHeader.css";
 
 const { Header } = Layout;
@@ -39,61 +41,65 @@ const MenuHeader = () => {
   const isPricingOrGoodbyePage = isPricingPage() || isGoodbyePage() || isInvitePage();
   const isPlanExpiredBannerClosed = useSelector(getIsPlanExpiredBannerClosed);
 
+  const [isRequestBotVisible, setIsRequestBotVisible] = useState(false);
+
   //don't show general app header component for editor screens
   const showMenuHeader = () => !PATHS_WITHOUT_HEADER.some((path) => pathname.includes(path));
 
   return showMenuHeader() ? (
-    <Header className="layout-header">
-      <Row wrap={false} align="middle" className="w-full">
-        {!isPricingOrGoodbyePage ? (
-          <Col
-            span={9}
-            flex="1 0 auto"
-            className={appMode === GLOBAL_CONSTANTS.APP_MODES.EXTENSION ? "extension" : "desktop"}
-          >
-            <div className="header-left-section">
-              <WorkspaceSelector />
+    <>
+      <Header className="layout-header">
+        <Row wrap={false} align="middle" className="w-full">
+          {!isPricingOrGoodbyePage ? (
+            <Col
+              span={9}
+              flex="1 0 auto"
+              className={appMode === GLOBAL_CONSTANTS.APP_MODES.EXTENSION ? "extension" : "desktop"}
+            >
+              <div className="header-left-section">
+                <WorkspaceSelector />
 
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={LINKS.YOUTUBE_TUTORIALS}
-                onClick={() => trackTopbarClicked("tutorials")}
-              >
-                Tutorials
-              </a>
-              <ProductsDropDown />
-            </div>
-          </Col>
-        ) : null}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={LINKS.YOUTUBE_TUTORIALS}
+                  onClick={() => trackTopbarClicked("tutorials")}
+                >
+                  Tutorials
+                </a>
+                <ProductsDropDown />
+              </div>
+            </Col>
+          ) : null}
 
-        {appMode !== GLOBAL_CONSTANTS.APP_MODES.EXTENSION && (
-          <Col xs={0} sm={0} md={0} lg={!isPricingOrGoodbyePage ? (isTabletView ? 10 : 8) : 10}>
-            <div className="header-middle-section">
-              <HeaderText />
-            </div>
-          </Col>
-        )}
+          {appMode !== GLOBAL_CONSTANTS.APP_MODES.EXTENSION && (
+            <Col xs={0} sm={0} md={0} lg={!isPricingOrGoodbyePage ? (isTabletView ? 10 : 8) : 10}>
+              <div className="header-middle-section">
+                <HeaderText />
+              </div>
+            </Col>
+          )}
 
-        <Col className="ml-auto">
-          <div className="header-right-section">
-            <Row align="middle" gutter={8} wrap={false}>
-              {appMode !== GLOBAL_CONSTANTS.APP_MODES.DESKTOP &&
-              user?.details?.planDetails?.status === "canceled" &&
-              isPlanExpiredBannerClosed ? (
-                <PlanExpiredBadge />
-              ) : null}
-              <RQButton
-                type="default"
-                className="header-search-btn"
-                onClick={() => dispatch(actions.updateIsCommandBarOpen(true))}
-              >
-                <div>
-                  <SearchOutlined style={{ marginRight: "2px" }} /> Search
-                </div>
-                <div>⌘+K</div>
-              </RQButton>
-              <Col className="hidden-on-small-screen">
+          <Col className="ml-auto">
+            <div className="header-right-section">
+              <Row align="middle" gutter={8} wrap={false}>
+                {appMode !== GLOBAL_CONSTANTS.APP_MODES.DESKTOP &&
+                user?.details?.planDetails?.status === "canceled" &&
+                isPlanExpiredBannerClosed ? (
+                  <PlanExpiredBadge />
+                ) : null}
+                <RQButton
+                  type="default"
+                  className="header-search-btn"
+                  onClick={() => dispatch(actions.updateIsCommandBarOpen(true))}
+                >
+                  <div>
+                    <SearchOutlined /> Search
+                  </div>
+                  <div className="search-shortcut-annotation">⌘+K</div>
+                </RQButton>
+                {/* TEMPORARILY HIDDEN  */}
+                {/* <Col className="hidden-on-small-screen">
                 <span className="github-star-button" onClick={() => trackHeaderClicked("github_star_button")}>
                   <GitHubButton
                     style={{ display: "flex" }}
@@ -105,38 +111,45 @@ const MenuHeader = () => {
                     aria-label="Star Requestly on GitHub"
                   />
                 </span>
-              </Col>
+              </Col> */}
+                <RQButton className="ask-ai-btn" onClick={() => setIsRequestBotVisible(true)}>
+                  <img src={BotIcon} alt="bot" />
+                  Ask AI
+                </RQButton>
 
-              <Divider type="vertical" className="header-vertical-divider hidden-on-small-screen" />
+                <Divider type="vertical" className="header-vertical-divider hidden-on-small-screen" />
 
-              {(appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP ||
-                (appMode !== GLOBAL_CONSTANTS.APP_MODES.DESKTOP &&
-                  user?.details?.planDetails?.status !== "canceled")) && (
+                {(appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP ||
+                  (appMode !== GLOBAL_CONSTANTS.APP_MODES.DESKTOP &&
+                    user?.details?.planDetails?.status !== "canceled")) && (
+                  <Col>
+                    <PremiumPlanBadge />
+                  </Col>
+                )}
+
+                {/* settings */}
                 <Col>
-                  <PremiumPlanBadge />
+                  <Tooltip title={<span className="text-gray text-sm">Settings</span>}>
+                    <Button
+                      type="text"
+                      className="header-icon-btn"
+                      icon={<Settings />}
+                      onClick={() => {
+                        trackHeaderClicked("settings");
+                        redirectToSettings(navigate, window.location.pathname, "header");
+                      }}
+                    />
+                  </Tooltip>
                 </Col>
-              )}
+                <HeaderUser />
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Header>
 
-              {/* settings */}
-              <Col>
-                <Tooltip title={<span className="text-gray text-sm">Settings</span>}>
-                  <Button
-                    type="text"
-                    className="header-icon-btn"
-                    icon={<Settings />}
-                    onClick={() => {
-                      trackHeaderClicked("settings");
-                      redirectToSettings(navigate, window.location.pathname, "header");
-                    }}
-                  />
-                </Tooltip>
-              </Col>
-              <HeaderUser />
-            </Row>
-          </div>
-        </Col>
-      </Row>
-    </Header>
+      <RequestBot isOpen={isRequestBotVisible} onClose={() => setIsRequestBotVisible(false)} />
+    </>
   ) : null;
 };
 
