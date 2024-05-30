536594b427a8fb98f1d3e4a502c5b4e972ec5930
Sahil Gupta <sahil865gupta@gmail.com>
2024-05-30 21:18:23 +0530

    fix: updatedToMv3 route (#1758)

--- app/src/routes/miscRoutes.tsx
+++ app/src/routes/miscRoutes.tsx
@@ -20,7 +20,7 @@ export const miscRoutes: RouteObject[] = [
   },
   {
     path: PATHS.EXTENSION_UPDATED.RELATIVE,
-    element: <Navigate to={PATHS.RULES.MY_RULES.ABSOLUTE + "?updatedToMV3"} />,
+    element: <Navigate to={PATHS.RULES.MY_RULES.ABSOLUTE + "?updatedToMv3"} />,
   },
   {
     path: PATHS.INSTALL_EXTENSION.RELATIVE,
