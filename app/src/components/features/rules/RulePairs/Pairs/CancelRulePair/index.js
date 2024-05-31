496663ab5a67ffa0b32312603ddedaca04141799
rohanmathur91 <mathurrohan04@gmail.com>
2024-05-31 15:38:17 +0530

    fix: redirect, cancel and replace rule for super rule

--- app/src/components/features/rules/RulePairs/Pairs/CancelRulePair/index.js
+++ app/src/components/features/rules/RulePairs/Pairs/CancelRulePair/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import RequestSourceRow from "../Rows/RowsMarkup/RequestSourceRow";
 
-const CancelRulePair = ({ pair, pairIndex, ruleDetails, isInputDisabled }) => (
-  <RequestSourceRow
-    rowIndex={1}
-    pair={pair}
-    pairIndex={pairIndex}
-    ruleDetails={ruleDetails}
-    isInputDisabled={isInputDisabled}
-  />
-);
+const CancelRulePair = ({ isSuperRule, ruleId, pair, pairIndex, ruleDetails, isInputDisabled }) =>
+  isSuperRule ? (
+    <div>Cancel rule added</div>
+  ) : (
+    <RequestSourceRow
+      rowIndex={1}
+      pair={pair}
+      pairIndex={pairIndex}
+      ruleDetails={ruleDetails}
+      isInputDisabled={isInputDisabled}
+    />
+  );
 
 export default CancelRulePair;
--- app/src/components/features/rules/RulePairs/Pairs/ReplaceRulePair/index.js
+++ app/src/components/features/rules/RulePairs/Pairs/ReplaceRulePair/index.js
@@ -3,26 +3,42 @@ import ReplacePartRow from "../Rows/RowsMarkup/ReplacePartRow";
 import RequestSourceRow from "../Rows/RowsMarkup/RequestSourceRow";
 import { Row, Col } from "antd";
 
-const ReplaceRulePair = ({ pair, pairIndex, ruleDetails, isInputDisabled }) => (
-  <React.Fragment>
-    <Row align="middle">
-      <Col span={24}>
-        <RequestSourceRow
-          rowIndex={2}
-          pair={pair}
-          pairIndex={pairIndex}
-          ruleDetails={ruleDetails}
-          isInputDisabled={isInputDisabled}
-        />
-      </Col>
-    </Row>
+const ReplaceRulePair = ({ isSuperRule, ruleId, pair, pairIndex, ruleDetails, isInputDisabled }) =>
+  isSuperRule ? (
+    <React.Fragment>
+      <Row>
+        <Col span={24}>
+          <ReplacePartRow
+            isSuperRule={isSuperRule}
+            ruleId={ruleId}
+            rowIndex={1}
+            pair={pair}
+            pairIndex={pairIndex}
+            isInputDisabled={isInputDisabled}
+          />
+        </Col>
+      </Row>
+    </React.Fragment>
+  ) : (
+    <React.Fragment>
+      <Row align="middle">
+        <Col span={24}>
+          <RequestSourceRow
+            rowIndex={2}
+            pair={pair}
+            pairIndex={pairIndex}
+            ruleDetails={ruleDetails}
+            isInputDisabled={isInputDisabled}
+          />
+        </Col>
+      </Row>
 
-    <Row>
-      <Col span={24}>
-        <ReplacePartRow rowIndex={1} pair={pair} pairIndex={pairIndex} isInputDisabled={isInputDisabled} />
-      </Col>
-    </Row>
-  </React.Fragment>
-);
+      <Row>
+        <Col span={24}>
+          <ReplacePartRow rowIndex={1} pair={pair} pairIndex={pairIndex} isInputDisabled={isInputDisabled} />
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
 
 export default ReplaceRulePair;
