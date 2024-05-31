95eac0ff238f328252875ab943d661942dc887a5
Sahil Gupta <sahil865gupta@gmail.com>
2024-06-01 04:48:57 +0530

    basic approach with marker

--- app/src/modules/extension/mv3RuleParser/parseReplaceRule.ts
+++ app/src/modules/extension/mv3RuleParser/parseReplaceRule.ts
@@ -1,67 +1,70 @@
-import { ReplaceRule } from "../../../types/rules";
-import { ExtensionRule, RuleActionType } from "../types";
-import { parseConditionFromSource } from "./utils";
+import { escapeRegExp } from "lodash";
+import { ReplaceRule, ReplaceRulePair } from "../../../types/rules";
+import { ExtensionRule, ExtensionRuleCondition, RuleActionType } from "../types";
+import { parseConditionFromSource, parseUrlParametersFromSourceV2 } from "./utils";
 
-const MARKER_QUERY_PARAM = "__rq_marker__";
+const getReplaceMatchingRegex = (rulePair: ReplaceRulePair): ExtensionRuleCondition => {
+  if (!rulePair.source.value) {
+    const regexCondition = parseUrlParametersFromSourceV2(rulePair.source, false);
+    return {
+      ...regexCondition,
+      regexFilter: `.*`,
+    };
+  } else {
+    const regexCondition = parseUrlParametersFromSourceV2(rulePair.source, false);
+    let finalRegexFilter = regexCondition.regexFilter;
+
+    // Remove ^ & $
+    if (finalRegexFilter?.[0] === "^") {
+      finalRegexFilter = finalRegexFilter.slice(1);
+    }
+    if (finalRegexFilter?.[finalRegexFilter.length - 1] === "$") {
+      finalRegexFilter = finalRegexFilter.slice(0, -1);
+    }
+
+    return {
+      ...regexCondition,
+      regexFilter: finalRegexFilter,
+    };
+  }
+};
 
 const parseReplaceRule = (rule: ReplaceRule): ExtensionRule[] => {
   const extensionRules: ExtensionRule[] = [];
 
   rule.pairs.forEach((rulePair, pairIndex) => {
-    if (!rulePair.source.value) {
-      const ruleCondition = {
-        condition: {
-          ...parseConditionFromSource(rulePair.source),
-          regexFilter: `(.*)${rulePair.from}(.*)`,
-        },
-        action: {
-          type: RuleActionType.REDIRECT,
-          redirect: {
-            regexSubstitution: `\\1${rulePair.to}\\2`,
-          },
-        },
-      };
-      extensionRules.push(ruleCondition);
-      return;
-    }
-
-    const markerValue = `${rule.id}_${pairIndex}`;
-
-    const rulePairExtensionRules: ExtensionRule[] = [
-      {
-        priority: 1,
-        condition: parseConditionFromSource(rulePair.source),
-        action: {
-          type: RuleActionType.REDIRECT,
-          redirect: {
-            transform: {
-              queryTransform: {
-                addOrReplaceParams: [
-                  {
-                    key: MARKER_QUERY_PARAM,
-                    value: markerValue,
-                  },
-                ],
-              },
-            },
-          },
+    const redirectWithMarkerRule: ExtensionRule = {
+      priority: 1,
+      condition: {
+        regexFilter: `(${parseConditionFromSource(rulePair.source)?.regexFilter})`,
+      },
+      action: {
+        type: RuleActionType.REDIRECT,
+        redirect: {
+          regexSubstitution: `\\1#__rq_marker=\\1`,
         },
       },
-      {
-        priority: 2,
-        condition: {
-          regexFilter: `(.*)${rulePair.from}(.*${MARKER_QUERY_PARAM}=${markerValue}.*)`,
-        },
-        action: {
-          type: RuleActionType.REDIRECT,
-          redirect: {
-            regexSubstitution: `\\1${rulePair.to}\\2`,
-          },
+    };
+
+    const subsitutionRegex = `(.*)${escapeRegExp(rulePair.from)}(.*)`;
+    const { regexFilter: matchingRegex, isUrlFilterCaseSensitive } = getReplaceMatchingRegex(rulePair);
+    const finalRegex = `^${subsitutionRegex}#__rq_marker=(?:${matchingRegex})$`;
+
+    let substitutionRule: ExtensionRule = {
+      priority: 2,
+      condition: {
+        regexFilter: finalRegex,
+        isUrlFilterCaseSensitive,
+      },
+      action: {
+        type: RuleActionType.REDIRECT,
+        redirect: {
+          regexSubstitution: `\\1${rulePair.to}\\2`,
         },
       },
-    ];
+    };
 
-    extensionRules.push(...rulePairExtensionRules);
+    extensionRules.push(...[redirectWithMarkerRule, substitutionRule]);
   });
 
   return extensionRules;
