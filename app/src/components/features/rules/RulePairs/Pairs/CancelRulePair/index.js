ec40ff8a7e6c700c56721792616cfe0671885afe
rohanmathur91 <mathurrohan04@gmail.com>
2024-06-01 14:01:04 +0530

    fix: cancel rule source

--- app/src/components/features/rules/RulePairs/Pairs/CancelRulePair/index.js
+++ app/src/components/features/rules/RulePairs/Pairs/CancelRulePair/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import RequestSourceRow from "../Rows/RowsMarkup/RequestSourceRow";
 import { MoreInfo } from "components/misc/MoreInfo";
+import { useSelector } from "react-redux";
+import { getCurrentlySelectedRuleData } from "store/selectors";
 
-const CancelRulePair = ({ isSuperRule, ruleId, pair, pairIndex, ruleDetails, isInputDisabled }) =>
-  isSuperRule ? (
+const CancelRulePair = ({ isSuperRule, ruleId, pair, pairIndex, ruleDetails, isInputDisabled }) => {
+  const currentlySelectedRuleData = useSelector(getCurrentlySelectedRuleData);
+  const cancelRulePair = !isSuperRule
+    ? pair
+    : {
+        ...(currentlySelectedRuleData?.rules?.[ruleId]?.pairs[0] ?? {}),
+        source: { ...currentlySelectedRuleData?.pairs?.[0]?.source },
+      };
+
+  return isSuperRule ? (
     <div>
       <RequestSourceRow
         rowIndex={1}
-        pair={pair}
+        pair={cancelRulePair}
         pairIndex={pairIndex}
         ruleDetails={ruleDetails}
         isInputDisabled={true}
@@ -22,5 +32,6 @@ const CancelRulePair = ({ isSuperRule, ruleId, pair, pairIndex, ruleDetails, isI
       isInputDisabled={isInputDisabled}
     />
   );
+};
 
 export default CancelRulePair;
