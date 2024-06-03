f8c8207e0c32a6b8b53f73714e2ca0d52a9183c2
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-06-03 13:20:59 +0530

    [ENGG-1759]: incentivization redeem and credits earned modal UI (#1720)
    * add redeem credits modal
    
    * fix:style
    
    * add touchpoint and refactor
    
    * incentive task completed modal UI
    
    * resolve conflicts
    
    ---------
    
    Co-authored-by: rohanmathur91 <mathurrohan04@gmail.com>

--- app/src/features/incentivization/components/CreditsProgressbar/CreditsProgessbar.tsx
+++ app/src/features/incentivization/components/CreditsProgressbar/CreditsProgessbar.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import { Button, Progress } from "antd";
 import { MdRedeem } from "@react-icons/all-files/md/MdRedeem";
+import { RedeemCreditsModal } from "features/incentivization";
 import "./creditsProgessbar.scss";
 
 export const CreditsProgressBar = () => {
+  const [isRedeemModalVisible, setIsRedeemModalVisible] = useState(false);
   return (
-    <div className="credits-progressbar-container">
-      <Progress percent={50} showInfo={false} className="incentive-credits-progessbar" />
-      {/* TODO: HANDLE CREDITS EARNED COUNTER HERE */}
-      <div className="credits-earned-counter">No credits earned</div>
-      <Button type="text" className="redeem-credits-btn" icon={<MdRedeem />} size="small">
-        redeem
-      </Button>
-    </div>
+    <>
+      <div className="credits-progressbar-container">
+        <Progress percent={50} showInfo={false} className="incentive-credits-progessbar" />
+        {/* TODO: HANDLE CREDITS EARNED COUNTER HERE */}
+        <div className="credits-earned-counter">No credits earned</div>
+        <Button
+          type="text"
+          className="redeem-credits-btn"
+          icon={<MdRedeem />}
+          size="small"
+          onClick={() => setIsRedeemModalVisible(true)}
+        >
+          Redeem
+        </Button>
+      </div>
+      {isRedeemModalVisible && (
+        <RedeemCreditsModal isOpen={isRedeemModalVisible} onClose={() => setIsRedeemModalVisible(false)} />
+      )}
+    </>
   );
 };
--- app/src/features/incentivization/index.ts
+++ app/src/features/incentivization/index.ts
@@ -2,3 +2,5 @@ export { IncentiveTasksListScreen } from "./components/IncentiveTasksListScreen/
 export { IncentiveSectionHeader } from "./components/IncentiveSectionHeader";
 export { CreditsProgressBar } from "./components/CreditsProgressbar/CreditsProgessbar";
 export { IncentiveTasksListModal } from "./modals/IncentiveTasksListModal/IncentiveTasksListModal";
+export { RedeemCreditsModal } from "./modals/RedeemCreditsModal/RedeemCreditsModal";
+export { IncentiveTaskCompletedModal } from "./modals/IncentiveTaskCompletedModal/IncentiveTaskCompletedModal";
