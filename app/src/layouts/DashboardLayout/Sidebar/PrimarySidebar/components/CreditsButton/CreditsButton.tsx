1366ac52c2430c0b5c5632582721276a2a5be26d
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-06-06 13:58:06 +0530

    ENGG-1801: Add tour for credits button for new users (#1775)
    * add tour for credits button for new users
    
    * fix

--- app/src/layouts/DashboardLayout/Sidebar/PrimarySidebar/components/CreditsButton/CreditsButton.tsx
+++ app/src/layouts/DashboardLayout/Sidebar/PrimarySidebar/components/CreditsButton/CreditsButton.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { getUserAuthDetails } from "store/selectors";
+import { useEffect, useState } from "react";
+import { getIsMiscTourCompleted, getUserAuthDetails } from "store/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { FiGift } from "@react-icons/all-files/fi/FiGift";
 import { Badge } from "antd";
@@ -7,16 +7,29 @@ import { IncentiveTasksListModal } from "features/incentivization";
 import { RQButton } from "lib/design-system/components";
 import { actions } from "store";
 import APP_CONSTANTS from "config/constants";
+import { ProductWalkthrough } from "components/misc/ProductWalkthrough";
+import { MISC_TOURS, TOUR_TYPES } from "components/misc/ProductWalkthrough/constants";
 import "./creditsButton.scss";
 
 export const CreditsButton = () => {
   const dispatch = useDispatch();
   const user = useSelector(getUserAuthDetails);
+  const isMiscTourCompleted = useSelector(getIsMiscTourCompleted);
   const [isTaskListModalVisible, setIsTaskListModalVisible] = useState(false);
+  const [isCreditsTourVisible, setIsCreditsTourVisible] = useState(false);
 
   const handleButtonClick = () => {
     if (user.loggedIn) {
       setIsTaskListModalVisible(true);
+      if (!isMiscTourCompleted.earnCredits) {
+        setIsCreditsTourVisible(false);
+        dispatch(
+          actions.updateProductTourCompleted({
+            tour: TOUR_TYPES.MISCELLANEOUS,
+            subTour: "earnCredits",
+          })
+        );
+      }
     } else {
       dispatch(
         // @ts-ignore
@@ -31,18 +44,57 @@ export const CreditsButton = () => {
       );
     }
   };
+
+  useEffect(() => {
+    // TODO: ADD CONDITION TO ONLY START WALKTHROUGH FOR NEW USERS ONLY
+    if (!isMiscTourCompleted.earnCredits) {
+      setIsCreditsTourVisible(true);
+    }
+  }, [isMiscTourCompleted.earnCredits]);
+
   return (
     <>
-      <RQButton className="primary-sidebar-link w-full" onClick={handleButtonClick}>
+      <ProductWalkthrough
+        completeTourOnUnmount={false}
+        tourFor={MISC_TOURS.APP_ENGAGEMENT.EARN_CREDITS}
+        startWalkthrough={isCreditsTourVisible}
+        onTourComplete={() => {
+          setIsCreditsTourVisible(false);
+          dispatch(
+            actions.updateProductTourCompleted({
+              tour: TOUR_TYPES.MISCELLANEOUS,
+              // TODO: IMPROVE WALKTHROUGH COMPONENT, SUBTOUR SHOULD BE PASSED AS A CONSTANT
+              subTour: "earnCredits",
+            })
+          );
+        }}
+      />
+      <RQButton
+        className="primary-sidebar-link w-full"
+        onClick={handleButtonClick}
+        data-tour-id={MISC_TOURS.APP_ENGAGEMENT.EARN_CREDITS}
+      >
         <div className="icon__wrapper">
-          <Badge
-            className="credits-earned-badge"
-            size="small"
-            // TODO: Replace with actual credits earned
-            count={<span className="credits-earned-count">$100</span>}
-          >
-            <FiGift />
-          </Badge>
+          {isCreditsTourVisible ? (
+            <Badge
+              className="credits-earned-badge"
+              size="small"
+              status="default"
+              count={<span className="earn-credits-nudge-dot"></span>}
+            >
+              <FiGift />
+            </Badge>
+          ) : (
+            <Badge
+              className="credits-earned-badge"
+              size="small"
+              // TODO: Replace with actual credits earned
+              status="default"
+              count={<span className="credits-earned-count">$100</span>}
+            >
+              <FiGift />
+            </Badge>
+          )}
         </div>
         <div className="link-title">Credits</div>
       </RQButton>
