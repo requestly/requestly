ecb35795bb4f1b56e2d4f2b489c0ae824d0061a5
nsrCodes <navdeepsinghrathore2000@gmail.com>
2024-06-06 07:28:46 +0530

    fix: fetch rules of groups in the delete modal action

--- app/src/features/rules/modals/UngroupOrDeleteRulesModalWrapper/index.tsx
+++ app/src/features/rules/modals/UngroupOrDeleteRulesModalWrapper/index.tsx
@@ -1,24 +1,46 @@
-import React, { useEffect, useState } from "react";
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import React, { useCallback, useEffect, useState } from "react";
 import UngroupOrDeleteRulesModal from "components/features/rules/UngroupOrDeleteRulesModal";
-import { Group } from "features/rules/types/rules";
+import { Group, Rule } from "features/rules/types/rules";
 import { useRulesModalsContext } from "features/rules/context/modals";
+import { StorageService } from "init";
+import { useSelector } from "react-redux";
+import { getAppMode } from "store/selectors";
 
 interface Props {}
 
 export const UngroupOrDeleteRulesModalWrapper: React.FC<Props> = () => {
   const { setOpenGroupDeleteModalAction } = useRulesModalsContext();
 
+  const appMode = useSelector(getAppMode);
+
   const [isModalActive, setIsModalActive] = useState(false);
   const [groupToDelete, setGroupToDelete] = useState<Group | null>(null);
 
+  const [groupRules, setGroupRules] = useState<Rule[]>([]);
+
+  const getAllRulesofGroup = useCallback(
+    async (groupId: Group["id"]) => {
+      return await StorageService(appMode)
+        .getRecords(GLOBAL_CONSTANTS.OBJECT_TYPES.RULE)
+        .then((rules: Rule[]) => {
+          const groupRules = rules.filter((rule) => rule.groupId === groupId);
+          return groupRules;
+        });
+    },
+    [appMode]
+  );
+
   useEffect(() => {
-    const openModal = (group: Group) => {
+    const openModal = async (group: Group) => {
+      const storageRules = await getAllRulesofGroup(group.id);
+      setGroupRules(storageRules);
       setGroupToDelete(group);
       setIsModalActive(true);
     };
 
     setOpenGroupDeleteModalAction(() => openModal);
-  }, [setOpenGroupDeleteModalAction]);
+  }, [setOpenGroupDeleteModalAction, getAllRulesofGroup]);
 
   const onClose = () => {
     setGroupToDelete(null);
@@ -28,8 +50,7 @@ export const UngroupOrDeleteRulesModalWrapper: React.FC<Props> = () => {
   return isModalActive ? (
     <UngroupOrDeleteRulesModal
       groupIdToDelete={groupToDelete?.id}
-      // @ts-ignore
-      groupRules={groupToDelete?.children ?? []}
+      groupRules={groupRules}
       isOpen={isModalActive}
       toggle={onClose}
       callback={() => {}}
