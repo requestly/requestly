faa706ee185e7871da103cd59b75b79f2c062bc9
RuntimeTerror10 <parthb.git@gmail.com>
2024-06-06 13:23:14 +0530

    add tour for credits button for new users

--- app/src/layouts/DashboardLayout/Sidebar/PrimarySidebar/components/CreditsButton/CreditsButton.tsx
+++ app/src/layouts/DashboardLayout/Sidebar/PrimarySidebar/components/CreditsButton/CreditsButton.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { getUserAuthDetails } from "store/selectors";
+import { getIsMiscTourCompleted, getUserAuthDetails } from "store/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { FiGift } from "@react-icons/all-files/fi/FiGift";
 import { Badge } from "antd";
@@ -7,16 +7,27 @@ import { IncentiveTasksListModal } from "features/incentivization";
 import { RQButton } from "lib/design-system/components";
 import { actions } from "store";
 import APP_CONSTANTS from "config/constants";
+import { ProductWalkthrough } from "components/misc/ProductWalkthrough";
+import { MISC_TOURS, TOUR_TYPES } from "components/misc/ProductWalkthrough/constants";
 import "./creditsButton.scss";
 
 export const CreditsButton = () => {
   const dispatch = useDispatch();
   const user = useSelector(getUserAuthDetails);
+  const isMiscTourCompleted = useSelector(getIsMiscTourCompleted);
   const [isTaskListModalVisible, setIsTaskListModalVisible] = useState(false);
 
   const handleButtonClick = () => {
     if (user.loggedIn) {
       setIsTaskListModalVisible(true);
+      if (!isMiscTourCompleted.earnCredits) {
+        dispatch(
+          actions.updateProductTourCompleted({
+            tour: TOUR_TYPES.MISCELLANEOUS,
+            subTour: "earnCredits",
+          })
+        );
+      }
     } else {
       dispatch(
         // @ts-ignore
@@ -33,16 +44,47 @@ export const CreditsButton = () => {
   };
   return (
     <>
-      <RQButton className="primary-sidebar-link w-full" onClick={handleButtonClick}>
+      <ProductWalkthrough
+        completeTourOnUnmount={false}
+        tourFor={MISC_TOURS.APP_ENGAGEMENT.EARN_CREDITS}
+        // TODO: ADD CONDITION TO ONLY START WALKTHROUGH FOR NEW USERS ONLY
+        startWalkthrough={!isMiscTourCompleted.earnCredits}
+        onTourComplete={() =>
+          dispatch(
+            actions.updateProductTourCompleted({
+              tour: TOUR_TYPES.MISCELLANEOUS,
+              // TODO: IMPROVE WALKTHROUGH COMPONENT, SUBTOUR SHOULD BE PASSED AS A CONSTANT
+              subTour: "earnCredits",
+            })
+          )
+        }
+      />
+      <RQButton
+        className="primary-sidebar-link w-full"
+        onClick={handleButtonClick}
+        data-tour-id={MISC_TOURS.APP_ENGAGEMENT.EARN_CREDITS}
+      >
         <div className="icon__wrapper">
-          <Badge
-            className="credits-earned-badge"
-            size="small"
-            // TODO: Replace with actual credits earned
-            count={<span className="credits-earned-count">$100</span>}
-          >
-            <FiGift />
-          </Badge>
+          {!isMiscTourCompleted.earnCredits ? (
+            <Badge
+              className="credits-earned-badge"
+              size="small"
+              status="default"
+              count={<span className="earn-credits-nudge-dot"></span>}
+            >
+              <FiGift />
+            </Badge>
+          ) : (
+            <Badge
+              className="credits-earned-badge"
+              size="small"
+              // TODO: Replace with actual credits earned
+              status="default"
+              count={<span className="credits-earned-count">$100</span>}
+            >
+              <FiGift />
+            </Badge>
+          )}
         </div>
         <div className="link-title">Credits</div>
       </RQButton>
