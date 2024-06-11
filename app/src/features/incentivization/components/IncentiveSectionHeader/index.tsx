2dcd05e650e5cfae80b0defcc50505f8682868a0
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-06-11 13:47:42 +0530

    fix: incentive onboarding days left badge visibility (#1802)

--- app/src/features/incentivization/components/IncentiveSectionHeader/index.tsx
+++ app/src/features/incentivization/components/IncentiveSectionHeader/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getUserAttributes } from "store/selectors";
 import { TbClockExclamation } from "@react-icons/all-files/tb/TbClockExclamation";
 // import { MdOutlineInfo } from "@react-icons/all-files/md/MdOutlineInfo";
 import "./index.scss";
@@ -8,13 +10,18 @@ interface IncentiveSectionHeaderProps {
 }
 
 export const IncentiveSectionHeader: React.FC<IncentiveSectionHeaderProps> = ({ title }) => {
+  const userAttributes = useSelector(getUserAttributes);
+  const daysLeft = 14 - userAttributes?.days_since_signup ?? 0;
+
   return (
     <div className="incentive-section-header-container">
       <div className="title-container">
         <span className="incentive-section-title">{title}</span>
-        <span className="incentive-section-duration-badge">
-          <TbClockExclamation /> <span>14 days left</span>
-        </span>
+        {daysLeft > 0 ? (
+          <span className="incentive-section-duration-badge">
+            <TbClockExclamation /> <span>{daysLeft} days left</span>
+          </span>
+        ) : null}
       </div>
       <div className="incentive-section-description">
         <span>Get the most out of your Requestly account and earn free credits by completing these steps.</span>
