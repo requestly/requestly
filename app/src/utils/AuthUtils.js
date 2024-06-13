2206e6c137d41ac1e55199edd4aa7616b92ea09f
Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>
2024-06-13 13:08:08 +0530

    feat: warning UI for disposable emails in auth form input (#1809)

--- app/src/utils/AuthUtils.js
+++ app/src/utils/AuthUtils.js
@@ -11,6 +11,8 @@ import {
   trackEmailVerificationSendSuccess,
 } from "modules/analytics/events/common/auth/emailVerification";
 import Logger from "lib/logger";
+import disposableEmailDomains from "disposable-email-domains/index.json";
+import disposableEmailDomainsWildcard from "disposable-email-domains/wildcard.json";
 
 const TRACKING = APP_CONSTANTS.GA_EVENTS;
 
@@ -31,6 +33,11 @@ export const isEmailVerified = (userId) => {
       } else return data.emailVerified;
     })
     .catch((err) => Logger.log(err));
+};
+
+export const isDisposableEmail = (email) => {
+  const domain = email.split("@")[1];
+  return disposableEmailDomains.includes(domain) || disposableEmailDomainsWildcard.includes(domain);
 };
 
 export const setEmailVerified = async (userId, value) => {
