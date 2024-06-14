641c9eca3f59e8b89ab4ea75ed3fb0e6a6c3ad38
rohanmathur91 <mathurrohan04@gmail.com>
2024-06-14 20:34:55 +0530

    fix: redeem click in logged out state

--- app/src/features/incentivization/components/CreditsProgressbar/CreditsProgessbar.tsx
+++ app/src/features/incentivization/components/CreditsProgressbar/CreditsProgessbar.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, Progress, Tooltip } from "antd";
 import { MdRedeem } from "@react-icons/all-files/md/MdRedeem";
 import { RedeemCreditsModal } from "features/incentivization";
@@ -13,6 +13,8 @@ import { getTotalCredits } from "features/incentivization/utils";
 import { getUserAuthDetails } from "store/selectors";
 import { getAvailableBillingTeams } from "store/features/billing/selectors";
 import "./creditsProgessbar.scss";
+import APP_CONSTANTS from "config/constants";
+import { actions } from "store";
 
 interface CreditsProgressBarProps {
   source: string;
@@ -21,6 +23,7 @@ interface CreditsProgressBarProps {
 export const CreditsProgressBar: React.FC<CreditsProgressBarProps> = ({ source }) => {
   const [isRedeemModalVisible, setIsRedeemModalVisible] = useState(false);
 
+  const dispatch = useDispatch();
   const user = useSelector(getUserAuthDetails);
   const isLoading = useSelector(getIsIncentivizationDetailsLoading);
   const milestones = useSelector(getIncentivizationMilestones);
@@ -39,6 +42,25 @@ export const CreditsProgressBar: React.FC<CreditsProgressBarProps> = ({ source }
   const creditsToBeRedeemed = userMilestoneAndRewardDetails?.creditsToBeRedeemed ?? 0;
   const isAllCreditsRedeemed =
     Object.keys(milestones ?? {}).length === (userMilestoneAndRewardDetails?.claimedMilestoneLogs?.length ?? 0);
+
+  const handleRedeemClick = () => {
+    if (user?.loggedIn) {
+      setIsRedeemModalVisible(true);
+    } else {
+      dispatch(
+        // @ts-ignore
+        actions.toggleActiveModal({
+          modalName: "authModal",
+          newValue: true,
+          newProps: {
+            authMode: APP_CONSTANTS.AUTH.ACTION_LABELS.LOG_IN,
+            warningMessage: "You must sign in to earn or redeem free credits.",
+            eventSource: "incentivization",
+          },
+        })
+      );
+    }
+  };
 
   return (
     <>
@@ -72,7 +94,7 @@ export const CreditsProgressBar: React.FC<CreditsProgressBarProps> = ({ source }
                   disabled={disableRedeem}
                   icon={<MdRedeem className="anticon" />}
                   className="redeem-credits-btn"
-                  onClick={() => setIsRedeemModalVisible(true)}
+                  onClick={handleRedeemClick}
                 >
                   Redeem now
                 </Button>
