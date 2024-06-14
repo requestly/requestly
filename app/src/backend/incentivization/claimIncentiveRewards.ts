ff57645dfd7961de80bac1eed38c1a3ed53b4c14
rohanmathur91 <mathurrohan04@gmail.com>
2024-06-14 16:04:42 +0530

    refactor

--- app/src/backend/incentivization/claimIncentiveRewards.ts
+++ app/src/backend/incentivization/claimIncentiveRewards.ts
@@ -3,5 +3,7 @@ import { getFunctions, httpsCallable } from "firebase/functions";
 
 export const claimIncentiveRewards = (event: UserIncentiveEvent) => {
   const claimRewards = httpsCallable<UserIncentiveEvent>(getFunctions(), "incentivization-claimIncentiveRewards");
+
+  console.log("claimIncentiveRewards - event", event);
   return claimRewards(event);
 };
