21e3ce494e8dcf853a2b01b961f3137f714a12f3
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-06-17 18:43:16 +0530

    ENGG-1908: Pricing page redesign (#1817)

--- app/src/components/landing/pricing/ComparisonTable/v2-free-enterprise/analytics.js
+++
@@ -1,9 +0,0 @@
-import { trackEvent } from "modules/analytics";
-
-export const EVENTS = {
-  ENTERPRISE_PLAN_SCHEDULE_MEET_BUTTON_CLICKED: "enterprise_plan_schedule_meet_button_clicked",
-};
-
-export function trackEnterprisePlanScheduleMeetButtonClicked() {
-  trackEvent(EVENTS.ENTERPRISE_PLAN_SCHEDULE_MEET_BUTTON_CLICKED);
-}
---
+++ app/src/features/pricing/components/PricingPage/PricingIndexPage.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Col, Layout, Row, Switch } from "antd";
+import HeaderUser from "layouts/DashboardLayout/MenuHeader/HeaderUser";
+import { useNavigate } from "react-router-dom";
+import { PricingTable } from "../PricingTable";
+import { PRICING } from "features/pricing/constants/pricing";
+import { SOURCE } from "modules/analytics/events/common/constants";
+import { redirectToRules } from "utils/RedirectionUtils";
+import RQLogo from "assets/img/brand/rq_logo_full.svg";
+import { EnterprisePlanCard } from "./components/EnterprisePlanCard/EnterprisePlanCard";
+import { CompaniesSection } from "./components/CompaniesSection/CompaniesSection";
+import { StatsCard } from "./components/StatsCard/StatsCard";
+import BuyAdditionalSeats from "./components/BuyAdditionalSeats";
+import HowToClaimVolumeDiscounts from "./components/HowToClaimVolumeDiscounts";
+import OtherWaysToMakePurchase from "./components/OtherWaysToMakePurchase";
+import LicensingAndSavings from "./components/LicensingAndSavings";
+import { CustomerReviews } from "./components/CustomerReviews/CustomerReviews";
+import PricingFAQs from "./components/FAQs";
+import PricingPageFooter from "./components/PricingPageFooter";
+import EnterpriseRequestBanner from "./components/EnterpriseRequestBanner";
+import "./pricingIndexPage.scss";
+
+export const PricingIndexPage = () => {
+  const navigate = useNavigate();
+  const [duration, setDuration] = useState(PRICING.DURATION.ANNUALLY);
+  return (
+    <div className="pricing-page-wrapper">
+      <div className="pricing-page-container">
+        <Layout.Header className="pricing-navbar">
+          <div className="pricing-navbar-content">
+            <img className="logo" src={RQLogo} alt="requestly logo" onClick={() => redirectToRules(navigate)} />
+            <HeaderUser />
+          </div>
+        </Layout.Header>
+        <div className="pricing-page-body-wrapper">
+          <EnterpriseRequestBanner />
+          <div className="pricing-page-body">
+            <div className="page-yellow-text">Plans & pricing</div>
+            <div className="pricing-page-title">50,000+ Companies Are Using Requestly Today</div>
+            <div className="pricing-page-description">
+              More than half of Fortune 500 companies already use Requestly
+            </div>
+            <Row justify="center" className="display-row-center w-full mt-24" gutter={24}>
+              <Col className="display-row-center plan-duration-switch-container">
+                <Switch
+                  size="small"
+                  checked={duration === PRICING.DURATION.ANNUALLY}
+                  onChange={(checked) => {
+                    setDuration(checked ? PRICING.DURATION.ANNUALLY : PRICING.DURATION.MONTHLY);
+                  }}
+                />
+                <span className="pricing-page-duartion-label">
+                  {" "}
+                  Annually<span className="success"> (save 20%)</span>
+                </span>
+              </Col>
+            </Row>
+            <div className="pricing-page-table-wrapper">
+              <PricingTable duration={duration} source={SOURCE.PRICING_PAGE} />
+            </div>
+            <EnterprisePlanCard />
+            <CompaniesSection />
+            <StatsCard />
+            <div className="pricing-page-other-actions-container">
+              {/* TODO: A COMMON COMPONENT TO BE CREATED FOR BELOW 2 COMPONENTS */}
+              <BuyAdditionalSeats />
+              <HowToClaimVolumeDiscounts />
+            </div>
+            <LicensingAndSavings />
+            <OtherWaysToMakePurchase />
+            <CustomerReviews />
+            <PricingFAQs />
+          </div>
+          <PricingPageFooter />
+        </div>
+      </div>
+    </div>
+  );
+};
