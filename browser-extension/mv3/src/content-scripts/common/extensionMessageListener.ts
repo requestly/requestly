609141ba79a8ee5be485f375767ae5fb033ff32d
Nafees Nehar <nafees87n@gmail.com>
2024-06-18 19:05:11 +0530

    fix: extensionMessageListener

--- browser-extension/mv3/src/content-scripts/common/extensionMessageListener.ts
+++ browser-extension/mv3/src/content-scripts/common/extensionMessageListener.ts
@@ -4,14 +4,11 @@ import { sendExtensionMessage } from "../app/messageHandler";
 export const initExtensionMessageListener = () => {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     switch (message.action) {
-      case EXTENSION_MESSAGES.NOTIFY_TEST_RULE_REPORT_UPDATED:
-        sendExtensionMessage(message);
-        break;
-
       case EXTENSION_MESSAGES.CLIENT_PAGE_LOADED:
         chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.CLIENT_PAGE_LOADED });
         break;
 
+      case EXTENSION_MESSAGES.NOTIFY_TEST_RULE_REPORT_UPDATED:
       case CLIENT_MESSAGES.NOTIFY_RECORD_UPDATED:
       case EXTENSION_MESSAGES.RULE_SAVE_ERROR:
         sendExtensionMessage(message);
