ef5cd579ab7ba74bc07e7840614085441c0c07f2
RuntimeTerror10 <parthb.git@gmail.com>
2024-06-26 03:58:51 +0530

    disable onboarding

--- app/src/features/onboarding/utils.ts
+++ app/src/features/onboarding/utils.ts
@@ -1,9 +1,14 @@
 import PATHS from "config/constants/sub/paths";
+import { getAppFlavour } from "utils/AppUtils";
 import { isEnvAutomation } from "utils/EnvUtils";
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 
 export const shouldShowOnboarding = () => {
+  const appFlavour = getAppFlavour();
+
   if (
     isEnvAutomation() ||
+    appFlavour === GLOBAL_CONSTANTS.APP_FLAVOURS.REQUESTLY ||
     window.location.href.includes(PATHS.AUTH.SIGN_IN.RELATIVE) ||
     window.location.href.includes(PATHS.AUTH.SIGN_UP.RELATIVE) ||
     window.location.href.includes(PATHS.AUTH.DEKSTOP_SIGN_IN.RELATIVE) ||
