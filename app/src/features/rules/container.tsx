ef7b6cdf26e1b1b2d0822f4cf7142240976945f3
Nafees Nehar <nafees87n@gmail.com>
2024-06-26 13:04:28 +0530

    [ENGG-1978] chore: enrich DNR saving issue (#1868)

--- app/src/features/rules/container.tsx
+++ app/src/features/rules/container.tsx
@@ -18,8 +18,13 @@ import PageScriptMessageHandler from "config/PageScriptMessageHandler";
 import { Row, notification } from "antd";
 import PATHS from "config/constants/sub/paths";
 import { trackErrorInSavingDNR } from "modules/analytics/events/common/rules";
+import { useSelector } from "react-redux";
+import { StorageService } from "init";
+import { getAppMode } from "store/selectors";
 
 const RulesFeatureContainer = () => {
+  const appMode = useSelector(getAppMode);
+
   useEffect(() => {
     PageScriptMessageHandler.addMessageListener("ruleSaveError", (message: any) => {
       notification.warn({
@@ -37,9 +42,23 @@ const RulesFeatureContainer = () => {
         duration: 0,
       });
       console.log(`[Requestly]: Error saving rule - ${message.error}`);
-      trackErrorInSavingDNR(message.error, message.rqRuleId.split("_")[0], message.rqRuleId);
+      StorageService(appMode)
+        .getRecord(message.rqRuleId)
+        .then((ruleDetails) => {
+          const sourceCondition = ruleDetails?.pairs?.[0]?.source;
+          trackErrorInSavingDNR({
+            rule_type: message.rqRuleId.split("_")[0],
+            rule_id: message.rqRuleId,
+            error: message.error.replace(/Rule with id \d+/g, "Rule with id"),
+            is_migration_triggered: window.location.search.includes("updatedToMv3"),
+            source_key: sourceCondition?.key,
+            source_operator: sourceCondition?.operator,
+            source_value: sourceCondition?.value,
+            page_path: window.location.pathname + window.location.search,
+          });
+        });
     });
-  }, []);
+  }, [appMode]);
 
   return (
     <SecondarySidebarLayout secondarySidebar={<RulesSidebar />}>
