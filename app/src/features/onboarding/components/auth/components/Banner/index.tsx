7f04b579d4626bc73e725eff4f65b45dc261fdb5
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-06-27 14:23:50 +0530

    ENGG-1920: Update touchpoints around SessionBear (#1866)

--- app/src/features/onboarding/components/auth/components/Banner/index.tsx
+++ app/src/features/onboarding/components/auth/components/Banner/index.tsx
@@ -1,13 +1,15 @@
 import { Col, Row, Typography } from "antd";
+import { getAppFlavour } from "utils/AppUtils";
 import underlineIcon from "assets/images/illustrations/yellow-highlight.svg";
 import { MdCheck } from "@react-icons/all-files/md/MdCheck";
 import indeedLogo from "assets/img/icons/common/indeed.svg";
 import atntLogo from "assets/img/icons/common/atnt.svg";
 import verizonLogo from "assets/img/icons/common/verizon.svg";
 import intuitLogo from "assets/img/icons/common/intuit.svg";
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import "./index.scss";
 
-const features = [
+const requestlyFeatures = [
   "Work from anywhere - Sync Requestly rules & other configurations across devices",
   "Access your organization's resources, including team workspaces, shared rules and mocks.",
   "Use All features like HTTP Sessions, API Mocks, Overriding API Responses, etc.",
@@ -15,23 +17,50 @@ const features = [
   "It's Free!",
 ];
 
+const sessionBearFeatures = [
+  "Unlimited Sessions",
+  "Collaborate with teammates using Team Workspaces",
+  "Work from anywhere - Sync Sessions and other configurations across devices",
+  "Join a Community trusted by over 200,000 customers worldwide from over 10,000",
+  "It's Free!",
+];
+
 const companyLogos = [indeedLogo, atntLogo, verizonLogo, intuitLogo];
 
 export const OnboardingAuthBanner = () => {
+  const appFlavour = getAppFlavour();
+
   return (
     <Col className="auth-banner">
       <Typography.Title level={3}>
-        <span className="banner-title-highlight">
-          Why Sign Up ? <img src={underlineIcon} alt="highlight" />
-        </span>
+        {appFlavour === GLOBAL_CONSTANTS.APP_FLAVOURS.SESSIONBEAR ? (
+          <>
+            Let’s build a{" "}
+            <span className="banner-title-highlight">
+              bug-free <img src={underlineIcon} alt="highlight" />
+            </span>{" "}
+            product
+          </>
+        ) : (
+          <span className="banner-title-highlight">
+            Why Sign Up ? <img src={underlineIcon} alt="highlight" />
+          </span>
+        )}
       </Typography.Title>
       <div className="mt-20">
-        {features.map((feature) => (
-          <Row className="banner-premium-feature-list-item">
-            <MdCheck />
-            <span className="feature-text">{feature}</span>
-          </Row>
-        ))}
+        {appFlavour === GLOBAL_CONSTANTS.APP_FLAVOURS.SESSIONBEAR
+          ? sessionBearFeatures.map((feature) => (
+              <Row className="banner-premium-feature-list-item">
+                <MdCheck />
+                <span className="feature-text">{feature}</span>
+              </Row>
+            ))
+          : requestlyFeatures.map((feature) => (
+              <Row className="banner-premium-feature-list-item">
+                <MdCheck />
+                <span className="feature-text">{feature}</span>
+              </Row>
+            ))}
       </div>
       <Row style={{ marginTop: "60px" }}>
         <Typography.Text className="banner-text-small" style={{ fontWeight: "500", color: "var(--white)" }}>
