850105d8b0bd1e21ad99c6778d3ba16ab9f691bd
RuntimeTerror10 <parthb.git@gmail.com>
2024-06-27 17:50:56 +0530

    add workspace routes and fix install extension modal

--- app/src/src-SessionBear/routes/index.ts
+++ app/src/src-SessionBear/routes/index.ts
@@ -3,5 +3,12 @@ import { sessionRoutes } from "./sessionRoutes";
 import { miscRoutes } from "./miscRoutes";
 import { authRoutes } from "routes/authRoutes";
 import { settingsRoutes } from "./settingsRoutes";
+import { accountRoutes } from "routes/accountRoutes";
 
-export const sessionBearRoutes: RouteObject[] = [...sessionRoutes, ...miscRoutes, ...authRoutes, ...settingsRoutes];
+export const sessionBearRoutes: RouteObject[] = [
+  ...sessionRoutes,
+  ...miscRoutes,
+  ...authRoutes,
+  ...settingsRoutes,
+  ...accountRoutes,
+];
