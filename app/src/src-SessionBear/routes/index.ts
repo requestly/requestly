8a979efeb3354be0280c4ce49682c49e41340c5a
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-06-27 14:23:50 +0530

    ENGG-1920: Update touchpoints around SessionBear (#1866)

--- app/src/src-SessionBear/routes/index.ts
+++ app/src/src-SessionBear/routes/index.ts
@@ -1,5 +1,6 @@
 import { RouteObject } from "react-router-dom";
 import { sessionRoutes } from "./sessionRoutes";
 import { miscRoutes } from "./miscRoutes";
+import { authRoutes } from "routes/authRoutes";
 
-export const sessionBearRoutes: RouteObject[] = [...sessionRoutes, ...miscRoutes];
+export const sessionBearRoutes: RouteObject[] = [...sessionRoutes, ...miscRoutes, ...authRoutes];
--- browser-extension/sessionbear/src/content-scripts/app/index.ts
+++ browser-extension/sessionbear/src/content-scripts/app/index.ts
@@ -4,13 +4,13 @@ import { initExtensionMessageListener } from "../common/extensionMessageListener
 import { initSessionRecording } from "../common/sessionRecorder";
 import { initMessageHandler } from "./messageHandler";
 
-//document.documentElement.setAttribute("rq-ext-version", chrome.runtime.getManifest()["version"]);
+document.documentElement.setAttribute("rq-ext-version", chrome.runtime.getManifest()["version"]);
 
 // manifest version
-//document.documentElement.setAttribute("rq-ext-mv", "3");
+document.documentElement.setAttribute("rq-ext-mv", "3");
 
 // extension id
-//document.documentElement.setAttribute("rq-ext-id", chrome.runtime.id);
+document.documentElement.setAttribute("rq-ext-id", chrome.runtime.id);
 
 initMessageHandler();
 initExtensionMessageListener();
