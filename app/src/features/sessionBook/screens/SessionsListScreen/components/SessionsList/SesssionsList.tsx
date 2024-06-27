ae309d092436a6a64e0118db23315a1e57ae2165
RuntimeTerror10 <parthb.git@gmail.com>
2024-06-27 10:26:48 +0530

    add session onboarding view

--- app/src/features/sessionBook/screens/SessionsListScreen/components/SessionsList/SesssionsList.tsx
+++ app/src/features/sessionBook/screens/SessionsListScreen/components/SessionsList/SesssionsList.tsx
@@ -2,15 +2,17 @@ import PageLoader from "components/misc/PageLoader";
 import { useFetchSessions } from "./hooks/useFetchSessions";
 import { useSelector } from "react-redux";
 import { getUserAuthDetails } from "store/selectors";
+import { SessionsOnboardingView } from "./components/OnboardingView/SessionsOnboardingView";
 
 export const SessionsList = () => {
   const user = useSelector(getUserAuthDetails);
   const { sessions, isSessionsListLoading } = useFetchSessions();
+
   if (isSessionsListLoading) {
     return <PageLoader message="Loading sessions..." />;
   }
 
   if (user.loggedIn && sessions.length) {
     return <>LIST HERE</>;
-  } else return <>LIST HERE</>;
+  } else return <SessionsOnboardingView />;
 };
