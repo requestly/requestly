b17b368e9ad810e30e3769db2e4b9d3eaa423a59
Rohan Mathur <mathurrohan04@gmail.com>
2024-07-08 13:07:44 +0600

    [ENGG-2006] chore: remove ask ai and credits tour (#1917)

--- app/src/components/misc/ProductWalkthrough/constants.ts
+++ app/src/components/misc/ProductWalkthrough/constants.ts
@@ -9,8 +9,6 @@ export const TOUR_TYPES = {
 export const MISC_TOURS = {
   APP_ENGAGEMENT: {
     FIRST_DRAFT_SESSION: "first_draft_session",
-    ASK_AI: "ask_ai",
-    EARN_CREDITS: "earn_credits",
   },
   PRICING: {
     UPGRADE_WORKSPACE_MENU: "upgrade_workspace_menu",
