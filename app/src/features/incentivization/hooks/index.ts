116e561ca25af03b339ca1cf86eabe1ee262e9c0
Rohan Mathur <mathurrohan04@gmail.com>
2024-07-08 14:21:24 +0600

    [ENGG-1969]: incentivization enhancements (#1918)
    * fix: update copy
    
    * chore: fix event
    
    * chore: fix event
    
    * refactor
    
    * add rule creat and test event
    
    * fix: test this rule event
    
    * fix: milestones for new users
    
    * fix: create rule
    
    * fix: test this rule
    
    * fix: init timer
    
    * refactor
    
    * fix: create rule handler
    
    * cleanup
    
    * add test this rule tour
    
    * fix

--- app/src/features/incentivization/hooks/index.ts
+++ app/src/features/incentivization/hooks/index.ts
@@ -2,3 +2,4 @@ export { useFetchIncentivizationDetails } from "./useFetchIncentivizationDetails
 export { useSyncLocalIncentivizationState } from "./useSyncLocalIncentivizationState";
 export { useIsIncentivizationEnabled } from "./useIsIncentivizationEnabled";
 export { useIncentiveActions } from "./useIncentiveActions";
+export { useIsNewUserForIncentivization } from "./useIsNewUserForIncentivization";
