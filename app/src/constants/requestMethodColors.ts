f8a67315cc7ee78ac602bcb181989e9882c90e47
nsrCodes <navdeepsinghrathore2000@gmail.com>
2024-07-09 20:53:07 +0530

    style: update method colors in mocks listing

--- app/src/constants/requestMethodColors.ts
+++ app/src/constants/requestMethodColors.ts
@@ -1,9 +1,13 @@
+import { generateColorTokens } from "lib/design-system-v2/tokens/colors";
+
+const tokens = generateColorTokens("#004EEB", "A447F8", "#787878", "#0BAA60", "#E43434", "#E09400");
+
 export const REQUEST_METHOD_COLORS = {
-  GET: "#54E6A2",
-  POST: "#1E69FF",
-  PUT: "#FF6905",
-  DELETE: "#E43434",
-  PATCH: "#FFCA5F",
-  HEAD: "#BEAAFF",
-  OPTIONS: "#57BEE6",
+  GET: tokens["success-300"],
+  POST: tokens["warning-300"],
+  PUT: tokens["primary-300"],
+  PATCH: tokens["secondary-300"],
+  DELETE: tokens["error-300"],
+  HEAD: tokens["success-100"],
+  OPTIONS: tokens["warning-100"],
 };
