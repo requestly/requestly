951eb38d2f8631a56f66fec2d63918cb30e574b9
Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>
2024-07-09 07:19:38 +0530

    test mv3: request rule (#1911)
    * test mv3: request rule
    
    * chore: comment out request test with explanation
    
    * chore: comment

---
+++ browser-extension/mv3/tests/rules/request/requestRule.spec.ts
@@ -0,0 +1,47 @@
+import { Page, Request, expect } from "@playwright/test";
+import { test } from "../../fixtures";
+import { loadRules } from "../../utils";
+import redirectRules from "./request_rules.json";
+import testScenarios, { RequestRuleTestScenario } from "./testScenarios";
+import { IBaseTestData } from "../types";
+
+test.describe("Request Rule", () => {
+  testScenarios.forEach((scenario, i) => {
+    test(scenario.description, async ({ appPage, context }) => {
+      await testRequestRule({ appPage, context, ...scenario });
+    });
+  });
+});
+
+const testRequestRule = async (testScenarioData: RequestRuleTestScenario & IBaseTestData) => {
+  const { appPage, context, ruleIds, testPageURL, expectedBodyUpdates, pageActions } = testScenarioData;
+  const rules = ruleIds.reduce((acc, ruleId) => ({ ...acc, [ruleId]: redirectRules[ruleId] }), {});
+  await loadRules(appPage, rules);
+
+  const testPage = await context.newPage();
+
+  const bodyMap = new Map<string, string[]>();
+
+  /* CURRENTLY THIS GIVES THE REQUEST BEFORE MODIFICATION */
+  /* @nsr: couldn't find any way to get the request after modification, seems like modifications are not even being done. I tried to add the custom fetch/xhr interceptors via pageActions they gave errors */
+  testPage.on("request", (request: Request) => {
+    if (request.method() === "POST") {
+      const body = request.postData();
+      if (bodyMap.has(request.url())) {
+        bodyMap[request.url()].push(body);
+      } else {
+        bodyMap.set(request.url(), [body ?? ""]);
+      }
+    }
+  });
+
+  await testPage.goto(testPageURL, { waitUntil: "domcontentloaded" });
+
+  await pageActions?.(testPage);
+
+  for (const { url, expectedBody } of expectedBodyUpdates) {
+    const finalBody = bodyMap.get(url);
+    expect(finalBody).toBeDefined();
+    expect(finalBody).toContain(expectedBody);
+  }
+};
