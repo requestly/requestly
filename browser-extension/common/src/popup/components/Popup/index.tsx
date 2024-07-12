920f8d3053f996c842117041b8f1d9ffaed45803
Nafees Nehar <nafees87n@gmail.com>
2024-07-12 16:57:40 +0530

    [ENGG-1960 ENGG-1991] feat: extension should not work on the defined blockedList (#1885)
    * added blocked_domains
    
    * added blockDomain support in ajaxinterceptor
    
    * fix: cache init
    
    * added check in bg
    
    * fix: messages
    
    * removed requestDomain check
    
    * remove not required
    
    * fix: pattern
    
    * cache blocked domains
    
    * moved to utils
    
    * [ENGG-1992] UI: option to add blocked domains in settings (#1903)
    
    * added blocklist skeleton
    
    * added style
    
    * fix: bg
    
    * added blockList UI
    
    * added debounce
    
    * added check in popup
    
    * added blocked state in extension
    
    * fix: useEffect
    
    * fix: toast message
    
    * replaced ant-icon
    
    * fix: overflow
    
    * fix:import
    
    * fix: imports
    
    * fix: blockList checks
    
    * fix: border
    
    * fix: blocklist change listener
    
    * fix: tab icons
    
    * fix: marking disable
    
    * fix: blocked domains cacher
    
    * fix: blockedDomains caching
    
    * fix: initiator check
    
    * added analytics events
    
    * added feature compatibility

--- browser-extension/common/src/popup/components/Popup/index.tsx
+++ browser-extension/common/src/popup/components/Popup/index.tsx
@@ -7,10 +7,19 @@ import { EVENT, sendEvent } from "../../events";
 import SessionRecordingView from "../SessionRecording/SessionRecordingView";
 import { getExtensionVersion } from "../../utils";
 import "./popup.css";
+import { BlockedExtensionView } from "../BlockedExtensionView/BlockedExtensionView";
 
 const Popup: React.FC = () => {
   const [ifNoRulesPresent, setIfNoRulesPresent] = useState<boolean>(true);
   const [isExtensionEnabled, setIsExtensionEnabled] = useState<boolean>(true);
+  const [isBlockedOnTab, setIsBlockedOnTab] = useState<boolean>(false);
+  const [currentTab, setCurrentTab] = useState<chrome.tabs.Tab>(null);
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
+      setCurrentTab(activeTab);
+    });
+  }, []);
 
   useEffect(() => {
     chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.CHECK_IF_NO_RULES_PRESENT }, (noRulesPresent) => {
@@ -22,6 +31,15 @@ const Popup: React.FC = () => {
 
     chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.CHECK_IF_EXTENSION_ENABLED }, setIsExtensionEnabled);
   }, []);
+
+  useEffect(() => {
+    chrome.runtime
+      .sendMessage({
+        action: EXTENSION_MESSAGES.IS_EXTENSION_BLOCKED_ON_TAB,
+        tabUrl: currentTab?.url,
+      })
+      .then(setIsBlockedOnTab);
+  }, [currentTab]);
 
   const handleToggleExtensionStatus = useCallback(() => {
     chrome.runtime.sendMessage({ action: EXTENSION_MESSAGES.TOGGLE_EXTENSION_STATUS }, (updatedStatus) => {
@@ -40,11 +58,17 @@ const Popup: React.FC = () => {
           handleToggleExtensionStatus={handleToggleExtensionStatus}
         />
         <div className="popup-body">
-          {!isExtensionEnabled && <div className="extension-paused-overlay"></div>}
-          <div className="popup-content">
-            {ifNoRulesPresent ? <HttpsRuleOptions /> : <PopupTabs />}
-            <SessionRecordingView />
-          </div>
+          {isBlockedOnTab ? (
+            <BlockedExtensionView />
+          ) : (
+            <>
+              {!isExtensionEnabled && <div className="extension-paused-overlay"></div>}
+              <div className="popup-content">
+                {ifNoRulesPresent ? <HttpsRuleOptions /> : <PopupTabs />}
+                <SessionRecordingView />
+              </div>
+            </>
+          )}
         </div>
         <div className="popup-footer">
           <div className="extension-version">v{getExtensionVersion()}</div>
--- common/constants.js
+++ common/constants.js
@@ -218,6 +218,7 @@ CONSTANTS.STORAGE_KEYS = {
   SEND_EXECUTION_EVENTS: "sendExecutionEvents",
   TEST_REPORTS: "testReports",
   IMPLICIT_RULE_TESTING_WIDGET_CONFIG: "implicit_rule_testing_widget_config",
+  BLOCKED_DOMAINS: "blocked_domains",
 };
 
 CONSTANTS.REQUEST_STATE = {
