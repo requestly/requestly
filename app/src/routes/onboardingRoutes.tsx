b2d987cfed3e8a27dd8d72b22b1bc1c8b5ad7ea6
rohanmathur91 <mathurrohan04@gmail.com>
2024-07-15 19:10:37 +0530

    cleanup

--- app/src/routes/onboardingRoutes.tsx
+++ app/src/routes/onboardingRoutes.tsx
@@ -1,16 +1,11 @@
 import { Navigate, RouteObject } from "react-router-dom";
 import PATHS from "config/constants/sub/paths";
-import RuleSelection from "components/landing/ruleSelection";
 import InviteView from "views/misc/Invite";
 
 export const onboardingRoutes: RouteObject[] = [
   {
     path: PATHS.ONBOARDING.RELATIVE,
     element: <Navigate to={PATHS.RULES.MY_RULES.ABSOLUTE} />,
-  },
-  {
-    path: PATHS.RULES.CREATE,
-    element: <RuleSelection />,
   },
   {
     path: PATHS.INVITE.RELATIVE,
