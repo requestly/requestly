5d34538b72918d3575ad6597a2a90e4bfc7dd5f4
rohanmathur91 <mathurrohan04@gmail.com>
2024-07-15 19:10:30 +0530

    chore: add analytics

--- app/src/features/rules/components/RulesSidebar/RulesCreateModeSidebar/RulesCreateModeSidebar.tsx
+++ app/src/features/rules/components/RulesSidebar/RulesCreateModeSidebar/RulesCreateModeSidebar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { RuleSelectionList } from "features/rules/screens/rulesList/components/RulesList/components";
 import BotIcon from "assets/icons/bot.svg?react";
 import { RQButton } from "lib/design-system/components";
+import { trackRuleTypeSwitched } from "modules/analytics/events/common/rules";
 import "./RulesCreateModeSidebar.scss";
 
 export const RulesCreateModeSidebar: React.FC = () => {
@@ -20,7 +21,13 @@ export const RulesCreateModeSidebar: React.FC = () => {
         </div>
       </RQButton>
 
-      <RuleSelectionList premiumIconSource="rule_sidebar" />
+      <RuleSelectionList
+        source="rule_sidebar"
+        premiumIconSource="rule_sidebar"
+        onRuleItemClick={(ruleType) => {
+          trackRuleTypeSwitched(ruleType, "rule_sidebar");
+        }}
+      />
     </div>
   );
 };
