d559031fcb062aff78b86198ba03586f2fcb4ab0
RuntimeTerror10 <parthb.git@gmail.com>
2024-07-15 19:10:44 +0530

    add sessions info tab

--- app/src/features/sessionsV2/components/SessionViewerBottomSheet/components/SessionInfo/SessionInfo.tsx
+++ app/src/features/sessionsV2/components/SessionViewerBottomSheet/components/SessionInfo/SessionInfo.tsx
@@ -1,29 +1,72 @@
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import { useSelector } from "react-redux";
+import { CgAlignCenter } from "@react-icons/all-files/cg/CgAlignCenter";
+import { MdOutlineViewHeadline } from "@react-icons/all-files/md/MdOutlineViewHeadline";
+import { IoIosGlobe } from "@react-icons/all-files/io/IoIosGlobe";
+import { IoMdTime } from "@react-icons/all-files/io/IoMdTime";
+import { MdToday } from "@react-icons/all-files/md/MdToday";
+import { MdOutlinePerson } from "@react-icons/all-files/md/MdOutlinePerson";
+import { CustomInlineInput } from "componentsV2/CustomInlineInput/CustomInlineInput";
+import { getSessionRecordingAttributes, getSessionRecordingMetaData } from "store/features/session-recording/selectors";
+import { epochToDateAndTimeString, msToHoursMinutesAndSeconds } from "utils/DateTimeUtils";
 import "./sessionInfo.scss";
 
 export const SessionInfo: React.FC = () => {
+  const sessionMetaData = useSelector(getSessionRecordingMetaData);
+  const sessionAttributes = useSelector(getSessionRecordingAttributes);
+  const [sessionName, setSessionName] = useState(sessionMetaData?.name || "");
+  const [sessionDescription, setSessionDescription] = useState(sessionMetaData?.description || "");
+
+  const handleSessionNameChange = useCallback((value: string) => {
+    setSessionName(value);
+  }, []);
+
+  const handleSessionDescriptionChange = useCallback((value: string) => {
+    setSessionDescription(value);
+  }, []);
+
+  const sessionInfoData = useMemo(() => {
+    return [
+      {
+        icon: <CgAlignCenter />,
+        label: "Name",
+        value: (
+          <CustomInlineInput
+            value={sessionName}
+            placeholder="Enter session name"
+            valueChangeCallback={handleSessionNameChange}
+          />
+        ),
+      },
+      {
+        icon: <MdOutlineViewHeadline />,
+        label: "Description",
+        value: (
+          <CustomInlineInput
+            value={sessionDescription}
+            placeholder="Enter session description"
+            valueChangeCallback={handleSessionDescriptionChange}
+          />
+        ),
+      },
+      { icon: <IoIosGlobe />, label: "Page URL", value: sessionAttributes?.url },
+      { icon: <IoMdTime />, label: "Duration", value: msToHoursMinutesAndSeconds(sessionAttributes?.duration ?? 0) },
+      { icon: <MdToday />, label: "Recorded at", value: epochToDateAndTimeString(sessionAttributes?.startTime) },
+      { icon: <MdOutlinePerson />, label: "Recorded by", value: "1:00 PM" },
+    ];
+  }, [handleSessionNameChange, sessionName, handleSessionDescriptionChange, sessionDescription, sessionAttributes]);
+
   return (
     <div className="sessions-info-container">
-      <div className="sessions-info-item">
-        <span className="sessions-info-item-label">Session ID</span>
-        <span className="sessions-info-item-value">123456789</span>
-      </div>
-      <div className="sessions-info-item">
-        <span className="sessions-info-item-label">Session URL</span>
-        <span className="sessions-info-item-value">https://www.example.com</span>
-      </div>
-      <div className="sessions-info-item">
-        <span className="sessions-info-item-label">Session Duration</span>
-        <span className="sessions-info-item-value">1 hour</span>
-      </div>
-      <div className="sessions-info-item">
-        <span className="sessions-info-item-label">Session Start Time</span>
-        <span className="sessions-info-item-value">12:00 PM</span>
-      </div>
-      <div className="sessions-info-item">
-        <span className="sessions-info-item-label">Session End Time</span>
-        <span className="sessions-info-item-value">1:00 PM</span>
-      </div>
+      {sessionInfoData.map((info, index) => (
+        <div className="sessions-info-item" key={index}>
+          <span className="sessions-info-item-label">
+            {info.icon}
+            {info.label}
+          </span>
+          <span className="sessions-info-item-value">{info.value}</span>
+        </div>
+      ))}
     </div>
   );
 };
