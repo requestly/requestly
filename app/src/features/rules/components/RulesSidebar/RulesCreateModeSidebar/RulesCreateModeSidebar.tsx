799c712c27e2373bbeb393769d2f10a49e487f8c
rohanmathur91 <mathurrohan04@gmail.com>
2024-07-15 18:18:11 +0530

    add ask ai button

--- app/src/features/rules/components/RulesSidebar/RulesCreateModeSidebar/RulesCreateModeSidebar.tsx
+++ app/src/features/rules/components/RulesSidebar/RulesCreateModeSidebar/RulesCreateModeSidebar.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { RuleSelectionList } from "features/rules/screens/rulesList/components/RulesList/components";
+import BotIcon from "assets/icons/bot.svg?react";
+import { RQButton } from "lib/design-system/components";
 import "./RulesCreateModeSidebar.scss";
 
 export const RulesCreateModeSidebar: React.FC = () => {
   return (
     <div className="rules-create-mode-sidebar-container">
-      {/* TODO: add ai input */}
+      <RQButton
+        className="ask-ai-btn"
+        onClick={() => {
+          // trackAskAIClicked();
+          // setIsRequestBotVisible(true);
+        }}
+      >
+        <div className="ask-ai-btn-content">
+          <BotIcon />
+          Ask AI for the right rule
+        </div>
+      </RQButton>
+
       <RuleSelectionList premiumIconSource="rule_sidebar" />
     </div>
   );
