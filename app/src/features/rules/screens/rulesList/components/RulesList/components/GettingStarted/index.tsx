db217a15c79cb0eff9a5e784bf8be2cd6120dec5
rohanmathur91 <mathurrohan04@gmail.com>
2024-07-18 18:53:15 +0530

    ab setup

--- app/src/features/rules/screens/rulesList/components/RulesList/components/GettingStarted/index.tsx
+++ app/src/features/rules/screens/rulesList/components/RulesList/components/GettingStarted/index.tsx
@@ -1,7 +1,8 @@
 import { GettingStarted as GettingStartedV1 } from "./v1";
 import { GettingStarted as GettingStartedV2 } from "./v2";
+import { useFeatureValue } from "@growthbook/growthbook-react";
 
 export const GettingStarted = () => {
-  const isff = 0;
-  return isff ? <GettingStartedV1 /> : <GettingStartedV2 />;
+  const variation = useFeatureValue("rules_screen_empty_state", "control");
+  return variation === "control" ? <GettingStartedV1 /> : <GettingStartedV2 />;
 };
