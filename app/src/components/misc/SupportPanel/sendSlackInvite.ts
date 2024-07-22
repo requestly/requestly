9efc332427dd2a46b4800ce7d0d8c49f8c1d2aa1
Dinesh Thakur <dineshthakur.dev@gmail.com>
2024-07-23 01:04:27 +0530

    Slack Invite data structure change and css changes

--- app/src/components/misc/SupportPanel/sendSlackInvite.ts
+++ app/src/components/misc/SupportPanel/sendSlackInvite.ts
@@ -1,36 +1,27 @@
 import { message } from "antd";
 import { getFunctions, httpsCallable } from "firebase/functions";
 
-interface ResInterface {
-  success: boolean;
-  message: string;
-  data: object;
-}
-
 export default async function sendSlackInvite() {
   const hide = message.loading({
     content: "Sending Slack Connect Invitation",
     className: "slack-connect-toast",
     duration: 0,
   });
-  const sendSlackInviation = httpsCallable<null, ResInterface>(getFunctions(), "slackConnect-sendSlackInvitation");
-  try {
-    const res = await sendSlackInviation();
-    if (res.data.success) {
+  const sendSlackInviation = httpsCallable<null, boolean>(getFunctions(), "slackConnect-sendSlackInvitation");
+  sendSlackInviation()
+    .then((res) => {
+      if (res.data) {
+        hide();
+        message.success({
+          content: "Slack invitation sent successfully",
+          className: "slack-connect-toast",
+        });
+      } else if (!res.data) {
+        hide();
+      }
+    })
+    .catch((err) => {
       hide();
-      message.success({
-        content: res.data.message || "Slack invitation sent successfully",
-        className: "slack-connect-toast",
-      });
-    } else if (!res.data.success) {
-      hide();
-      message.error({
-        content: res.data.message || "Error sending slack invitation",
-        className: "slack-connect-toast",
-      });
-    }
-  } catch (err) {
-    hide();
-    console.error("Error sending slack invitation", err);
-  }
+      console.error("Error sending slack invite", err);
+    });
 }
