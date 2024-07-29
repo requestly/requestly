80c2d089c6fd1c7beadc6a988d8fcabcd06c8bd6
rohanmathur91 <mathurrohan04@gmail.com>
2024-07-29 16:32:33 +0530

    fix: invite layout

--- app/src/layouts/MinimalLayout/index.tsx
+++ app/src/layouts/MinimalLayout/index.tsx
@@ -4,12 +4,31 @@ import RQLogo from "assets/img/brand/rq_logo_full.svg";
 import "./index.scss";
 import { redirectToRoot } from "utils/RedirectionUtils";
 import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getActiveModals } from "store/selectors";
+import AuthModal from "components/authentication/AuthModal";
+import { actions } from "store";
 
 const MinimalLayout = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const activeModals = useSelector(getActiveModals);
+
+  const toggleAuthModal = () => {
+    // @ts-ignore
+    dispatch(actions.toggleActiveModal({ modalName: "authModal" }));
+  };
 
   return (
     <>
+      {activeModals.authModal.isActive ? (
+        <AuthModal
+          isOpen={activeModals.authModal.isActive}
+          toggle={() => toggleAuthModal()}
+          {...activeModals.authModal.props}
+        />
+      ) : null}
+
       <Layout.Header className="minimal-layout-navbar">
         <img className="logo" src={RQLogo} alt="requestly logo" onClick={() => redirectToRoot(navigate)} />
         <HeaderUser />
