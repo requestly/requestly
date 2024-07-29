eca09e76c7db9aeb69538736f6cd0cbeffea4675
rohanmathur91 <mathurrohan04@gmail.com>
2024-07-29 13:57:11 +0530

    add feature section

--- app/src/features/pricing/components/PricingPage/components/PricingPageFooter/index.tsx
+++ app/src/features/pricing/components/PricingPage/components/PricingPageFooter/index.tsx
@@ -62,7 +62,7 @@ const PricingPageFooter: React.FC = () => {
         </div>
         <div className="content">
           <Row className="links-container" justify="space-between" align="top" gutter={[16, 16]}>
-            <Col xs={24} md={6}>
+            <Col xs={24} md={4}>
               <Title className="title" level={4}>
                 Get
               </Title>
@@ -84,7 +84,60 @@ const PricingPageFooter: React.FC = () => {
                 </Link>
               </Space>
             </Col>
-            <Col xs={24} md={6}>
+            <Col xs={24} md={4}>
+              <Title className="title" level={4}>
+                Features
+              </Title>
+              <Space direction="vertical">
+                <Link target="_blank" href="https://requestly.com/products/web-debugger/redirect-rule/">
+                  Redirect URL
+                </Link>
+                <Link target="_blank" href="https://requestly.com/products/web-debugger/replace-rule-switch-hosts/">
+                  Replace Rule
+                </Link>
+                <Link
+                  target="_blank"
+                  href="https://requestly.com/products/web-debugger/best-chrome-extension-to-inject-javascript-and-css-on-any-webpage/"
+                >
+                  Insert Custom Script
+                </Link>
+                <Link target="_blank" href="https://requestly.com/products/modify-and-mock-graphql-apis/">
+                  Override GraphQL APIs
+                </Link>
+                <Link target="_blank" href="https://requestly.com/products/web-debugger/modify-http-headers/">
+                  Modify HTTP Headers
+                </Link>
+                <Link
+                  target="_blank"
+                  href="https://requestly.com/products/web-debugger/intercept-modify-mock-api-responses-test-develop-like-an-expert/"
+                >
+                  Modify API Response
+                </Link>
+                <Link target="_blank" href="https://requestly.com/products/web-debugger/delay-http-request/">
+                  Delay request
+                </Link>
+                <Link target="_blank" href="https://requestly.com/products/web-debugger/cross-device-testing/">
+                  Cross Device Testing
+                </Link>
+              </Space>
+            </Col>
+            <Col xs={24} md={4}>
+              <Title className="title" level={4}>
+                Comparisons
+              </Title>
+              <Space direction="vertical">
+                <Link href="https://requestly.com/alternatives/a-better-alternate-to-charles-proxy/">
+                  Charles Proxy VS Requestly
+                </Link>
+                <Link href="https://requestly.com/alternatives/fiddler/">Fiddler VS Requestly</Link>
+                <Link href="https://requestly.com/alternatives/a-better-and-well-documented-alternate-to-modheader/">
+                  MohHeader VS Requestly
+                </Link>
+                <Link href="https://requestly.com/alternatives/proxyman/">Proxyman VS Requestly</Link>
+                <Link href="https://requestly.com/alternatives/wireshark/">WireShark VS Requestly</Link>
+              </Space>
+            </Col>
+            <Col xs={24} md={4}>
               <Title className="title" level={4}>
                 Resources
               </Title>
@@ -109,9 +162,9 @@ const PricingPageFooter: React.FC = () => {
                 </Link>
               </Space>
             </Col>
-            <Col xs={24} md={6}>
+            <Col xs={24} md={4}>
               <Title className="title" level={4}>
-                More Resources
+                Compliance & Security
               </Title>
               <Space direction="vertical">
                 <Link onClick={handleDocRequiredOnClick}>SOC 2 Report</Link>
@@ -119,22 +172,6 @@ const PricingPageFooter: React.FC = () => {
                 <Link onClick={handleDocRequiredOnClick}>W9 Form</Link>
                 <Link onClick={handleDocRequiredOnClick}>Data Processing Agreement (DPA)</Link>
                 <Link onClick={handleDocRequiredOnClick}>Request GDPR Data Removal </Link>
-              </Space>
-            </Col>
-            <Col xs={24} md={6}>
-              <Title className="title" level={4}>
-                Comparisons
-              </Title>
-              <Space direction="vertical">
-                <Link href="https://requestly.com/alternatives/a-better-alternate-to-charles-proxy/">
-                  Charles Proxy VS Requestly
-                </Link>
-                <Link href="https://requestly.com/alternatives/fiddler/">Fiddler VS Requestly</Link>
-                <Link href="https://requestly.com/alternatives/a-better-and-well-documented-alternate-to-modheader/">
-                  MohHeader VS Requestly
-                </Link>
-                <Link href="https://requestly.com/alternatives/proxyman/">Proxyman VS Requestly</Link>
-                <Link href="https://requestly.com/alternatives/wireshark/">WireShark VS Requestly</Link>
               </Space>
             </Col>
           </Row>
