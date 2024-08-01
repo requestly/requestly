94077a7c4ef923592f907933c60c8c8280e2a831
RuntimeTerror10 <parthb.git@gmail.com>
2024-08-01 19:21:33 +0530

    ENGG-2109: fix saved session viewer description layout

--- app/src/componentsV2/InlineInput/InlineInput.tsx
+++ app/src/componentsV2/InlineInput/InlineInput.tsx
@@ -7,43 +7,76 @@ interface Props {
   value: string;
   placeholder: string;
   disabled?: boolean;
+  textarea?: boolean;
   onChange: (value: String) => void;
   onBlur?: () => void;
 }
 
-export const InlineInput: React.FC<Props> = ({ value, placeholder, onChange, onBlur, disabled = false }) => {
+export const InlineInput: React.FC<Props> = ({
+  value,
+  placeholder,
+  onChange,
+  onBlur,
+  disabled = false,
+  textarea = false,
+}) => {
   const [isEditable, setIsEditable] = useState(false);
 
   return (
     <div className="inline-input-container">
       <Row className="inline-input-row">
         {value?.length === 0 || isEditable ? (
-          <Input
-            className="active-inline-input"
-            onFocus={() => setIsEditable(true)}
-            onBlur={() => {
-              setIsEditable(false);
-              onBlur?.();
-            }}
-            bordered={false}
-            autoFocus={true}
-            spellCheck={false}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder={placeholder}
-            onPressEnter={() => setIsEditable(false)}
-          />
+          <>
+            {textarea ? (
+              <Input.TextArea
+                className="active-inline-textarea"
+                value={value}
+                spellCheck={false}
+                autoFocus={true}
+                placeholder={placeholder}
+                autoSize={{ minRows: 2, maxRows: 4 }}
+                onChange={(e) => onChange(e.target.value)}
+                onPressEnter={() => setIsEditable(false)}
+                onFocus={() => setIsEditable(true)}
+                onBlur={() => {
+                  setIsEditable(false);
+                  onBlur?.();
+                }}
+              />
+            ) : (
+              <Input
+                className="active-inline-input"
+                onFocus={() => setIsEditable(true)}
+                onBlur={() => {
+                  setIsEditable(false);
+                  onBlur?.();
+                }}
+                bordered={false}
+                autoFocus={true}
+                spellCheck={false}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+                placeholder={placeholder}
+                onPressEnter={() => setIsEditable(false)}
+              />
+            )}
+          </>
         ) : (
           <div className="inactive-inline-input">
             <Typography.Text
-              ellipsis={true}
+              ellipsis={!textarea}
               onClick={() => {
                 if (!disabled) setIsEditable(true);
               }}
             >
               {value || placeholder}
             </Typography.Text>
-            {!disabled && <MdOutlineEdit onClick={() => setIsEditable(true)} />}
+            {!disabled && (
+              <MdOutlineEdit
+                className={`${textarea ? "align-self-start" : "align-self-center"}`}
+                onClick={() => setIsEditable(true)}
+              />
+            )}
           </div>
         )}
       </Row>
