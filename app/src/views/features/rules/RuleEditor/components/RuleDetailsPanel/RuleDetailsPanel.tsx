540c7b13f8bc7c2bcf0930720c1efd970277da16
rohanmathur91 <mathurrohan04@gmail.com>
2024-08-01 17:01:38 +0530

    refactor: rule details

--- app/src/views/features/rules/RuleEditor/components/RuleDetailsPanel/RuleDetailsPanel.tsx
+++ app/src/views/features/rules/RuleEditor/components/RuleDetailsPanel/RuleDetailsPanel.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { RULE_DETAILS } from "./constants";
+import React, { ReactNode, useEffect } from "react";
 import { RuleType } from "types";
 import { Button } from "antd";
 import { MdMenuBook } from "@react-icons/all-files/md/MdMenuBook";
@@ -17,15 +16,25 @@ import { useNavigate } from "react-router-dom";
 import PATHS from "config/constants/sub/paths";
 import "./RuleDetailsPanel.scss";
 
+export type RuleDetails = {
+  type: RuleType;
+  name: string;
+  icon?: () => ReactNode;
+  description: string;
+  useCases?: { useCase: string; suggestedTemplateId?: string }[];
+  documentationLink: string;
+};
+
 interface RuleDetailsPanelProps {
   isSample?: boolean;
-  ruleType: RuleType | undefined;
+  ruleDetails: RuleDetails;
   source: "docs_sidebar" | "new_rule_editor";
 }
 
-export const RuleDetailsPanel: React.FC<RuleDetailsPanelProps> = ({ ruleType, source, isSample = false }) => {
+export const RuleDetailsPanel: React.FC<RuleDetailsPanelProps> = ({ source, isSample = false, ruleDetails }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { type: ruleType, name, description, useCases, documentationLink } = ruleDetails;
 
   const handleCloseClick = () => {
     trackRuleDetailsPanelClosed(ruleType, source);
@@ -58,15 +67,15 @@ export const RuleDetailsPanel: React.FC<RuleDetailsPanelProps> = ({ ruleType, so
 
       <div className="details-panel">
         <div className="rule-details-container">
-          <div className="title">{RULE_DETAILS[ruleType].name}</div>
-          <div className="description">{RULE_DETAILS[ruleType].description}</div>
+          <div className="title">{name}</div>
+          <div className="description">{description}</div>
 
           <div className="use-cases-container">
             <div className="title">Use cases</div>
 
             <ul className="use-cases-list">
-              {RULE_DETAILS[ruleType].useCases?.length > 0 &&
-                RULE_DETAILS[ruleType].useCases?.map(({ useCase, suggestedTemplateId }, index) => {
+              {useCases?.length > 0 &&
+                useCases?.map(({ useCase, suggestedTemplateId }, index) => {
                   return (
                     <li key={index} className="use-case-list-item">
                       <div className="use-case">
@@ -113,7 +122,7 @@ export const RuleDetailsPanel: React.FC<RuleDetailsPanelProps> = ({ ruleType, so
         target="_blank"
         rel="noreferrer"
         className="link documentation-link"
-        href={RULE_DETAILS[ruleType].documentationLink}
+        href={documentationLink}
         icon={<MdMenuBook className="anticon" />}
         onClick={() => trackRuleDetailsPanelDocsClicked(ruleType, source)}
       >
