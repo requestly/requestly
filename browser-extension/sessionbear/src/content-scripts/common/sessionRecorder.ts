926c3b377f99e5eac184619bce69114d3dcf3f6f
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-08-02 09:54:20 +0530

    Fix: authentication flow in draft session iframe (#2036)

--- browser-extension/sessionbear/src/content-scripts/common/sessionRecorder.ts
+++ browser-extension/sessionbear/src/content-scripts/common/sessionRecorder.ts
@@ -343,14 +343,14 @@ const injectDraftSessionViewer = async () => {
     postSaveSessionWidget.remove();
   }
 
-  const userId: string = await getRecord(STORAGE_KEYS.USER_ID);
+  const refreshToken: string = await getRecord(STORAGE_KEYS.REFRESH_TOKEN);
   const activeWorkspaceId: string = await getRecord(STORAGE_KEYS.ACTIVE_WORKSPACE_ID);
 
   const newSessionViewer = document.createElement(CUSTOM_ELEMENTS.DRAFT_SESSION_VIEWER);
   newSessionViewer.classList.add("rq-element");
 
   const iframeUrlParams = new URLSearchParams();
-  if (userId) iframeUrlParams.set("userId", userId);
+  if (refreshToken) iframeUrlParams.set("refreshToken", refreshToken);
   if (activeWorkspaceId) iframeUrlParams.set("workspaceId", activeWorkspaceId);
 
   newSessionViewer.setAttribute(
