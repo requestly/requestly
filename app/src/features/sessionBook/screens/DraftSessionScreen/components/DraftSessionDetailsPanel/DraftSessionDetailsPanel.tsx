6762f7f4fcdeae2ec8f3cf8c49b064913b32e0ea
RuntimeTerror10 <parthb.git@gmail.com>
2024-08-13 09:58:48 +0530

    Add split and drawer layout for bottom sheet

--- app/src/features/sessionBook/screens/DraftSessionScreen/components/DraftSessionDetailsPanel/DraftSessionDetailsPanel.tsx
+++ app/src/features/sessionBook/screens/DraftSessionScreen/components/DraftSessionDetailsPanel/DraftSessionDetailsPanel.tsx
@@ -45,6 +45,7 @@ const DraftSessionDetailsPanel: React.FC<DraftSessionDetailsPanelProps> = ({ pla
     <BottomSheetProvider defaultPlacement={BottomSheetPlacement.BOTTOM}>
       <div className="session-details-panel-wrapper">
         <BottomSheetLayout
+          layout="drawer"
           hideBottomSheet={isOpenedInIframe}
           initialOffset={-390}
           bottomSheet={<SessionViewerBottomSheet playerTimeOffset={playerTimeOffset} disableDocking />}
