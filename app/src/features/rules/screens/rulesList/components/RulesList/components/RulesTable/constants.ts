08b7b2c7cb2ef9bab63aeb2e7f7b8fc023fb47b8
rohanmathur91 <mathurrohan04@gmail.com>
2024-08-14 15:50:24 +0530

    fix: description

--- app/src/features/rules/screens/rulesList/components/RulesList/components/RulesTable/constants.ts
+++ app/src/features/rules/screens/rulesList/components/RulesList/components/RulesTable/constants.ts
@@ -11,7 +11,7 @@ export const sampleRuleDetails: Record<string, { id: string; demoLink: string; d
       type: RuleType.SCRIPT,
       name: "Inject CSS/JS on a website",
       description:
-        "The Insert Script rule allows you to inject custom JavaScript or styles into any website. With this rule, the background of the items is changed by injecting custom CSS. Toggle the rule on and off and reload the website to observe the differences.",
+        "The Insert Script rule allows you to inject custom JavaScript or styles into any website. With this rule, the background of the items is changed by injecting custom CSS.",
       documentationLink: LINKS.REQUESTLY_SCRIPT_RULE_DOCS,
     },
   },
@@ -22,7 +22,7 @@ export const sampleRuleDetails: Record<string, { id: string; demoLink: string; d
       type: RuleType.RESPONSE,
       name: "Override GraphQL API Response",
       description:
-        "The Modify Response rule allows overriding existing GraphQL responses. For all requests containing operationName = Products, we override the existing responses with a specific one. As a result, all product responses will display details as if they were apples, showing apple inventory information. Toggle the rule on and off and reload the website to observe the differences.",
+        "The Modify Response rule allows overriding existing GraphQL responses. For all requests containing operationName = Products, we override the existing responses with a specific one. As a result, all product responses will display details as if they were apples, showing apple inventory information.",
       documentationLink: LINKS.REQUESTLY_RESPONSE_RULE_DOCS,
     },
   },
@@ -33,7 +33,7 @@ export const sampleRuleDetails: Record<string, { id: string; demoLink: string; d
       type: RuleType.REDIRECT,
       name: "Redirect URL to another URL",
       description:
-        "The Redirect Rule changes the HTTP request location to a new destination, serving the response from the new location transparently. With this rule, we redirect the API that fetches bakery items to the one that fetches gadgets and electronic items. This changes the listed items in the store from juices and bread to gaming consoles and computers. Toggle the rule on and off and reload the website to observe the differences.",
+        "The Redirect Rule changes the HTTP request location to a new destination, serving the response from the new location transparently. With this rule, we redirect the API that fetches bakery items to the one that fetches gadgets and electronic items. This changes the listed items in the store from juices and bread to gaming consoles and computers.",
       documentationLink: LINKS.REQUESTLY_REDIRECT_RULE_DOCS,
     },
   },
@@ -44,7 +44,7 @@ export const sampleRuleDetails: Record<string, { id: string; demoLink: string; d
       type: RuleType.RESPONSE,
       name: "Modify HTTP Status Code",
       description:
-        "The Modify Response rule allows overriding the status code of existing API responses. Instead of the regular responses, we'll be overriding the response code with 404. As a result, the products will continue loading indefinitely on the site. Toggle the rule on and off and reload the website to observe the differences.",
+        "The Modify Response rule allows overriding the status code of existing API responses. Instead of the regular responses, we'll be overriding the response code with 404. As a result, the products will continue loading indefinitely on the site.",
       documentationLink: LINKS.REQUESTLY_RESPONSE_RULE_DOCS,
     },
   },
@@ -54,7 +54,7 @@ export const sampleRuleDetails: Record<string, { id: string; demoLink: string; d
     details: {
       type: RuleType.REPLACE,
       name: "Replace a part of the URL",
-      description: `The Replace Rule allows replacing a particular string in the URL with the desired string. In the playground, an API initially requested a list of bakery items, but we want a list of grocery items instead. By applying the Replace Rule, the category is switched from "bakery" to "grocery". This changes the listed items from juices and bread to fruits and vegetables. Toggle the rule on and off and reload the website to observe the differences.`,
+      description: `The Replace Rule allows replacing a particular string in the URL with the desired string. In the playground, an API initially requested a list of bakery items, but we want a list of grocery items instead. By applying the Replace Rule, the category is switched from "bakery" to "grocery". This changes the listed items from juices and bread to fruits and vegetables.`,
       documentationLink: LINKS.REQUESTLY_REPLACE_RULE_DOCS,
     },
   },
@@ -74,7 +74,7 @@ export const sampleRuleDetails: Record<string, { id: string; demoLink: string; d
     details: {
       type: RuleType.RESPONSE,
       name: "Override REST API Response",
-      description: `The Modify Response rule allows overriding existing API responses. In this example, we modify the API response that provides product descriptions. Click on “Try this rule” to go to the playground and check the listed items. You will notice the descriptions of each item have changed. Toggle the rule on and off and reload the website to observe the differences.`,
+      description: `The Modify Response rule allows overriding existing API responses. In this example, we modify the API response that provides product descriptions. Click on “Try this rule” to go to the playground and check the listed items. You will notice the descriptions of each item have changed.`,
       documentationLink: LINKS.REQUESTLY_RESPONSE_RULE_DOCS,
     },
   },
