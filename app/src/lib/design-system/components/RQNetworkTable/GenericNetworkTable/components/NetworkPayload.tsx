091c1765d94246a6b566277b4e9670a93bb83568
nsrCodes <navdeepsinghrathore2000@gmail.com>
2024-08-16 09:56:42 +0530

    feat: dynamically fetch payload based on extra fields in the harEntry

--- app/src/lib/design-system/components/RQNetworkTable/GenericNetworkTable/components/NetworkPayload.tsx
+++ app/src/lib/design-system/components/RQNetworkTable/GenericNetworkTable/components/NetworkPayload.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import { ThemeProvider } from "@devtools-ds/themes";
 import { ObjectInspector } from "@devtools-ds/object-inspector";
@@ -6,36 +6,65 @@ import { NetworkLogProperty } from "./NetworkLogProperty";
 
 type Props = {
   label?: string;
-  payload: any;
+  payload?: any;
   isPayloadTooLarge?: boolean;
+  fetchPayload?: () => Promise<any>;
 };
 
-export const NetworkPayload: React.FC<Props> = ({ label, payload }) => {
-  const parsedPayload = useMemo(() => {
-    if (typeof payload === "string") {
-      try {
-        return JSON.parse(payload);
-      } catch (e) {
-        // skip
+export const NetworkPayload: React.FC<Props> = ({ label, payload, fetchPayload }) => {
+  const [fetching, setFetching] = useState(false);
+  const [finalPayload, setFinalPayload] = useState<any>();
+  useEffect(() => {
+    if (payload) {
+      if (typeof payload === "string") {
+        try {
+          return JSON.parse(payload);
+        } catch (e) {
+          // skip
+        }
       }
+      setFinalPayload(payload);
+    } else if (fetchPayload && typeof fetchPayload === "function") {
+      setFetching(true);
+      fetchPayload()
+        .then((fetchedPayload) => {
+          if (typeof fetchedPayload === "string") {
+            try {
+              setFinalPayload(JSON.parse(fetchedPayload));
+            } catch (e) {
+              setFinalPayload(fetchedPayload);
+            }
+          } else {
+            setFinalPayload(fetchedPayload);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .finally(() => {
+          setFetching(false);
+        });
     }
-    return payload;
-  }, [payload]);
+  }, [payload, fetchPayload]);
 
-  if (typeof parsedPayload === "object") {
+  if (fetching) {
+    return <NetworkLogProperty label={label}>Loading...</NetworkLogProperty>;
+  }
+
+  if (typeof finalPayload === "object") {
     return (
       <NetworkLogProperty label={label}>
         <ThemeProvider theme={"chrome"} colorScheme={"dark"}>
-          <ObjectInspector data={parsedPayload} includePrototypes={false} className="network-payload-object" />
+          <ObjectInspector data={finalPayload} includePrototypes={false} className="network-payload-object" />
         </ThemeProvider>
       </NetworkLogProperty>
     );
   }
 
-  if (payload && typeof payload === "string") {
+  if (finalPayload && typeof finalPayload === "string") {
     return (
       <NetworkLogProperty label={label} isCodeBlock>
-        {payload}
+        {finalPayload}
       </NetworkLogProperty>
     );
   }
--- app/src/lib/design-system/components/RQNetworkTable/GenericNetworkTable/types.ts
+++ app/src/lib/design-system/components/RQNetworkTable/GenericNetworkTable/types.ts
@@ -1,7 +1,12 @@
 import { Entry } from "har-format";
 import { Column as ResourceTableColumn } from "@requestly-ui/resource-table";
 
-export type HarEntry = Entry;
+export interface HarEntry extends Entry {
+  _RQ?: {
+    requestBodyPath?: string;
+    responseBodyPath?: string;
+  };
+}
 
 export type NetworkEntry = Partial<HarEntry>; // Add more in future. WSEntry || GRPCEntry
 
