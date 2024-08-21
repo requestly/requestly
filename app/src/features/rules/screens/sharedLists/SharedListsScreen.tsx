54b0ee774ed1c2dc00e322a4bb78b3150e2c3840
RuntimeTerror10 <parthb.git@gmail.com>
2024-08-21 16:47:58 +0530

    sharedlist screen

--- app/src/features/rules/screens/sharedLists/SharedListsScreen.tsx
+++ app/src/features/rules/screens/sharedLists/SharedListsScreen.tsx
@@ -1,9 +1,12 @@
 import { SharedLists } from "./components/SharedLists/SharedLists";
+import "./sharedListScreen.scss";
 
 export const SharedListsScreen = () => {
   return (
-    <div className="sharedlists-screen">
-      <SharedLists />
+    <div className="sharedlists-screen-container">
+      <div className="sharedlists-screen">
+        <SharedLists />
+      </div>
     </div>
   );
 };
