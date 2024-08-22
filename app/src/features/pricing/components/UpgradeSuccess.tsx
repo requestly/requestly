7c8505fc5eb332928881d621dc1f56ca4bdb0ccd
Nafees Nehar <nafees87n@gmail.com>
2024-08-22 20:21:00 +0530

    added upgrade success component

--- app/src/features/pricing/components/UpgradeSuccess.tsx
+++ app/src/features/pricing/components/UpgradeSuccess.tsx
@@ -6,13 +6,27 @@ import PageLoader from "components/misc/PageLoader";
 import { getUserAuthDetails } from "store/selectors";
 import { getBillingTeamRedirectURL } from "backend/billing";
 import PATHS from "config/constants/sub/paths";
+import { getFunctions, httpsCallable } from "firebase/functions";
+import { getPrettyPlanName } from "utils/FormattingHelper";
+import Logger from "lib/logger";
 
 export const UpgradeSuccess: React.FC = () => {
   const navigate = useNavigate();
   const user = useSelector(getUserAuthDetails);
+  const userPlanName = user?.details?.planDetails?.planName;
 
   useEffect(() => {
-    if (user.loggedIn) {
+    if (user.loggedIn && userPlanName) {
+      const sendPlanUpgradedNotification = httpsCallable(
+        getFunctions(),
+        "premiumNotifications-sendPlanUpgradedNotification"
+      );
+      sendPlanUpgradedNotification({
+        planName: getPrettyPlanName(userPlanName),
+      }).catch((e) => {
+        Logger.log(e);
+      });
+
       getBillingTeamRedirectURL(user?.details?.profile?.uid).then((redirectUrl) => {
         if (!redirectUrl) {
           navigate(PATHS.SETTINGS.BILLING.RELATIVE);
@@ -21,7 +35,7 @@ export const UpgradeSuccess: React.FC = () => {
         }
       });
     }
-  }, [navigate, user?.details?.profile?.uid, user.loggedIn]);
+  }, [navigate, user?.details?.profile?.uid, user.loggedIn, userPlanName]);
 
   return (
     <Card>
