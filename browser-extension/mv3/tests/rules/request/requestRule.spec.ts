9aa80ea1f9655ce41ad2bc6f71707cf423de7971
Nafees Nehar <nafees87n@gmail.com>
2024-08-26 20:51:55 +0530

    fix

--- browser-extension/mv3/tests/rules/request/requestRule.spec.ts
+++ browser-extension/mv3/tests/rules/request/requestRule.spec.ts
@@ -8,7 +8,7 @@ import testScenarios from "./testScenarios";
 
 test.describe("Request Rule", () => {
   testScenarios.forEach((scenario, i) => {
-    test.only(`${i + 1}. @RequestRule: ${scenario.description}`, async ({ appPage, context }) => {
+    test(`${i + 1}. @RequestRule: ${scenario.description}`, async ({ appPage, context }) => {
       await testRequestRule({ appPage, context, ...scenario });
     });
   });
