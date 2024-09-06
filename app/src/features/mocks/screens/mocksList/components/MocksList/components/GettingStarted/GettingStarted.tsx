fd69987c8ca3301a791a1f7e3affbf5f0199ca7e
Rohan Mathur <mathurrohan04@gmail.com>
2024-09-06 15:19:04 +0600

    Revert "[ENGG-2116] feat: import and export for mocks  (#2123)" (#2147)
    This reverts commit a13f9b8d625a0530b625721e283158f62c3c0ff3.

--- app/src/features/mocks/screens/mocksList/components/MocksList/components/GettingStarted/GettingStarted.tsx
+++ app/src/features/mocks/screens/mocksList/components/MocksList/components/GettingStarted/GettingStarted.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { GettingStartedWithMocks } from "./GettingStartedWithMocks/GettingStartedWithMocks";
-import { ImportMocksModalWrapper, MockUploaderModalWrapper, NewFileModalWrapper } from "features/mocks/modals";
+import { MockUploaderModalWrapper, NewFileModalWrapper } from "features/mocks/modals";
 import { MockListSource, MockType } from "components/features/mocksV2/types";
 import { useMocksActionContext } from "features/mocks/contexts/actions";
-import { SOURCE } from "modules/analytics/events/common/constants";
 
 interface Props {
   mockType: MockType;
   source: MockListSource;
-  forceRender: () => void;
 }
 
-export const GettingStarted: React.FC<Props> = ({ mockType, source, forceRender = () => {} }) => {
-  const { uploadMockAction, createNewMockAction, importMocksAction } = useMocksActionContext() ?? {};
+export const GettingStarted: React.FC<Props> = ({ mockType, source }) => {
+  const { uploadMockAction, createNewMockAction } = useMocksActionContext() ?? {};
 
   return (
     <>
@@ -20,13 +18,11 @@ export const GettingStarted: React.FC<Props> = ({ mockType, source, forceRender 
         mockType={mockType}
         handleCreateNew={() => createNewMockAction(mockType, source)}
         handleUploadAction={() => uploadMockAction(mockType)}
-        handleImportAction={() => importMocksAction(mockType, SOURCE.MOCKS_GETTING_STARTED)}
       />
 
       {/* TODO: move this into container */}
       <NewFileModalWrapper />
       <MockUploaderModalWrapper />
-      <ImportMocksModalWrapper forceRender={forceRender} />
     </>
   );
 };
