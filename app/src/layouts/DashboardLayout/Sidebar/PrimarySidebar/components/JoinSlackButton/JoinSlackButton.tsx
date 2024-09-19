76083dbb895a01fd301968fc0a8f6950e4a50e7d
RuntimeTerror10 <parthb.git@gmail.com>
2024-09-19 13:38:03 +0530

    ENGG-2294: slack invite button iteration

--- app/src/layouts/DashboardLayout/Sidebar/PrimarySidebar/components/JoinSlackButton/JoinSlackButton.tsx
+++ app/src/layouts/DashboardLayout/Sidebar/PrimarySidebar/components/JoinSlackButton/JoinSlackButton.tsx
@@ -3,18 +3,27 @@ import SlackIcon from "assets/icons/slack.svg?react";
 import sendSlackInvite from "components/misc/SupportPanel/sendSlackInvite";
 import { useCallback, useState } from "react";
 import starAnimation from "assets/images/gifs/Stars.gif";
-import "./joinSlackButton.scss";
 import { trackSlackConnectClicked } from "modules/analytics/events/misc/UnifiedSupport";
+import { useDispatch } from "react-redux";
+import { actions } from "store";
+import "./joinSlackButton.scss";
 
 const JoinSlackButton = () => {
+  const dispatch = useDispatch();
   const [isDisabled, setIsDisabled] = useState(false);
+
   const handleJoinSlack = useCallback(() => {
     setIsDisabled(true);
-    return sendSlackInvite()
-      .then(() => trackSlackConnectClicked("sidebar"))
+    sendSlackInvite()
+      .then(() => {
+        // @ts-ignore
+        dispatch(actions.updateIsSlackConnectButtonVisible(false));
+        trackSlackConnectClicked("sidebar");
+      })
       .catch(console.error)
       .finally(() => setIsDisabled(false));
   }, [setIsDisabled]);
+
   return (
     <>
       <RQButton type="text" onClick={handleJoinSlack} className="primary-sidebar-link w-full" disabled={isDisabled}>
