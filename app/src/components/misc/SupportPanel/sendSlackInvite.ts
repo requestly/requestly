3c839d03cb0fcdf47a41d84d1310cbfea249ec57
RuntimeTerror10 <parthb.git@gmail.com>
2024-09-20 12:46:46 +0530

    address comments

--- app/src/components/misc/SupportPanel/sendSlackInvite.ts
+++ app/src/components/misc/SupportPanel/sendSlackInvite.ts
@@ -1,33 +1,21 @@
-import { message } from "antd";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { throttle } from "lodash";
+import { toast } from "utils/Toast";
 
 async function sendSlackInvite() {
-  const hide = message.loading({
-    content: "Sending Slack Connect Invitation",
-    className: "slack-connect-toast",
-    duration: 0,
-  });
+  const hide = toast.loading("Sending Slack Connect Invitation", 0);
 
   const sendSlackInviation = httpsCallable(getFunctions(), "slackConnect-sendSlackInvitation");
   sendSlackInviation()
     .then((res: any) => {
       if (res.data.success) {
-        message.success({
-          content: "You'll receive the Slack invite in your email shortly!",
-          className: "slack-connect-toast",
-          duration: 4,
-        });
+        toast.success("You'll receive the Slack invite in your email shortly!", 4);
       }
     })
     .catch(() => {
-      message.error({
-        content: "Failed to send Slack invite",
-        className: "slack-connect-toast",
-        duration: 4,
-      });
+      toast.error("Failed to send Slack invite", 4);
     })
-    .finally(hide);
+    .finally(() => hide);
 }
 
 export default throttle(sendSlackInvite, 5000);
