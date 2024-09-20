cb88db47339ac80ba0f6dd60241eb0e6a055dbc5
RuntimeTerror10 <parthb.git@gmail.com>
2024-09-20 14:52:28 +0530

    chore: fix invoice request copy

--- app/src/features/settings/components/BillingTeam/components/BillingDetails/MyBillingTeamDetails/components/BillingInvoiceCard/index.tsx
+++ app/src/features/settings/components/BillingTeam/components/BillingDetails/MyBillingTeamDetails/components/BillingInvoiceCard/index.tsx
@@ -53,7 +53,7 @@ export const BillingInvoiceCard: React.FC = () => {
           isRequestSent ? (
             <div className="request-result success">
               {" "}
-              <MdCheckCircleOutline /> Email sent successfully
+              <MdCheckCircleOutline /> Request sent successfully. Invoice will be sent shortly.
             </div>
           ) : (
             <div className="request-result error">
