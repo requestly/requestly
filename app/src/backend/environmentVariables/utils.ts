bd3b8ab1a63963372fec74fa0dfd4c3b274f63c4
Nafees Nehar <nafees87n@gmail.com>
2024-10-02 21:35:10 +0530

    fix

--- app/src/backend/environmentVariables/utils.ts
+++ app/src/backend/environmentVariables/utils.ts
@@ -1,13 +1,18 @@
 import lodash from "lodash";
 import { EnvironmentVariableValue } from "./types";
+import Logger from "lib/logger";
 
-export const renderString = (template: string, variables: Record<string, EnvironmentVariableValue>) => {
+export const renderTemplate = (template: string, variables: Record<string, EnvironmentVariableValue>) => {
   lodash.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
-  Object.entries(variables).reduce((acc, [key, value]) => {
+  const parsedVariable = Object.entries(variables).reduce((acc, [key, value]) => {
     acc[key] = value.localValue ?? value.syncValue;
     return acc;
   }, {} as Record<string, string | number | boolean>);
-
-  return lodash.template(template)(variables);
+  try {
+    return lodash.template(template)(parsedVariable);
+  } catch (e) {
+    Logger.error("Error while rendering template");
+    return template;
+  }
 };
