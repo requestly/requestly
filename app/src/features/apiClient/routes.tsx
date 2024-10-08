93717ee89735c07dfe55248564a3056ac4fef144
rohanmathur91 <mathurrohan04@gmail.com>
2024-10-08 13:57:34 +0530

    add request route

--- app/src/features/apiClient/routes.tsx
+++ app/src/features/apiClient/routes.tsx
@@ -2,6 +2,7 @@ import { RouteObject } from "react-router-dom";
 import PATHS from "config/constants/sub/paths";
 import ApiClientFeatureContainer from "./container";
 import { APIClient } from "./screens/apiClient/APIClient";
+import ProtectedRoute from "components/authentication/ProtectedRoute";
 
 export const apiClientRoutes: RouteObject[] = [
   {
@@ -11,6 +12,10 @@ export const apiClientRoutes: RouteObject[] = [
       {
         index: true,
         element: <APIClient />,
+      },
+      {
+        path: PATHS.API_CLIENT.REQUEST.INDEX,
+        element: <ProtectedRoute component={APIClient} />,
       },
     ],
   },
