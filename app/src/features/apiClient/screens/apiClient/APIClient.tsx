fa8352407c39f049561eaf56dd84e5455bdea5c9
rohanmathur91 <mathurrohan04@gmail.com>
2024-10-08 14:33:59 +0530

    add secondary sidebar

--- app/src/features/apiClient/screens/apiClient/APIClient.tsx
+++ app/src/features/apiClient/screens/apiClient/APIClient.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import APIClientSidebar from "./components/sidebar/APIClientSidebar";
 import APIClientView from "./components/clientView/APIClientView";
 import { RQAPI } from "../../types";
@@ -10,14 +11,44 @@ import {
   trackNewRequestClicked,
 } from "modules/analytics/events/features/apiClient";
 import ImportRequestModal from "./components/modals/ImportRequestModal";
+import { getApiRecord } from "backend/apiClient";
+import Logger from "lib/logger";
 import "./apiClient.scss";
 
 interface Props {}
 
 export const APIClient: React.FC<Props> = () => {
+  const { requestId } = useParams();
+
+  const [, setIsLoading] = useState(false);
   const [history, setHistory] = useState<RQAPI.Entry[]>(getHistoryFromStore());
   const [selectedEntry, setSelectedEntry] = useState<RQAPI.Entry>();
   const [isImportModalOpen, setIsImportModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (!requestId) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    getApiRecord(requestId)
+      .then((result) => {
+        if (result.success) {
+          if (result.data.type === RQAPI.RecordType.API) {
+            setSelectedEntry(result.data.data);
+          }
+        }
+      })
+      .catch((error) => {
+        setSelectedEntry(null);
+        // TODO: redirect to new empty entry
+        Logger.error("Error loading api record", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [requestId]);
 
   const addToHistory = useCallback((apiEntry: RQAPI.Entry) => {
     setHistory((history) => [...history, apiEntry]);
@@ -62,7 +93,7 @@ export const APIClient: React.FC<Props> = () => {
           onNewClick={onNewClick}
           onImportClick={onImportClick}
         />
-        <APIClientView apiEntry={selectedEntry} notifyApiRequestFinished={addToHistory} />
+        <APIClientView key={requestId} apiEntry={selectedEntry} notifyApiRequestFinished={addToHistory} />
         <ImportRequestModal
           isOpen={isImportModalOpen}
           handleImportRequest={handleImportRequest}
