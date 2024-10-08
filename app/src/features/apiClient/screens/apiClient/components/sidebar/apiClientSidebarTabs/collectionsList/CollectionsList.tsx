feb836ced3736d08162f3e3efac0db3cc2ae092a
rohanmathur91 <mathurrohan04@gmail.com>
2024-10-08 19:36:54 +0530

    fix: request row style

--- app/src/features/apiClient/screens/apiClient/components/sidebar/apiClientSidebarTabs/collectionsList/CollectionsList.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/apiClientSidebarTabs/collectionsList/CollectionsList.tsx
@@ -7,6 +7,7 @@ import PATHS from "config/constants/sub/paths";
 import { useApiClientContext } from "features/apiClient/contexts";
 import { RQButton } from "lib/design-system-v2/components";
 import { CodeOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import { REQUEST_METHOD_COLORS } from "../../../../../../../../constants";
 import "./collectionsList.scss";
 
 interface Props {
@@ -39,14 +40,20 @@ export const CollectionsList: React.FC<Props> = ({ onNewClick, onImportClick }) 
           ) : apiClientRecords.length > 0 ? (
             <div className="collections-list">
               {apiClientRecords.map((record) => {
-                const apiRecord = record as RQAPI.ApiRecord;
+                const apiClientRecord = record as RQAPI.ApiRecord;
 
                 return (
                   <NavLink
                     to={`${PATHS.API_CLIENT.ABSOLUTE}/request/${record.id}`}
                     className={({ isActive }) => `collections-list-item api  ${isActive ? "active" : ""}`}
                   >
-                    {apiRecord.data.request.url}
+                    <span
+                      className="request-method"
+                      style={{ color: REQUEST_METHOD_COLORS[apiClientRecord.data.request.method] }}
+                    >
+                      {apiClientRecord.data.request.method.toUpperCase()}
+                    </span>
+                    <div className="request-url">{apiClientRecord.data.request.url}</div>
                   </NavLink>
                 );
               })}
