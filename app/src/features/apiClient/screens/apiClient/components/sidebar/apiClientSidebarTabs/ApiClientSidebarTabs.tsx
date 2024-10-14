d68f817872d6934d04d2213980f2785a3cc17fb7
rohanmathur91 <mathurrohan04@gmail.com>
2024-10-14 14:26:57 +0530

    fix: history route

--- app/src/features/apiClient/screens/apiClient/components/sidebar/apiClientSidebarTabs/ApiClientSidebarTabs.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/apiClientSidebarTabs/ApiClientSidebarTabs.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import { BsCollection } from "@react-icons/all-files/bs/BsCollection";
 import { MdOutlineHistory } from "@react-icons/all-files/md/MdOutlineHistory";
 import { Tabs, TabsProps, Tooltip } from "antd";
 import { RQAPI } from "features/apiClient/types";
 import { CollectionsList } from "./collectionsList/CollectionsList";
 import { HistoryList } from "./historyList/HistoryList";
+import PATHS from "config/constants/sub/paths";
 import "./apiClientSidebarTabs.scss";
 
 export enum ApiClientSidebarTabKey {
@@ -27,14 +29,37 @@ export const ApiClientSidebarTabs: React.FC<Props> = ({
   onNewClick,
   onImportClick,
 }) => {
+  const location = useLocation();
+  const [activeKey, setActiveKey] = useState<ApiClientSidebarTabKey>(ApiClientSidebarTabKey.COLLECTIONS);
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case PATHS.API_CLIENT.ABSOLUTE: {
+        setActiveKey(ApiClientSidebarTabKey.COLLECTIONS);
+        return;
+      }
+      case PATHS.API_CLIENT.HISTORY.ABSOLUTE: {
+        setActiveKey(ApiClientSidebarTabKey.HISTORY);
+        return;
+      }
+      default: {
+        setActiveKey(ApiClientSidebarTabKey.COLLECTIONS);
+        return;
+      }
+    }
+  }, [location.pathname]);
+
   const items: TabsProps["items"] = [
     {
       key: ApiClientSidebarTabKey.COLLECTIONS,
       label: (
         <Tooltip title="Collections" placement="right">
-          <div className="api-client-tab-label">
+          <NavLink
+            to={PATHS.API_CLIENT.ABSOLUTE}
+            className={({ isActive }) => `${isActive ? "active" : ""} api-client-tab-link`}
+          >
             <BsCollection />
-          </div>
+          </NavLink>
         </Tooltip>
       ),
       children: <CollectionsList onNewClick={onNewClick} onImportClick={onImportClick} />,
@@ -43,9 +68,12 @@ export const ApiClientSidebarTabs: React.FC<Props> = ({
       key: ApiClientSidebarTabKey.HISTORY,
       label: (
         <Tooltip title="History" placement="right">
-          <div className="api-client-tab-label">
+          <NavLink
+            to={PATHS.API_CLIENT.HISTORY.ABSOLUTE}
+            className={({ isActive }) => `${isActive ? "active" : ""} api-client-tab-link`}
+          >
             <MdOutlineHistory />
-          </div>
+          </NavLink>
         </Tooltip>
       ),
       children: (
@@ -54,5 +82,18 @@ export const ApiClientSidebarTabs: React.FC<Props> = ({
     },
   ];
 
-  return <Tabs tabPosition="left" size="small" className="api-client-sidebar-tabs" items={items} />;
+  const handleActiveTabChange = (activeKey: ApiClientSidebarTabKey) => {
+    setActiveKey(activeKey);
+  };
+
+  return (
+    <Tabs
+      items={items}
+      size="small"
+      tabPosition="left"
+      className="api-client-sidebar-tabs"
+      activeKey={activeKey}
+      onChange={handleActiveTabChange}
+    />
+  );
 };
