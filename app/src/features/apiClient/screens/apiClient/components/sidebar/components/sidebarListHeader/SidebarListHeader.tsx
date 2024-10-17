3c75bf4f03da3e04852827b4ec4a8f53b47f6f18
RuntimeTerror10 <parthb.git@gmail.com>
2024-10-18 02:27:55 +0530

    add env creation flow in sidebar

--- app/src/features/apiClient/screens/apiClient/components/sidebar/components/sidebarListHeader/SidebarListHeader.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/components/sidebarListHeader/SidebarListHeader.tsx
@@ -6,32 +6,29 @@ import { RQButton } from "lib/design-system-v2/components";
 import "./sidebarListHeader.scss";
 
 interface ListHeaderProps {
-  onAdd?: () => void;
-  onSearch?: (value: string) => void;
+  onAddRecordClick: () => void;
+  onSearch: (value: string) => void;
   menuItems?: MenuProps["items"];
 }
 
-export const SidebarListHeader: React.FC<ListHeaderProps> = ({ onAdd, onSearch, menuItems }) => {
+export const SidebarListHeader: React.FC<ListHeaderProps> = ({ onAddRecordClick, onSearch, menuItems }) => {
   return (
     <div className="sidebar-list-header">
-      {onAdd && (
-        <RQButton
-          size="small"
-          type="transparent"
-          icon={<PlusOutlined />}
-          onClick={onAdd}
-          className="sidebar-list-header-button"
-        />
-      )}
-      {onSearch && (
-        <Input
-          size="small"
-          prefix={<SearchOutlined />}
-          placeholder="Search"
-          onChange={(e) => onSearch(e.target.value)}
-          className="sidebar-list-header-search"
-        />
-      )}
+      <RQButton
+        size="small"
+        type="transparent"
+        icon={<PlusOutlined />}
+        onClick={onAddRecordClick}
+        className="sidebar-list-header-button"
+      />
+      <Input
+        size="small"
+        prefix={<SearchOutlined />}
+        placeholder="Search"
+        onChange={(e) => onSearch(e.target.value)}
+        className="sidebar-list-header-search"
+      />
+
       {menuItems && (
         <Dropdown menu={{ items: menuItems }} trigger={["click"]} placement="bottomRight">
           <RQButton size="small" type="transparent" icon={<MoreOutlined />} className="sidebar-list-header-button" />
