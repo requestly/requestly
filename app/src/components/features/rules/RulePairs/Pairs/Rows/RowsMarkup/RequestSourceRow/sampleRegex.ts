da09445759c75d9c1c99ae3ea682591f7df4de22
RuntimeTerror10 <parthb.git@gmail.com>
2024-10-18 18:11:17 +0530

    fix: list copy

--- app/src/components/features/rules/RulePairs/Pairs/Rows/RowsMarkup/RequestSourceRow/sampleRegex.ts
+++ app/src/components/features/rules/RulePairs/Pairs/Rows/RowsMarkup/RequestSourceRow/sampleRegex.ts
@@ -6,27 +6,27 @@ interface SampleRegexItem {
 
 export const sampleRegex: SampleRegexItem[] = [
   {
-    title: "Filter URLs that start with https:// only",
+    title: "Match URLs that start with https:// only",
     regex: "^https://.*",
     url: "https://example.com",
   },
   {
-    title: "Filter URLs that belong specific domain",
+    title: "Match URLs that belong specific domain",
     regex: "^https://.*\\.example\\.com",
     url: "https://app.example.com",
   },
   {
-    title: "Filter URLs that contain a query parameter",
+    title: "Match URLs that contain a query parameter",
     regex: "^https://.*\\?.*",
     url: "https://example.com?param=value",
   },
   {
-    title: "Filter URLs ending with specific file types",
+    title: "Match URLs ending with specific file types",
     regex: "^https?://.*\\.(jpg|png|gif)$",
     url: "https://example.com/gallery/image.jpg",
   },
   {
-    title: "Filter URLs with optional “www” and subdomains and paths.",
+    title: "Match URLs with optional “www” and subdomains and paths.",
     regex: "^https?://([a-zA-Z0-9-]+\\.)*example\\.com(/.*)?$",
     url: "https://app.example.com/path/to/resource",
   },
