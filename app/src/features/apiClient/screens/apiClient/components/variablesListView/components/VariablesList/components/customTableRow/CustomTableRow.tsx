6d5c943dbf9b556f687daaa101d1621fc7c708fc
RuntimeTerror10 <parthb.git@gmail.com>
2024-10-18 13:15:51 +0530

    update validation UX

--- app/src/features/apiClient/screens/apiClient/components/variablesListView/components/VariablesList/components/customTableRow/CustomTableRow.tsx
+++ app/src/features/apiClient/screens/apiClient/components/variablesListView/components/VariablesList/components/customTableRow/CustomTableRow.tsx
@@ -1,4 +1,4 @@
-import { Form, FormInstance, Input, Select } from "antd";
+import { Form, FormInstance, Input, InputNumber, Select } from "antd";
 import React, { useCallback, useContext, useMemo, useRef, useEffect, useState } from "react";
 import { EnvironmentVariableTableRow } from "../../VariablesList";
 import { EnvironmentVariableType } from "backend/environment/types";
@@ -13,8 +13,7 @@ interface EditableCellProps {
   children: React.ReactNode;
   dataIndex: keyof EnvironmentVariableTableRow;
   record: EnvironmentVariableTableRow;
-  handleSaveVariable: (record: EnvironmentVariableTableRow) => void;
-  inputType: "text" | "select";
+  handleSaveVariable: (record: EnvironmentVariableTableRow, isVariableTypeChanged?: boolean) => void;
   options?: string[];
 }
 
@@ -36,7 +35,6 @@ export const EditableCell: React.FC<EditableCellProps> = ({
   dataIndex,
   record,
   handleSaveVariable,
-  inputType,
   options,
   ...restProps
 }) => {
@@ -74,12 +72,17 @@ export const EditableCell: React.FC<EditableCellProps> = ({
   const debouncedSave = useMemo(() => debounce(handleSaveCellValue, 2000), [handleSaveCellValue]);
 
   const handleChange = useCallback(
-    (value: string) => {
+    (value: string | number | boolean) => {
       form.setFieldsValue({ [dataIndex]: value });
       setEditing(true);
-      debouncedSave();
+      if (dataIndex === "type") {
+        handleSaveVariable({ ...record, [dataIndex]: value, syncValue: "", localValue: "" }, true);
+        form.setFieldsValue({ syncValue: "", localValue: "" });
+      } else {
+        debouncedSave();
+      }
     },
-    [form, dataIndex, debouncedSave]
+    [form, dataIndex, debouncedSave, handleSaveVariable, record]
   );
 
   useEffect(() => {
@@ -88,37 +91,45 @@ export const EditableCell: React.FC<EditableCellProps> = ({
     }
   }, [editing]);
 
-  const getPlaceholderText = () => {
+  const getPlaceholderText = useCallback((dataIndex: string) => {
     if (dataIndex === "key") {
       return "Add new variable";
     }
     return "Enter value";
-  };
+  }, []);
 
-  const validateValue = useCallback(
-    (rule: unknown, value: any) => {
-      if (value === "" || value === undefined) {
-        return Promise.resolve();
-      }
-
-      if (dataIndex === "syncValue" || dataIndex === "localValue") {
-        switch (record.type) {
-          case EnvironmentVariableType.Number:
-            if (isNaN(Number(value))) {
-              return Promise.reject();
-            }
-            break;
-          case EnvironmentVariableType.Boolean:
-            if (value.toLowerCase() !== "true" && value.toLowerCase() !== "false") {
-              return Promise.reject();
-            }
-            break;
-        }
-      }
-      return Promise.resolve();
-    },
-    [dataIndex, record?.type]
-  );
+  const renderValueInputByType = useCallback(() => {
+    switch (record.type) {
+      case EnvironmentVariableType.String:
+        return (
+          <Input
+            ref={inputRef}
+            onChange={(e) => handleChange(e.target.value)}
+            onBlur={() => setEditing(false)}
+            placeholder={getPlaceholderText(dataIndex)}
+            autoFocus={editing}
+          />
+        );
+      case EnvironmentVariableType.Number:
+        return (
+          <InputNumber
+            controls={false}
+            ref={inputRef}
+            onChange={(value) => handleChange(value)}
+            onBlur={() => setEditing(false)}
+            placeholder={getPlaceholderText(dataIndex)}
+            autoFocus={editing}
+          />
+        );
+      case EnvironmentVariableType.Boolean:
+        return (
+          <Select onChange={handleChange} value={record[dataIndex]} placeholder="Select value">
+            <Select.Option value={true}>True</Select.Option>
+            <Select.Option value={false}>False</Select.Option>
+          </Select>
+        );
+    }
+  }, [record, handleChange, editing, dataIndex, getPlaceholderText]);
 
   if (!editable) {
     return <td {...restProps}>{children}</td>;
@@ -126,28 +137,29 @@ export const EditableCell: React.FC<EditableCellProps> = ({
 
   return (
     <td {...restProps}>
-      <Form.Item
-        style={{ margin: 0 }}
-        name={dataIndex}
-        initialValue={record?.[dataIndex]}
-        rules={[{ validator: validateValue }]}
-      >
-        {inputType === "select" ? (
-          <Select className="w-full" onChange={handleSaveCellValue} defaultValue={EnvironmentVariableType.String}>
+      <Form.Item style={{ margin: 0 }} name={dataIndex} initialValue={record?.[dataIndex]}>
+        {dataIndex === "type" ? (
+          <Select
+            className="w-full"
+            onChange={(value) => handleChange(value as EnvironmentVariableType)}
+            value={record.type}
+          >
             {options?.map((option) => (
               <Select.Option key={option} value={option}>
                 {option}
               </Select.Option>
             ))}
           </Select>
-        ) : (
+        ) : dataIndex === "key" ? (
           <Input
             ref={inputRef}
             onChange={(e) => handleChange(e.target.value)}
             onBlur={() => setEditing(false)}
-            placeholder={getPlaceholderText()}
+            placeholder={getPlaceholderText(dataIndex)}
             autoFocus={editing}
           />
+        ) : (
+          renderValueInputByType()
         )}
       </Form.Item>
     </td>
