23df6c3923abf10559378f3be24a3a0e8464520a
Aarush Srivastava <aarush@requestly.io>
2024-10-22 15:35:46 +0530

    Debouncing on pricing page (#2249)

--- app/src/features/pricing/components/PricingPage/components/OtherWaysToMakePurchase/index.tsx
+++ app/src/features/pricing/components/PricingPage/components/OtherWaysToMakePurchase/index.tsx
@@ -13,47 +13,56 @@ import { PurchaseOption } from "./PurchaseOption";
 import Logger from "lib/logger";
 import BuyAdditionalSeatsModal from "./BuyAdditionalSeatsModal";
 import "./OtherWaysToMakePurchase.scss";
+import { getUserAuthDetails } from "store/selectors";
+import { useSelector } from "react-redux";
 
 const OtherWaysToMakePurchase: React.FC = () => {
+  const user = useSelector(getUserAuthDetails);
   const [isRequestQuoteModalOpen, setIsRequestQuoteModalOpen] = useState(false);
   const [isSendPurchaseOrderModalOpen, setIsSendPurchaseOrderModalOpen] = useState(false);
   const [isBuyAdditionalSeatsModalOpen, setIsBuyAdditionalSeatsModalOpen] = useState(false);
 
   const handleRequestQuoteOnClick = () => {
-    trackRequestQuoteButtonClicked();
-    const salesInboundNotification = httpsCallable(getFunctions(), "premiumNotifications-salesInboundNotification");
-    try {
-      salesInboundNotification({
-        notificationText: EVENTS.REQUEST_QUOTE_BUTTON_CLICKED,
-      });
-    } catch (error) {
-      console.error(error);
+    if (user.loggedIn) {
+      trackRequestQuoteButtonClicked();
+      const salesInboundNotification = httpsCallable(getFunctions(), "premiumNotifications-salesInboundNotification");
+      try {
+        salesInboundNotification({
+          notificationText: EVENTS.REQUEST_QUOTE_BUTTON_CLICKED,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
     setIsRequestQuoteModalOpen(true);
   };
 
   const handleSendPurchaseOrderButtonOnClick = () => {
-    trackSendPurhcaseOrderButtonClicked();
-    const salesInboundNotification = httpsCallable(getFunctions(), "premiumNotifications-salesInboundNotification");
-    try {
-      salesInboundNotification({
-        notificationText: EVENTS.SEND_PURCHASE_ORDER_BUTTON_CLICKED,
-      });
-    } catch (error) {
-      console.error(error);
+    if (user.loggedIn) {
+      trackSendPurhcaseOrderButtonClicked();
+      const salesInboundNotification = httpsCallable(getFunctions(), "premiumNotifications-salesInboundNotification");
+      try {
+        salesInboundNotification({
+          notificationText: EVENTS.SEND_PURCHASE_ORDER_BUTTON_CLICKED,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
     setIsSendPurchaseOrderModalOpen(true);
   };
 
   const handleBuyAdditionalUsersButtonOnClick = () => {
-    trackBuyAdditionalUsersButtonClicked();
-    const salesInboundNotification = httpsCallable(getFunctions(), "premiumNotifications-salesInboundNotification");
-    try {
-      salesInboundNotification({
-        notificationText: EVENTS.BUY_ADDITIONAL_USERS_BUTTON_CLICKED,
-      });
-    } catch (error) {
-      Logger.error("Failed to send sales inbound notification", error);
+    if (user.loggedIn) {
+      trackBuyAdditionalUsersButtonClicked();
+      const salesInboundNotification = httpsCallable(getFunctions(), "premiumNotifications-salesInboundNotification");
+      try {
+        salesInboundNotification({
+          notificationText: EVENTS.BUY_ADDITIONAL_USERS_BUTTON_CLICKED,
+        });
+      } catch (error) {
+        Logger.error("Failed to send sales inbound notification", error);
+      }
     }
     setIsBuyAdditionalSeatsModalOpen(true);
   };
