3f64e16adea3643a33f959893542a71b6f72f3ed
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-10-22 12:32:43 +0530

    Chore: Seed user country attribute (#2251)

--- app/src/hooks/useGeoLocation.js
+++ app/src/hooks/useGeoLocation.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { actions } from "../store";
 import { getUserGeoDetails } from "utils/geoUtils";
+import { submitAttrUtil } from "utils/AnalyticsUtils";
+import APP_CONSTANTS from "config/constants";
 
 const useGeoLocation = () => {
   const dispatch = useDispatch();
@@ -13,10 +15,12 @@ const useGeoLocation = () => {
       .then((details) => {
         // setCountry(currentCountry);
         dispatch(actions.updateUserCountry(details?.loc));
+        submitAttrUtil(APP_CONSTANTS.GA_EVENTS.ATTR.COUNTRY, details?.loc);
         window.country = details?.loc;
       })
       .catch(() => {
         dispatch(actions.updateUserCountry("US"));
+        submitAttrUtil(APP_CONSTANTS.GA_EVENTS.ATTR.COUNTRY, "US");
         window.country = "US";
       });
   }, [dispatch]);
