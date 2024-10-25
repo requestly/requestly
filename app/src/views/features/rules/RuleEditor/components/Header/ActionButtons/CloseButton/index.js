c5d317e0d685848a6afbb872e6a97f4daf5fa396
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-10-25 13:20:52 +0530

    Chore: fix create group dropdown button type and update useEffect dependency (#2262)

--- app/src/views/features/rules/RuleEditor/components/Header/ActionButtons/CloseButton/index.js
+++ app/src/views/features/rules/RuleEditor/components/Header/ActionButtons/CloseButton/index.js
@@ -1,5 +1,4 @@
 import React, { useCallback } from "react";
-import { useDispatch } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Tooltip } from "antd";
 import { navigateBack, redirectToRules } from "utils/RedirectionUtils";
@@ -11,13 +10,12 @@ import { KEYBOARD_SHORTCUTS } from "../../../../../../../../constants/keyboardSh
 
 const CloseButton = ({ ruleType, mode }) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const location = useLocation();
 
   const closeButtonHandler = useCallback(() => {
     trackRuleEditorClosed("cancel_button", ruleType, snakeCase(mode));
     navigateBack(navigate, location, () => redirectToRules(navigate));
-  }, [dispatch, mode, navigate, ruleType]);
+  }, [mode, navigate, ruleType, location]);
 
   return (
     <Tooltip title="Back to rules (esc)" placement="bottom">
