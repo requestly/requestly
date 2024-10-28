3c4083dc78f6a14d2d378185bdd93bf7ae912f3c
Rohan Mathur <mathurrohan04@gmail.com>
2024-10-28 14:47:21 +0600

    [ENGG-2374]: Design System - Breadcrumbs with Record Name update support (#2257)
    * update routes for breadcrumbs
    
    * add RQBreadcrumb v2
    
    * update breadcrumb for rules listing page
    
    * update rule editor breadcrumb
    
    * fix: path click

---
+++ app/src/lib/design-system-v2/components/RQBreadcrumb/RQBreadcrumb.tsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import { Link, Params, useMatches } from "react-router-dom";
+import { MdOutlineChevronRight } from "@react-icons/all-files/md/MdOutlineChevronRight";
+import { Input, Typography } from "antd";
+import { MdOutlineEdit } from "@react-icons/all-files/md/MdOutlineEdit";
+import "./RQBreadcrumb.scss";
+
+interface Props {
+  disabled?: boolean;
+  onBlur?: () => void;
+  recordName?: string;
+  placeholder?: string;
+  onRecordNameUpdate?: (s: string) => void;
+}
+
+interface MatchedRoute {
+  id: string;
+  pathname: string;
+  params: Params<string>;
+  data?: unknown;
+  handle?: {
+    breadcrumb?: {
+      label: string;
+      disabled?: boolean;
+      isEditable?: boolean;
+    };
+  };
+}
+
+export const RQBreadcrumb: React.FC<Props> = ({
+  onBlur,
+  disabled = false,
+  recordName,
+  placeholder,
+  onRecordNameUpdate,
+}) => {
+  const [name, setName] = useState(recordName || "");
+  const [isEditRecord, setIsEditRecord] = useState(false);
+  const matchedRoutes = useMatches() as MatchedRoute[];
+
+  useEffect(() => {
+    setName(recordName);
+  }, [recordName]);
+
+  const breadcrumbs: ({
+    pathname: MatchedRoute["pathname"];
+  } & MatchedRoute["handle"]["breadcrumb"])[] = matchedRoutes.reduce((result, route) => {
+    return route.handle?.breadcrumb ? [...result, { ...route.handle.breadcrumb, pathname: route.pathname }] : result;
+  }, []);
+
+  const handleOnChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const updatedValue = e.target.value;
+
+    if (!updatedValue) {
+      // TODO: show toast
+    }
+
+    setName(updatedValue);
+    onRecordNameUpdate(updatedValue);
+  };
+
+  const handleOnBlur = () => {
+    setIsEditRecord(false);
+    onBlur?.();
+  };
+
+  const handleRecordNameEditClick = () => {
+    if (disabled) {
+      return;
+    }
+
+    setIsEditRecord(true);
+  };
+
+  return (
+    <ol className="rq-breadcrumb">
+      {breadcrumbs.map(({ label, isEditable, pathname, disabled: isPathDisabled }, index) => {
+        return (
+          <>
+            {isEditable ? (
+              isEditRecord ? (
+                <Input
+                  autoFocus
+                  value={name}
+                  onChange={handleOnChange}
+                  placeholder={placeholder}
+                  className={`rq-breadcrumb-input`}
+                  onBlur={() => {
+                    handleOnBlur();
+                  }}
+                  onPressEnter={() => {
+                    handleOnBlur();
+                  }}
+                />
+              ) : (
+                <div className="rq-breadcrumb-record-name">
+                  <Typography.Text className="record-name" ellipsis={true} onClick={handleRecordNameEditClick}>
+                    {recordName || placeholder}
+                  </Typography.Text>
+                  {disabled ? null : <MdOutlineEdit className="edit-icon" onClick={handleRecordNameEditClick} />}
+                </div>
+              )
+            ) : (
+              <>
+                {isPathDisabled ? (
+                  <li key={index} className="rq-breadcrumb-item">
+                    {label}
+                  </li>
+                ) : (
+                  <Link key={index} to={pathname} className="rq-breadcrumb-item">
+                    {label}
+                  </Link>
+                )}
+              </>
+            )}
+
+            {index < breadcrumbs.length - 1 ? (
+              <span className="rq-breadcrumb-separator">
+                <MdOutlineChevronRight />
+              </span>
+            ) : null}
+          </>
+        );
+      })}
+    </ol>
+  );
+};
