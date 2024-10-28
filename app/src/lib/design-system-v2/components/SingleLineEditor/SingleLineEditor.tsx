5f37a5843995dde7b9b68dfcbfb5a074103254a3
Nafees Nehar <nafees87n@gmail.com>
2024-10-29 00:47:56 +0530

    address review comments

--- app/src/lib/design-system-v2/components/SingleLineEditor/SingleLineEditor.tsx
+++ app/src/lib/design-system-v2/components/SingleLineEditor/SingleLineEditor.tsx
@@ -1,17 +1,14 @@
-import React, { useRef, useEffect, useState, useMemo } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { EditorView, placeholder as cmPlaceHolder } from "@codemirror/view";
 import { EditorState } from "@codemirror/state";
 import "./SingleLineEditor.scss";
 import { highlightVariablesPlugin } from "./plugins/highlightVariables";
-import { Popover, Row } from "antd";
-import useEnvironmentManager from "backend/environment/hooks/useEnvironmentManager";
-import { EnvironmentVariables, EnvironmentVariableValue } from "backend/environment/types";
-import { capitalize } from "lodash";
+import { SingleLineEditorPopover } from "./SingleLineEditorPopover";
 
 interface RQSingleLineEditorProps {
-  className?: string;
   defaultValue?: string;
-  onChange?: (value: string) => void;
+  className?: string;
+  onChange: (value: string) => void;
   placeholder?: string;
   onPressEnter?: (event: KeyboardEvent, text: string) => void;
   onBlur?: (text: string) => void;
@@ -30,12 +27,6 @@ export const RQSingleLineEditor: React.FC<RQSingleLineEditorProps> = ({
 
   const [hoveredVariable, setHoveredVariable] = useState(null); // Track hovered variable
   const [popupPosition, setPopupPosition] = useState({ x: 0, y: 0 });
-  const { getCurrentEnvironmentVariables, setVariables, getCurrentEnvironment } = useEnvironmentManager();
-  const { currentEnvironmentId } = getCurrentEnvironment();
-
-  const currentEnvironmentVariables = useMemo(() => {
-    return getCurrentEnvironmentVariables();
-  }, [getCurrentEnvironmentVariables]);
 
   useEffect(() => {
     if (editorViewRef.current) {
@@ -83,118 +74,21 @@ export const RQSingleLineEditor: React.FC<RQSingleLineEditorProps> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [placeholder]);
 
-  // useEffect(() => {
-  //   // if (value && editorViewRef.current) {
-  //   //   console.log("!!!debug", "before dispatch", value);
-  //   //   editorViewRef.current?.dispatch({
-  //   //     changes: {
-  //   //       from: 0,
-  //   //       to: editorViewRef.current.state.doc.length,
-  //   //       insert: value,
-  //   //     },
-  //   //   });
-  //   // }
-  // }, [value]);
-
-  const addNewVariable = (newVariable: EnvironmentVariables) => {
-    setVariables(currentEnvironmentId, {
-      ...currentEnvironmentVariables,
-      ...newVariable,
-    });
-  };
-
   return (
     <>
       <div
         ref={editorRef}
-        className="single-line-editor-container ant-input"
+        className={`${className ?? ""} single-line-editor-container ant-input`}
         onMouseLeave={() => setHoveredVariable(null)}
       >
         {hoveredVariable && (
-          <Popover
-            content={
-              <div className="variable-info-body">
-                {currentEnvironmentVariables[hoveredVariable] ? (
-                  <VariableInfo
-                    variable={{
-                      name: hoveredVariable,
-                      ...currentEnvironmentVariables[hoveredVariable],
-                    }}
-                  />
-                ) : (
-                  <AddNewVariable variableName={hoveredVariable} addNewVariable={addNewVariable} />
-                )}
-              </div>
-            }
-            showArrow={false}
-            open={!!hoveredVariable}
-            destroyTooltipOnHide
-            placement="bottom"
-            arrowContent={null}
-            arrowPointAtCenter={null}
-            overlayClassName="variable-info-popover"
-          >
-            <div
-              style={{
-                position: "absolute",
-                top: popupPosition?.y - editorRef.current.getBoundingClientRect().top + 10,
-                left: popupPosition?.x - editorRef.current.getBoundingClientRect().left + 10,
-                zIndex: 1000,
-              }}
-              className="variable-info-div"
-            ></div>
-          </Popover>
+          <SingleLineEditorPopover
+            editorRef={editorRef}
+            hoveredVariable={hoveredVariable}
+            popupPosition={popupPosition}
+          />
         )}
       </div>
-    </>
-  );
-};
-
-const VariableInfo: React.FC<{
-  variable: { name: string } & EnvironmentVariableValue;
-}> = ({ variable }) => {
-  return (
-    <>
-      <Row className="variable-info-header">{variable.name}</Row>
-      <div className="variable-info-content">
-        <div className="variable-info-title">Type</div>
-        <div className="variable-info-value">{capitalize(variable.type as string)}</div>
-        <div className="variable-info-title">Initial Value</div>
-        <div className="variable-info-value">{variable.syncValue}</div>
-        <div className="variable-info-title">Current Value</div>
-        <div className="variable-info-value">{variable.localValue}</div>
-      </div>
-    </>
-  );
-};
-
-const AddNewVariable: React.FC<{
-  addNewVariable: (environmentVariables: EnvironmentVariables) => void;
-  variableName: string;
-}> = ({ addNewVariable, variableName }) => {
-  return (
-    <>
-      <Row className="variable-info-header">{"Variable is not defined or resolved"}</Row>
-      <Row className="add-new-variable-info-content">
-        {"Make sure that the variable is defined in the globals or any of the active environments."}
-      </Row>
-      {/* <RQButton
-        block
-        type="primary"
-        className="add-new-variable-btn"
-        onClick={() => {
-          //TODO: Fix this to add new Variables
-          addNewVariable({
-            [variableName]: {
-              syncValue: "",
-              localValue: "",
-              type: "string",
-            },
-          });
-        }}
-      >
-        {"Add as a new variable"}
-      </RQButton> */}
     </>
   );
 };
