e5ebcdebc4cd7748d1d8abd6eabebfa14dd4cc84
Devansh Gupta <devansh@requestly.io>
2024-12-02 13:13:38 +0530

    Revert "chore: fix black friday banner visibility (#2333)"
    This reverts commit 91459f3b7983f3d5c9f650f56b3f473560c0c2df.

--- app/src/componentsV2/AppNotificationBanner/index.tsx
+++ app/src/componentsV2/AppNotificationBanner/index.tsx
@@ -36,7 +36,6 @@ enum BANNER_ID {
   ACCELERATOR_PROGRAM = "accelerator_program",
   COMMERCIAL_LICENSE = "commercial_license",
   REQUEST_TEAM_ACCESS = "request_team_access",
-  BLACK_FRIDAY = "black_friday",
 }
 
 interface Banner {
@@ -139,12 +138,6 @@ export const AppNotificationBanner = () => {
         }
         case BANNER_ID.ACCELERATOR_PROGRAM: {
           if (!user.details?.isPremium && !billingTeams?.length && user.loggedIn) {
-            dispatch(actions.updateIsAppBannerVisible(true));
-            return true;
-          } else return false;
-        }
-        case BANNER_ID.BLACK_FRIDAY: {
-          if (!user.details?.isPremium) {
             dispatch(actions.updateIsAppBannerVisible(true));
             return true;
           } else return false;
