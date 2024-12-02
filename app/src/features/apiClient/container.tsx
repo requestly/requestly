061db8fec8805a2b754860dc190c2198a19cfab9
Rohan Mathur <mathurrohan04@gmail.com>
2024-12-02 12:56:01 +0600

    [ENGG-2443] refactor: common sidebar for api-client feature (#2326)
    * refactor: api client context
    
    * remove duplicate file
    
    * fix: api client style
    
    * cleanup
    
    * refactor

--- app/src/features/apiClient/container.tsx
+++ app/src/features/apiClient/container.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { ApiClientProvider } from "./contexts";
+import APIClientSidebar from "./screens/apiClient/components/sidebar/APIClientSidebar";
+import "./container.scss";
 
 const ApiClientFeatureContainer: React.FC = () => {
   return (
     <ApiClientProvider>
-      <Outlet />
+      <div className="api-client-container">
+        <APIClientSidebar />
+        <Outlet />
+      </div>
     </ApiClientProvider>
   );
 };
