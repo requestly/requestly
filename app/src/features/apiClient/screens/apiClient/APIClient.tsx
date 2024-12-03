6f97558d061ec16d7a6bc22ec1968b52f481d0b0
rohanmathur91 <mathurrohan04@gmail.com>
2024-12-03 16:18:49 +0530

    fix: local state update

--- app/src/features/apiClient/screens/apiClient/APIClient.tsx
+++ app/src/features/apiClient/screens/apiClient/APIClient.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import APIClientView from "./components/clientView/APIClientView";
 import { DeleteApiRecordModal, ImportRequestModal } from "./components/modals";
 import { useApiClientContext } from "features/apiClient/contexts";
 import { BottomSheetPlacement, BottomSheetProvider } from "componentsV2/BottomSheet";
+import { RQAPI } from "features/apiClient/types";
+import { useTabsLayoutContext } from "layouts/TabsLayout";
 import "./apiClient.scss";
 
 interface Props {}
@@ -13,7 +15,6 @@ export const APIClient: React.FC<Props> = () => {
     isDeleteModalOpen,
     onDeleteModalClose,
     addToHistory,
-    selectedEntry,
     selectedEntryDetails,
     isLoading,
     isImportModalOpen,
@@ -21,12 +22,41 @@ export const APIClient: React.FC<Props> = () => {
     onImportRequestModalClose,
   } = useApiClientContext();
 
+  const { activeTab } = useTabsLayoutContext();
+
+  const [apiEntry, setApiEntry] = useState<RQAPI.Entry>();
+  const [apiEntryDetails, setApiEntryDetails] = useState<RQAPI.ApiRecord>();
+
+  const hasUpdated = useRef(false);
+
+  const shouldUpdate = activeTab?.id === selectedEntryDetails?.id;
+
+  const isApiEntryDetailsExist = !!apiEntryDetails;
+
+  // Stopping the internal re-renders
+  useEffect(() => {
+    if (!shouldUpdate) {
+      return;
+    }
+
+    if (hasUpdated.current) {
+      return;
+    }
+
+    if (isApiEntryDetailsExist) {
+      hasUpdated.current = true;
+    }
+
+    setApiEntry(selectedEntryDetails?.data);
+    setApiEntryDetails(selectedEntryDetails);
+  }, [selectedEntryDetails, isApiEntryDetailsExist, shouldUpdate]);
+
   return (
     <BottomSheetProvider defaultPlacement={BottomSheetPlacement.BOTTOM} isSheetOpenByDefault={true}>
       <div className="api-client-container-content">
         <APIClientView
           // TODO: Fix - "apiEntry" is used for history, remove this prop and derive everything from "apiEntryDetails"
-          apiEntry={selectedEntry ?? selectedEntryDetails?.data}
+          apiEntry={selectedEntryDetails?.data}
           apiEntryDetails={selectedEntryDetails}
           notifyApiRequestFinished={addToHistory}
         />
