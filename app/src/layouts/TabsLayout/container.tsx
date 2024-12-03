b9104ffb4a44358b4c2528a7ce4dceddaa5e16dc
rohanmathur91 <mathurrohan04@gmail.com>
2024-12-03 16:08:18 +0530

    update TabsLayoutContainer

--- app/src/layouts/TabsLayout/container.tsx
+++ app/src/layouts/TabsLayout/container.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { TabsLayoutProvider } from "./contexts";
 import { TabsLayoutContent } from "./components";
 
-export const TabsLayoutContainer: React.FC = () => {
-  return (
-    <TabsLayoutProvider>
-      {/* TabsLayoutHeader */}
-      <TabsLayoutContent />
-    </TabsLayoutProvider>
-  );
+interface TabsLayoutContainerProps {
+  children: React.ReactElement;
+}
+
+export const TabsLayoutContainer: React.FC<TabsLayoutContainerProps> & {
+  TabsLayoutContent: typeof TabsLayoutContent;
+} = ({ children }) => {
+  return <TabsLayoutProvider>{children}</TabsLayoutProvider>;
 };
+
+TabsLayoutContainer.TabsLayoutContent = TabsLayoutContent;
