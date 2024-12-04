4b88b109c4f9669b0536c7e14bd6771cfebc886e
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2024-12-04 08:36:44 +0530

    ENGG-2465:Fix API Client response loading state (#2357)

--- app/src/features/apiClient/screens/apiClient/components/clientView/components/response/ResponseHeaders/ResponseHeaders.tsx
+++ app/src/features/apiClient/screens/apiClient/components/clientView/components/response/ResponseHeaders/ResponseHeaders.tsx
@@ -2,6 +2,8 @@ import React, { useMemo } from "react";
 import { KeyValuePair } from "../../../../../../../types";
 import { PropertiesGrid } from "componentsV2/PropertiesGrid/PropertiesGrid";
 import { EmptyResponsePlaceholder } from "../EmptyResponsePlaceholder/EmptyResponsePlaceholder";
+import { Spin } from "antd";
+import { RQButton } from "lib/design-system/components";
 import "./responseHeaders.scss";
 
 interface Props {
@@ -21,15 +23,21 @@ const ResponseHeaders: React.FC<Props> = ({ headers, isLoading, isFailed, onCanc
 
   return (
     <>
+      {isLoading ? (
+        <div className="api-client-response__loading-overlay">
+          <Spin size="large" tip="Request in progress..." />
+          <RQButton onClick={onCancelRequest} className="mt-16">
+            Cancel request
+          </RQButton>
+        </div>
+      ) : null}
       {headers?.length ? (
         <div className="api-client-response-headers">
           <PropertiesGrid data={transformedHeaders} />
         </div>
       ) : (
         <EmptyResponsePlaceholder
-          isLoading={isLoading}
           isFailed={isFailed}
-          onCancelRequest={onCancelRequest}
           emptyDescription="Please run a request to see the response headers"
         />
       )}
