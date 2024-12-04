21be0b130b765c8104684737e70773450736e293
rohanmathur91 <mathurrohan04@gmail.com>
2024-12-04 16:51:10 +0530

    fix: TabsLayoutContent

--- app/src/layouts/TabsLayout/components/TabsLayoutContent/TabsLayoutContent.tsx
+++ app/src/layouts/TabsLayout/components/TabsLayoutContent/TabsLayoutContent.tsx
@@ -1,52 +1,51 @@
 import React from "react";
 import { useTabsLayoutContext } from "layouts/TabsLayout/contexts";
-import { Outlet, OutletProps } from "react-router-dom";
 import { Tabs, TabsProps } from "antd";
+import { MdClose } from "@react-icons/all-files/md/MdClose";
+import { RQButton } from "lib/design-system-v2/components";
 import "./tabsLayoutContent.scss";
-// import { RQButton } from "lib/design-system-v2/components";
 
-export const TabsLayoutContent: React.FC = () => {
-  const { tabs, activeTab, openTab } = useTabsLayoutContext();
+interface Props {
+  Outlet: (props: any) => React.ReactElement;
+}
 
-  /**
-   * - Do the processing here for rendering the current tabs and all the multiple tabs
-   */
-  const context: OutletProps["context"] = {};
+export const TabsLayoutContent: React.FC<Props> = ({ Outlet }) => {
+  const { tabs, activeTab, openTab, closeTab } = useTabsLayoutContext();
 
-  // Test performance, data loss, lag???
-  const items: TabsProps["items"] = tabs.map((tabDetails) => {
-    const clonedOutlet = React.cloneElement(<Outlet />, { context });
-
+  const items: TabsProps["items"] = tabs.map((tab) => {
     return {
-      key: tabDetails.id,
+      key: tab.id,
       label: (
-        <div>
-          {tabDetails.title}{" "}
-          {/* <RQButton
+        <div title={tab.title} className="tab-title-container">
+          <div className="tab-title">{tab.title}</div>
+
+          <RQButton
             size="small"
-            onClick={() => {
-              closeTab();
+            type="transparent"
+            // className="tab-close-button"
+            onClick={(e) => {
+              e.stopPropagation();
+              closeTab(tab.id);
             }}
-          >
-            x
-          </RQButton> */}
+            icon={<MdClose />}
+          />
+          {tab.hasUnsavedChanges ? <div className="unsaved-changes-indicator" /> : null}
         </div>
       ),
-      children: clonedOutlet,
+      children: <Outlet key={tab.id} />,
     };
   });
-
-  // return <Outlet />;
 
   return (
     <div className="tabs-layout-container">
       {tabs.length ? (
         <Tabs
+          hideAdd
           activeKey={activeTab?.id}
-          // destroyInactiveTabPane={false}
+          destroyInactiveTabPane={false}
           items={items}
-          onChange={(activeTab) => {
-            openTab(activeTab);
+          onChange={(activeTabId) => {
+            openTab(activeTabId);
           }}
         />
       ) : (
--- app/src/layouts/TabsLayout/contexts/tabsLayoutContext.tsx
+++ app/src/layouts/TabsLayout/contexts/tabsLayoutContext.tsx
@@ -8,6 +8,7 @@ const TabsLayoutContext = createContext<TabsLayoutContextInterface>({
   closeTab: (tabId: TabsLayout.Tab["id"]) => {},
   openTab: (tabId: TabsLayout.Tab["id"], tabDetails?: Partial<TabsLayout.Tab>) => {},
   updateTab: (tabId: TabsLayout.Tab["id"], updatedTabData?: Partial<TabsLayout.Tab>) => {},
+  replaceTab: (tabId: TabsLayout.Tab["id"], newTabData?: Partial<TabsLayout.Tab>) => {},
 });
 
 interface TabsLayoutProviderProps {
@@ -20,32 +21,38 @@ export const TabsLayoutProvider: React.FC<TabsLayoutProviderProps> = ({ children
   const [tabs, setTabs] = useState<TabsLayout.Tab[]>([]); // TODO: Persist saved tabs
   const [activeTab, setActiveTab] = useState<TabsLayout.Tab>();
 
+  const updateActivetab = useCallback(
+    (tab: TabsLayout.Tab) => {
+      navigate(tab.url);
+      setActiveTab((prev) => ({ ...(prev ?? {}), ...tab }));
+    },
+    [navigate]
+  );
+
   const closeTab = useCallback(
     (tabId: TabsLayout.Tab["id"]) => {
-      const tabIndex = tabs.findIndex((tab) => tab.id !== tabId);
+      const copiedTabs = [...tabs];
+      const targetTabIndex = copiedTabs.findIndex((tab) => tab.id === tabId);
+      const targetTab = copiedTabs[targetTabIndex];
+      const updatedTabs = copiedTabs.filter((tab) => tab.id !== tabId);
 
-      if (tabIndex !== -1 && tabIndex > 0) {
-        setTabs((prev) => prev.filter((tab) => tab.id !== tabId));
-        // openTab(tabs[tabIndex - 1].id);
-      } else {
-        // Open new tab
+      setTabs((prev) => prev.filter((tab) => tab.id !== tabId));
+
+      if (updatedTabs.length && targetTab.id === activeTab?.id) {
+        const nextActiveTab = updatedTabs[targetTabIndex === updatedTabs.length ? targetTabIndex - 1 : targetTabIndex];
+        console.log({ nextActiveTab });
+        updateActivetab(nextActiveTab);
       }
-
-      // check if its active tab then make another tab as active and close this
-      // Before closing check for the unsaved changes
     },
-    [tabs]
+    [tabs, activeTab?.id, updateActivetab]
   );
 
   const openTab = useCallback(
     (tabId: TabsLayout.Tab["id"], tabDetails?: Partial<TabsLayout.Tab>) => {
       const tab = tabs.find((item) => item.id === tabId);
 
-      console.log("find tab", { tab });
-
       if (tab) {
-        navigate(tab.url);
-        setActiveTab(tab);
+        updateActivetab(tab);
         return;
       }
 
@@ -58,20 +65,27 @@ export const TabsLayoutProvider: React.FC<TabsLayoutProviderProps> = ({ children
       } as TabsLayout.Tab;
 
       setTabs((prev) => [...prev, newTabDetails]);
-      setActiveTab(newTabDetails);
+      updateActivetab(newTabDetails);
     },
-    [tabs, navigate]
+    [tabs, updateActivetab]
   );
 
   const updateTab = useCallback((tabId: TabsLayout.Tab["id"], updatedTabData?: Partial<TabsLayout.Tab>) => {
-    setTabs((prev) => prev.map((tab) => (tab.id === tabId ? { ...tab, ...updatedTabData } : tab)));
-
-    // if (tabId === activeTab?.id) {
-    //   setActiveTab((prev) => ({ ...prev, ...updatedTabData }));
-    // }
+    setTabs((prev) => prev.map((tab) => (tab.id === tabId ? { ...tab, ...updatedTabData, id: tabId } : tab)));
   }, []);
 
-  const value = { activeTab, tabs, openTab, closeTab, updateTab };
+  const replaceTab = useCallback(
+    (tabId: TabsLayout.Tab["id"], newTabData?: Partial<TabsLayout.Tab>) => {
+      setTabs((prev) => prev.map((tab) => (tab.id === tabId ? { ...tab, ...newTabData } : tab)));
+
+      if (tabId === activeTab?.id) {
+        updateActivetab({ ...activeTab, ...newTabData } as TabsLayout.Tab);
+      }
+    },
+    [activeTab, updateActivetab]
+  );
+
+  const value = { activeTab, tabs, openTab, closeTab, updateTab, replaceTab };
 
   return <TabsLayoutContext.Provider value={value}>{children}</TabsLayoutContext.Provider>;
 };
