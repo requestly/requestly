47d3eac7c99a5ab632f16fc64ba51f8a4eed043a
Nafees Nehar <nafees87n@gmail.com>
2024-12-12 13:47:07 +0100

    fix: documentation link

--- app/src/features/apiClient/screens/environment/components/environmentsList/components/environmentsListItem/EnvironmentsListItem.tsx
+++ app/src/features/apiClient/screens/environment/components/environmentsList/components/environmentsListItem/EnvironmentsListItem.tsx
@@ -1,13 +1,14 @@
 import { MdOutlineCheckCircle } from "@react-icons/all-files/md/MdOutlineCheckCircle";
 import { MdOutlineMoreHoriz } from "@react-icons/all-files/md/MdOutlineMoreHoriz";
 import { IoMdGlobe } from "@react-icons/all-files/io/IoMdGlobe";
+import { MdInfoOutline } from "@react-icons/all-files/md/MdInfoOutline";
 import { Dropdown, Input, Tooltip, Typography } from "antd";
 import useEnvironmentManager from "backend/environment/hooks/useEnvironmentManager";
 import PATHS from "config/constants/sub/paths";
 import { TabsLayoutContextInterface, useTabsLayoutContext } from "layouts/TabsLayout";
 import { RQButton } from "lib/design-system-v2/components";
 import React, { useCallback, useMemo, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { redirectToEnvironment } from "utils/RedirectionUtils";
 import { toast } from "utils/Toast";
 
@@ -153,6 +154,28 @@ export const EnvironmentsListItem: React.FC<EnvironmentsListItemProps> = ({ envi
         >
           {environment.isGlobal && <IoMdGlobe className="global-var-icon" />}
           {environment.name}
+          {environment.isGlobal && (
+            <Tooltip
+              overlayClassName="active-environment-tooltip"
+              title={
+                <span>
+                  Global variables are accessible across the workspace and editable by all the workspace members.{" "}
+                  <Link
+                    to="https://docs.requestly.com/general/api-client/environments-and-variables/add-and-use-variables"
+                    target="_blank"
+                  >
+                    Learn more.
+                  </Link>
+                </span>
+              }
+              placement="top"
+              showArrow={false}
+            >
+              <span>
+                <MdInfoOutline className="global-var-info-icon" />
+              </span>
+            </Tooltip>
+          )}
         </Typography.Text>
         <Tooltip
           overlayClassName="active-environment-tooltip"
