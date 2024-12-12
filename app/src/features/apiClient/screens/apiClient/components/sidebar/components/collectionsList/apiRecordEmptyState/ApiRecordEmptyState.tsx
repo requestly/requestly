bc630998230da98654de39c1e772066d94b69b78
Mac <mac@Macs-MacBook-Pro.local>
2024-12-13 00:36:55 +0530

    fix

--- app/src/features/apiClient/screens/apiClient/components/sidebar/components/collectionsList/apiRecordEmptyState/ApiRecordEmptyState.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/components/collectionsList/apiRecordEmptyState/ApiRecordEmptyState.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { EmptyState, EmptyStateProps } from "../../emptyState/EmptyState";
 import { NewRecordNameInputProps } from "../newRecordNameInput/NewRecordNameInput";
-import { RQAPI } from "features/apiClient/types";
 
 interface Props extends EmptyStateProps, Omit<NewRecordNameInputProps, "onSuccess"> {}
 
@@ -9,24 +8,15 @@ export const ApiRecordEmptyState: React.FC<Props> = ({
   message,
   newRecordBtnText,
   onNewRecordClick,
-  recordToBeEdited,
-  recordType,
-  newRecordCollectionId,
   analyticEventSource,
 }) => {
-  const handleNewRecordClick = () => {
-    if (recordType === RQAPI.RecordType.API) {
-      onNewRecordClick?.();
-    }
-  };
-
   return (
     <div className="api-record-empty-state-container">
       <div className="mt-8">
         <EmptyState
           message={message}
           newRecordBtnText={newRecordBtnText}
-          onNewRecordClick={handleNewRecordClick}
+          onNewRecordClick={onNewRecordClick}
           analyticEventSource={analyticEventSource}
         />
       </div>
