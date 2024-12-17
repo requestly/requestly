9ed19eaa5a2c12f6dca2543c2552c3d2f92a1f5c
Devansh Gupta <devansh@requestly.io>
2024-12-17 18:11:12 +0530

    [ENG-2463]. add auth support in postman importer

--- app/src/features/apiClient/constants.ts
+++ app/src/features/apiClient/constants.ts
@@ -1,3 +1,11 @@
+import { AUTHORIZATION_TYPES } from "./screens/apiClient/components/clientView/components/request/components/AuthorizationView/authStaticData";
+
 export const CONTENT_TYPE_HEADER = "Content-Type";
 
 export const DEMO_API_URL = "https://app.requestly.io/echo";
+
+export const POSTMAN_AUTH_TYPES_MAPPING = {
+  apikey: AUTHORIZATION_TYPES.API_KEY,
+  bearer: AUTHORIZATION_TYPES.BEARER_TOKEN,
+  basic: AUTHORIZATION_TYPES.BASIC_AUTH,
+};
