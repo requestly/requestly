db38ac2fe47a7158c8668c9b78049bb2d105d17f
Sahil Gupta <sahil865gupta@gmail.com>
2024-12-20 15:51:21 +0530

    added desktopStorageClient

--- app/src/services/clientStorageService/clients/desktop.ts
+++ app/src/services/clientStorageService/clients/desktop.ts
@@ -1,23 +1,67 @@
 import { BaseStorageClient } from "./base";
 
+const isDesktopStorageServiceAvailable = () => {
+  return window.RQ && window.RQ.DESKTOP && window.RQ.DESKTOP.SERVICES && window.RQ.DESKTOP.SERVICES.STORAGE_SERVICE;
+};
+
 class DesktopStorageClient extends BaseStorageClient {
   /** get */
-  async getStorageObject(key: string) {}
+  async getStorageObject(key: string) {
+    return new Promise((resolve, reject) => {
+      if (isDesktopStorageServiceAvailable()) {
+        resolve(window.RQ.DESKTOP.SERVICES.STORAGE_SERVICE.getStorageObject(key));
+      } else reject("Couldnt hit desktop API");
+    });
+  }
 
   /** getAll */
-  async getStorageSuperObject(): Promise<Record<string, any> | void> {}
+  async getStorageSuperObject(): Promise<Record<string, any> | void> {
+    return new Promise((resolve, reject) => {
+      if (isDesktopStorageServiceAvailable()) {
+        resolve(window.RQ.DESKTOP.SERVICES.STORAGE_SERVICE.getStorageSuperObject());
+      } else reject("Couldnt hit desktop API");
+    });
+  }
 
   /** Save Multiple */
-  async saveStorageObject(object: Record<string, any>) {}
+  async saveStorageObject(object: Record<string, any>): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (isDesktopStorageServiceAvailable()) {
+        resolve(window.RQ.DESKTOP.SERVICES.STORAGE_SERVICE.setStorageObject(object));
+      } else reject("Couldnt hit desktop API");
+    });
+  }
 
   /** Remove */
-  async removeStorageObject(key: string) {}
+  async removeStorageObject(key: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (isDesktopStorageServiceAvailable()) {
+        resolve(window.RQ.DESKTOP.SERVICES.STORAGE_SERVICE.deleteItem(key));
+      } else reject("Couldnt hit desktop API");
+    });
+  }
 
   /** Remove Multiple */
-  async removeStorageObjects(key: string[]): Promise<any> {}
+  async removeStorageObjects(keys: string[]): Promise<any> {
+    if (!keys) {
+      return new Error("Empty objects to remove list");
+    }
+
+    const removeStorageObjectPromises: Promise<any>[] = [];
+    keys.forEach((key) => {
+      removeStorageObjectPromises.push(this.removeStorageObject(key));
+    });
+    return Promise.all(removeStorageObjectPromises).catch((err) => err);
+  }
 
   /** Reset */
-  async clearStorage() {}
+  async clearStorage(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (isDesktopStorageServiceAvailable()) {
+        resolve(window.RQ.DESKTOP.SERVICES.STORAGE_SERVICE.clearStorage());
+      } else reject("Couldnt hit desktop API");
+    });
+  }
 }
 
 export default DesktopStorageClient;
