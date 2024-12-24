4812edac0cdc3185c562e1cdd7afbea84c77ee5e
Aarush Srivastava <aarush@requestly.io>
2024-12-24 10:26:03 +0530

    [ENGG-2546] Keyboard Shortcuts - UX improvement in API client (#2450)
    * keyboard shortcut UX improvement
    
    * fix: onKeyDown not getting latest entry snapshot
    
    * added cmd+enter key support
    
    * removed comments
    
    * removed redundant preventDefault()
    
    ---------
    
    Co-authored-by: Mac <mac@Macs-MacBook-Pro.local>

--- app/src/features/apiClient/screens/environment/components/SingleLineEditor/SingleLineEditor.tsx
+++ app/src/features/apiClient/screens/environment/components/SingleLineEditor/SingleLineEditor.tsx
@@ -11,6 +11,7 @@ interface RQSingleLineEditorProps {
   onChange: (value: string) => void;
   placeholder?: string;
   onPressEnter?: (event: KeyboardEvent, text: string) => void;
+  onKeyDown?: (event: KeyboardEvent, text: string) => void;
   onBlur?: (text: string) => void;
   variables?: Record<string, any>;
 }
@@ -21,19 +22,35 @@ export const RQSingleLineEditor: React.FC<RQSingleLineEditorProps> = ({
   onChange,
   placeholder,
   onPressEnter,
+  onKeyDown,
   onBlur,
   variables = {},
 }) => {
   const editorRef = useRef(null);
   const editorViewRef = useRef(null);
+  /*
+  onKeyDown, onBlur and onChange is in the useEffect dependencies (implicitly through the editor setup), 
+  which causes the editor to be recreated when onKeyDown changes
+  Hence creating a ref for onKeyDown, onBlur and onChange to avoid the editor being recreated
+  
+  */
+  const onKeyDownRef = useRef(onKeyDown);
+  const onBlurRef = useRef(onBlur);
+  const onChangeRef = useRef(onChange);
+
+  useEffect(() => {
+    onKeyDownRef.current = onKeyDown;
+    onBlurRef.current = onBlur;
+    onChangeRef.current = onChange;
+  }, [onKeyDown, onBlur, onChange]);
 
   const [hoveredVariable, setHoveredVariable] = useState(null); // Track hovered variable
   const [popupPosition, setPopupPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     if (editorViewRef.current) {
-      editorViewRef.current.destroy(); // Destroy the current editor view
-      editorViewRef.current = null; // Reset the reference
+      editorViewRef.current.destroy();
+      editorViewRef.current = null;
     }
 
     editorViewRef.current = new EditorView({
@@ -42,16 +59,26 @@ export const RQSingleLineEditor: React.FC<RQSingleLineEditorProps> = ({
         doc: defaultValue ?? "",
         extensions: [
           EditorState.transactionFilter.of((tr) => {
-            return tr.newDoc.lines > 1 ? [] : [tr]; // Prevent new lines
+            return tr.newDoc.lines > 1 ? [] : [tr];
           }),
           EditorView.updateListener.of((update) => {
             if (update.docChanged) {
-              onChange?.(update.state.doc.toString());
+              onChangeRef.current?.(update.state.doc.toString());
             }
           }),
           EditorView.domEventHandlers({
             blur: (_, view) => {
-              onBlur?.(view.state.doc.toString());
+              onBlurRef.current?.(view.state.doc.toString());
+            },
+            keydown: (event, view) => {
+              const currentText = view.state.doc.toString();
+              if ((event.metaKey || event.ctrlKey) && event.key.toLowerCase() === "s") {
+                event.preventDefault();
+                onKeyDownRef.current?.(event, currentText);
+              } else if ((event.metaKey || event.ctrlKey) && event.key.toLowerCase() === "enter") {
+                event.preventDefault();
+                onKeyDownRef.current?.(event, currentText);
+              }
             },
             keypress: (event, view) => {
               if (event.key === "Enter") {
@@ -73,26 +100,12 @@ export const RQSingleLineEditor: React.FC<RQSingleLineEditorProps> = ({
 
     return () => {
       editorViewRef.current?.destroy();
-      editorViewRef.current = null; // Ensure cleanup
+      editorViewRef.current = null;
     };
-
     //Need to disable to implement the onChange handler
     // Shouldn't be recreated every render
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [placeholder, variables]);
-
-  // Commenting out for now, breaking the the editors cursor position
-  // useEffect(() => {
-  //   if (editorViewRef.current && defaultValue !== undefined) {
-  //     editorViewRef.current.dispatch({
-  //       changes: {
-  //         from: 0,
-  //         to: editorViewRef.current.state.doc.length,
-  //         insert: defaultValue,
-  //       },
-  //     });
-  //   }
-  // }, [defaultValue]);
 
   return (
     <>
