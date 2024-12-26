8da1fa4416c382c8ab6d5b650b046fc101713466
Devansh Gupta <devansh@requestly.io>
2024-12-27 00:10:15 +0530

    [ENG-2717]: Dynamic defult value for auth  according to root level entry or not

--- app/src/features/apiClient/screens/apiClient/components/clientView/components/request/components/RequestTabs/RequestTabs.tsx
+++ app/src/features/apiClient/screens/apiClient/components/clientView/components/request/components/RequestTabs/RequestTabs.tsx
@@ -128,7 +128,7 @@ const RequestTabs: React.FC<Props> = ({
     }
 
     return items;
-  }, [requestEntry, setRequestEntry, setContentType, isApiClientScripts, variables, collectionId]);
+  }, [requestEntry, setRequestEntry, setContentType, isApiClientScripts, variables]);
 
   return (
     <Tabs
