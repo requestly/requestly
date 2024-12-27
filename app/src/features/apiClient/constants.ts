55ccf631828ac9c4e9fd1ebe09915b7daf422685
Devansh Gupta <44257691+Ethical98@users.noreply.github.com>
2024-12-27 13:42:50 +0530

    ENG-2717 -> Add Inherit from auth in postman importer (#2461)
    * [ENG-2717]. add inherit from auth support in postman importer
    
    * chore: update postman import copy
    
    * [ENG-2717]. code refactor
    
    * [ENG-2717]. fix error
    
    * [ENG-2717]-typo fix
    
    * [ENG-2717]. typo fix
    
    * [ENG-2717]: PR feedbacks
    
    * [ENG-2717]: Dynamic defult value for auth  according to root level entry or not
    
    * [ENG-2717]: Dynamic defult value for auth  according to root level entry or not
    
    * [ENG-2717]. handle default cases for postman importer
    
    * [ENG-2717]. handle default cases when new request or collection is created
    
    * [ENG-2717]. fix code break when auth is empty
    
    ---------
    
    Co-authored-by: Parth Bhardwaj <mac@Macs-MacBook-Pro.local>

--- app/src/features/apiClient/constants.ts
+++ app/src/features/apiClient/constants.ts
@@ -5,7 +5,20 @@ export const CONTENT_TYPE_HEADER = "Content-Type";
 export const DEMO_API_URL = "https://app.requestly.io/echo";
 
 export const POSTMAN_AUTH_TYPES_MAPPING = {
+  inherit: AUTHORIZATION_TYPES.INHERIT,
+  noauth: AUTHORIZATION_TYPES.NO_AUTH,
   apikey: AUTHORIZATION_TYPES.API_KEY,
   bearer: AUTHORIZATION_TYPES.BEARER_TOKEN,
   basic: AUTHORIZATION_TYPES.BASIC_AUTH,
+};
+
+export const POSTMAN_FIELD_MAPPING = {
+  mapping: {
+    in: "addTo",
+    query: "QUERY",
+    token: "bearer",
+  },
+  get(key: string): string {
+    return this.mapping[key] ?? key;
+  },
 };
