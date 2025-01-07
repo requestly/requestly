3fd2ad4fa2fc2cf798c6ff6ab4177beaf6be35fc
Devansh Gupta <devansh@requestly.io>
2025-01-07 15:49:00 +0530

    ENG-2786. Add dynamic sizing for bottom sheet according to screen

--- app/src/componentsV2/BottomSheet/components/BottomSheetLayout/components/SplitPaneLayout/SplitPaneLayout.tsx
+++ app/src/componentsV2/BottomSheet/components/BottomSheetLayout/components/SplitPaneLayout/SplitPaneLayout.tsx
@@ -8,9 +8,10 @@ interface Props {
   bottomSheet: ReactNode;
   children: ReactNode;
   minSize?: number;
+  defaultSizing?: boolean;
 }
 
-export const SplitPaneLayout: React.FC<Props> = ({ bottomSheet, children, minSize = 100 }) => {
+export const SplitPaneLayout: React.FC<Props> = ({ bottomSheet, children, minSize = 100, defaultSizing = true }) => {
   const { sheetPlacement, isBottomSheetOpen } = useBottomSheetContext();
   const isSheetPlacedAtBottom = sheetPlacement === BottomSheetPlacement.BOTTOM;
   const splitPane = useRef(null);
@@ -20,7 +21,7 @@ export const SplitPaneLayout: React.FC<Props> = ({ bottomSheet, children, minSiz
   useEffect(() => {
     if (isSheetPlacedAtBottom && splitPane.current) {
       if (isBottomSheetOpen) {
-        splitPane.current.split.setSizes([40, 60]);
+        splitPane.current.split.setSizes(defaultSizing ? [40, 60] : [50, 50]);
       } else {
         splitPane.current.split.setSizes([100, 0]);
       }
@@ -28,38 +29,40 @@ export const SplitPaneLayout: React.FC<Props> = ({ bottomSheet, children, minSiz
   }, [isBottomSheetOpen, isSheetPlacedAtBottom]);
 
   return (
-    <Split
-      ref={splitPane}
-      direction={splitDirection}
-      sizes={isSheetPlacedAtBottom ? [100, 0] : [58, 42]}
-      minSize={isSheetPlacedAtBottom ? minSize : 350}
-      className={`bottomsheet-layout-container ${
-        splitDirection === SplitDirection.HORIZONTAL ? "horizontal-split" : "vertical-split"
-      }`}
-      gutterStyle={() => {
-        return {
-          height: splitDirection === SplitDirection.HORIZONTAL ? "100%" : "8px",
-          width: splitDirection === SplitDirection.HORIZONTAL ? "8px" : "100%",
-        };
-      }}
-      gutterAlign="center"
-    >
-      <Row style={splitDirection === SplitDirection.HORIZONTAL ? { height: "100%" } : { width: "100%" }}>
-        <Col className="content" span={24}>
-          {children}
-        </Col>
-      </Row>
-      <Row style={splitDirection === SplitDirection.HORIZONTAL ? { height: "100%" } : { width: "100%" }}>
-        <Col
-          span={24}
-          className={`${isSheetPlacedAtBottom ? "bottom-sheet-container" : "bottom-sheet-panel-container"}`}
-          style={{
-            height: isSheetPlacedAtBottom ? "auto" : "100%",
-          }}
-        >
-          {bottomSheet}
-        </Col>
-      </Row>
-    </Split>
+    <div className={`split-pane-layout-wrapper ${defaultSizing ? "default" : ""}`}>
+      <Split
+        ref={splitPane}
+        direction={splitDirection}
+        sizes={isSheetPlacedAtBottom ? [100, 0] : [58, 42]}
+        minSize={isSheetPlacedAtBottom ? minSize : 350}
+        className={`bottomsheet-layout-container ${
+          splitDirection === SplitDirection.HORIZONTAL ? "horizontal-split" : "vertical-split"
+        }`}
+        gutterStyle={() => {
+          return {
+            height: splitDirection === SplitDirection.HORIZONTAL ? "100%" : "8px",
+            width: splitDirection === SplitDirection.HORIZONTAL ? "8px" : "100%",
+          };
+        }}
+        gutterAlign="center"
+      >
+        <Row style={splitDirection === SplitDirection.HORIZONTAL ? { height: "100%" } : { width: "100%" }}>
+          <Col className="content" span={24}>
+            {children}
+          </Col>
+        </Row>
+        <Row style={splitDirection === SplitDirection.HORIZONTAL ? { height: "100%" } : { width: "100%" }}>
+          <Col
+            span={24}
+            className={`${isSheetPlacedAtBottom ? "bottom-sheet-container" : "bottom-sheet-panel-container"}`}
+            style={{
+              height: isSheetPlacedAtBottom ? "auto" : "100%",
+            }}
+          >
+            {bottomSheet}
+          </Col>
+        </Row>
+      </Split>
+    </div>
   );
 };
