0681b39eb31408cf1b8b9ed9c9b9fbfaf1684fd7
Nafees Nehar <nafees87n@gmail.com>
2025-01-10 16:14:23 +0530

    [ENGG-2657] feat: documentation links along the script panel (#2532)
    * updated editor doc link
    
    * added info icon docs
    
    * fix: style

--- app/src/features/apiClient/screens/apiClient/components/clientView/components/Scripts/components/ScriptEditor/ScriptEditor.tsx
+++ app/src/features/apiClient/screens/apiClient/components/clientView/components/Scripts/components/ScriptEditor/ScriptEditor.tsx
@@ -3,7 +3,8 @@ import { EditorLanguage } from "componentsV2/CodeEditor";
 import Editor from "componentsV2/CodeEditor/components/Editor/Editor";
 import { useMemo, useState } from "react";
 import { RQAPI } from "features/apiClient/types";
-import { Radio } from "antd";
+import { Radio, Tooltip } from "antd";
+import { MdInfoOutline } from "@react-icons/all-files/md/MdInfoOutline";
 import "./scriptEditor.scss";
 
 interface ScriptEditorProps {
@@ -12,8 +13,10 @@ interface ScriptEditorProps {
 }
 
 const DEFAULT_SCRIPT_VALUES = {
-  [RQAPI.ScriptType.PRE_REQUEST]: "// Use JavaScript to configure this request dynamically\n",
-  [RQAPI.ScriptType.POST_RESPONSE]: "// Use JavaScript to visualize responses\n",
+  [RQAPI.ScriptType.PRE_REQUEST]:
+    "// **********************************************\n// 🛠️ Learn more about scripts and snippets: https://docs.requestly.com/general/api-client/scripts\n// **********************************************\n",
+  [RQAPI.ScriptType.POST_RESPONSE]:
+    "// **********************************************\n// 🛠️ Use JavaScript to visualize responses: https://docs.requestly.com/general/api-client/scripts\n// **********************************************\n",
 };
 
 export const ScriptEditor: React.FC<ScriptEditorProps> = ({ scripts, setScripts }) => {
@@ -27,19 +30,27 @@ export const ScriptEditor: React.FC<ScriptEditorProps> = ({ scripts, setScripts 
 
   const scriptTypeOptions = useMemo(() => {
     return (
-      <Radio.Group
-        className="api-client-script-type-selector"
-        value={scriptType}
-        onChange={(e) => setScriptType(e.target.value)}
-        size="small"
-      >
-        <Radio.Button className="api-client-script-type-selector__btn" value={RQAPI.ScriptType.PRE_REQUEST}>
-          Pre-request
-        </Radio.Button>
-        <Radio.Button className="api-client-script-type-selector__btn" value={RQAPI.ScriptType.POST_RESPONSE}>
-          Post-response
-        </Radio.Button>
-      </Radio.Group>
+      <>
+        <Radio.Group
+          className="api-client-script-type-selector"
+          value={scriptType}
+          onChange={(e) => setScriptType(e.target.value)}
+          size="small"
+        >
+          <Radio.Button className="api-client-script-type-selector__btn" value={RQAPI.ScriptType.PRE_REQUEST}>
+            Pre-request
+          </Radio.Button>
+          <Radio.Button className="api-client-script-type-selector__btn" value={RQAPI.ScriptType.POST_RESPONSE}>
+            Post-response
+          </Radio.Button>
+        </Radio.Group>
+        <Tooltip title="Learn more about using scripts in API requests" showArrow={false} placement="right">
+          <MdInfoOutline
+            className="api-client-script-type-selector__info-icon"
+            onClick={() => window.open("https://docs.requestly.com/general/api-client/scripts", "_blank")}
+          />
+        </Tooltip>
+      </>
     );
   }, [scriptType]);
 
