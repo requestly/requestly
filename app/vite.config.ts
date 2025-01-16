7ccba8abde94faeb37d3e2cc62731392c367b308
Nafees Nehar <nafees87n@gmail.com>
2025-01-16 13:44:14 +0530

    fix: package-local

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -19,9 +19,10 @@ const config = async ({ mode }) => {
 
   return defineConfig({
     define: {
-      global: "window",
+      global: "globalThis",
       "process.env": loadEnv(mode, process.cwd(), ""),
     },
+    worker: {},
     plugins: [
       nodePolyfills(),
 
