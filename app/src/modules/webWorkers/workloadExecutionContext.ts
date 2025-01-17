9a5a8f54c79b612dbd0595e325890eeeaf6572e8
Nafees Nehar <nafees87n@gmail.com>
2025-01-17 21:16:34 +0530

    fix: type

--- app/src/modules/webWorkers/workloadExecutionContext.ts
+++ app/src/modules/webWorkers/workloadExecutionContext.ts
@@ -1,4 +1,4 @@
-import { RQWorker } from "../../features/apiClient/helpers/modules/scriptsV2/worker/implementation/RQScriptWebWorker";
+import { RQWorker } from "features/apiClient/helpers/modules/scriptsV2/worker/interface/RQWorker";
 
 export class WorkloadExecutionContext<T extends RQWorker> {
   constructor(private worker: T, private workerReleaseFunction: (worker: RQWorker) => void) {}
