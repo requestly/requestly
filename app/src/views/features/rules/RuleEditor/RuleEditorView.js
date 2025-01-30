979fc8af6fb881f380e13cafb113dcf5c7ffd861
Nafees Nehar <nafees87n@gmail.com>
2025-01-30 17:46:50 +0530

    fix: extension install CTA

--- app/src/views/features/rules/RuleEditor/RuleEditorView.js
+++ app/src/views/features/rules/RuleEditor/RuleEditorView.js
@@ -1,7 +1,20 @@
 import { BottomSheetPlacement, BottomSheetProvider } from "componentsV2/BottomSheet";
 import RuleEditor from "./RuleEditor";
+import { useSelector } from "react-redux";
+import { getAppMode } from "store/selectors";
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import { isExtensionInstalled } from "actions/ExtensionActions";
+import InstallExtensionCTA from "components/misc/InstallExtensionCTA";
 
 const RuleEditorView = () => {
+  const appMode = useSelector(getAppMode);
+
+  if (appMode !== GLOBAL_CONSTANTS.APP_MODES.DESKTOP) {
+    if (!isExtensionInstalled()) {
+      return <InstallExtensionCTA />;
+    }
+  }
+
   return (
     <BottomSheetProvider defaultPlacement={BottomSheetPlacement.BOTTOM}>
       <RuleEditor />
