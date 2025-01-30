207c121f9b1cce297c844c705cebde4d03c323fe
Parth Bhardwaj <parthb.git@gmail.com>
2025-01-30 12:54:30 +0530

    ENGG-2929 Fix: global env variable disappear when switching between workspaces

--- app/src/layouts/AppLayout/index.tsx
+++ app/src/layouts/AppLayout/index.tsx
@@ -44,7 +44,7 @@ const App: React.FC = () => {
   useGeoLocation();
   useIsExtensionEnabled();
   useBillingTeamsListener();
-  useEnvironmentManager();
+  useEnvironmentManager({ manageGlobalEnv: true });
   useAppLanguageObserver();
   // useInitializeNewUserSessionRecordingConfig();
 
