147ceef89f09955a8562d51be91be6996b1fd482
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-01-31 21:50:00 +0530

    [ENGG-2933]: Changes in user plan details card and FAQ for student program (#2626)

--- app/src/config/constants/sub/links.js
+++ app/src/config/constants/sub/links.js
@@ -144,6 +144,8 @@ const LINKS = {
   CALENDLY_LINK: "https://calendly.com/requestly/sagar",
 
   ACCELERATOR_PROGRAM_FORM_LINK: "https://app.formbricks.com/s/cm1ewcpkt0000djs29ct7rpy2",
+
+  STUDENT_PROGRAM_DOC: "https://requestly.notion.site/Requestly-Student-Program-12dd193f3c998018ad45d31c784f3131",
 };
 
 export default LINKS;
