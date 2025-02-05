21b9c163a5d602042684b9868b6d814bd6ca543d
Rahul Ramteke <iostreamer007@gmail.com>
2025-02-05 13:22:01 +0530

    Added tracking event when env changes are performed in db

--- app/src/backend/environment/index.ts
+++ app/src/backend/environment/index.ts
@@ -15,9 +15,10 @@ import {
 } from "firebase/firestore";
 import { EnvironmentData, EnvironmentMap, EnvironmentVariables } from "./types";
 import { CollectionVariableMap, RQAPI } from "features/apiClient/types";
-import { trackEnvironmentCreatedInDB } from "features/apiClient/screens/environment/analytics";
+import { trackEnvironmentCreatedInDB, trackEnvironmentDeletedFromDB, trackEnvironmentsFetchedFromDB, trackEnvironmentUpdatedInDB } from "features/apiClient/screens/environment/analytics";
 import { fetchLock } from "./fetch-lock";
 import { patchMissingIdInVariables } from "backend/apiClient/utils";
+import { isGlobalEnvironment } from "features/apiClient/screens/environment/utils";
 
 const db = getFirestore(firebaseApp);
 
@@ -34,7 +35,7 @@ export const createNonGlobalEnvironmentInDB = async (
     name: environmentName,
     variables,
   }).then((doc) => {
-    trackEnvironmentCreatedInDB("non_global");
+    trackEnvironmentCreatedInDB(doc.id, "non_global");
     return {
       id: doc.id,
       name: environmentName,
@@ -55,9 +56,11 @@ export const updateEnvironmentVariablesInDB = async (
     ])
   );
 
-  return updateDoc(getDocPath(ownerId, environmentId), {
+  await updateDoc(getDocPath(ownerId, environmentId), {
     variables: newVariables,
   });
+
+  trackEnvironmentUpdatedInDB(environmentId, isGlobalEnvironment(environmentId) ? "global" : "non_global");
 };
 
 export const removeEnvironmentVariableFromDB = async (
@@ -67,9 +70,11 @@ export const removeEnvironmentVariableFromDB = async (
     key: string;
   }
 ) => {
-  return updateDoc(getDocPath(ownerId, payload.environmentId), {
+  await updateDoc(getDocPath(ownerId, payload.environmentId), {
     [`variables.${payload.key}`]: deleteField(),
   });
+
+  trackEnvironmentUpdatedInDB(payload.environmentId, isGlobalEnvironment(payload.environmentId) ? "global" : "non_global");
 };
 
 export const attachEnvironmentVariableListener = (
@@ -120,7 +125,7 @@ const createGlobalEnvironmentInDB = async (ownerId: string) => {
   const docRef = doc(db, "environments", ownerId, "environments", "global");
   const globalEnvironment: EnvironmentData = { name: "Global variables", variables: {}, id: "global" };
   await setDoc(docRef, globalEnvironment);
-  trackEnvironmentCreatedInDB("global");
+  trackEnvironmentCreatedInDB("global", "global");
   return globalEnvironment;
 };
 
@@ -155,13 +160,16 @@ export const fetchAllEnvironmentDetails = async (ownerId: string) => {
     return environmentDetails;
   } finally {
     releaseLock();
+    trackEnvironmentsFetchedFromDB();
   }
 };
 
 export const updateEnvironmentNameInDB = async (ownerId: string, environmentId: string, newName: string) => {
-  return updateDoc(getDocPath(ownerId, environmentId), {
+  await updateDoc(getDocPath(ownerId, environmentId), {
     name: newName,
   });
+
+  trackEnvironmentUpdatedInDB(environmentId, isGlobalEnvironment(environmentId) ? "global" : "non_global");
 };
 
 export const duplicateEnvironmentInDB = async (
@@ -181,5 +189,6 @@ export const duplicateEnvironmentInDB = async (
 };
 
 export const deleteEnvironmentFromDB = async (ownerId: string, environmentId: string) => {
-  return deleteDoc(getDocPath(ownerId, environmentId));
+  await deleteDoc(getDocPath(ownerId, environmentId));
+  trackEnvironmentDeletedFromDB(environmentId);
 };
