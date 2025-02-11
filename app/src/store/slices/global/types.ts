8e94e23ba4d29f5fe2b73774413857031716d432
Devansh Gupta <devansh@requestly.io>
2025-02-11 12:38:04 +0530

    [ENG-2997]: Add types to Rules actions

--- app/src/store/slices/global/types.ts
+++ app/src/store/slices/global/types.ts
@@ -1,8 +1,10 @@
 import { EditorToastState } from "./editorToast/types";
 import { GlobalModals } from "./modals/types";
+import { RulesState } from "./rules/types";
 
 export interface GlobalSliceState {
   activeModals: GlobalModals;
   editorToast: EditorToastState["editorToast"];
+  rules: RulesState;
   [key: string]: any;
 }
