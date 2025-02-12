b3a9b1ae4f78ee6a10eefe0aef10e830ff329099
Devansh Gupta <44257691+Ethical98@users.noreply.github.com>
2025-02-12 11:30:21 +0530

    Revert "[ENG-2860]: Homepage Design Changes (#2653)"
    This reverts commit 80311afea26a888fc7368812ba0f8d3d125fa5a7.

--- app/src/components/Home/components/EmptyCard/index.tsx
+++ app/src/components/Home/components/EmptyCard/index.tsx
@@ -3,38 +3,25 @@ import React, { ReactNode } from "react";
 interface Props {
   icon: string | ReactNode;
   title: string;
-  features: string[];
   description?: string;
-  primaryAction?: ReactNode;
-  playIcon: { src: string; label: string; url: string; onClick: () => void };
+  primaryButton?: ReactNode;
+  secondaryButton?: ReactNode;
 }
 
-export const HomepageEmptyCard: React.FC<Props> = ({ icon, title, description, features, primaryAction, playIcon }) => {
+export const HomepageEmptyCard: React.FC<Props> = ({ icon, title, description, primaryButton, secondaryButton }) => {
   return (
-    <>
-      <div className="homepage-emptycard">
-        <div className="header-section">
-          {typeof icon === "string" ? (
-            <img src={icon} alt="icon" className="homepage-emptycard-icon-img" />
-          ) : (
-            <div className="homepage-emptycard-icon">{icon}</div>
-          )}
-          <div className="header-content">
-            <h1 className="homepage-emptycard-title">{title}</h1>
-            <p className="mt-8 text-center homepage-emptycard-description">{description}</p>
-          </div>
-        </div>
-        <ul className="features">
-          {features.map((feature) => (
-            <li>{feature}</li>
-          ))}
-        </ul>
-        <a className="play-icon" href={playIcon.url} target="__blank" onClick={playIcon.onClick}>
-          <img src={playIcon.src} alt="Play" />
-          <p>{playIcon.label}</p>
-        </a>
+    <div className="homepage-emptycard">
+      {typeof icon === "string" ? (
+        <img src={icon} alt="icon" className="homepage-emptycard-icon-img" />
+      ) : (
+        <div className="homepage-emptycard-icon">{icon}</div>
+      )}
+      <div className="homepage-emptycard-title">{title}</div>
+      <div className="mt-8 text-center homepage-emptycard-description">{description}</div>
+      <div className="homepage-emptycard-actions">
+        <div>{primaryButton}</div>
+        <div className="mt-8">{secondaryButton}</div>
       </div>
-      <div className="action-section">{primaryAction}</div>
-    </>
+    </div>
   );
 };
