e5d0b25490355922138ad7cedce31054fb655118
Nafees Nehar <nafees87n@gmail.com>
2025-02-14 17:03:42 +0530

    [ENGG-2996] fix: typescript errors (#2688)
    * chore: fix mocks types
    
    * chore: fix session recording types
    
    * chore: fix workspace types
    
    * chore: fix mocks types
    
    * chore: fix mock editor types
    
    * chore: fix AuthHandler type
    
    * chore: fix intial value for useAutoScrollableContainer containe ref
    
    * chore: fix initial value
    
    * fix: import
    
    * cleanup
    
    * cleanup
    
    * fix: ts issue
    
    * fix
    
    * fix: errors
    
    * fix: appMode specific actions
    
    * fix: mv3 parser
    
    * fix: charles module
    
    * fix: types
    
    * remove ts-ignore
    
    * refactor
    
    * fix: initial value
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * fix
    
    * cleanup
    
    * cleanup
    
    * added comment
    
    * fix: type
    
    ---------
    
    Co-authored-by: rohanmathur91 <mathurrohan04@gmail.com>

--- app/src/features/apiClient/screens/apiClient/components/sidebar/components/apiClientSidebarHeader/ApiClientSidebarHeader.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/components/apiClientSidebarHeader/ApiClientSidebarHeader.tsx
@@ -179,7 +179,6 @@ export const ApiClientSidebarHeader: React.FC<Props> = ({
       onClick: () => {
         if (!user.loggedIn) {
           dispatch(
-            // @ts-ignore
             globalActions.toggleActiveModal({
               modalName: "authModal",
               newValue: true,
@@ -210,7 +209,6 @@ export const ApiClientSidebarHeader: React.FC<Props> = ({
       onClick: () => {
         if (!user.loggedIn) {
           dispatch(
-            // @ts-ignore
             globalActions.toggleActiveModal({
               modalName: "authModal",
               newValue: true,
