2d9770813de60e762690762cbce3d75632b30566
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-02-17 12:13:48 +0530

    [ENGG-3008]: Update workspace creation modal UI (#2673)

--- app/src/features/mocks/container.tsx
+++ app/src/features/mocks/container.tsx
@@ -3,8 +3,16 @@ import { Outlet } from "react-router-dom";
 import { MocksSidebar } from "./components/MocksSidebar/MocksSidebar";
 import { SecondarySidebarLayout } from "componentsV2/SecondarySidebar";
 import { MocksContextProvider } from "./contexts";
+import { getIsWorkspaceLocal } from "store/features/teams/selectors";
+import { useSelector } from "react-redux";
 
 const MocksFeatureContainer: React.FC = () => {
+  const isWorkspaceLocal = useSelector(getIsWorkspaceLocal);
+
+  if (isWorkspaceLocal) {
+    return <div>NO ACCESS - LOCAL WORKSPACE</div>;
+  }
+
   return (
     <SecondarySidebarLayout secondarySidebar={<MocksSidebar />}>
       <MocksContextProvider>
