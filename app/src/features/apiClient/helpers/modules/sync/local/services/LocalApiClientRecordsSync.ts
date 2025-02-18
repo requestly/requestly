1047a067daa3ddbaa402dc655efd1ca35c9fdac9
Rahul Ramteke <iostreamer007@gmail.com>
2025-02-18 12:35:50 +0530

    Integrating getAllRecords

--- app/src/features/apiClient/helpers/modules/sync/local/services/LocalApiClientRecordsSync.ts
+++ app/src/features/apiClient/helpers/modules/sync/local/services/LocalApiClientRecordsSync.ts
@@ -1,28 +1,41 @@
 import { ApiClientLocalMeta, ApiClientRecordsInterface } from "../../interfaces";
 import { RQAPI } from "features/apiClient/types";
+import { FsManagerServiceAdapter, fsManagerServiceAdapterProvider } from "services/fsManagerServiceAdapter";
 
 export class LocalApiClientRecordsSync implements ApiClientRecordsInterface<ApiClientLocalMeta> {
   meta: ApiClientLocalMeta;
+	fsManagerService: FsManagerServiceAdapter;
 
-  constructor(readonly metadata: ApiClientLocalMeta) {
+	constructor(readonly metadata: ApiClientLocalMeta) {
     this.meta = metadata;
   }
-    async getAllRecords(): RQAPI.RecordsPromise {
-			return [] as any;
-    }
-    getRecord(recordId: string): RQAPI.RecordPromise {
-        throw new Error("Method not implemented.");
-    }
-    createRecord(record: Partial<RQAPI.Record>): RQAPI.RecordPromise {
-        throw new Error("Method not implemented.");
-    }
-    createRecordWithId(record: Partial<RQAPI.Record>, id: string): RQAPI.RecordPromise {
-        throw new Error("Method not implemented.");
-    }
-    updateRecord(record: Partial<RQAPI.Record>, id?: string): RQAPI.RecordPromise {
-        throw new Error("Method not implemented.");
-    }
-    deleteRecords(recordIds: string[]): Promise<{ success: boolean; data: unknown; message?: string; }> {
-        throw new Error("Method not implemented.");
-    }
+
+  private async getAdapter() {
+    return fsManagerServiceAdapterProvider.get(this.meta.rootPath);
+  }
+
+  async getAllRecords(): RQAPI.RecordsPromise {
+		const service = await this.getAdapter();
+		// const records = await service.getAllRecords();
+		console.log('csccd', service);
+		return {
+			success: true,
+			data: [],
+		};
+  }
+  getRecord(recordId: string): RQAPI.RecordPromise {
+      throw new Error("Method not implemented.");
+  }
+  createRecord(record: Partial<RQAPI.Record>): RQAPI.RecordPromise {
+      throw new Error("Method not implemented.");
+  }
+  createRecordWithId(record: Partial<RQAPI.Record>, id: string): RQAPI.RecordPromise {
+      throw new Error("Method not implemented.");
+  }
+  updateRecord(record: Partial<RQAPI.Record>, id?: string): RQAPI.RecordPromise {
+      throw new Error("Method not implemented.");
+  }
+  deleteRecords(recordIds: string[]): Promise<{ success: boolean; data: unknown; message?: string; }> {
+      throw new Error("Method not implemented.");
+  }
 }
