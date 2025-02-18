be5d05d17517bc694a3dc2262b79e29b4df1ab9d
Rahul Ramteke <iostreamer007@gmail.com>
2025-02-18 13:09:55 +0530

    Fixed bug where repo initialization was failing

--- app/src/features/apiClient/helpers/modules/sync/useApiClientSyncRepo.ts
+++ app/src/features/apiClient/helpers/modules/sync/useApiClientSyncRepo.ts
@@ -9,18 +9,20 @@ import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import { ApiClientRepositoryInterface } from "./interfaces";
 
 export const useGetApiClientSyncRepo = () => {
-  const user = useSelector(getUserAuthDetails);
-  const appMode = useSelector(getAppMode);
-  const workspace = useSelector(getCurrentlyActiveWorkspace);
-  const isWorkspaceLocal = useSelector(getIsWorkspaceLocal);
-  const workspaceRootPath = useSelector(getWorkspaceRootPath);
+  const user: Record<string, any> = useSelector(getUserAuthDetails);
+  const appMode: string = useSelector(getAppMode);
+  const workspace: Record<string, any> = useSelector(getCurrentlyActiveWorkspace);
+  const isWorkspaceLocal: boolean = useSelector(getIsWorkspaceLocal);
+  const workspaceRootPath: string = useSelector(getWorkspaceRootPath);
 
   const getRepository:  () => ApiClientRepositoryInterface = useCallback(() => {
 		if (!user.loggedIn) {
 			throw new Error('Data can not be synced unless you log in!');
 		};
     if (isWorkspaceLocal && appMode === GLOBAL_CONSTANTS.APP_MODES.DESKTOP) {
-      return new ApiClientLocalRepository(workspaceRootPath);
+			return new ApiClientLocalRepository({
+				rootPath: workspaceRootPath,
+			});
     } else {
       return new ApiClientCloudRepository({ uid: user.details.profile.uid, teamId: workspace?.id });
     }
