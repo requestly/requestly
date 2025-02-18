477738efcf8683f0531efd058d6c492506b65080
Rahul Ramteke <iostreamer007@gmail.com>
2025-02-18 11:47:02 +0530

    Removed namespaces

--- app/src/features/apiClient/helpers/modules/sync/local/LocalSync.ts
+++
@@ -1,7 +0,0 @@
-import { LocalEnvSync as BaseLocalEnvSync } from "./services/LocalApiClientEnvSync";
-import { LocalApiClientRecordsSync as BaseLocalApiClientRecordsSync } from "./services/LocalApiClientRecordsSync";
-
-export namespace ApiClientLocalSync {
-  export class LocalEnvSync extends BaseLocalEnvSync {}
-  export class LocalApiClientRecordsSync extends BaseLocalApiClientRecordsSync {}
-}
