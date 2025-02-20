d3919076465ccdae43ca3b071454e4cc1dede9ce
Parth Bhardwaj <parthb.git@gmail.com>
2025-02-20 18:13:55 +0530

    update global env check

--- app/src/features/apiClient/screens/environment/utils.ts
+++ app/src/features/apiClient/screens/environment/utils.ts
@@ -1,3 +1,4 @@
 export const isGlobalEnvironment = (environmentId: string) => {
-  return environmentId === "global";
+  // FIXME: isGlobalEnvironment should be a method, which operates on an object or a flag.
+  return environmentId === "global" || environmentId.endsWith("/environments/global.json");
 };
