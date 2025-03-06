773803a510874a1ee2a8bd3b463ebb5757a8235a
rohanmathur91 <mathurrohan04@gmail.com>
2025-03-06 15:19:11 +0530

    fix: validation util for undefined role

--- app/src/features/rbac/utils.ts
+++ app/src/features/rbac/utils.ts
@@ -2,12 +2,12 @@ import { ROLE_TO_RESOURCE_PERMISSIONS_MAP } from "./constants/config";
 import { RBAC } from "./types";
 
 export const validateResourcePermissionByRole = (
-  role: RBAC.Role,
+  role: RBAC.Role | undefined,
   resource: RBAC.Resource,
   permissionToValidate: RBAC.Permission
-): {isValidPermission: boolean} => {
-  const resourcePermissions = ROLE_TO_RESOURCE_PERMISSIONS_MAP[role][resource];
-  const isAllowed = resourcePermissions[permissionToValidate];
+): { isValidPermission: boolean } => {
+  const resourcePermissions = ROLE_TO_RESOURCE_PERMISSIONS_MAP[role]?.[resource];
+  const isAllowed = resourcePermissions?.[permissionToValidate];
   if (!isAllowed) {
     return {
       isValidPermission: false,
