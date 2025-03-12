21bbc16aec3d0d1a2ce999216a1b77d49ca52dbf
rohanmathur91 <mathurrohan04@gmail.com>
2025-03-12 20:21:41 +0530

    fix: make RoleBasedComponent children prop optional

--- app/src/features/rbac/components/RoleBasedComponent/index.tsx
+++ app/src/features/rbac/components/RoleBasedComponent/index.tsx
@@ -5,11 +5,11 @@ import { RBAC } from "features/rbac/types";
 interface Props {
   resource: keyof typeof RBAC.Resource;
   permission: keyof typeof RBAC.Permission;
-  children: React.ReactNode;
+  children?: React.ReactNode;
   fallback?: React.ReactNode;
 }
 
-export const RoleBasedComponent: React.FC<Props> = ({ resource, permission, children, fallback = null }) => {
+export const RoleBasedComponent: React.FC<Props> = ({ resource, permission, children = null, fallback = null }) => {
   const { validatePermission } = useRBAC();
   const { isValidPermission } = validatePermission(resource, permission);
 
