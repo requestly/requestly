cb0bc169121df7864a4526c95f6cd350de4b9dea
Nafees Nehar <nafees87n@gmail.com>
2025-03-13 13:50:02 +0530

    added genericstate

--- app/src/componentsV2/Tabs/components/TabItem.tsx
+++ app/src/componentsV2/Tabs/components/TabItem.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import { TabState } from "../store/tabStore";
 import { StoreApi } from "zustand";
+import { GenericStateContext } from "hooks/useGenericState";
+import { useTabService } from "../store/tabServiceStore";
 
 export const TabItem: React.FC<React.PropsWithChildren<{ store: StoreApi<TabState> }>> = React.memo((props) => {
-  return <>{props.children}</>;
+  const { incrementVersion } = useTabService();
+
+  return (
+    <GenericStateContext.Provider
+      value={{
+        setTitle: (title: string) => {
+          props.store.getState().setTitle(title);
+          incrementVersion();
+        },
+      }}
+    >
+      {props.children}
+    </GenericStateContext.Provider>
+  );
 });
