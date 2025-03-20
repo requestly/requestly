1ace5530039a3caabd6e1b9b4449e1e4b9a775af
Aarushsr12 <aarushsrivastava1212@gmail.com>
2025-03-20 21:07:21 +0530

    cleanup

--- app/src/componentsV2/CodeEditor/components/EditorV2/index.ts
+++ app/src/componentsV2/CodeEditor/components/EditorV2/index.ts
@@ -3,7 +3,6 @@ import lazyWithRetry from "utils/lazyWithRetry";
 const EditorV2 = lazyWithRetry(
   () =>
     import(/* webpackChunkName: "EditorComponent" */ "./Editor").then((module) => {
-      console.log("debug");
       return module;
     }),
   null
