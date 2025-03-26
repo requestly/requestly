0185c59c8e651a3ec2623af6e5897b394e2eb13d
Nafees Nehar <nafees87n@gmail.com>
2025-03-26 14:32:33 +0530

    [ENGG-3253] integrate draft request component with tabs (#2817)
    * added poc code
    
    * fix
    
    * remove temp changes
    
    * updated details
    
    * fix: name
    
    * fix: id for draft tab
    
    * fix: apiEntryDetails in draft container
    
    * fix: tabsource placement
    
    * refactor: variable names and folder structure

--- app/src/componentsV2/Tabs/components/TabItem.tsx
+++ app/src/componentsV2/Tabs/components/TabItem.tsx
@@ -3,6 +3,7 @@ import { TabState } from "../store/tabStore";
 import { StoreApi } from "zustand";
 import { GenericStateContext } from "hooks/useGenericState";
 import { useTabServiceStore } from "../store/tabServiceStore";
+import { updateUrlPath } from "../utils";
 
 export const TabItem: React.FC<React.PropsWithChildren<{ store: StoreApi<TabState> }>> = React.memo((props) => {
   const incrementVersion = useTabServiceStore().use.incrementVersion();
@@ -13,6 +14,10 @@ export const TabItem: React.FC<React.PropsWithChildren<{ store: StoreApi<TabStat
         setTitle: (title: string) => {
           props.store.getState().setTitle(title);
           incrementVersion();
+        },
+        setUrl: (path: string) => {
+          props.store.getState().source.setUrlPath(path);
+          updateUrlPath(path);
         },
       }}
     >
--- app/src/componentsV2/Tabs/helpers/tabSource.ts
+++ app/src/componentsV2/Tabs/helpers/tabSource.ts
@@ -3,6 +3,7 @@ import React from "react";
 export abstract class AbstractTabSource {
   abstract component: NonNullable<React.ReactNode>;
   abstract metadata: Record<string, any>;
+  abstract urlPath: string;
 
   abstract getSourceId(): string; // Identifier of the entity
   abstract getSourceName(): string;
