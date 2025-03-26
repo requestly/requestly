6928142c35a4d8b4c92c8e3de6b465932871dcb6
rohanmathur91 <mathurrohan04@gmail.com>
2025-03-26 16:16:43 +0530

    chore: update type

---
+++ app/src/componentsV2/Tabs/types.ts
@@ -0,0 +1,12 @@
+import { CollectionViewTabSource } from "features/apiClient/screens/apiClient/components/clientView/components/Collection/collectionViewTabSource";
+import { DraftRequestContainerTabSource } from "features/apiClient/screens/apiClient/components/clientView/components/DraftRequestContainer/draftRequestContainerTabSource";
+import { RequestViewTabSource } from "features/apiClient/screens/apiClient/components/clientView/components/RequestView/requestViewTabSource";
+import { EnvironmentViewTabSource } from "features/apiClient/screens/environment/components/environmentView/EnvironmentViewTabSource";
+
+export type TabSource =
+  | DraftRequestContainerTabSource
+  | RequestViewTabSource
+  | CollectionViewTabSource
+  | EnvironmentViewTabSource;
+
+export type TabSourceFactory = (...args: unknown[]) => TabSource;
