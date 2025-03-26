8981a8fde175e4a6465381d456853fe108e6bfff
rohanmathur91 <mathurrohan04@gmail.com>
2025-03-26 19:18:57 +0530

    update tab source factory

--- app/src/componentsV2/Tabs/types.ts
+++ app/src/componentsV2/Tabs/types.ts
@@ -9,6 +9,11 @@ export interface MatchedTabSource {
   matchedPath: PathMatch<string>;
 }
 
+export interface MatchedTabSource {
+  sourceFactory: TabSourceFactory;
+  matchedPath: PathMatch<string>;
+}
+
 export type TabSource =
   | DraftRequestContainerTabSource
   | RequestViewTabSource
