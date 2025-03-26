0fdfc012f56ab24e6792b7221e633913995c5ed2
Nafees Nehar <nafees87n@gmail.com>
2025-03-26 14:32:33 +0530

    [ENGG-3253] integrate draft request component with tabs (#2817)
    * added poc code
    
    * fix
    
    * remove temp changes
    
    * updated details
    
    * fix: name
    
    * fix: id for draft tab
    
    * fix: apiEntryDetails in draft container
    
    * fix: tabsource placement
    
    * refactor: variable names and folder structure

---
+++ app/src/features/apiClient/screens/apiClient/components/clientView/components/DraftRequestContainer/DraftRequestContainer.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback, useState } from "react";
+import { RequestView } from "../RequestContainer";
+import { DraftRequestView } from "../DraftRequestView";
+import { useGenericState } from "hooks/useGenericState";
+import PATHS from "config/constants/sub/paths";
+import { RQAPI } from "features/apiClient/types";
+
+type RequestViewState =
+  | {
+      entryDetails: RQAPI.ApiRecord;
+      isCreateMode: false;
+    }
+  | {
+      isCreateMode: true;
+    };
+
+//TODO: losing focus in the apiClientViewer on first focus
+export const DraftRequestContainer: React.FC = () => {
+  const [requestViewState, setRequestViewState] = useState<RequestViewState>({
+    isCreateMode: true,
+  });
+
+  const { setTitle, setUrl } = useGenericState();
+
+  const onSaveCallback = useCallback(
+    (apiEntryDetails: RQAPI.ApiRecord) => {
+      setRequestViewState({
+        isCreateMode: false,
+        entryDetails: apiEntryDetails,
+      });
+      setTitle(apiEntryDetails.name);
+      setUrl(`${PATHS.API_CLIENT.ABSOLUTE}/request/${apiEntryDetails.id}`);
+    },
+    [setTitle, setUrl]
+  );
+
+  if (requestViewState.isCreateMode === true) {
+    return <DraftRequestView onSaveCallback={onSaveCallback} />;
+  } else {
+    return <RequestView apiEntryDetails={requestViewState.entryDetails} />;
+  }
+};
