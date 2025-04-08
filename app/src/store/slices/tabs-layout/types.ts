ab6270ce038dcfaadfe330b88537d53227f56973
rohanmathur91 <mathurrohan04@gmail.com>
2025-04-08 21:28:59 +0530

    remove legacy tabs lsyout

--- app/src/store/slices/tabs-layout/types.ts
+++
@@ -1,9 +0,0 @@
-import { TabsLayout } from "layouts/TabsLayout";
-
-export type TabsLayoutState = Record<
-  string,
-  {
-    tabs: TabsLayout.Tab[];
-    activeTab: TabsLayout.Tab | null;
-  }
->;
