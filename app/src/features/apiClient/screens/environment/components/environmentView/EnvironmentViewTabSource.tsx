1f5832fe9807515fe81c9f5863f87b57282c82a0
rohanmathur91 <mathurrohan04@gmail.com>
2025-04-08 19:59:42 +0530

    fix: rename tab sources metadata.isNewTab

--- app/src/features/apiClient/screens/environment/components/environmentView/EnvironmentViewTabSource.tsx
+++ app/src/features/apiClient/screens/environment/components/environmentView/EnvironmentViewTabSource.tsx
@@ -4,7 +4,9 @@ import PATHS from "config/constants/sub/paths";
 import { MatchedTabSource, TabSourceMetadata } from "componentsV2/Tabs/types";
 import { MdHorizontalSplit } from "@react-icons/all-files/md/MdHorizontalSplit";
 
-interface EnvironmentViewTabSourceMetadata extends TabSourceMetadata {}
+interface EnvironmentViewTabSourceMetadata extends TabSourceMetadata {
+  focusBreadcrumb?: boolean;
+}
 
 export class EnvironmentViewTabSource extends BaseTabSource {
   constructor(metadata: EnvironmentViewTabSourceMetadata) {
@@ -14,7 +16,7 @@ export class EnvironmentViewTabSource extends BaseTabSource {
       id: metadata.id,
       name: "environments", // FIXME: Its legacy, should be "environment"
       title: metadata.title,
-      isNewTab: metadata.isNewTab,
+      isNewTab: metadata.focusBreadcrumb,
     };
     this.urlPath = `${PATHS.API_CLIENT.ABSOLUTE}/${this.metadata.name}/${encodeURI(this.metadata.id)}`;
     this.icon = <MdHorizontalSplit />;
