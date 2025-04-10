93c890aa094475eca66fed19e66bdf7c34cb9f47
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-04-10 20:29:54 +0530

    Revert "[ENGG-3356]: local sync tabs regression fixes (#2895)"
    This reverts commit 1fac55390ba2d4d0c172e3435e2f07caab661d0a.

--- app/src/features/apiClient/screens/apiClient/components/clientView/components/Collection/CollectionView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/clientView/components/Collection/CollectionView.tsx
@@ -10,8 +10,6 @@ import CollectionAuthorizationView from "./components/CollectionAuthorizationVie
 import { toast } from "utils/Toast";
 import { useGenericState } from "hooks/useGenericState";
 import "./collectionView.scss";
-import { useTabServiceWithSelector } from "componentsV2/Tabs/store/tabServiceStore";
-import { CollectionViewTabSource } from "./collectionViewTabSource";
 
 const TAB_KEYS = {
   OVERVIEW: "overview",
@@ -24,15 +22,8 @@ interface CollectionViewProps {
 }
 
 export const CollectionView: React.FC<CollectionViewProps> = ({ collectionId }) => {
-  const {
-    apiClientRecords,
-    onSaveRecord,
-    isLoadingApiClientRecords,
-    apiClientRecordsRepository,
-    forceRefreshApiClientRecords,
-  } = useApiClientContext();
-
-  const closeTab = useTabServiceWithSelector((state) => state.closeTab);
+  const { apiClientRecords, onSaveRecord, isLoadingApiClientRecords, apiClientRecordsRepository } =
+    useApiClientContext();
 
   const { setTitle, getIsNew } = useGenericState();
   const isNewCollection = getIsNew();
@@ -110,19 +101,10 @@ export const CollectionView: React.FC<CollectionViewProps> = ({ collectionId }) 
         }
 
         onSaveRecord(result.data, "open");
-        const wasForceRefreshed = await forceRefreshApiClientRecords();
-        if (wasForceRefreshed) {
-          closeTab(
-            new CollectionViewTabSource({
-              id: record.id,
-              title: "",
-            })
-          );
-        }
         setTitle(result.data.name);
       });
     },
-    [collection, setTitle, apiClientRecordsRepository, onSaveRecord, closeTab, forceRefreshApiClientRecords]
+    [collection, setTitle, apiClientRecordsRepository, onSaveRecord]
   );
 
   if (isLoadingApiClientRecords) {
