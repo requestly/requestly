9410e011c5bb8d5fb75a3663d43bfc72b85d7976
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-04-16 11:21:58 +0530

    [ENGG-3361]: App header UI improvements (#2893)

--- app/src/components/sections/Navbars/NavbarRightContent/DesktopAppProxyInfo/index.jsx
+++ app/src/components/sections/Navbars/NavbarRightContent/DesktopAppProxyInfo/index.jsx
@@ -1,18 +1,14 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAppMode, getDesktopSpecificDetails, getIsConnectedAppsTourCompleted } from "store/selectors";
+import { getAppMode, getDesktopSpecificDetails } from "store/selectors";
 import { getAllLogs } from "store/features/desktop-traffic-table/selectors";
-//@ts-ignore
-import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
-import { TOUR_TYPES } from "components/misc/ProductWalkthrough/constants";
-import { Badge } from "antd";
+import { ProxyServerStatusIcon } from "componentsV2/ProxyServerStatusIcon/ProxyServerStatusIcon";
 import { RQButton } from "lib/design-system/components";
-import { ProductWalkthrough } from "components/misc/ProductWalkthrough";
-import "./desktopAppProxyInfo.scss";
 import { globalActions } from "store/slices/global/slice";
-import { trackConnectAppsClicked } from "modules/analytics/events/desktopApp/apps";
 import { getConnectedAppsCount } from "utils/Misc";
-import FEATURES from "config/constants/sub/features";
+import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
+import { trackConnectAppsClicked } from "modules/analytics/events/desktopApp/apps";
+import "./desktopAppProxyInfo.scss";
 
 const DesktopAppProxyInfo = () => {
   // Global State
@@ -20,17 +16,13 @@ const DesktopAppProxyInfo = () => {
   const appMode = useSelector(getAppMode);
   const desktopSpecificDetails = useSelector(getDesktopSpecificDetails);
   const networkLogs = useSelector(getAllLogs);
-  const isConnectedAppsTourCompleted = useSelector(getIsConnectedAppsTourCompleted);
   const { isBackgroundProcessActive, isProxyServerRunning, proxyPort, proxyIp, appsList } = desktopSpecificDetails;
-
   const [numberOfConnectedApps, setNumberOfConnectedApps] = useState(0);
-  const [startWalkthrough, setStartWalkthrough] = useState(false);
 
   const hasInterceptionStarted = useRef(false);
 
   useEffect(() => {
     if (!hasInterceptionStarted.current && networkLogs.length > 0) {
-      setStartWalkthrough(true);
       hasInterceptionStarted.current = true;
     }
   }, [networkLogs.length]);
@@ -38,13 +30,6 @@ const DesktopAppProxyInfo = () => {
   useEffect(() => {
     setNumberOfConnectedApps(getConnectedAppsCount(Object.values(appsList)));
   }, [appsList]);
-
-  const renderProxyBadgeStatus = () => {
-    if (!isBackgroundProcessActive || !isProxyServerRunning) {
-      return <Badge status="warning" />;
-    }
-    return <Badge status="success" />;
-  };
 
   const handleConnectAppsButtonClick = () => {
     dispatch(
@@ -57,43 +42,32 @@ const DesktopAppProxyInfo = () => {
     trackConnectAppsClicked("app_header");
   };
 
-  const renderProxyStatusText = () => {
-    if (!isBackgroundProcessActive)
-      return <span className="proxy-status-text">Waiting for proxy server to start...</span>;
-
-    if (!isProxyServerRunning) return <span className="proxy-status-text">Proxy server is not running</span>;
-
-    return (
-      <>
-        <ProductWalkthrough
-          tourFor={FEATURES.CONNECTED_APPS}
-          startWalkthrough={startWalkthrough && !isConnectedAppsTourCompleted}
-          onTourComplete={() => dispatch(globalActions.updateProductTourCompleted({ tour: TOUR_TYPES.CONNECTED_APPS }))}
-        />
-        <span className="proxy-status-text">Proxy server is listening at{` ${proxyIp}:${proxyPort}`}</span>
-        <RQButton
-          data-tour-id="connected-apps-header-cta"
-          type="default"
-          size="small"
-          className="connected-apps-btn"
-          onClick={handleConnectAppsButtonClick}
-        >
-          Connect apps
-          {numberOfConnectedApps > 0 ? <div className="rq-count-badge">{numberOfConnectedApps}</div> : null}
-        </RQButton>
-      </>
-    );
-  };
-
   if (appMode !== GLOBAL_CONSTANTS.APP_MODES.DESKTOP) return null;
 
   return (
-    <>
-      <div className="desktop-app-proxy-info">
-        <>{renderProxyBadgeStatus()}</>
-        <>{renderProxyStatusText()}</>
-      </div>
-    </>
+    <div className="desktop-app-proxy-info-container">
+      <ProxyServerStatusIcon />
+      <span className="desktop-app-proxy-info">
+        {isBackgroundProcessActive
+          ? isProxyServerRunning
+            ? `${proxyIp}:${proxyPort}`
+            : "No Proxy"
+          : "Waiting for proxy server to start"}
+      </span>
+      <RQButton
+        size="small"
+        className={`desktop-app-proxy-connect-btn ${numberOfConnectedApps > 0 ? "connected" : ""}`}
+        onClick={handleConnectAppsButtonClick}
+      >
+        {numberOfConnectedApps > 0 ? (
+          <div className="desktop-app-proxy-connect-btn-content">
+            Connected <div className="connected-apps-count-badge">{numberOfConnectedApps}</div>
+          </div>
+        ) : (
+          "Connect"
+        )}
+      </RQButton>
+    </div>
   );
 };
 
