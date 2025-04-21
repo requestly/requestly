0272e1f6187bdc00c584da40b792871f361d6497
rohanmathur91 <mathurrohan04@gmail.com>
2025-04-21 11:24:16 +0530

    cleanup

--- app/src/layouts/MinimalLayout/index.tsx
+++ app/src/layouts/MinimalLayout/index.tsx
@@ -4,7 +4,6 @@ import HeaderUser from "layouts/DashboardLayout/MenuHeader/HeaderUser";
 import { redirectToRoot } from "utils/RedirectionUtils";
 import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "components/sections/Footer";
-import { useFeatureIsOn } from "@growthbook/growthbook-react";
 import "./index.scss";
 
 interface MinimalLayoutProps {
@@ -13,30 +12,18 @@ interface MinimalLayoutProps {
 
 const MinimalLayout: React.FC<MinimalLayoutProps> = ({ children = null }) => {
   const navigate = useNavigate();
-  const isBrowserstackIntegrationEnabled = useFeatureIsOn("browserstack_integration");
 
   return (
     <div className="minimal-layout">
       <Layout.Header className="minimal-layout-navbar">
-        {isBrowserstackIntegrationEnabled ? (
-          <img
-            width={94}
-            height={32}
-            className="logo"
-            src="/assets/media/common/RQ-BStack Logo.svg"
-            alt="Requestly by Browserstack"
-            onClick={() => redirectToRoot(navigate)}
-          />
-        ) : (
-          <img
-            width={94}
-            height={32}
-            className="logo"
-            src={"/assets/media/common/rq_logo_full.svg"}
-            alt="Requestly"
-            onClick={() => redirectToRoot(navigate)}
-          />
-        )}
+        <img
+          width={94}
+          height={32}
+          className="logo"
+          src={"/assets/media/common/rq_logo_full.svg"}
+          alt="Requestly"
+          onClick={() => redirectToRoot(navigate)}
+        />
 
         <HeaderUser />
       </Layout.Header>
