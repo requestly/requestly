98f2131c30642a1efa76dc16c3c9699a664fab2e
Nafees Nehar <nafees87n@gmail.com>
2025-04-23 14:45:12 +0530

    [ENGG-3431] added listener to check GRR in block config (#20)
    * added listener to check grr in block config
    
    * fix: listener only updates blockConfig
    
    * remove log
    
    * fix: listen only for the change
    
    * remove slice action
    
    * remove dispatch

--- app/src/features/grr/hooks/useIsGrrEnabled.ts
+++ app/src/features/grr/hooks/useIsGrrEnabled.ts
@@ -1,32 +1,34 @@
-import { getUser } from "backend/user/getUser";
+import firebaseApp from "firebase";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getUserAuthDetails } from "store/slices/global/user/selectors";
 
 export const useIsGrrEnabled = () => {
   const user = useSelector(getUserAuthDetails);
-  const [isGrrEnabled, setIsGrrEnabled] = useState(false);
-
   const isLoggedIn = user?.loggedIn;
   const uid = user?.details?.profile?.uid;
+  const [isGrrEnabled, setIsGrrEnabled] = useState(false);
 
   useEffect(() => {
-    if (!isLoggedIn) {
+    if (!isLoggedIn || !uid) {
       setIsGrrEnabled(false);
       return;
     }
 
-    if (!uid) {
-      setIsGrrEnabled(false);
-      return;
-    }
-
-    getUser(uid).then((profile) => {
-      if (profile?.["block-config"]?.grr?.isBlocked) {
-        setIsGrrEnabled(true);
+    const db = getFirestore(firebaseApp);
+    const unsubscribeListener = onSnapshot(doc(db, "users", uid), (doc) => {
+      if (doc.exists()) {
+        const userDetails = doc.data();
+        const isGrrBlocked = !!userDetails?.["block-config"]?.grr?.isBlocked;
+        setIsGrrEnabled(isGrrBlocked);
       }
     });
-  }, [uid, isLoggedIn]);
+
+    return () => {
+      unsubscribeListener?.();
+    };
+  }, [isLoggedIn, uid]);
 
   return { isGrrEnabled };
 };
