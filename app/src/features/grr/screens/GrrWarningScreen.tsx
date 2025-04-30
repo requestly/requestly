c1e6929d166d974ecfdcd8c3c385596ac8cb10b7
Aarush Srivastava <aarush@requestly.io>
2025-04-30 10:55:04 +0530

    feat: seed grr blocked screen event (#56)

--- app/src/features/grr/screens/GrrWarningScreen.tsx
+++ app/src/features/grr/screens/GrrWarningScreen.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GrrWarningMessage } from "../components/GrrWarningMessage/GrrWarningMessage";
 import "./grrWarningScreen.scss";
+import { trackGrrBlockedScreenViewed } from "../analytics";
 
 export const GrrWarningScreen: React.FC = () => {
+  useEffect(() => {
+    trackGrrBlockedScreenViewed();
+  }, []);
+
   return (
     <div className="grr-warning-screen">
       <GrrWarningMessage />
