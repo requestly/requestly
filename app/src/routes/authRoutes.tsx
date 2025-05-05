df67e0b73cdbaa8d05b75160d3782af5eafd6190
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-05-05 13:19:47 +0530

    fix: existing forgot and reset password routes redirecting to 404 (#64)

--- app/src/routes/authRoutes.tsx
+++ app/src/routes/authRoutes.tsx
@@ -1,12 +1,14 @@
 import { Navigate, RouteObject } from "react-router-dom";
 import PATHS from "config/constants/sub/paths";
-// import AUTH from "config/constants/sub/auth";
+import AUTH from "config/constants/sub/auth";
 import DesktopSignIn from "components/authentication/DesktopSignIn";
 import EmailAction from "components/misc/EmailAction";
 import VerifyEmail from "components/misc/VerifyEmail";
 import SignInViaEmailLink from "components/misc/SignInViaEmailLink";
 import LoginHandler from "components/authentication/LoginHandler";
 import { AuthPage } from "features/onboarding/screens/auth/components/AuthPage/AuthPage";
+import RQAuthPage from "components/authentication/AuthPage/AuthPage";
+
 export const authRoutes: RouteObject[] = [
   {
     path: PATHS.AUTH.LOGIN.RELATIVE,
@@ -24,18 +26,18 @@ export const authRoutes: RouteObject[] = [
     path: PATHS.AUTH.SIGN_UP.RELATIVE,
     element: <Navigate to={PATHS.AUTH.START.RELATIVE} />,
   },
-  // {
-  //   path: PATHS.AUTH.FORGOT_PASSWORD.RELATIVE,
-  //   element: <AuthPage authMode={AUTH.ACTION_LABELS.REQUEST_RESET_PASSWORD} />,
-  // },
+  {
+    path: PATHS.AUTH.FORGOT_PASSWORD.RELATIVE,
+    element: <RQAuthPage authMode={AUTH.ACTION_LABELS.REQUEST_RESET_PASSWORD} />,
+  },
   {
     path: PATHS.AUTH.EMAIL_ACTION.RELATIVE,
     element: <EmailAction />,
   },
-  // {
-  //   path: PATHS.AUTH.RESET_PASSWORD.RELATIVE,
-  //   element: <AuthPage authMode={AUTH.ACTION_LABELS.DO_RESET_PASSWORD} />,
-  // },
+  {
+    path: PATHS.AUTH.RESET_PASSWORD.RELATIVE,
+    element: <RQAuthPage authMode={AUTH.ACTION_LABELS.DO_RESET_PASSWORD} />,
+  },
   {
     path: PATHS.AUTH.VERIFY_EMAIL.RELATIVE,
     element: <VerifyEmail />,
