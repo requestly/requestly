a55d9e6730d8d53c7192eaadffed8d811c537b60
Aarushsr12 <aarushsrivastava1212@gmail.com>
2025-05-05 07:40:03 +0530

    fix: method color change in header

--- app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/FixedRequestLogPane.js
+++ app/src/components/mode-specific/desktop/InterceptTraffic/WebTraffic/TrafficTableV2/FixedRequestLogPane.js
@@ -8,6 +8,8 @@ import RequestSummary from "./RequestSummary";
 import { CONSTANTS as GLOBAL_CONSTANTS } from "@requestly/requestly-core";
 import Editor, { EditorLanguage } from "componentsV2/CodeEditor";
 import "./FixedRequestLogPane.css";
+import { REQUEST_METHOD_COLORS, REQUEST_METHOD_BACKGROUND_COLORS } from "../../../../../../constants";
+import { RequestMethod } from "features/apiClient/types";
 
 const { Text } = Typography;
 
@@ -15,7 +17,18 @@ const Header = (props) => {
   return (
     <Row className="request-log-pane-header" align="middle" wrap={false}>
       <Space>
-        <Badge count={props.method} style={{ backgroundColor: "grey" }} />
+        <Typography.Text
+          strong
+          className="request-method"
+          style={{
+            color: REQUEST_METHOD_COLORS[props.method],
+            backgroundColor: REQUEST_METHOD_BACKGROUND_COLORS[props.method],
+          }}
+        >
+          {[RequestMethod.OPTIONS, RequestMethod.DELETE].includes(props.method)
+            ? props.method.slice(0, 3)
+            : props.method}
+        </Typography.Text>
         <Badge overflowCount={699} count={props.statusCode} style={{ backgroundColor: "#87d068" }} />
         <Text ellipsis={{ tooltip: props.url }} className="request-log-pane-url">
           {props.url}
