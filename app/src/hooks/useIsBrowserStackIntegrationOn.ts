b923edb76d8844f1d47cd93e8b84c450348941a8
Parth Bhardwaj <parthb.git@gmail.com>
2025-05-09 16:40:38 +0530

    replace api client toasts with antd notifications

--- app/src/hooks/useIsBrowserStackIntegrationOn.ts
+++ app/src/hooks/useIsBrowserStackIntegrationOn.ts
@@ -3,5 +3,6 @@ import { useFeatureIsOn } from "@growthbook/growthbook-react";
 export const useIsBrowserStackIntegrationOn = () => {
   const isBrowserStackIntegrationEnabled = useFeatureIsOn("browserstack_integration");
   // TODO: Add localStorage.get("forceBsIntegration") check when bookmarklet is implemented
-  return isBrowserStackIntegrationEnabled;
+  // return isBrowserStackIntegrationEnabled;
+  return false;
 };
