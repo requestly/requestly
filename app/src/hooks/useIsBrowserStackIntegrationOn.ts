531d176238445c3f2b0b8769b6a6a922249ec674
Parth Bhardwaj <parthb.git@gmail.com>
2025-05-09 17:41:29 +0530

    revert flag change

--- app/src/hooks/useIsBrowserStackIntegrationOn.ts
+++ app/src/hooks/useIsBrowserStackIntegrationOn.ts
@@ -3,6 +3,5 @@ import { useFeatureIsOn } from "@growthbook/growthbook-react";
 export const useIsBrowserStackIntegrationOn = () => {
   const isBrowserStackIntegrationEnabled = useFeatureIsOn("browserstack_integration");
   // TODO: Add localStorage.get("forceBsIntegration") check when bookmarklet is implemented
-  // return isBrowserStackIntegrationEnabled;
-  return false;
+  return isBrowserStackIntegrationEnabled;
 };
