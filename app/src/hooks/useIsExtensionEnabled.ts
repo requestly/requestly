bac2243cdec29c16d0035b40a293ab959a40e8ff
nafees87n <nafees87n@gmail.com>
2025-05-19 20:46:28 +0530

    added feature compatibility check

--- app/src/hooks/useIsExtensionEnabled.ts
+++ app/src/hooks/useIsExtensionEnabled.ts
@@ -8,6 +8,8 @@ import APP_CONSTANTS from "config/constants";
 import { globalActions } from "store/slices/global/slice";
 import { isExtensionEnabled, isExtensionManifestVersion3 } from "actions/ExtensionActions";
 import PageScriptMessageHandler from "config/PageScriptMessageHandler";
+import { isFeatureCompatible } from "utils/CompatibilityUtils";
+import FEATURES from "config/constants/sub/features";
 
 export const useIsExtensionEnabled = () => {
   const dispatch = useDispatch();
@@ -17,18 +19,30 @@ export const useIsExtensionEnabled = () => {
     if (appMode === GLOBAL_CONSTANTS.APP_MODES.EXTENSION) {
       Logger.log(`Reading storage in App`);
       if (isExtensionManifestVersion3()) {
-        isExtensionEnabled().then((isEnabled) => {
-          dispatch(globalActions.updateIsExtensionEnabled(isEnabled));
-        });
+        if (isFeatureCompatible(FEATURES.EXTENSION_STATUS_NOTIFICATION)) {
+          isExtensionEnabled().then((isEnabled) => {
+            dispatch(globalActions.updateIsExtensionEnabled(isEnabled));
+          });
 
-        PageScriptMessageHandler.addMessageListener("notifyExtensionStatusUpdated", (message: any) => {
-          const { isExtensionEnabled } = message;
-          if (isExtensionEnabled !== undefined) {
-            dispatch(globalActions.updateIsExtensionEnabled(isExtensionEnabled));
-          } else {
-            dispatch(globalActions.updateIsExtensionEnabled(true));
-          }
-        });
+          PageScriptMessageHandler.addMessageListener("notifyExtensionStatusUpdated", (message: any) => {
+            const { isExtensionEnabled } = message;
+            if (isExtensionEnabled !== undefined) {
+              dispatch(globalActions.updateIsExtensionEnabled(isExtensionEnabled));
+            } else {
+              dispatch(globalActions.updateIsExtensionEnabled(true));
+            }
+          });
+        } else {
+          StorageService(appMode)
+            .getRecord("rq_var_isExtensionEnabled")
+            .then((value) => {
+              if (value !== undefined) {
+                dispatch(globalActions.updateIsExtensionEnabled(value));
+              } else {
+                dispatch(globalActions.updateIsExtensionEnabled(true));
+              }
+            });
+        }
       } else {
         //TODO @nafees87n: cleanup reading settings from storage
         StorageService(appMode)
