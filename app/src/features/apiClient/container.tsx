d162f5bbcf08ab901b97dbc7016ae52a37819038
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-05-22 14:04:25 +0530

    ENGG-3660: fix api client modal crashes when user is logged out (#3023)

--- app/src/features/apiClient/container.tsx
+++ app/src/features/apiClient/container.tsx
@@ -1,71 +1,21 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { ApiClientProvider } from "./contexts";
 import APIClientSidebar from "./screens/apiClient/components/sidebar/APIClientSidebar";
 import { getUserAuthDetails } from "store/slices/global/user/selectors";
-import { useDispatch, useSelector } from "react-redux";
-import { NudgePrompt } from "componentsV2/Nudge/NudgePrompt";
-import { Typography } from "antd";
-import { RQButton } from "lib/design-system-v2/components";
-import { globalActions } from "store/slices/global/slice";
-import { redirectToUrl } from "utils/RedirectionUtils";
-import LINKS from "config/constants/sub/links";
+import { useSelector } from "react-redux";
 import { TabsContainer } from "componentsV2/Tabs/components/TabsContainer";
 import { TabServiceProvider } from "componentsV2/Tabs/store/TabServiceContextProvider";
-import { trackLoginButtonClicked } from "modules/analytics/events/common/auth/login";
-import { SOURCE } from "modules/analytics/events/common/constants";
+import ApiClientLoggedOutView from "./components/common/LoggedOutView/LoggedOutView";
 import "./container.scss";
 
 const ApiClientFeatureContainer: React.FC = () => {
   const user = useSelector(getUserAuthDetails);
-  const dispatch = useDispatch();
-
-  const handleSignUp = useCallback(() => {
-    trackLoginButtonClicked(SOURCE.API_CLIENT_EMPTY_STATE);
-    dispatch(
-      globalActions.toggleActiveModal({
-        modalName: "authModal",
-        newValue: true,
-        newProps: {
-          eventSource: SOURCE.API_CLIENT_EMPTY_STATE,
-        },
-      })
-    );
-  }, [dispatch]);
-
-  const handleReadAnnouncementClick = useCallback(() => {
-    redirectToUrl(LINKS.API_CLIENT_LOCAL_FIRST_ANNOUNCEMENT, true);
-  }, []);
-
-  const loggedOutView = (
-    <div className="api-client-logged-out-view">
-      <NudgePrompt
-        icon={"/assets/media/apiClient/mandatory-login.svg"}
-        buttons={[
-          <RQButton type="secondary" onClick={handleReadAnnouncementClick}>
-            Read announcement
-          </RQButton>,
-          <RQButton type="primary" onClick={handleSignUp}>
-            Sign in to get started
-          </RQButton>,
-        ]}
-      >
-        <div className="api-client-logged-out-view_content">
-          <Typography.Title level={5}>Please sign in first to use API client</Typography.Title>
-          <Typography.Text className="api-client-logged-out-view_description">
-            Requestly API client requires an account for <span className="success">secure</span> workspace
-            collaboration. A <span className="success">local-first</span> version is coming soon, and creating an
-            account will no longer be required!
-          </Typography.Text>
-        </div>
-      </NudgePrompt>
-    </div>
-  );
 
   if (!user.loggedIn) {
     return (
       <div className="api-client-container">
         <APIClientSidebar />
-        {loggedOutView}
+        <ApiClientLoggedOutView />
       </div>
     );
   }
@@ -75,7 +25,7 @@ const ApiClientFeatureContainer: React.FC = () => {
       <ApiClientProvider>
         <div className="api-client-container">
           <APIClientSidebar />
-          {user.loggedIn ? <TabsContainer /> : <>{loggedOutView}</>}
+          {user.loggedIn ? <TabsContainer /> : <ApiClientLoggedOutView />}
         </div>
       </ApiClientProvider>
     </TabServiceProvider>
