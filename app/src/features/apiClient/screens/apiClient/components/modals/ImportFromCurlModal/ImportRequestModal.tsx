0ab6c4ac7e896f7ab247f29e5059455f7728a368
Parth Bhardwaj <parthb.git@gmail.com>
2025-05-22 13:18:20 +0530

    ENGG-3656: Fix error messaging in curl importer modal

--- app/src/features/apiClient/screens/apiClient/components/modals/ImportFromCurlModal/ImportRequestModal.tsx
+++ app/src/features/apiClient/screens/apiClient/components/modals/ImportFromCurlModal/ImportRequestModal.tsx
@@ -1,11 +1,12 @@
 import { Input, Modal } from "antd";
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import { parseCurlRequest } from "../../utils";
-import { RQAPI } from "../../../../types";
+import { parseCurlRequest } from "../../../utils";
+import { RQAPI } from "../../../../../types";
 import { toast } from "utils/Toast";
 import { trackCurlImportFailed, trackCurlImported } from "modules/analytics/events/features/apiClient";
 import { trackRQDesktopLastActivity, trackRQLastActivity } from "utils/AnalyticsUtils";
 import { API_CLIENT } from "modules/analytics/events/features/constants";
+import "./importFromCurlModal.scss";
 
 interface Props {
   isOpen: boolean;
@@ -14,8 +15,9 @@ interface Props {
   onClose: () => void;
 }
 
-export const ImportRequestModal: React.FC<Props> = ({ isOpen, handleImportRequest, onClose, isRequestLoading }) => {
+export const ImportFromCurlModal: React.FC<Props> = ({ isOpen, handleImportRequest, onClose, isRequestLoading }) => {
   const [curlCommand, setCurlCommand] = useState("");
+  const [error, setError] = useState(null);
   const inputRef = useRef<HTMLInputElement>();
 
   useEffect(() => {
@@ -28,24 +30,35 @@ export const ImportRequestModal: React.FC<Props> = ({ isOpen, handleImportReques
 
   const onImportClicked = useCallback(() => {
     if (!curlCommand) {
-      toast.error("Please enter the cURL command");
+      setError("Please enter a cURL command");
       inputRef.current?.focus();
       return;
     }
 
-    const requestFromCurl: RQAPI.Request = parseCurlRequest(curlCommand);
+    try {
+      const requestFromCurl: RQAPI.Request = parseCurlRequest(curlCommand);
 
-    if (!requestFromCurl || !requestFromCurl.url) {
-      toast.error("Could not process the cURL command");
+      if (!requestFromCurl || !requestFromCurl.url) {
+        toast.error("Could not process the cURL command");
+        trackCurlImportFailed();
+        return;
+      }
+
+      handleImportRequest(requestFromCurl);
+      trackCurlImported();
+      trackRQLastActivity(API_CLIENT.CURL_IMPORTED);
+      trackRQDesktopLastActivity(API_CLIENT.CURL_IMPORTED);
+    } catch (error) {
       trackCurlImportFailed();
-      return;
+      setError(error?.message || "Could not process the cURL command");
     }
-
-    handleImportRequest(requestFromCurl);
-    trackCurlImported();
-    trackRQLastActivity(API_CLIENT.CURL_IMPORTED);
-    trackRQDesktopLastActivity(API_CLIENT.CURL_IMPORTED);
   }, [curlCommand, handleImportRequest]);
+
+  const handleClose = () => {
+    setError(null);
+    setCurlCommand("");
+    onClose();
+  };
 
   return (
     <Modal
@@ -57,9 +70,14 @@ export const ImportRequestModal: React.FC<Props> = ({ isOpen, handleImportReques
       onOk={onImportClicked}
       maskClosable={false}
       confirmLoading={isRequestLoading}
-      onCancel={onClose}
+      onCancel={handleClose}
       width="70%"
     >
+      {error ? (
+        <pre className="curl-import-errror">
+          <code>{error}</code>
+        </pre>
+      ) : null}
       <Input.TextArea
         ref={inputRef}
         className="curl-command-input"
