84b51363dc97a8338ab939ae0108096c1624cca3
rohanmathur91 <mathurrohan04@gmail.com>
2025-05-26 18:14:19 +0530

    cleanup

--- app/src/features/pricing/components/PremiumFeature/index.tsx
+++ app/src/features/pricing/components/PremiumFeature/index.tsx
@@ -153,7 +153,6 @@ export const PremiumFeature: React.FC<PremiumFeatureProps> = ({
           onOpenChange={(open) => {
             if (open) trackUpgradePopoverViewed("default", source);
           }}
-          // cancelButtonProps={{ style: { display: isIncentivizationEnabled ? "inline-flex" : "none" } }}
         >
           {React.Children.map(children, (child) => {
             return React.cloneElement(child as React.ReactElement, {
