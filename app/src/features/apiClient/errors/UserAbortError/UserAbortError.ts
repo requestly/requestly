294c0d6ef8bb29cb72457118f03a3c67af9eb8c9
Parth Bhardwaj <parthb.git@gmail.com>
2025-05-28 17:12:49 +0530

    update message UI

--- app/src/features/apiClient/errors/UserAbortError/UserAbortError.ts
+++ app/src/features/apiClient/errors/UserAbortError/UserAbortError.ts
@@ -2,7 +2,7 @@ import { NativeError } from "errors/NativeError";
 
 export class UserAbortError extends NativeError {
   constructor() {
-    super("Request execution aborted");
+    super("Request cancelled");
     this.name = "UserAbortError";
   }
 }
--- app/src/features/apiClient/screens/apiClient/components/clientView/components/errors/UserAbortErrorView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/clientView/components/errors/UserAbortErrorView.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RQAPI } from "features/apiClient/types";
 import { RQButton } from "lib/design-system-v2/components";
+import { MdCheckCircleOutline } from "@react-icons/all-files/md/MdCheckCircleOutline";
 import "./ErrorPlaceholder/errorPlaceholder.scss";
 
 interface UserAbortErrorViewProps {
@@ -14,10 +15,13 @@ export const UserAbortErrorView: React.FC<UserAbortErrorViewProps> = ({ error, h
       <div className="api-client-error-placeholder-content">
         <img src="/assets/media/apiClient/abort-error.svg" alt="Error card" width={80} height={80} />
         <div className="error-container">
-          <span className="error-message">{error.message}</span>
+          <span className="error-message">
+            <MdCheckCircleOutline style={{ color: "var(--requestly-color-success)" }} />
+            {error.message}
+          </span>
         </div>
         <RQButton type="primary" onClick={handleDismiss}>
-          Got it
+          Dismiss
         </RQButton>
       </div>
     </div>
