9e114974f062d58b33b37d6ddde0e880efc7e3d3
Aarush Srivastava <aarush@requestly.io>
2025-06-10 20:36:15 +0530

    [ENGG-2854] feat: restrict invalid character in keys (#3105)
    * feat: restrict invalid char in keys
    
    * fix: added flag to control check
    
    * fix: Conditional used
    
    * fix: whitelist the characters
    
    * fix: style correction
    
    * fix: outline icon
    
    * fix: cleanup
    
    * fix: bug: input focus on div
    
    * fix: restrict on save
    
    * fix: restrict on send & throw error
    
    * fix: save on record name update
    
    * fix: cleanup
    
    * fix: move constant
    
    * fix: formState issue (addTo field)

--- app/src/features/apiClient/helpers/apiClientExecutor/apiClientExecutor.ts
+++ app/src/features/apiClient/helpers/apiClientExecutor/apiClientExecutor.ts
@@ -19,6 +19,7 @@ import { isMethodSupported, isOnline, isUrlProtocolValid, isUrlValid } from "./a
 import { isEmpty } from "lodash";
 import { DEFAULT_SCRIPT_VALUES } from "features/apiClient/constants";
 import { UserAbortError } from "features/apiClient/errors/UserAbortError/UserAbortError";
+import { INVALID_KEY_CHARACTERS } from "features/apiClient/constants";
 
 type InternalFunctions = {
   getEnvironmentVariables(): EnvironmentVariables;
@@ -195,6 +196,13 @@ export class ApiClientExecutor {
 
     try {
       this.preValidateRequest();
+      const invalidHeader = this.entryDetails?.request?.headers?.find((header) => {
+        return INVALID_KEY_CHARACTERS.test(header.key);
+      });
+
+      if (invalidHeader) {
+        throw new Error(`Invalid header key: "${invalidHeader.key}". Header keys must not contain special characters.`);
+      }
     } catch (err) {
       const error = this.buildExecutionErrorObject(err, "request", RQAPI.ApiClientErrorType.PRE_VALIDATION);
       return {
