2503910402147992ad318f266af4ff97b31e13cd
Aarushsr12 <aarushsrivastava1212@gmail.com>
2025-06-15 20:16:39 +0530

    fix: table styles

--- app/src/features/apiClient/screens/apiClient/components/clientView/components/request/components/KeyValueTable/AutoGenTable.tsx
+++ app/src/features/apiClient/screens/apiClient/components/clientView/components/request/components/KeyValueTable/AutoGenTable.tsx
@@ -3,6 +3,7 @@ import type { TableProps } from "antd";
 import { ContentListTable } from "componentsV2/ContentList";
 import { Collapse } from "antd";
 import "./AutoGenTable.scss";
+import InfoIcon from "components/misc/InfoIcon";
 
 type ColumnTypes = Exclude<TableProps<{ key: string; value: string }>["columns"], undefined>;
 
@@ -19,7 +20,7 @@ export const AutoGenTable: React.FC<KeyValueTableProps> = ({ data, type }) => {
       {
         title: "key",
         dataIndex: "key",
-        width: "48.5%",
+        width: "50%",
         editable: false,
       },
       {
@@ -37,10 +38,25 @@ export const AutoGenTable: React.FC<KeyValueTableProps> = ({ data, type }) => {
           header={
             <span className="heading-text">
               {type === "headers" ? "Auto-Generated Headers" : "Auto-Generated Query Parameters"}
+              <InfoIcon
+                text={
+                  type === "headers"
+                    ? "These headers are automatically generated and included with each request you send."
+                    : "This query param is added based on the selected authorization method. Use the Authorization tab to remove it or change its value."
+                }
+                showArrow={false}
+                tooltipPlacement="right"
+                style={{
+                  position: "relative",
+                  top: "3px",
+                  marginLeft: "8px",
+                  width: "14px",
+                  height: "14px",
+                }}
+              />
             </span>
           }
           key="1"
-          className="custom-collapse-panel"
         >
           <ContentListTable
             id="api-key-value-table"
@@ -52,6 +68,7 @@ export const AutoGenTable: React.FC<KeyValueTableProps> = ({ data, type }) => {
             data={memoizedData}
             locale={{ emptyText: `No entries found` }}
             scroll={{ x: true }}
+            footer={null}
           />
         </Collapse.Panel>
       </Collapse>
