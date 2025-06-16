32e66551a5d6a26097dc9ab731950f0bbc1a0bde
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-16 20:22:46 +0530

    fix: key

--- app/src/features/apiClient/helpers/modules/sync/localStorage/services/types.ts
+++ app/src/features/apiClient/helpers/modules/sync/localStorage/services/types.ts
@@ -2,9 +2,6 @@ import { EnvironmentMap } from "backend/environment/types";
 import { RQAPI } from "features/apiClient/types";
 
 export type LocalStorageSyncRecords = {
-  version: number;
-  data: {
-    apis: RQAPI.Record[];
-    environments: EnvironmentMap;
-  };
+  apis: RQAPI.Record[];
+  environments: EnvironmentMap;
 };
