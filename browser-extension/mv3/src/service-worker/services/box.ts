4d279d8cac5c6b383678deb27ab1b83b5faf90a6
Rahul Ramteke <iostreamer007@gmail.com>
2025-06-16 19:15:00 +0530

    Add support for stitching requests and fetch

---
+++ browser-extension/mv3/src/service-worker/services/box.ts
@@ -0,0 +1,28 @@
+class Box {
+  private handlers: Function[] = [];
+  private registeredHandlers: Map<string, Function> = new Map();
+
+  addHandler(fn: Function) {
+    this.handlers.push(fn);
+  }
+
+  registerHandler(id: string) {
+    const handler = this.handlers.pop();
+    if (!handler) {
+      return;
+    }
+
+    this.registeredHandlers.set(id, handler)
+  }
+
+  invokeHandler(id: string) {
+    const handler = this.registeredHandlers.get(id);
+    if (!handler) {
+      return;
+    }
+
+    handler(id);
+  }
+}
+
+export const box = new Box();
