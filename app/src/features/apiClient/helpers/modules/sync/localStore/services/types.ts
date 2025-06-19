d9556a4951a6e1668bf614c4f786e67c575eef7a
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-19 16:25:52 +0530

    refactor

--- app/src/features/apiClient/helpers/modules/sync/localStore/services/types.ts
+++ app/src/features/apiClient/helpers/modules/sync/localStore/services/types.ts
@@ -1,11 +1,6 @@
 import { EnvironmentMap } from "backend/environment/types";
 import { RQAPI } from "features/apiClient/types";
 
-export type ApiClientLocalStoreMeta = {
-  version: number;
-  storageKey: string;
-};
-
 export type LocalStoreSyncRecords = {
   apis: RQAPI.Record[];
   environments: EnvironmentMap;
