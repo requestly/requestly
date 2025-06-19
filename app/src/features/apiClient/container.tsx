9deab7d67bb91a1718dfa474a17ec1c776fd7561
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-19 16:30:31 +0530

    cleanup

--- app/src/features/apiClient/container.tsx
+++ app/src/features/apiClient/container.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import { ApiClientProvider } from "./contexts";
 import APIClientSidebar from "./screens/apiClient/components/sidebar/APIClientSidebar";
-import { getUserAuthDetails } from "store/slices/global/user/selectors";
-import { useSelector } from "react-redux";
 import { TabsContainer } from "componentsV2/Tabs/components/TabsContainer";
 import { TabServiceProvider } from "componentsV2/Tabs/store/TabServiceContextProvider";
-import ApiClientLoggedOutView from "./components/common/LoggedOutView/LoggedOutView";
 import { LocalSyncRefreshHandler } from "./LocalSyncRefreshHandler";
 import "./container.scss";
 
 const ApiClientFeatureContainer: React.FC = () => {
-  const user = useSelector(getUserAuthDetails);
-
-  if (!user.loggedIn) {
-    return <ApiClientLoggedOutView />;
-  }
-
   return (
     <TabServiceProvider>
       <ApiClientProvider>
@@ -23,7 +14,7 @@ const ApiClientFeatureContainer: React.FC = () => {
           <LocalSyncRefreshHandler />
           <div className="api-client-container">
             <APIClientSidebar />
-            {user.loggedIn ? <TabsContainer /> : <ApiClientLoggedOutView />}
+            <TabsContainer />
           </div>
         </>
       </ApiClientProvider>
