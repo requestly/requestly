8c682141f9c3c5968521585bbaf89b0bb4a99120
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-23 17:43:30 +0530

    update ApiClientLocalStorage

--- app/src/features/apiClient/helpers/modules/sync/localStore/services/types.ts
+++
@@ -1,7 +0,0 @@
-import { EnvironmentMap } from "backend/environment/types";
-import { RQAPI } from "features/apiClient/types";
-
-export type LocalStoreSyncRecords = {
-  apis: RQAPI.Record[];
-  environments: EnvironmentMap;
-};
