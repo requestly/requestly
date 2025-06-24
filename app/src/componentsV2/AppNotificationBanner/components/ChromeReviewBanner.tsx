f150e530e9f18559ca55fe57c990e78885e2b82b
kanishkrawatt <rawat27kanishk@gmail.com>
2025-06-24 13:59:04 +0530

    Requested Changes

--- app/src/componentsV2/AppNotificationBanner/components/ChromeReviewBanner.tsx
+++ app/src/componentsV2/AppNotificationBanner/components/ChromeReviewBanner.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { Banner } from "../banner.types";
-import { capitalize } from "lodash";
+import { capitalize, has } from "lodash";
 import { trackAppBannerCtaClicked } from "../analytics";
 import { RQButton } from "lib/design-system/components";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import { redirectToUrl } from "utils/RedirectionUtils";
+import BaseBanner from "./BaseBanner";
+import { useRenderBannerText } from "../hooks/useRenderBannerText";
+import { useBannerAction } from "../hooks/useBannerAction";
 
 interface Props {
   banner: Banner;
@@ -14,59 +17,20 @@ interface Props {
 
 export const ChromeReviewBanner: React.FC<Props> = ({ banner, onClose }) => {
   const [hasReviewed, setHasReviewed] = useState(
-    // HELP! NEED TO DECIDE THE VARIABLE FOR NOW USING ["redirect_to_chrome_store_reviews"]
     () => localStorage.getItem("isRedirectedTOChromeStoreReview") === "true"
   );
+  const actions = useBannerAction(banner.actions || []);
+  const bannerText = useRenderBannerText(banner);
+  const text = !hasReviewed ? bannerText.split("||")[0] : bannerText.split("||")[1];
+  const actionsConfig = hasReviewed
+    ? []
+    : actions.map((action) => ({
+        ...action,
+        onClick: () => {
+          setHasReviewed(true);
+          action.onClick();
+        },
+      }));
 
-  const handleReviewClick = () => {
-    localStorage.setItem("isRedirectedTOChromeStoreReview", "true");
-    setHasReviewed(true);
-    // HELP! NEED TO DECIDE THE ACTION NAME FOR NOW USING ["redirect_to_chrome_store_reviews"]
-    trackAppBannerCtaClicked(banner?.id, "redirect_to_chrome_store_reviews");
-    redirectToUrl("https://rqst.ly/chrome-review", true);
-  };
-
-  const bannerText = hasReviewed
-    ? "Thanks! Upload a screenshot of your review via the chatbot (bottom right) to claim your free month."
-    : "Enjoying Requestly? Leave a Chrome review and get 1 month Professional Plan free!";
-
-  return (
-    <div className={`app-banner`} style={{ backgroundColor: banner?.backgroundColor || "var(--blue-blue-600)" }}>
-      {banner?.short_text && (
-        <span className="app-banner-badge" style={{ backgroundColor: banner?.badgeColor || "var(--blue-blue-600)" }}>
-          {banner.short_text}
-        </span>
-      )}
-
-      <div className="app-banner-text">
-        <ReactMarkdown rehypePlugins={[rehypeRaw]}>{bannerText}</ReactMarkdown>
-      </div>
-
-      {!hasReviewed && (
-        <div className="app-banner-action-buttons">
-          <RQButton type="primary" onClick={handleReviewClick}>
-            {capitalize("Review Now")}
-          </RQButton>
-        </div>
-      )}
-
-      {banner?.isDismissable !== false && (
-        <div className="close-button-container">
-          <RQButton
-            iconOnly
-            className="close-btn"
-            onClick={onClose}
-            icon={
-              <svg width="11.67" height="11.67" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill="#ffffff"
-                  d="M1.08736 1.08492C1.31516 0.857111 1.68451 0.857111 1.91232 1.08492L4.99984 4.17244L8.08736 1.08492C8.31517 0.857111 8.68451 0.857111 8.91232 1.08492C9.14012 1.31272 9.14012 1.68207 8.91232 1.90988L5.8248 4.9974L8.91232 8.08492C9.14012 8.31272 9.14012 8.68207 8.91232 8.90988C8.68451 9.13768 8.31517 9.13768 8.08736 8.90988L4.99984 5.82235L1.91232 8.90988C1.68451 9.13768 1.31516 9.13768 1.08736 8.90988C0.859552 8.68207 0.859552 8.31272 1.08736 8.08492L4.17488 4.9974L1.08736 1.90988C0.859552 1.68207 0.859552 1.31272 1.08736 1.08492Z"
-                />
-              </svg>
-            }
-          />
-        </div>
-      )}
-    </div>
-  );
+  return <BaseBanner banner={banner} onClose={onClose} text={text} actionsConfig={actionsConfig} />;
 };
