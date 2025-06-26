2ab263895c24d7138f14117801cf829819e47482
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-26 17:45:33 +0530

    fix: interface

--- app/src/features/apiClient/helpers/modules/sync/interfaces.ts
+++ app/src/features/apiClient/helpers/modules/sync/interfaces.ts
@@ -66,7 +66,6 @@ export interface ApiClientRecordsInterface<Meta extends Record<string, any>> {
   ): Promise<{ success: boolean; message?: string }>;
   duplicateApiEntities(entities: Partial<RQAPI.Record>[]): Promise<RQAPI.Record[]>;
   moveAPIEntities(entities: Partial<RQAPI.Record>[], newParentId: string): Promise<RQAPI.Record[]>;
-  clear?(): Promise<void>;
 }
 
 export interface ApiClientRepositoryInterface {
