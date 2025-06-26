71f263cc1e858047b42dd8bf3cbcfb4f54ebdc3c
Rohan Mathur <mathurrohan04@gmail.com>
2025-06-26 21:08:31 +0530

    [ENGG-3778]: api client local store repository layer (#3154)
    * add `LocalStorageEnvSync`
    
    * refactor
    
    * add `LocalStorageRecordsSync`
    
    * add `ApiClientLocalStorageRepository`
    
    * fix: key
    
    * fix: deleted records
    
    * use `ApiClientLocalStorageRepository`
    
    * refactor
    
    * refactor
    
    * cleanup
    
    * add `batchCreateRecords`
    
    * add `ApiClientLocalStorage`
    
    * cleanup
    
    * add `init` method for `ApiClientLocalStorage`
    
    * update ApiClientLocalStorage
    
    * chore: fix type
    
    * fix: id generation
    
    * rename
    
    * add createEnvironments
    
    * rename
    
    * add createEnvironments
    
    * cleanup
    
    * add `useRecordsSyncService`
    
    * cleanup
    
    * update local storage for indexdb
    
    * update `LocalStoreRecordsSync`
    
    * add createEnvironments
    
    * fix: LocalStoreEnvSync
    
    * fix
    
    * undo
    
    * cleanup
    
    * refactor
    
    * fix: result check
    
    * fix: init
    
    * rename `ApiClientLocalDb`
    
    * refactor: local db methods
    
    * fix: interface
    
    * fix: metadata type
    
    * fix: duplicate env
    
    * undo
    
    * reset
    
    * revert
    
    * clear
    
    * fix: id creation
    
    * update db layer
    
    * refactor
    
    * cleanup
    
    * cleanup
    
    * review comments
    
    * revert

---
+++ app/src/features/apiClient/helpers/modules/sync/localStore/helpers/ApiClientLocalDb.ts
@@ -0,0 +1,21 @@
+import { EnvironmentData } from "backend/environment/types";
+import { RQAPI } from "features/apiClient/types";
+import Dexie, { EntityTable } from "dexie";
+import { ApiClientLocalDbTable } from "./types";
+import { ApiClientLocalStoreMeta } from "../../interfaces";
+
+export class ApiClientLocalDb {
+  db: Dexie;
+
+  constructor(metadata: ApiClientLocalStoreMeta) {
+    this.db = new Dexie("apiClientLocalStorageDB") as Dexie & {
+      [ApiClientLocalDbTable.APIS]: EntityTable<RQAPI.Record, "id">; // indexed by id
+      [ApiClientLocalDbTable.ENVIRONMENTS]: EntityTable<EnvironmentData, "id">;
+    };
+
+    this.db.version(metadata.version).stores({
+      [ApiClientLocalDbTable.APIS]: "id",
+      [ApiClientLocalDbTable.ENVIRONMENTS]: "id",
+    });
+  }
+}
---
+++ app/src/features/apiClient/helpers/modules/sync/localStore/helpers/index.ts
@@ -0,0 +1,1 @@
+export { ApiClientLocalDbQueryService } from "./ApiClientLocalDbQueryService";
