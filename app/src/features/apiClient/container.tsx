4575c969fb0a0eb9e03d434b8a2a2201d9eeb8d2
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-27 11:23:25 +0530

    rebase

--- app/src/features/apiClient/container.tsx
+++ app/src/features/apiClient/container.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import APIClientSidebar from "./screens/apiClient/components/sidebar/APIClientSidebar";
-import { getUserAuthDetails } from "store/slices/global/user/selectors";
-import { useSelector } from "react-redux";
 import { TabsContainer } from "componentsV2/Tabs/components/TabsContainer";
 import { TabServiceProvider } from "componentsV2/Tabs/store/TabServiceContextProvider";
-import ApiClientLoggedOutView from "./components/common/LoggedOutView/LoggedOutView";
 import { LocalSyncRefreshHandler } from "./LocalSyncRefreshHandler";
 import "./container.scss";
 import { ApiRecordsProvider } from "./store/apiRecords/ApiRecordsContextProvider";
 
 const ApiClientFeatureContainer: React.FC = () => {
-  const user = useSelector(getUserAuthDetails);
-
-  if (!user.loggedIn) {
-    return <ApiClientLoggedOutView />;
-  }
-
   return (
     <TabServiceProvider>
       <ApiRecordsProvider>
