d83bf0ec29ed860f0c5f26cf644fbb360b6bcd60
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-27 11:59:58 +0530

    cleanup

--- app/src/features/apiClient/helpers/modules/sync/useRecordsSyncService.ts
+++ app/src/features/apiClient/helpers/modules/sync/useRecordsSyncService.ts
@@ -42,7 +42,6 @@ export const useRecordsSyncService = () => {
         return localStoreRepository.apiClientRecordsRepository.clear();
       })
       .catch((error) => {
-        console.error("Error syncing APIs:", error);
         Sentry.captureException(error);
         setApisSyncStatus(RecordsSyncStatus.ERROR);
       });
