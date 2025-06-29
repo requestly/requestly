59dfe38e2ba335b8a67c20efc891fe6bbecd4e32
kanishkrawatt <rawat27kanishk@gmail.com>
2025-06-30 01:36:24 +0530

    feat: Add automation header modification features

---
+++ app/src/views/misc/Automation/layout.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useTransition } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import Footer from "components/sections/Footer";
+import { redirectToRoot } from "utils/RedirectionUtils";
+import { isEnvAutomation } from "utils/EnvUtils";
+import removePreloader from "actions/UI/removePreloader";
+import "./automation.css";
+
+const AutomationTemplate = () => {
+  const navigate = useNavigate();
+  const [, startTransition] = useTransition();
+  const isAutomation = isEnvAutomation();
+
+  useEffect(() => {
+    startTransition(() => {
+      try {
+        removePreloader();
+      } catch (err) {
+        console.error("Error removing preloader:", err);
+      }
+    });
+  }, []);
+
+  return (
+    <div className={`automation-layout${isAutomation ? "" : " manual-mode"}`}>
+      {!isAutomation && (
+        <div className="page-error-message-banner">
+          <span>
+            <b>Note:</b> This page only works in automation mode. If you're using it manually, things will not work.
+          </span>
+        </div>
+      )}
+
+      <Layout.Header className="automation-layout-navbar">
+        <img
+          width={94}
+          height={32}
+          className="logo"
+          src="/assets/media/common/RQ-BStack Logo.svg"
+          alt="Requestly Logo"
+          onClick={() => redirectToRoot(navigate)}
+        />
+      </Layout.Header>
+
+      <div className="automation-layout-main">
+        <Outlet />
+      </div>
+
+      <div className="automation-layout-footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default AutomationTemplate;
