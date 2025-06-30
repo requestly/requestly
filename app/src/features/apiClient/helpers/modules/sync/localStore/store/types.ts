106042984bbd174167873a0f56e77da21d962864
rohanmathur91 <mathurrohan04@gmail.com>
2025-06-30 15:09:05 +0530

    add sync service store

---
+++ app/src/features/apiClient/helpers/modules/sync/localStore/store/types.ts
@@ -0,0 +1,27 @@
+import { ApiClientRepositoryInterface } from "../../interfaces";
+
+export namespace APIClientSyncService {
+  export enum Status {
+    IDLE = "idle",
+    ERROR = "error",
+    SYNCING = "syncing",
+    SUCCESS = "success",
+  }
+
+  export type State = {
+    apisSyncStatus: Status;
+    envsSyncStatus: Status;
+  };
+
+  export type Actions = {
+    setApisSyncStatus: (status: Status) => void;
+    setEnvsSyncStatus: (status: Status) => void;
+
+    reset: () => void;
+    syncApis: (syncRepository: ApiClientRepositoryInterface) => Promise<void>;
+    syncEnvs: (syncRepository: ApiClientRepositoryInterface) => Promise<void>;
+    syncAll: (syncRepository: ApiClientRepositoryInterface) => Promise<boolean>;
+  };
+
+  export type Store = State & Actions;
+}
