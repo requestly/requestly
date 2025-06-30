c8ce019339f67985ef77152d59a07557bedcc864
Aarushsr12 <aarushsrivastava1212@gmail.com>
2025-06-30 18:55:30 +0530

    fix: react max-depth error

--- app/src/features/apiClient/hooks/useParentApiRecord.hook.ts
+++ app/src/features/apiClient/hooks/useParentApiRecord.hook.ts
@@ -2,12 +2,14 @@ import { useAPIRecords } from "../store/apiRecords/ApiRecordsContextProvider";
 import { useStore } from "zustand";
 import { createRecordStore } from "../store/apiRecords/apiRecords.store";
 import { RQAPI } from "../types";
+import { useState } from "react";
 
 export function useParentApiRecord(id: string) {
+  const [noopStore] = useState(() => createRecordStore({} as RQAPI.Record));
   const [getParent, getRecordStore] = useAPIRecords((s) => [s.getParent, s.getRecordStore]);
 
   const parent = getParent(id);
   // Returning an empty object because hook cannot be conditionally returned for records that don't have a parent
-  const versionStateStore = parent ? getRecordStore(parent) : createRecordStore({} as RQAPI.Record);
+  const versionStateStore = parent ? getRecordStore(parent) : noopStore;
   return useStore(versionStateStore);
 }
