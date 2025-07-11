c1584bc39f56ce6de739c8de397c3fbb784a843f
rohanmathur91 <mathurrohan04@gmail.com>
2025-07-11 12:41:12 +0530

    remove subscribe with selector middlerware

--- app/src/features/apiClient/helpers/modules/sync/localStore/store/syncServiceStore.ts
+++ app/src/features/apiClient/helpers/modules/sync/localStore/store/syncServiceStore.ts
@@ -1,5 +1,4 @@
 import { create } from "zustand";
-import { subscribeWithSelector } from "zustand/middleware";
 import localStoreRepository from "../ApiClientLocalStorageRepository";
 import * as Sentry from "@sentry/react";
 import { APIClientSyncService } from "./types";
@@ -24,159 +23,156 @@ async function getEntitySyncStatus(respository: LocalStoreRecordsSync | LocalSto
 }
 
 export const createSyncServiceStore = () => {
-  const syncServiceStore = create<APIClientSyncService.State>()(
-    subscribeWithSelector((set, get) => ({
-      apisSyncStatus: APIClientSyncService.Status.PENDING_RECORDS,
-      envsSyncStatus: APIClientSyncService.Status.PENDING_RECORDS,
+  const syncServiceStore = create<APIClientSyncService.State>((set, get) => ({
+    apisSyncStatus: APIClientSyncService.Status.PENDING_RECORDS,
+    envsSyncStatus: APIClientSyncService.Status.PENDING_RECORDS,
 
-      async updateSyncStatus() {
-        const [apisSyncStatus, envsSyncStatus] = await getSyncStatus();
-        set({ apisSyncStatus, envsSyncStatus });
-        return {
-          apisSyncStatus,
-          envsSyncStatus,
-        };
-      },
+    async updateSyncStatus() {
+      const [apisSyncStatus, envsSyncStatus] = await getSyncStatus();
+      set({ apisSyncStatus, envsSyncStatus });
+      return {
+        apisSyncStatus,
+        envsSyncStatus,
+      };
+    },
 
-      async syncApis(syncRepository, recordsToSkip) {
-        if (get().apisSyncStatus === APIClientSyncService.Status.SUCCESS) {
-          return { success: true, data: [] };
+    async syncApis(syncRepository, recordsToSkip) {
+      if (get().apisSyncStatus === APIClientSyncService.Status.SUCCESS) {
+        return { success: true, data: [] };
+      }
+
+      set({ apisSyncStatus: APIClientSyncService.Status.SYNCING });
+
+      try {
+        const result = await localStoreRepository.apiClientRecordsRepository.getAllRecords();
+        const recordsToSync = recordsToSkip
+          ? result.data.records.filter((r) => !recordsToSkip.has(r.id))
+          : result.data.records;
+
+        const syncResult = await syncRepository.apiClientRecordsRepository.batchCreateRecordsWithExistingId(
+          recordsToSync
+        );
+
+        if (!syncResult.success) {
+          throw new Error(syncResult.message);
         }
 
-        set({ apisSyncStatus: APIClientSyncService.Status.SYNCING });
+        await localStoreRepository.apiClientRecordsRepository.clear();
+        set({ apisSyncStatus: APIClientSyncService.Status.SUCCESS });
 
-        try {
-          const result = await localStoreRepository.apiClientRecordsRepository.getAllRecords();
-          const recordsToSync = recordsToSkip
-            ? result.data.records.filter((r) => !recordsToSkip.has(r.id))
-            : result.data.records;
+        return { success: true, data: result.data.records };
+      } catch (error) {
+        Sentry.captureException(error);
+        set({ apisSyncStatus: APIClientSyncService.Status.ERROR });
+        return { success: false, error: error.message };
+      }
+    },
 
-          const syncResult = await syncRepository.apiClientRecordsRepository.batchCreateRecordsWithExistingId(
-            recordsToSync
-          );
+    async syncEnvs(syncRepository, recordsToSkip) {
+      if (get().envsSyncStatus === APIClientSyncService.Status.SUCCESS) {
+        return { success: true, data: [] };
+      }
 
-          if (!syncResult.success) {
-            throw new Error(syncResult.message);
-          }
+      set({ envsSyncStatus: APIClientSyncService.Status.SYNCING });
 
-          await localStoreRepository.apiClientRecordsRepository.clear();
-          set({ apisSyncStatus: APIClientSyncService.Status.SUCCESS });
-
-          return { success: true, data: result.data.records };
-        } catch (error) {
-          Sentry.captureException(error);
-          set({ apisSyncStatus: APIClientSyncService.Status.ERROR });
-          return { success: false, error: error.message };
-        }
-      },
-
-      async syncEnvs(syncRepository, recordsToSkip) {
-        if (get().envsSyncStatus === APIClientSyncService.Status.SUCCESS) {
-          return { success: true, data: [] };
-        }
-
-        set({ envsSyncStatus: APIClientSyncService.Status.SYNCING });
-
-        try {
-          const result = await localStoreRepository.environmentVariablesRepository.getAllEnvironments();
-          const rawEnvironments = Object.values(result.data.environments);
-          const recordsToSync = recordsToSkip
-            ? rawEnvironments.filter((r) => !recordsToSkip.has(r.id))
-            : rawEnvironments;
-          const environments = await syncRepository.environmentVariablesRepository.createEnvironments(recordsToSync);
-
-          await localStoreRepository.environmentVariablesRepository.clear();
-          set({ envsSyncStatus: APIClientSyncService.Status.SUCCESS });
-
-          return { success: true, data: environments };
-        } catch (error) {
-          Sentry.captureException(error);
-          set({ envsSyncStatus: APIClientSyncService.Status.ERROR });
-          return { success: false, error: error.message };
-        }
-      },
-
-      async syncGlobalEnv(syncRepository) {
-        const allEnvs = await localStoreRepository.environmentVariablesRepository._getAllEnvironments();
-        if (!allEnvs.success) {
-          throw new Error("Could not get all environments!");
-        }
-
-        const envs = Object.values(allEnvs.data.environments);
-        const isJustGlobalEnv =
-          envs.length === 1 &&
-          envs[0].id === localStoreRepository.environmentVariablesRepository.getGlobalEnvironmentId();
-
-        if (!isJustGlobalEnv) {
-          return;
-        }
-
-        // const globalEnvId = localStoreRepository.environmentVariablesRepository.getGlobalEnvironmentId();
-        // const globalEnv = allEnvs.data.environments[globalEnvId];
-        // if (globalEnv.variables && Object.keys(globalEnv.variables).length) {
-        //   await syncRepository.environmentVariablesRepository.updateEnvironment(globalEnvId, {
-        //     variables: globalEnv.variables,
-        //   });
-        // }
+      try {
+        const result = await localStoreRepository.environmentVariablesRepository.getAllEnvironments();
+        const rawEnvironments = Object.values(result.data.environments);
+        const recordsToSync = recordsToSkip ? rawEnvironments.filter((r) => !recordsToSkip.has(r.id)) : rawEnvironments;
+        const environments = await syncRepository.environmentVariablesRepository.createEnvironments(recordsToSync);
 
         await localStoreRepository.environmentVariablesRepository.clear();
-      },
+        set({ envsSyncStatus: APIClientSyncService.Status.SUCCESS });
 
-      async syncAll(syncRepository, skip) {
-        const { syncApis, syncEnvs, updateSyncStatus, syncGlobalEnv } = get();
+        return { success: true, data: environments };
+      } catch (error) {
+        Sentry.captureException(error);
+        set({ envsSyncStatus: APIClientSyncService.Status.ERROR });
+        return { success: false, error: error.message };
+      }
+    },
 
-        await syncGlobalEnv(syncRepository);
+    async syncGlobalEnv(syncRepository) {
+      const allEnvs = await localStoreRepository.environmentVariablesRepository._getAllEnvironments();
+      if (!allEnvs.success) {
+        throw new Error("Could not get all environments!");
+      }
 
-        const { apisSyncStatus, envsSyncStatus } = await updateSyncStatus();
-        if (
-          apisSyncStatus === APIClientSyncService.Status.SUCCESS &&
-          envsSyncStatus === APIClientSyncService.Status.SUCCESS
-        ) {
-          return {
-            records: [],
-            environments: [],
-          };
-        }
+      const envs = Object.values(allEnvs.data.environments);
+      const isJustGlobalEnv =
+        envs.length === 1 &&
+        envs[0].id === localStoreRepository.environmentVariablesRepository.getGlobalEnvironmentId();
 
-        trackLocalStorageSyncStarted({ type: "api" });
-        toast.loading("Getting your local APIs ready...", 15);
+      if (!isJustGlobalEnv) {
+        return;
+      }
 
-        const [apis, envs] = await Promise.allSettled([
-          syncApis(syncRepository, skip?.recordsToSkip),
-          syncEnvs(syncRepository, skip?.environmentsToSkip),
-        ]);
-        const records = apis.status === "fulfilled" ? (apis.value.success ? apis.value.data : []) : [];
-        const environments = envs.status === "fulfilled" ? (envs.value.success ? envs.value.data : []) : [];
+      await localStoreRepository.environmentVariablesRepository.clear();
+    },
 
-        if (apis.status === "fulfilled") {
-          if (!apis.value.success) {
-            throw new Error("Not able to sync local APIs");
-          }
-        }
+    async syncAll(syncRepository, skip) {
+      const { syncApis, syncEnvs, updateSyncStatus, syncGlobalEnv } = get();
 
-        if (envs.status === "fulfilled") {
-          if (!envs.value.success) {
-            throw new Error("Not able to sync local Environments");
-          }
-        }
+      await syncGlobalEnv(syncRepository);
 
+      const { apisSyncStatus, envsSyncStatus } = await updateSyncStatus();
+      if (
+        apisSyncStatus === APIClientSyncService.Status.SUCCESS &&
+        envsSyncStatus === APIClientSyncService.Status.SUCCESS
+      ) {
         return {
-          records,
-          environments,
+          records: [],
+          environments: [],
         };
-      },
+      }
 
-      setSyncTask(task) {
-        const existingTask = get().syncTask;
-        if (existingTask) {
-          throw new Error("Multiple sync tasks started!");
+      trackLocalStorageSyncStarted({ type: "api" });
+      toast.loading("Getting your local APIs ready...", 15);
+
+      const [apis, envs] = await Promise.allSettled([
+        syncApis(syncRepository, skip?.recordsToSkip),
+        syncEnvs(syncRepository, skip?.environmentsToSkip),
+      ]);
+      const records = apis.status === "fulfilled" ? (apis.value.success ? apis.value.data : []) : [];
+      const environments = envs.status === "fulfilled" ? (envs.value.success ? envs.value.data : []) : [];
+
+      if (apis.status === "fulfilled") {
+        if (!apis.value.success) {
+          throw new Error("Not able to sync local APIs");
         }
+      }
+
+      if (envs.status === "fulfilled") {
+        if (!envs.value.success) {
+          throw new Error("Not able to sync local Environments");
+        }
+      }
+
+      return {
+        records,
+        environments,
+      };
+    },
+
+    setSyncTask(task) {
+      const existingTask = get().syncTask;
+      if (task === null) {
         set({
           syncTask: task,
         });
-      },
-    }))
-  );
+
+        return;
+      }
+
+      if (existingTask) {
+        throw new Error("Multiple sync tasks started!");
+      }
+
+      set({
+        syncTask: task,
+      });
+    },
+  }));
 
   return syncServiceStore;
 };
