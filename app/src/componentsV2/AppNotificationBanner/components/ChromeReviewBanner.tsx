35e1bfbc29071d61043d819f1a37e1623443d0c6
Parth Bhardwaj <parthb.git@gmail.com>
2025-07-14 16:38:10 +0530

    chore: update chrome store review banner

--- app/src/componentsV2/AppNotificationBanner/components/ChromeReviewBanner.tsx
+++ app/src/componentsV2/AppNotificationBanner/components/ChromeReviewBanner.tsx
@@ -1,37 +1,37 @@
-import React, { useState, useMemo } from "react";
+import React, { useMemo } from "react";
 import { Banner } from "../banner.types";
 import BaseBanner from "./BaseBanner";
 import { useRenderBannerText } from "../hooks/useRenderBannerText";
 import { useBannerAction } from "../hooks/useBannerAction";
-import STORAGE from "config/constants/sub/storage";
+// import STORAGE from "config/constants/sub/storage";
 interface Props {
   banner: Banner;
   onClose: (e: React.MouseEvent) => void;
 }
 
 export const ChromeReviewBanner: React.FC<Props> = ({ banner, onClose }) => {
-  const [hasReviewed, setHasReviewed] = useState(
-    () => localStorage.getItem(STORAGE.LOCAL_STORAGE.REDIRECT_TO_CHROME_STORE_REVIEW) === "true"
-  );
+  // const [hasReviewed] = useState(
+  //   () => localStorage.getItem(STORAGE.LOCAL_STORAGE.REDIRECT_TO_CHROME_STORE_REVIEW) === "true"
+  // );
   const actions = useBannerAction(banner.actions || []);
   const bannerText = useRenderBannerText(banner);
-  const [preReviewText, postReviewText] = bannerText.split("||");
+  const [preReviewText] = bannerText.split("||");
   const actionsConfig = useMemo(() => {
-    if (hasReviewed) return [];
     return actions.map((action) => ({
       ...action,
       onClick: () => {
-        setHasReviewed(true);
+        // setHasReviewed(true);
         action.onClick();
       },
     }));
-  }, [hasReviewed, actions]);
+  }, [actions]);
 
   return (
     <BaseBanner
       banner={banner}
       onClose={onClose}
-      text={!hasReviewed ? preReviewText : postReviewText}
+      // text={!hasReviewed ? preReviewText : postReviewText}
+      text={preReviewText}
       actionsConfig={actionsConfig}
     />
   );
