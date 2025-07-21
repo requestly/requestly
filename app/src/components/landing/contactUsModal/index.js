26ae0137a9e933805f24c8e480d1677edceb964d
dinex-dev <dineshthakur.dev@gmail.com>
2025-07-21 22:21:54 +0530

    fix: keep old link for popup

--- app/src/components/landing/contactUsModal/index.js
+++ app/src/components/landing/contactUsModal/index.js
@@ -8,7 +8,7 @@ const ContactUsModal = ({ isOpen, handleToggleModal }) => {
       <div className="rq-modal-content">
         <InlineWidget
           styles={{ height: "65vh", marginTop: "1px" }}
-          url="https://www.browserstack.com/contact?utm_source=Requestly&utm_medium=redirect&utm_platform=external"
+          url="https://calendly.com/requestly/sales"
           pageSettings={{
             hideEventTypeDetails: true,
             hideLandingPageDetails: true,
