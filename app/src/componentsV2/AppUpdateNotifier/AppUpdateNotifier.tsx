32fd9c5dbc3d1c575c6b881a54ddc50f907d485c
rohanmathur91 <mathurrohan04@gmail.com>
2025-07-23 11:50:27 +0530

    cleanup

--- app/src/componentsV2/AppUpdateNotifier/AppUpdateNotifier.tsx
+++ app/src/componentsV2/AppUpdateNotifier/AppUpdateNotifier.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getDatabase, onValue, ref } from "firebase/database";
 import firebaseApp from "firebase";
 import * as semver from "semver";
