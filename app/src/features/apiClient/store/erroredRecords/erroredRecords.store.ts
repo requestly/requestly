c068f559c3d179f1a2d4a905b8d359d0c10f74cb
Rohan Mathur <mathurrohan04@gmail.com>
2025-07-24 22:20:06 +0530

    [ENGG-3988]: create stores for each logical unit for environments  (#3280)
    * update base types
    
    * add variable store
    
    * add createEnvironmentsStore
    
    * add erroredRecords
    
    * review comments
    
    * review comments
    
    * fix
    
    * fix
    
    * refactor
    
    * throw errors
    
    * errored records store (#3289)
    
    * add errored records store
    
    * add todo
    
    * cleanup

---
+++ app/src/features/apiClient/store/erroredRecords/erroredRecords.store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { ErroredRecord } from "features/apiClient/helpers/modules/sync/local/services/types";
+
+type ErroredRecords<T = ErroredRecord> = {
+  apiErroredRecords: T[];
+  environmentErroredRecords: T[];
+
+  setApiErroredRecords: (erroredRecord: T[]) => void;
+  setEnvironmentErroredRecords: (erroredRecord: T[]) => void;
+};
+
+type ErroredRecordsStore = ErroredRecords;
+
+export const createErroredRecordsStore = ({ apiErroredRecords, environmentErroredRecords }: ErroredRecords) => {
+  return create<ErroredRecordsStore>()((set, get) => ({
+    apiErroredRecords,
+    environmentErroredRecords,
+
+    setApiErroredRecords(erroredRecords) {
+      set({ apiErroredRecords: erroredRecords });
+    },
+
+    setEnvironmentErroredRecords(erroredRecords) {
+      set({ environmentErroredRecords: erroredRecords });
+    },
+  }));
+};
