f9d7d62ec913654979e5523af54c047a597df390
rohanmathur91 <mathurrohan04@gmail.com>
2025-07-24 11:34:34 +0530

    review comments

--- app/src/features/apiClient/store/variables/variables.store.ts
+++ app/src/features/apiClient/store/variables/variables.store.ts
@@ -3,45 +3,65 @@ import { create } from "zustand";
 
 export type VariablesStore = {
   // state
+  version: number;
   data: Map<EnvironmentVariableKey, EnvironmentVariableValue>;
 
   // actions
   delete: (key: EnvironmentVariableKey) => void;
   add: (key: EnvironmentVariableKey, variable: EnvironmentVariableValue) => void;
   update: (key: EnvironmentVariableKey, updates: Omit<EnvironmentVariableValue, "id">) => void;
-  get: (key: EnvironmentVariableKey) => EnvironmentVariableValue;
+  getVariable: (key: EnvironmentVariableKey) => EnvironmentVariableValue | undefined;
   getAll: () => Map<EnvironmentVariableKey, EnvironmentVariableValue>;
   search: (value: string) => Map<EnvironmentVariableKey, EnvironmentVariableValue>;
+  incrementVersion: () => void;
 };
 
 export const createVariablesStore = ({ variables }: { variables: EnvironmentVariables }) => {
-  // Need a way to tell parent that variable is updated
   return create<VariablesStore>()((set, get) => ({
+    version: 0,
     data: new Map(Object.entries(variables)),
 
     delete(key) {
       const { data } = get();
+
+      if (!data.has(key)) {
+        return;
+      }
+
       data.delete(key);
       set({ data });
+      get().incrementVersion();
     },
 
     add(key, variable) {
       const { data } = get();
       data.set(key, variable);
       set({ data });
+      get().incrementVersion();
     },
 
     update(key, updates) {
       const { data } = get();
+
+      if (!data.has(key)) {
+        return;
+      }
+
       const existingValue = data.get(key);
       const updatedValue = { ...existingValue, ...updates };
 
       data.set(key, updatedValue);
       set({ data });
+      get().incrementVersion();
     },
 
-    get(key) {
+    getVariable(key) {
       const { data } = get();
+
+      if (!data.has(key)) {
+        return;
+      }
+
       return data.get(key);
     },
 
@@ -54,6 +74,12 @@ export const createVariablesStore = ({ variables }: { variables: EnvironmentVari
       const { data } = get();
       const searchResults = Object.entries(data).filter(([key]) => key.toLowerCase().includes(value.toLowerCase()));
       return new Map(searchResults);
+    },
+
+    incrementVersion() {
+      set({
+        version: get().version + 1,
+      });
     },
   }));
 };
