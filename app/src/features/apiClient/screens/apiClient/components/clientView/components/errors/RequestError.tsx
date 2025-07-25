18a9b42fba19cda7b19714f4e877636d2299490d
Aarushsr12 <aarushsrivastava1212@gmail.com>
2025-07-25 14:16:01 +0530

    fix: file-error svg

--- app/src/features/apiClient/screens/apiClient/components/clientView/components/errors/RequestError.tsx
+++ app/src/features/apiClient/screens/apiClient/components/clientView/components/errors/RequestError.tsx
@@ -5,7 +5,7 @@ import ApiClientFileMissingError from "./APIClientFileMissingError";
 
 export const RequestError: React.FC<{ error: RQAPI.ExecutionError; onRetry: () => void }> = (props) => {
   if (props.error.type === RQAPI.ApiClientErrorType.MISSING_FILE) {
-    return <ApiClientFileMissingError imageUrl="/assets/media/apiClient/request-file-error.svg" {...props} />;
+    return <ApiClientFileMissingError imageUrl="/assets/media/apiClient/file-error.svg" {...props} />;
   }
 
   return <ApiClientErrorPlaceholder imageUrl="/assets/media/apiClient/request-error.svg" {...props} />;
