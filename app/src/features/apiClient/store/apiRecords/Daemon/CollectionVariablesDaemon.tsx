5065bfb78501621a40f4761a1a77b5f62b8d0854
Navdeep Singh Rathore <navdeep.r@browserstack.com>
2025-07-28 13:32:50 +0530

    refactor: remove react dependence and move logic to store methods

--- app/src/features/apiClient/store/apiRecords/Daemon/CollectionVariablesDaemon.tsx
+++ app/src/features/apiClient/store/apiRecords/Daemon/CollectionVariablesDaemon.tsx
@@ -1,46 +1,22 @@
 import { VariableScope } from "backend/environment/types";
 import { useApiClientRepository } from "features/apiClient/helpers/modules/sync/useApiClientSyncRepo";
-import React, { useCallback, useEffect } from "react";
-import { useAPIRecords } from "../ApiRecordsContextProvider";
-import { CollectionVariableMap, RQAPI } from "features/apiClient/types";
+import React, { useEffect } from "react";
+import { useAPIRecordsStore } from "../ApiRecordsContextProvider";
 
 const CollectionVariablesDaemon: React.FC = () => {
-  const indexStore = useAPIRecords((state) => state.indexStore);
-
-  const getCollectionVariableStore = useCallback(
-    (id: string) => {
-      const recordState = indexStore.get(id)?.getState();
-      if (!recordState || recordState.type !== RQAPI.RecordType.COLLECTION) return null;
-
-      return recordState.collectionVariables;
-    },
-    [indexStore]
-  );
-
-  const updateCollections = useCallback(
-    (newCollectionVariables: CollectionVariableMap) => {
-      for (const [recordId, newData] of Object.entries(newCollectionVariables)) {
-        const varStore = getCollectionVariableStore(recordId);
-        if (varStore) {
-          varStore.getState().mergeAndUpdate(newData.variables);
-        }
-      }
-    },
-    [getCollectionVariableStore]
-  );
-
+  const recordsStore = useAPIRecordsStore();
   const { environmentVariablesRepository } = useApiClientRepository();
 
   useEffect(() => {
     const unsubscribe = environmentVariablesRepository.attachListener({
       scope: VariableScope.COLLECTION,
       callback: (newCollectionVariables) => {
-        updateCollections(newCollectionVariables);
+        recordsStore.getState().updateCollectionVariables(newCollectionVariables);
       },
     });
 
     return unsubscribe;
-  }, [environmentVariablesRepository, updateCollections]);
+  }, [environmentVariablesRepository, recordsStore]);
 
   return null;
 };
