e0d5c7303f1c12403a14aab25a7f3147cc0451e8
Navdeep Singh Rathore <navdeep.r@browserstack.com>
2025-07-30 15:41:39 +0530

    chore: handle null inside meta hook

--- app/src/features/apiClient/contexts/meta.tsx
+++ app/src/features/apiClient/contexts/meta.tsx
@@ -2,13 +2,17 @@ import { useContext } from "react";
 import { ApiClientRepositoryInterface } from "../helpers/modules/sync/interfaces";
 import { useApiClientRepository } from "../helpers/modules/sync/useApiClientSyncRepo";
 import { AllApiClientStores, ApiRecordsStoreContext } from "../store/apiRecords/ApiRecordsContextProvider";
+import { NativeError } from "errors/NativeError";
 
 export type ApiClientFeatureContext = {
-  stores: AllApiClientStores | null;
+  stores: AllApiClientStores;
   repositories: ApiClientRepositoryInterface;
 };
-export function useApiClientFeatureContext() {
+export function useApiClientFeatureContext(): ApiClientFeatureContext {
   const stores = useContext(ApiRecordsStoreContext);
+  if (!stores) {
+    throw new NativeError("Command can't be called before stores are initialized");
+  }
   const repositories = useApiClientRepository();
 
   return { stores, repositories };
