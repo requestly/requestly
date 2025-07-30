5149c28bb1ac4670706539e57477d1c2f2dcb52c
Navdeep Singh Rathore <navdeep.r@browserstack.com>
2025-07-30 15:31:53 +0530

    chore: move files around

--- app/src/features/apiClient/commands/index.tsx
+++ app/src/features/apiClient/commands/index.tsx
@@ -1,19 +1,9 @@
 import { useApiClientFeatureContext } from "features/apiClient/contexts/meta";
 import { NativeError } from "errors/NativeError";
-import { sampleCmd1, sampleCmd2, sampleCmd3, sampleCmd4, sampleCmd5 } from "./sampleCommands";
 import { bindCommands } from "./bindCommands";
 
 const commands = {
-  // strucutre for commands to be added
-  env: {
-    cmd1: sampleCmd1,
-    cmd2: sampleCmd2,
-  },
-  records: {
-    cmd3: sampleCmd3,
-    cmd4: sampleCmd4,
-    cmd5: sampleCmd5,
-  },
+  /* todo: add commands and scope */
 };
 
 export function useCommand() {
