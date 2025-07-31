a5f38fdd6ad5eeaf4eb80a979bf9fb01a2d11cb1
rohanmathur91 <mathurrohan04@gmail.com>
2025-07-31 15:08:26 +0530

    fix: force refresh for local sync

--- app/src/features/apiClient/commands/index.tsx
+++ app/src/features/apiClient/commands/index.tsx
@@ -2,9 +2,11 @@ import { useApiClientFeatureContext } from "features/apiClient/contexts/meta";
 import { bindCommands } from "./bindCommands";
 import { useMemo } from "react";
 import * as envCommands from "./environments";
+import * as apiRecordCommands from "./records";
 
 const commands = {
   env: envCommands,
+  api: apiRecordCommands,
 };
 
 export function useCommand() {
