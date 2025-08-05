ac03f8d08c6e96b87250031cf6cd505a72945452
Nafees Nehar <nafees87n@gmail.com>
2025-08-05 12:40:09 +0530

    [ENGG-4014] support to fetch graphql introspection schema (#3337)

---
+++ app/src/features/apiClient/helpers/introspectionQuery.ts
@@ -0,0 +1,39 @@
+import { getIntrospectionQuery, IntrospectionQuery } from "graphql";
+import { RQAPI } from "../types";
+import { makeRequest } from "../screens/apiClient/utils";
+import { graphQLRequestToHttpRequestAdapter } from "../screens/apiClient/components/views/graphql/utils";
+
+const createGraphQLIntrospectionRequest = (url: string): RQAPI.GraphQLRequest => {
+  return {
+    url,
+    headers: [
+      // {
+      //   id: 0,
+      //   isEnabled: true,
+      //   key: "Content-Type",
+      //   value: "application/json",
+      // },
+    ],
+    operation: getIntrospectionQuery(),
+    variables: "",
+  };
+};
+
+export type IntrospectionData = IntrospectionQuery;
+
+export const fetchGraphQLIntrospectionData = async (url: string, appMode: string): Promise<IntrospectionData> => {
+  const request = createGraphQLIntrospectionRequest(url);
+  const httpRequest = graphQLRequestToHttpRequestAdapter(request);
+  const response = await makeRequest(appMode, httpRequest);
+
+  if (response.status !== 200) {
+    throw new Error(`Failed to fetch introspection data: ${response.statusText}`);
+  }
+
+  try {
+    const responseBodyJson = JSON.parse(response.body);
+    return responseBodyJson.data;
+  } catch (error) {
+    throw new Error(`Failed to parse introspection data: ${error.message}`);
+  }
+};
--- app/src/features/apiClient/screens/apiClient/components/views/graphql/components/GraphQLClientUrl/GraphQLClientUrl.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/graphql/components/GraphQLClientUrl/GraphQLClientUrl.tsx
@@ -1,22 +1,62 @@
 import { memo } from "react";
 import { EnvironmentVariableValue } from "backend/environment/types";
+
 import { ApiClientUrl } from "features/apiClient/screens/apiClient/components/views/components/request/components/ApiClientUrl/ApiClientUrl";
+import { MdOutlineCheckCircle } from "@react-icons/all-files/md/MdOutlineCheckCircle";
+import { MdOutlineWarningAmber } from "@react-icons/all-files/md/MdOutlineWarningAmber";
+import { Spin, Tooltip } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import { useGraphQLRecordStore } from "features/apiClient/hooks/useGraphQLRecordStore";
+import "./graphqlClientUrl.scss";
 
 interface GraphQLClientUrlProps {
   url: string;
   currentEnvironmentVariables: Record<string, EnvironmentVariableValue>;
   onEnterPress: (e: KeyboardEvent) => void;
   onUrlChange: (value: string) => void;
+  fetchingIntrospectionData: boolean;
+  isIntrospectionDataFetchingFailed: boolean;
 }
 
-const GraphQLClientUrl = ({ url, currentEnvironmentVariables, onEnterPress, onUrlChange }: GraphQLClientUrlProps) => {
+const GraphQLClientUrl = ({
+  url,
+  currentEnvironmentVariables,
+  onEnterPress,
+  onUrlChange,
+  fetchingIntrospectionData,
+  isIntrospectionDataFetchingFailed,
+}: GraphQLClientUrlProps) => {
+  const [introspectionData] = useGraphQLRecordStore((state) => [state.introspectionData]);
+
   return (
-    <ApiClientUrl
-      url={url}
-      currentEnvironmentVariables={currentEnvironmentVariables}
-      onEnterPress={onEnterPress}
-      onUrlChange={onUrlChange}
-    />
+    <div className="gql-url-container">
+      <ApiClientUrl
+        url={url}
+        currentEnvironmentVariables={currentEnvironmentVariables}
+        onEnterPress={onEnterPress}
+        onUrlChange={onUrlChange}
+      />
+      <div className="gql-url__fetch-status">
+        {fetchingIntrospectionData ? (
+          <div className="gql-url-container__loading">
+            <Spin indicator={<LoadingOutlined spin />} size="small" />
+            <span>Fetching schema</span>
+          </div>
+        ) : isIntrospectionDataFetchingFailed ? (
+          <Tooltip
+            color="#000"
+            title="Error while fetching the schema. The endpoint may not exist or misconfigured."
+            placement="top"
+          >
+            <MdOutlineWarningAmber className="gql-url__fetch-status-icon schema-failed" />
+          </Tooltip>
+        ) : introspectionData !== null ? (
+          <Tooltip title="Schema fetched successfully" placement="top" color="#000">
+            <MdOutlineCheckCircle className=" gql-url__fetch-status-icon schema-success" />
+          </Tooltip>
+        ) : null}
+      </div>
+    </div>
   );
 };
 
