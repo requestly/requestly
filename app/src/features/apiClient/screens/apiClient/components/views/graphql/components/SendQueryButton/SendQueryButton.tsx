97aac51610668262526944ac1cb5f2ebd9112756
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-08-07 18:55:32 +0530

    [ENGG-4102]: Support for multiple operations in GraphQL client (#3353)

---
+++ app/src/features/apiClient/screens/apiClient/components/views/graphql/components/SendQueryButton/SendQueryButton.tsx
@@ -0,0 +1,49 @@
+import React, { useMemo } from "react";
+import { useGraphQLRecordStore } from "features/apiClient/hooks/useGraphQLRecordStore";
+import { RQButton } from "lib/design-system-v2/components";
+import { KEYBOARD_SHORTCUTS } from "../../../../../../../../../constants/keyboardShortcuts";
+import { Dropdown, DropDownProps } from "antd";
+import { MdOutlineKeyboardArrowDown } from "@react-icons/all-files/md/MdOutlineKeyboardArrowDown";
+
+interface Props {
+  disabled?: boolean;
+  loading?: boolean;
+  onSendClick: (operationName?: string) => void;
+}
+
+export const SendQueryButton: React.FC<Props> = ({ disabled, loading, onSendClick }) => {
+  const [operationNames] = useGraphQLRecordStore((state) => [state.operationNames]);
+
+  const operationItems: DropDownProps["menu"]["items"] = useMemo(() => {
+    return operationNames.map((operationName) => ({
+      key: operationName,
+      label: operationName,
+      onClick: () => onSendClick(operationName),
+    }));
+  }, [operationNames, onSendClick]);
+
+  if (operationNames.length <= 1) {
+    return (
+      <RQButton
+        type="primary"
+        showHotKeyText
+        onClick={() => onSendClick()}
+        hotKey={KEYBOARD_SHORTCUTS.API_CLIENT.SEND_REQUEST.hotKey}
+        className="text-bold"
+        disabled={disabled}
+        loading={loading}
+      >
+        Send
+      </RQButton>
+    );
+  } else {
+    return (
+      <Dropdown menu={{ items: operationItems }} placement="bottom">
+        <RQButton className="gql-send-query-dropdown-button" type="primary">
+          Send
+          <MdOutlineKeyboardArrowDown />
+        </RQButton>
+      </Dropdown>
+    );
+  }
+};
