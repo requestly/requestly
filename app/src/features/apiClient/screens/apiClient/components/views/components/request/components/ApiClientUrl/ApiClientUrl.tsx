ca54597ee645c0967389f83b1bc95e96494571a8
Parth Bhardwaj <parthb.git@gmail.com>
2025-08-08 09:43:59 +0530

    fix: rename issues on save and breadcrumb input blur

--- app/src/features/apiClient/screens/apiClient/components/views/components/request/components/ApiClientUrl/ApiClientUrl.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/request/components/ApiClientUrl/ApiClientUrl.tsx
@@ -1,5 +1,5 @@
 import { EnvironmentVariableValue } from "backend/environment/types";
-import { DEMO_API_URL } from "features/apiClient/constants";
+import { DEMO_HTTP_API_URL } from "features/apiClient/constants";
 import SingleLineEditor from "features/apiClient/screens/environment/components/SingleLineEditor";
 import { useCallback } from "react";
 import "./apiClientUrl.css";
@@ -22,7 +22,7 @@ export const ApiClientUrl = ({ url, currentEnvironmentVariables, onEnterPress, o
   return (
     <SingleLineEditor
       className="api-request-url"
-      placeholder={DEMO_API_URL}
+      placeholder={DEMO_HTTP_API_URL}
       //value={entry.request.url}
       defaultValue={url}
       onChange={(value) => {
