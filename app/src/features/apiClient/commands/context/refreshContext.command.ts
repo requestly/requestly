81fcadb009dcbe180219050be7516e73c08f1d60
Navdeep Singh Rathore <navdeep.r@browserstack.com>
2025-08-11 21:43:30 +0530

    feat: commands for setting up context and view

---
+++ app/src/features/apiClient/commands/context/refreshContext.command.ts
@@ -0,0 +1,30 @@
+import { NativeError } from "errors/NativeError";
+import {
+  ApiClientFeatureContext,
+  apiClientFeatureContextProviderStore,
+} from "features/apiClient/store/apiClientFeatureContext/apiClientFeatureContext.store";
+import { extractSetupDataFromRepository } from "./utils";
+
+async function _refreshContext(
+  stores: ApiClientFeatureContext["stores"],
+  repo: ApiClientFeatureContext["repositories"]
+) {
+  const { apiClientRecords, environments, erroredRecords } = await extractSetupDataFromRepository(repo);
+
+  stores.records.getState().refresh(apiClientRecords.records);
+  stores.environments.getState().refresh({
+    globalEnvironment: environments.globalEnvironment,
+    environments: environments.nonGlobalEnvironments,
+  });
+  stores.erroredRecords.getState().setApiErroredRecords(erroredRecords.apiErroredRecords);
+  stores.erroredRecords.getState().setEnvironmentErroredRecords(erroredRecords.environmentErroredRecords);
+}
+
+export const refreshContext = async (ctxId: ApiClientFeatureContext["id"]) => {
+  const contexts = apiClientFeatureContextProviderStore.getState().contexts;
+
+  if (!contexts.has(ctxId)) throw new NativeError("Add context to the store before trying to refresh");
+
+  const context = contexts.get(ctxId);
+  return _refreshContext(context.stores, context.repositories);
+};
