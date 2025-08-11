4361bae8dc1c3b990698febce631eecc3548ddc3
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-11 16:34:39 +0530

    cleanup

--- app/src/features/apiClient/screens/apiClient/components/sidebar/MultiWorkspaceSidebar/ContextualCollectionsList/ContextualCollectionsList.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/MultiWorkspaceSidebar/ContextualCollectionsList/ContextualCollectionsList.tsx
@@ -1,143 +1,40 @@
-import { useApiClientRepository } from "features/apiClient/helpers/modules/sync/useApiClientSyncRepo";
-import { filterOutChildrenRecords, processRecordsForDuplication } from "features/apiClient/screens/apiClient/utils";
 import { useApiClientMultiWorkspaceView } from "features/apiClient/store/multiWorkspaceView/multiWorkspaceView.store";
-import { BulkActions, RQAPI } from "features/apiClient/types";
+import { RQAPI } from "features/apiClient/types";
 import { useRBAC } from "features/rbac";
-import { head, isEmpty } from "lodash";
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { DndProvider } from "react-dnd";
-import { toast } from "utils/Toast";
 import { SidebarListHeader } from "../../components/sidebarListHeader/SidebarListHeader";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { CollectionsList } from "./CollectionsList/CollectionsList";
-import ActionMenu from "../../components/collectionsList/BulkActionsMenu";
-import { useApiClientContext } from "features/apiClient/contexts";
-import { notification } from "antd";
-import * as Sentry from "@sentry/react";
-import { useAPIRecords } from "features/apiClient/store/apiRecords/ApiRecordsContextProvider";
 import { ContextId } from "features/apiClient/contexts/contextId.context";
 
 export const ContextualCollectionsList: React.FC<{
-  searchValue: string;
-  setSearchValue: (value: string) => void;
   onNewClick: (src: RQAPI.AnalyticsEventSource, recordType: RQAPI.RecordType) => Promise<void>;
   recordTypeToBeCreated: RQAPI.RecordType | null;
-}> = ({ searchValue, setSearchValue, onNewClick, recordTypeToBeCreated }) => {
+}> = ({ onNewClick, recordTypeToBeCreated }) => {
   const { validatePermission } = useRBAC();
   const { isValidPermission } = validatePermission("api_client_request", "create");
   const selectedWorkspaces = useApiClientMultiWorkspaceView((s) => s.selectedWorkspaces);
 
-  const [showSelection, setShowSelection] = useState(false);
-  const [isMoveCollectionModalOpen, setIsMoveCollectionModalOpen] = useState(false);
-  const [selectedRecords, setSelectedRecords] = useState<Set<RQAPI.ApiClientRecord["id"]>>(new Set());
-  const [isAllRecordsSelected, setIsAllRecordsSelected] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
-  const { apiClientRecordsRepository } = useApiClientRepository();
-  // const { isRecordBeingCreated, setIsDeleteModalOpen, updateRecordsToBeDeleted, onSaveRecord, onSaveBulkRecords } =
-  //   useApiClientContext();
+  // const toggleSelection = useCallback(() => {
+  //   setSelectedRecords(new Set());
+  //   setShowSelection((prev) => !prev);
+  //   setIsAllRecordsSelected(false);
+  // }, [setSelectedRecords, setShowSelection]);
 
-  // const [childParentMap] = useAPIRecords((state) => [state.childParentMap]);
-  // const bulkActionHandler = useCallback(
-  //   async (action: BulkActions) => {
-  //     if (isEmpty(selectedRecords) && action !== BulkActions.SELECT_ALL) {
-  //       toast.error("Please Select Records");
-  //       return;
-  //     }
-
-  //     const processedRecords = filterOutChildrenRecords(selectedRecords, childParentMap, updatedRecords.recordsMap);
-  //     switch (action) {
-  //       case BulkActions.DUPLICATE: {
-  //         const recordsToDuplicate = processRecordsForDuplication(processedRecords, apiClientRecordsRepository);
-
-  //         try {
-  //           const result = await apiClientRecordsRepository.duplicateApiEntities(recordsToDuplicate);
-
-  //           toast.success("Records Duplicated successfully");
-  //           result.length === 1 ? onSaveRecord(head(result)!, "open") : onSaveBulkRecords(result);
-  //         } catch (error) {
-  //           console.error("Error Duplicating records: ", error);
-  //           notification.error({
-  //             message: "Failed to duplicate some records",
-  //             description: error?.message,
-  //             placement: "bottomRight",
-  //           });
-  //           Sentry.withScope((scope) => {
-  //             scope.setTag("error_type", "api_client_record_duplication");
-  //             Sentry.captureException(error);
-  //           });
-  //         }
-
-  //         break;
-  //       }
-
-  //       case BulkActions.DELETE:
-  //         setIsDeleteModalOpen(true);
-  //         updateRecordsToBeDeleted(processedRecords);
-  //         break;
-
-  //       case BulkActions.EXPORT:
-  //         setIsExportModalOpen(true);
-  //         setCollectionsToExport(processedRecords);
-
-  //         break;
-
-  //       case BulkActions.MOVE:
-  //         setIsMoveCollectionModalOpen(true);
-  //         break;
-
-  //       case BulkActions.SELECT_ALL:
-  //         setIsAllRecordsSelected((prev) => !prev);
-  //         if (isAllRecordsSelected) {
-  //           setSelectedRecords(new Set());
-  //         } else {
-  //           const newSelectedRecords: Set<RQAPI.ApiClientRecord["id"]> = new Set();
-  //           updatedRecords.collections.forEach((record) => {
-  //             addNestedCollection(record, newSelectedRecords);
-  //           });
-  //           updatedRecords.requests.forEach((record) => {
-  //             newSelectedRecords.add(record.id);
-  //           });
-  //           setSelectedRecords(newSelectedRecords);
-  //         }
-  //         break;
-
-  //       default:
-  //         break;
-  //     }
-  //   },
-  //   [
-  //     selectedRecords,
-  //     childParentMap,
-  //     updatedRecords.recordsMap,
-  //     updatedRecords.collections,
-  //     updatedRecords.requests,
-  //     setIsDeleteModalOpen,
-  //     updateRecordsToBeDeleted,
-  //     isAllRecordsSelected,
-  //     apiClientRecordsRepository,
-  //     onSaveRecord,
-  //     onSaveBulkRecords,
-  //     addNestedCollection,
-  //   ]
-  // );
-
-  const toggleSelection = useCallback(() => {
-    setSelectedRecords(new Set());
-    setShowSelection((prev) => !prev);
-    setIsAllRecordsSelected(false);
-  }, [setSelectedRecords, setShowSelection]);
-
-  const multiSelectOptions = {
-    showMultiSelect: isValidPermission,
-    toggleMultiSelect: toggleSelection,
-  };
+  // const multiSelectOptions = {
+  //   showMultiSelect: isValidPermission,
+  //   toggleMultiSelect: toggleSelection,
+  // };
 
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="api-client-sidebar-header-container">
         <SidebarListHeader
           onSearch={setSearchValue}
-          multiSelectOptions={multiSelectOptions}
+          // multiSelectOptions={multiSelectOptions}
           newRecordActionOptions={{
             showNewRecordAction: isValidPermission,
             onNewRecordClick: onNewClick,
