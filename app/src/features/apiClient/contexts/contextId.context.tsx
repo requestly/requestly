7b2e2319fa905f602f4c01c781dc81e5c2a53b63
Rahul Ramteke <iostreamer007@gmail.com>
2025-08-11 14:03:57 +0530

    Added ContextId HoC, and made context hook use context_id

---
+++ app/src/features/apiClient/contexts/contextId.context.tsx
@@ -0,0 +1,15 @@
+import React, { createContext, useContext } from "react";
+
+const ContextIdContext = createContext<string | null>(null);
+
+export function ContextId(props: {
+  id: string,
+  children: React.ReactNode,
+}) {
+  return <ContextIdContext.Provider value={props.id}>{props.children}</ContextIdContext.Provider>
+}
+
+export function useContextId() {
+ const contextId = useContext(ContextIdContext);
+ return contextId;
+}
--- app/src/features/apiClient/contexts/meta.tsx
+++ app/src/features/apiClient/contexts/meta.tsx
@@ -1,22 +1,30 @@
-import { useContext, useMemo } from "react";
-import { ApiClientRepositoryInterface } from "../helpers/modules/sync/interfaces";
-import { useApiClientRepository } from "../helpers/modules/sync/useApiClientSyncRepo";
-import { AllApiClientStores, ApiRecordsStoreContext } from "../store/apiRecords/ApiRecordsContextProvider";
-import { NativeError } from "errors/NativeError";
+import { ApiClientViewMode, useApiClientMultiWorkspaceView } from "../store/multiWorkspaceView/multiWorkspaceView.store";
+import { useContextId } from "./contextId.context";
+import { ApiClientFeatureContext, useApiClientFeatureContextProvider } from "../store/apiClientFeatureContext/apiClientFeatureContext.store";
+import { useMemo } from "react";
 
-export type ApiClientFeatureContext = {
-  stores: AllApiClientStores;
-  repositories: ApiClientRepositoryInterface;
-};
 
 export function useApiClientFeatureContext(): ApiClientFeatureContext {
-  const stores = useContext(ApiRecordsStoreContext);
-  if (!stores) {
-    throw new NativeError("Command can't be called before stores are initialized");
+  const viewMode = useApiClientMultiWorkspaceView(s => s.viewMode);
+  const [getSingleViewContext, getContext] = useApiClientFeatureContextProvider(s => [s.getSingleViewContext, s.getContext]);
+  const contextId = useContextId();
+
+  const context = (() => {
+    if(viewMode === ApiClientViewMode.SINGLE) {
+      return getSingleViewContext();
+    }
+    if(!contextId) {
+      throw new Error("Mode is multi but no contextId found!");
+    }
+    return getContext(contextId);
+  })();
+
+  if(!context) {
+    throw new Error("No context found!");
   }
-  const repositories = useApiClientRepository();
+  
 
   return useMemo(() => {
-    return { stores, repositories };
-  }, [stores, repositories]);
+    return context;
+  }, [context]);
 }
