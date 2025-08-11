f4c3fb5c82c0c046ab3ac065bf7e63fa5b823962
Parth Bhardwaj <parthb.git@gmail.com>
2025-08-11 20:14:05 +0530

    fix: Introspection UX

--- app/src/features/apiClient/screens/apiClient/components/views/components/request/components/ApiClientUrl/ApiClientUrl.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/request/components/ApiClientUrl/ApiClientUrl.tsx
@@ -1,5 +1,4 @@
 import { ScopedVariables } from "features/apiClient/helpers/variableResolver/variable-resolver";
-import { DEMO_HTTP_API_URL } from "features/apiClient/constants";
 import SingleLineEditor from "features/apiClient/screens/environment/components/SingleLineEditor";
 import { useCallback } from "react";
 import "./apiClientUrl.css";
@@ -22,7 +21,7 @@ export const ApiClientUrl = ({ url, currentEnvironmentVariables, onEnterPress, o
   return (
     <SingleLineEditor
       className="api-request-url"
-      placeholder={DEMO_HTTP_API_URL}
+      placeholder="Enter or paste GraphQL endpoint URL"
       //value={entry.request.url}
       defaultValue={url}
       onChange={(value) => {
