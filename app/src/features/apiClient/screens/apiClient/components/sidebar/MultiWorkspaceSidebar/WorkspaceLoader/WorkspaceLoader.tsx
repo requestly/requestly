8a77c3b883908d71efbf44fc3e8ee3ca71b44e72
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-11 20:33:17 +0530

    add loader

---
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/MultiWorkspaceSidebar/WorkspaceLoader/WorkspaceLoader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Skeleton } from "antd";
+import { useContextId } from "features/apiClient/contexts/contextId.context";
+import { useWorkspace } from "features/apiClient/store/multiWorkspaceView/multiWorkspaceView.store";
+import "./workspaceLoader.scss";
+
+export const WorkspaceLoader: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const contextId = useContextId();
+  const state = useWorkspace(contextId, (s) => s.state);
+
+  if (state.loading) {
+    return (
+      <div className="workspace-loader-container">
+        <Skeleton paragraph={{ rows: 6 }} title={false} />
+      </div>
+    );
+  }
+
+  return children;
+};
