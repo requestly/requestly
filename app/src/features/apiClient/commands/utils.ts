e4fc42b75a90ddc22eacc5ad32c0424f7e0fa922
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-14 23:28:32 +0530

    update utils

--- app/src/features/apiClient/commands/utils.ts
+++ app/src/features/apiClient/commands/utils.ts
@@ -1,4 +1,12 @@
 import { EnvironmentVariables, EnvironmentVariableType } from "backend/environment/types";
+import { RQAPI } from "../types";
+import { apiClientFeatureContextProviderStore } from "../store/apiClientFeatureContext/apiClientFeatureContext.store";
+import {
+  isApiRequest,
+  isApiCollection,
+  filterRecordsBySearch,
+  convertFlatRecordsToNestedRecords,
+} from "../screens/apiClient/utils";
 
 export function sanitizePatch(patch: EnvironmentVariables) {
   return Object.fromEntries(
@@ -13,4 +21,111 @@ export function sanitizePatch(patch: EnvironmentVariables) {
       ];
     })
   );
+}
+
+export function addNestedCollection(
+  record: RQAPI.CollectionRecord,
+  newSelectedRecords: Set<RQAPI.ApiClientRecord["id"]>
+) {
+  newSelectedRecords.add(record.id);
+  if (record.data.children) {
+    record.data.children.forEach((child) => {
+      if (child.type === "collection") {
+        addNestedCollection(child, newSelectedRecords);
+      } else {
+        newSelectedRecords.add(child.id);
+      }
+    });
+  }
+}
+
+export function getChildParentMap(contextId: string) {
+  const currentContext = apiClientFeatureContextProviderStore.getState().getContext(contextId);
+  return currentContext.stores.records.getState().childParentMap;
+}
+
+export function prepareRecordsToRender(records: RQAPI.ApiClientRecord[]) {
+  const { updatedRecords, recordsMap } = convertFlatRecordsToNestedRecords(records);
+
+  updatedRecords.sort((recordA, recordB) => {
+    // If different type, then keep collection first
+    if (recordA.type === RQAPI.RecordType.COLLECTION && recordA.isExample) {
+      return -1;
+    }
+
+    if (recordB.type === RQAPI.RecordType.COLLECTION && recordB.isExample) {
+      return -1;
+    }
+
+    if (recordA.type !== recordB.type) {
+      return recordA.type === RQAPI.RecordType.COLLECTION ? -1 : 1;
+    }
+
+    // If types are the same, sort lexicographically by name
+    if (recordA.name.toLowerCase() !== recordB.name.toLowerCase()) {
+      return recordA.name.toLowerCase() < recordB.name.toLowerCase() ? -1 : 1;
+    }
+
+    // If names are the same, sort by creation date
+    return recordA.createdTs - recordB.createdTs;
+  });
+
+  return {
+    count: updatedRecords.length,
+    collections: updatedRecords.filter((record) => isApiCollection(record)) as RQAPI.CollectionRecord[],
+    requests: updatedRecords.filter((record) => isApiRequest(record)) as RQAPI.ApiRecord[],
+    recordsMap: recordsMap,
+  };
+}
+
+export function getUpdatedRecords(params: {
+  contextId: string;
+  apiClientRecords: RQAPI.ApiClientRecord[];
+  searchValue: string;
+}) {
+  const { contextId, apiClientRecords, searchValue } = params;
+  const childParentMap = getChildParentMap(contextId);
+
+  const filteredRecords = filterRecordsBySearch(apiClientRecords, searchValue);
+  const recordsToRender = prepareRecordsToRender(filteredRecords);
+
+  if (searchValue) {
+    const recordsToExpand: string[] = [];
+    filteredRecords.forEach((record) => {
+      if (record.collectionId) {
+        recordsToExpand.push(record.collectionId);
+        let parentId = childParentMap.get(record.collectionId);
+        while (parentId) {
+          recordsToExpand.push(parentId);
+          parentId = childParentMap.get(parentId);
+        }
+      }
+    });
+
+    return recordsToRender;
+  }
+}
+
+export function selectAllRecords(params: { contextId: string; searchValue: string }) {
+  const { contextId, searchValue } = params;
+
+  const newSelectedRecords: Set<RQAPI.ApiClientRecord["id"]> = new Set();
+
+  const context = apiClientFeatureContextProviderStore.getState().getContext(contextId);
+  const apiClientRecords = context.stores.records.getState().apiClientRecords;
+  const updatedRecords = getUpdatedRecords({
+    contextId,
+    apiClientRecords,
+    searchValue,
+  });
+
+  updatedRecords.collections.forEach((record) => {
+    addNestedCollection(record, newSelectedRecords);
+  });
+
+  updatedRecords.requests.forEach((record) => {
+    newSelectedRecords.add(record.id);
+  });
+
+  return newSelectedRecords;
 }
