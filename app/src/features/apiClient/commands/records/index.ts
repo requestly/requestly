a5738f8a42382ad618c2670b5afdbe77876ce2e1
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-15 02:30:11 +0530

    move duplicateRecords command

--- app/src/features/apiClient/commands/records/index.ts
+++ app/src/features/apiClient/commands/records/index.ts
@@ -1,3 +1,5 @@
 export { forceRefreshRecords } from "./force-refresh.command";
 export { patchCollectionVariables } from "./patch-collection-variables.command";
 export { setCollectionVariables } from "./setCollectionVariables.command";
+export { duplicateRecords } from "./duplicateRecords.command";
+export { moveRecords } from "./moveRecords.command";
