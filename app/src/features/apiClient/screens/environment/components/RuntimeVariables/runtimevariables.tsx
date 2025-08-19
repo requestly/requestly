c4f3bfa46ff0fca648287a0d07b635698f1222cd
Aarushsr12 <aarushsrivastava1212@gmail.com>
2025-08-19 12:16:17 +0530

    fix: pr feedback

--- app/src/features/apiClient/screens/environment/components/RuntimeVariables/runtimevariables.tsx
+++ app/src/features/apiClient/screens/environment/components/RuntimeVariables/runtimevariables.tsx
@@ -1,17 +1,16 @@
 import { useTabServiceWithSelector } from "componentsV2/Tabs/store/tabServiceStore";
-import React from "react";
+import React, { useEffect } from "react";
 import { RuntimeVariablesViewTabSource } from "./runtimevariablesTabSource";
 import "./runtimevariables.scss";
 
 export const RuntimeVariables: React.FC = () => {
   const [openTab] = useTabServiceWithSelector((state) => [state.openTab]);
+  useEffect(() => {
+    openTab(new RuntimeVariablesViewTabSource());
+  }, [openTab]);
+
   return (
-    <div
-      className="runtime-variables-container"
-      onClick={() => {
-        openTab(new RuntimeVariablesViewTabSource());
-      }}
-    >
+    <div className="runtime-variables-container">
       <div className="runtime-variables-text-placeholder">
         <span className="text">Runtime variables</span>
       </div>
