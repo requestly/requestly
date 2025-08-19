1db19377aedeb4db8bf76329a03d362a7d05c9ab
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-19 13:46:59 +0530

    fix: workspace context

--- app/src/features/apiClient/screens/apiClient/components/sidebar/MultiWorkspaceSidebar/ContextualEnvironmentsSidebar/ContextualEnvironmentsSidebar.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/MultiWorkspaceSidebar/ContextualEnvironmentsSidebar/ContextualEnvironmentsSidebar.tsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import { SidebarListHeader } from "../../components/sidebarListHeader/SidebarListHeader";
 import { useApiClientMultiWorkspaceView } from "features/apiClient/store/multiWorkspaceView/multiWorkspaceView.store";
-import { ContextId } from "features/apiClient/contexts/contextId.context";
-import { WorkspaceLoader } from "../WorkspaceLoader/WorkspaceLoader";
-import { useApiClientFeatureContextProvider } from "features/apiClient/store/apiClientFeatureContext/apiClientFeatureContext.store";
+import { WorkspaceProvider } from "../WorkspaceProvider/WorkspaceProvider";
 import { ContextualEnvironmentsList } from "./ContextualEnvironmentsList/ContextualEnvironmentsList";
 
 export const ContextualEnvironmentsSidebar: React.FC = () => {
   const [searchValue, setSearchValue] = useState("");
   const selectedWorkspaces = useApiClientMultiWorkspaceView((s) => s.selectedWorkspaces);
-  const getContext = useApiClientFeatureContextProvider((s) => s.getContext);
 
   return (
     <div className="multiview-environments-sidebar-container">
@@ -25,12 +22,10 @@ export const ContextualEnvironmentsSidebar: React.FC = () => {
         const workspaceId = workspace.getState().id;
 
         return (
-          <WorkspaceLoader key={workspaceId} workspaceId={workspaceId}>
-            <ContextId id={getContext(workspaceId)?.id}>
-              <h3>Workspace: {workspace.getState().name}</h3>
-              <ContextualEnvironmentsList searchValue={searchValue} />
-            </ContextId>
-          </WorkspaceLoader>
+          <WorkspaceProvider key={workspaceId} workspaceId={workspaceId}>
+            <h3>Workspace: {workspace.getState().name}</h3>
+            <ContextualEnvironmentsList searchValue={searchValue} />
+          </WorkspaceProvider>
         );
       })}
     </div>
---
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/MultiWorkspaceSidebar/WorkspaceProvider/WorkspaceProvider.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Skeleton } from "antd";
+import { useWorkspace } from "features/apiClient/store/multiWorkspaceView/multiWorkspaceView.store";
+import "./workspaceProvider.scss";
+import { ContextId } from "features/apiClient/contexts/contextId.context";
+import { useApiClientFeatureContextProvider } from "features/apiClient/store/apiClientFeatureContext/apiClientFeatureContext.store";
+
+export const WorkspaceProvider: React.FC<{ workspaceId: string; children: React.ReactNode }> = ({
+  workspaceId,
+  children,
+}) => {
+  const state = useWorkspace(workspaceId, (s) => s.state);
+  const [getContext] = useApiClientFeatureContextProvider((s) => [s.getContext]);
+
+  if (state.errored) {
+    // TBD
+  }
+
+  if (state.loading) {
+    return (
+      <div className="workspace-loader-container">
+        <Skeleton paragraph={{ rows: 6 }} title={false} />
+      </div>
+    );
+  }
+
+  return <ContextId id={getContext(workspaceId)?.id}>{children}</ContextId>;
+};
