db435a1a507ce684e221abd0439dfb51aaabeefa
nafees87n <nafees87n@gmail.com>
2025-08-20 00:29:48 +0530

    chore: created extension constants in shared module

---
+++ shared/src/constants/extension.ts
@@ -0,0 +1,9 @@
+export namespace ExtensionConstants {
+  export enum StorageKeys {
+    POPUP_CONFIG = "popup_config",
+  }
+
+  export enum ExtensionMessages {
+    OPEN_CURL_IMPORT_MODAL = "openCurlImportModal",
+  }
+}
