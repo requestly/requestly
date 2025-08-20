7580345e2b8d2f6fdc0a8a74f96b323f395c360e
nafees87n <nafees87n@gmail.com>
2025-08-20 14:29:57 +0530

    [ENGG-4128] integrated refactored fetcher with existing code

--- app/src/hooks/workspaceFetcher/useFetchLocalWorkspaces.ts
+++ app/src/hooks/workspaceFetcher/useFetchLocalWorkspaces.ts
@@ -1,20 +1,29 @@
 import APP_CONSTANTS from "config/constants";
 import { useCheckLocalSyncSupport } from "features/apiClient/helpers/modules/sync/useCheckLocalSyncSupport";
+import { WorkspaceMemberRole } from "features/workspaces/types";
 import { useCallback, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllWorkspaces } from "services/fsManagerServiceAdapter";
+import { getUserAuthDetails } from "store/slices/global/user/selectors";
 import { workspaceActions } from "store/slices/workspaces/slice";
 import { WorkspaceType } from "types";
 import { submitAttrUtil } from "utils/AnalyticsUtils";
 
 export const useFetchLocalWorkspaces = () => {
   const dispatch = useDispatch();
+  const user = useSelector(getUserAuthDetails);
   const isLocalSyncEnabled = useCheckLocalSyncSupport({ skipWorkspaceCheck: true });
 
   const fetchLocalWorkspaces = useCallback(async () => {
     if (!isLocalSyncEnabled) {
       return;
     }
+
+    if (!user.loggedIn) {
+      return;
+    }
+
+    const uid = user?.details?.profile?.uid;
 
     const allLocalWorkspacesResult = await getAllWorkspaces();
     const allLocalWorkspaces = allLocalWorkspacesResult.type === "success" ? allLocalWorkspacesResult.content : [];
@@ -25,14 +34,14 @@ export const useFetchLocalWorkspaces = () => {
       const localWorkspace = {
         id: partialWorkspace.id,
         name: partialWorkspace.name,
-        // owner: uid,
+        owner: uid,
         accessCount: 1,
         adminCount: 1,
-        // members: {
-        //   [uid]: {
-        //     role: "admin",
-        //   },
-        // },
+        members: {
+          [uid]: {
+            role: WorkspaceMemberRole.admin,
+          },
+        },
         // appsumo: null,
         workspaceType: WorkspaceType.LOCAL,
         rootPath: partialWorkspace.path,
@@ -42,7 +51,7 @@ export const useFetchLocalWorkspaces = () => {
     }
     dispatch(workspaceActions.upsertManyWorkspaces(localRecords));
     submitAttrUtil(APP_CONSTANTS.GA_EVENTS.ATTR.NUM_LOCAL_WORKSPACES, localRecords.length);
-  }, [dispatch, isLocalSyncEnabled]);
+  }, [dispatch, isLocalSyncEnabled, user?.details?.profile?.uid, user.loggedIn]);
 
   useEffect(() => {
     fetchLocalWorkspaces();
