dab343c2de8942e318135fb5cf8cbe17bff45c8a
Sahil Gupta <wrongsahil@gmail.com>
2025-08-21 15:23:12 +0530

    chore: remove deprecated teams slice

--- app/src/store/slices/workspaces/slice.ts
+++ app/src/store/slices/workspaces/slice.ts
@@ -17,33 +17,10 @@ export const workspacesEntityAdapter = createEntityAdapter<Workspace>({
   },
 });
 
-const getTeamSliceCurrentWorkspaceId = () => {
-  const teamsSlice = window.localStorage.getItem("persist:teams");
-  if (teamsSlice) {
-    try {
-      const teamsSliceObj = JSON.parse(teamsSlice);
-      const teamsSliceCurrentlyActiveWorkspace = teamsSliceObj?.currentlyActiveWorkspace
-        ? JSON.parse(teamsSliceObj?.currentlyActiveWorkspace)
-        : {};
-      // console.log("Migrated Successfully", { teamsSliceObj, teamsSliceCurrentlyActiveWorkspace });
-
-      // For backward compatibility in case of any issues. Uncomment when everything is working fine along with all the reducers usage of teams/slice.ts
-      // window.localStorage.removeItem("persist:teams");
-      return teamsSliceCurrentlyActiveWorkspace?.id ? [teamsSliceCurrentlyActiveWorkspace?.id] : [];
-    } catch (e) {
-      console.log("Error while migration teams slice to workspaces slice");
-      return [];
-    }
-  } else {
-    // console.log("Already Migrated or New User");
-    return [];
-  }
-};
-
 const initialState: WorkspaceSliceState = {
   allWorkspaces: workspacesEntityAdapter.getInitialState(),
   workspacesUpdatedAt: 0,
-  activeWorkspaceIds: getTeamSliceCurrentWorkspaceId(),
+  activeWorkspaceIds: [],
   activeWorkspacesMembers: {},
 };
 
