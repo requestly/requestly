4109a0ee3d2abb9c6185bb8c1750af6360ac55db
Nafees Nehar <nafees87n@gmail.com>
2025-08-21 16:56:09 +0530

    fix: add support to resolve url before introspection query (#3433)

--- app/src/features/apiClient/store/apiRecord/graphqlRecord/GraphQLRecordContextProvider.tsx
+++ app/src/features/apiClient/store/apiRecord/graphqlRecord/GraphQLRecordContextProvider.tsx
@@ -5,7 +5,15 @@ import { RQAPI } from "features/apiClient/types";
 
 export const GraphQLRecordStoreContext = createContext<StoreApi<GraphQLRecordState> | null>(null);
 
-export const GraphQLRecordProvider = ({ children, entry }: { children: ReactNode; entry: RQAPI.GraphQLApiEntry }) => {
-  const [store] = useState(() => createGraphQLRecordStore(entry));
+export const GraphQLRecordProvider = ({
+  children,
+  entry,
+  recordId,
+}: {
+  children: ReactNode;
+  entry: RQAPI.GraphQLApiEntry;
+  recordId: RQAPI.ApiRecord["id"];
+}) => {
+  const [store] = useState(() => createGraphQLRecordStore(entry, recordId));
   return <GraphQLRecordStoreContext.Provider value={store}>{children}</GraphQLRecordStoreContext.Provider>;
 };
