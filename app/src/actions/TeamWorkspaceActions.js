f7ebb856e00a595376c277ca14d0b082683a7ae5
Rahul Ramteke <iostreamer007@gmail.com>
2025-08-21 15:21:27 +0530

    Fixed bug which revived tabs through url

--- app/src/actions/TeamWorkspaceActions.js
+++ app/src/actions/TeamWorkspaceActions.js
@@ -88,7 +88,7 @@ export const switchWorkspace = async (
     await StorageService(appMode).clearDB();
   }
 
-  getTabServiceActions().resetTabs();
+  getTabServiceActions().resetTabs(true);
 
   // Just in case
   window.skipSyncListenerForNextOneTime = false;
