f46871ea0ffd67fd5ffcbf713af2a6cd9ca7913b
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-25 21:06:24 +0530

    add `MultiViewImportModalWorkspaceSelection`

---
+++ app/src/features/apiClient/screens/apiClient/components/modals/common/MultiViewImportModalWorkspaceSelection/MultiViewImportModalWorkspaceSelection.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback } from "react";
+import { Alert } from "antd";
+import { useImportModalContext } from "../../../sidebar/components/apiClientSidebarHeader/contexts/importModal.context";
+import { MoveIntoWorkspaceSelect } from "../../MoveToCollectionModal/MoveToCollectionModal";
+import { RQButton } from "lib/design-system-v2/components";
+import "./multiViewImportModalWorkspaceSelection.scss";
+
+interface MultiViewImportModalWorkspaceSelectionProps {
+  isImporting: boolean;
+  onClose: () => void;
+  onImportClick: () => void;
+}
+
+export const MultiViewImportModalWorkspaceSelection: React.FC<MultiViewImportModalWorkspaceSelectionProps> = ({
+  isImporting,
+  onClose,
+  onImportClick,
+}) => {
+  const { setImportModalContext } = useImportModalContext();
+
+  const handleWorkspaceSelect = useCallback(
+    (w: { label: string; value: string }) => {
+      setImportModalContext(w.value);
+    },
+    [setImportModalContext]
+  );
+
+  return (
+    <div className="multi-view-import-modal-success-container">
+      <Alert showIcon type="success" message="Data has been parsed and is ready to import" />
+
+      <label>
+        <div>Select a workspace to import the data</div>
+        <MoveIntoWorkspaceSelect onWorkspaceSelect={handleWorkspaceSelect} />
+      </label>
+
+      <div className="actions">
+        <RQButton onClick={onClose} loading={isImporting}>
+          Cancel
+        </RQButton>
+        <RQButton type="primary" onClick={onImportClick} loading={isImporting}>
+          Import
+        </RQButton>
+      </div>
+    </div>
+  );
+};
