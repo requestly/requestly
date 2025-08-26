dd0bfe8acb29282151c6931a7aa2a5646775bb1b
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-26 14:43:55 +0530

    fix: delete modal messaging

--- app/src/features/apiClient/screens/apiClient/components/modals/deleteApiRecordModal/DeleteApiRecordModal.tsx
+++ app/src/features/apiClient/screens/apiClient/components/modals/deleteApiRecordModal/DeleteApiRecordModal.tsx
@@ -8,6 +8,9 @@ import { isEmpty } from "lodash";
 import { useTabServiceWithSelector } from "componentsV2/Tabs/store/tabServiceStore";
 import { ApiClientFeatureContext } from "features/apiClient/store/apiClientFeatureContext/apiClientFeatureContext.store";
 import { deleteRecords } from "features/apiClient/commands/records";
+import { toast } from "utils/Toast";
+import { isApiCollection } from "../../../utils";
+import * as Sentry from "@sentry/react";
 
 interface DeleteApiRecordModalProps {
   open: boolean;
@@ -27,9 +30,13 @@ export const DeleteApiRecordModal: React.FC<DeleteApiRecordModalProps> = ({
 }) => {
   const closeTabBySource = useTabServiceWithSelector((state) => state.closeTabBySource);
 
-  const records = useMemo(() => {
+  const recordsWithContext = useMemo(() => {
     return getRecordsToDelete();
   }, [getRecordsToDelete]);
+
+  const records = useMemo(() => {
+    return recordsWithContext.map(({ records }) => records).flat();
+  }, [recordsWithContext]);
 
   const [isDeleting, setIsDeleting] = useState(false);
 
@@ -37,12 +44,14 @@ export const DeleteApiRecordModal: React.FC<DeleteApiRecordModalProps> = ({
     return null;
   }
 
-  // let apiRequestCount = records.length === 1 ? (isApiCollection(records[0]) ? records[0].data.children.length : 1) : "";
+  let apiRequestCount = records.length === 1 ? (isApiCollection(records[0]) ? records[0].data.children.length : 1) : "";
 
   const handleDeleteApiRecord = async () => {
     try {
-      records.forEach(async ({ context, records }) => {
-        const { deletedApiRecords, deletedCollectionRecords } = await deleteRecords(context, { records });
+      recordsWithContext.forEach(async ({ context, records }) => {
+        const { deletedApiRecords, deletedCollectionRecords } = await deleteRecords(context, {
+          records,
+        });
 
         const isExampleCollection = deletedCollectionRecords.some((record) => !!record.isExample);
         trackCollectionDeleted(isExampleCollection ? "example" : "");
@@ -58,56 +67,45 @@ export const DeleteApiRecordModal: React.FC<DeleteApiRecordModalProps> = ({
 
       // First delete records
 
-      // toast.success(
-      //   records.length === 1
-      //     ? records[0].type === RQAPI.RecordType.API
-      //       ? "API request deleted"
-      //       : "Collection deleted"
-      //     : "Records Deleted"
-      // );
+      toast.success(
+        records.length === 1
+          ? records[0].type === RQAPI.RecordType.API
+            ? "API request deleted"
+            : "Collection deleted"
+          : "Records Deleted"
+      );
       onClose();
       onSuccess?.();
-
-      // TODO: add analytics
     } catch (error) {
-      console.log(error);
       // const erroredResult = !recordDeletionResult.success ? recordDeletionResult : collectionsDeletionResult;
       // notification.error({
       //   message: `Error deleting ${records.length === 1 ? "record" : "records"}`,
       //   description: erroredResult?.message,
       //   placement: "bottomRight",
       // });
-      // Sentry.withScope((scope) => {
-      //   scope.setTag("error_type", "api_client_record_deletion");
-      //   Sentry.captureException(
-      //     erroredResult.message || `Error deleting ${records.length === 1 ? "record" : "records"}`
-      //   );
-      // });
+
+      Sentry.withScope((scope) => {
+        scope.setTag("error_type", "api_client_record_deletion");
+        Sentry.captureException(`Error deleting ${records.length === 1 ? "record" : "records"}`);
+      });
     } finally {
       setIsDeleting(false);
     }
   };
 
-  // const header =
-  //   records.length === 1
-  //     ? records[0].type === RQAPI.RecordType.API
-  //       ? "Delete API Request"
-  //       : "Delete Collection"
-  //     : "Delete Records";
+  const header =
+    records.length === 1
+      ? records[0].type === RQAPI.RecordType.API
+        ? "Delete API Request"
+        : "Delete Collection"
+      : "Delete Records";
 
-  const header = "Delete Records";
-  //   records.length === 1
-  //     ? records[0].type === RQAPI.RecordType.API
-  //       ? "Delete API Request"
-  //       : "Delete Collection"
-  //     : "Delete Records";
-
-  const description = " This action will permanently delete this API request. Are you sure you want to continue?";
-  // records.length === 1
-  //   ? records[0].records.type === RQAPI.RecordType.API
-  //     ? `This action will permanently delete this API request. Are you sure you want to continue?`
-  //     : `This action will permanently delete the entire collection and its ${apiRequestCount} requests. Are you sure you want to continue?`
-  //   : "This action will permanently delete the selected Collections, APIs, and their associated requests. Are you sure you want to proceed?";
+  const description =
+    records.length === 1
+      ? records[0].type === RQAPI.RecordType.API
+        ? `This action will permanently delete this API request. Are you sure you want to continue?`
+        : `This action will permanently delete the entire collection and its ${apiRequestCount} requests. Are you sure you want to continue?`
+      : "This action will permanently delete the selected Collections, APIs, and their associated requests. Are you sure you want to proceed?";
 
   return (
     <RQModal
@@ -127,12 +125,11 @@ export const DeleteApiRecordModal: React.FC<DeleteApiRecordModalProps> = ({
           Cancel
         </RQButton>
         <RQButton block type="danger" loading={isDeleting} onClick={handleDeleteApiRecord}>
-          {/* {records.length === 1
+          {records.length === 1
             ? records[0].type === RQAPI.RecordType.API
               ? "Delete API"
               : "Delete collection"
-            : "Delete Records"} */}
-          Delete Records
+            : "Delete Records"}
         </RQButton>
       </div>
     </RQModal>
