f216fd79076caed072740f2246a0eff7748c226d
Parth Bhardwaj <parthb.git@gmail.com>
2025-08-29 16:12:02 +0530

    fix: tooltip

--- app/src/componentsV2/modals/CreateWorkspaceModal/components/CreateWorkspaceFooter/CreateWorkspaceFooter.tsx
+++ app/src/componentsV2/modals/CreateWorkspaceModal/components/CreateWorkspaceFooter/CreateWorkspaceFooter.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { RQButton } from "lib/design-system-v2/components";
-import "./createWorkspaceFooter.scss";
 import { Tooltip } from "antd";
+import "./createWorkspaceFooter.scss";
 
 interface CreateWorkspaceFooterProps {
   onCancel: () => void;
@@ -15,16 +16,31 @@ export const CreateWorkspaceFooter = ({
   isLoading,
   disabled,
 }: CreateWorkspaceFooterProps) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
   return (
     <>
       <div className="create-workspace-footer">
         <RQButton onClick={onCancel}>Cancel</RQButton>
+        {/* Tooltip does not work when button is disabled, so we need to use onMouseOver and onMouseLeave to show/hide the tooltip */}
         <Tooltip
-          title={disabled ? "Please complete all required fields to continue." : null}
+          open={isTooltipVisible}
+          title={"Please complete all required fields to continue."}
           color="#000"
           placement="top"
         >
-          <RQButton type="primary" onClick={onCreateWorkspaceClick} loading={isLoading} disabled={disabled}>
+          <RQButton
+            type="primary"
+            onClick={onCreateWorkspaceClick}
+            loading={isLoading}
+            disabled={disabled}
+            onMouseOver={() => {
+              if (disabled) setIsTooltipVisible(true);
+            }}
+            onMouseLeave={() => {
+              if (disabled) setIsTooltipVisible(false);
+            }}
+          >
             Create workspace
           </RQButton>
         </Tooltip>
