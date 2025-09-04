f907cd4c2f5b2ab96fe213480fdcc7aeaec534a9
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-09-04 15:24:26 +0530

    [ENGG-3728]: Update app shell UI and color tokens (#3477)

--- app/src/features/apiClient/contexts/contextId.context.tsx
+++ app/src/features/apiClient/contexts/contextId.context.tsx
@@ -2,14 +2,11 @@ import React, { createContext, useContext } from "react";
 
 const ContextIdContext = createContext<string | null>(null);
 
-export function ContextId(props: {
-  id: string,
-  children: React.ReactNode,
-}) {
-  return <ContextIdContext.Provider value={props.id}>{props.children}</ContextIdContext.Provider>
+export function ContextId(props: { id: string; children: React.ReactNode }) {
+  return <ContextIdContext.Provider value={props.id}>{props.children}</ContextIdContext.Provider>;
 }
 
 export function useContextId() {
- const contextId = useContext(ContextIdContext);
- return contextId;
+  const contextId = useContext(ContextIdContext);
+  return contextId;
 }
