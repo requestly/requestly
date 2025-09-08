f8ef4d84a6f10a8aab7db1069a657970b28092df
rohanmathur91 <mathurrohan04@gmail.com>
2025-09-08 18:45:18 +0530

    cleanup

--- app/src/features/apiClient/commands/multiView/index.ts
+++ app/src/features/apiClient/commands/multiView/index.ts
@@ -2,4 +2,3 @@ export { addWorkspaceToView } from "./addWorkspaceToView.command";
 export { removeWorkspaceFromView } from "./removeWorkspaceFromView.command";
 export { resetToSingleView } from "./resetToSingleView.command";
 export { markWorkspaceLoaded } from "./markWorkspaceLoaded.command";
-export { clearStaleWorkspacesOnAuthChange } from "./clearStaleWorkspacesOnAuthChange.command";
