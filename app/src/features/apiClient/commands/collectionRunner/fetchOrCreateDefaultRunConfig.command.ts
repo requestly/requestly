9b8bfc60f051c4946ecb16652c1bebca8665eba7
rohanmathur91 <mathurrohan04@gmail.com>
2025-09-11 14:08:28 +0530

    add `fetchOrCreateDefaultRunConfig`

---
+++ app/src/features/apiClient/commands/collectionRunner/fetchOrCreateDefaultRunConfig.command.ts
@@ -0,0 +1,45 @@
+import { ApiClientFeatureContext } from "features/apiClient/store/apiClientFeatureContext/apiClientFeatureContext.store";
+import { getAllChildren } from "features/apiClient/store/apiRecords/apiRecords.store";
+import { RQAPI } from "features/apiClient/types";
+import { getChildParentMap } from "../store.utils";
+import { RunConfigState } from "features/apiClient/store/collectionRunConfig/runConfig.store";
+import { NativeError } from "errors/NativeError";
+
+function getDefaultRunOrderByCollectionId(
+  ctx: ApiClientFeatureContext,
+  id: RQAPI.ApiClientRecord["collectionId"]
+): RunConfigState["runOrder"] {
+  const childParentMap = getChildParentMap(ctx);
+  const runOrder = getAllChildren(id, childParentMap);
+  return runOrder;
+}
+
+export async function fetchOrCreateDefaultRunConfig(
+  ctx: ApiClientFeatureContext,
+  params: { collectionId: RQAPI.ApiClientRecord["collectionId"] }
+): Promise<Partial<RQAPI.RunConfig>> {
+  const DEFAULT_RUN_CONFIG_ID = "default"; // In future configId will be provided
+
+  const { collectionId } = params;
+  const { apiClientRecordsRepository } = ctx.repositories;
+
+  const result = await apiClientRecordsRepository.getRunConfig(collectionId, DEFAULT_RUN_CONFIG_ID);
+
+  if (result.success) {
+    return result.data;
+  }
+
+  // create deafult config
+  const defaultConfig: Partial<RQAPI.RunConfig> = {
+    id: DEFAULT_RUN_CONFIG_ID,
+    runOrder: getDefaultRunOrderByCollectionId(ctx, collectionId),
+  };
+
+  const upsertResult = await apiClientRecordsRepository.upsertRunConfig(collectionId, defaultConfig);
+
+  if (!upsertResult.success) {
+    throw new NativeError("Something went wrong while fetching run config!").addContext({ upsertResult });
+  }
+
+  return upsertResult.data;
+}
---
+++ app/src/features/apiClient/commands/collectionRunner/index.ts
@@ -0,0 +1,1 @@
+export { fetchOrCreateDefaultRunConfig } from "./fetchOrCreateDefaultRunConfig.command";
