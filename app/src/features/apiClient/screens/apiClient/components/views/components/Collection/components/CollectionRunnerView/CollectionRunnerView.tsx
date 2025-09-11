c51d266973a430512045324bbe168f7a25035179
rohanmathur91 <mathurrohan04@gmail.com>
2025-09-12 00:18:14 +0530

    add `CollectionRunnerView`

--- app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/CollectionRunnerView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/CollectionRunnerView.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { RunConfigViewManager } from "./components/RunConfigViewManager";
 import { RQAPI } from "features/apiClient/types";
+// import { BottomSheetLayout, BottomSheetProvider } from "componentsV2/BottomSheet";
+// import { BottomSheetPlacement, SheetLayout } from "componentsV2/BottomSheet/types";
+import { CollectionRunnerViewContextProvider } from "./collectionRunnerView.context";
+import "./collectionRunnerView.scss";
 
 interface Props {
   collectionId: RQAPI.CollectionRecord["id"];
@@ -8,10 +12,16 @@ interface Props {
 
 export const CollectionRunnerView: React.FC<Props> = ({ collectionId }) => {
   return (
-    <div className="collection-runner-view">
-      {/* TODO: should be resizable */}
-      <RunConfigViewManager collectionId={collectionId} />
-      {/* RunResults will be here */}
-    </div>
+    <CollectionRunnerViewContextProvider collectionId={collectionId}>
+      <div className="collection-runner-view">
+        {/* <BottomSheetProvider defaultPlacement={BottomSheetPlacement.RIGHT}>
+        <BottomSheetLayout minSize={350} initialSizes={[50, 50]} layout={SheetLayout.SPLIT} bottomSheet={<></>}>
+          <RunConfigViewManager collectionId={collectionId} />
+        </BottomSheetLayout>
+      </BottomSheetProvider> */}
+
+        <RunConfigViewManager />
+      </div>
+    </CollectionRunnerViewContextProvider>
   );
 };
