6407710111064faa6d8e0b67f2a7cde69df241b7
rohanmathur91 <mathurrohan04@gmail.com>
2025-09-12 21:19:06 +0530

    add `RunConfigSettings`

--- app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunConfigView/components/RunConfigContainer/RunConfigContainer.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunConfigView/components/RunConfigContainer/RunConfigContainer.tsx
@@ -1,13 +1,60 @@
 import React, { useState } from "react";
 import { RunConfigOrderedRequests } from "../RunConfigOrderedRequests/RunConfigOrderedRequests";
-import { Checkbox } from "antd";
+import { Checkbox, InputNumber } from "antd";
 import { CheckboxChangeEvent } from "antd/lib/checkbox";
-import { RQButton } from "lib/design-system-v2/components";
+import { RQButton, RQTooltip } from "lib/design-system-v2/components";
 import { MdOutlineRestartAlt } from "@react-icons/all-files/md/MdOutlineRestartAlt";
 import "./runConfigContainer.scss";
+import { MdOutlineInfo } from "@react-icons/all-files/md/MdOutlineInfo";
 
-const RunConfiguration: React.FC = () => {
-  return <></>;
+const RunConfigSettings: React.FC = () => {
+  const [iterations, setIterations] = useState(1);
+  const [delay, setDelay] = useState(0);
+
+  const handleIterationsChange = (value: number) => {
+    setIterations(value);
+  };
+
+  const handleDelayChange = (value: number) => {
+    setDelay(value);
+  };
+
+  return (
+    <div className="run-config-settings">
+      <div className="base-settings">
+        <div className="setting-container">
+          <label htmlFor="run-iterations">
+            Iterations
+            <RQTooltip>
+              <MdOutlineInfo />
+            </RQTooltip>
+          </label>
+          {/* TODO: limit TBD */}
+          <InputNumber
+            min={1}
+            max={10}
+            value={iterations}
+            size="small"
+            name="run-iterations"
+            onChange={handleIterationsChange}
+          />
+        </div>
+
+        <div className="setting-container">
+          <label htmlFor="run-delay">
+            Delay
+            <RQTooltip>
+              <MdOutlineInfo />
+            </RQTooltip>
+          </label>
+          {/* TODO: limit TBD */}
+          <InputNumber min={0} max={10} value={delay} size="small" name="run-iterations" onChange={handleDelayChange} />
+        </div>
+      </div>
+
+      <div className="advanced-settings">{/* TODO: for later */}</div>
+    </div>
+  );
 };
 
 export const RunConfigContainer: React.FC<{}> = (props) => {
@@ -33,7 +80,7 @@ export const RunConfigContainer: React.FC<{}> = (props) => {
       </div>
 
       <RunConfigOrderedRequests />
-      <RunConfiguration />
+      <RunConfigSettings />
     </div>
   );
 };
