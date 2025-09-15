56b2b7c932963e6aecfc4d7892b2f985c16108ee
Nafees Nehar <nafees87n@gmail.com>
2025-09-15 12:54:05 +0530

    [ENGG-4276] added support to run batched requests (#3504)
    * separated out executor services
    
    * abstracted scriptExecutor
    
    * replace old executor
    
    * fix: client code
    
    * renamed functions
    
    * updated graphql executor
    
    * fix: renderedEntry
    
    * added executeBatches
    
    * fix: entry passing
    
    * fix: entry
    
    * fix: entry sanitization
    
    * fix: type errors
    
    * fix: abortControlelr
    
    * fix: types
    
    * added batchRequestExecutor
    
    * fix: url scheme
    
    * fix: variable resolver
    
    * added factor for requestExecutors
    
    * fix: request preparation
    
    * removed log
    
    * fix: timeout
    
    * fix: comment

---
+++ app/src/features/apiClient/helpers/batchRequestExecutor.ts
@@ -0,0 +1,32 @@
+import { isGraphQLApiEntry, isHTTPApiEntry } from "../screens/apiClient/utils";
+import { RQAPI } from "../types";
+import { GraphQLRequestExecutor } from "./graphQLRequestExecutor/GraphQLRequestExecutor";
+import { HttpRequestExecutor } from "./httpRequestExecutor/httpRequestExecutor";
+
+export class BatchRequestExecutor {
+  constructor(
+    private httpRequestExecutor: HttpRequestExecutor,
+    private graphQLRequestExecutor: GraphQLRequestExecutor
+  ) {}
+
+  async *executeBatches(
+    batchedEntryDetails: {
+      recordId: string;
+      entry: RQAPI.ApiEntry;
+    }[]
+  ) {
+    for (const { recordId, entry } of batchedEntryDetails) {
+      yield this.executeSingleRequest(recordId, entry);
+    }
+  }
+
+  private async executeSingleRequest(recordId: string, entry: RQAPI.ApiEntry): Promise<RQAPI.ExecutionResult> {
+    if (isGraphQLApiEntry(entry)) {
+      return this.graphQLRequestExecutor.executeGraphQLRequest(recordId, entry);
+    } else if (isHTTPApiEntry(entry)) {
+      return this.httpRequestExecutor.execute(recordId, entry);
+    }
+
+    throw new Error("Unsupported API entry type");
+  }
+}
