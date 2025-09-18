c0ba3c2eaa88f87b6d4a7a3995ac96a7330f9eb6
nafees87n <nafees87n@gmail.com>
2025-09-18 14:47:49 +0530

    fix: types

--- app/src/features/apiClient/helpers/apiClientTreeBus/subscription.ts
+++ app/src/features/apiClient/helpers/apiClientTreeBus/subscription.ts
@@ -1,13 +1,14 @@
 import { ApiClientFeatureContext } from "features/apiClient/store/apiClientFeatureContext/apiClientFeatureContext.store";
 import { ApiClientEventTopic } from "./types";
+import { ApiClientEvents } from "./apiClientTreeBus";
 
 export class Subscription<K extends ApiClientEventTopic, T> {
   constructor(
     private readonly emitter: (arg: T) => void,
-    private readonly filterMap: (event: K, ctx: ApiClientFeatureContext) => T | undefined
+    private readonly filterMap: (event: ApiClientEvents<K>, ctx: ApiClientFeatureContext) => T | undefined
   ) {}
 
-  next(event: K, ctx: ApiClientFeatureContext) {
+  next(event: ApiClientEvents<K>, ctx: ApiClientFeatureContext) {
     const data = this.filterMap(event, ctx);
     if (data !== undefined) {
       this.emitter(data);
