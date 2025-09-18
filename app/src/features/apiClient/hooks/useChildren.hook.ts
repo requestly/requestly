17c35902cf8e00f851deff6dfb14d8de31b985f7
nafees87n <nafees87n@gmail.com>
2025-09-18 14:48:03 +0530

    added use children hook

---
+++ app/src/features/apiClient/hooks/useChildren.hook.ts
@@ -0,0 +1,24 @@
+import { useEffect, useMemo, useState } from "react";
+import { useApiClientFeatureContext } from "../contexts/meta";
+import { Subscription } from "../helpers/apiClientTreeBus/subscription";
+import { ApiClientEventTopic } from "../helpers/apiClientTreeBus/types";
+
+export const useChildren = (id: string) => {
+  const ctx = useApiClientFeatureContext();
+  const treeBus = useMemo(() => ctx.treeBus, [ctx]);
+
+  const [children, setChildren] = useState(ctx.stores.records.getState().getAllChildren(id));
+
+  useEffect(() => {
+    const subscription = new Subscription<ApiClientEventTopic.CHILD_ADDED, string[]>(setChildren, () =>
+      ctx.stores.records.getState().getAllChildren(id)
+    );
+    treeBus.subscribe({ nodeId: id, topic: ApiClientEventTopic.CHILD_ADDED, subscription });
+
+    return () => {
+      treeBus.unsubscribe({ nodeId: id, topic: ApiClientEventTopic.CHILD_ADDED, subscription });
+    };
+  }, [ctx, id, treeBus]);
+
+  return children;
+};
