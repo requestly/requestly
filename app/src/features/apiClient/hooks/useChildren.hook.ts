108cc3f45440a54bf02d5dd1a2e9355b75bf068a
nafees87n <nafees87n@gmail.com>
2025-09-22 12:51:59 +0530

    fix: useChildren hook

--- app/src/features/apiClient/hooks/useChildren.hook.ts
+++ app/src/features/apiClient/hooks/useChildren.hook.ts
@@ -1,24 +1,40 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useApiClientFeatureContext } from "../contexts/meta";
 import { Subscription } from "../helpers/apiClientTreeBus/subscription";
 import { ApiClientEventTopic } from "../helpers/apiClientTreeBus/types";
+import { RQAPI } from "../types";
 
-export const useChildren = (id: string) => {
+export const useChildren = (nodeId: string) => {
   const ctx = useApiClientFeatureContext();
   const treeBus = useMemo(() => ctx.treeBus, [ctx]);
 
-  const [children, setChildren] = useState(ctx.stores.records.getState().getAllChildren(id));
+  const getAllChildrenRecords = useCallback(
+    function () {
+      const children = ctx.stores.records.getState().getAllChildren(nodeId);
+      const allRecords = ctx.stores.records.getState().getAllRecords();
+      return allRecords.filter((r) => children.includes(r.id));
+    },
+    [ctx, nodeId]
+  );
+
+  const [children, setChildren] = useState<RQAPI.ApiClientRecord[]>(getAllChildrenRecords());
 
   useEffect(() => {
-    const subscription = new Subscription<ApiClientEventTopic.TREE_CHANGED, string[]>(setChildren, (TreeChanged, ctx) =>
-      ctx.stores.records.getState().getAllChildren(id)
+    const subscription = new Subscription<ApiClientEventTopic.TREE_CHANGED, RQAPI.ApiClientRecord[]>(
+      setChildren,
+      (_, ctx) => {
+        const children = ctx.stores.records.getState().getAllChildren(nodeId);
+        const allRecords = ctx.stores.records.getState().getAllRecords();
+        const childrenRecords = allRecords.filter((r) => children.includes(r.id));
+        return childrenRecords;
+      }
     );
-    treeBus.subscribe({ nodeId: id, topic: ApiClientEventTopic.TREE_CHANGED, subscription });
+    treeBus.subscribe({ nodeId: nodeId, topic: ApiClientEventTopic.TREE_CHANGED, subscription });
 
     return () => {
-      treeBus.unsubscribe({ nodeId: id, topic: ApiClientEventTopic.TREE_CHANGED, subscription });
+      treeBus.unsubscribe({ nodeId: nodeId, topic: ApiClientEventTopic.TREE_CHANGED, subscription });
     };
-  }, [ctx, id, treeBus]);
+  }, [ctx, nodeId, treeBus]);
 
   return children;
 };
