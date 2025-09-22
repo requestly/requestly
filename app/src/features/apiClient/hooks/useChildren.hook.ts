273c11690039512e9ae37ea092666ce46114cc90
nafees87n <nafees87n@gmail.com>
2025-09-22 20:03:59 +0530

    added generic way to create apiClientTreeBus

--- app/src/features/apiClient/hooks/useChildren.hook.ts
+++ app/src/features/apiClient/hooks/useChildren.hook.ts
@@ -1,9 +1,7 @@
-import { useEffect, useMemo, useState } from "react";
-import { useApiClientFeatureContext } from "../contexts/meta";
-import { Subscription } from "../helpers/apiClientTreeBus/subscription";
 import { ApiClientEventTopic } from "../helpers/apiClientTreeBus/types";
 import { RQAPI } from "../types";
 import { ApiClientFeatureContext } from "../store/apiClientFeatureContext/apiClientFeatureContext.store";
+import { createApiClientTreeBus } from "../helpers/apiClientTreeBus/createReactiveHook";
 
 export const getAllChildrenRecords = (ctx: ApiClientFeatureContext, nodeId: string) => {
   const children = ctx.stores.records.getState().getAllChildren(nodeId);
@@ -11,25 +9,9 @@ export const getAllChildrenRecords = (ctx: ApiClientFeatureContext, nodeId: stri
   return children.map((child) => getRecord(child)).filter(Boolean);
 };
 
-export const useChildren = (nodeId: string) => {
-  const ctx = useApiClientFeatureContext();
-  const treeBus = useMemo(() => ctx.treeBus, [ctx]);
-
-  const [children, setChildren] = useState<RQAPI.ApiClientRecord[]>(getAllChildrenRecords(ctx, nodeId));
-
-  useEffect(() => {
-    const subscription = new Subscription<ApiClientEventTopic.TREE_CHANGED, RQAPI.ApiClientRecord[]>(
-      setChildren,
-      () => {
-        return getAllChildrenRecords(ctx, nodeId);
-      }
-    );
-    treeBus.subscribe({ nodeId: nodeId, topic: ApiClientEventTopic.TREE_CHANGED, subscription });
-
-    return () => {
-      treeBus.unsubscribe({ nodeId: nodeId, topic: ApiClientEventTopic.TREE_CHANGED, subscription });
-    };
-  }, [ctx, nodeId, treeBus]);
-
-  return children;
-};
+export const useChildren = createApiClientTreeBus<ApiClientEventTopic.TREE_CHANGED, RQAPI.ApiClientRecord[]>(
+  ApiClientEventTopic.TREE_CHANGED,
+  (ctx: ApiClientFeatureContext, nodeId: string) => {
+    return getAllChildrenRecords(ctx, nodeId);
+  }
+);
