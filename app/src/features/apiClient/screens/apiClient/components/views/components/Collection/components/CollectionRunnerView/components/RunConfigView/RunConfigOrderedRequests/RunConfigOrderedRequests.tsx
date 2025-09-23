acb17576ea0a775f82867ea210ffed5ee6728378
Rohan Mathur <mathurrohan04@gmail.com>
2025-09-23 18:20:39 +0530

    [ENGG-4427]:  add run context + provide run result store (#3564)
    * add run context
    
    * fix: loader style
    
    * refactor

--- app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunConfigView/RunConfigOrderedRequests/RunConfigOrderedRequests.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunConfigView/RunConfigOrderedRequests/RunConfigOrderedRequests.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ReorderableList } from "./ReorderableList/ReorderableList";
-import { useRunConfigStore } from "features/apiClient/store/collectionRunConfig/runConfigStoreContext.hook";
+import { useRunConfigStore } from "../../../run.context";
 
 export const RunConfigOrderedRequests: React.FC<{ selectAll: { value: boolean } }> = ({ selectAll }) => {
   const [orderedRequests, setOrderedRequests] = useRunConfigStore((s) => [s.orderedRequests, s.setOrderedRequests]);
