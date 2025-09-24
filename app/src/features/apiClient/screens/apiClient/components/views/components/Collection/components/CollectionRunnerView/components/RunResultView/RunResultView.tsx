d3706928373b73dc37e47ee14ff714cd2993d4e7
rohanmathur91 <mathurrohan04@gmail.com>
2025-09-24 23:08:01 +0530

    add comment

--- app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunResultView/RunResultView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunResultView/RunResultView.tsx
@@ -7,7 +7,11 @@ import "./runResultView.scss";
 
 export const RunResultView: React.FC = () => {
   const [result, startTime, getRunSummary] = useRunResultStore((s) => [s.result, s.startTime, s.getRunSummary]);
-  const testResults = useMemo(() => getRunSummary(), [getRunSummary, result]);
+  const testResults = useMemo(
+    () => getRunSummary(),
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- need `result` for reactivity
+    [getRunSummary, result]
+  );
 
   return (
     <div className="run-result-view-container">
