2d49d81d89a3d48ae1781b7f1b85b76eb1b0bb59
rohanmathur91 <mathurrohan04@gmail.com>
2025-09-25 14:49:25 +0530

    add test running loader

--- app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunResultView/RunResultView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunResultView/RunResultView.tsx
@@ -3,10 +3,18 @@ import { RQButton } from "lib/design-system-v2/components";
 import { MdOutlineHistory } from "@react-icons/all-files/md/MdOutlineHistory";
 import { useRunResultStore } from "../../run.context";
 import { RunResultContainer } from "./RunResultContainer/RunResultContainer";
+import { TestsRunningLoader } from "./TestsRunningLoader/TestsRunningLoader";
+import { RunStatus } from "features/apiClient/store/collectionRunResult/runResult.store";
 import "./runResultView.scss";
 
 export const RunResultView: React.FC = () => {
-  const [result, startTime, getRunSummary] = useRunResultStore((s) => [s.result, s.startTime, s.getRunSummary]);
+  const [result, startTime, getRunSummary, runStatus] = useRunResultStore((s) => [
+    s.result,
+    s.startTime,
+    s.getRunSummary,
+    s.runStatus,
+  ]);
+
   const testResults = useMemo(
     () => getRunSummary(),
     // eslint-disable-next-line react-hooks/exhaustive-deps -- need `result` for reactivity
@@ -23,6 +31,7 @@ export const RunResultView: React.FC = () => {
       </div>
 
       <RunResultContainer result={testResults} ranAt={startTime} />
+      {runStatus === RunStatus.RUNNING ? <TestsRunningLoader /> : null}
     </div>
   );
 };
