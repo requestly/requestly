74ec2a9134cfffdda546182655a1c7e33a90a081
Rahul Ramteke <iostreamer007@gmail.com>
2025-10-01 14:48:42 +0530

    Added sort support to getAllChildren

--- app/src/features/apiClient/hooks/useChildren.hook.ts
+++ app/src/features/apiClient/hooks/useChildren.hook.ts
@@ -8,7 +8,30 @@ import { ApiClientFeatureContext } from "../store/apiClientFeatureContext/apiCli
 export const getAllChildrenRecords = (ctx: ApiClientFeatureContext, nodeId: string) => {
   const children = ctx.stores.records.getState().getAllChildren(nodeId);
   const getRecord = ctx.stores.records.getState().getData;
-  return children.map((child) => getRecord(child)).filter(Boolean);
+  return children.map((child) => getRecord(child)).filter(Boolean).sort(
+    (recordA, recordB) => {
+      // If different type, then keep collection first
+      if (recordA.type === RQAPI.RecordType.COLLECTION && recordA.isExample && !recordB.isExample) {
+        return -1;
+      }
+
+      if (recordB.type === RQAPI.RecordType.COLLECTION && recordB.isExample && !recordA.isExample) {
+        return 1;
+      }
+
+      if (recordA.type !== recordB.type) {
+        return recordA.type === RQAPI.RecordType.COLLECTION ? -1 : 1;
+      }
+
+      // If types are the same, sort lexicographically by name
+      if (recordA.name.toLowerCase() !== recordB.name.toLowerCase()) {
+        return recordA.name.toLowerCase() < recordB.name.toLowerCase() ? -1 : 1;
+      }
+
+      // If names are the same, sort by creation date
+      return recordA.createdTs - recordB.createdTs;
+    }
+  );
 };
 
 export const useChildren = (nodeId: string) => {
