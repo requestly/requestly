8cfb9a5fbf21c0418a18c5f68e5fbcae001197bb
rohanmathur91 <mathurrohan04@gmail.com>
2025-10-09 17:17:50 +0530

    cleanup

--- app/src/features/apiClient/commands/collectionRunner/types.ts
+++ app/src/features/apiClient/commands/collectionRunner/types.ts
@@ -1,4 +1,3 @@
-import { IterationDetails, RunResult, RunStatus } from "features/apiClient/store/collectionRunResult/runResult.store";
 import { RQAPI } from "features/apiClient/types";
 
 export type SavedRunConfig = {
@@ -9,12 +8,4 @@ export type SavedRunConfig = {
 export type SavedRunConfigRecord = SavedRunConfig & {
   createdTs: number;
   updatedTs: number;
-  collectionId: RQAPI.ApiClientRecord["collectionId"];
-};
-
-export type SavedRunResultRecord = RunResult & {
-  id: string;
-  iterations: IterationDetails[];
-  runStatus: RunStatus.COMPLETED;
-  collectionId: RQAPI.ApiClientRecord["collectionId"];
 };
