63587a9c1a2eb091c825edc6b622577f3dfd4edc
Sahil Gupta <wrongsahil@gmail.com>
2025-10-15 18:04:34 +0530

    fix: clearing history crash

--- app/src/features/apiClient/screens/apiClient/components/views/components/request/HistoryView/HistoryView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/request/HistoryView/HistoryView.tsx
@@ -8,6 +8,7 @@ import {
   ApiClientViewMode,
   useApiClientMultiWorkspaceView,
 } from "features/apiClient/store/multiWorkspaceView/multiWorkspaceView.store";
+import "./historyView.scss";
 
 export const HistoryView: React.FC = () => {
   const [openTab] = useTabServiceWithSelector((state) => [state.openTab]);
@@ -15,10 +16,14 @@ export const HistoryView: React.FC = () => {
   const [viewMode] = useApiClientMultiWorkspaceView((s) => [s.viewMode]);
 
   const entry = useMemo(() => {
-    return {
-      type: RQAPI.RecordType.API,
-      data: { ...history[selectedHistoryIndex] },
-    } as RQAPI.ApiRecord;
+    if (selectedHistoryIndex != null && selectedHistoryIndex !== undefined && history?.[selectedHistoryIndex]) {
+      return {
+        type: RQAPI.RecordType.API,
+        data: { ...history[selectedHistoryIndex] },
+      } as RQAPI.ApiRecord;
+    }
+
+    return null;
   }, [history, selectedHistoryIndex]);
 
   const hideSaveBtn = viewMode === ApiClientViewMode.MULTI;
@@ -49,6 +54,16 @@ export const HistoryView: React.FC = () => {
     },
     [addToHistory, setCurrentHistoryIndex, history]
   );
+
+  if (!entry) {
+    return (
+      <div className="empty-state">
+        <img src={"/assets/media/apiClient/response-empty-state.svg"} alt="empty card" width={80} height={80} />
+        <div className="api-client-empty-response-view__title">Nothing to see here!</div>
+        <div className="api-client-empty-response-view__description">Select a request from the history to view it</div>
+      </div>
+    );
+  }
 
   return (
     <GenericApiClient
