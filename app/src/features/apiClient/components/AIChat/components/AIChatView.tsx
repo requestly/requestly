48884d91ee35645d42fee33630acbb32a6b0c5b6
Parth Bhardwaj <parthb.git@gmail.com>
2025-10-16 17:01:21 +0530

    ENGG-4543: Sync chats in DB

--- app/src/features/apiClient/components/AIChat/components/AIChatView.tsx
+++ app/src/features/apiClient/components/AIChat/components/AIChatView.tsx
@@ -3,44 +3,82 @@ import { ChatHeader } from "./ChatHeader/ChatHeader";
 import { ChatThreadView } from "./ChatThreadView/ChatThreadView";
 import { useChatSessionsStore } from "features/apiClient/hooks/useChatSessionsStore.hook";
 import { ChatSessionsContextProvider } from "features/apiClient/store/apiClientFeatureContext/chatSessions/chatSessionsContextProvider";
-import { demoSessionsMap } from "../demo-session";
 import { ChatInput } from "./ChatThreadView/components/ChatInput/ChatInput";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { toast } from "utils/Toast";
+import { useSelector } from "react-redux";
+import { getUserAuthDetails } from "store/slices/global/user/selectors";
+import { RQButton } from "lib/design-system-v2/components";
+import { getActiveWorkspace } from "store/slices/workspaces/selectors";
+import { createSession } from "backend/apiClient";
+import { useEffect, useState } from "react";
+import { addMessage, getAllSessions } from "backend/apiClient";
+import { AIChat } from "../types";
 import "./aiChatView.scss";
 
 const generateResponse = httpsCallable(getFunctions(), "generateAiAgentResponse", { timeout: 200_000 });
 
 const AIChatView = () => {
-  const [sessionId, isProcessing, setIsProcessing, updateSession] = useChatSessionsStore((s) => [
+  const user = useSelector(getUserAuthDetails);
+  const activeWorkspace = useSelector(getActiveWorkspace);
+
+  const [
+    sessionId,
+    isProcessing,
+    setIsProcessing,
+    updateSession,
+    createChatSession,
+    setActiveSessionId,
+  ] = useChatSessionsStore((s) => [
     s.activeSessionId,
     s.isProcessing,
     s.setIsProcessing,
     s.updateSession,
+    s.createChatSession,
+    s.setActiveSessionId,
   ]);
+
+  console.log("sessionId", sessionId);
 
   const handleSendMessage = async (message: string) => {
     if (message.trim().length === 0) return;
     setIsProcessing(true);
-    updateSession(sessionId, {
+    const userMessage: AIChat.UserMessage = {
       role: "user",
       text: message,
-      createdAt: Date.now(),
-    });
-    setIsProcessing(false);
+      createdTs: Date.now(),
+    };
+    updateSession(sessionId, userMessage);
+    const userMessageResult = await addMessage(
+      user.details.profile.uid,
+      activeWorkspace.id ?? "private",
+      sessionId,
+      userMessage
+    );
+    console.log("userMessageResult", userMessageResult);
     try {
       const modelResult = await generateResponse({
         sessionId,
         message,
       });
-      updateSession(sessionId, {
+      const modelMessage: AIChat.ModelMessage = {
         role: "model",
-        // @ts-ignore
         text: modelResult.data.text,
-        // @ts-ignore
         actions: modelResult.data.actions,
-        createdAt: Date.now(),
-      });
+        createdTs: Date.now(),
+      };
+      const modelMessageResult = await addMessage(
+        user.details.profile.uid,
+        activeWorkspace.id ?? "private",
+        sessionId,
+        modelMessage
+      );
+      console.log("modelMessageResult", modelMessageResult);
+      if (modelMessageResult.success && modelMessageResult.data) {
+        updateSession(sessionId, modelMessageResult.data);
+      } else {
+        toast.error("Failed to add model message. Please try again.");
+      }
     } catch (error) {
       console.error(error);
       toast.error("Something went wrong! Please try again.");
@@ -48,6 +86,36 @@ const AIChatView = () => {
       setIsProcessing(false);
     }
   };
+
+  if (!user.loggedIn) {
+    return null;
+  }
+
+  if (!sessionId) {
+    return (
+      <div className="ai-chat">
+        No session found
+        <RQButton
+          onClick={async () => {
+            const result = await createSession(user.details.profile.uid, activeWorkspace.id ?? "private");
+            if (result.success && result.data) {
+              setActiveSessionId(result.data.id);
+              createChatSession({
+                id: result.data.id,
+                messages: [],
+                createdTs: result.data.createdTs,
+                updatedTs: result.data.updatedTs,
+              });
+            } else {
+              toast.error("Failed to create session. Please try again.");
+            }
+          }}
+        >
+          Create
+        </RQButton>
+      </div>
+    );
+  }
 
   return (
     <div className="ai-chat">
@@ -68,8 +136,33 @@ const AIChatView = () => {
 };
 
 const ChatViewWithProvider = () => {
+  const [sessions, setSessions] = useState(null);
+  const [hasInitialized, setHasInitialized] = useState(false);
+  const user = useSelector(getUserAuthDetails);
+  const activeWorkspace = useSelector(getActiveWorkspace);
+
+  useEffect(() => {
+    if (!user.loggedIn) return;
+    getAllSessions(user.details?.profile?.uid, activeWorkspace.id ?? "private")
+      .then((result) => {
+        if (result.success && result.data) {
+          console.log("result.data", result.data);
+          setSessions(result.data);
+          setHasInitialized(true);
+        } else throw new Error("Failed to get sessions. Please try again.");
+      })
+      .catch((error) => {
+        toast.error("Failed to get sessions. Please try again.");
+      });
+  }, [hasInitialized, user.loggedIn, user.details?.profile?.uid, activeWorkspace.id]);
+
+  if (!hasInitialized) {
+    // TODO: fix this, split container breaks when returning null
+    return <div className="ai-chat">Loading...</div>;
+  }
+
   return (
-    <ChatSessionsContextProvider sessions={demoSessionsMap}>
+    <ChatSessionsContextProvider sessions={sessions}>
       <AIChatView />
     </ChatSessionsContextProvider>
   );
