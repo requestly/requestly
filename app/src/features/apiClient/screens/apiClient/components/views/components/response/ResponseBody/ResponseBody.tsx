1c62e53e8a39ccabe992362e4bf4801e7ff12569
Vishal Ola <vishalola555@gmail.com>
2025-10-24 16:54:42 +0530

    Editor now used in both Preview and Raw tabs, code prettified on init

--- app/src/features/apiClient/screens/apiClient/components/views/components/response/ResponseBody/ResponseBody.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/response/ResponseBody/ResponseBody.tsx
@@ -3,10 +3,7 @@ import { Radio, RadioChangeEvent, Tooltip } from "antd";
 import { trackRawResponseViewed } from "modules/analytics/events/features/apiClient";
 import { getEditorLanguageFromContentType } from "componentsV2/CodeEditor";
 import "./responseBody.scss";
-import { RQButton } from "lib/design-system-v2/components";
-import { IoMdCopy } from "@react-icons/all-files/io/IoMdCopy";
 import Editor from "componentsV2/CodeEditor";
-import { copyToClipBoard } from "utils/Misc";
 
 interface Props {
   responseText: string;
@@ -28,7 +25,6 @@ const ImageResponsePreview: React.FC<{ responseText: string; mimeType: string }>
 
 const ResponseBody: React.FC<Props> = ({ responseText, contentTypeHeader }) => {
   const [responseMode, setResponseMode] = useState(ResponseMode.PREVIEW);
-  const [isResponseCopied, setIsResponseCopied] = useState(false);
 
   const onResponseModeChange = useCallback((e: RadioChangeEvent) => {
     const responseMode: ResponseMode = e.target.value;
@@ -38,16 +34,6 @@ const ResponseBody: React.FC<Props> = ({ responseText, contentTypeHeader }) => {
       trackRawResponseViewed();
     }
   }, []);
-
-  const onCopyButtonClick = useCallback(async () => {
-    const result = await copyToClipBoard(responseText);
-    if (result.success) {
-      setIsResponseCopied(true);
-      setTimeout(() => {
-        setIsResponseCopied(false);
-      }, 1000);
-    }
-  }, [responseText]);
 
   const bodyPreviewModeOptions = useMemo(() => {
     return (
@@ -81,10 +67,10 @@ const ResponseBody: React.FC<Props> = ({ responseText, contentTypeHeader }) => {
     return (
       <div className="api-client-code-editor-container api-response-body-editor-container">
         <Editor
-          prettifyOnInit
           value={responseText}
           language={editorLanguage}
           isReadOnly
+          responseMode={responseMode}
           toolbarOptions={{
             title: "",
             options: [bodyPreviewModeOptions],
@@ -96,28 +82,7 @@ const ResponseBody: React.FC<Props> = ({ responseText, contentTypeHeader }) => {
 
   return (
     <div className="api-client-response-body">
-      <div className="api-response-body-content">
-        {preview && responseMode === ResponseMode.PREVIEW ? (
-          preview
-        ) : (
-          <div className="api-response-body-raw-content">
-            <div className="api-response-body-raw-content__header">
-              {bodyPreviewModeOptions}
-              <Tooltip title={isResponseCopied ? "Copied!" : "Copy"} placement="left" color="#000">
-                <RQButton
-                  type="transparent"
-                  icon={<IoMdCopy className="copy-raw-response-icon" />}
-                  size="small"
-                  onClick={onCopyButtonClick}
-                />
-              </Tooltip>
-            </div>
-            <div className="api-response-body-raw-content__body">
-              <pre>{responseText}</pre>
-            </div>
-          </div>
-        )}
-      </div>
+      <div className="api-response-body-content">{preview}</div>
     </div>
   );
 };
