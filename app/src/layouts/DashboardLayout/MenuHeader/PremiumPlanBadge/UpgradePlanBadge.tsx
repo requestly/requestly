a2f5cd35ab564a29b87cf7bf3d41b13c0f0522ee
Yuvaraj Sai Thadishetty <yuvarajsait@gmail.com>
2025-10-26 01:43:32 +0530

    add main logic

---
+++ app/src/layouts/DashboardLayout/MenuHeader/PremiumPlanBadge/UpgradePlanBadge.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import clsx from "clsx";
+import { RQButton } from "lib/design-system-v2/components";
+import "./premiumPlanBadge.scss";
+
+interface UpgradePlanBadgeProps {
+  icon: React.ReactNode;
+  planStatusInfo: string;
+  handleBadgeClick: () => void;
+  badgeText: string;
+  containerClassName?: string;
+}
+
+const UpgradePlanBadge: React.FC<UpgradePlanBadgeProps> = ({
+  icon,
+  planStatusInfo,
+  handleBadgeClick,
+  badgeText,
+  containerClassName = "",
+}) => {
+  return (
+    <div className={clsx("premium-plan-badge-container", containerClassName)}>
+      {icon}
+      <span className="premium-plan-name-info">{planStatusInfo}</span>
+      <RQButton type="primary" shape="round" className="upgrade-button" onClick={handleBadgeClick}>
+        {badgeText}
+      </RQButton>
+    </div>
+  );
+};
+export default UpgradePlanBadge;
