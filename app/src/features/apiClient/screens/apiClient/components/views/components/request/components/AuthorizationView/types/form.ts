b0b62f76d052ec2912a83af985213ebad99d4dfe
Saurabh <saurabh.bh@browserstack.com>
2025-11-03 17:21:49 +0530

    Refactor Basic Auth to allow empty password and use Input.Password

--- app/src/features/apiClient/screens/apiClient/components/views/components/request/components/AuthorizationView/types/form.ts
+++ app/src/features/apiClient/screens/apiClient/components/views/components/request/components/AuthorizationView/types/form.ts
@@ -10,7 +10,7 @@ export interface BEARER_TOKEN_FORM_VALUES {
 
 export interface BASIC_AUTH_FORM_VALUES {
   username?: string;
-  password: string;
+  password?: string;
 }
 
 export type AUTH_OPTIONS = API_KEY_FORM_VALUES | BEARER_TOKEN_FORM_VALUES | BASIC_AUTH_FORM_VALUES;
