3d43f367437f4d86db5f2da164b7cf971b90208f
nafees87n <nafees87n@gmail.com>
2025-11-07 11:23:00 +0530

    fix: variables unsetter

--- app/src/features/apiClient/helpers/modules/scriptsV2/worker/script-internals/IterationData.ts
+++ app/src/features/apiClient/helpers/modules/scriptsV2/worker/script-internals/IterationData.ts
@@ -18,13 +18,7 @@ export class IterationData {
   // only unsets in the current class instance, does not modify the localScope
   unset(key: string) {
     const variables = this.iterationData;
-
-    if (!variables[key]) {
-      return;
-    }
-
-    variables[key].localValue = undefined;
-    variables[key].syncValue = undefined;
+    delete variables[key];
   }
 
   toObject() {
