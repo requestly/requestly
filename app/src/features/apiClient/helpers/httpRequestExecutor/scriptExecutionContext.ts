2b49a2ba097d3274b910b69c689276f5be03c70f
nafees87n <nafees87n@gmail.com>
2025-11-07 11:08:11 +0530

    fix: mutation check

--- app/src/features/apiClient/helpers/httpRequestExecutor/scriptExecutionContext.ts
+++ app/src/features/apiClient/helpers/httpRequestExecutor/scriptExecutionContext.ts
@@ -89,6 +89,10 @@ export class ScriptExecutionContext {
     return this.isMutated;
   }
 
+  public resetIsMutated() {
+    this.isMutated = false;
+  }
+
   public setResponse(response: SnapshotForPostResponse["response"]) {
     this.context.response = response;
   }
