062af2a2a8bffef048ba0a67a8704b8cfb1e4361
Rahul Ramteke <iostreamer007@gmail.com>
2025-11-10 15:27:24 +0530

    Add support for require and some builtin modules and globals (#3778)
    * Added support to specify error type
    
    * Add support to sanitize value when script sets a key
    
    * Add support for require and globals in scripts
    
    * Added error handling to xml2Json
    
    * Fixed types for try
    
    * Added more packages
    
    * added lodash in require
    
    * Updated package-lock

---
+++ app/src/features/apiClient/helpers/modules/scriptsV2/worker/script-internals/scriptExecutionWorker/globals/xml2Json.ts
@@ -0,0 +1,24 @@
+
+import * as xml2Js from 'xml2js';
+
+const xml2jsOptions = {
+  explicitArray: false,
+  async: false,
+  trim: true,
+  mergeAttrs: false
+};
+
+export function xml2Json(string: string) {
+  let parsedResult = {};
+
+  xml2Js.parseString(string, xml2jsOptions, function(err: any, result: any) {
+    if(err) {
+      throw new Error(`XML Parsing failed! ${err.message}`);
+    }
+    parsedResult = result;
+  });
+
+  return parsedResult;
+};
+
+
