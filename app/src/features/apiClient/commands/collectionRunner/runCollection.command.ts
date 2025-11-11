66d20284bce24ae3fc5c2ab0c828ce8712945c9a
Nafees Nehar <nafees87n@gmail.com>
2025-11-11 17:24:06 +0530

    [ENGG-4628] fix: use execution context when preparing the result after prescript (#3806)
    * render using execution context in repreparation of the result
    
    * fix: auth header preparation
    
    * added override config for getting scoped variables
    
    * fix: naming
    
    * remove comments
    
    * remove comment
    
    * fix: dummy variables store

--- app/src/features/apiClient/commands/collectionRunner/runCollection.command.ts
+++ app/src/features/apiClient/commands/collectionRunner/runCollection.command.ts
@@ -27,12 +27,12 @@ import { CloseTopic } from "componentsV2/Tabs/store/tabStore";
 import { cancelRun } from "./cancelRun.command";
 import { Scope } from "features/apiClient/helpers/variableResolver/variable-resolver";
 import { VariableScope } from "backend/environment/types";
-import { createDummyVariablesStore } from "features/apiClient/store/variables/variables.store";
 import { apiClientFileStore } from "features/apiClient/store/apiClientFilesStore";
 import { RunnerFileMissingError } from "features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunResultView/errors/RunnerFileMissingError/RunnerFileMissingError";
 import { DataFileParseError } from "features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunResultView/errors/DataFileParseError/DataFileParseError";
 import { ITERATIONS_MAX_LIMIT } from "features/apiClient/store/collectionRunConfig/runConfig.store";
 import { renderVariables } from "backend/environment/utils";
+import { createDummyVariablesStoreFromPrimitives } from "features/apiClient/store/variables/variables.store";
 
 function parseExecutingRequestEntry(entry: RQAPI.ApiEntry): RequestExecutionResult["entry"] {
   return isHTTPApiEntry(entry)
@@ -236,7 +236,7 @@ class Runner {
           name: "Data File",
           level: 0,
         },
-        createDummyVariablesStore(this.variables[iteration - 1]),
+        createDummyVariablesStoreFromPrimitives(this.variables[iteration - 1]),
       ]);
     }
 
