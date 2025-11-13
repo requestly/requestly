1f997e2cfed59ff9aa09c7a601779686bbf3b51c
rohanmathur91 <mathurrohan04@gmail.com>
2025-11-13 12:56:51 +0530

    cleanup

--- app/src/features/apiClient/commands/multiView/markWorkspaceLoaded.command.ts
+++ app/src/features/apiClient/commands/multiView/markWorkspaceLoaded.command.ts
@@ -3,15 +3,5 @@ import { closeCorruptedTabs } from "../tabs";
 
 export function markWorkspaceLoaded() {
   closeCorruptedTabs();
-  /**
-   * - Close all the tabs for which context was not found and continue the flow
-   *    - Cons:
-   *        - In worst case, all the tabs will be closed, which affects UX
-   * - Close the tabs and if error then return without marking
-   *    - Cons:
-   *       - UI stuck on loading state
-   *       - No way to recover
-   * - Close tabs by failed workspaces
-   */
   apiClientMultiWorkspaceViewStore.getState().setIsLoaded(true);
 }
