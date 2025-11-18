87eceab49c5f0c957a4bd20473fa3657430e52cd
Sahil Gupta <wrongsahil@gmail.com>
2025-11-18 23:46:51 +0530

    chore: firebase auth for dev env (#3876)

--- app/src/hooks/useIsBrowserStackIntegrationOn.ts
+++ app/src/hooks/useIsBrowserStackIntegrationOn.ts
@@ -1,7 +1,9 @@
 import { useFeatureValue } from "@growthbook/growthbook-react";
+import { isEnvDev } from "utils/EnvUtils";
 
 export const useIsBrowserStackIntegrationOn = () => {
-  const isBrowserStackIntegrationEnabled = useFeatureValue("browserstack_integration", true);
+  const featureValue = useFeatureValue("browserstack_integration", true);
+  const isBrowserStackIntegrationEnabled = isEnvDev() ? false : featureValue;
   // TODO: Add localStorage.get("forceBsIntegration") check when bookmarklet is implemented
   return isBrowserStackIntegrationEnabled;
 };
