d67615f35abdda4f8717425a4efb244753f32a80
Ajinkya Vidwans <ajinkya@browserstack.com>
2025-11-27 16:17:48 +0530

    refactor: Rename local workspace deletion component and move styling to a separate file

--- app/src/features/settings/components/Profile/ManageTeams/TeamViewer/LocalWorkspaceSettings/DeleteLocalWorkspaceSection.tsx
+++ app/src/features/settings/components/Profile/ManageTeams/TeamViewer/LocalWorkspaceSettings/DeleteLocalWorkspaceSection.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from "react";
+import "./deleteLocalWorkspaceSection.scss";
 import { Alert, Button, Checkbox } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
