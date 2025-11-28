85dcd5326c8fed72e21fac02bb3ce3bd0f13aa28
TejaChitturi <teja@browserstack.com>
2025-11-28 19:53:28 +0530

    [ENGG-4953]: API client tabs improvements

---
+++ app/src/componentsV2/Tabs/components/CloseAllTabsButton/CloseAllTabsButton.tsx
@@ -0,0 +1,55 @@
+import { Tooltip } from "antd";
+import { RQButton } from "lib/design-system-v2/components";
+import React, { useState } from "react";
+import { MdClose } from "@react-icons/all-files/md/MdClose";
+import { CloseAllTabsModal } from "features/apiClient/screens/apiClient/components/modals/CloseAllTabsModal/CloseAllTabsModal";
+
+interface Props {
+  unSavedTabsCount: number;
+  closeAllOpenTabs: (type: string) => void;
+}
+
+export const CloseAllTabsButton: React.FC<Props> = ({ unSavedTabsCount, closeAllOpenTabs }) => {
+  const [showUnSavedModal, setShowUnSavedModal] = useState(false);
+
+  const onToggleModal = (state: boolean) => {
+    setShowUnSavedModal(state);
+  };
+
+  const onCloseAllButtonClick = () => {
+    if (unSavedTabsCount > 1) {
+      setShowUnSavedModal(true);
+    } else {
+      closeAllOpenTabs("close-without-close-modal");
+    }
+  };
+
+  return (
+    <>
+      <Tooltip
+        title="Close all opened tabs"
+        mouseEnterDelay={0.5}
+        overlayClassName="tab-title-tooltip"
+        placement="bottomLeft"
+      >
+        <RQButton
+          type="transparent"
+          icon={<MdClose size={14} className="close-all-tabs-icon" />}
+          size="small"
+          className="close-all-tabs-button"
+          onClick={onCloseAllButtonClick}
+        >
+          <span className="close-all-tabs-text">Close all</span>
+        </RQButton>
+      </Tooltip>
+      {showUnSavedModal && unSavedTabsCount > 1 && (
+        <CloseAllTabsModal
+          open={true}
+          onClose={() => onToggleModal(false)}
+          unSavedTabsCount={unSavedTabsCount}
+          closeAllOpenTabs={closeAllOpenTabs}
+        />
+      )}
+    </>
+  );
+};
--- app/src/componentsV2/Tabs/components/TabsEmptyState/TabsEmptyState.tsx
+++ app/src/componentsV2/Tabs/components/TabsEmptyState/TabsEmptyState.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NoTabsFound from "/assets/media/apiClient/no-tabs-found.svg";
+import NoSearchFound from "/assets/media/apiClient/empty-search.svg";
 import "./tabsEmptyState.scss";
 
 // Empty state component for when no tabs are found in search
@@ -10,7 +10,7 @@ export interface TabsEmptyStateProps {
 export const TabsEmptyState: React.FC<TabsEmptyStateProps> = ({ onClearFilter }) => {
   return (
     <div className="tabs-empty-state">
-      <img src={NoTabsFound} alt="No tabs found" className="tabs-empty-icon" />
+      <img src={NoSearchFound} alt="No tabs found" className="tabs-empty-icon" />
       <div className="tabs-empty-text">No tabs found!</div>
       <div className="tabs-clear-filter" onClick={onClearFilter}>
         <span className="clear-icon">✕</span>
