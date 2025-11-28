b928062b75f2ad1bb64d433bc098c2ba9b17f147
Nafees Nehar <nafees87n@gmail.com>
2025-11-28 15:49:24 +0530

    chore: fix ts errors (#3935)
    * fix: sanitizeRecord
    
    * fix: null checks in api client backend
    
    * fix: auth backend
    
    * fix: useBillingTeamListener
    
    * fix: env and billing utils
    
    * fix: mocks types
    
    * fix: backend user
    
    * fix: loginHandler
    
    * fix: null types in common components
    
    * fix: copy button and text
    
    * fix: workspace menu types
    
    * fix: backend and typo
    
    * fix: suggestions
    
    * fix: shareFromWorkspace
    
    * fix: types
    
    * fix

--- app/src/backend/user/userSubscription.ts
+++ app/src/backend/user/userSubscription.ts
@@ -2,7 +2,7 @@ import { UserSubscription } from "backend/models/userSubscriptions";
 import firebaseApp from "../../firebase";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 
-export const getUserSubscription = async (uid: string): Promise<UserSubscription> => {
+export const getUserSubscription = async (uid: string): Promise<UserSubscription | null> => {
   const db = getFirestore(firebaseApp);
   const userSubscriptionDocRef = doc(db, "individualSubscriptions", uid);
 
