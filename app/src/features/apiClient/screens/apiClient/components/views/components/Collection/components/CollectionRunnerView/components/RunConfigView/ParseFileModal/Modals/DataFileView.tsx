eb43e6e75a6df02a2812bb287aece36fe3a0c07a
Aarushsr12 <aarushsrivastava1212@gmail.com>
2025-12-11 02:06:15 +0530

    fix: datafileview

--- app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunConfigView/ParseFileModal/Modals/DataFileView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/Collection/components/CollectionRunnerView/components/RunConfigView/ParseFileModal/Modals/DataFileView.tsx
@@ -15,6 +15,10 @@ export const getformattedFileSize = (bytes: number): string => {
 export const DataFileView: React.FC<ModalProps> = ({ buttonOptions, viewMode }) => {
   const { dataFileMetadata, parsedData } = useDataFileModalContext();
 
+  if (!dataFileMetadata) {
+    return;
+  }
+
   if (!parsedData) {
     throw new NativeError("Parsed data is required for DataFileView").addContext({
       dataFileMetadata,
