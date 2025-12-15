d8a479dcc8592d85d499a5c301ae494afddcef9b
rohanmathur91 <mathurrohan04@gmail.com>
2025-12-15 11:05:36 +0530

    fix: workspace state

--- app/src/features/apiClient/slices/multiWorkspaceView/types.ts
+++ app/src/features/apiClient/slices/multiWorkspaceView/types.ts
@@ -1,14 +1,12 @@
 import { EntityState } from "@reduxjs/toolkit";
+import { RawResult } from "utils/try";
 
 export enum ApiClientViewMode {
   SINGLE = "SINGLE",
   MULTI = "MULTI",
 }
 
-export type WorkspaceState =
-  | { loading: false; errored: true; error: string }
-  | { loading: true; errored: false }
-  | { loading: false; errored: false };
+export type WorkspaceState = { loading: true } | { loading: false; state: RawResult<null> };
 
 export interface WorkspaceViewState {
   id: string;
