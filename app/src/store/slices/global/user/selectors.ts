a5c34f5b313de167a0d4d545c6dc39c6882c19b8
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2025-12-19 13:38:43 +0530

    [AI-55]: Enable AI consent flow by default (#4057)

--- app/src/store/slices/global/user/selectors.ts
+++ app/src/store/slices/global/user/selectors.ts
@@ -1,13 +1,12 @@
 import { ReducerKeys } from "store/constants";
 import { RootState } from "store/types";
 import { UserAuth } from "./types";
-import { User } from "backend/models/users";
 
 // TODO: Make "user" as a separate slice
 export const getUserAuthDetails = (state: RootState): UserAuth => {
   return state[ReducerKeys.GLOBAL]["user"];
 };
 
-export const getUserMetadata = (state: RootState): User["metadata"] => {
-  return state[ReducerKeys.GLOBAL]["user"].details?.metadata;
+export const getIsOptedforAIFeatures = (state: RootState): boolean => {
+  return state[ReducerKeys.GLOBAL]["user"].details?.metadata?.ai_consent ?? true;
 };
