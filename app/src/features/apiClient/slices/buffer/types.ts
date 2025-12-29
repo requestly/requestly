d49d4a72c50a6957b9fa3f6008cb82821f007209
Saurabh <saurabh.bh@browserstack.com>
2025-12-29 18:31:40 +0530

    ref - migration env view

--- app/src/features/apiClient/slices/buffer/types.ts
+++ app/src/features/apiClient/slices/buffer/types.ts
@@ -1,14 +1,5 @@
 import type { EntityState } from "@reduxjs/toolkit";
 import type { ApiClientEntityType } from "../entities/types";
-import type { RQAPI } from "features/apiClient/types";
-import type { EnvironmentEntity } from "../environments/types";
-
-export type EntityDataMap = {
-  [ApiClientEntityType.HTTP_RECORD]: RQAPI.HttpApiRecord;
-  [ApiClientEntityType.GRAPHQL_RECORD]: RQAPI.GraphQLApiRecord;
-  [ApiClientEntityType.ENVIRONMENT]: EnvironmentEntity;
-  [ApiClientEntityType.GLOBAL_ENVIRONMENT]: EnvironmentEntity;
-};
 
 export interface BufferEntry<T = unknown> {
   readonly id: string;
