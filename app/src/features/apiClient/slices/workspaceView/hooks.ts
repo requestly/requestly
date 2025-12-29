ae5eb7f54ca279e2fdfd339bbd510e6088dd1a5e
rohanmathur91 <mathurrohan04@gmail.com>
2025-12-29 10:40:13 +0530

    add `useGetSingleModeWorkspace`

--- app/src/features/apiClient/slices/workspaceView/hooks.ts
+++ app/src/features/apiClient/slices/workspaceView/hooks.ts
@@ -1,6 +1,8 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { getAllSelectedWorkspaces, getWorkspaceViewSlice } from "./slice";
+import { ApiClientViewMode } from "./types";
+import { NativeError } from "errors/NativeError";
 
 const selectAllSelectedWorkspaces = createSelector([getWorkspaceViewSlice], (slice) => getAllSelectedWorkspaces(slice));
 
@@ -20,4 +22,29 @@ const selectViewMode = createSelector([getWorkspaceViewSlice], (s) => s.viewMode
 
 export function useViewMode() {
   return useSelector(selectViewMode);
+}
+
+const selectSingleModeWorkspace = createSelector([getWorkspaceViewSlice], (s) => {
+  if (s.viewMode !== ApiClientViewMode.SINGLE) {
+    throw new NativeError("Can't use 'useGetSingleModeWorkspace' in single view mode!");
+  }
+
+  const workspaces = getAllSelectedWorkspaces(s);
+
+  if (workspaces.length !== 1) {
+    throw new NativeError("Zero or more than one workspaces found in single view mode!").addContext({
+      workspaceCount: workspaces.length,
+    });
+  }
+
+  const workspace = workspaces[0];
+  if (!workspace) {
+    throw new NativeError("Workspace not found in single view mode!");
+  }
+
+  return workspace;
+});
+
+export function useGetSingleModeWorkspace() {
+  return useSelector(selectSingleModeWorkspace);
 }
