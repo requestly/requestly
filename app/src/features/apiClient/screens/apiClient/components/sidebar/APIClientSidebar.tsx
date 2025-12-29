d17edd50602744753a1d7e6159b9dd397443d84f
rohanmathur91 <mathurrohan04@gmail.com>
2025-12-29 11:12:32 +0530

    fix: sidebar wrapper to consume workspace

--- app/src/features/apiClient/screens/apiClient/components/sidebar/APIClientSidebar.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/APIClientSidebar.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { SingleWorkspaceSidebar } from "./SingleWorkspaceSidebar/SingleWorkspaceSidebar";
-import {
-  ApiClientViewMode,
-  useApiClientMultiWorkspaceView,
-} from "features/apiClient/store/multiWorkspaceView/multiWorkspaceView.store";
+import { ApiClientViewMode } from "features/apiClient/store/multiWorkspaceView/multiWorkspaceView.store";
+import { useViewMode, useGetSingleModeWorkspace } from "features/apiClient/slices/workspaceView/hooks";
 import { MultiWorkspaceSidebar } from "./MultiWorkspaceSidebar/MultiWorkspaceSidebar";
+import { WorkspaceProvider } from "features/apiClient/common/WorkspaceProvider";
+
+const SingleViewSidebarWrapper = () => {
+  const workspace = useGetSingleModeWorkspace();
+
+  return (
+    <WorkspaceProvider workspaceId={workspace.id}>
+      <SingleWorkspaceSidebar />
+    </WorkspaceProvider>
+  );
+};
 
 const APIClientSidebar: React.FC = () => {
-  const viewMode = useApiClientMultiWorkspaceView((s) => s.viewMode);
+  const viewMode = useViewMode();
 
-  return viewMode === ApiClientViewMode.SINGLE ? <SingleWorkspaceSidebar /> : <MultiWorkspaceSidebar />;
+  if (viewMode === ApiClientViewMode.SINGLE) {
+    return <SingleViewSidebarWrapper />;
+  }
+
+  // TODO
+  return <MultiWorkspaceSidebar />;
 };
 
 export default APIClientSidebar;
