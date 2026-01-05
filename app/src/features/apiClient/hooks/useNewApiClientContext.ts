1b2938ccec1913ed6e94d8e171e971519efd3973
rohanmathur91 <mathurrohan04@gmail.com>
2026-01-05 11:23:42 +0530

    fix: rename collection and api

--- app/src/features/apiClient/hooks/useNewApiClientContext.ts
+++ app/src/features/apiClient/hooks/useNewApiClientContext.ts
@@ -1,13 +1,15 @@
 import { useCallback } from "react";
 import { useAPIRecords } from "../store/apiRecords/ApiRecordsContextProvider";
 import { RQAPI } from "../types";
-import { useTabServiceWithSelector } from "componentsV2/Tabs/store/tabServiceStore";
 import { RequestViewTabSource } from "../screens/apiClient/components/views/components/RequestView/requestViewTabSource";
 import { CollectionViewTabSource } from "../screens/apiClient/components/views/components/Collection/collectionViewTabSource";
 import { useApiClientFeatureContext } from "../slices";
+import { saveOrUpdateRecord } from "../commands/store.utils";
+import { useTabActions } from "componentsV2/Tabs/slice";
 
 export function useNewApiClientContext() {
   const context = useApiClientFeatureContext();
+  const { openBufferedTab } = useTabActions();
   // const [openTab] = useTabServiceWithSelector((state) => [state.openTab]);
 
   // const [addNewRecord, updateRecord, updateRecords, addNewRecords, getData] = useAPIRecords((state) => [
@@ -47,46 +49,41 @@ export function useNewApiClientContext() {
 
   const onSaveRecord: (apiClientRecord: RQAPI.ApiClientRecord, onSaveTabAction?: "open") => void = useCallback(
     (apiClientRecord, onSaveTabAction) => {
-      // const recordId = apiClientRecord.id;
-      // const doesRecordExist = !!getData(recordId);
-      // if (doesRecordExist) {
-      //   updateRecord(apiClientRecord);
-      // } else {
-      //   addNewRecord(apiClientRecord);
-      // }
-      // if (onSaveTabAction === "open") {
-      //   if (apiClientRecord.type === RQAPI.RecordType.API) {
-      //     openTab(
-      //       new RequestViewTabSource({
-      //         id: recordId,
-      //         apiEntryDetails: apiClientRecord,
-      //         title: apiClientRecord.name,
-      //         isNewTab: !doesRecordExist,
-      //         context: {
-      //           id: context.workspaceId,
-      //         },
-      //       })
-      //     );
-      //     return;
-      //   }
-      //   if (apiClientRecord.type === RQAPI.RecordType.COLLECTION) {
-      //     openTab(
-      //       new CollectionViewTabSource({
-      //         id: recordId,
-      //         title: apiClientRecord.name,
-      //         isNewTab: !doesRecordExist,
-      //         context: {
-      //           id: context.workspaceId,
-      //         },
-      //       })
-      //     );
-      //     return;
-      //   }
-      // }
+      const recordId = apiClientRecord.id;
+      saveOrUpdateRecord(context, apiClientRecord);
+
+      if (onSaveTabAction === "open") {
+        if (apiClientRecord.type === RQAPI.RecordType.API) {
+          openBufferedTab({
+            // isNew: !doesRecordExist,
+            source: new RequestViewTabSource({
+              id: recordId,
+              apiEntryDetails: apiClientRecord,
+              title: apiClientRecord.name,
+
+              context: {
+                id: context.workspaceId,
+              },
+            }),
+          });
+          return;
+        }
+        if (apiClientRecord.type === RQAPI.RecordType.COLLECTION) {
+          openBufferedTab({
+            // isNewTab: !doesRecordExist,
+            source: new CollectionViewTabSource({
+              id: recordId,
+              title: apiClientRecord.name,
+              context: {
+                id: context.workspaceId,
+              },
+            }),
+          });
+          return;
+        }
+      }
     },
-    [
-      // getData, updateRecord, addNewRecord, openTab, context.workspaceId
-    ]
+    [context, openBufferedTab]
   );
 
   return {
