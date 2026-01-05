934d73e83f3fac69aa5daf9da25f7eb469347acc
TejaChitturi <teja@browserstack.com>
2026-01-05 13:29:59 +0530

    Revert "Revert "[ONCALL-227]: fix Active tab names change to History""
    This reverts commit c804c6a0c5782fbf6c5f3c95099c5322e9df1386.

--- app/src/features/apiClient/screens/apiClient/components/views/graphql/GraphQLClientView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/graphql/GraphQLClientView.tsx
@@ -497,6 +497,14 @@ const GraphQLClientView: React.FC<Props> = ({
   }, [setIcon]);
 
   useEffect(() => {
+    if (isHistoryView) {
+      setTitle("History");
+    } else {
+      setTitle(record.name || "Untitled request");
+    }
+  }, [setTitle, isHistoryView, record.name]);
+
+  useEffect(() => {
     setUnsaved(hasUnsavedChanges);
   }, [hasUnsavedChanges, setUnsaved]);
 
