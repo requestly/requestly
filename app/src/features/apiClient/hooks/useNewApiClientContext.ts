2288539ca969327df83d3629b0b7609a458eda92
rohanmathur91 <mathurrohan04@gmail.com>
2026-01-05 12:31:20 +0530

    add duplicate records thunk

--- app/src/features/apiClient/hooks/useNewApiClientContext.ts
+++ app/src/features/apiClient/hooks/useNewApiClientContext.ts
@@ -1,5 +1,4 @@
 import { useCallback } from "react";
-import { useAPIRecords } from "../store/apiRecords/ApiRecordsContextProvider";
 import { RQAPI } from "../types";
 import { RequestViewTabSource } from "../screens/apiClient/components/views/components/RequestView/requestViewTabSource";
 import { CollectionViewTabSource } from "../screens/apiClient/components/views/components/Collection/collectionViewTabSource";
@@ -10,42 +9,6 @@ import { useTabActions } from "componentsV2/Tabs/slice";
 export function useNewApiClientContext() {
   const context = useApiClientFeatureContext();
   const { openBufferedTab } = useTabActions();
-  // const [openTab] = useTabServiceWithSelector((state) => [state.openTab]);
-
-  // const [addNewRecord, updateRecord, updateRecords, addNewRecords, getData] = useAPIRecords((state) => [
-  //   state.addNewRecord,
-  //   state.updateRecord,
-  //   state.updateRecords,
-  //   state.addNewRecords,
-  //   state.getData,
-  // ]);
-
-  const onSaveBulkRecords = useCallback(
-    (records: RQAPI.ApiClientRecord[]) => {
-      const existingRecords: RQAPI.ApiClientRecord[] = [];
-      const newRecords: RQAPI.ApiClientRecord[] = [];
-
-      // records.forEach((record) => {
-      //   const doesRecordExist = !!getData(record.id);
-      //   if (doesRecordExist) {
-      //     existingRecords.push(record);
-      //   } else {
-      //     newRecords.push(record);
-      //   }
-      // });
-
-      // if (existingRecords.length > 0) {
-      //   updateRecords(existingRecords);
-      // }
-
-      // if (newRecords.length > 0) {
-      //   // addNewRecords(newRecords);
-      // }
-    },
-    [
-      // updateRecords, addNewRecords, getData
-    ]
-  );
 
   const onSaveRecord: (apiClientRecord: RQAPI.ApiClientRecord, onSaveTabAction?: "open") => void = useCallback(
     (apiClientRecord, onSaveTabAction) => {
@@ -88,6 +51,5 @@ export function useNewApiClientContext() {
 
   return {
     onSaveRecord,
-    onSaveBulkRecords,
   };
 }
