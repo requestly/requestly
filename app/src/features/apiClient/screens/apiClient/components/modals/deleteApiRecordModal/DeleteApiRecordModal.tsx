102071a8698ef5f5f4cf698c0e1f7e9d7173e8d9
rohanmathur91 <mathurrohan04@gmail.com>
2026-01-05 11:23:42 +0530

    fix: rename collection and api

--- app/src/features/apiClient/screens/apiClient/components/modals/deleteApiRecordModal/DeleteApiRecordModal.tsx
+++ app/src/features/apiClient/screens/apiClient/components/modals/deleteApiRecordModal/DeleteApiRecordModal.tsx
@@ -11,6 +11,7 @@ import { isApiCollection } from "../../../utils";
 import * as Sentry from "@sentry/react";
 import { useTabActions } from "componentsV2/Tabs/slice";
 import { ApiClientFeatureContext } from "features/apiClient/slices";
+import { useApiClientDispatch } from "features/apiClient/slices/hooks/base.hooks";
 
 interface DeleteApiRecordModalProps {
   open: boolean;
@@ -28,6 +29,7 @@ export const DeleteApiRecordModal: React.FC<DeleteApiRecordModalProps> = ({
   onClose,
   onSuccess,
 }) => {
+  const dispatch = useApiClientDispatch();
   const { closeTabByEntityId } = useTabActions();
 
   const recordsWithContext = useMemo(() => {
@@ -52,13 +54,13 @@ export const DeleteApiRecordModal: React.FC<DeleteApiRecordModalProps> = ({
       recordsWithContext.forEach(async ({ context, records }) => {
         if (!context) return;
 
-        const { dispatch } = context.store;
+        // const { dispatch } = context.store;
         const { apiClientRecordsRepository } = context.repositories;
         const result = await dispatch(
           deleteRecords({
             records,
             repository: apiClientRecordsRepository,
-          }) as any
+          })
         ).unwrap();
 
         const { deletedApiRecords, deletedCollectionRecords } = result;
