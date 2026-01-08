2dfc17e0f25407ff4a5337c9c74a91c7ca761fb4
Sahil Gupta <wrongsahil@gmail.com>
2026-01-08 19:36:02 +0530

    add Sentry Custom Span decorator

--- app/src/utils/sentry.ts
+++ app/src/utils/sentry.ts
@@ -27,3 +27,28 @@ export const wrapWithCustomSpan = <T, Args extends any[] = []>(
     );
   };
 };
+
+// WIP - Needs detailed testing
+/**
+ * Decorator that wraps a method with Sentry span tracking
+ **/
+export const SentryCustomSpan = (options: Parameters<typeof Sentry.startSpan>[0]) => {
+  return <T, Args extends any[] = []>(
+    target: any,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<(...args: Args) => T>
+  ): TypedPropertyDescriptor<(...args: Args) => T> => {
+    const originalMethod = descriptor.value;
+
+    if (!originalMethod) {
+      return descriptor;
+    }
+
+    descriptor.value = function (...args: Args): T {
+      const wrapped = wrapWithCustomSpan(options, originalMethod.bind(this) as (...args: Args) => T);
+      return wrapped(...args);
+    };
+
+    return descriptor;
+  };
+};
