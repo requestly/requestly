ab302fda55b672ad4916e87b743c17f943f8eef3
dinex-dev <dineshthakur.dev@gmail.com>
2026-01-08 11:21:25 +0530

    remove commented out sorting logic

--- app/src/features/apiClient/commands/utils.ts
+++ app/src/features/apiClient/commands/utils.ts
@@ -54,12 +54,7 @@ export function prepareRecordsToRender(records: RQAPI.ApiClientRecord[]) {
       return 1;
     }
 
-    // If different type, then keep collection first
-    // if (recordA.type !== recordB.type) {
-    //   return recordA.type === RQAPI.RecordType.COLLECTION ? -1 : 1;
-    // }
-
-    // For same type, use ranking manager to sort by rank
+    // Use ranking manager to sort by rank
     const aRank = apiRecordsRankingManager.getEffectiveRank(recordA);
     const bRank = apiRecordsRankingManager.getEffectiveRank(recordB);
     return apiRecordsRankingManager.compareFn(aRank, bRank);
