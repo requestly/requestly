bf4da3b806cef562aecfe1a0b298cc1840bca776
rohanmathur91 <mathurrohan04@gmail.com>
2026-01-12 17:08:28 +0530

    fix: draft view

--- app/src/features/apiClient/screens/apiClient/clientView/ClientViewFactory.tsx
+++ app/src/features/apiClient/screens/apiClient/clientView/ClientViewFactory.tsx
@@ -1,47 +1,40 @@
 import { RQAPI } from "features/apiClient/types";
 import HttpClientView, { HttpClientViewProps } from "../components/views/http/HttpClientView";
+import GraphQLClientView, { GraphQLClientViewProps } from "../components/views/graphql/GraphQLClientView";
 import { BufferedGraphQLRecordEntity, BufferedHttpRecordEntity } from "features/apiClient/slices/entities";
 import { ApiClientEntityType } from "features/apiClient/slices/entities/types";
 
 interface Props {
-  entity: BufferedHttpRecordEntity | BufferedGraphQLRecordEntity,
+  entity: BufferedHttpRecordEntity | BufferedGraphQLRecordEntity;
   handleRequestFinished: (entry: RQAPI.ApiEntry) => void;
   isOpenInModal?: boolean;
-  override?: HttpClientViewProps['override'],
+  override?: HttpClientViewProps["override"] | GraphQLClientViewProps["override"];
 }
 
-export const ClientViewFactory = ({
-  entity,
-  handleRequestFinished,
-  isOpenInModal,
-  override,
-}: Props) => {
+export const ClientViewFactory = ({ entity, handleRequestFinished, isOpenInModal, override }: Props) => {
   if (entity.type === ApiClientEntityType.HTTP_RECORD) {
     return (
       <HttpClientView
         entity={entity}
         notifyApiRequestFinished={handleRequestFinished}
         openInModal={isOpenInModal}
-        override={override}
+        override={override as HttpClientViewProps["override"]}
       />
     );
   }
 
-  return 'wtf!';
+  if (entity.type === ApiClientEntityType.GRAPHQL_RECORD) {
+    return (
+      <GraphQLClientView
+        entity={entity}
+        notifyApiRequestFinished={handleRequestFinished}
+        openInModal={isOpenInModal}
+        override={override as GraphQLClientViewProps["override"]}
+      />
+    );
+  }
 
-  // if (isGraphQLApiRecord(apiRecord)) {
-  //   return (
-  //     <GraphQLClientView
-  //       recordId={apiRecord.id}
-  //       apiEntryDetails={apiRecord}
-  //       notifyApiRequestFinished={handleRequestFinished}
-  //       onSaveCallback={onSaveCallback}
-  //       isCreateMode={isCreateMode}
-  //       openInModal={isOpenInModal}
-  //     />
-  //   );
-  // }
-
+  return "Not found";
   // return (
   //   <HttpClientView
   //     apiEntryDetails={apiRecord as RQAPI.HttpApiRecord}
--- app/src/features/apiClient/screens/apiClient/components/views/components/DraftRequestContainer/DraftRequestView.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/DraftRequestContainer/DraftRequestView.tsx
@@ -1,77 +1,19 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { RQAPI } from "features/apiClient/types";
-import { useHostContext } from "hooks/useHostContext";
-import { bufferActions, EntityNotFound, useApiClientRepository, useApiClientStore } from "features/apiClient/slices";
-import { useOriginUndefinedBufferedEntity } from "features/apiClient/slices/entities/hooks";
-import { ApiClientEntityType } from "features/apiClient/slices/entities/types";
-import { notification } from "antd";
-import { ApiClientRepositoryInterface } from "features/apiClient/helpers/modules/sync/interfaces";
-import { NativeError } from "errors/NativeError";
-import { GenericApiClient } from "features/apiClient/screens/apiClient/clientView/GenericApiClient";
-import { useApiClientDispatch } from "features/apiClient/slices/hooks/base.hooks";
+import { DraftHttpRequestView } from "./DraftHttpRequestView";
+import { DraftGraphQLRequestView } from "./DraftGraphQLRequestView";
 
 export const DraftRequestView: React.FC<{
   onSaveCallback: (apiEntryDetails: RQAPI.ApiRecord) => void;
-}> = ({ onSaveCallback }) => {
-  const bufferId = useHostContext().getBufferId();
-  if (!bufferId) {
-    throw new EntityNotFound('buffer_id', 'buffer');
+  apiEntryType: RQAPI.ApiEntryType;
+}> = ({ onSaveCallback, apiEntryType }) => {
+  if (apiEntryType === RQAPI.ApiEntryType.HTTP) {
+    return <DraftHttpRequestView onSaveCallback={onSaveCallback} />;
   }
-  const scratchBuffer = useOriginUndefinedBufferedEntity<ApiClientEntityType.HTTP_RECORD>({
-    bufferId
-  });
 
-  const repositories = useApiClientRepository();
-  const store = useApiClientStore();
-  const dispatch = useApiClientDispatch();
+  if (apiEntryType === RQAPI.ApiEntryType.GRAPHQL) {
+    return <DraftGraphQLRequestView onSaveCallback={onSaveCallback} />;
+  }
 
-  const handleRecordNameUpdate = useCallback(async (name: string) => {
-    const scratchRecord = scratchBuffer.getEntityFromState(store.getState());
-    const dataToSave: Partial<RQAPI.ApiRecord> = {
-      ...scratchRecord,
-      name
-    }
-    const result = await repositories.apiClientRecordsRepository.createRecord(dataToSave);
-    if (!result.success) {
-      notification.error({
-        message: "Could not rename record",
-        description: result?.message,
-        placement: "bottomRight",
-      });
-      return;
-    }
-    scratchBuffer.origin.upsert(result.data as RQAPI.HttpApiRecord);
-    dispatch(bufferActions.markSaved({
-      id: scratchBuffer.id,
-      savedData: result.data,
-      referenceId: result.data.id,
-    }));
-    onSaveCallback(result.data as RQAPI.ApiRecord);
-  }, [scratchBuffer, repositories, store, onSaveCallback, dispatch]);
-
-  const save = useCallback(async (record: RQAPI.HttpApiRecord, repositories: ApiClientRepositoryInterface) => {
-    const result = await repositories.apiClientRecordsRepository.createRecord(record);
-    if (!result.success) {
-      throw new NativeError(result.message || "Could not save request!");
-    }
-    return result.data as RQAPI.HttpApiRecord;
-
-  }, []);
-
-
-  return (
-    <GenericApiClient
-      override={{
-        handleNameChange: handleRecordNameUpdate,
-
-        onSaveClick: {
-          save,
-          onSuccess: onSaveCallback,
-        }
-      }}
-
-      entity={scratchBuffer}
-      handleAppRequestFinished={() => { }}
-    />
-  );
+  return null;
 };
