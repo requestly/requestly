3376e08a0562d91689597bf665e5b2693934eaa0
Saurabh <167960286+srbh777@users.noreply.github.com>
2026-01-12 13:01:30 +0530

    [PLAT-33] fix: api client importer and exporters to use RTK  (#4141)
    * exporter
    
    * importer
    
    * remove `any`
    
    * fix: type
    
    * fix
    
    ---------
    
    Co-authored-by: rohanmathur91 <mathurrohan04@gmail.com>

--- app/src/features/apiClient/slices/apiRecords/types.ts
+++ app/src/features/apiClient/slices/apiRecords/types.ts
@@ -1,10 +1,8 @@
 import { EntityState } from "@reduxjs/toolkit";
 import { RQAPI } from "features/apiClient/types";
 import { TreeIndices } from "../types";
-import { ErroredRecord } from "features/apiClient/helpers/modules/sync/local/services/types";
 
 export interface ApiRecordsState {
-  erroredRecords: ErroredRecord[];
   records: EntityState<RQAPI.ApiClientRecord>;
   tree: TreeIndices;
 }
