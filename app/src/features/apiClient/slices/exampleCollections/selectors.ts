36a16ae4eb3bccfdf6fd7e7d50be3f0f7c3790c5
Saurabh <saurabh.bh@browserstack.com>
2026-01-12 15:29:53 +0530

    clean up

--- app/src/features/apiClient/slices/exampleCollections/selectors.ts
+++ app/src/features/apiClient/slices/exampleCollections/selectors.ts
@@ -1,18 +1,8 @@
 import { RootState } from "store/types";
 
-export const selectExampleCollectionsState = (state: RootState) => state.exampleCollections;
-
 export const selectIsNudgePermanentlyClosed = (state: RootState) => state.exampleCollections.isNudgePermanentlyClosed;
 
-export const selectImportStatus = (state: RootState) => state.exampleCollections.importStatus;
-
-export const selectIsImported = (state: RootState) => state.exampleCollections.importStatus.type === "IMPORTED";
-
 export const selectIsImporting = (state: RootState) => state.exampleCollections.importStatus.type === "IMPORTING";
-
-export const selectIsFailed = (state: RootState) => state.exampleCollections.importStatus.type === "FAILED";
-
-export const selectIsNotImported = (state: RootState) => state.exampleCollections.importStatus.type === "NOT_IMPORTED";
 
 export const selectShouldShowNudge = (state: RootState, uid: string | undefined): boolean => {
   const { isNudgePermanentlyClosed, importStatus } = state.exampleCollections;
@@ -22,14 +12,4 @@ export const selectShouldShowNudge = (state: RootState, uid: string | undefined)
   if (importStatus.type === "IMPORTED") return false;
 
   return true;
-};
-
-export const selectImportError = (state: RootState): string | null => {
-  const status = state.exampleCollections.importStatus;
-  return status.type === "FAILED" ? status.error : null;
-};
-
-export const selectImportedAt = (state: RootState): number | null => {
-  const status = state.exampleCollections.importStatus;
-  return status.type === "IMPORTED" ? status.importedAt : null;
 };
