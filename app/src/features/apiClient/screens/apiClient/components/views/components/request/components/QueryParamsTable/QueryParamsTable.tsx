0ebcfa96c5ad6e5b18bc4fde93d2545224f12d2e
Shubhranshu Dash <shubhranshu@browserstack.com>
2026-01-14 11:44:23 +0530

    fix: added tableType in key value tables

--- app/src/features/apiClient/screens/apiClient/components/views/components/request/components/QueryParamsTable/QueryParamsTable.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/request/components/QueryParamsTable/QueryParamsTable.tsx
@@ -38,6 +38,7 @@ function Data(props: {
         dataType: { visible: true },
       }}
       useStore={useQueryParamStore}
+      tableType="params"
     >
       {props.children}
     </KeyValueTable>
