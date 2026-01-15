d65a223a805e942f5111758bc37b2462af5c2b3b
Rohan Mathur <mathurrohan04@gmail.com>
2026-01-15 13:37:17 +0530

    Revert "[PLAT-33] fix: api client importer and exporters to use RTK  (#4141)"
    This reverts commit da10946e0b3387e82daadf317fad9649801427dd.

--- app/src/features/apiClient/slices/apiRecords/types.ts
+++ app/src/features/apiClient/slices/apiRecords/types.ts
@@ -1,8 +1,10 @@
 import { EntityState } from "@reduxjs/toolkit";
 import { RQAPI } from "features/apiClient/types";
 import { TreeIndices } from "../types";
+import { ErroredRecord } from "features/apiClient/helpers/modules/sync/local/services/types";
 
 export interface ApiRecordsState {
+  erroredRecords: ErroredRecord[];
   records: EntityState<RQAPI.ApiClientRecord>;
   tree: TreeIndices;
 }
