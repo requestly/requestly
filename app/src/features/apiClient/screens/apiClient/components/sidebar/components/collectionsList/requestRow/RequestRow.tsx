812261d5bc57c66951022f657e376fee6082b501
dinex-dev <dineshthakur.dev@gmail.com>
2026-01-22 16:35:58 +0530

    Move GetRankForDroppedRecords to features/apiClient/helpers/RankingManager

--- app/src/features/apiClient/screens/apiClient/components/sidebar/components/collectionsList/requestRow/RequestRow.tsx
+++ app/src/features/apiClient/screens/apiClient/components/sidebar/components/collectionsList/requestRow/RequestRow.tsx
@@ -34,7 +34,7 @@ import { saveOrUpdateRecord } from "features/apiClient/commands/store.utils";
 import clsx from "clsx";
 import { isFeatureCompatible } from "utils/CompatibilityUtils";
 import FEATURES from "config/constants/sub/features";
-import { getRankForDroppedRecord } from "../dropHelper/getRankForDroppedRecord";
+import { getRankForDroppedRecord } from "features/apiClient/helpers/RankingManager/getRankForDroppedRecord";
 
 interface Props {
   record: RQAPI.ApiRecord;
