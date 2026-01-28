260a59255cd36beaf00d11a5fa3862cd2aa7acda
Ajinkya Vidwans <ajinkya@browserstack.com>
2026-01-28 17:25:46 +0530

    feat: [init] Event stream core implementation

---
+++ app/src/lib/eventStream/APILogger.ts
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { EventLogger } from "./EventLogger";
+import { APIClientEvent } from "features/apiClient/eventStream/types";
+import { eventsActions } from "store/slices/eventsStream/slice";
+
+export class APILogger extends EventLogger {
+  private dispatch: ReturnType<typeof useDispatch>;
+
+  constructor(dispatch: ReturnType<typeof useDispatch>) {
+    super();
+    this.dispatch = dispatch;
+  }
+
+  pushEvent(event: APIClientEvent): void {
+    this.dispatch(eventsActions.eventAdd(event));
+  }
+}
+
+// Hook to create an APILogger instance with dispatch
+export const useAPILogger = (): APILogger => {
+  const dispatch = useDispatch();
+  return new APILogger(dispatch);
+};
