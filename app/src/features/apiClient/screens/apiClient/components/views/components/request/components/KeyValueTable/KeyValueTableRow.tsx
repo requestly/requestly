82f3f646e4ab8ef1ae1fb04615c7a888faa7f88c
Ajinkya Vidwans <ajinkya@browserstack.com>
2026-02-10 17:42:25 +0530

    [WIP] feat: Add Antd Autocomplete to Header Key Field

--- app/src/features/apiClient/screens/apiClient/components/views/components/request/components/KeyValueTable/KeyValueTableRow.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/request/components/KeyValueTable/KeyValueTableRow.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Checkbox, Form, FormInstance } from "antd";
+import { AutoComplete, Checkbox, Form, FormInstance, Input } from "antd";
 import { KeyValueDataType, KeyValuePair } from "features/apiClient/types";
 import SingleLineEditor from "features/apiClient/screens/environment/components/SingleLineEditor";
 import InfoIcon from "components/misc/InfoIcon";
@@ -9,6 +9,7 @@ import { ScopedVariables } from "features/apiClient/helpers/variableResolver/var
 import KeyValueDescriptionCell from "./KeyValueTableDescriptionCell";
 import { KeyValueTypeCell, ValidationWarning } from "./KeyValueTableTypeCell";
 import { captureException } from "@sentry/react";
+import HEADER_SUGGESTIONS from "config/constants/sub/header-suggestions";
 
 const EditableContext = React.createContext<FormInstance<any> | null>(null);
 
@@ -36,6 +37,7 @@ interface EditableCellProps {
   handleUpdatePair: (record: KeyValuePair) => void;
   checkInvalidCharacter: boolean;
   error?: string | null;
+  tableType?: string;
 }
 
 export const KeyValueTableEditableCell: React.FC<React.PropsWithChildren<EditableCellProps>> = ({
@@ -48,6 +50,7 @@ export const KeyValueTableEditableCell: React.FC<React.PropsWithChildren<Editabl
   handleUpdatePair,
   checkInvalidCharacter,
   error,
+  tableType,
   ...restProps
 }) => {
   const form = useContext(EditableContext);
@@ -116,16 +119,48 @@ export const KeyValueTableEditableCell: React.FC<React.PropsWithChildren<Editabl
           }
         `}
           >
-            <SingleLineEditor
-              className={`key-value-table-input ${record.isEnabled === false ? "key-value-table-input-disabled" : ""}`}
-              placeholder={dataIndex === "key" ? "Key" : "Value"}
-              defaultValue={record?.[dataIndex] as string}
-              onChange={(value) => {
-                form.setFieldsValue({ [dataIndex]: value });
-                save();
-              }}
-              variables={variables}
-            />
+            {tableType === "Headers" && dataIndex === "key" ? (
+              <AutoComplete
+                options={HEADER_SUGGESTIONS.Request}
+                onChange={(value) => {
+                  form.setFieldsValue({ [dataIndex]: value?.trim() });
+                  save();
+                }}
+                filterOption={(input, option) =>
+                  option?.value ? option.value.toLowerCase().includes(input.toLowerCase()) : false
+                }
+                value={record?.[dataIndex] as string}
+                style={{ width: "100%" }}
+              >
+                <Input
+                  className={`key-value-table-input ${
+                    record.isEnabled === false ? "key-value-table-input-disabled" : ""
+                  }`}
+                  placeholder="Header Name"
+                  style={{
+                    width: "100%",
+                    border: "1px solid transparent",
+                    background: "transparent",
+                    padding: "4px 0px",
+                    height: "24px",
+                    fontSize: "var(--requestly-font-size-sm)",
+                  }}
+                />
+              </AutoComplete>
+            ) : (
+              <SingleLineEditor
+                className={`key-value-table-input ${
+                  record.isEnabled === false ? "key-value-table-input-disabled" : ""
+                }`}
+                placeholder={dataIndex === "key" ? "Key" : "Value"}
+                defaultValue={record?.[dataIndex] as string}
+                onChange={(value) => {
+                  form.setFieldsValue({ [dataIndex]: value });
+                  save();
+                }}
+                variables={variables}
+              />
+            )}
 
             <Conditional
               condition={INVALID_KEY_CHARACTERS.test(record?.key) && dataIndex === "key" && checkInvalidCharacter}
