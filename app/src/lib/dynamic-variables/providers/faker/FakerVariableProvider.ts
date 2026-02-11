4c9f07e1db4d636ccfaf0c31ed770c548fb7c7cf
rohanmathur91 <mathurrohan04@gmail.com>
2026-02-11 13:50:02 +0530

    add faker provider

--- app/src/lib/dynamic-variables/providers/faker/FakerVariableProvider.ts
+++ app/src/lib/dynamic-variables/providers/faker/FakerVariableProvider.ts
@@ -11,5 +11,4 @@ export class FakerVariableProvider extends DynamicVariableProvider {
     super();
     const fakerVariables = createFakerVariables(faker);
     this.variableNames = new Map(fakerVariables.map((v) => [v.name, v]));
-  }
 }
