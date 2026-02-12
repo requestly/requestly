a9bf37bf85fafe9e055a97e8224aade282649a63
rohanmathur91 <mathurrohan04@gmail.com>
2026-02-12 11:33:17 +0530

    update dynamic variable resolver instance name

--- app/src/lib/dynamic-variables/index.ts
+++ app/src/lib/dynamic-variables/index.ts
@@ -2,4 +2,4 @@ import { FakerVariableProvider } from "./providers/faker/FakerVariableProvider";
 import { HandlebarsResolver } from "./resolvers/handlebars";
 
 const fakerProvider = new FakerVariableProvider();
-export const dynamicVariables = new HandlebarsResolver(fakerProvider);
+export const dynamicVariableResolver = new HandlebarsResolver(fakerProvider);
