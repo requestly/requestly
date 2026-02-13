fcdf36c4b55f253bb56f9ea417e2311c9a08782a
Rohan Mathur <mathurrohan04@gmail.com>
2026-02-13 13:40:23 +0530

    [ENGG-5300] feat: add support to resolve dynamic variables in request (#4333)
    * add faker provider
    
    * refactor
    
    * add dynamic variables support in pre and post response scripts
    
    * update dynamic variable resolver instance name
    
    * fix: handlebars context
    
    * fix: dynamic variable generation
    
    * fix: add dynamic variable support for in request
    
    * add migration for dynamic variables in postman import
    
    * undo
    
    * refactor
    
    * refactor
    
    * fix: import

--- app/src/lib/dynamic-variables/errors/index.ts
+++ app/src/lib/dynamic-variables/errors/index.ts
@@ -5,3 +5,10 @@ export class DynamicVariableNotFoundError extends NativeError {
     super(`${name} dynamic variable not found!`);
   }
 }
+
+export class DynamicVariableValueError extends NativeError {
+  constructor(name: string, e: Error) {
+    super(`Something went wrong while generating value for ${name} dynamic variable!`);
+    this.set("stack", e.stack);
+  }
+}
