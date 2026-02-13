e1a93a570a2d7251f812bbe7adcc297a13d0353d
Rohan Mathur <mathurrohan04@gmail.com>
2026-02-13 12:32:54 +0530

    [ENGG-5243]: dynamic variables provider and resolver (#4324)
    * add DynamicVariableProvider
    
    * add faker provider
    
    * add DynamicVariableResolver
    
    * add resolver
    
    * add comment
    
    * fix: generate method
    
    * refactor
    
    * add comments
    
    * [ENGG-5246] feat: add dynamic variable support in api client pre and post response script (#4326)
    
    * add dynamic variables support in pre and post response scripts
    
    * update dynamic variable resolver instance name

---
+++ app/src/lib/dynamic-variables/index.ts
@@ -0,0 +1,5 @@
+import { FakerVariableProvider } from "./providers/faker/FakerVariableProvider";
+import { HandlebarsResolver } from "./resolvers/handlebars";
+
+const fakerProvider = new FakerVariableProvider();
+export const dynamicVariableResolver = new HandlebarsResolver(fakerProvider);
