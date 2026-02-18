83e9c75b4d1db9217b96bd40dcd8682449dff7fe
Aarushsr12 <aarushsrivastava1212@gmail.com>
2026-02-18 08:54:45 +0530

    feat: detect vault variables

--- app/src/modules/analytics/events/features/apiClient/index.js
+++ app/src/modules/analytics/events/features/apiClient/index.js
@@ -176,6 +176,10 @@ export const trackImportSuccess = (type, collectionsCount, requestsCount) => {
   });
 };
 
+export const trackPostmanUnsupportedFeatures = (params) => {
+  trackEvent(API_CLIENT.POSTMAN_UNSUPPORTED_FEATURES, params);
+};
+
 export const trackImportFailed = (type, reason) => {
   trackEvent(API_CLIENT.IMPORT_FAILED, { import_type: type, reason });
 };
