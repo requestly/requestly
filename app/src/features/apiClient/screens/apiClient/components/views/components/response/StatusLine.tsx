8b5a26d6586f115ce42d7d9c6af3e6443c2a68d5
Parth Bhardwaj <parthb.git@gmail.com>
2026-02-18 23:17:34 +0530

    ENGG-5256: Add Save response button in response panel

--- app/src/features/apiClient/screens/apiClient/components/views/components/response/StatusLine.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/response/StatusLine.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { RQAPI } from "../../../../../../types";
 import { Popover, Space } from "antd";
 import PropertyRow from "./PropertyRow/PropertyRow";
@@ -6,12 +6,27 @@ import { statusCodes } from "config/constants/sub/statusCode";
 import NetworkStatusField from "components/misc/NetworkStatusField";
 import { MdOutlineSwapCalls } from "@react-icons/all-files/md/MdOutlineSwapCalls";
 import { isHttpResponse } from "features/apiClient/screens/apiClient/utils";
+import { RQButton } from "lib/design-system-v2/components";
+import { MdOutlineDashboardCustomize } from "@react-icons/all-files/md/MdOutlineDashboardCustomize";
+import { BufferedGraphQLRecordEntity, BufferedHttpRecordEntity } from "features/apiClient/slices/entities";
+import { createExampleRequest, useApiClientFeatureContext } from "features/apiClient/slices";
+import { useTabActions } from "componentsV2/Tabs/slice";
+import { ExampleViewTabSource } from "../ExampleRequestView/exampleViewTabSource";
+import { toast } from "utils/Toast";
 
 interface Props {
   response: RQAPI.Response;
+  entity: BufferedHttpRecordEntity | BufferedGraphQLRecordEntity;
 }
 
-const StatusLine: React.FC<Props> = ({ response }) => {
+const StatusLine: React.FC<Props> = ({ response, entity }) => {
+  const context = useApiClientFeatureContext();
+  const { openBufferedTab } = useTabActions();
+
+  const [isSavingAsExample, setIsSavingAsExample] = useState(false);
+
+  const entityType = entity.getType(context.store.getState());
+
   const formattedTime = useMemo(() => {
     if (response?.time) {
       const ms = Math.ceil(response.time);
@@ -31,6 +46,44 @@ const StatusLine: React.FC<Props> = ({ response }) => {
     return response?.statusText || statusCodes[response?.status];
   }, [response?.status, response?.statusText]);
 
+  const handleSaveResponseClick = useCallback(async () => {
+    try {
+      setIsSavingAsExample(true);
+      const requestRecord = entity.getEntityFromState(context.store.getState());
+      const exampleRecordToCreate: RQAPI.ExampleApiRecord = {
+        ...requestRecord,
+        parentRequestId: entity.meta.referenceId,
+        type: RQAPI.RecordType.EXAMPLE_API,
+      };
+      exampleRecordToCreate.collectionId = null;
+
+      const { exampleRecord } = await context.store
+        .dispatch(
+          createExampleRequest({
+            parentRequestId: entity.meta.referenceId,
+            example: exampleRecordToCreate,
+            repository: context.repositories.apiClientRecordsRepository,
+          }) as any
+        )
+        .unwrap();
+
+      openBufferedTab({
+        preview: false,
+        source: new ExampleViewTabSource({
+          id: exampleRecord.id,
+          title: exampleRecord.name || "Example",
+          apiEntryDetails: exampleRecord,
+          context: { id: context.workspaceId },
+        }),
+      });
+      toast.success("Response saved as example successfully.");
+    } catch (error) {
+      toast.error("Something went wrong while saving the response as example.");
+    } finally {
+      setIsSavingAsExample(false);
+    }
+  }, [context.repositories.apiClientRecordsRepository, context.store, context.workspaceId, entity, openBufferedTab]);
+
   if (!response) {
     return null;
   }
@@ -45,11 +98,24 @@ const StatusLine: React.FC<Props> = ({ response }) => {
         </Popover>
       )}
       <PropertyRow
-        name="Status"
+        name={`Status: ${formattedStatusText}`}
         className="api-response-status-row__status"
-        value={<NetworkStatusField status={response.status} statusText={formattedStatusText} />}
+        value={<NetworkStatusField status={response.status} />}
       />
       <PropertyRow className="api-response-status-row__time" name="Time" value={formattedTime} />
+      {entityType === RQAPI.RecordType.API && (
+        <div className="api-response-status-row__save-button-wapper">
+          <RQButton
+            size="small"
+            type="transparent"
+            icon={<MdOutlineDashboardCustomize />}
+            onClick={handleSaveResponseClick}
+            loading={isSavingAsExample}
+          >
+            Save
+          </RQButton>
+        </div>
+      )}
     </Space>
   );
 };
