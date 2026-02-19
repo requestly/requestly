906c8e7b0bce509d940a8751e34467c2371c18db
Aarushsr12 <aarushsrivastava1212@gmail.com>
2026-02-20 02:56:01 +0530

    cleanups

--- app/src/features/apiClient/screens/apiClient/components/views/graphql/utils.ts
+++ app/src/features/apiClient/screens/apiClient/components/views/graphql/utils.ts
@@ -81,9 +81,6 @@ export const graphQLRequestToHttpRequestAdapter = (request: RQAPI.GraphQLRequest
 };
 
 export const httpRequestToGraphQLRequestAdapter = (request: RQAPI.HttpRequest): RQAPI.GraphQLRequest => {
-  // default setting to Post
-  // adapter to convert graphQL to http request
-
   if (isEmpty(request)) {
     throw new Error("HTTP request cannot be empty");
   }
