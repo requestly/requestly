25592a4755746ffcd267fa25b5b526e1f5e6c769
dinex-dev <dineshthakur.dev@gmail.com>
2026-02-26 13:10:11 +0530

    refactor: Make response body container common

--- app/src/features/apiClient/screens/apiClient/components/views/components/response/ResponseBody/ResponseBody.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/response/ResponseBody/ResponseBody.tsx
@@ -69,38 +69,32 @@ const ResponseBody: React.FC<Props> = ({ responseText, contentTypeHeader }) => {
   }, [onResponseModeChange, responseMode]);
 
   const preview = useMemo<ReactElement>(() => {
+    let previewComponent: ReactElement | null = null;
     if (contentTypeHeader?.includes("text/html")) {
+      previewComponent = <HTMLResponsePreview responseText={responseText} />;
+    } else if (contentTypeHeader?.includes("image/")) {
+      previewComponent = <ImageResponsePreview responseText={responseText} mimeType={contentTypeHeader} />;
+    } else {
+      const editorLanguage = getEditorLanguageFromContentType(contentTypeHeader);
       return (
         <div className="api-response-body-editor-container">
-          <div>{bodyPreviewModeOptions}</div>
-          <HTMLResponsePreview responseText={responseText} />
+          <Editor
+            prettifyOnInit
+            value={shouldRenderPreview(responseText, contentTypeHeader) ? responseText : "Preview not available"}
+            language={editorLanguage}
+            isReadOnly
+            toolbarOptions={{
+              title: "",
+              options: [bodyPreviewModeOptions],
+            }}
+          />
         </div>
       );
     }
-
-    if (contentTypeHeader?.includes("image/")) {
-      return (
-        <div className="api-response-body-editor-container">
-          <div>{bodyPreviewModeOptions}</div>
-          <ImageResponsePreview responseText={responseText} mimeType={contentTypeHeader} />
-        </div>
-      );
-    }
-
-    const editorLanguage = getEditorLanguageFromContentType(contentTypeHeader);
-
     return (
       <div className="api-response-body-editor-container">
-        <Editor
-          prettifyOnInit
-          value={shouldRenderPreview(responseText, contentTypeHeader) ? responseText : "Preview not available"}
-          language={editorLanguage}
-          isReadOnly
-          toolbarOptions={{
-            title: "",
-            options: [bodyPreviewModeOptions],
-          }}
-        />
+        <div>{bodyPreviewModeOptions}</div>
+        {previewComponent}
       </div>
     );
   }, [contentTypeHeader, responseText, bodyPreviewModeOptions]);
