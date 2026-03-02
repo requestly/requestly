c8a554890bd70ebc9a6b3636cd251c7c7f08eb8b
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2026-03-02 18:10:09 +0530

    [ENGG-5271]: Example import/export support (#4455)

--- app/src/features/apiClient/screens/apiClient/components/modals/postmanCollectionExportModal/PostmanCollectionExportModal.tsx
+++ app/src/features/apiClient/screens/apiClient/components/modals/postmanCollectionExportModal/PostmanCollectionExportModal.tsx
@@ -12,7 +12,7 @@ import {
   trackExportCollectionsClicked,
 } from "modules/analytics/events/features/apiClient";
 import { MdOutlineIosShare } from "@react-icons/all-files/md/MdOutlineIosShare";
-import { postmanCollectionExporter } from "@requestly/alternative-importers";
+import { postmanCollectionExporter } from "alternative-importers-beta";
 import { apiRecordsRankingManager } from "features/apiClient/helpers/RankingManager";
 
 interface PostmanExportModalProps {
