26f6c787a4e10ce5fefe52c288df96b1213e41f5
Teja Chitturi <teja@browserstack.com>
2026-03-02 12:47:25 +0530

    [DB-36]: Added provider modal (#4446)

--- app/src/features/settings/Secrets/Secrets.tsx
+++
@@ -1,8 +0,0 @@
-import NoProvidersEmptyState from "./NoProviderEmptyState";
-
-const Secrets = () => {
-  const showEmptyMessage = true; // Set this to false when the actual Secrets page is implemented
-  return <>{showEmptyMessage ? <NoProvidersEmptyState /> : <h1>Secrets Management Coming Soon!</h1>}</>;
-};
-
-export default Secrets;
