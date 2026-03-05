f77e9972fc6333dc15b94c5e70e20293fd47928b
rohitneharabrowserstack <rohit.ne@browserstack.com>
2026-03-05 16:08:33 +0530

    Fixed collection variable sync on cloud for empty row

--- app/src/backend/apiClient/upsertApiRecord.ts
+++ app/src/backend/apiClient/upsertApiRecord.ts
@@ -24,10 +24,12 @@ export function sanitizeRecord(record: Partial<RQAPI.ApiClientRecord>) {
       // Remove localValue from collection variables
       if (sanitizedRecord.data.variables) {
         sanitizedRecord.data.variables = Object.fromEntries(
-          Object.entries(sanitizedRecord.data.variables).map(([key, variable]) => {
-            const { localValue, ...rest } = variable;
-            return [key, rest];
-          })
+          Object.entries(sanitizedRecord.data.variables)
+            .filter(([key]) => key !== "")
+            .map(([key, variable]) => {
+              const { localValue, ...rest } = variable;
+              return [key, rest];
+            })
         );
       }
     }
