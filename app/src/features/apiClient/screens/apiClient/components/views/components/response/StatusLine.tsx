8e79b275f139ada209ffca1a9240cc3c0204b6bf
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2026-03-06 10:35:56 +0530

    [ENGG-5268]: Examples support in API Client (#4416)

--- app/src/features/apiClient/screens/apiClient/components/views/components/response/StatusLine.tsx
+++ app/src/features/apiClient/screens/apiClient/components/views/components/response/StatusLine.tsx
@@ -1,17 +1,33 @@
 import React, { useMemo } from "react";
 import { RQAPI } from "../../../../../../types";
-import { Popover, Space } from "antd";
+import { Popover, Space, Tooltip } from "antd";
 import PropertyRow from "./PropertyRow/PropertyRow";
 import { statusCodes } from "config/constants/sub/statusCode";
 import NetworkStatusField from "components/misc/NetworkStatusField";
 import { MdOutlineSwapCalls } from "@react-icons/all-files/md/MdOutlineSwapCalls";
 import { isHttpResponse } from "features/apiClient/screens/apiClient/utils";
+import { RQButton } from "lib/design-system-v2/components";
+import { MdOutlineDashboardCustomize } from "@react-icons/all-files/md/MdOutlineDashboardCustomize";
+import { BufferedGraphQLRecordEntity, BufferedHttpRecordEntity } from "features/apiClient/slices/entities";
+import { useApiClientFeatureContext } from "features/apiClient/slices";
+import { useSaveAsExample } from "features/apiClient/hooks/useSaveAsExample";
+import { useCheckLocalSyncSupport } from "features/apiClient/helpers/modules/sync/useCheckLocalSyncSupport";
 
 interface Props {
+  isDraftMode: boolean;
   response: RQAPI.Response;
+  entity: BufferedHttpRecordEntity | BufferedGraphQLRecordEntity;
 }
 
-const StatusLine: React.FC<Props> = ({ response }) => {
+const StatusLine: React.FC<Props> = ({ response, entity, isDraftMode }) => {
+  const context = useApiClientFeatureContext();
+
+  const isLocalSyncEnabled = useCheckLocalSyncSupport();
+
+  const { isSavingAsExample, handleSaveExample } = useSaveAsExample(entity);
+
+  const entityType = entity.getType(context.store.getState());
+
   const formattedTime = useMemo(() => {
     if (response?.time) {
       const ms = Math.ceil(response.time);
@@ -45,11 +61,26 @@ const StatusLine: React.FC<Props> = ({ response }) => {
         </Popover>
       )}
       <PropertyRow
-        name="Status"
+        name={`Status: ${formattedStatusText}`}
         className="api-response-status-row__status"
-        value={<NetworkStatusField status={response.status} statusText={formattedStatusText} />}
+        value={<NetworkStatusField status={response.status} />}
       />
       <PropertyRow className="api-response-status-row__time" name="Time" value={formattedTime} />
+      {entityType === RQAPI.RecordType.API && !isLocalSyncEnabled && !isDraftMode && (
+        <div className="api-response-status-row__save-button-wrapper">
+          <Tooltip title="Save the current request and response as an example." placement="bottom" color="#000">
+            <RQButton
+              size="small"
+              type="transparent"
+              icon={<MdOutlineDashboardCustomize />}
+              onClick={handleSaveExample}
+              loading={isSavingAsExample}
+            >
+              Save
+            </RQButton>
+          </Tooltip>
+        </div>
+      )}
     </Space>
   );
 };
